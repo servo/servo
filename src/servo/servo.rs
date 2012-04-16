52034ec12c950ee535c2e7c0a2c90feb5744dfba
Brian Anderson <banderson@mozilla.com>
2012-04-15 21:42:58 -0700

    Create a cairo surface from the SDL surface

--- src/servo/servo.rs
+++ src/servo/servo.rs
@@ -1,4 +1,5 @@
 import azure::cairo;
+import azure::cairo::bindgen::*;
 
 fn on_main(f: fn~()) {
     let builder = task::builder();
@@ -23,7 +24,7 @@ fn main() {
             [sdl::video::swsurface],
             [sdl::video::doublebuf]);
         assert ptr::is_not_null(screen);
-        let surface = sdl::video::create_rgb_surface(
+        let sdl_surf = sdl::video::create_rgb_surface(
             [sdl::video::swsurface],
             320, 200, 32,
             0x00FF0000u32,
@@ -31,14 +32,24 @@ fn main() {
             0x000000FFu32,
             0x00000000u32
             );
-        assert ptr::is_not_null(surface);
+        assert ptr::is_not_null(sdl_surf);
+        let cairo_surf = unsafe {
+            cairo_image_surface_create_for_data(
+                unsafe::reinterpret_cast((*sdl_surf).pixels),
+                cairo::CAIRO_FORMAT_RGB24,
+                (*sdl_surf).w,
+                (*sdl_surf).h,
+                (*sdl_surf).pitch as libc::c_int
+            )
+        };
         loop {
-            sdl::video::blit_surface(surface, ptr::null(),
+            sdl::video::blit_surface(sdl_surf, ptr::null(),
                                      screen, ptr::null());
             sdl::video::flip(screen);
             sdl::event::poll_event {|_event|
             }
         }
+        cairo_surface_destroy(cairo_surf);
         sdl::quit();
     }
 }
