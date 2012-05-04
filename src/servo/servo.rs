9103d928e67553b9d0b1da78cbb5d889624c9a22
Patrick Walton <pcwalton@mimiga.net>
2012-05-03 18:17:54 -0700

    Invoke the HTML scanner when a filename is given

--- src/servo/servo.rs
+++ src/servo/servo.rs
@@ -4,8 +4,26 @@ import azure::*;
 import azure::bindgen::*;
 import azure::cairo;
 import azure::cairo::bindgen::*;
+import parser::html;
+import parser::html::methods;
+import result::extensions;
 
-fn main() {
+fn parse(filename: str) {
+    let file_data = io::read_whole_file(filename).get();
+    let reader = io::bytes_reader(file_data);
+    let parser = html::parser(reader);
+    loop {
+        let t = parser.parse();
+        log(error, #fmt("token: %?", t));
+        if t == html::to_eof { break; }
+    }
+}
+
+fn main(args: [str]) {
+    if args.len() >= 2u {
+        parse(args[1]);
+    }
+
     // The platform event handler thread
     let osmain = osmain::osmain();
 
