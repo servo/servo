0b46922d6d64675fe60e61e5eefa548a094502a6
Brian Anderson <banderson@mozilla.com>
2012-05-03 17:14:39 -0700

    Rename lister to layout

--- src/servo/servo.rs
+++ src/servo/servo.rs
@@ -12,24 +12,20 @@ fn main() {
     // The compositor
     let renderer = gfx::renderer::renderer(osmain);
 
-    // The display list builder
-    let lister = layout::lister::lister(renderer);
-
     // The layout task
-    let layout = layout::layout::layout(lister);
+    let layout = layout::layout::layout(renderer);
 
     // The keyboard handler
     let key_po = port();
     send(osmain, osmain::add_key_handler(chan(key_po)));
 
     loop {
-        send(layout, layout::layout::do_layout);
+        send(layout, layout::layout::build);
 
         std::timer::sleep(10u);
 
         if peek(key_po) {
             comm::send(layout, layout::layout::exit);
-            comm::send(lister, layout::lister::exit);
 
             let draw_exit_confirm_po = comm::port();
             comm::send(renderer, gfx::renderer::exit(comm::chan(draw_exit_confirm_po)));
