d3f26133088a65370eaf78472f3af1ddc2f72531
Brian Anderson <banderson@mozilla.com>
2012-05-10 16:03:27 -0700

    Require autoconf 2.13

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-(cd src/mozjs/js/src && autoconf)
+# Spidermonkey requires autoconf 2.13 exactly
+
+if [ ! -z `which autoconf213` ]
+then
+    AUTOCONF=autoconf213
+fi
+
+if [ ! -z `which autoconf2.13` ]
+then
+    AUTOCONF=autoconf2.13
+fi
+
+(cd src/mozjs/js/src && $AUTOCONF)
 
 cp -f configure.in configure
