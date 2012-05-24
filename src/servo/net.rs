b281a508e066b64577c3b8950210bfd8d9b7c37d
Brian Anderson <banderson@mozilla.com>
2012-05-24 14:36:25 -0700

    Start sketching a net module

---
+++ src/servo/net.rs
@@ -0,0 +1,25 @@
+export uri, input_stream, channel, io_service, file_channel;
+
+import uri::uri;
+
+iface input_stream {
+    fn close();
+    fn read() -> [u8];
+}
+
+iface channel {
+    fn uri() -> uri;
+    fn open() -> input_stream;
+}
+
+iface io_service {
+    fn new_uri(spec: str) -> uri;
+    fn new_channel(uri: uri) -> channel;
+}
+
+class file_channel implements channel {
+    new() { }
+
+    fn uri() -> uri { fail }
+    fn open() -> input_stream { fail }
+}
---
+++ src/servo/net/uri.rs
@@ -0,0 +1,13 @@
+export uri, build_uri;
+
+type uri = {
+    spec: str,
+    scheme: str,
+    host: option<str>,
+    port: option<uint>,
+    path: str
+};
+
+fn build_uri(_spec: str) -> uri {
+    fail
+}
