d8a8a4bc3fbc709676e8cf630aa42c45e45a148a
Brian Anderson <banderson@mozilla.com>
2012-10-31 16:33:12 -0700

    Generate node names for CSS selection

--- src/servo/css/select_handler.rs
+++ src/servo/css/select_handler.rs
@@ -1,10 +1,24 @@
-use dom::node::Node;
+use dom::node::{Node, Doctype, Comment, Element, Text};
 use newcss::SelectHandler;
 
 pub struct NodeSelectHandler {
     node: Node
 }
 
+/// Placeholder names
+fn unnamed_node(name: &str) -> ~str {
+    fmt!("unnamed-%s", name)
+}
+
 impl NodeSelectHandler: SelectHandler<Node> {
-    
+    fn node_name(node: &Node) -> ~str {
+        do node.read |data| {
+            match *data.kind {
+                Doctype(*) => unnamed_node("doctype"),
+                Comment(*) => unnamed_node("comment"),
+                Element(ref data) => copy data.tag_name,
+                Text(*) => unnamed_node("text")
+            }
+        }
+    }
 }
