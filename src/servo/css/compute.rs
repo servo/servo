4ae8ca5d29d73bbce249e3ff0d263f2d31e90d6f
Brian Anderson <banderson@mozilla.com>
2012-10-31 17:39:27 -0700

    Begin working on background color

--- src/servo/css/compute.rs
+++ src/servo/css/compute.rs
@@ -1,16 +1,29 @@
 /*!
-Calculate styles for nodes based on SelectResults
+Calculate styles for Nodes based on SelectResults
 */
 
 use dom::node::Node;
 use newcss::color::{Color, rgba};
+use newcss::values::{CSSValue, Specified, Inherit};
+use newcss::ComputedStyle;
 
 pub trait ComputeStyles {
-    fn compute_background_color() -> Color;
+    fn compute_background_color(&self) -> Color;
 }
 
 impl Node: ComputeStyles {
-    fn compute_background_color() -> Color {
-        rgba(255, 0, 255, 1.0)
+    fn compute_background_color(&self) -> Color {
+        compute(self, |cs| cs.background_color(), rgba(0, 0, 0, 0.0))
+    }
+}
+
+fn compute<T>(node: &Node, get: &fn(cs: ComputedStyle) -> CSSValue<T>, default: T) -> T {
+    let style = node.get_style();
+    let computed = style.computed_style();
+    let value = get(computed);
+    match move value {
+        Inherit => /* FIXME */ move default,
+        Specified(move value) => move value,
+        _ => fail
     }
 }
