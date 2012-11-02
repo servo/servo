5dda40646ee47878b2f2264376ae717f9fa8b67d
Brian Anderson <banderson@mozilla.com>
2012-11-02 16:20:01 -0700

    Update libcss, rust-netsurfcss, rust-css

---
+++ src/servo/css/node_void_ptr.rs
@@ -0,0 +1,19 @@
+//! CSS library requires that DOM nodes be convertable to *c_void through this trait
+
+use dom::node::Node;
+
+// FIXME: Rust #3908. rust-css can't reexport VoidPtrLike
+extern mod netsurfcss;
+use netsurfcss::VoidPtrLike;
+
+impl Node: VoidPtrLike {
+    static fn from_void_ptr(node: *libc::c_void) -> Node {
+        assert node.is_not_null();
+        unsafe { cast::reinterpret_cast(&node) }
+    }
+
+    fn to_void_ptr(&self) -> *libc::c_void {
+        let node: *libc::c_void = unsafe { cast::reinterpret_cast(self) };
+        node
+    }
+}
