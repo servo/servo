cd9e0c020be5c0255ed8396f7ff863204c48e8ef
Brian J. Burg <burg@cs.washington.edu>
2012-11-06 12:04:34 -0800

    Update rust-geom and shorten geom imports.

--- src/servo/gfx/font.rs
+++ src/servo/gfx/font.rs
@@ -6,9 +6,7 @@ use azure::{
 };
 use core::dvec::DVec;
 use gfx::RenderContext;
-use geom::point::Point2D;
-use geom::rect::Rect;
-use geom::size::Size2D;
+use geom::{Point2D, Rect, Size2D};
 use glyph::{GlyphStore, GlyphIndex};
 use servo_util::range::Range;
 
