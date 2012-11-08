0f85164d138af6e9050ca91b144dc31d225a3af4
Brian J. Burg <burg@cs.washington.edu>
2012-11-08 09:44:05 -0800

    Add QuartzFontListHandle::get_available_families() stub, and update CF, CT, and IoSurface submodules.

--- src/servo/gfx/quartz/font_list.rs
+++ src/servo/gfx/quartz/font_list.rs
@@ -1,12 +1,37 @@
-pub struct QuartzFontListHandle {
-    ctx: (),
+extern mod core_foundation;
+extern mod core_text;
 
-    drop { }
+use cf = core_foundation;
+use cf::array::CFArray;
+use ct = core_text;
+use ct::font_collection::CTFontCollection;
+use ct::font_descriptor::{CTFontDescriptor, CTFontDescriptorRef};
+
+use gfx::font::FontFamily;
+
+use dvec::DVec;
+
+pub struct QuartzFontListHandle {
+    collection: CTFontCollection,
 }
 
 pub impl QuartzFontListHandle {
-    // this is a placeholder until CTFontCollection is bound here.
     static pub fn new() -> QuartzFontListHandle {
-        QuartzFontListHandle { ctx: () }
+        QuartzFontListHandle { collection: CTFontCollection::new() }
+    }
+
+    fn get_available_families(_fctx: FontContext) -> ~[@FontFamily] {
+        // TODO: make a hashtable from family name to DVec<FontEntry>
+        let descriptors : CFArray<CTFontDescriptorRef, CTFontDescriptor>;
+        descriptors = self.collection.get_descriptors();
+        for descriptors.each |desc: &CTFontDescriptor| {
+            // TODO: for each descriptor, make a FontEntry.
+            // TODO: append FontEntry to hashtable value
+        }
+
+        let families: DVec<@FontFamily> = DVec();
+        // TODO: iterate over (key,val) pairs and create FontFamily instances
+
+        return move dvec::unwrap(move families);
     }
 }
