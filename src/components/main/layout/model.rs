ef1785d0c05f00b3f160b767095b16934e7e2c51
Eric Atkinson <eatkinson@mozilla.com>
2013-05-30 10:55:42 -0700

    Add horizontal borders, margins and padding. Broken until rust-css supports padding

--- src/components/main/layout/model.rs
+++ src/components/main/layout/model.rs
@@ -19,12 +19,54 @@ use newcss::complete::CompleteStyle;
 use newcss::units::{Em, Pt, Px};
 use newcss::values::{CSSBorderWidth, CSSBorderWidthLength, CSSBorderWidthMedium};
 use newcss::values::{CSSBorderWidthThick, CSSBorderWidthThin};
-
+use newcss::values::{CSSWidth, CSSWidthLength, CSSWidthPercentage, CSSWidthAuto};
+use newcss::values::{CSSMargin, CSSMarginLength, CSSMarginPercentage, CSSMarginAuto};
+use newcss::values::{CSSPadding, CSSPaddingLength, CSSPaddingPercentage};
 /// Encapsulates the borders, padding, and margins, which we collectively call the "box model".
 pub struct BoxModel {
     border: SideOffsets2D<Au>,
     padding: SideOffsets2D<Au>,
     margin: SideOffsets2D<Au>,
+    content_width: Au,
+}
+
+/// Useful helper data type when computing values for blocks and positioned elements.
+pub enum MaybeAuto{
+    Auto,
+    Specified(Au),
+}
+
+impl MaybeAuto{
+    pub fn from_margin(margin: CSSMargin) -> MaybeAuto{
+        match margin {
+            CSSMarginAuto => Auto,
+            //FIXME(eatkinson): Compute percents properly
+            CSSMarginPercentage(_) => Specified(Au(0)),
+            //FIXME(eatkinson): Compute pt and em values properly
+            CSSMarginLength(Px(v)) | 
+            CSSMarginLength(Pt(v)) | 
+            CSSMarginLength(Em(v)) => Specified(Au::from_frac_px(v)),
+        }
+    }
+
+    pub fn from_width(width: CSSWidth) -> MaybeAuto{
+        match width{
+            CSSWidthAuto => Auto,
+            //FIXME(eatkinson): Compute percents properly
+            CSSWidthPercentage(_) => Specified(Au(0)),
+            //FIXME(eatkinson): Compute pt and em values properly
+            CSSWidthLength(Px(v)) | 
+            CSSWidthLength(Pt(v)) | 
+            CSSWidthLength(Em(v)) => Specified(Au::from_frac_px(v)),
+        }
+    }
+
+    pub fn spec_or_default(&self, default: Au) -> Au{
+        match *self{
+            Auto => default,
+            Specified(value) => value
+        }
+    }
 }
 
 impl Zero for BoxModel {
@@ -33,24 +75,31 @@ impl Zero for BoxModel {
             border: Zero::zero(),
             padding: Zero::zero(),
             margin: Zero::zero(),
+            content_width: Zero::zero(),
         }
     }
 
     fn is_zero(&self) -> bool {
-        self.padding.is_zero() && self.border.is_zero() && self.margin.is_zero()
+        self.padding.is_zero() && self.border.is_zero() && self.margin.is_zero() &&
+            self.content_width.is_zero()
     }
 }
 
 impl BoxModel {
     /// Populates the box model parameters from the given computed style.
-    pub fn populate(&mut self, style: CompleteStyle) {
-        // Populate the borders.
+    pub fn compute_borders(&mut self, style: CompleteStyle) {
+        // Compute the borders.
         self.border.top = self.compute_border_width(style.border_top_width());
         self.border.right = self.compute_border_width(style.border_right_width());
         self.border.bottom = self.compute_border_width(style.border_bottom_width());
         self.border.left = self.compute_border_width(style.border_left_width());
+    }
 
-        // TODO(pcwalton): Padding, margins.
+    pub fn compute_padding(&mut self, style: CompleteStyle, cb_width: Au){
+        self.padding.top = self.compute_padding(style.padding_top(), cb_width);
+        self.padding.right = self.compute_padding(style.padding_right(), cb_width);
+        self.padding.bottom = self.compute_padding(style.padding_bottom(), cb_width);
+        self.padding.left = self.compute_padding(style.padding_left(), cb_width);
     }
 
     /// Helper function to compute the border width in app units from the CSS border width.
@@ -65,6 +114,18 @@ impl BoxModel {
             CSSBorderWidthThin => Au::from_px(1),
             CSSBorderWidthMedium => Au::from_px(5),
             CSSBorderWidthThick => Au::from_px(10),
+        }
+    }
+
+    fn compute_padding(&self, padding: CSSPadding, cb_width: Au) -> Au{
+        match padding {
+            CSSPaddingLength(Px(v)) |
+            CSSPaddingLength(Pt(v)) |
+            CSSPaddingLength(Em(v)) => {
+                // FIXME(eatkinson): Handle 'em' and 'pt' correctly
+                Au::from_frac_px(v)
+            }
+            CSSPaddingPercentage(p) => cb_width.scale_by(p)
         }
     }
 }
