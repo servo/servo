ff1f4e62ec99100eaba7c048fcc37b70b42284de
Simon Sapin <simon.sapin@exyr.org>
2013-08-09 16:41:42 +0100

    Add a license (MPL2) and readme.

--- properties.rs
+++ properties.rs
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 use cssparser::*;
 
 
@@ -8,5 +12,3 @@ pub fn parse_property_declaration_list(input: &[Node]) -> ~[PropertyDeclaration]
     let _ = input;
     ~[]
 }
-
-
