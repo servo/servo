2ad350531a233b14c7c146fa4c5f32fc9686b013
Ilyong Cho <ilyoan@gmail.com>
2013-08-12 14:09:26 +0900

    Handle style attributes in element setter instead of html parser

--- src/test/html/color-change-text.js
+++ src/test/html/color-change-text.js
@@ -1,3 +1,4 @@
 window.setTimeout(function () {
     window.document.getElementsByTagName('div')[0].setAttribute('class', 'blue');
+    window.document.getElementsByTagName('div')[1].setAttribute('style', 'color:red;');
 }, 1000);
