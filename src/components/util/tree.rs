47c5279e3145cf47d2d61da7b98c1d7b112fde81
Ilyong Cho <ilyoan@gmail.com>
2013-09-09 15:32:07 +0900

    Add AncestorIterator by jgraham

--- src/components/util/tree.rs
+++ src/components/util/tree.rs
@@ -42,6 +42,23 @@ impl<Node, Ref: TreeNodeRef<Node>> Iterator<Ref> for ChildIterator<Ref> {
     }
 }
 
+pub struct AncestorIterator<Ref> {
+    priv current: Option<Ref>,
+}
+
+impl<Node, Ref: TreeNodeRef<Node>> Iterator<Ref> for AncestorIterator<Ref> {
+    fn next(&mut self) -> Option<Ref> {
+        if self.current.is_none() {
+            return None;
+        }
+
+        // FIXME: Do we need two clones here?
+        let x = self.current.get_ref().clone();
+        self.current = x.with_base(|n| TreeNodeRef::<Node>::parent_node(n));
+        Some(x.clone())
+    }
+}
+
 // FIXME: Do this without precomputing a vector of refs.
 // Easy for preorder; harder for postorder.
 pub struct TreeIterator<Ref> {
@@ -193,6 +210,13 @@ pub trait TreeNodeRef<Node>: Clone {
     fn children(&self) -> ChildIterator<Self> {
         ChildIterator {
             current: self.with_base(|n| get!(n, first_child)),
+        }
+    }
+
+    /// Iterates over all ancestors of this node.
+    fn ancestors(&self) -> AncestorIterator<Self> {
+        AncestorIterator {
+            current: self.with_base(|n| get!(n, parent_node)),
         }
     }
 
