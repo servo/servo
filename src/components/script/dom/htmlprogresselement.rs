d4d9395c908cec2878fc9280521572738b73c48a
Ms2ger <ms2ger@gmail.com>
2013-10-31 20:47:57 +0100

    Introduce HTMLProgressElement::new.

--- src/components/script/dom/htmlprogresselement.rs
+++ src/components/script/dom/htmlprogresselement.rs
@@ -2,11 +2,28 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+use dom::bindings::codegen::HTMLProgressElementBinding;
 use dom::bindings::utils::{ErrorResult, Fallible};
+use dom::document::AbstractDocument;
+use dom::element::HTMLProgressElementTypeId;
 use dom::htmlelement::HTMLElement;
+use dom::node::{AbstractNode, Node, ScriptView};
 
 pub struct HTMLProgressElement {
     htmlelement: HTMLElement,
+}
+
+impl HTMLProgressElement {
+    pub fn new_inherited(localName: ~str, document: AbstractDocument) -> HTMLProgressElement {
+        HTMLProgressElement {
+            htmlelement: HTMLElement::new(HTMLProgressElementTypeId, localName, document)
+        }
+    }
+
+    pub fn new(localName: ~str, document: AbstractDocument) -> AbstractNode<ScriptView> {
+        let element = HTMLProgressElement::new_inherited(localName, document);
+        Node::reflect_node(@mut element, document, HTMLProgressElementBinding::Wrap)
+    }
 }
 
 impl HTMLProgressElement {
