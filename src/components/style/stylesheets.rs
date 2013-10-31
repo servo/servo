a9295a16c53044d9d6df59d72f3ea42d9dc2d51f
S Pradeep Kumar <gohanpra@gmail.com>
2013-10-31 16:25:05 +0900

    Implement Rule Hash for Selector Matching.
    + Add index fields in Rule for parent StyleRule and parent Stylesheet to break
      ties while cascading.
      Sorting by (specificity, stylesheet index, rule index) removes the need to use stable sorts.

--- src/components/style/stylesheets.rs
+++ src/components/style/stylesheets.rs
@@ -15,6 +15,8 @@ use media_queries;
 
 
 pub struct Stylesheet {
+    /// List of rules in the order they were found (important for
+    /// cascading order)
     rules: ~[CSSRule],
     namespaces: NamespaceMap,
 }
