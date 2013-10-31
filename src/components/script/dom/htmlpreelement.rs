ec2fd955aa37a381360bd910d401bf4dbbae0c63
Ms2ger <ms2ger@gmail.com>
2013-10-31 21:00:19 +0100

    Introduce HTMLPreElement::new.

--- src/components/script/dom/htmlpreelement.rs
+++ src/components/script/dom/htmlpreelement.rs
@@ -2,11 +2,28 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+use dom::bindings::codegen::HTMLPreElementBinding;
 use dom::bindings::utils::{ErrorResult};
+use dom::document::AbstractDocument;
+use dom::element::HTMLPreElementTypeId;
 use dom::htmlelement::HTMLElement;
+use dom::node::{AbstractNode, Node, ScriptView};
 
 pub struct HTMLPreElement {
     htmlelement: HTMLElement,
+}
+
+impl HTMLPreElement {
+    pub fn new_inherited(localName: ~str, document: AbstractDocument) -> HTMLPreElement {
+        HTMLPreElement {
+            htmlelement: HTMLElement::new(HTMLPreElementTypeId, localName, document)
+        }
+    }
+
+    pub fn new(localName: ~str, document: AbstractDocument) -> AbstractNode<ScriptView> {
+        let element = HTMLPreElement::new_inherited(localName, document);
+        Node::reflect_node(@mut element, document, HTMLPreElementBinding::Wrap)
+    }
 }
 
 impl HTMLPreElement {
