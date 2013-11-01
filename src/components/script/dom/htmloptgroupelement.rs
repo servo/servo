21be4752a1640dcffa91cab93447b3d9af6ca771
Ms2ger <ms2ger@gmail.com>
2013-11-01 21:47:25 +0100

    Introduce HTMLOptGroupElement::new.

--- src/components/script/dom/htmloptgroupelement.rs
+++ src/components/script/dom/htmloptgroupelement.rs
@@ -2,11 +2,28 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+use dom::bindings::codegen::HTMLOptGroupElementBinding;
 use dom::bindings::utils::{DOMString, ErrorResult};
+use dom::document::AbstractDocument;
+use dom::element::HTMLOptGroupElementTypeId;
 use dom::htmlelement::HTMLElement;
+use dom::node::{AbstractNode, Node, ScriptView};
 
 pub struct HTMLOptGroupElement {
     htmlelement: HTMLElement
+}
+
+impl HTMLOptGroupElement {
+    pub fn new_inherited(localName: ~str, document: AbstractDocument) -> HTMLOptGroupElement {
+        HTMLOptGroupElement {
+            htmlelement: HTMLElement::new(HTMLOptGroupElementTypeId, localName, document)
+        }
+    }
+
+    pub fn new(localName: ~str, document: AbstractDocument) -> AbstractNode<ScriptView> {
+        let element = HTMLOptGroupElement::new_inherited(localName, document);
+        Node::reflect_node(@mut element, document, HTMLOptGroupElementBinding::Wrap)
+    }
 }
 
 impl HTMLOptGroupElement {
