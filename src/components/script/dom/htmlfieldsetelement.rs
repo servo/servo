f3b1cabf3288271bf170a7ab4299926c63f47dfe
Ms2ger <ms2ger@gmail.com>
2013-11-02 12:10:27 +0100

    Introduce HTMLFieldSetElement::new.

--- src/components/script/dom/htmlfieldsetelement.rs
+++ src/components/script/dom/htmlfieldsetelement.rs
@@ -2,14 +2,30 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+use dom::bindings::codegen::HTMLFieldSetElementBinding;
 use dom::bindings::utils::{DOMString, ErrorResult};
+use dom::document::AbstractDocument;
+use dom::element::HTMLFieldSetElementTypeId;
 use dom::htmlcollection::HTMLCollection;
 use dom::htmlelement::HTMLElement;
-use dom::node::{AbstractNode, ScriptView};
+use dom::node::{AbstractNode, Node, ScriptView};
 use dom::validitystate::ValidityState;
 
 pub struct HTMLFieldSetElement {
     htmlelement: HTMLElement
+}
+
+impl HTMLFieldSetElement {
+    pub fn new_inherited(localName: ~str, document: AbstractDocument) -> HTMLFieldSetElement {
+        HTMLFieldSetElement {
+            htmlelement: HTMLElement::new(HTMLFieldSetElementTypeId, localName, document)
+        }
+    }
+
+    pub fn new(localName: ~str, document: AbstractDocument) -> AbstractNode<ScriptView> {
+        let element = HTMLFieldSetElement::new_inherited(localName, document);
+        Node::reflect_node(@mut element, document, HTMLFieldSetElementBinding::Wrap)
+    }
 }
 
 impl HTMLFieldSetElement {
--- src/components/script/html/hubbub_html_parser.rs
+++ src/components/script/html/hubbub_html_parser.rs
@@ -223,7 +223,6 @@ pub fn build_element_from_tag(cx: *JSContext, tag: &str, document: AbstractDocum
     handle_element!(cx, document, tag, "div",     HTMLDivElementTypeId, HTMLDivElement, []);
     handle_element!(cx, document, tag, "dl",      HTMLDListElementTypeId, HTMLDListElement, []);
     handle_element!(cx, document, tag, "embed",   HTMLEmbedElementTypeId, HTMLEmbedElement, []);
-    handle_element!(cx, document, tag, "fieldset",HTMLFieldSetElementTypeId, HTMLFieldSetElement, []);
 
     handle_htmlelement!(cx, document, tag, "aside",   HTMLElementTypeId, HTMLElement);
     handle_htmlelement!(cx, document, tag, "b",       HTMLElementTypeId, HTMLElement);
@@ -236,6 +235,7 @@ pub fn build_element_from_tag(cx: *JSContext, tag: &str, document: AbstractDocum
     handle_newable_element!(document, tag, "col",       HTMLTableColElement);
     handle_newable_element!(document, tag, "colgroup",  HTMLTableColElement);
     handle_newable_element!(document, tag, "del",       HTMLModElement);
+    handle_newable_element!(document, tag, "fieldset",  HTMLFieldSetElement);
     handle_newable_element!(document, tag, "font",      HTMLFontElement);
     handle_newable_element!(document, tag, "form",      HTMLFormElement);
     handle_newable_element!(document, tag, "frame",     HTMLFrameElement);
