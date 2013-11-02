7290e75c6ba8b4c9c205395be085e444885c2f55
Ms2ger <ms2ger@gmail.com>
2013-11-02 12:15:51 +0100

    Introduce HTMLEmbedElement::new.

--- src/components/script/html/hubbub_html_parser.rs
+++ src/components/script/html/hubbub_html_parser.rs
@@ -222,7 +222,6 @@ pub fn build_element_from_tag(cx: *JSContext, tag: &str, document: AbstractDocum
     handle_element!(cx, document, tag, "dir",     HTMLDirectoryElementTypeId, HTMLDirectoryElement, []);
     handle_element!(cx, document, tag, "div",     HTMLDivElementTypeId, HTMLDivElement, []);
     handle_element!(cx, document, tag, "dl",      HTMLDListElementTypeId, HTMLDListElement, []);
-    handle_element!(cx, document, tag, "embed",   HTMLEmbedElementTypeId, HTMLEmbedElement, []);
 
     handle_htmlelement!(cx, document, tag, "aside",   HTMLElementTypeId, HTMLElement);
     handle_htmlelement!(cx, document, tag, "b",       HTMLElementTypeId, HTMLElement);
@@ -235,6 +234,7 @@ pub fn build_element_from_tag(cx: *JSContext, tag: &str, document: AbstractDocum
     handle_newable_element!(document, tag, "col",       HTMLTableColElement);
     handle_newable_element!(document, tag, "colgroup",  HTMLTableColElement);
     handle_newable_element!(document, tag, "del",       HTMLModElement);
+    handle_newable_element!(document, tag, "embed",     HTMLEmbedElement);
     handle_newable_element!(document, tag, "fieldset",  HTMLFieldSetElement);
     handle_newable_element!(document, tag, "font",      HTMLFontElement);
     handle_newable_element!(document, tag, "form",      HTMLFormElement);
