94937b2f2a7065b9f50062dbf054c701dac11c1c
Keegan McAllister <kmcallister@mozilla.com>
2013-11-14 14:16:21 +0900

    Pass Mac OS version cfg flags to rust-opengles build
    This was in the rust-opengles Makefile.in pre-rustpkg.

--- mk/rustpkg.mk
+++ mk/rustpkg.mk
@@ -10,6 +10,8 @@ $(1) : $$(DONE_$(1))
 
 DO_CLEAN_$(1) = rm -rf $$(DONE_$(1)) $(CFG_BUILD_HOME)/workspace/build/$(CFG_TARGET_TRIPLES)/$(1)
 
+EXTRA_RFLAGS_$(1) =
+
 clean-$(1) :
 	$$(Q) $$(DO_CLEAN_$(1))
 .PHONY : clean-$(1)
@@ -18,7 +20,7 @@ clean-$(1) :
 $$(DONE_$(1)) : $$(DONE_rust) $$(DONE_DEPS_$(1)) $$(ROUGH_DEPS_$(1))
 	$$(Q) $$(DO_CLEAN_$(1))
 	$$(Q) RUST_PATH=$(CFG_BUILD_HOME)workspace:$(subst $(SPACE),:,$(foreach submodule,$(strip $(CFG_SUBMODULES_RUSTPKG)),$(S)src/$(submodule))) \
-	$(CFG_RUSTPKG) --rust-path-hack install $(CFG_RUSTC_FLAGS) $(1)
+	$(CFG_RUSTPKG) --rust-path-hack install $(CFG_RUSTC_FLAGS) $$(EXTRA_RFLAGS_$(1)) $(1)
 
 endef
 
