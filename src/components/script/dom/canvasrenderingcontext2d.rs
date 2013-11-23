a38b24c8613dc283f12cc7a6fd6395116c158d1b
Aalhad <aalhad_22@yahoo.com>
2013-11-23 11:47:10 -0500

    Update canvasrenderingcontext2d.rs
    Fixed.

--- src/components/script/dom/canvasrenderingcontext2d.rs
+++ src/components/script/dom/canvasrenderingcontext2d.rs
@@ -1,18 +1,20 @@
-use dom::bindings::utils::{Reflectable, Reflector, reflect_dom_object};
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+use dom::bindings::utils::{DOMString, Reflectable, Reflector, reflect_dom_object};
 use dom::bindings::utils::Fallible;
 use dom::bindings::codegen::CanvasRenderingContext2DBinding;
 use dom::window::Window;
-use js::jsapi::{JSContext, JSObject};
 use dom::bindings::utils::{ErrorResult};
 
-pub struct CanvasRenderingContext2D {                         
+pub struct CanvasRenderingContext2D {
     reflector_: Reflector,
     window: @mut Window,
 }
 
-
-impl CanvasRenderingContext2D {                               
-    pub fn new_inherited(window: @mut Window) -> CanvasRenderingContext2D { 
+impl CanvasRenderingContext2D {
+    pub fn new_inherited(window: @mut Window) -> CanvasRenderingContext2D {
         CanvasRenderingContext2D {
             reflector_: Reflector::new(),
             window: window,
@@ -23,7 +25,7 @@ impl CanvasRenderingContext2D {
         reflect_dom_object(@mut CanvasRenderingContext2D::new_inherited(window), window, CanvasRenderingContext2DBinding::Wrap)
     }
 
- pub fn Width(&self) -> u32 {                    
+    pub fn Width(&self) -> u32 {                    
         0
     }
 
@@ -41,11 +43,7 @@ impl CanvasRenderingContext2D {
 
 }
 
-}
-
-
-
-impl Reflectable for CanvasRenderingContext2D {           
+impl Reflectable for CanvasRenderingContext2D {
     fn reflector<'a>(&'a self) -> &'a Reflector {
         &self.reflector_
     }
@@ -53,6 +51,4 @@ impl Reflectable for CanvasRenderingContext2D {
     fn mut_reflector<'a>(&'a mut self) -> &'a mut Reflector {
         &mut self.reflector_
     }
-
-  
 }
