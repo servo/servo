020db4118c8fa62d48fd082174bb7b563f2aed52
Lars Bergstrom <lars@lars.com>
2014-03-19 14:19:28 -0500

    Clean files associated with static linking

--- mk/clean.mk
+++ mk/clean.mk
@@ -27,29 +27,29 @@ clean-fast: $(DEPS_CLEAN_TARGETS_FAST) clean-servo
 
 clean-util:
 	@$(call E, "cleaning util")
-	$(Q)cd $(B)/src/components/util/ && rm -rf libutil*.dylib libutil*.dSYM libutil*.so $(DONE_util)
+	$(Q)cd $(B)/src/components/util/ && rm -rf libutil*.dylib libutil*.rlib libutil*.dSYM libutil*.so $(DONE_util)
 
 clean-msg:
 	@$(call E, "cleaning msg")
-	$(Q)cd $(B)/src/components/msg/ && rm -rf libmsg*.dylib libmsg*.dSYM libmsg*.so $(DONE_msg)
+	$(Q)cd $(B)/src/components/msg/ && rm -rf libmsg*.dylib libmsg*.rlib libmsg*.dSYM libmsg*.so $(DONE_msg)
 
 clean-net:
 	@$(call E, "cleaning net")
-	$(Q)cd $(B)/src/components/net/ && rm -rf libnet*.dylib libnet*.dSYM libnet*.so $(DONE_net)
+	$(Q)cd $(B)/src/components/net/ && rm -rf libnet*.dylib libnet*.rlib libnet*.dSYM libnet*.so $(DONE_net)
 
 clean-gfx:
 	@$(call E, "cleaning gfx")
-	$(Q)cd $(B)/src/components/gfx/ && rm -rf libgfx*.dylib libgfx*.dSYM libgfx*.so $(DONE_gfx)
+	$(Q)cd $(B)/src/components/gfx/ && rm -rf libgfx*.dylib libgfx*.rlib libgfx*.dSYM libgfx*.so $(DONE_gfx)
 
 clean-script:
 	@$(call E, "cleaning script")
-	$(Q)cd $(B)/src/components/script/ && rm -rf libscript*.dylib libscript*.dSYM libscript*.so $(DONE_script) && find $(S)/src/components/script/ -name \*.pyc -delete
+	$(Q)cd $(B)/src/components/script/ && rm -rf libscript*.dylib libscript*.rlib libscript*.dSYM libscript*.so $(DONE_script) && find $(S)/src/components/script/ -name \*.pyc -delete
 
 clean-style:
 	@$(call E, "cleaning style")
-	$(Q)cd $(B)/src/components/style/ && rm -rf libstyle*.dylib libstyle*.dSYM libstyle*.so $(DONE_style)
+	$(Q)cd $(B)/src/components/style/ && rm -rf libstyle*.dylib libstyle*.rlib libstyle*.dSYM libstyle*.so $(DONE_style)
 
 clean-servo: clean-gfx clean-util clean-net clean-script clean-msg clean-style
 	@$(call E, "cleaning servo")
-	$(Q)rm -f servo servo-test $(foreach lib_crate,$(SERVO_LIB_CRATES),servo-test-$(lib_crate)) libservo*.so
+	$(Q)rm -f servo servo-test $(foreach lib_crate,$(SERVO_LIB_CRATES),servo-test-$(lib_crate)) libservo*.so libservo*.a
 	$(Q)cd $(BINDINGS_SRC) && rm -f *.pkl *.rs
