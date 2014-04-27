3db1c2a6f3798c36d664b446dcf6c27dceacb410
Matt Murphy <matthew.john.murphy@gmail.com>
2014-04-27 15:49:14 -0500

    ~[] to Vec in script/script_task.rs

--- src/components/script/script_task.rs
+++ src/components/script/script_task.rs
@@ -162,7 +162,6 @@ pub struct PageTreeIterator<'a> {
 
 pub struct PageTreeIterator<'a> {
     priv stack: Vec<&'a mut PageTree>,
->>>>>>> ~[] to Vec in script/script_task.rs
 }
 
 impl PageTree {
@@ -654,7 +653,7 @@ impl ScriptTask {
     fn handle_msgs(&self) -> bool {
         // Handle pending resize events.
         // Gather them first to avoid a double mut borrow on self.
-        let mut resizes = vec!();
+        let mut resizes = Vec::new();
 
         {
             let mut page_tree = self.page_tree.borrow_mut();
@@ -676,7 +675,7 @@ impl ScriptTask {
         }
 
         // Store new resizes, and gather all other events.
-        let mut sequential = vec!();
+        let mut sequential = Vec::new();
 
         // Receive at least one message so we don't spinloop.
         let mut event = self.port.recv();
