5011253c07f02b5bd83ee5e0f382dd6cae2406b0
Ms2ger <ms2ger@gmail.com>
2014-05-03 11:16:12 +0200

    Update rust-opengles.
    This includes changes to move from ~[T] to Vec<T>.

--- src/components/main/compositing/compositor.rs
+++ src/components/main/compositing/compositor.rs
@@ -757,7 +757,7 @@ impl IOCompositor {
                 width: width as u32,
                 height: height as u32,
                 color_type: png::RGB8,
-                pixels: pixels,
+                pixels: pixels.move_iter().collect(),
             };
             let res = png::store_png(&img, &path);
             assert!(res.is_ok());
