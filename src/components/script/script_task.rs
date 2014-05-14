765cea73d94f5b2a17272a895e0d99cf239e5b81
Ms2ger <ms2ger@gmail.com>
2014-05-14 17:58:40 +0200

    Implement iframe.contentWindow.

--- src/components/script/script_task.rs
+++ src/components/script/script_task.rs
@@ -173,7 +173,7 @@ pub struct PageIterator {
     stack: Vec<Rc<Page>>,
 }
 
-trait IterablePage {
+pub trait IterablePage {
     fn iter(&self) -> PageIterator;
     fn find(&self, id: PipelineId) -> Option<Rc<Page>>;
 }
