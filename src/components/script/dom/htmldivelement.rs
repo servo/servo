d6563d01d40bc52a1518c2ddbd6bdc5bee6daebd
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2014-05-31 02:57:02 +0900

    Remove needless '&mut self' from HTMLDivElementMethods.

--- src/components/script/dom/htmldivelement.rs
+++ src/components/script/dom/htmldivelement.rs
@@ -39,7 +39,7 @@ impl HTMLDivElement {
 
 pub trait HTMLDivElementMethods {
     fn Align(&self) -> DOMString;
-    fn SetAlign(&mut self, _align: DOMString) -> ErrorResult;
+    fn SetAlign(&self, _align: DOMString) -> ErrorResult;
 }
 
 impl<'a> HTMLDivElementMethods for JSRef<'a, HTMLDivElement> {
@@ -47,7 +47,7 @@ impl<'a> HTMLDivElementMethods for JSRef<'a, HTMLDivElement> {
         "".to_owned()
     }
 
-    fn SetAlign(&mut self, _align: DOMString) -> ErrorResult {
+    fn SetAlign(&self, _align: DOMString) -> ErrorResult {
         Ok(())
     }
 }
