256173be43ecd007e1c1aef25e00f984bf6f08f3
Ms2ger <ms2ger@gmail.com>
2014-06-25 16:18:24 +0200

    Run wpt in parallel.

--- travis.osx.script.sh
+++ travis.osx.script.sh
@@ -1,3 +1,4 @@
 cd build
 ../configure
-make -j2 && make check-servo && make check-content && make check-ref-cpu && make check-wpt
+make -j2 && make check-servo && make check-content && make check-ref-cpu
+WPTARGS="--processes=4" make check-wpt
