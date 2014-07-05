b6edd5318f3fe5c751d1c011e87adbfe0cac4fcc
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2014-07-05 21:33:09 +0900

    Implement Element.matches().

--- src/components/script/dom/element.rs
+++ src/components/script/dom/element.rs
@@ -451,6 +451,7 @@ pub trait ElementMethods {
     fn QuerySelector(&self, selectors: DOMString) -> Fallible<Option<Temporary<Element>>>;
     fn QuerySelectorAll(&self, selectors: DOMString) -> Fallible<Temporary<NodeList>>;
     fn Remove(&self);
+    fn Matches(&self, selectors: DOMString) -> Fallible<bool>;
 }
 
 impl<'a> ElementMethods for JSRef<'a, Element> {
@@ -756,6 +757,12 @@ impl<'a> ElementMethods for JSRef<'a, Element> {
     fn Remove(&self) {
         let node: &JSRef<Node> = NodeCast::from_ref(self);
         node.remove_self();
+    }
+
+    // http://dom.spec.whatwg.org/#dom-element-matches
+    fn Matches(&self, selectors: DOMString) -> Fallible<bool> {
+        let root: &JSRef<Node> = NodeCast::from_ref(self);
+        root.matches(selectors)
     }
 }
 
