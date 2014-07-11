44a76e542fc7c07dfb874b3f9e6d1fdbb1609665
Ms2ger <ms2ger@gmail.com>
2014-07-11 13:21:00 +0200

    Run tidy.py on Travis.

--- travis.osx.script.sh
+++ travis.osx.script.sh
@@ -1,3 +1,3 @@
 cd build
 ../configure
-make -j2 && make check-servo && make check-content && make check-ref-cpu
+make tidy && make -j2 && make check-servo && make check-content && make check-ref-cpu
