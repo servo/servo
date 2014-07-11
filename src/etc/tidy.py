695fe128630b2280bd58c8da2811477240601f25
Ms2ger <ms2ger@gmail.com>
2014-07-11 13:06:21 +0200

    Fix PEP8 violations in tidy.py.

--- src/etc/tidy.py
+++ src/etc/tidy.py
@@ -9,28 +9,35 @@
 
 #!/usr/bin/env python
 
-import sys, os
+import os
+import sys
 from licenseck import check_license
 
 err = 0
 
+
 def report_error_name_no(name, no, s):
     global err
     print("%s:%d: %s" % (name, no, s))
-    err=1
+    err = 1
+
 
 def do_license_check(name, contents):
     if not check_license(name, contents):
         report_error_name_no(name, 1, "incorrect license")
 
+
 exceptions = [
-    "src/support", # Upstream
-    "src/platform", # Upstream
-    "src/compiler", # Upstream
-    "src/components/main/dom/bindings/codegen", # Generated and upstream code combined with our own. Could use cleanup
-    "src/components/script/dom/bindings/codegen", # Generated and upstream code combined with our own. Could use cleanup
-    "src/test/wpt/web-platform-tests", # Upstream
+    # Upstream
+    "src/support",
+    "src/platform",
+    "src/compiler",
+    "src/test/wpt/web-platform-tests",
+
+    # Generated and upstream code combined with our own. Could use cleanup
+    "src/components/script/dom/bindings/codegen",
 ]
+
 
 def should_check(name):
     if ".#" in name:
@@ -46,6 +53,7 @@ def should_check(name):
         if exception in name:
             return False
     return True
+
 
 file_names = []
 for root, dirs, files in os.walk(sys.argv[1]):
