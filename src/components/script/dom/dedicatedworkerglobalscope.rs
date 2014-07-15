7b7303e6bc4309c7f0fefbef2191d38b3759ca50
Ms2ger <ms2ger@gmail.com>
2014-07-15 16:36:53 +0200

    Add a constructor for DedicatedWorkerGlobalScope.

--- src/components/script/dom/dedicatedworkerglobalscope.rs
+++ src/components/script/dom/dedicatedworkerglobalscope.rs
@@ -2,16 +2,33 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+use dom::bindings::codegen::Bindings::DedicatedWorkerGlobalScopeBinding;
 use dom::bindings::codegen::InheritTypes::DedicatedWorkerGlobalScopeDerived;
+use dom::bindings::js::Temporary;
 use dom::bindings::utils::{Reflectable, Reflector};
 use dom::eventtarget::EventTarget;
 use dom::eventtarget::WorkerGlobalScopeTypeId;
 use dom::workerglobalscope::DedicatedGlobalScope;
 use dom::workerglobalscope::WorkerGlobalScope;
 
+use js::jsapi::JSContext;
+
 #[deriving(Encodable)]
 pub struct DedicatedWorkerGlobalScope {
     workerglobalscope: WorkerGlobalScope,
+}
+
+impl DedicatedWorkerGlobalScope {
+    pub fn new_inherited() -> DedicatedWorkerGlobalScope {
+        DedicatedWorkerGlobalScope {
+            workerglobalscope: WorkerGlobalScope::new_inherited(DedicatedGlobalScope),
+        }
+    }
+
+    pub fn new(cx: *mut JSContext) -> Temporary<DedicatedWorkerGlobalScope> {
+        let scope = box DedicatedWorkerGlobalScope::new_inherited();
+        DedicatedWorkerGlobalScopeBinding::Wrap(cx, scope)
+    }
 }
 
 pub trait DedicatedWorkerGlobalScopeMethods {
--- src/components/script/dom/workerglobalscope.rs
+++ src/components/script/dom/workerglobalscope.rs
@@ -3,7 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 use dom::bindings::utils::{Reflectable, Reflector};
-use dom::eventtarget::EventTarget;
+use dom::eventtarget::{EventTarget, WorkerGlobalScopeTypeId};
 
 #[deriving(PartialEq,Encodable)]
 pub enum WorkerGlobalScopeId {
@@ -13,6 +13,14 @@ pub enum WorkerGlobalScopeId {
 #[deriving(Encodable)]
 pub struct WorkerGlobalScope {
     pub eventtarget: EventTarget,
+}
+
+impl WorkerGlobalScope {
+    pub fn new_inherited(type_id: WorkerGlobalScopeId) -> WorkerGlobalScope {
+        WorkerGlobalScope {
+            eventtarget: EventTarget::new_inherited(WorkerGlobalScopeTypeId(type_id)),
+        }
+    }
 }
 
 pub trait WorkerGlobalScopeMethods {
