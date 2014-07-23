038ce828460822728e6467dfd33b6ef531d4fdf8
Glenn Watson <gw@intuitionlibrary.com>
2014-07-23 14:04:45 +1000

    Modify Linux travis config to run content and cpu ref tests.

---
+++ travis.linux.before_script.sh
@@ -0,0 +1,1 @@
+/usr/bin/Xorg :1 -noreset +extension GLX +extension RANDR +extension RENDER -logfile ./xorg.log -config ./src/test/ci/xorg.conf &
--- travis.linux.install.deps.sh
+++ travis.linux.install.deps.sh
@@ -1,5 +1,6 @@
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update -q
+sudo apt-get install -qq --force-yes -y xserver-xorg-input-void xserver-xorg-video-dummy xpra
 sudo apt-get install -qq --force-yes -y autoconf2.13 gperf libxxf86vm-dev libglfw-dev libstdc++6-4.7-dev
 echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
 sudo apt-get install ttf-mscorefonts-installer > /dev/null
