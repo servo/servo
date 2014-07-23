ee771e96b3f6113af9dece3649decc9d850a4856
Simon Sapin <simon.sapin@exyr.org>
2014-07-23 19:27:08 +0100

    fixup! Ignore spaces in base64 data URLs. Fix #1268.

--- src/components/net/data_loader.rs
+++ src/components/net/data_loader.rs
@@ -64,7 +64,8 @@ fn load(load_data: LoadData, start_chan: Sender<LoadResponse>) {
     let bytes = percent_decode(parts.get(1).as_bytes());
 
     if is_base64 {
-        // See #1268
+        // FIXME(#2909): It’s unclear what to do with non-alphabet characters,
+        // but Acid 3 apparently depends on spaces being ignored.
         let bytes = bytes.move_iter().filter(|&b| b != ' ' as u8).collect::<Vec<u8>>();
         // FIXME(#2877): use bytes.as_slice().from_base64() when we upgrade to a Rust version
         // that includes https://github.com/rust-lang/rust/pull/15810
