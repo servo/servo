54773d3ccdd86db6faf92c755e5fb1e88844cceb
Simon Sapin <simon.sapin@exyr.org>
2014-07-25 16:52:23 +0100

    Build docs on Travis and upload them to GitHub Pages.
    http://servo.github.io/servo/servo/index.html

--- mk/doc.mk
+++ mk/doc.mk
@@ -1,11 +1,15 @@
-# These crates make rustdoc fail for undetermined reasons.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# FIXME(#2924) These crates make rustdoc fail for undetermined reasons.
 DOC_BLACKLISTED := style layout
 
 define DEF_DOC_RULES
 .PHONY: doc-$(1)
 doc-$(1): doc/$(1)/index.html
 
-ifeq (,$(findstring $(1),$(DOC_BLACKLISTED)))
+ifeq (,$(filter $(1),$(DOC_BLACKLISTED)))
 
 doc/$(1)/index.html: $$(DEPS_$(1))
 	@$$(call E, rustdoc: $$@)
--- travis.linux.script.sh
+++ travis.linux.script.sh
@@ -1,5 +1,19 @@
+set -e
 cd build
 ../configure
 export DISPLAY=:1.0
 export RUST_TEST_TASKS=1
-make tidy && make -j2 && make check-servo && make check-content && make check-ref-cpu
+make tidy
+make -j2
+make check-servo
+make check-content
+make check-ref-cpu
+make doc
+
+if [ $TRAVIS_BRANCH = master ] && [ $TRAVIS_PULL_REQUEST = false ]
+then
+    echo '<meta http-equiv=refresh content=0;url=servo/index.html>' > doc/index.html
+    sudo pip install ghp-import
+    ghp-import -n doc
+    git push -fq https://${TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
+fi
