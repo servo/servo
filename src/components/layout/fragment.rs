2d7246996bb56cbf43f53fcade1a28e90c772e6c
Matt Brubeck <mbrubeck@limpet.net>
2014-07-29 11:21:04 -0700

    Change Box to Fragment in fragment.rs doc comment

--- src/components/layout/fragment.rs
+++ src/components/layout/fragment.rs
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-//! The `Box` type, which represents the leaves of the layout tree.
+//! The `Fragment` type, which represents the leaves of the layout tree.
 
 #![deny(unsafe_block)]
 
