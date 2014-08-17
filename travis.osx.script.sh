885a2466357e84c342303afa2d039e944ba73034
Ms2ger <ms2ger@gmail.com>
2014-08-17 10:47:27 +0200

    Split up web-platform-tests on Travis.

--- travis.osx.script.sh
+++ travis.osx.script.sh
@@ -3,6 +3,7 @@ case $1 in
 unit) make check-servo ;;
 content) make check-content ;;
 ref) make check-ref-cpu ;;
-wpt) WPTARGS="--processes=4" make check-wpt ;;
+wpt1) WPTARGS="--processes=4 --total-chunks=2 --this-chunk=1" make check-wpt ;;
+wpt2) WPTARGS="--processes=4 --total-chunks=2 --this-chunk=2" make check-wpt ;;
 *) echo "Task $1 not enabled for OSX"
 esac
