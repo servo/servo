47e939367b4ce77807f27bcab35768a8c73d0a11
Lars Bergstrom <lars@lars.com>
2014-09-12 12:27:46 -0500

    Enable optimized builds on Travis

--- etc/ci/travis.script.sh
+++ etc/ci/travis.script.sh
@@ -16,11 +16,11 @@ build_docs() {
 }
 
 build_servo() {
-    ./mach build -j 2
+    ./mach build --release -j 2
 }
 
 build_cef() {
-    ./mach build-cef -j 2
+    ./mach build-cef --release -j 2
 }
 
 IFS="," read -ra tasks <<< "${TASKS}"
@@ -37,10 +37,10 @@ for t in "${tasks[@]}"; do
 
     case $t in
         build)
-            ./mach build -j 2
+            ./mach build --release -j 2
             ;;
         build-cef)
-            ./mach build-cef -j 2
+            ./mach build-cef --release -j 2
             ;;
         test-content)
             ./mach test-content
