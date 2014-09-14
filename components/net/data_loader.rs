0b0c2ef461094b5d43a97af5d39c78f22527f8a6
Josh Matthews <josh@joshmatthews.net>
2014-09-14 11:02:33 -0400

    Fix broken build after denying unused imports.

--- components/net/data_loader.rs
+++ components/net/data_loader.rs
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-use std::str;
-
 use resource_task::{Done, Payload, Metadata, LoadData, LoadResponse, start_sending};
 
 use serialize::base64::FromBase64;
