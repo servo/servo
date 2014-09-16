2bd93ed070e852dda57faf9954af1574060b995d
Josh Matthews <josh@joshmatthews.net>
2014-09-15 23:16:45 -0400

    Delay initiating layout operations for as long as possible.

--- components/script/script_task.rs
+++ components/script/script_task.rs
@@ -693,6 +693,7 @@ impl ScriptTask {
         // Kick off the initial reflow of the page.
         debug!("kicking off initial reflow of {}", url);
         document.deref().content_changed();
+        window.flush_layout(ReflowForDisplay);
 
         let fragment = url.fragment.as_ref().map(|ref fragment| fragment.to_string());
 
@@ -722,6 +723,8 @@ impl ScriptTask {
                     Ok(_) => (),
                     Err(_) => println!("evaluate_script failed")
                 }
+
+                window.flush_layout(ReflowForDisplay);
             }
         });
 
