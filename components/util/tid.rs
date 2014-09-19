3b280ac8b0d654f11e99938904c65ce556500123
Clark Gaebel <cgaebel@mozilla.com>
2014-09-19 11:34:43 -0700

    added valid license to tid.rs

--- components/util/tid.rs
+++ components/util/tid.rs
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 use std::sync::atomics::{AtomicUint, INIT_ATOMIC_UINT, SeqCst};
 
 static mut next_tid: AtomicUint = INIT_ATOMIC_UINT;
