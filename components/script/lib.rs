a94e13f8886211b2ead8d52b3cfe43cd0c8ff998
Patrick Walton <pcwalton@mimiga.net>
2014-10-28 11:24:41 -0700

    script: Use an FNV hash to hash event listeners.
    The security properties of SipHash are irrelevant for event listeners
    and the creation of the random number generator was showing up high in
    the profiles.

--- components/script/lib.rs
+++ components/script/lib.rs
@@ -5,7 +5,7 @@
 #![comment = "The Servo Parallel Browser Project"]
 #![license = "MPL"]
 
-#![feature(globs, macro_rules, struct_variant, phase, unsafe_destructor)]
+#![feature(default_type_params, globs, macro_rules, struct_variant, phase, unsafe_destructor)]
 
 #![deny(unused_imports, unused_variable)]
 #![allow(non_snake_case)]
