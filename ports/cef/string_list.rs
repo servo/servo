caea3a87b3da913208cb2f87769b7a837819ada7
Mike Blumenkrantz <zmike@osg.samsung.com>
2014-11-07 15:35:59 -0500

    embedding: rename string_list internal function to string_list instead of string_map

--- ports/cef/string_list.rs
+++ ports/cef/string_list.rs
@@ -9,7 +9,7 @@ use string::{cef_string_userfree_utf8_alloc,cef_string_userfree_utf8_free,cef_st
 use types::{cef_string_list_t,cef_string_t};
 
 
-fn string_map_to_vec(lt: *mut cef_string_list_t) -> *mut Vec<*mut cef_string_t> {
+fn string_list_to_vec(lt: *mut cef_string_list_t) -> *mut Vec<*mut cef_string_t> {
     lt as *mut Vec<*mut cef_string_t>
 }
 
@@ -27,7 +27,7 @@ pub extern "C" fn cef_string_list_alloc() -> *mut cef_string_list_t {
 pub extern "C" fn cef_string_list_size(lt: *mut cef_string_list_t) -> c_int {
     unsafe {
         if fptr_is_null(mem::transmute(lt)) { return 0; }
-        let v = string_map_to_vec(lt);
+        let v = string_list_to_vec(lt);
         (*v).len() as c_int
     }
 }
@@ -36,7 +36,7 @@ pub extern "C" fn cef_string_list_size(lt: *mut cef_string_list_t) -> c_int {
 pub extern "C" fn cef_string_list_append(lt: *mut cef_string_list_t, value: *const cef_string_t) {
     unsafe {
         if fptr_is_null(mem::transmute(lt)) { return; }
-        let v = string_map_to_vec(lt);
+        let v = string_list_to_vec(lt);
         let cs = cef_string_userfree_utf8_alloc();
         cef_string_utf8_set(mem::transmute((*value).str), (*value).length, cs, 1);
         (*v).push(cs);
@@ -47,7 +47,7 @@ pub extern "C" fn cef_string_list_append(lt: *mut cef_string_list_t, value: *con
 pub extern "C" fn cef_string_list_value(lt: *mut cef_string_list_t, index: c_int, value: *mut cef_string_t) -> c_int {
     unsafe {
         if index < 0 || fptr_is_null(mem::transmute(lt)) { return 0; }
-        let v = string_map_to_vec(lt);
+        let v = string_list_to_vec(lt);
         if index as uint > (*v).len() - 1 { return 0; }
         let cs = (*v)[index as uint];
         cef_string_utf8_set(mem::transmute((*cs).str), (*cs).length, value, 1)
@@ -58,7 +58,7 @@ pub extern "C" fn cef_string_list_value(lt: *mut cef_string_list_t, index: c_int
 pub extern "C" fn cef_string_list_clear(lt: *mut cef_string_list_t) {
     unsafe {
         if fptr_is_null(mem::transmute(lt)) { return; }
-        let v = string_map_to_vec(lt);
+        let v = string_list_to_vec(lt);
         if (*v).len() == 0 { return; }
         let mut cs;
         while (*v).len() != 0 {
@@ -82,7 +82,7 @@ pub extern "C" fn cef_string_list_free(lt: *mut cef_string_list_t) {
 pub extern "C" fn cef_string_list_copy(lt: *mut cef_string_list_t) -> *mut cef_string_list_t {
     unsafe {
         if fptr_is_null(mem::transmute(lt)) { return 0 as *mut cef_string_list_t; }
-        let v = string_map_to_vec(lt);
+        let v = string_list_to_vec(lt);
         let lt2 = cef_string_list_alloc();
         for cs in (*v).iter() {
             cef_string_list_append(lt2, mem::transmute((*cs)));
