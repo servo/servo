608181d3435ce9bcf53d93f2c1136b46adbb5ae1
Shraddha Naik <shraddhan512@gmail.com>
2014-12-01 20:11:57 -0800

    pending indentation issues

--- components/net/ws_loader.rs
+++ components/net/ws_loader.rs
@@ -2,9 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-use resource_task::{Done, LoadResponse, LoadData, start_sending};
 use http_loader;
 use http::headers::HeaderEnum;
+use resource_task::{Done, LoadResponse, LoadData, start_sending};
 use servo_util::task::spawn_named;
 use std::ascii::AsciiExt;
 use std::comm::{channel, Sender};
@@ -22,7 +22,7 @@ fn load(load_data: LoadData, start_chan: Sender<LoadResponse>) {
     response.metadata.headers.as_ref().map(|headers| {
         let header = headers.iter().find(|h|
             h.header_name().as_slice().to_ascii_lower() == "upgrade".to_string()
-        );        
+        );
         match header {
             Some(h) => {    if h.header_value().as_slice().to_ascii_lower() == "websocket".to_string()
                             {
@@ -36,7 +36,7 @@ fn load(load_data: LoadData, start_chan: Sender<LoadResponse>) {
     let progress_chan = start_sending(start_chan, response.metadata);
     if flag == 1
     {
-       progress_chan.send(Done(Ok(()))); 
+       progress_chan.send(Done(Ok(())));
     } else {
        progress_chan.send(Done(Err("invalid upgrade header value".to_string())));
     }
