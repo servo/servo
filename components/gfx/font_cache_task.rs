9c688163b1e3ed20f43bbcd9c8224bf54467355c
Ms2ger <ms2ger@gmail.com>
2014-12-14 17:23:18 +0100

    font_cache_task

--- components/gfx/font_cache_task.rs
+++ components/gfx/font_cache_task.rs
@@ -2,6 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+use self::Command::*;
+use self::Reply::*;
+
 use platform::font_list::get_available_families;
 use platform::font_list::get_system_default_family;
 use platform::font_list::get_variations_for_family;
