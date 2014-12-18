1d68ee689f26b9318e287a5db1f5c96d2a7905ce
Ms2ger <ms2ger@gmail.com>
2014-12-18 12:30:13 +0100

    Update wptrunner and start running reftests.

--- tests/wpt/run.sh
+++ tests/wpt/run.sh
@@ -16,7 +16,7 @@ source $wpt_root/_virtualenv/bin/activate
 if [[ $* == *--update-manifest* ]]; then
     (python -c "import html5lib" &>/dev/null) || pip install html5lib
 fi
-(python -c "import wptrunner"  &>/dev/null) || pip install 'wptrunner==1.7'
+(python -c "import wptrunner"  &>/dev/null) || pip install 'wptrunner==1.8'
 
 python $wpt_root/run.py \
   --config $wpt_root/config.ini \
