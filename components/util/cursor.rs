717cb74530041cc8181c996cb077a6bf57681af6
Josh Matthews <josh@joshmatthews.net>
2014-12-18 19:43:52 -0500

    Add missing CSS properties and remove incorrect serialization deriving via Show.

--- components/util/cursor.rs
+++ components/util/cursor.rs
@@ -4,7 +4,9 @@
 
 //! A list of common mouse cursors per CSS3-UI § 8.1.1.
 
-#[deriving(Clone, PartialEq, FromPrimitive, Show)]
+use std::fmt;
+
+#[deriving(Clone, PartialEq, FromPrimitive)]
 #[repr(u8)]
 pub enum Cursor {
     NoCursor = 0,
@@ -44,3 +46,44 @@ pub enum Cursor {
     ZoomOutCursor = 34,
 }
 
+impl fmt::Show for Cursor {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Cursor::NoCursor => write!(f, "none"),
+            Cursor::DefaultCursor => write!(f, "default"),
+            Cursor::PointerCursor => write!(f, "pointer"),
+            Cursor::ContextMenuCursor => write!(f, "context-menu"),
+            Cursor::HelpCursor => write!(f, "help"),
+            Cursor::ProgressCursor => write!(f, "progress"),
+            Cursor::WaitCursor => write!(f, "wait"),
+            Cursor::CellCursor => write!(f, "cell"),
+            Cursor::CrosshairCursor => write!(f, "crosshair"),
+            Cursor::TextCursor => write!(f, "text"),
+            Cursor::VerticalTextCursor => write!(f, "vertical-text"),
+            Cursor::AliasCursor => write!(f, "alias"),
+            Cursor::CopyCursor => write!(f, "copy"),
+            Cursor::MoveCursor => write!(f, "move"),
+            Cursor::NoDropCursor => write!(f, "no-drop"),
+            Cursor::NotAllowedCursor => write!(f, "not-allowed"),
+            Cursor::GrabCursor => write!(f, "grab"),
+            Cursor::GrabbingCursor => write!(f, "grabbing"),
+            Cursor::EResizeCursor => write!(f, "e-resize"),
+            Cursor::NResizeCursor => write!(f, "n-resize"),
+            Cursor::NeResizeCursor => write!(f, "ne-resize"),
+            Cursor::NwResizeCursor => write!(f, "nw-resize"),
+            Cursor::SResizeCursor => write!(f, "s-resize"),
+            Cursor::SwResizeCursor => write!(f, "sw-resize"),
+            Cursor::SeResizeCursor => write!(f, "se-resize"),
+            Cursor::WResizeCursor => write!(f, "w-resize"),
+            Cursor::EwResizeCursor => write!(f, "ew-resize"),
+            Cursor::NsResizeCursor => write!(f, "ns-resize"),
+            Cursor::NeswResizeCursor => write!(f, "nesw-resize"),
+            Cursor::NwseResizeCursor => write!(f, "nwse-resize"),
+            Cursor::ColResizeCursor => write!(f, "col-resize"),
+            Cursor::RowResizeCursor => write!(f, "row-resize"),
+            Cursor::AllScrollCursor => write!(f, "all-scroll"),
+            Cursor::ZoomInCursor => write!(f, "zoom-in"),
+            Cursor::ZoomOutCursor => write!(f, "zoom-out"),
+        }
+    }
+}
