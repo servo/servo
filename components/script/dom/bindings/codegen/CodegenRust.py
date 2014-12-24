bdb75c2146f3519a9526d589d25ae7c2499fcc56
Matt McCoy <mattnenterprise@yahoo.com>
2014-12-24 17:16:48 -0500

    This fixes #4166. throw_dom_exception will take the GlobalRef by value, and all generated code will pass it by value.

--- components/script/dom/bindings/codegen/CodegenRust.py
+++ components/script/dom/bindings/codegen/CodegenRust.py
@@ -2215,7 +2215,7 @@ class CGCallGenerator(CGThing):
                 "    Ok(result) => result,\n"
                 "    Err(e) => {\n"
                 "%s"
-                "        throw_dom_exception(cx, &global.root_ref(), e);\n"
+                "        throw_dom_exception(cx, global.root_ref(), e);\n"
                 "        return%s;\n"
                 "    },\n"
                 "};\n" % (glob, errorResult)))
