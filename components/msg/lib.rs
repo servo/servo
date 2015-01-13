c3d23a063080ff7446af904e32ac6b661a4cd653
Martin Robinson <mrobinson@igalia.com>
2015-01-12 16:00:07 -0800

    Update to latest rust-azure and rust-layers
    We no longer need to implement from_azure_surface in Servo, now that
    rust-layers is handling more of the glue between rust-layers and Azure.

--- components/msg/lib.rs
+++ components/msg/lib.rs
@@ -21,27 +21,3 @@ extern crate io_surface;
 
 pub mod compositor_msg;
 pub mod constellation_msg;
-
-pub mod platform {
-    #[cfg(target_os="macos")]
-    pub mod macos {
-        #[cfg(target_os="macos")]
-        pub mod surface;
-    }
-
-    #[cfg(target_os="linux")]
-    pub mod linux {
-        #[cfg(target_os="linux")]
-        pub mod surface;
-    }
-
-    #[cfg(target_os="android")]
-    pub mod android {
-        #[cfg(target_os="android")]
-        pub mod surface;
-    }
-
-
-    pub mod surface;
-}
-
