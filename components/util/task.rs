be4442dc06827b953170f4a1371caf6bf6cbcfb9
Christopher Hotchkiss <christopher.hotchkiss@gmail.com>
2015-01-17 15:34:40 -0500

    Reworked the task implementation
    The public api has changed due to the removal of the green threads.

--- components/util/task.rs
+++ components/util/task.rs
@@ -2,46 +2,39 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-use std::borrow::IntoCow;
 use std::thread;
 use std::sync::mpsc::Sender;
 use std::thread::Builder;
-use std::thunk::Thunk;
-// use rtinstrument;
-use task_state;
 
-pub fn spawn_named<S: IntoCow<'static, String, str>>(name: S, f: Thunk) {
-    let builder = thread::Builder::new().named(name);
+pub fn spawn_named<F:FnOnce()+Send>(name: String, f: F) {
+    let builder = thread::Builder::new().name(name);
     builder.spawn(move || {
-        // rtinstrument::instrument(f);
-        f();
+        f()
     });
 }
 
 /// Arrange to send a particular message to a channel if the task fails.
-pub fn spawn_named_with_send_on_failure<T: Send>(name: &'static str,
-                                                 state: task_state::TaskState,
-                                                 f: Thunk,
+pub fn spawn_named_with_send_on_failure<F:FnOnce()+Send,T: Send>(name: String,
+                                                 f: F,
                                                  msg: T,
                                                  dest: Sender<T>) {
-    let future_result = Builder::new().named(name).try_future(move || {
-        task_state::initialize(state);
-        // FIXME: Find replacement for this post-runtime removal
-        // rtinstrument::instrument(f);
-        f();
+    let future_handle = thread::Builder::new().name(name).scoped(move || {
+        f()
     });
 
-    let watched_name = name.into_string();
-    let watcher_name = format!("{}Watcher", watched_name);
-    Builder::new().named(watcher_name).spawn(move || {
-        //rtinstrument::instrument(move || {
-            match future_result.into_inner() {
-                Ok(()) => (),
-                Err(..) => {
-                    debug!("{} failed, notifying constellation", name);
-                    dest.send(msg);
-                }
-            }
-        //});
+    let watcher_name = format!("{}Watcher", name);
+    Builder::new().name(watcher_name).spawn(move || {
+        let future_result = future_handle.join();
+        if(future_result.is_err()){
+            debug!("{} failed, notifying constellation", name);
+            dest.send(msg);
+        }
+    });
+}
+
+#[test]
+fn spawn_named_test(){
+    spawn_named("Test", move || {
+        debug!("I can run!");
     });
 }
