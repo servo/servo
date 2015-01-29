8d3c38c25ac77794796faf084270f9d17b25b3d4
Alexandru Cojocaru <xojoc@gmx.com>
2015-01-29 02:36:27 +0100

    replace uint/int with usize/isize inside `components/util`
    Fixes #4745

--- components/util/debug_utils.rs
+++ components/util/debug_utils.rs
@@ -2,33 +2,33 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-use std::old_io as io;
-use std::old_io::Writer;
+use std::io;
+use std::io::Writer;
 use std::mem;
 use std::mem::size_of;
 use std::slice;
 
 fn hexdump_slice(buf: &[u8]) {
     let mut stderr = io::stderr();
-    stderr.write_all(b"    ").unwrap();
+    stderr.write(b"    ").unwrap();
     for (i, &v) in buf.iter().enumerate() {
-        let output = format!("{:02X} ", v as uint);
-        stderr.write_all(output.as_bytes()).unwrap();
+        let output = format!("{:02X} ", v as usize);
+        stderr.write(output.as_bytes()).unwrap();
         match i % 16 {
-            15 => { stderr.write_all(b"\n    ").unwrap(); },
-            7 =>  { stderr.write_all(b"   ").unwrap(); },
+            15 => { stderr.write(b"\n    ").unwrap(); },
+            7 =>  { stderr.write(b"   ").unwrap(); },
              _ => ()
         }
         stderr.flush().unwrap();
     }
-    stderr.write_all(b"\n").unwrap();
+    stderr.write(b"\n").unwrap();
 }
 
 pub fn hexdump<T>(obj: &T) {
     unsafe {
         let buf: *const u8 = mem::transmute(obj);
         debug!("dumping at {:p}", buf);
-        let from_buf = slice::from_raw_parts(buf, size_of::<T>());
+        let from_buf = slice::from_raw_buf(&buf, size_of::<T>());
         hexdump_slice(from_buf);
     }
 }
