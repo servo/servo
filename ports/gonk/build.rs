a19311298881631ee96fee352675497f0bb5a4fb
Manish Goregaokar <manishsmail@gmail.com>
2015-02-06 18:16:14 +0530

    Switch to build.rs, upgrade Gonk

---
+++ ports/gonk/build.rs
@@ -0,0 +1,19 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+use std::io::process::{Command, ProcessExit, StdioContainer};
+use std::os;
+
+
+fn main() {
+    let out_dir = os::getenv("OUT_DIR").unwrap();
+    let result = Command::new("make")
+        .args(&["-f", "makefile.cargo"])
+        .stdout(StdioContainer::InheritFd(1))
+        .stderr(StdioContainer::InheritFd(2))
+        .status()
+        .unwrap();
+    assert_eq!(result, ProcessExit::ExitStatus(0));
+    println!("cargo:rustc-flags=-L native={}", out_dir);
+}
