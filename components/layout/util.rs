18b49fd68ed56c586b4d16b12a7e12ce5a50ffa8
Ms2ger <ms2ger@gmail.com>
2015-02-08 20:14:46 +0100

    Reduce duplicated code in from_thread_safe_layout_node.

--- components/layout/util.rs
+++ components/layout/util.rs
@@ -143,9 +143,7 @@ impl OpaqueNodeMethods for OpaqueNode {
 
     fn from_thread_safe_layout_node(node: &ThreadSafeLayoutNode) -> OpaqueNode {
         unsafe {
-            let abstract_node = node.get_jsmanaged();
-            let ptr: uintptr_t = abstract_node.get_jsobject() as uintptr_t;
-            OpaqueNode(ptr)
+            OpaqueNodeMethods::from_jsmanaged(node.get_jsmanaged())
         }
     }
 
