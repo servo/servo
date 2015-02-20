ddcd335e44c252bc76e931cf224d53ec82f780b5
Patrick Walton <pcwalton@mimiga.net>
2015-02-20 13:00:21 -0800

    Add `rust-media` as a dependency of script, in preparation for audio and video tag support.
    This does not actually use the library; it's just here to prevent
    bitrotting.

---
+++ components/media/lib.rs
@@ -0,0 +1,8 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+extern crate "rust-media" as rust_media;
+
+// Just a stub for now.
+
