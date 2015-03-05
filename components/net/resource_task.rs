7b3043a59bd01c77d22da3cfd0e4951820c8e972
Josh Matthews <josh@joshmatthews.net>
2015-03-05 11:15:01 -0500

    Warning fixes.

--- components/net/resource_task.rs
+++ components/net/resource_task.rs
@@ -35,7 +35,6 @@ use std::thunk::Invoke;
 static mut HOST_TABLE: Option<*mut HashMap<String, String>> = None;
 
 pub fn global_init() {
-<<<<<<< HEAD
     //TODO: handle bad file path
     let path = match env::var("HOST_FILE") {
         Ok(host_file_path) => host_file_path,
