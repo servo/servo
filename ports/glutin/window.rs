3b3fb16cd209cf19fdb5aeff0671561848c61bf7
Simon Sapin <simon.sapin@exyr.org>
2015-03-13 13:56:17 +0100

    glutin port builds

--- ports/glutin/window.rs
+++ ports/glutin/window.rs
@@ -450,7 +450,7 @@ impl WindowMethods for Window {
     }
 
     fn hidpi_factor(&self) -> ScaleFactor<ScreenPx, DevicePixel, f32> {
-        ScaleFactor(self.window.hidpi_factor())
+        ScaleFactor::new(self.window.hidpi_factor())
     }
 
     fn set_page_title(&self, title: Option<String>) {
