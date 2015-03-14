323baf92db2ae3af24c3ee3e47dd74e02f7ec8e7
Zack Slayton <zack.slayton@gmail.com>
2015-03-14 12:33:48 -0400

    localStorage shim, fixes #5195

--- components/net/storage_task.rs
+++ components/net/storage_task.rs
@@ -11,26 +11,32 @@ use url::Url;
 use util::str::DOMString;
 use util::task::spawn_named;
 
+#[derive(Copy)]
+pub enum StorageType {
+    Session,
+    Local
+}
+
 /// Request operations on the storage data associated with a particular url
 pub enum StorageTaskMsg {
     /// gets the number of key/value pairs present in the associated storage data
-    Length(Sender<u32>, Url),
+    Length(Sender<u32>, Url, StorageType),
 
     /// gets the name of the key at the specified index in the associated storage data
-    Key(Sender<Option<DOMString>>, Url, u32),
+    Key(Sender<Option<DOMString>>, Url, StorageType, u32),
 
     /// gets the value associated with the given key in the associated storage data
-    GetItem(Sender<Option<DOMString>>, Url, DOMString),
+    GetItem(Sender<Option<DOMString>>, Url, StorageType, DOMString),
 
     /// sets the value of the given key in the associated storage data
     /// TODO throw QuotaExceededError in case of error
-    SetItem(Sender<bool>, Url, DOMString, DOMString),
+    SetItem(Sender<bool>, Url, StorageType, DOMString, DOMString),
 
     /// removes the key/value pair for the given key in the associated storage data
-    RemoveItem(Sender<bool>, Url, DOMString),
+    RemoveItem(Sender<bool>, Url, StorageType, DOMString),
 
     /// clears the associated storage data by removing all the key/value pairs
-    Clear(Sender<bool>, Url),
+    Clear(Sender<bool>, Url, StorageType),
 
     /// shut down this task
     Exit
@@ -56,14 +62,16 @@ impl StorageTaskFactory for StorageTask {
 
 struct StorageManager {
     port: Receiver<StorageTaskMsg>,
-    data: HashMap<String, BTreeMap<DOMString, DOMString>>,
+    session_data: HashMap<String, BTreeMap<DOMString, DOMString>>,
+    local_data: HashMap<String, BTreeMap<DOMString, DOMString>>,
 }
 
 impl StorageManager {
     fn new(port: Receiver<StorageTaskMsg>) -> StorageManager {
         StorageManager {
             port: port,
-            data: HashMap::new(),
+            session_data: HashMap::new(),
+            local_data: HashMap::new(),
         }
     }
 }
@@ -72,23 +80,23 @@ impl StorageManager {
     fn start(&mut self) {
         loop {
             match self.port.recv().unwrap() {
-                StorageTaskMsg::Length(sender, url) => {
-                    self.length(sender, url)
+                StorageTaskMsg::Length(sender, url, storage_type) => {
+                    self.length(sender, url, storage_type)
                 }
-                StorageTaskMsg::Key(sender, url, index) => {
-                    self.key(sender, url, index)
+                StorageTaskMsg::Key(sender, url, storage_type, index) => {
+                    self.key(sender, url, storage_type, index)
                 }
-                StorageTaskMsg::SetItem(sender, url, name, value) => {
-                    self.set_item(sender, url, name, value)
+                StorageTaskMsg::SetItem(sender, url, storage_type, name, value) => {
+                    self.set_item(sender, url, storage_type, name, value)
                 }
-                StorageTaskMsg::GetItem(sender, url, name) => {
-                    self.get_item(sender, url, name)
+                StorageTaskMsg::GetItem(sender, url, storage_type, name) => {
+                    self.get_item(sender, url, storage_type, name)
                 }
-                StorageTaskMsg::RemoveItem(sender, url, name) => {
-                    self.remove_item(sender, url, name)
+                StorageTaskMsg::RemoveItem(sender, url, storage_type, name) => {
+                    self.remove_item(sender, url, storage_type, name)
                 }
-                StorageTaskMsg::Clear(sender, url) => {
-                    self.clear(sender, url)
+                StorageTaskMsg::Clear(sender, url, storage_type) => {
+                    self.clear(sender, url, storage_type)
                 }
                 StorageTaskMsg::Exit => {
                     break
@@ -97,25 +105,42 @@ impl StorageManager {
         }
     }
 
-    fn length(&self, sender: Sender<u32>, url: Url) {
-        let origin = self.get_origin_as_string(url);
-        sender.send(self.data.get(&origin).map_or(0u, |entry| entry.len()) as u32).unwrap();
+    fn select_data(& self, storage_type: StorageType) -> &HashMap<String, BTreeMap<DOMString, DOMString>> {
+        match storage_type {
+            StorageType::Session => &self.session_data,
+            StorageType::Local => &self.local_data
+        }
     }
 
-    fn key(&self, sender: Sender<Option<DOMString>>, url: Url, index: u32) {
+    fn select_data_mut(&mut self, storage_type: StorageType) -> &mut HashMap<String, BTreeMap<DOMString, DOMString>> {
+        match storage_type {
+            StorageType::Session => &mut self.session_data,
+            StorageType::Local => &mut self.local_data
+        }
+    }
+
+    fn length(&self, sender: Sender<u32>, url: Url, storage_type: StorageType) {
         let origin = self.get_origin_as_string(url);
-        sender.send(self.data.get(&origin)
+        let data = self.select_data(storage_type);
+        sender.send(data.get(&origin).map_or(0u, |entry| entry.len()) as u32).unwrap();
+    }
+
+    fn key(&self, sender: Sender<Option<DOMString>>, url: Url, storage_type: StorageType, index: u32) {
+        let origin = self.get_origin_as_string(url);
+        let data = self.select_data(storage_type);
+        sender.send(data.get(&origin)
                     .and_then(|entry| entry.keys().nth(index as uint))
                     .map(|key| key.clone())).unwrap();
     }
 
-    fn set_item(&mut self, sender: Sender<bool>, url: Url, name: DOMString, value: DOMString) {
+    fn set_item(&mut self, sender: Sender<bool>, url: Url, storage_type: StorageType, name: DOMString, value: DOMString) {
         let origin = self.get_origin_as_string(url);
-        if !self.data.contains_key(&origin) {
-            self.data.insert(origin.clone(), BTreeMap::new());
+        let data = self.select_data_mut(storage_type);
+        if !data.contains_key(&origin) {
+            data.insert(origin.clone(), BTreeMap::new());
         }
 
-        let updated = self.data.get_mut(&origin).map(|entry| {
+        let updated = data.get_mut(&origin).map(|entry| {
             if entry.get(&origin).map_or(true, |item| item.as_slice() != value.as_slice()) {
                 entry.insert(name.clone(), value.clone());
                 true
@@ -127,22 +152,25 @@ impl StorageManager {
         sender.send(updated).unwrap();
     }
 
-    fn get_item(&self, sender: Sender<Option<DOMString>>, url: Url, name: DOMString) {
+    fn get_item(&self, sender: Sender<Option<DOMString>>, url: Url, storage_type: StorageType, name: DOMString) {
         let origin = self.get_origin_as_string(url);
-        sender.send(self.data.get(&origin)
+        let data = self.select_data(storage_type);
+        sender.send(data.get(&origin)
                     .and_then(|entry| entry.get(&name))
                     .map(|value| value.to_string())).unwrap();
     }
 
-    fn remove_item(&mut self, sender: Sender<bool>, url: Url, name: DOMString) {
+    fn remove_item(&mut self, sender: Sender<bool>, url: Url, storage_type: StorageType, name: DOMString) {
         let origin = self.get_origin_as_string(url);
-        sender.send(self.data.get_mut(&origin)
+        let data = self.select_data_mut(storage_type);
+        sender.send(data.get_mut(&origin)
                     .map_or(false, |entry| entry.remove(&name).is_some())).unwrap();
     }
 
-    fn clear(&mut self, sender: Sender<bool>, url: Url) {
+    fn clear(&mut self, sender: Sender<bool>, url: Url, storage_type: StorageType) {
         let origin = self.get_origin_as_string(url);
-        sender.send(self.data.get_mut(&origin)
+        let data = self.select_data_mut(storage_type);
+        sender.send(data.get_mut(&origin)
                     .map_or(false, |entry| {
                         if !entry.is_empty() {
                             entry.clear();
