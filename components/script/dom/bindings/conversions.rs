9cd1b2c158f6a92aa48ed2702f591109b7a32eaa
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2015-03-25 01:16:08 +0900

    Use Finite<T> for our dom code (excluding CanvasRenderingContext2D)

--- components/script/dom/bindings/conversions.rs
+++ components/script/dom/bindings/conversions.rs
@@ -259,7 +259,7 @@ impl FromJSValConvertible for f32 {
 
 impl ToJSValConvertible for Finite<f32> {
     fn to_jsval(&self, cx: *mut JSContext) -> JSVal {
-        let value = self.clone().unwrap();
+        let value = **self;
         value.to_jsval(cx)
     }
 }
@@ -293,7 +293,7 @@ impl FromJSValConvertible for f64 {
 impl ToJSValConvertible for Finite<f64> {
     #[inline]
     fn to_jsval(&self, cx: *mut JSContext) -> JSVal {
-        let value = self.clone().unwrap();
+        let value = **self;
         value.to_jsval(cx)
     }
 }
