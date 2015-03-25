638ce20e6f75d3179f1b515dd0428a3d8d788f52
Ms2ger <ms2ger@gmail.com>
2015-03-25 11:18:22 +0100

    Move devtools to std::net (fixes #5355).

--- components/devtools/protocol.rs
+++ components/devtools/protocol.rs
@@ -8,42 +8,45 @@
 
 use rustc_serialize::{json, Encodable};
 use rustc_serialize::json::Json;
-use std::old_io::{IoError, OtherIoError, EndOfFile, TcpStream, IoResult};
+use std::io::{self, Read, ReadExt, Write, ErrorKind};
+use std::net::TcpStream;
 use std::num;
 
 pub trait JsonPacketStream {
     fn write_json_packet<'a, T: Encodable>(&mut self, obj: &T);
-    fn read_json_packet(&mut self) -> IoResult<Json>;
+    fn read_json_packet(&mut self) -> io::Result<Json>;
 }
 
 impl JsonPacketStream for TcpStream {
     fn write_json_packet<'a, T: Encodable>(&mut self, obj: &T) {
         let s = json::encode(obj).unwrap().replace("__type__", "type");
         println!("<- {}", s);
-        self.write_str(&s.len().to_string()).unwrap();
-        self.write_u8(':' as u8).unwrap();
-        self.write_str(&s).unwrap();
+        self.write_all(s.len().to_string().as_bytes()).unwrap();
+        self.write_all(&[':' as u8]).unwrap();
+        self.write_all(s.as_bytes()).unwrap();
     }
 
-    fn read_json_packet<'a>(&mut self) -> IoResult<Json> {
+    fn read_json_packet<'a>(&mut self) -> io::Result<Json> {
         // https://wiki.mozilla.org/Remote_Debugging_Protocol_Stream_Transport
         // In short, each JSON packet is [ascii length]:[JSON data of given length]
         let mut buffer = vec!();
         loop {
-            let colon = ':' as u8;
-            match self.read_byte() {
-                Ok(c) if c != colon => buffer.push(c as u8),
-                Ok(_) => {
+            let mut buf = [0];
+            let byte = match try!(self.read(&mut buf)) {
+                0 => return Err(io::Error::new(ErrorKind::Other, "EOF", None)),
+                1 => buf[0],
+                _ => unreachable!(),
+            };
+            match byte {
+                b':' => {
                     let packet_len_str = String::from_utf8(buffer).unwrap();
                     let packet_len = num::from_str_radix(&packet_len_str, 10).unwrap();
-                    let packet_buf = self.read_exact(packet_len).unwrap();
-                    let packet = String::from_utf8(packet_buf).unwrap();
+                    let mut packet = String::new();
+                    self.take(packet_len).read_to_string(&mut packet).unwrap();
                     println!("{}", packet);
                     return Ok(Json::from_str(&packet).unwrap())
                 },
-                Err(ref e) if e.kind == EndOfFile =>
-                    return Err(IoError { kind: EndOfFile, desc: "EOF", detail: None }),
-                _ => return Err(IoError { kind: OtherIoError, desc: "connection error", detail: None })
+                c => buffer.push(c),
             }
         }
     }
