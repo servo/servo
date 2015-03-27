8a5d5097f348713b1915240219c315e4a494e1b5
William Galliher <wpgallih@ncsu.edu>
2015-03-27 16:02:33 -0400

    Implemented ReadyState function and variables.

--- components/script/dom/websocket.rs
+++ components/script/dom/websocket.rs
@@ -12,17 +12,33 @@ use dom::eventtarget::{EventTarget, EventTargetTypeId};
 use util::str::DOMString;
 
 // https://html.spec.whatwg.org/#the-websocket-interface
+use websocket::{Message, Sender, Receiver};
+use websocket::client::request::Url;
+use websocket::Client;
+use std::cell::Cell;
+
+#[derive(PartialEq, Copy)]
+#[jstraceable]
+enum WebsocketRequestState {
+	Unsent = 0,
+	Opened = 1,
+	Sending = 2,
+	Receiving = 3,
+}
+
 #[dom_struct]
 pub struct WebSocket {
     eventtarget: EventTarget,
-    url: DOMString
+    url: DOMString,
+	ready_state: Cell<WebsocketRequestState>
 }
 
 impl WebSocket {
     pub fn new_inherited(url: DOMString) -> WebSocket {
         WebSocket {
             eventtarget: EventTarget::new_inherited(EventTargetTypeId::WebSocket),
-            url: url
+            url: url,
+		ready_state: Cell::new(WebsocketRequestState::Unsent)
         }
     }
 
@@ -40,6 +56,10 @@ impl WebSocket {
 impl<'a> WebSocketMethods for JSRef<'a, WebSocket> {
     // https://html.spec.whatwg.org/#dom-websocket-url
     fn Url(self) -> DOMString {
-        self.url.clone()
+       self.url.clone()
     }
+	
+   fn ReadyState(self) -> u16 {
+   	self.ready_state.get() as u16
+   }
 }
