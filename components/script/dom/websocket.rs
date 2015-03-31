df83b095f0dfe4c2f3a98deacff23ffad1928e58
William Galliher <wpgallih@ncsu.edu>
2015-03-30 23:11:34 -0400

    Websocket creation is occurring. Opening still needs to take place, however, and tracking where stuff stalls.

--- components/script/dom/websocket.rs
+++ components/script/dom/websocket.rs
@@ -23,6 +23,8 @@ enum WebsocketRequestState {
 	Opened = 1,
 	Sending = 2,
 	Receiving = 3,
+	Closing = 4,
+	Closed = 5,
 }
 
 #[dom_struct]
@@ -34,7 +36,8 @@ pub struct WebSocket {
 
 impl WebSocket {
     pub fn new_inherited(url: DOMString) -> WebSocket {
-        WebSocket {
+        println!("Creating websocket...");
+	WebSocket {
             eventtarget: EventTarget::new_inherited(EventTargetTypeId::WebSocket),
             url: url,
 		ready_state: Cell::new(WebsocketRequestState::Unsent)
@@ -54,6 +57,7 @@ impl WebSocket {
 
 impl<'a> WebSocketMethods for JSRef<'a, WebSocket> {
     fn Url(self) -> DOMString {
+	println!("Setting URL");
        self.url.clone()
     }
 	
