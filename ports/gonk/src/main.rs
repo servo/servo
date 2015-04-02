4f59bae10f8bb2f6eaea4a3ffed33402c7c717c0
Lars Bergstrom <lars@lars.com>
2015-04-02 13:19:06 -0500

    Add documenation from brson's cleanup changes to the original sources to these derived ones

--- ports/gonk/src/main.rs
+++ ports/gonk/src/main.rs
@@ -11,6 +11,19 @@
 // For FFI
 #![allow(non_snake_case, dead_code)]
 
+//! The `servo` test application.
+//!
+//! Creates a `Browser` instance with a simple implementation of
+//! the compositor's `WindowMethods` to create a working web browser.
+//!
+//! This browser's implementation of `WindowMethods` is built on top
+//! of [glutin], the cross-platform OpenGL utility and windowing
+//! library.
+//!
+//! For the engine itself look next door in lib.rs.
+//!
+//! [glutin]: https://github.com/tomaka/glutin
+
 extern crate servo;
 extern crate time;
 extern crate util;
@@ -41,6 +54,7 @@ struct BrowserWrapper {
 }
 
 fn main() {
+    // Parse the command line options and store them globally
     if opts::from_cmdline_args(env::args().collect::<Vec<_>>().as_slice()) {
         resource_task::global_init();
 
@@ -50,6 +64,8 @@ fn main() {
             Some(window::Window::new())
         };
 
+        // Our wrapper around `Browser` that also implements some
+        // callbacks required by the glutin window implementation.
         let mut browser = BrowserWrapper {
             browser: Browser::new(window.clone()),
         };
@@ -61,6 +77,8 @@ fn main() {
 
         browser.browser.handle_event(WindowEvent::InitializeCompositing);
 
+        // Feed events from the window to the browser until the browser
+        // says to stop.
         loop {
             let should_continue = match window {
                 None => browser.browser.handle_event(WindowEvent::Idle),
