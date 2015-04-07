e0b98a35c4f1971ec14adede1704115d3292ae48
Simon Sapin <simon.sapin@exyr.org>
2015-04-07 17:04:40 +0200

    Move gfx unit tests to the unit_tests crate.

---
+++ tests/unit/gfx/mod.rs
@@ -0,0 +1,1 @@
+mod text_util;
--- tests/unit/lib.rs
+++ tests/unit/lib.rs
@@ -1,4 +1,6 @@
 extern crate geom;
+extern crate gfx;
 extern crate util;
 
 #[cfg(test)] #[path="util/mod.rs"] mod util_tests;
+#[cfg(test)] #[path="gfx/mod.rs"] mod gfx_tests;
