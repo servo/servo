bc8b7952b80fc34a4651ecbf6551e21c391293ad
Anthony Ramine <n.oxyde@gmail.com>
2015-04-18 13:43:33 +0200

    Implement NodeHelpers::index()

--- components/script/dom/node.rs
+++ components/script/dom/node.rs
@@ -428,6 +428,7 @@ pub trait NodeHelpers {
 
     fn type_id(self) -> NodeTypeId;
     fn len(self) -> u32;
+    fn index(self) -> u32;
 
     fn parent_node(self) -> Option<Temporary<Node>>;
     fn first_child(self) -> Option<Temporary<Node>>;
@@ -577,6 +578,11 @@ impl<'a> NodeHelpers for JSRef<'a, Node> {
             },
             _ => self.children().count() as u32
         }
+    }
+
+    // https://dom.spec.whatwg.org/#concept-tree-index
+    fn index(self) -> u32 {
+        self.preceding_siblings().count() as u32
     }
 
     fn parent_node(self) -> Option<Temporary<Node>> {
