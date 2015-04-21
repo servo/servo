ce1f2bab7bdc12ed3ff2b0d96f0000a8d478586b
Simon Sapin <simon.sapin@exyr.org>
2015-04-21 18:24:11 +0200

    Fix up the splitting of the unit tests crate.
    Closes #5707. (Includes a rebase of it.)
    Fixes #5688.

--- tests/unit/gfx/lib.rs
+++ tests/unit/gfx/lib.rs
@@ -2,4 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-mod text_util;
+extern crate gfx;
+
+#[cfg(test)] mod text_util;
