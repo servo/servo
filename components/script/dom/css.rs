5a84a541e9be6ccc18f96a6dc867aacc88915dc5
Anthony Ramine <n.oxyde@gmail.com>
2015-04-21 22:54:58 +0200

    Implement the CSS interface

---
+++ components/script/dom/css.rs
@@ -0,0 +1,26 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+use dom::bindings::error::{Error, Fallible};
+use dom::bindings::utils::Reflector;
+use util::str::DOMString;
+
+use cssparser::serialize_identifier;
+
+#[dom_struct]
+pub struct CSS {
+	reflector_: Reflector,
+}
+
+impl CSS {
+    // http://dev.w3.org/csswg/cssom/#serialize-an-identifier
+    pub fn Escape(ident: DOMString) -> Fallible<DOMString> {
+        if ident.chars().any(|c| c == '\x00') {
+            return Err(Error::InvalidCharacter);
+        }
+        let mut escaped = DOMString::new();
+        serialize_identifier(&ident, &mut escaped).unwrap();
+        Ok(escaped)
+    }
+}
