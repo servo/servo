3fd6cf5035b5cc25906301f8bd7ea248739d1c0d
Michael Wu <mwu@mozilla.com>
2015-04-22 13:31:58 -0400

    Fix system includes used for stlport support
    See https://android.googlesource.com/platform/external/stlport/+/android-5.0.2_r1/libstlport.mk

--- python/servo/command_base.py
+++ python/servo/command_base.py
@@ -181,10 +181,12 @@ class CommandBase(object):
                                "-isystem %(gonkdir)s/bionic/libm/include -I%(gonkdir)s/system -I%(gonkdir)s/system/core/include "
                                "-isystem %(gonkdir)s/bionic -I%(gonkdir)s/frameworks/native/opengl/include -I%(gonkdir)s/external/zlib "
                                "-I%(gonkdir)s/hardware/libhardware/include/hardware/") % {"gonkdir": env["GONKDIR"], "archdir": env["ARCH_DIR"] }
-            env["CXXFLAGS"] = ("-O2 -mandroid -fPIC  %(cppflags)s -I%(gonkdir)s/ndk/sources/cxx-stl/stlport/stlport "
-                                "-I%(gonkdir)s/ndk/sources/cxx-stl/system/include") % {"gonkdir": env["GONKDIR"], "cppflags": env["CPPFLAGS"] }
-            env["CFLAGS"] = ("-O2 -mandroid -fPIC  %(cppflags)s -I%(gonkdir)s/ndk/sources/cxx-stl/stlport/stlport "
-                             "-I%(gonkdir)s/ndk/sources/cxx-stl/system/include") % {"gonkdir": env["GONKDIR"], "cppflags": env["CPPFLAGS"] }
+            env["CXXFLAGS"] = ("-O2 -mandroid -fPIC -isystem %(gonkdir)s/api/cpp/include -isystem %(gonkdir)s/ndk/sources/cxx-stl/stlport/stlport "
+                               "-isystem %(gonkdir)s/bionic -isystem %(gonkdir)s/bionic/libstdc++/include %(cppflags)s ") % \
+                               {"gonkdir": env["GONKDIR"], "cppflags": env["CPPFLAGS"] }
+            env["CFLAGS"] = ("-O2 -mandroid -fPIC -isystem %(gonkdir)s/api/cpp/include -isystem %(gonkdir)s/ndk/sources/cxx-stl/stlport/stlport "
+                             "-isystem %(gonkdir)s/bionic -isystem %(gonkdir)s/bionic/libstdc++/include %(cppflags)s ") % \
+                             {"gonkdir": env["GONKDIR"], "cppflags": env["CPPFLAGS"] }
 
             another_extra_path = path.join(env["GONKDIR"], "prebuilts", "gcc", "linux-x86", "arm", "arm-linux-androideabi-4.7", "bin")
             env["PATH"] = "%s%s%s" % (another_extra_path, os.pathsep, env["PATH"])
