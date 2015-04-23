1cc98f0322db3cff86e0a89dc29dad97596c593c
James Graham <james@hoppipolla.co.uk>
2015-04-23 16:03:04 +0100

    fixup! Add script execution support via WebDriver
    Add missing MPL headers

--- components/script/webdriver_handlers.rs
+++ components/script/webdriver_handlers.rs
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 use webdriver_traits::{EvaluateJSReply};
 use dom::bindings::conversions::FromJSValConvertible;
 use dom::bindings::conversions::StringificationBehavior;
