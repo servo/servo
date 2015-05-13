75275045d9da3ea1c3f334814707a84f70a9f167
Patrick Walton <pcwalton@mimiga.net>
2015-05-12 17:09:04 -0700

    script: Reflow 200 milliseconds after the `<body>` is parsed, like Gecko does.

--- components/script/dom/window.rs
+++ components/script/dom/window.rs
@@ -81,6 +81,7 @@ enum WindowState {
 #[derive(Debug)]
 pub enum ReflowReason {
     CachedPageNeededReflow,
+    RefreshTick,
     FirstLoad,
     KeyEvent,
     MouseEvent,
@@ -1042,6 +1043,7 @@ fn debug_reflow_events(goal: &ReflowGoal, query_type: &ReflowQueryType, reason: 
 
     debug_msg.push_str(match *reason {
         ReflowReason::CachedPageNeededReflow => "\tCachedPageNeededReflow",
+        ReflowReason::RefreshTick => "\tRefreshTick",
         ReflowReason::FirstLoad => "\tFirstLoad",
         ReflowReason::KeyEvent => "\tKeyEvent",
         ReflowReason::MouseEvent => "\tMouseEvent",
