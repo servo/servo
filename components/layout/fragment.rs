945adabd48e9b71f1df1b66c7ff7b432cc3188d8
Jinwoo Song <jinwoo7.song@samsung.com>
2015-05-13 14:37:17 +0900

    Add support for 'padding-box' value for 'box-sizing' property (fixes #6032)
    Spec: http://dev.w3.org/csswg/css-ui/#valdef-box-sizing-padding-box
    Currently, only Firefox is supporting this value.

--- components/layout/fragment.rs
+++ components/layout/fragment.rs
@@ -999,6 +999,10 @@ impl Fragment {
         }
     }
 
+    pub fn padding_width(&self) -> LogicalMargin<Au> {
+        self.border_padding - self.border_width()
+    }
+
     /// Computes the margins in the inline direction from the containing block inline-size and the
     /// style. After this call, the inline direction of the `margin` field will be correct.
     ///
