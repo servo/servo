707f6016177b75747e245c1298eaf944cf152415
James Gilbertson <james.gilbertson@luniv.ca>
2015-05-21 01:08:37 -0600

    Implement CSS Device Adaption § 9 (Viewport <META> element)
    Spec: http://dev.w3.org/csswg/css-device-adapt/#viewport-meta

--- tests/unit/style/lib.rs
+++ tests/unit/style/lib.rs
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#![feature(core_intrinsics)]
 #![feature(plugin)]
 #![plugin(string_cache_plugin)]
 
