0d5f87c8e1a8dc6b373844cc05c2249cea2b2a2e
r0e <rohinjpatel@gmail.com>
2015-05-22 12:44:13 -0700

    Amended code after review for issue #6148

--- components/net_traits/lib.rs
+++ components/net_traits/lib.rs
@@ -148,9 +148,7 @@ impl PendingLoadGuard {
 
 impl Drop for PendingLoadGuard {
     fn drop(&mut self) {
-        if thread::panicking() {
-            ()
-        } else {
+        if !thread::panicking() {
         assert!(self.loaded)
         }
     }
