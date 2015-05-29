08d05aef3246c939021457c7695ed670fc5332fe
Mike Blumenkrantz <zmike@osg.samsung.com>
2015-05-29 15:01:32 -0400

    implement cef_display_handler::on_favicon_urlchange
    also fixes #6166 but with more fix than last fix

--- ports/cef/window.rs
+++ ports/cef/window.rs
@@ -316,11 +316,16 @@ impl WindowMethods for Window {
         }
     }
 
-    fn set_favicon(&self, _: Url) {
+    fn set_favicon(&self, url: Url) {
+        let browser = self.cef_browser.borrow();
+        let browser = match *browser {
+            None => return,
+            Some(ref browser) => browser,
+        };
+        browser.downcast().favicons.borrow_mut().push(url.to_string().clone());
     }
 
     fn load_start(&self, back: bool, forward: bool) {
-        // FIXME(pcwalton): The status code 200 is a lie.
         let browser = self.cef_browser.borrow();
         let browser = match *browser {
             None => return,
@@ -329,6 +334,7 @@ impl WindowMethods for Window {
         browser.downcast().loading.set(true);
         browser.downcast().back.set(back);
         browser.downcast().forward.set(forward);
+        browser.downcast().favicons.borrow_mut().clear();
         if check_ptr_exist!(browser.get_host().get_client(), get_load_handler) &&
            check_ptr_exist!(browser.get_host().get_client().get_load_handler(), on_loading_state_change) {
             browser.get_host()
@@ -348,6 +354,10 @@ impl WindowMethods for Window {
         browser.downcast().loading.set(false);
         browser.downcast().back.set(back);
         browser.downcast().forward.set(forward);
+        if check_ptr_exist!(browser.get_host().get_client(), get_display_handler) &&
+           check_ptr_exist!(browser.get_host().get_client().get_display_handler(), on_favicon_urlchange) {
+            browser.get_host().get_client().get_display_handler().on_favicon_urlchange((*browser).clone(), browser.downcast().favicons.borrow().clone());
+        }
         if check_ptr_exist!(browser.get_host().get_client(), get_load_handler) &&
            check_ptr_exist!(browser.get_host().get_client().get_load_handler(), on_loading_state_change) {
             browser.get_host()
