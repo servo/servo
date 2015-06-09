1553fada53115f58c86113b43b799615f4025b08
Nicholas Nethercote <nnethercote@mozilla.com>
2015-06-08 20:38:01 -0700

    Instrument FreeType allocations.
    This will allow reporting of memory usage by FreeType instances, which
    are measured in the MiBs for LayoutWorker threads.
    
    This change also makes FreeType allocations happen with jemalloc instead
    of the system malloc, which is a good thing.
    
    Finally, the change documents some dubiousness involving
    FontContextHandle.

--- components/gfx/platform/macos/font_context.rs
+++ components/gfx/platform/macos/font_context.rs
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+use util::mem::HeapSizeOf;
+
 #[derive(Clone)]
 pub struct FontContextHandle {
     ctx: ()
@@ -11,5 +13,11 @@ impl FontContextHandle {
     // this is a placeholder until NSFontManager or whatever is bound in here.
     pub fn new() -> FontContextHandle {
         FontContextHandle { ctx: () }
+    }
+}
+
+impl HeapSizeOf for FontContextHandle {
+    fn heap_size_of_children(&self) -> usize {
+        0
     }
 }
