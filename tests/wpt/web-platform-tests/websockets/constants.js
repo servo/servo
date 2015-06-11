477bfa3675740ab3f04c9f623ee3226235af3a88
Ms2ger <ms2ger@gmail.com>
2015-06-11 20:13:45 +0200

    Remove the SCHEME_AND_DOMAIN constant.
    All its users are connecting to port 80, which is not typically under the
    control of the wpt server.

--- tests/wpt/web-platform-tests/websockets/constants.js
+++ tests/wpt/web-platform-tests/websockets/constants.js
@@ -1,18 +1,12 @@
-//This file requires server-side substitutions and must be included as constants.js?pipe=config
-var DOMAIN_FOR_WS_TESTS = "{{host}}";
-var DOMAIN_FOR_WSS_TESTS = "{{host}}";
+//This file requires server-side substitutions and must be included as constants.js?pipe=sub
 
 var PORT = "{{ports[ws][0]}}";
 //FIXME: Add support for wss
 var PORT_SSL = "{{ports[ws][0]}}";
 
-// logic for using wss URLs instead of ws
-var SCHEME_AND_DOMAIN;
 var SCHEME_DOMAIN_PORT;
 if (location.search == '?wss') {
-  SCHEME_AND_DOMAIN = 'wss://'+DOMAIN_FOR_WSS_TESTS;
-  SCHEME_DOMAIN_PORT = SCHEME_AND_DOMAIN + ":" + PORT_SSL;
+  SCHEME_DOMAIN_PORT = 'wss://{{host}}:' + PORT_SSL;
 } else {
-  SCHEME_AND_DOMAIN = 'ws://'+DOMAIN_FOR_WS_TESTS;
-  SCHEME_DOMAIN_PORT = SCHEME_AND_DOMAIN + ":" + PORT;
+  SCHEME_DOMAIN_PORT = 'ws://{{host}}:' + PORT;
 }
