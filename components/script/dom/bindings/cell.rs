baa6d109e06aa85cfd7835c2fbe102e109c3a70f
Michael Wu <mwu@mozilla.com>
2015-06-13 18:40:18 -0400

    Disable IN_GC assertion.
    This SM doesn't always tell us when it's in GC.

--- components/script/dom/bindings/cell.rs
+++ components/script/dom/bindings/cell.rs
@@ -40,7 +40,8 @@ impl<T> DOMRefCell<T> {
     /// so you have to be careful in trace code!
     #[allow(unsafe_code)]
     pub unsafe fn borrow_for_gc_trace<'a>(&'a self) -> &'a T {
-        debug_assert!(task_state::get().contains(SCRIPT | IN_GC));
+        // FIXME: IN_GC isn't reliable enough - doesn't catch minor GCs
+        //debug_assert!(task_state::get().contains(SCRIPT | IN_GC));
         &*self.value.as_unsafe_cell().get()
     }
 
