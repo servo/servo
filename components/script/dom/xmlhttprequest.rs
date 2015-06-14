9e3f25606db7da4feee7aad5978af55a3cc9389a
Michael Wu <mwu@mozilla.com>
2015-06-14 01:45:29 -0400

    Fix overlong lines

--- components/script/dom/xmlhttprequest.rs
+++ components/script/dom/xmlhttprequest.rs
@@ -686,7 +686,10 @@ impl<'a> XMLHttpRequestMethods for &'a XMLHttpRequest {
                 let decoded = UTF_8.decode(&self.response.borrow(), DecoderTrap::Replace).unwrap().to_owned();
                 let decoded: Vec<u16> = decoded.utf16_units().collect();
                 unsafe {
-                    if JS_ParseJSON(cx, decoded.as_ptr() as *const i16, decoded.len() as u32, rval.handle_mut()) == 0 {
+                    if JS_ParseJSON(cx,
+                                    decoded.as_ptr() as *const i16,
+                                    decoded.len() as u32,
+                                    rval.handle_mut()) == 0 {
                         JS_ClearPendingException(cx);
                         return NullValue();
                     }
