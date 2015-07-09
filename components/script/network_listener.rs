964c1207cc86282f154dccb9e4be9433228d0555
Patrick Walton <pcwalton@mimiga.net>
2015-07-09 16:50:06 -0700

    net: Use a thread for each `AsyncResponseTarget` to avoid having to send trait objects across process boundaries.

--- components/script/network_listener.rs
+++ components/script/network_listener.rs
@@ -3,7 +3,8 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 use script_task::{ScriptChan, ScriptMsg, Runnable};
-use net_traits::{AsyncResponseTarget, AsyncResponseListener, ResponseAction};
+use net_traits::{AsyncResponseListener, ResponseAction};
+use std::sync::mpsc::Receiver;
 use std::sync::{Arc, Mutex};
 
 /// An off-thread sink for async network event runnables. All such events are forwarded to
@@ -11,14 +12,17 @@ use std::sync::{Arc, Mutex};
 pub struct NetworkListener<T: AsyncResponseListener + PreInvoke + Send + 'static> {
     pub context: Arc<Mutex<T>>,
     pub script_chan: Box<ScriptChan+Send>,
+    pub receiver: Receiver<ResponseAction>,
 }
 
-impl<T: AsyncResponseListener + PreInvoke + Send + 'static> AsyncResponseTarget for NetworkListener<T> {
-    fn invoke_with_listener(&self, action: ResponseAction) {
-        self.script_chan.send(ScriptMsg::RunnableMsg(box ListenerRunnable {
-            context: self.context.clone(),
-            action: action,
-        })).unwrap();
+impl<T: AsyncResponseListener + PreInvoke + Send + 'static> NetworkListener<T> {
+    pub fn run(&self) {
+        while let Ok(action) = self.receiver.recv() {
+            self.script_chan.send(ScriptMsg::RunnableMsg(box ListenerRunnable {
+                context: self.context.clone(),
+                action: action,
+            })).unwrap();
+        }
     }
 }
 
