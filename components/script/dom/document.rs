f5ffcac9d9dcff32e78f7147783efdeec14bb19e
Tom Schuster <evilpies@gmail.com>
2015-07-30 00:36:29 +0200

    implement document.domain getter

--- components/script/dom/document.rs
+++ components/script/dom/document.rs
@@ -1181,6 +1181,11 @@ impl<'a> DocumentMethods for &'a Document {
         }
     }
 
+    // https://html.spec.whatwg.org/multipage/browsers.html#relaxing-the-same-origin-restriction
+    fn Domain(self) -> DOMString {
+        self.url().serialize_host().unwrap_or_else(|| "".to_owned())
+    }
+
     // https://dom.spec.whatwg.org/#dom-document-documenturi
     fn DocumentURI(self) -> DOMString {
         self.URL()
