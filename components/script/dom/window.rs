43553527b5c2c60e4a832e697c33f0226f3de795
David Zbarsky <dzbarsky@gmail.com>
2015-07-31 16:27:35 -0400

    Don't try to unwrap the result of requestAnimationFrame callback

--- components/script/dom/window.rs
+++ components/script/dom/window.rs
@@ -502,7 +502,7 @@ impl<'a> WindowMethods for &'a Window {
 
         let callback  = move |now: f64| {
             // TODO: @jdm The spec says that any exceptions should be suppressed;
-            callback.Call__(Finite::wrap(now), ExceptionHandling::Report).unwrap();
+            let _ = callback.Call__(Finite::wrap(now), ExceptionHandling::Report);
         };
 
         doc.r().request_animation_frame(Box::new(callback))
