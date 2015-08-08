b327e9b595c2d55f499b872a5ffd72835e1f3e24
Corey Farwell <coreyf@rwell.org>
2015-08-08 18:44:11 -0400

    Remove no longer needed Python dependencies
    See the parent commit for more information why these were removed
    
    tl;dr: these dependencies should now get installed by pip in a
    virtualenv

--- python/mozlog/mozlog/structured/__init__.py
+++
@@ -1,7 +0,0 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-import commandline
-import structuredlog
-from structuredlog import get_default_logger, set_default_logger
