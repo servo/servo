8dcf8bde894a6dbc3331e277b942734622757f98
Josh Matthews <josh@joshmatthews.net>
2015-08-14 09:11:11 -0400

    fixup! Fix existing syntactics nits.

--- components/gfx/platform/freetype/font_list.rs
+++ components/gfx/platform/freetype/font_list.rs
@@ -9,10 +9,10 @@ extern crate fontconfig;
 
 use fontconfig::fontconfig::{FcChar8, FcResultMatch, FcSetSystem};
 use fontconfig::fontconfig::{FcConfigGetCurrent, FcConfigGetFonts, FcConfigSubstitute};
-use fontconfig::{FcDefaultSubstitute, FcFontMatch, FcNameParse, FcPatternGetString};
-use fontconfig::{FcPatternDestroy, FcFontSetDestroy, FcMatchPattern, FcPatternCreate};
-use fontconfig::{FcPatternAddString, FcFontSetList, FcObjectSetCreate, FcObjectSetDestroy};
-use fontconfig::{FcObjectSetAdd, FcPatternGetInteger};
+use fontconfig::fontconfig::{FcDefaultSubstitute, FcFontMatch, FcNameParse, FcPatternGetString};
+use fontconfig::fontconfig::{FcPatternDestroy, FcFontSetDestroy, FcMatchPattern, FcPatternCreate};
+use fontconfig::fontconfig::{FcPatternAddString, FcFontSetList, FcObjectSetCreate, FcObjectSetDestroy};
+use fontconfig::fontconfig::{FcObjectSetAdd, FcPatternGetInteger};
 
 use util::str::c_str_to_string;
 
