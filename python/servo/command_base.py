1dab9294f481ac94d95a24c69553aa0738aaf2db
Simon Sapin <simon.sapin@exyr.org>
2015-08-17 09:19:29 +0200

    Only use a rustc wrapper script when ld.gold is available.
    Fixes #7247.

--- python/servo/command_base.py
+++ python/servo/command_base.py
@@ -11,6 +11,7 @@ import os
 from os import path
 import contextlib
 import subprocess
+from subprocess import PIPE
 import sys
 import toml
 
@@ -303,7 +304,9 @@ class CommandBase(object):
             env['HOST_FILE'] = hosts_file_path
 
         env['RUSTDOC'] = path.join(self.context.topdir, 'etc', 'rustdoc-with-private')
-        env['RUSTC'] = path.join(self.context.topdir, 'etc', 'rustc-with-gold')
+
+        if subprocess.call(['which', 'ld.gold'], stdout=PIPE, stderr=PIPE) == 0:
+            env['RUSTC'] = path.join(self.context.topdir, 'etc', 'rustc-with-gold')
 
         return env
 
