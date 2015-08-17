46c1aa755b1f3fc1f5b803903e82ef79e304a171
Corey Farwell <coreyf@rwell.org>
2015-08-17 13:12:47 -0400

    Initial implementation of ownPropertyKeys proxy handler
    Generates `SupportedPropertyNames` on DOM structs that should implement
    it. Most of them are unimplemented now (which can be implemented in
    later PRs), with the exception of `HTMLCollection`. Also added a couple
    relevant WPT tests.
    
    Closes #6390
    
    Closes #2215

--- components/script/dom/domstringmap.rs
+++ components/script/dom/domstringmap.rs
@@ -67,5 +67,10 @@ impl<'a> DOMStringMapMethods for &'a DOMStringMap {
             }
         }
     }
-}
 
+    // https://html.spec.whatwg.org/multipage/#domstringmap
+    fn SupportedPropertyNames(self) -> Vec<DOMString> {
+        // FIXME: unimplemented
+        vec![]
+    }
+}
