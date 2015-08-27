eefa7bd61a51d2ac053a40d7d181d9b17058d7d5
David Zbarsky <dzbarsky@gmail.com>
2015-08-26 22:24:09 -0700

    Allow calc for max-width and max-height

--- components/style/values.rs
+++ components/style/values.rs
@@ -913,6 +913,7 @@ pub mod specified {
     pub enum LengthOrPercentageOrNone {
         Length(Length),
         Percentage(Percentage),
+        Calc(Calc),
         None,
     }
 
@@ -921,6 +922,7 @@ pub mod specified {
             match self {
                 &LengthOrPercentageOrNone::Length(length) => length.to_css(dest),
                 &LengthOrPercentageOrNone::Percentage(percentage) => percentage.to_css(dest),
+                &LengthOrPercentageOrNone::Calc(calc) => calc.to_css(dest),
                 &LengthOrPercentageOrNone::None => dest.write_str("none"),
             }
         }
@@ -936,6 +938,10 @@ pub mod specified {
                     Ok(LengthOrPercentageOrNone::Percentage(Percentage(value.unit_value))),
                 Token::Number(ref value) if value.value == 0. =>
                     Ok(LengthOrPercentageOrNone::Length(Length::Absolute(Au(0)))),
+                Token::Function(ref name) if name.eq_ignore_ascii_case("calc") => {
+                    let calc = try!(input.parse_nested_block(Calc::parse_length_or_percentage));
+                    Ok(LengthOrPercentageOrNone::Calc(calc))
+                },
                 Token::Ident(ref value) if value.eq_ignore_ascii_case("none") =>
                     Ok(LengthOrPercentageOrNone::None),
                 _ => Err(())
@@ -1578,6 +1584,7 @@ pub mod computed {
     pub enum LengthOrPercentageOrNone {
         Length(Au),
         Percentage(CSSFloat),
+        Calc(Calc),
         None,
     }
     impl fmt::Debug for LengthOrPercentageOrNone {
@@ -1585,6 +1592,7 @@ pub mod computed {
             match self {
                 &LengthOrPercentageOrNone::Length(length) => write!(f, "{:?}", length),
                 &LengthOrPercentageOrNone::Percentage(percentage) => write!(f, "{}%", percentage * 100.),
+                &LengthOrPercentageOrNone::Calc(calc) => write!(f, "{:?}", calc),
                 &LengthOrPercentageOrNone::None => write!(f, "none"),
             }
         }
@@ -1602,6 +1610,9 @@ pub mod computed {
                 specified::LengthOrPercentageOrNone::Percentage(value) => {
                     LengthOrPercentageOrNone::Percentage(value.0)
                 }
+                specified::LengthOrPercentageOrNone::Calc(calc) => {
+                    LengthOrPercentageOrNone::Calc(calc.to_computed_value(context))
+                }
                 specified::LengthOrPercentageOrNone::None => {
                     LengthOrPercentageOrNone::None
                 }
@@ -1613,8 +1624,8 @@ pub mod computed {
         fn to_css<W>(&self, dest: &mut W) -> fmt::Result where W: fmt::Write {
             match self {
                 &LengthOrPercentageOrNone::Length(length) => length.to_css(dest),
-                &LengthOrPercentageOrNone::Percentage(percentage) =>
-                    write!(dest, "{}%", percentage * 100.),
+                &LengthOrPercentageOrNone::Percentage(percentage) => percentage.to_css(dest),
+                &LengthOrPercentageOrNone::Calc(calc) => calc.to_css(dest),
                 &LengthOrPercentageOrNone::None => dest.write_str("none"),
             }
         }
