419a26e6192a31478302c310910f1ae8f6f5e3ae
Sam Gibson <sam@ifdown.net>
2015-09-01 13:51:41 +1200

    Adds a test for set_content to ignore max_length

--- tests/unit/script/textinput.rs
+++ tests/unit/script/textinput.rs
@@ -21,6 +21,16 @@ fn text_input(lines: Lines, s: &str) -> TextInput<DummyClipboardContext> {
 }
 
 #[test]
+fn test_set_content_ignores_max_length() {
+    let mut textinput = TextInput::new(
+        Lines::Single, "".to_owned(), DummyClipboardContext::new(""), Some(1)
+    );
+
+    textinput.set_content("mozilla rocks".to_owned());
+    assert_eq!(textinput.get_content(), "mozilla rocks".to_owned());
+}
+
+#[test]
 fn test_textinput_when_inserting_multiple_lines_over_a_selection_respects_max_length() {
     let mut textinput = TextInput::new(
         Lines::Multiple,
