2dfd438f3752bdf4ff3f73ee8d73c70a02845402
Brandon Fairchild <csbit32@gmail.com>
2015-09-01 15:15:55 -0400

    python/tidy.py: Ignore macros in style nit check
    Lines with macros were flagged as errors when checking
    for space after a comma.

--- python/tidy.py
+++ python/tidy.py
@@ -199,7 +199,9 @@ def check_rust(file_name, contents):
         line = re.sub('^#[A-Za-z0-9\(\)\[\]_]*?$', '', line)
 
         match = re.search(r",[^\s]", line)
-        if match:
+        macro_match = re.search(r"[\$]", line)
+
+        if match and not macro_match:
             yield (idx + 1, "missing space after ,")
 
         if line_is_attribute(line):
