25d803d45b9ee02fe36b7530249c159ae3a0ffae
Anthony Ramine <n.oxyde@gmail.com>
2015-09-02 23:50:20 +0200

    Implement adopting and cloning steps for HTMLTemplateElement

--- components/script/dom/htmltemplateelement.rs
+++ components/script/dom/htmltemplateelement.rs
@@ -5,14 +5,17 @@
 use dom::bindings::codegen::Bindings::DocumentBinding::DocumentMethods;
 use dom::bindings::codegen::Bindings::HTMLTemplateElementBinding;
 use dom::bindings::codegen::Bindings::HTMLTemplateElementBinding::HTMLTemplateElementMethods;
-use dom::bindings::codegen::InheritTypes::HTMLTemplateElementDerived;
+use dom::bindings::codegen::Bindings::NodeBinding::NodeMethods;
+use dom::bindings::codegen::InheritTypes::{HTMLElementCast, HTMLTemplateElementCast};
+use dom::bindings::codegen::InheritTypes::{HTMLTemplateElementDerived, NodeCast};
 use dom::bindings::js::{JS, MutNullableHeap, Root};
 use dom::document::Document;
 use dom::documentfragment::DocumentFragment;
 use dom::element::ElementTypeId;
 use dom::eventtarget::{EventTarget, EventTargetTypeId};
 use dom::htmlelement::{HTMLElement, HTMLElementTypeId};
-use dom::node::{Node, NodeTypeId, document_from_node};
+use dom::node::{CloneChildrenFlag, Node, NodeTypeId, document_from_node};
+use dom::virtualmethods::VirtualMethods;
 use util::str::DOMString;
 
 #[dom_struct]
@@ -58,5 +61,39 @@ impl HTMLTemplateElementMethods for HTMLTemplateElement {
             let doc = document_from_node(self);
             doc.appropriate_template_contents_owner_document().CreateDocumentFragment()
         })
+    }
+}
+
+impl VirtualMethods for HTMLTemplateElement {
+    fn super_type(&self) -> Option<&VirtualMethods> {
+        Some(HTMLElementCast::from_ref(self) as &VirtualMethods)
+    }
+
+    /// https://html.spec.whatwg.org/multipage/#template-adopting-steps
+    fn adopting_steps(&self, old_doc: &Document) {
+        self.super_type().unwrap().adopting_steps(old_doc);
+        // Step 1.
+        let doc = document_from_node(self).appropriate_template_contents_owner_document();
+        // Step 2.
+        Node::adopt(NodeCast::from_ref(&*self.Content()), &doc);
+    }
+
+    /// https://html.spec.whatwg.org/multipage/#the-template-element:concept-node-clone-ext
+    fn cloning_steps(&self, copy: &Node, maybe_doc: Option<&Document>,
+                     clone_children: CloneChildrenFlag) {
+        self.super_type().unwrap().cloning_steps(copy, maybe_doc, clone_children);
+        if clone_children == CloneChildrenFlag::DoNotCloneChildren {
+            // Step 1.
+            return;
+        }
+        let copy = HTMLTemplateElementCast::to_ref(copy).unwrap();
+        // Steps 2-3.
+        let copy_contents = NodeCast::from_root(copy.Content());
+        let copy_contents_doc = copy_contents.owner_doc();
+        for child in NodeCast::from_root(self.Content()).children() {
+            let copy_child = Node::clone(
+                &child, Some(&copy_contents_doc), CloneChildrenFlag::CloneChildren);
+            copy_contents.AppendChild(&copy_child).unwrap();
+        }
     }
 }
