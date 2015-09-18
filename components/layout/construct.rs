40b95904fa8779ed39c4745e7d7b4d8ddb3c61d5
Simon Sapin <simon.sapin@exyr.org>
2015-09-18 15:27:08 +0200

    Disable incremental reflow for multicol and their descendants.
    Fragmentation with dynamic updates is hard.

--- components/layout/construct.rs
+++ components/layout/construct.rs
@@ -1318,6 +1318,9 @@ impl<'a, 'ln, ConcreteThreadSafeLayoutNode: ThreadSafeLayoutNode<'ln>>
             return false
         }
 
+        if node.in_fragmentation_container() {
+            return false
+        }
 
         let mut style = node.style().clone();
         let mut data = node.mutate_layout_data().unwrap();
