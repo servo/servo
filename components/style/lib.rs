e2d8d76952962594dd42bd3681501f358afdca38
Manish Goregaokar <manishsmail@gmail.com>
2015-09-21 20:14:22 +0530

    Bump serde, misc fixes

--- components/style/lib.rs
+++ components/style/lib.rs
@@ -2,7 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#![feature(arc_unique)]
 #![feature(box_syntax)]
 #![feature(box_patterns)]
 #![feature(concat_idents)]
