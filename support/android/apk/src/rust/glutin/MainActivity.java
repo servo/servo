db410f2d27ab090a4863389e24393c31fcc1dc7d
Lars Bergstrom <lars@lars.com>
2015-09-24 13:33:55 -0500

    New Android suppport

---
+++ support/android/apk/src/rust/glutin/MainActivity.java
@@ -0,0 +1,8 @@
+package rust.glutin;
+import android.util.Log;
+
+public class MainActivity extends android.app.NativeActivity {
+    static {
+        Log.i("servo_wrapper", "Loading the NativeActivity");
+    }
+}
