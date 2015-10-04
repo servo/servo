2e2e97247362d04725d987ac02e82cdf9aad0395
Corey Farwell <coreyf@rwell.org>
2015-10-04 17:17:45 -0400

    Implement deleteRow and insertRow for <table> element
    Continued from #6936

--- components/script/dom/htmltablesectionelement.rs
+++ components/script/dom/htmltablesectionelement.rs
@@ -4,16 +4,20 @@
 
 use cssparser::RGBA;
 use dom::attr::Attr;
+use dom::bindings::codegen::Bindings::HTMLCollectionBinding::HTMLCollectionMethods;
 use dom::bindings::codegen::Bindings::HTMLTableSectionElementBinding::{self, HTMLTableSectionElementMethods};
 use dom::bindings::codegen::Bindings::NodeBinding::NodeMethods;
 use dom::bindings::codegen::InheritTypes::NodeCast;
 use dom::bindings::codegen::InheritTypes::{HTMLElementCast, HTMLTableRowElementDerived, HTMLTableSectionElementDerived};
+use dom::bindings::error::Error::IndexSize;
+use dom::bindings::error::{ErrorResult, Fallible};
 use dom::bindings::js::{Root, RootedReference};
 use dom::document::Document;
 use dom::element::{AttributeMutation, Element, ElementTypeId};
 use dom::eventtarget::{EventTarget, EventTargetTypeId};
 use dom::htmlcollection::{CollectionFilter, HTMLCollection};
 use dom::htmlelement::{HTMLElement, HTMLElementTypeId};
+use dom::htmltablerowelement::HTMLTableRowElement;
 use dom::node::{Node, NodeTypeId, window_from_node};
 use dom::virtualmethods::VirtualMethods;
 use std::cell::Cell;
@@ -70,6 +74,49 @@ impl HTMLTableSectionElementMethods for HTMLTableSectionElement {
     // https://html.spec.whatwg.org/multipage/#dom-tbody-rows
     fn Rows(&self) -> Root<HTMLCollection> {
         HTMLCollection::create(&window_from_node(self), NodeCast::from_ref(self), box RowsFilter)
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-tbody-insertrow
+    fn InsertRow(&self, index: i32) -> Fallible<Root<HTMLElement>> {
+        let length = self.Rows().Length();
+
+        let index = match index {
+            index if index < -1 => return Err(IndexSize),
+            -1 => length,
+            index => index as u32,
+        };
+
+        let self_node = NodeCast::from_ref(self);
+        let tr = HTMLTableRowElement::new(String::from("tr"), None, self_node.owner_doc().r());
+
+        if index == length {
+            let tr_node = NodeCast::from_ref(tr.r());
+            try!(self_node.AppendChild(tr_node));
+        } else if let Some(element) = self.Rows().Item(index) {
+            let tr_node = NodeCast::from_ref(tr.r());
+            try!(self_node.InsertBefore(tr_node, Some(NodeCast::from_root(element)).r()));
+        } else {
+            return Err(IndexSize);
+        }
+
+        Ok(HTMLElementCast::from_root(tr))
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-tbody-deleterow
+    fn DeleteRow(&self, index: i32) -> ErrorResult {
+        let index = match index {
+            index if index < -1 => return Err(IndexSize),
+            -1 => self.Rows().Length() - 1,
+            index => index as u32,
+        };
+
+        let element = match self.Rows().Item(index) {
+            None => return Err(IndexSize),
+            Some(element) => element,
+        };
+
+        NodeCast::from_ref(element.r()).remove_self();
+        Ok(())
     }
 }
 
