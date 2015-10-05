a6b9cbf42b9c243a059b0fae70aee3517f94c522
Vladimir Vukicevic <vladimir@pobox.com>
2015-10-05 12:07:27 -0400

    win32: make sleep_microseconds for backoff just do a win32 Sleep(0)

--- components/util/workqueue.rs
+++ components/util/workqueue.rs
@@ -8,7 +8,11 @@
 //! higher-level API on top of this could allow safe fork-join parallelism.
 
 use deque::{Abort, BufferPool, Data, Empty, Stealer, Worker};
+
+#[cfg(not(windows))]
 use libc::funcs::posix88::unistd::usleep;
+#[cfg(windows)]
+extern crate kernel32;
 use rand::{Rng, weak_rng, XorShiftRng};
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::mpsc::{channel, Sender, Receiver};
@@ -92,6 +96,20 @@ fn next_power_of_two(mut v: u32) -> u32 {
     v
 }
 
+#[cfg(not(windows))]
+fn sleep_microseconds(usec: u32) -> () {
+    unsafe {
+        usleep(usec);
+    }
+}
+
+#[cfg(windows)]
+fn sleep_microseconds(usec: u32) -> () {
+    unsafe {
+        kernel32::Sleep(0);
+    }
+}
+
 impl<QueueData: Sync, WorkData: Send> WorkerThread<QueueData, WorkData> {
     /// The main logic. This function starts up the worker and listens for
     /// messages.
@@ -151,9 +169,8 @@ impl<QueueData: Sync, WorkData: Send> WorkerThread<QueueData, WorkData> {
                                     }
                                 }
 
-                                unsafe {
-                                    usleep(back_off_sleep as u32);
-                                }
+                                sleep_microseconds(back_off_sleep);
+
                                 back_off_sleep += BACKOFF_INCREMENT_IN_US;
                                 i = 0
                             } else {
