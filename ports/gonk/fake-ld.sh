a62e1c681c9a544861d7318a90eac9522f1b589f
Matt Brubeck <mbrubeck@limpet.net>
2015-10-08 21:05:53 -0700

    fixup! Don't pass -pie when linking a dylib on gonk

--- ports/gonk/fake-ld.sh
+++ ports/gonk/fake-ld.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-# Add the -pie (position-independent executable) flag.
-PIE_FLAG="-pie"
-while getopts :o: OPT; do
-  case $OPT in
-    o)
-      case $OPTARG in
-        *.so)
-          # Not an executable
-          PIE_FLAG=""
-          ;;
-      esac
-      ;;
-  esac
-done
-arm-linux-androideabi-g++ $@ $LDFLAGS $PIE_FLAG -lGLESv2 -L$GONKDIR/backup-flame/system/lib/
+# Add the position-independent executable flag if not building a shared lib.
+if echo $@ | grep -qv " -shared "
+then
+        PIE_FLAG="-pie"
+fi
+echo $PIE_FLAG
+echo arm-linux-androideabi-g++ $@ $LDFLAGS $PIE_FLAG -lGLESv2 -L$GONKDIR/backup-flame/system/lib/ >linker.log
