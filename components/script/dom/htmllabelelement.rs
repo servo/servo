5d10d773d9599d7b171685a86dc7af698e5509ba
Corey Farwell <coreyf@rwell.org>
2015-10-24 12:46:43 -0400

    Implement 'control' attribute for <label> elements

--- components/script/dom/htmllabelelement.rs
+++ components/script/dom/htmllabelelement.rs
@@ -2,13 +2,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+use dom::bindings::codegen::Bindings::DocumentBinding::DocumentMethods;
 use dom::bindings::codegen::Bindings::HTMLLabelElementBinding;
 use dom::bindings::codegen::Bindings::HTMLLabelElementBinding::HTMLLabelElementMethods;
+use dom::bindings::conversions::Castable;
 use dom::bindings::js::Root;
 use dom::document::Document;
+use dom::element::Element;
 use dom::htmlelement::HTMLElement;
 use dom::htmlformelement::{FormControl, HTMLFormElement};
-use dom::node::Node;
+use dom::node::{document_from_node, Node};
 use util::str::DOMString;
 
 #[dom_struct]
@@ -39,6 +42,30 @@ impl HTMLLabelElementMethods for HTMLLabelElement {
     // https://html.spec.whatwg.org/multipage/#dom-fae-form
     fn GetForm(&self) -> Option<Root<HTMLFormElement>> {
         self.form_owner()
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-label-control
+    fn GetControl(&self) -> Option<Root<HTMLElement>> {
+        if !self.upcast::<Element>().has_attribute(&atom!("for")) {
+            return self.first_labelable_descendant();
+        }
+
+        let for_ = self.upcast::<Element>().get_string_attribute(&atom!("for"));
+        document_from_node(self).GetElementById(for_)
+                                .and_then(Root::downcast::<HTMLElement>)
+                                .into_iter()
+                                .filter(|e| e.is_labelable_element())
+                                .next()
+    }
+}
+
+impl HTMLLabelElement {
+    fn first_labelable_descendant(&self) -> Option<Root<HTMLElement>> {
+        self.upcast::<Node>()
+            .traverse_preorder()
+            .filter_map(Root::downcast::<HTMLElement>)
+            .filter(|elem| elem.is_labelable_element())
+            .next()
     }
 }
 
