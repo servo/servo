497e25765629b93bfaee2f4f87e233d7c524dab2
Shashwath <shashwath@localhost.localdomain>
2015-10-26 09:38:20 -0400

    tidy correction parser.rs

--- components/style/parser.rs
+++ components/style/parser.rs
@@ -18,8 +18,7 @@ pub struct ParserContext<'a> {
 }
 
 impl<'a> ParserContext<'a> {
-    pub fn new(stylesheet_origin: Origin, base_url: &'a Url, error_reporter: &'a (ParseErrorReporter + 'a)) 
-               -> ParserContext<'a> {
+pub fn new(stylesheet_origin: Origin, base_url:&'a Url, error_reporter:&'a(ParseErrorReporter + 'a))->ParserContext<'a>{
         let mut selector_context = SelectorParserContext::new();
         selector_context.in_user_agent_stylesheet = stylesheet_origin == Origin::UserAgent;
         ParserContext {
