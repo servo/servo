b50f9f2941a732d4ceb880283bb1abe50549c7e7
jsharda <jsharda@ncsu.edu>
2015-10-30 17:17:59 -0400

    added servoxmlparser with necessary stubs

---
+++ components/script/dom/servoxmlparser.rs
@@ -0,0 +1,29 @@
+
+use dom::bindings::utils::{Reflectable,Reflector};
+use dom::bindings::trace::JSTraceable;
+use util::mem::HeapSizeOf;
+use js::jsapi::JSTracer;
+
+#[derive(HeapSizeOf)]
+#[must_root]
+pub struct ServoXMLParser{
+	reflector_: Reflector,
+}
+
+impl ServoXMLParser {
+pub fn new(){
+       
+}
+}
+
+impl Reflectable for ServoXMLParser{
+	fn reflector<'a>(&'a self) -> &'a Reflector{
+		&self.reflector_
+	}
+}
+
+impl JSTraceable for ServoXMLParser{
+	fn trace(&self, trc: *mut JSTracer){
+	}
+}
+
