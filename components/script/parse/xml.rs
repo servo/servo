c45f78be84d5fa8853fd4d4944230f148a915127
jsharda <jsharda@ncsu.edu>
2015-10-30 16:50:15 -0400

    M1503 - Integrate XML parse -Initial Steps

---
+++ components/script/parse/xml.rs
@@ -0,0 +1,19 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#![allow(unrooted_must_root)]
+
+use dom::document::Document;
+use url::Url;
+
+pub enum ParseContext {
+    Owner(Option<i32>)
+}
+
+
+pub fn parse_xml(document: &Document,
+                 input: String,
+                 url: Url,
+                 context: ParseContext) {
+}
