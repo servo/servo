85d486de78d4293a431f695e3834099304628d80
Josh Matthews <josh@joshmatthews.net>
2015-11-06 16:28:31 -0600

    Stub out readyState and networkState attributes for HTMLMediaElement.

--- components/script/dom/htmlmediaelement.rs
+++ components/script/dom/htmlmediaelement.rs
@@ -2,13 +2,18 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+use dom::bindings::codegen::Bindings::HTMLMediaElementBinding::HTMLMediaElementMethods;
+use dom::bindings::codegen::Bindings::HTMLMediaElementBinding::HTMLMediaElementConstants;
 use dom::document::Document;
 use dom::htmlelement::HTMLElement;
+use std::cell::Cell;
 use util::str::DOMString;
 
 #[dom_struct]
 pub struct HTMLMediaElement {
     htmlelement: HTMLElement,
+    network_state: Cell<u16>,
+    ready_state: Cell<u16>,
 }
 
 impl HTMLMediaElement {
@@ -17,12 +22,24 @@ impl HTMLMediaElement {
                          -> HTMLMediaElement {
         HTMLMediaElement {
             htmlelement:
-                HTMLElement::new_inherited(tag_name, prefix, document)
+                HTMLElement::new_inherited(tag_name, prefix, document),
+            network_state: Cell::new(HTMLMediaElementConstants::NETWORK_EMPTY),
+            ready_state: Cell::new(HTMLMediaElementConstants::HAVE_NOTHING),
         }
     }
 
     #[inline]
     pub fn htmlelement(&self) -> &HTMLElement {
         &self.htmlelement
+    }
+}
+
+impl HTMLMediaElementMethods for HTMLMediaElement {
+    fn NetworkState(&self) -> u16 {
+        self.network_state.get()
+    }
+
+    fn ReadyState(&self) -> u16 {
+        self.ready_state.get()
     }
 }
