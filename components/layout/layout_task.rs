0c7c6bacf7bb907597c8c0791177e80230d641fd
Ms2ger <Ms2ger@gmail.com>
2015-11-06 18:22:24 +0100

    Move stylist from LayoutTaskData to LayoutTask.

--- components/layout/layout_task.rs
+++ components/layout/layout_task.rs
@@ -108,9 +108,6 @@ pub struct LayoutTaskData {
     /// The root stacking context.
     pub stacking_context: Option<Arc<StackingContext>>,
 
-    /// Performs CSS selector matching and style resolution.
-    pub stylist: Box<Stylist>,
-
     /// A queued response for the union of the content boxes of a node.
     pub content_box_response: Rect<Au>,
 
@@ -222,6 +219,9 @@ pub struct LayoutTask {
     /// constraints.
     viewport_size: Size2D<Au>,
 
+    /// Performs CSS selector matching and style resolution.
+    stylist: Box<Stylist>,
+
     /// A mutex to allow for fast, read-only RPC of layout's internal data
     /// structures, while still letting the LayoutTask modify them.
     ///
@@ -329,6 +329,7 @@ impl<'a, 'b: 'a> RwData<'a, 'b> {
     /// If no reflow has ever been triggered, this will keep the lock, locked
     /// (and saved in `possibly_locked_rw_data`). If it has been, the lock will
     /// be unlocked.
+    #[allow(dead_code)]
     fn block(&mut self, rw_data: RWGuard<'b>) {
         match rw_data {
             RWGuard::Used(x) => drop(x),
@@ -434,12 +435,12 @@ impl LayoutTask {
             running_animations: Arc::new(HashMap::new()),
             epoch: Epoch(0),
             viewport_size: screen_size,
+            stylist: stylist,
             rw_data: Arc::new(Mutex::new(
                 LayoutTaskData {
                     constellation_chan: constellation_chan,
                     screen_size: screen_size,
                     stacking_context: None,
-                    stylist: stylist,
                     content_box_response: Rect::zero(),
                     content_boxes_response: Vec::new(),
                     client_rect_response: Rect::zero(),
@@ -478,7 +479,7 @@ impl LayoutTask {
             layout_chan: self.chan.clone(),
             font_cache_task: self.font_cache_task.clone(),
             canvas_layers_sender: self.canvas_layers_sender.clone(),
-            stylist: &*rw_data.stylist,
+            stylist: &*self.stylist,
             url: (*url).clone(),
             visible_rects: self.visible_rects.clone(),
             generation: self.generation,
@@ -747,7 +748,7 @@ impl LayoutTask {
         response_port.recv().unwrap()
     }
 
-    fn handle_load_stylesheet<'a, 'b>(&self,
+    fn handle_load_stylesheet<'a, 'b>(&mut self,
                                       url: Url,
                                       mq: MediaQueryList,
                                       pending: PendingAsyncLoad,
@@ -775,42 +776,35 @@ impl LayoutTask {
         self.handle_add_stylesheet(sheet, mq, possibly_locked_rw_data);
     }
 
-    fn handle_add_stylesheet<'a, 'b>(&self,
+    fn handle_add_stylesheet<'a, 'b>(&mut self,
                                      sheet: Stylesheet,
                                      mq: MediaQueryList,
-                                     possibly_locked_rw_data: &mut RwData<'a, 'b>) {
+                                     _: &mut RwData<'a, 'b>) {
         // Find all font-face rules and notify the font cache of them.
         // GWTODO: Need to handle unloading web fonts (when we handle unloading stylesheets!)
 
-        let mut rw_data = possibly_locked_rw_data.lock();
-        if mq.evaluate(&rw_data.stylist.device) {
+        if mq.evaluate(&self.stylist.device) {
             add_font_face_rules(&sheet,
-                                &rw_data.stylist.device,
+                                &self.stylist.device,
                                 &self.font_cache_task,
                                 &self.font_cache_sender,
                                 &self.outstanding_web_fonts);
-            rw_data.stylist.add_stylesheet(sheet);
+            self.stylist.add_stylesheet(sheet);
         }
-
-        possibly_locked_rw_data.block(rw_data);
     }
 
-    fn handle_add_meta_viewport<'a, 'b>(&self,
+    fn handle_add_meta_viewport<'a, 'b>(&mut self,
                                         translated_rule: ViewportRule,
-                                        possibly_locked_rw_data: &mut RwData<'a, 'b>) {
-        let mut rw_data = possibly_locked_rw_data.lock();
-        rw_data.stylist.add_stylesheet(Stylesheet {
+                                        _: &mut RwData<'a, 'b>) {
+        self.stylist.add_stylesheet(Stylesheet {
             rules: vec![CSSRule::Viewport(translated_rule)],
             origin: Origin::Author
         });
-        possibly_locked_rw_data.block(rw_data);
     }
 
     /// Sets quirks mode for the document, causing the quirks mode stylesheet to be loaded.
-    fn handle_set_quirks_mode<'a, 'b>(&self, possibly_locked_rw_data: &mut RwData<'a, 'b>) {
-        let mut rw_data = possibly_locked_rw_data.lock();
-        rw_data.stylist.add_quirks_mode_stylesheet();
-        possibly_locked_rw_data.block(rw_data);
+    fn handle_set_quirks_mode<'a, 'b>(&mut self, _: &mut RwData<'a, 'b>) {
+        self.stylist.add_quirks_mode_stylesheet();
     }
 
     fn try_get_layout_root(&self, node: LayoutNode) -> Option<FlowRef> {
@@ -1074,7 +1068,7 @@ impl LayoutTask {
                     flow_ref::deref_mut(layout_root));
                 let root_size = {
                     let root_flow = flow::base(&**layout_root);
-                    if rw_data.stylist.constrain_viewport().is_some() {
+                    if self.stylist.constrain_viewport().is_some() {
                         root_flow.position.size.to_physical(root_flow.writing_mode)
                     } else {
                         root_flow.overflow.size
@@ -1158,9 +1152,9 @@ impl LayoutTask {
 
         // Calculate the actual viewport as per DEVICE-ADAPT § 6
         let device = Device::new(MediaType::Screen, initial_viewport);
-        rw_data.stylist.set_device(device);
+        self.stylist.set_device(device);
 
-        let constraints = rw_data.stylist.constrain_viewport();
+        let constraints = self.stylist.constrain_viewport();
         self.viewport_size = match constraints {
             Some(ref constraints) => {
                 debug!("Viewport constraints: {:?}", constraints);
@@ -1177,7 +1171,7 @@ impl LayoutTask {
         if viewport_size_changed {
             if let Some(constraints) = constraints {
                 let device = Device::new(MediaType::Screen, constraints.size);
-                rw_data.stylist.set_device(device);
+                self.stylist.set_device(device);
 
                 // let the constellation know about the viewport constraints
                 let ConstellationChan(ref constellation_chan) = rw_data.constellation_chan;
@@ -1187,7 +1181,7 @@ impl LayoutTask {
         }
 
         // If the entire flow tree is invalid, then it will be reflowed anyhow.
-        let needs_dirtying = rw_data.stylist.update();
+        let needs_dirtying = self.stylist.update();
         let needs_reflow = viewport_size_changed && !needs_dirtying;
         unsafe {
             if needs_dirtying {
@@ -1204,9 +1198,9 @@ impl LayoutTask {
         if !needs_dirtying {
             for &(el, state_change) in state_changes.iter() {
                 debug_assert!(!state_change.is_empty());
-                let hint = rw_data.stylist.restyle_hint_for_state_change(&el,
-                                                                         el.get_state(),
-                                                                         state_change);
+                let hint = self.stylist.restyle_hint_for_state_change(&el,
+                                                                      el.get_state(),
+                                                                      state_change);
                 el.note_restyle_hint(hint);
             }
         }
@@ -1217,7 +1211,7 @@ impl LayoutTask {
                                                                          &self.url,
                                                                          data.reflow_info.goal);
 
-        if node.is_dirty() || node.has_dirty_descendants() || rw_data.stylist.is_dirty() {
+        if node.is_dirty() || node.has_dirty_descendants() || self.stylist.is_dirty() {
             // Recalculate CSS styles and rebuild flows and fragments.
             profile(time::ProfilerCategory::LayoutStyleRecalc,
                     self.profiler_metadata(),
