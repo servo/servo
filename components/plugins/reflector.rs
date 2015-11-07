4f7603717016bb3bd90367942cea3181ab4009b5
Manish Goregaokar <manishsmail@gmail.com>
2015-11-07 22:40:18 +0530

    Rust upgrade to rustc 1.6.0-nightly (5b4986fa5 2015-11-08)

--- components/plugins/reflector.rs
+++ components/plugins/reflector.rs
@@ -15,7 +15,7 @@ pub fn expand_reflector(cx: &mut ExtCtxt, span: Span, _: &MetaItem, annotatable:
         if let ast::ItemStruct(ref def, _) = item.node {
             let struct_name = item.ident;
             // This path has to be hardcoded, unfortunately, since we can't resolve paths at expansion time
-            match def.fields.iter().find(
+            match def.fields().iter().find(
                     |f| match_ty_unwrap(&*f.node.ty, &["dom", "bindings", "reflector", "Reflector"]).is_some()) {
                 // If it has a field that is a Reflector, use that
                 Some(f) => {
@@ -34,7 +34,7 @@ pub fn expand_reflector(cx: &mut ExtCtxt, span: Span, _: &MetaItem, annotatable:
                 },
                 // Or just call it on the first field (supertype).
                 None => {
-                    let field_name = def.fields[0].node.ident();
+                    let field_name = def.fields()[0].node.ident();
                     let impl_item = quote_item!(cx,
                         impl ::dom::bindings::reflector::Reflectable for $struct_name {
                             fn reflector<'a>(&'a self) -> &'a ::dom::bindings::reflector::Reflector {
