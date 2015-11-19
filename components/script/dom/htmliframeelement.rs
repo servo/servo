972077f9ec919dd9b79d648637edcc1ee0de2b6f
Josh Matthews <josh@joshmatthews.net>
2015-11-19 04:04:04 -0500

    Flesh out the iframe attribute processing steps.

--- components/script/dom/htmliframeelement.rs
+++ components/script/dom/htmliframeelement.rs
@@ -17,6 +17,7 @@ use dom::bindings::error::{Error, ErrorResult, Fallible};
 use dom::bindings::global::GlobalRef;
 use dom::bindings::inheritance::Castable;
 use dom::bindings::js::{Root, LayoutJS};
+use dom::bindings::refcounted::Trusted;
 use dom::bindings::reflector::Reflectable;
 use dom::customevent::CustomEvent;
 use dom::document::Document;
@@ -36,15 +37,24 @@ use msg::constellation_msg::{ConstellationChan};
 use msg::constellation_msg::{NavigationDirection, PipelineId, SubpageId};
 use net_traits::response::HttpsState;
 use page::IterablePage;
+use script_runtime::ScriptChan;
+use script_thread::{MainThreadScriptChan, Runnable};
 use script_traits::IFrameSandboxState::{IFrameSandboxed, IFrameUnsandboxed};
 use script_traits::{IFrameLoadInfo, MozBrowserEvent, ScriptMsg as ConstellationMsg};
 use std::ascii::AsciiExt;
 use std::cell::Cell;
 use string_cache::Atom;
 use style::context::ReflowGoal;
+use task_source::dom_manipulation::DOMManipulationTask;
 use url::Url;
 use util::prefs;
 use util::str::{DOMString, LengthOrPercentageOrAuto};
+
+#[derive(PartialEq)]
+enum ProcessingMode {
+    FirstTime,
+    NotFirstTime,
+}
 
 pub fn mozbrowser_enabled() -> bool {
     prefs::get_pref("dom.mozbrowser.enabled").as_boolean().unwrap_or(false)
@@ -142,11 +152,21 @@ impl HTMLIFrameElement {
         }
     }
 
-    pub fn process_the_iframe_attributes(&self) {
+    /// https://html.spec.whatwg.org/multipage/#process-the-iframe-attributes
+    fn process_the_iframe_attributes(&self, mode: ProcessingMode) {
+        // TODO: srcdoc
+
         let url = match self.get_url() {
             Some(url) => url.clone(),
+            None if mode == ProcessingMode::FirstTime => {
+                let event_loop = window_from_node(self).dom_manipulation_task_source();
+                let _ = event_loop.queue(DOMManipulationTask::Runnable(box IframeLoadEventSteps::new(self)));
+                return;
+            }
             None => Url::parse("about:blank").unwrap(),
         };
+
+        // TODO: check ancestor browsing contexts for same URL
 
         self.navigate_or_reload_child_browsing_context(Some(url));
     }
@@ -523,10 +543,8 @@ impl VirtualMethods for HTMLIFrameElement {
                 }));
             },
             &atom!("src") => {
-                if let AttributeMutation::Set(_) = mutation {
-                    if self.upcast::<Node>().is_in_doc() {
-                        self.process_the_iframe_attributes();
-                    }
+                if self.upcast::<Node>().is_in_doc() {
+                    self.process_the_iframe_attributes(ProcessingMode::NotFirstTime);
                 }
             },
             _ => {},
@@ -548,7 +566,7 @@ impl VirtualMethods for HTMLIFrameElement {
         }
 
         if tree_in_doc {
-            self.process_the_iframe_attributes();
+            self.process_the_iframe_attributes(ProcessingMode::FirstTime);
         }
     }
 
@@ -596,5 +614,26 @@ impl VirtualMethods for HTMLIFrameElement {
             self.subpage_id.set(None);
             self.pipeline_id.set(None);
         }
+    }
+}
+
+struct IframeLoadEventSteps {
+    frame_element: Trusted<HTMLIFrameElement>,
+}
+
+impl IframeLoadEventSteps {
+    fn new(frame_element: &HTMLIFrameElement) -> IframeLoadEventSteps {
+        let win = window_from_node(frame_element);
+        let target = MainThreadScriptChan(win.main_thread_script_chan().clone()).clone();
+        IframeLoadEventSteps {
+            frame_element: Trusted::new(frame_element, target),
+        }
+    }
+}
+
+impl Runnable for IframeLoadEventSteps {
+    fn handler(self: Box<IframeLoadEventSteps>) {
+        let this = self.frame_element.root();
+        this.iframe_load_event_steps(this.pipeline().unwrap());
     }
 }
