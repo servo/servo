3cafde1a1e4fb62c0f5e0cbae6e860b7e793fa18
Patrick Walton <pcwalton@mimiga.net>
2015-11-23 18:22:20 -0800

    When an iframe loads a new page, dirty and reflow it so that the layout fragment will pick up the new subpage ID.
    This is necessary to make its new contents show up!
    
    Closes #8081.

--- components/script/dom/window.rs
+++ components/script/dom/window.rs
@@ -103,6 +103,8 @@ pub enum ReflowReason {
     ImageLoaded,
     RequestAnimationFrame,
     WebFontLoaded,
+    /// An iframe loaded a new page.
+    IframeLoadedNewPage,
 }
 
 pub type ScrollPoint = Point2D<Au>;
@@ -1346,6 +1348,7 @@ fn debug_reflow_events(goal: &ReflowGoal, query_type: &ReflowQueryType, reason: 
         ReflowReason::ImageLoaded => "\tImageLoaded",
         ReflowReason::RequestAnimationFrame => "\tRequestAnimationFrame",
         ReflowReason::WebFontLoaded => "\tWebFontLoaded",
+        ReflowReason::IframeLoadedNewPage => "\tIframeLoadedNewPage",
     });
 
     println!("{}", debug_msg);
