6d4b847e7d23bbf4a5f2d02143289421e96b649d
Simon Sapin <simon.sapin@exyr.org>
2015-11-26 17:19:40 +0100

    Allow duplicate libc versions.

--- python/tidy.py
+++ python/tidy.py
@@ -164,7 +164,7 @@ def check_lock(file_name, contents):
     packages = {}
 
     # package names to be neglected (as named by cargo)
-    exceptions = ["glutin", "wayland-kbd"]
+    exceptions = ["glutin", "wayland-kbd", "libc"]
 
     while idx < len(contents):
         content = contents[idx].strip()
