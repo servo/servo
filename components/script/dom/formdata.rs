606196cfb9627052dee694b1ed0e309502c30e89
Keith Yeung <kungfukeith11@gmail.com>
2015-12-20 00:42:59 -0800

    Refactor FormData code to match updated spec

--- components/script/dom/formdata.rs
+++ components/script/dom/formdata.rs
@@ -5,13 +5,13 @@
 use dom::bindings::cell::DOMRefCell;
 use dom::bindings::codegen::Bindings::FormDataBinding;
 use dom::bindings::codegen::Bindings::FormDataBinding::FormDataMethods;
-use dom::bindings::codegen::UnionTypes::FileOrString;
-use dom::bindings::codegen::UnionTypes::FileOrString::{eFile, eString};
+use dom::bindings::codegen::UnionTypes::BlobOrUSVString::{self, eBlob, eUSVString};
 use dom::bindings::error::{Fallible};
 use dom::bindings::global::{GlobalField, GlobalRef};
 use dom::bindings::inheritance::Castable;
 use dom::bindings::js::{JS, Root};
 use dom::bindings::reflector::{Reflector, reflect_dom_object};
+use dom::bindings::str::USVString;
 use dom::blob::Blob;
 use dom::file::File;
 use dom::htmlformelement::HTMLFormElement;
@@ -23,14 +23,14 @@ use util::str::DOMString;
 #[must_root]
 #[derive(HeapSizeOf)]
 pub enum FormDatum {
-    StringData(DOMString),
-    FileData(JS<File>)
+    StringData(String),
+    BlobData(JS<Blob>)
 }
 
 #[dom_struct]
 pub struct FormData {
     reflector_: Reflector,
-    data: DOMRefCell<HashMap<DOMString, Vec<FormDatum>>>,
+    data: DOMRefCell<HashMap<String, Vec<FormDatum>>>,
     global: GlobalField,
     form: Option<JS<HTMLFormElement>>
 }
@@ -51,72 +51,83 @@ impl FormData {
     }
 
     pub fn Constructor(global: GlobalRef, form: Option<&HTMLFormElement>) -> Fallible<Root<FormData>> {
+        // TODO: Construct form data set for form if it is supplied
         Ok(FormData::new(form, global))
     }
 }
 
 impl FormDataMethods for FormData {
+    // https://xhr.spec.whatwg.org/#dom-formdata-append
+    fn Append(&self, name: USVString, value: USVString) {
+        let mut data = self.data.borrow_mut();
+        match data.entry(name.0) {
+            Occupied(entry) => entry.into_mut().push(FormDatum::StringData(value.0)),
+            Vacant  (entry) => { entry.insert(vec!(FormDatum::StringData(value.0))); }
+        }
+    }
+
     #[allow(unrooted_must_root)]
     // https://xhr.spec.whatwg.org/#dom-formdata-append
-    fn Append(&self, name: DOMString, value: &Blob, filename: Option<DOMString>) {
-        let file = FormDatum::FileData(JS::from_rooted(&self.get_file_from_blob(value, filename)));
+    fn Append_(&self, name: USVString, value: &Blob, filename: Option<USVString>) {
+        let blob = FormDatum::BlobData(JS::from_rooted(&self.get_file_or_blob(value, filename)));
         let mut data = self.data.borrow_mut();
-        match data.entry(name) {
-            Occupied(entry) => entry.into_mut().push(file),
+        match data.entry(name.0) {
+            Occupied(entry) => entry.into_mut().push(blob),
             Vacant(entry) => {
-                entry.insert(vec!(file));
+                entry.insert(vec!(blob));
             }
         }
     }
 
-    // https://xhr.spec.whatwg.org/#dom-formdata-append
-    fn Append_(&self, name: DOMString, value: DOMString) {
-        let mut data = self.data.borrow_mut();
-        match data.entry(name) {
-            Occupied(entry) => entry.into_mut().push(FormDatum::StringData(value)),
-            Vacant  (entry) => { entry.insert(vec!(FormDatum::StringData(value))); },
-        }
-    }
-
     // https://xhr.spec.whatwg.org/#dom-formdata-delete
-    fn Delete(&self, name: DOMString) {
-        self.data.borrow_mut().remove(&name);
+    fn Delete(&self, name: USVString) {
+        self.data.borrow_mut().remove(&name.0);
     }
 
     // https://xhr.spec.whatwg.org/#dom-formdata-get
-    fn Get(&self, name: DOMString) -> Option<FileOrString> {
+    fn Get(&self, name: USVString) -> Option<BlobOrUSVString> {
         self.data.borrow()
-                 .get(&name)
+                 .get(&name.0)
                  .map(|entry| match entry[0] {
-                     FormDatum::StringData(ref s) => eString(s.clone()),
-                     FormDatum::FileData(ref f) => eFile(Root::from_ref(&*f)),
+                     FormDatum::StringData(ref s) => eUSVString(USVString(s.clone())),
+                     FormDatum::BlobData(ref b) => eBlob(Root::from_ref(&*b)),
                  })
     }
 
-    // https://xhr.spec.whatwg.org/#dom-formdata-has
-    fn Has(&self, name: DOMString) -> bool {
-        self.data.borrow().contains_key(&name)
+    // https://xhr.spec.whatwg.org/#dom-formdata-getall
+    fn GetAll(&self, name: USVString) -> Vec<BlobOrUSVString> {
+        self.data.borrow()
+                 .get(&name.0)
+                 .map_or(vec![], |data|
+                    data.iter().map(|item| match *item {
+                        FormDatum::StringData(ref s) => eUSVString(USVString(s.clone())),
+                        FormDatum::BlobData(ref b) => eBlob(Root::from_ref(&*b)),
+                    }).collect()
+                 )
     }
 
-    // https://xhr.spec.whatwg.org/#dom-formdata-set
-    fn Set_(&self, name: DOMString, value: DOMString) {
-        self.data.borrow_mut().insert(name, vec!(FormDatum::StringData(value)));
+    // https://xhr.spec.whatwg.org/#dom-formdata-has
+    fn Has(&self, name: USVString) -> bool {
+        self.data.borrow().contains_key(&name.0)
     }
 
     #[allow(unrooted_must_root)]
     // https://xhr.spec.whatwg.org/#dom-formdata-set
-    fn Set(&self, name: DOMString, value: &Blob, filename: Option<DOMString>) {
-        let file = FormDatum::FileData(JS::from_rooted(&self.get_file_from_blob(value, filename)));
-        self.data.borrow_mut().insert(name, vec!(file));
+    fn Set(&self, name: USVString, value: BlobOrUSVString) {
+        let val = match value {
+            eUSVString(s) => FormDatum::StringData(s.0),
+            eBlob(b) => FormDatum::BlobData(JS::from_rooted(&b))
+        };
+        self.data.borrow_mut().insert(name.0, vec!(val));
     }
 }
 
 
 impl FormData {
-    fn get_file_from_blob(&self, value: &Blob, filename: Option<DOMString>) -> Root<File> {
+    fn get_file_or_blob(&self, value: &Blob, filename: Option<USVString>) -> Root<Blob> {
+        if filename.is_none() { return Root::from_ref(value); }
         let global = self.global.root();
-        let f = value.downcast::<File>();
-        let name = filename.unwrap_or(f.map(|inner| inner.name().clone()).unwrap_or(DOMString::from("blob")));
-        File::new(global.r(), value, name)
+        let name = DOMString::from(filename.unwrap().0);
+        Root::upcast(File::new(global.r(), value, name))
     }
 }
