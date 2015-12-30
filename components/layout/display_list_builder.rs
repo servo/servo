229c9b79f77ca7afed4239cc93f7273f94ff4670
Simon Sapin <simon.sapin@exyr.org>
2015-12-30 23:50:24 +0000

    Add Multicolumn support block fragmentation.

--- components/layout/display_list_builder.rs
+++ components/layout/display_list_builder.rs
@@ -1083,6 +1083,8 @@ impl FragmentDisplayListBuilding for Fragment {
             SpecificFragmentInfo::TableCell |
             SpecificFragmentInfo::TableRow |
             SpecificFragmentInfo::TableWrapper |
+            SpecificFragmentInfo::Multicol |
+            SpecificFragmentInfo::MulticolColumn |
             SpecificFragmentInfo::InlineBlock(_) |
             SpecificFragmentInfo::InlineAbsoluteHypothetical(_) |
             SpecificFragmentInfo::InlineAbsolute(_) => {
