c416b33345c2923f569f3f78604fbf512b85857e
Jaydeep <jd.ramani111@gmail.com>
2016-01-03 17:14:30 -0800

    Implement HTMLHyperlinkElementUtils for HTMLAnchorElement

--- components/script/dom/htmlanchorelement.rs
+++ components/script/dom/htmlanchorelement.rs
@@ -5,6 +5,7 @@
 
 use dom::activation::Activatable;
 use dom::attr::AttrValue;
+use dom::bindings::cell::DOMRefCell;
 use dom::bindings::codegen::Bindings::AttrBinding::AttrMethods;
 use dom::bindings::codegen::Bindings::HTMLAnchorElementBinding;
 use dom::bindings::codegen::Bindings::HTMLAnchorElementBinding::HTMLAnchorElementMethods;
@@ -12,6 +13,7 @@ use dom::bindings::codegen::Bindings::MouseEventBinding::MouseEventMethods;
 use dom::bindings::codegen::Bindings::NodeBinding::NodeMethods;
 use dom::bindings::inheritance::Castable;
 use dom::bindings::js::{JS, MutNullableHeap, Root};
+use dom::bindings::str::USVString;
 use dom::document::Document;
 use dom::domtokenlist::DOMTokenList;
 use dom::element::Element;
@@ -21,16 +23,19 @@ use dom::htmlelement::HTMLElement;
 use dom::htmlimageelement::HTMLImageElement;
 use dom::mouseevent::MouseEvent;
 use dom::node::{Node, document_from_node, window_from_node};
+use dom::urlhelper::UrlHelper;
 use dom::virtualmethods::VirtualMethods;
 use num::ToPrimitive;
 use std::default::Default;
 use string_cache::Atom;
+use url::Url;
 use util::str::DOMString;
 
 #[dom_struct]
 pub struct HTMLAnchorElement {
     htmlelement: HTMLElement,
     rel_list: MutNullableHeap<JS<DOMTokenList>>,
+    url: DOMRefCell<Option<Url>>,
 }
 
 impl HTMLAnchorElement {
@@ -41,6 +46,7 @@ impl HTMLAnchorElement {
             htmlelement:
                 HTMLElement::new_inherited(localName, prefix, document),
             rel_list: Default::default(),
+            url: DOMRefCell::new(None),
         }
     }
 
@@ -50,6 +56,33 @@ impl HTMLAnchorElement {
                document: &Document) -> Root<HTMLAnchorElement> {
         let element = HTMLAnchorElement::new_inherited(localName, prefix, document);
         Node::reflect_node(box element, document, HTMLAnchorElementBinding::Wrap)
+    }
+
+    // https://html.spec.whatwg.org/multipage/#concept-hyperlink-url-set
+    fn set_url(&self) {
+        let attribute = self.upcast::<Element>().get_attribute(&ns!(), &atom!("href"));
+        *self.url.borrow_mut() = attribute.and_then(|attribute| {
+            Url::parse(&attribute.value()).ok()
+        });
+    }
+
+    // https://html.spec.whatwg.org/multipage/#reinitialise-url
+    fn reinitialize_url(&self) {
+        // Step 1.
+        match *self.url.borrow() {
+            None => return,
+            Some(ref url) if url.non_relative_scheme_data().is_some() => return,
+            _ => (),
+        }
+
+        // Step 2.
+        self.set_url();
+    }
+
+    // https://html.spec.whatwg.org/multipage/#update-href
+    fn update_href(&self) {
+        self.upcast::<Element>().set_string_attribute(&atom!("href"),
+            self.url.borrow().as_ref().unwrap().serialize().into());
     }
 }
 
@@ -107,6 +140,120 @@ impl HTMLAnchorElementMethods for HTMLAnchorElement {
 
     // https://html.spec.whatwg.org/multipage/#dom-a-shape
     make_setter!(SetShape, "shape");
+
+    // https://html.spec.whatwg.org/multipage/#dom-hyperlink-hash
+    fn Hash(&self) -> USVString {
+        UrlHelper::Hash(&self.url.borrow().as_ref().unwrap())
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-hyperlink-hash
+    fn SetHash(&self, value: USVString) {
+        UrlHelper::SetHash(self.url.borrow_mut().as_mut().unwrap(), value);
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-hyperlink-host
+    fn Host(&self) -> USVString {
+        UrlHelper::Host(&self.url.borrow().as_ref().unwrap())
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-hyperlink-host
+    fn SetHost(&self, value: USVString) {
+        UrlHelper::SetHost(self.url.borrow_mut().as_mut().unwrap(), value);
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-hyperlink-hostname
+    fn Hostname(&self) -> USVString {
+        UrlHelper::Hostname(&self.url.borrow().as_ref().unwrap())
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-hyperlink-hostname
+    fn SetHostname(&self, value: USVString) {
+        UrlHelper::SetHostname(self.url.borrow_mut().as_mut().unwrap(), value);
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-hyperlink-href
+    fn Href(&self) -> USVString {
+        // Step 1.
+        self.reinitialize_url();
+
+        USVString(match *self.url.borrow() {
+            None => {
+                match self.upcast::<Element>().get_attribute(&ns!(), &atom!("href")) {
+                    // Step 3.
+                    None => String::new(),
+                    // Step 4.
+                    Some(attribute) => (**attribute.value()).to_owned(),
+                }
+            },
+            // Step 5.
+            Some(ref url) => url.serialize(),
+        })
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-hyperlink-href
+    fn SetHref(&self, value: USVString) {
+        unimplemented!()
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-hyperlink-password
+    fn Password(&self) -> USVString {
+        UrlHelper::Password(&self.url.borrow().as_ref().unwrap())
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-hyperlink-password
+    fn SetPassword(&self, value: USVString) {
+        UrlHelper::SetPassword(self.url.borrow_mut().as_mut().unwrap(), value);
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-hyperlink-pathname
+    fn Pathname(&self) -> USVString {
+        UrlHelper::Pathname(&self.url.borrow().as_ref().unwrap())
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-hyperlink-pathname
+    fn SetPathname(&self, value: USVString) {
+        UrlHelper::SetPathname(self.url.borrow_mut().as_mut().unwrap(), value);
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-hyperlink-port
+    fn Port(&self) -> USVString {
+        UrlHelper::Port(&self.url.borrow().as_ref().unwrap())
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-hyperlink-port
+    fn SetPort(&self, value: USVString) {
+        UrlHelper::SetPort(self.url.borrow_mut().as_mut().unwrap(), value);
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-hyperlink-protocol
+    fn Protocol(&self) -> USVString {
+        UrlHelper::Protocol(&self.url.borrow().as_ref().unwrap())
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-hyperlink-protocol
+    fn SetProtocol(&self, value: USVString) {
+        UrlHelper::SetProtocol(self.url.borrow_mut().as_mut().unwrap(), value);
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-hyperlink-search
+    fn Search(&self) -> USVString {
+        UrlHelper::Search(&self.url.borrow().as_ref().unwrap())
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-hyperlink-search
+    fn SetSearch(&self, value: USVString) {
+        UrlHelper::SetSearch(self.url.borrow_mut().as_mut().unwrap(), value);
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-hyperlink-username
+    fn Username(&self) -> USVString {
+        UrlHelper::Username(&self.url.borrow().as_ref().unwrap())
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-hyperlink-username
+    fn SetUsername(&self, value: USVString) {
+        UrlHelper::SetUsername(self.url.borrow_mut().as_mut().unwrap(), value);
+    }
 }
 
 impl Activatable for HTMLAnchorElement {
