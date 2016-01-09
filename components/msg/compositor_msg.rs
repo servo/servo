f32995543de85db093272fc53587dc7fc2203b18
John DeSilva <desilvjo@umich.edu>
2016-01-09 15:26:58 -0500

    Move Epoch to gfx_traits
    Resolves #9222. Moved Epoch from components/msg/compositor_msg to
    components/gfx_traits/lib. Updated use statements to reflect the move.

--- components/msg/compositor_msg.rs
+++ components/msg/compositor_msg.rs
@@ -2,17 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/// A newtype struct for denoting the age of messages; prevents race conditions.
-#[derive(PartialEq, Eq, Debug, Copy, Clone, PartialOrd, Ord, Deserialize, Serialize)]
-pub struct Epoch(pub u32);
-
-impl Epoch {
-    pub fn next(&mut self) {
-        let Epoch(ref mut u) = *self;
-        *u += 1;
-    }
-}
-
 #[derive(PartialEq, Eq, Debug, Copy, Clone)]
 pub struct FrameTreeId(pub u32);
 
