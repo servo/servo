aa987b1cbaa52047208ea899d2ddfc2ca3257045
Nikki <nikkicubed@gmail.com>
2016-01-12 14:45:51 -0700

    fixed a couple tidy-test errors in the fetch test file

--- tests/unit/net/fetch.rs
+++ tests/unit/net/fetch.rs
@@ -1,13 +1,16 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 use hyper::server::{Server, Request as HyperRequest, Response as HyperResponse};
 use net::fetch::request::{Context, fetch, Referer, Request};
-use net_traits::{ResponseBody};
-use std::cell::RefCell;
+use net_traits::response::{ResponseBody};
 use std::rc::Rc;
 use url::Url;
 
 #[test]
 fn test_fetch_response_body_matches_const_message() {
-    
+
     static MESSAGE: &'static [u8] = b"Hello World!";
     fn handler(_: HyperRequest, response: HyperResponse) {
         response.send(MESSAGE).unwrap();
@@ -19,10 +22,10 @@ fn test_fetch_response_body_matches_const_message() {
     let mut url_string = "http://localhost:".to_owned();
     url_string.push_str(&port);
     let url = Url::parse(&url_string).unwrap();
-    
+
     let mut request = Request::new(url, Context::Fetch, false);
     request.referer = Referer::NoReferer;
-    let wrapped_request = Rc::new(RefCell::new(request));
+    let wrapped_request = Rc::new(request);
 
     println!("calling fetch");
     let fetch_response = fetch(wrapped_request, false);
