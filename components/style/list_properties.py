0d4330f23f5981d38b35adaae754ce2265d10711
Simon Sapin <simon.sapin@exyr.org>
2016-01-15 16:18:58 +0100

    Make the properties list JSON and add some more info.

--- components/style/list_properties.py
+++ components/style/list_properties.py
@@ -2,13 +2,20 @@
 
 import os.path
 import sys
+import json
+import operator
 
 style = os.path.dirname(__file__)
 sys.path.insert(0, os.path.join(style, "Mako-0.9.1.zip"))
-
 from mako.template import Template
+
 template = Template(filename=os.path.join(style, "properties.mako.rs"), input_encoding='utf8')
 template.render()
-properties = template.module.LONGHANDS + template.module.SHORTHANDS
-for name in sorted(p.name for p in properties):
-    print(name)
+properties = dict(
+    (p.name, {
+        "flag": p.experimental,
+        "shorthand": hasattr(p, "sub_properties")
+    })
+    for p in template.module.LONGHANDS + template.module.SHORTHANDS
+)
+print(json.dumps(properties, indent=4))
