f0765890fe9a33aa0cd1380cffa827924cb15c42
Joshua Holmer <holmerj@uindy.edu>
2016-01-25 14:21:32 -0500

    Implement console#time and console#timeEnd methods
    Fixes #9325

--- components/script/dom/console.rs
+++ components/script/dom/console.rs
@@ -3,23 +3,28 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 use devtools_traits::{ConsoleMessage, LogLevel, ScriptToDevtoolsControlMsg};
+use dom::bindings::cell::DOMRefCell;
 use dom::bindings::codegen::Bindings::ConsoleBinding;
 use dom::bindings::codegen::Bindings::ConsoleBinding::ConsoleMethods;
 use dom::bindings::global::GlobalRef;
 use dom::bindings::js::Root;
 use dom::bindings::reflector::{Reflectable, Reflector, reflect_dom_object};
+use std::collections::HashMap;
+use time::{Timespec, get_time};
 use util::str::DOMString;
 
 // https://developer.mozilla.org/en-US/docs/Web/API/Console
 #[dom_struct]
 pub struct Console {
     reflector_: Reflector,
+    timers: DOMRefCell<HashMap<DOMString, u64>>,
 }
 
 impl Console {
     fn new_inherited() -> Console {
         Console {
             reflector_: Reflector::new(),
+            timers: DOMRefCell::new(HashMap::new()),
         }
     }
 
@@ -92,6 +97,40 @@ impl ConsoleMethods for Console {
             self.send_to_devtools(LogLevel::Error, message);
         }
     }
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/Console/time
+    fn Time(&self, label: DOMString) {
+        let mut timers = self.timers.borrow_mut();
+        if timers.contains_key(&label) {
+            // Timer already started
+            return;
+        }
+        if timers.len() >= 10000 {
+            // Too many timers on page
+            return;
+        }
+
+        timers.insert(label.clone(), timestamp_in_ms(get_time()));
+        let message = DOMString::from(format!("{}: timer started", label));
+        println!("{}", message);
+        self.send_to_devtools(LogLevel::Log, message);
+    }
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/Console/timeEnd
+    fn TimeEnd(&self, label: DOMString) {
+        let mut timers = self.timers.borrow_mut();
+        if let Some(start) = timers.remove(&label) {
+            let message = DOMString::from(
+                format!("{}: {}ms", label, timestamp_in_ms(get_time()) - start)
+            );
+            println!("{}", message);
+            self.send_to_devtools(LogLevel::Log, message);
+        };
+    }
+}
+
+fn timestamp_in_ms(time: Timespec) -> u64 {
+    (time.sec * 1000 + (time.nsec / 1000000) as i64) as u64
 }
 
 fn prepare_message(logLevel: LogLevel, message: DOMString) -> ConsoleMessage {
