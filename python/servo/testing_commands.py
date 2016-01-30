83a492a533aecdb122052a9cacd735779ab9dfdd
Shing Lyu <shing.lyu@gmail.com>
2016-01-30 09:33:39 +0800

    Fixed nits

--- python/servo/testing_commands.py
+++ python/servo/testing_commands.py
@@ -242,8 +242,10 @@ class MachCommands(CommandBase):
     def test_webidl(self, quiet, tests):
         self.ensure_bootstrapped()
 
-        test_file_dir = path.abspath(path.join(PROJECT_TOPLEVEL_PATH, "components", "script", "dom", "bindings", "codegen", "parser"))
-        sys.path.insert(0, test_file_dir) # For the `import WebIDL` in runtests.py
+        test_file_dir = path.abspath(path.join(PROJECT_TOPLEVEL_PATH, "components", "script",
+                                               "dom", "bindings", "codegen", "parser"))
+        # For the `import WebIDL` in runtests.py
+        sys.path.insert(0, test_file_dir)
 
         run_file = path.abspath(path.join(test_file_dir, "runtests.py"))
         run_globals = {"__file__": run_file}
