de5ef0c55fcda1a13a4b671678e92cb7cba5a7f4
Paul Rouget <me@paulrouget.com>
2016-02-04 09:01:38 +0100

    Implement focus, blur, focusin and focusout events

---
+++ components/script/dom/focusevent.rs
@@ -0,0 +1,104 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+use dom::bindings::codegen::Bindings::FocusEventBinding;
+use dom::bindings::codegen::Bindings::FocusEventBinding::FocusEventMethods;
+use dom::bindings::codegen::Bindings::UIEventBinding::UIEventMethods;
+use dom::bindings::error::Fallible;
+use dom::bindings::global::GlobalRef;
+use dom::bindings::inheritance::Castable;
+use dom::bindings::js::{JS, MutNullableHeap, Root, RootedReference};
+use dom::bindings::reflector::reflect_dom_object;
+use dom::event::{Event, EventBubbles, EventCancelable};
+use dom::eventtarget::EventTarget;
+use dom::uievent::UIEvent;
+use dom::window::Window;
+use std::default::Default;
+use util::str::DOMString;
+
+#[dom_struct]
+pub struct FocusEvent {
+    uievent: UIEvent,
+    related_target: MutNullableHeap<JS<EventTarget>>,
+}
+
+impl FocusEvent {
+    fn new_inherited() -> FocusEvent {
+        FocusEvent {
+            uievent: UIEvent::new_inherited(),
+            related_target: Default::default(),
+        }
+    }
+
+    pub fn new_uninitialized(window: &Window) -> Root<FocusEvent> {
+        reflect_dom_object(box FocusEvent::new_inherited(),
+                           GlobalRef::Window(window),
+                           FocusEventBinding::Wrap)
+    }
+
+    pub fn new(window: &Window,
+               type_: DOMString,
+               canBubble: EventBubbles,
+               cancelable: EventCancelable,
+               view: Option<&Window>,
+               detail: i32,
+               relatedTarget: Option<&EventTarget>) -> Root<FocusEvent> {
+        let ev = FocusEvent::new_uninitialized(window);
+        ev.r().InitFocusEvent(type_, canBubble == EventBubbles::Bubbles, cancelable == EventCancelable::Cancelable,
+                              view, detail,
+                              relatedTarget);
+        ev
+    }
+
+    pub fn Constructor(global: GlobalRef,
+                       type_: DOMString,
+                       init: &FocusEventBinding::FocusEventInit) -> Fallible<Root<FocusEvent>> {
+        let bubbles = if init.parent.parent.bubbles {
+            EventBubbles::Bubbles
+        } else {
+            EventBubbles::DoesNotBubble
+        };
+        let cancelable = if init.parent.parent.cancelable {
+            EventCancelable::Cancelable
+        } else {
+            EventCancelable::NotCancelable
+        };
+        let event = FocusEvent::new(global.as_window(), type_,
+                                    bubbles,
+                                    cancelable,
+                                    init.parent.view.r(),
+                                    init.parent.detail,
+                                    init.relatedTarget.r());
+        Ok(event)
+    }
+}
+
+impl FocusEventMethods for FocusEvent {
+    // https://w3c.github.io/uievents/#widl-FocusEvent-relatedTarget
+    fn GetRelatedTarget(&self) -> Option<Root<EventTarget>> {
+        self.related_target.get()
+    }
+
+    // https://w3c.github.io/uievents/#widl-FocusEvent-initFocusEvent
+    fn InitFocusEvent(&self,
+                      typeArg: DOMString,
+                      canBubbleArg: bool,
+                      cancelableArg: bool,
+                      viewArg: Option<&Window>,
+                      detailArg: i32,
+                      relatedTargetArg: Option<&EventTarget>) {
+        if self.upcast::<Event>().dispatching() {
+            return;
+        }
+
+        self.upcast::<UIEvent>()
+            .InitUIEvent(typeArg, canBubbleArg, cancelableArg, viewArg, detailArg);
+        self.related_target.set(relatedTargetArg);
+    }
+
+    // https://dom.spec.whatwg.org/#dom-event-istrusted
+    fn IsTrusted(&self) -> bool {
+        self.uievent.IsTrusted()
+    }
+}
