50faf1818ce40f8f8bfe263c7177f6486e817daf
Ravi Shankar <wafflespeanut@gmail.com>
2016-02-20 02:41:51 +0530

    Added 'about:sslfail' and a reftest for SSL failure

--- components/net/about_loader.rs
+++ components/net/about_loader.rs
@@ -15,6 +15,13 @@ use std::sync::Arc;
 use url::Url;
 use util::resource_files::resources_dir_path;
 
+fn url_from_non_relative_scheme(load_data: &mut LoadData, filename: &str) {
+    let mut path = resources_dir_path();
+    path.push(filename);
+    assert!(path.exists());
+    load_data.url = Url::from_file_path(&*path).unwrap();
+}
+
 pub fn factory(mut load_data: LoadData,
                start_chan: LoadConsumer,
                classifier: Arc<MIMEClassifier>,
@@ -41,13 +48,9 @@ pub fn factory(mut load_data: LoadData,
             return
         }
         "crash" => panic!("Loading the about:crash URL."),
-        "failure" | "not-found" => {
-            let mut path = resources_dir_path();
-            let file_name = non_relative_scheme_data.to_owned() + ".html";
-            path.push(&file_name);
-            assert!(path.exists());
-            load_data.url = Url::from_file_path(&*path).unwrap();
-        }
+        "failure" | "not-found" =>
+            url_from_non_relative_scheme(&mut load_data, &(non_relative_scheme_data.to_owned() + ".html")),
+        "sslfail" => url_from_non_relative_scheme(&mut load_data, "badcert.html"),
         _ => {
             send_error(load_data.url, NetworkError::Internal("Unknown about: URL.".to_owned()), start_chan);
             return
