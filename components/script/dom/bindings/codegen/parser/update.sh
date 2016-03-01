549f2669f06954fa2eda243bcfd5014f6ca03549
Ms2ger <Ms2ger@gmail.com>
2016-03-01 09:42:41 +0100

    Stop generating a parser.out file in the source.

--- components/script/dom/bindings/codegen/parser/update.sh
+++ components/script/dom/bindings/codegen/parser/update.sh
@@ -1,5 +1,6 @@
 wget https://mxr.mozilla.org/mozilla-central/source/dom/bindings/parser/WebIDL.py?raw=1 -O WebIDL.py
 patch < abstract.patch
+patch < debug.patch
 patch < legacy-unenumerable-named-properties.patch
 
 # TODO: update test files from https://dxr.mozilla.org/mozilla-central/source/dom/bindings/parser/tests
