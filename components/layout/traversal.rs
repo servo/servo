fd1e359e7330eb3bc1792ac6ccd890e92e44b4da
Patrick Walton <pcwalton@mimiga.net>
2016-03-15 16:13:46 -0700

    layout: Don't rebuild display lists at all unless restyling tells us some object needs to be repainted.
    Reduces CPU usage when mousing over simple documents. (More complex
    documents tend to trigger unnecessary reflow bugs and so still have high
    CPU.)
    
    Part of #9999.

--- components/layout/traversal.rs
+++ components/layout/traversal.rs
@@ -234,6 +234,6 @@ impl<'a> BuildDisplayList<'a> {
 
     #[inline]
     fn should_process(&self) -> bool {
-        self.state.layout_context.shared_context().goal == ReflowGoal::ForDisplay
+        true
     }
 }
