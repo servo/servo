617224717cfba2556cecb7c4439faacd35a66622
Rebecca <rebstar6@gmail.com>
2016-03-15 17:11:29 -0400

    Fix htmllabelelement click to activate correct element

--- components/script/dom/htmllabelelement.rs
+++ components/script/dom/htmllabelelement.rs
@@ -63,12 +63,12 @@ impl Activatable for HTMLLabelElement {
 
     // https://html.spec.whatwg.org/multipage/#run-post-click-activation-steps
     fn activation_behavior(&self, _event: &Event, _target: &EventTarget) {
-        synthetic_click_activation(self.upcast::<Element>(),
-                                   false,
-                                   false,
-                                   false,
-                                   false,
-                                   ActivationSource::NotFromClick);
+        self.GetControl().map(|elem| synthetic_click_activation(elem.upcast::<Element>(),
+                                     false,
+                                     false,
+                                     false,
+                                     false,
+                                     ActivationSource::NotFromClick));
     }
 
     // https://html.spec.whatwg.org/multipage/#implicit-submission
