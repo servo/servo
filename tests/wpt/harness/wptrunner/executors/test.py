5e44f5d6811b20cc389f0d7518ee8c46dbfdeae8
Krunal Gala <krunal3103@gmail.com>
2016-03-20 22:35:56 -0400

    few changes

--- tests/wpt/harness/wptrunner/executors/test.py
+++ tests/wpt/harness/wptrunner/executors/test.py
@@ -1,4 +1,7 @@
 import webdriver
 session = webdriver.Session('127.0.0.1', 7000)
 session.start()
-#session.url = 'http://google.com'
+print(session.url)
+session.url = 'http://www.google.com'
+
+print(session.url)
