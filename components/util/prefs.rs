67923613313c90b16160c4d860702eb1f580c2e1
Matthew Bentley <matthew.t.bentley@gmail.com>
2016-03-21 23:26:29 -0400

    Fix order of imports to satisfy the lint Fix split line in test Fix multi-line string

--- components/util/prefs.rs
+++ components/util/prefs.rs
@@ -2,9 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+use opts;
 use resource_files::resources_dir_path;
 use rustc_serialize::json::{Json, ToJson};
-use opts;
 use std::borrow::ToOwned;
 use std::collections::HashMap;
 use std::fs::File;
