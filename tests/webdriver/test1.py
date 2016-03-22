1bad14891712300ca9267ef45c6bd1c7e03aa83e
Krunal Gala <krunal3103@gmail.com>
2016-03-21 22:54:52 -0400

    tests done

--- tests/webdriver/test1.py
+++ tests/webdriver/test1.py
@@ -1,26 +1,21 @@
+import sys
 import unittest
-from selenium import webdriver
-
+sys.path.insert(0, '/Users/krunal/Git/servo/tests/wpt/harness/wptrunner/executors/')
+import webdriver
+import subprocess
 
 class TestStringMethods(unittest.TestCase):
+  def test_get(self):
+      proc = subprocess.Popen(["./mach run --webdriver 9000 tests/html/about-mozilla.html"], shell=True)
+      session = webdriver.Session('127.0.0.1', 9000)
+      session.start()
+      session.url="http://google.com/careers"
+      a=self.assertEqual( session.url , "https://www.google.com/about/careers/")
+      if a:
+          session.end()
+          proc.kill()
 
-  def test_upper(self):
-      self.assertEqual('foo'.upper(), 'FOOd')
 
-  def test_isupper(self):
-      self.assertTrue('FOO'.isupper())
-      self.assertFalse('Foo'.isupper())
-
-  def test_split(self):
-      s = 'hello world'
-      self.assertEqual(s.split(), ['hello', 'world'])
-      # check that s.split fails when the separator is not a string
-      with self.assertRaises(TypeError):
-          s.split(2)
-
-  def test_urlone(self):
-      String URL = driver.getCurrentUrl();
-      Assert.assertEquals(URL, "http://google.com" );
 
 if __name__ == '__main__':
     unittest.main()
--- tests/wpt/harness/wptrunner/executors/test.py
+++
@@ -1,6 +0,0 @@
-import webdriver
-session = webdriver.Session('127.0.0.1', 7000)
-session.start()
-
-session.url = 'http://google.com'
-print session.url
