be8916afd15194e55b3d9c37ffdbf9d10c48d831
dhaval0603 <dhaval0603@gmail.com>
2016-03-23 15:28:49 -0400

    Private browsing - Initial steps

--- components/script/dom/htmliframeelement.rs
+++ components/script/dom/htmliframeelement.rs
@@ -123,6 +123,7 @@ impl HTMLIFrameElement {
         let window = window.r();
         let (new_subpage_id, old_subpage_id) = self.generate_new_subpage_id();
         let new_pipeline_id = self.pipeline_id.get().unwrap();
+        let private_iframe = self.privatebrowsing();
 
         self.containing_page_pipeline_id.set(Some(window.pipeline()));
 
@@ -134,6 +135,7 @@ impl HTMLIFrameElement {
             old_subpage_id: old_subpage_id,
             new_pipeline_id: new_pipeline_id,
             sandbox: sandboxed,
+            is_private: private_iframe,
         };
         chan.send(ConstellationMsg::ScriptLoadedURLInIFrame(load_info)).unwrap();
 
@@ -247,6 +249,17 @@ impl HTMLIFrameElement {
                       ReflowQueryType::NoQuery,
                       ReflowReason::IFrameLoadEvent);
     }
+
+    /// Check whether the iframe has the mozprivatebrowsing attribute set
+    pub fn privatebrowsing(&self) -> bool {
+        if self.Mozbrowser() {
+            let element = self.upcast::<Element>();
+            element.has_attribute(&Atom::from("mozprivatebrowsing"))
+        } else {
+            false
+        }
+    }
+
 }
 
 pub trait HTMLIFrameElementLayoutMethods {
