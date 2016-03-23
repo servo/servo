d0606c9f061e3b323eb945652379eabcf02085fd
sneha1302 <smshah4@ncsu.edu>
2016-03-23 01:38:33 -0400

    HTMLImageElement , currentRequest and pendingrequest working

--- components/script/dom/htmlimageelement.rs
+++ components/script/dom/htmlimageelement.rs
@@ -153,23 +153,13 @@ impl HTMLImageElement {
         }
     }
     fn new_inherited(localName: Atom, prefix: Option<DOMString>, document: &Document) -> HTMLImageElement {
-        
-        let currreq: ImageRequest = ImageRequest{
-            state: State::Unavailable,
-            url: DOMRefCell::new(None),
-            image: DOMRefCell::new(None),
-            metadata: DOMRefCell::new(None) 
-        };
-
         HTMLImageElement {
             htmlelement: HTMLElement::new_inherited(localName, prefix, document),
             //url: DOMRefCell::new(None),
             //image: DOMRefCell::new(None),
             //metadata: DOMRefCell::new(None),
-            currentrequest: ImageRequest(State::Unavailable,None,None,None),
-            ImageRequest: pendingrequest(State::Unavailable,None,None,None),
-        //    currentrequest: currreq,
-        //    pendingrequest: currreq,
+            currentrequest: ImageRequest{state: State::CompletelyAvailable,url:DOMRefCell::new(None),image:DOMRefCell::new(None),metadata:DOMRefCell::new(None)},
+            pendingrequest: ImageRequest{state: State::Unavailable,url:DOMRefCell::new(None),image:DOMRefCell::new(None),metadata:DOMRefCell::new(None)},
         }
     }
 
@@ -318,8 +308,15 @@ impl HTMLImageElementMethods for HTMLImageElement {
         let image = self.currentrequest.image.borrow();
         image.is_some()
     }
-
-
+  /*  fn CurrentSrc(&self) -> DOMString {
+        let url = self.currentrequest.url.borrow();
+        let uri = *url;
+     //   DOMString::from(uri.serialize())
+        match *url {
+          Some(ref url) => DOMString::from(*url.serialize())        
+        }
+    }
+  */
     // https://html.spec.whatwg.org/multipage/#dom-img-name
     make_getter!(Name, "name");
 
