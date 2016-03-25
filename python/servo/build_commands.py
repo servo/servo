e83369dd25361d1b55feee42ddad681cb5e5ba87
Lars Bergstrom <lars@lars.com>
2016-03-25 10:50:54 -0500

    Enable NEON feature to get hard floats on Android devices

--- python/servo/build_commands.py
+++ python/servo/build_commands.py
@@ -225,6 +225,8 @@ class MachCommands(CommandBase):
         build_start = time()
         env = self.build_env()
         if android:
+            # Ensure Rust uses hard floats on Android
+            env['RUSTFLAGS'] = env.get('RUSTFLAGS', "") + " -C target-feature=+neon"
             # Build OpenSSL for android
             make_cmd = ["make"]
             if jobs is not None:
