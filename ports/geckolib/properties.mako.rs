d52299b17c1bcfaaea3ef9b5c949be14d21037ff
Bobby Holley <bobbyholley@gmail.com>
2016-03-28 23:29:18 -0700

    Move handling of additional-methods into the data structures.

--- ports/geckolib/properties.mako.rs
+++ ports/geckolib/properties.mako.rs
@@ -149,77 +149,9 @@ impl T${style_struct.name} for Gecko${style_struct.name} {
         unimplemented!()
     }
     % endfor
-    % if style_struct.name == "Animation":
-    fn transition_count(&self) -> usize {
-        unimplemented!()
-    }
-    % elif style_struct.name == "Border":
-    % for side in ["top", "right", "bottom", "left"]:
-    fn border_${side}_is_none_or_hidden_and_has_nonzero_width(&self) -> bool {
-        unimplemented!()
-    }
+    % for additional in style_struct.additional_methods:
+    ${additional.stub()}
     % endfor
-    % elif style_struct.name == "Box":
-    fn clone_display(&self) -> longhands::display::computed_value::T {
-        unimplemented!()
-    }
-    fn clone_position(&self) -> longhands::position::computed_value::T {
-        unimplemented!()
-    }
-    fn is_floated(&self) -> bool {
-        unimplemented!()
-    }
-    fn overflow_x_is_visible(&self) -> bool {
-        unimplemented!()
-    }
-    fn overflow_y_is_visible(&self) -> bool {
-        unimplemented!()
-    }
-    % elif style_struct.name == "Color":
-    fn clone_color(&self) -> longhands::color::computed_value::T {
-        unimplemented!()
-    }
-    % elif style_struct.name == "Font":
-    fn clone_font_size(&self) -> longhands::font_size::computed_value::T {
-        unimplemented!()
-    }
-    fn clone_font_weight(&self) -> longhands::font_weight::computed_value::T {
-        unimplemented!()
-    }
-    fn compute_font_hash(&mut self) {
-        unimplemented!()
-    }
-    % elif style_struct.name == "InheritedBox":
-    fn clone_direction(&self) -> longhands::direction::computed_value::T {
-        unimplemented!()
-    }
-    fn clone_writing_mode(&self) -> longhands::writing_mode::computed_value::T {
-        unimplemented!()
-    }
-    fn clone_text_orientation(&self) -> longhands::text_orientation::computed_value::T {
-        unimplemented!()
-    }
-    % elif style_struct.name == "InheritedText":
-    fn clone__servo_text_decorations_in_effect(&self) ->
-        longhands::_servo_text_decorations_in_effect::computed_value::T {
-        unimplemented!()
-    }
-    % elif style_struct.name == "Outline":
-    fn outline_is_none_or_hidden_and_has_nonzero_width(&self) -> bool {
-        unimplemented!()
-    }
-    % elif style_struct.name == "Text":
-    fn has_underline(&self) -> bool {
-        unimplemented!()
-    }
-    fn has_overline(&self) -> bool {
-        unimplemented!()
-    }
-    fn has_line_through(&self) -> bool {
-        unimplemented!()
-    }
-    % endif
-
 }
 
 % endfor
