cc6976d92c94037f98932ccc0dd6a9e138008b76
Ms2ger <Ms2ger@gmail.com>
2016-04-05 10:33:03 +0200

    Update webdriver.

--- components/webdriver_server/lib.rs
+++ components/webdriver_server/lib.rs
@@ -602,7 +602,7 @@ impl Handler {
             "page load" => self.load_timeout = value,
             "script" => self.script_timeout = value,
             x => return Err(WebDriverError::new(ErrorStatus::InvalidSelector,
-                                                &format!("Unknown timeout type {}", x)))
+                                                format!("Unknown timeout type {}", x)))
         }
         Ok(WebDriverResponse::Void)
     }
