b7257e15fc388bdc7b7a0b7ddae091134b469410
Matthias Krüger <matthias.krueger@famsik.de>
2016-04-07 02:19:16 +0200

    travis: add and run script which checks if listed files contain "unwrap". Should fix #10446.

---
+++ etc/ci/check_no_unwrap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Make sure listed files do not contain "unwrap"
+set -e
+
+cd `git rev-parse --show-toplevel` # cd into repo root so make sure paths works in any case
+
+# files that should not contain "unwrap"
+FILES="components/compositing/compositor.rs
+components/compositing/pipeline.rs
+components/compositing/constellation.rs"
+
+! grep -n "\unwrap(" ${FILES}
