a432fe377baebb16ab3b99223a51f85a33e0576f
Bobby Holley <bobbyholley@gmail.com>
2016-04-07 20:59:38 -0700

    Add more annotations to the properties.

--- components/style/properties.mako.rs
+++ components/style/properties.mako.rs
@@ -685,7 +685,7 @@ pub mod longhands {
         }
     </%self:longhand>
 
-    ${new_style_struct("InheritedBox", is_inherited=True,
+    ${new_style_struct("InheritedBox", is_inherited=True, gecko_name="nsStyleVisibility",
                        additional_methods=[Method("clone_direction",
                                                   "longhands::direction::computed_value::T"),
                                            Method("clone_writing_mode",
@@ -909,7 +909,7 @@ pub mod longhands {
     // CSS 2.1, Section 11 - Visual effects
 
     // Non-standard, see https://developer.mozilla.org/en-US/docs/Web/CSS/overflow-clip-box#Specifications
-    ${single_keyword("-servo-overflow-clip-box", "padding-box content-box", internal=True)}
+    ${single_keyword("-servo-overflow-clip-box", "padding-box content-box", internal=True, products="servo")}
 
     // FIXME(pcwalton, #2742): Implement scrolling for `scroll` and `auto`.
     ${single_keyword("overflow-x", "visible hidden scroll auto")}
@@ -959,7 +959,7 @@ pub mod longhands {
 
     // CSS 2.1, Section 12 - Generated content, automatic numbering, and lists
 
-    ${new_style_struct("Counters", is_inherited=False)}
+    ${new_style_struct("Counters", is_inherited=False, gecko_name="nsStyleContent")}
 
     <%self:longhand name="content">
         use cssparser::Token;
@@ -2217,7 +2217,7 @@ pub mod longhands {
     ${single_keyword("word-break", "normal break-all")}
 
     // TODO(pcwalton): Support `text-justify: distribute`.
-    ${single_keyword("text-justify", "auto none inter-word")}
+    ${single_keyword("text-justify", "auto none inter-word", products="servo")}
 
     ${new_style_struct("Text", is_inherited=False, gecko_name="nsStyleTextReset",
                        additional_methods=[Method("has_underline", "bool"),
@@ -2444,7 +2444,7 @@ pub mod longhands {
 
     ${single_keyword("table-layout", "auto fixed")}
 
-    ${new_style_struct("InheritedTable", is_inherited=True)}
+    ${new_style_struct("InheritedTable", is_inherited=True, gecko_name="nsStyleTableBorder")}
 
     ${single_keyword("border-collapse", "separate collapse")}
 
@@ -2560,7 +2560,7 @@ pub mod longhands {
 
     ${single_keyword("box-sizing", "content-box border-box")}
 
-    ${new_style_struct("Pointing", is_inherited=True)}
+    ${new_style_struct("Pointing", is_inherited=True, gecko_name="nsStyleUserInterface")}
 
     <%self:longhand name="cursor">
         pub use self::computed_value::T as SpecifiedValue;
@@ -2804,7 +2804,7 @@ pub mod longhands {
     </%self:longhand>
 
     // Box-shadow, etc.
-    ${new_style_struct("Effects", is_inherited=False)}
+    ${new_style_struct("Effects", is_inherited=False, gecko_name="nsStyleEffects")}
 
     <%self:longhand name="opacity">
         use cssparser::ToCss;
