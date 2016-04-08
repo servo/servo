c016ba92ee245c5e18eb218c326616166c88667d
Bobby Holley <bobbyholley@gmail.com>
2016-04-07 17:41:26 -0700

    Add automatic generation of geckolib property setters for keyword properties.
    This adds gecko support for ~20 CSS properties.

--- ports/geckolib/build.rs
+++ ports/geckolib/build.rs
@@ -62,7 +62,8 @@ try:
     style_template.render(PRODUCT='gecko')
 
     geckolib_template = Template(filename=os.environ['GECKOLIB_TEMPLATE'], input_encoding='utf8')
-    output = geckolib_template.render(STYLE_STRUCTS = style_template.module.STYLE_STRUCTS)
+    output = geckolib_template.render(STYLE_STRUCTS = style_template.module.STYLE_STRUCTS,
+                                      to_rust_ident = style_template.module.to_rust_ident)
     print(output.encode('utf8'))
 except:
     sys.stderr.write(exceptions.text_error_template().render().encode('utf8'))
