3f95e4c8e13ac1253f662804229e3b37c85894b6
Ms2ger <Ms2ger@gmail.com>
2016-04-11 10:30:08 +0200

    Use upcast() in XMLDocument methods.

--- components/script/dom/xmldocument.rs
+++ components/script/dom/xmldocument.rs
@@ -76,16 +76,16 @@ impl XMLDocument {
 impl XMLDocumentMethods for XMLDocument {
     // https://html.spec.whatwg.org/multipage/#dom-document-location
     fn GetLocation(&self) -> Option<Root<Location>> {
-        self.document.GetLocation()
+        self.upcast::<Document>().GetLocation()
     }
 
     // https://html.spec.whatwg.org/multipage/#dom-tree-accessors:supported-property-names
     fn SupportedPropertyNames(&self) -> Vec<DOMString> {
-        self.document.SupportedPropertyNames()
+        self.upcast::<Document>().SupportedPropertyNames()
     }
 
     // https://html.spec.whatwg.org/multipage/#dom-tree-accessors:dom-document-nameditem-filter
     fn NamedGetter(&self, _cx: *mut JSContext, name: DOMString, found: &mut bool) -> *mut JSObject {
-        self.document.NamedGetter(_cx, name, found)
+        self.upcast::<Document>().NamedGetter(_cx, name, found)
     }
 }
