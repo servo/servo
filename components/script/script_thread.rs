dcda51a8beba58fa708c8af3922b9f4b1644365b
Rebecca <rebstar6@gmail.com>
2016-04-11 18:26:45 -0400

    add script pass-through for referrer

--- components/script/script_thread.rs
+++ components/script/script_thread.rs
@@ -1894,8 +1894,8 @@ impl ScriptThread {
             cors: None,
             pipeline_id: Some(id),
             credentials_flag: true,
-            referrer_policy: None,
-            referrer_url: None,
+            referrer_policy: load_data.referrer_policy,
+            referrer_url: load_data.referrer_url,
         }, LoadConsumer::Listener(response_target), None)).unwrap();
 
         self.incomplete_loads.borrow_mut().push(incomplete);
