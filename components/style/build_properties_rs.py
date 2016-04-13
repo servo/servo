005fcaea583a55f86b456b0db67e5b7fc9745b35
Per Lundberg <perlun@gmail.com>
2016-04-13 23:11:19 +0300

    Added support for %include files, by providing a TemplateLookup.

--- components/style/build_properties_rs.py
+++ components/style/build_properties_rs.py
@@ -1,11 +1,15 @@
 import os
 import sys
 
-from mako.template import Template
 from mako import exceptions
+from mako.lookup import TemplateLookup
+from mako.template import Template
 
 try:
-    template = Template(open(os.environ['TEMPLATE'], 'rb').read(), input_encoding='utf8')
+    lookup = TemplateLookup(directories=['.'])
+    template = Template(open(os.environ['TEMPLATE'], 'rb').read(),
+                        input_encoding='utf8',
+                        lookup=lookup)
     print(template.render(PRODUCT=os.environ['PRODUCT']).encode('utf8'))
 except:
     sys.stderr.write(exceptions.text_error_template().render().encode('utf8'))
