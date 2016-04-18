771628b473cc0639f826b4cb209bccb59a49f2c6
Sagar Muchhal <muchhalsagar88@gmail.com>
2016-04-18 16:29:24 -0400

    Private browsing with tests

--- components/script/dom/document.rs
+++ components/script/dom/document.rs
@@ -2565,7 +2565,8 @@ impl DocumentMethods for Document {
 
         let url = self.url();
         let (tx, rx) = ipc::channel().unwrap();
-        let _ = self.window.resource_threads().send(GetCookiesForUrl((*url).clone(), tx, NonHTTP));
+        let _ = self.window.resource_threads().send(GetCookiesForUrl(
+            self.window.pipeline(), (*url).clone(), tx, NonHTTP));
         let cookies = rx.recv().unwrap();
         Ok(cookies.map_or(DOMString::new(), DOMString::from))
     }
@@ -2583,7 +2584,7 @@ impl DocumentMethods for Document {
         let url = self.url();
         let _ = self.window
                     .resource_threads()
-                    .send(SetCookiesForUrl((*url).clone(), String::from(cookie), NonHTTP));
+                    .send(SetCookiesForUrl(self.window.pipeline(), (*url).clone(), String::from(cookie), NonHTTP));
         Ok(())
     }
 
