91a1aa763089a8a1cca84946e07fb348651aa3ce
Simon Sapin <simon.sapin@exyr.org>
2016-04-23 01:16:29 +0200

    Make chrome: URLs have a "host".

--- components/net/chrome_loader.rs
+++ components/net/chrome_loader.rs
@@ -8,14 +8,18 @@ use net_traits::{LoadConsumer, LoadData, NetworkError};
 use resource_thread::{CancellationListener, send_error};
 use std::sync::Arc;
 use url::Url;
+use url::percent_encoding::percent_decode;
 use util::resource_files::resources_dir_path;
 
 pub fn resolve_chrome_url(url: &Url) -> Result<Url, ()> {
     assert_eq!(url.scheme(), "chrome");
+    if url.host_str() != Some("resources") {
+        return Err(())
+    }
     let resources = resources_dir_path();
     let mut path = resources.clone();
     for segment in url.path_segments().unwrap() {
-        path.push(segment)
+        path.push(&*try!(percent_decode(segment.as_bytes()).decode_utf8().map_err(|_| ())))
     }
     // Don't allow chrome URLs access to files outside of the resources directory.
     if !(path.starts_with(resources) && path.exists()) {
