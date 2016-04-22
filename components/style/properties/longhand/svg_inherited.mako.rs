6ca3d78713630aafa8ae78d58655c53315b0b43d
Per Lundberg <perlun@gmail.com>
2016-04-23 00:35:13 +0300

    Moved the rest of the longhand structs to separate files.
    Sorry for the bulk size of this; I know already it's not going to be a fun thing to review. Nevertheless, it should be done so we finalize the split of this huge file into smaller, more maintable parts.
    
    The content of stuff being moved to separate files is unchanged. Only some minor formatting changes have been made and similar, but nothing of particular interest. IMHO it should be safe to merge if all the tests are fine.

---
+++ components/style/properties/longhand/svg_inherited.mako.rs
@@ -0,0 +1,38 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+<%namespace name="helpers" file="/helpers.mako.rs" />
+
+// SVG 1.1 (Second Edition)
+// https://www.w3.org/TR/SVG/
+<% data.new_style_struct("SVGInherited",
+                         inherited=True,
+                         gecko_ffi_name="nsStyleSVG") %>
+
+// Section 10 - Text
+
+${helpers.single_keyword("text-anchor", "start middle end", products="gecko")}
+
+// Section 11 - Painting: Filling, Stroking and Marker Symbols
+${helpers.single_keyword("color-interpolation", "auto sRGB linearRGB", products="gecko")}
+
+${helpers.single_keyword("color-interpolation-filters",
+                         "auto sRGB linearRGB",
+                         products="gecko",
+                         gecko_constant_prefix="NS_STYLE_COLOR_INTERPOLATION")}
+
+${helpers.single_keyword("fill-rule", "nonzero evenodd", products="gecko")}
+
+${helpers.single_keyword("shape-rendering",
+                         "auto optimizeSpeed crispEdges geometricPrecision",
+                         products="gecko")}
+
+${helpers.single_keyword("stroke-linecap", "butt round square", products="gecko")}
+
+${helpers.single_keyword("stroke-linejoin", "miter round bevel", products="gecko")}
+
+// Section 14 - Clipping, Masking and Compositing
+${helpers.single_keyword("clip-rule", "nonzero evenodd",
+                         products="gecko",
+                         gecko_constant_prefix="NS_STYLE_FILL_RULE")}
