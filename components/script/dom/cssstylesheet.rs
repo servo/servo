5f8a60cefc0c188b986bb4be0ef90f7d60ab91de
Isha Bobra <ibobra@localhost.localdomain>
2016-04-22 18:28:03 -0400

    addressed majority of review comments

--- components/script/dom/cssstylesheet.rs
+++ components/script/dom/cssstylesheet.rs
@@ -3,50 +3,43 @@ use dom::bindings::codegen::Bindings::CSSStyleSheetBinding::CSSStyleSheetMethods
 use dom::bindings::global::GlobalRef;
 use dom::bindings::js::{Root};
 use dom::bindings::reflector::{Reflector, reflect_dom_object};
-use dom::cssrule::CSSRule;
 use dom::cssrulelist::CSSRuleList;
 use dom::window::Window;
 use style::servo::Stylesheet;
 use std::sync::Arc;
-use util::str::DOMString;
 
 #[dom_struct]
 pub struct CSSStyleSheet {
-	reflector_: Reflector,
+        reflector_: Reflector,
 	stylesheet: Arc<Stylesheet>,
-	//ownerRule: CSSRule,
-	//cssRules: CSSRuleList,
+	cssrules: Root<CSSRuleList>,
 }
 
 impl CSSStyleSheet {
     #[allow(unrooted_must_root)]
-    fn new_inherited(stylesheet: Arc<Stylesheet>) -> CSSStyleSheet {
+    fn new_inherited(stylesheet: Arc<Stylesheet>, cssrules: Root<CSSRuleList>) -> CSSStyleSheet {
         CSSStyleSheet {
-        	reflector_: Reflector::new(),
-            //stylesheet: Arc::new(stylesheet)
+            reflector_: Reflector::new(),
             stylesheet: stylesheet,
+            cssrules: cssrules, // cssrule: CSSRule::new(),
         }
     }
 
     #[allow(unrooted_must_root)]
-    pub fn new(window: &Window, stylesheet: Arc<Stylesheet>) -> Root<CSSStyleSheet> {
-        reflect_dom_object(box CSSStyleSheet::new_inherited(stylesheet),
+    pub fn new(window: &Window, stylesheet: Arc<Stylesheet>, cssrules: Root<CSSRuleList>) -> Root<CSSStyleSheet> {
+        reflect_dom_object(box CSSStyleSheet::new_inherited(stylesheet, cssrules),
                            GlobalRef::Window(window),
                            CSSStyleSheetBinding::Wrap)
     }
 }
 
 impl CSSStyleSheetMethods for CSSStyleSheet {
-	
-	fn InsertRule(&self, rule: DOMString, index: u32) -> u32 {
-		0
+	fn CssRules(&self) -> Root<CSSRuleList>  {
+	     /*if self.disabled() == false {
+                 panic!(DOMErrorName::SecurityError)
+             }
+             else{*/
+                self.cssrules.clone()
+             //}    
 	}
-
-	fn DeleteRule(&self, index: u32) {
-		//self.stylesheet.borrow_mut().rules.remove(index);
-	}
-
-	/*fn CssRules(&self) -> Root<CSSRuleList> {
-
-	}*/
 }
