dd8ddb1c59d0af75d73b320876dda6d9a709e7bb
Tyler Southwick <tyler@northfuse.net>
2016-04-22 01:32:01 -0700

    implement webglcontextevent

--- components/script/dom/document.rs
+++ components/script/dom/document.rs
@@ -77,6 +77,7 @@ use dom::touchevent::TouchEvent;
 use dom::touchlist::TouchList;
 use dom::treewalker::TreeWalker;
 use dom::uievent::UIEvent;
+use dom::webglcontextevent::WebGLContextEvent;
 use dom::window::{ReflowReason, Window};
 use encoding::EncodingRef;
 use encoding::all::UTF_8;
@@ -2168,6 +2169,8 @@ impl DocumentMethods for Document {
                         &TouchList::new(&self.window, &[]),
                     )
                 )),
+            "webglcontextevent" =>
+                Ok(Root::upcast(WebGLContextEvent::new_uninitialized(GlobalRef::Window(&self.window)))),
             _ =>
                 Err(Error::NotSupported),
         }
