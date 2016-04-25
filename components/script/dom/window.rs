ee613af929c2ded4d7e90dc5b2c4640d44de7119
Connor Brewster <brewsterc@my.caspercollege.edu>
2016-04-25 14:59:48 -0600

    Implement alert dialogs
    Fixed conflict
    
    Fixed merge issue
    
    Finished implementation

--- components/script/dom/window.rs
+++ components/script/dom/window.rs
@@ -425,10 +425,7 @@ impl WindowMethods for Window {
         stdout.flush().unwrap();
         stderr.flush().unwrap();
 
-        // https://developer.mozilla.org/en-US/docs/Web/Events/mozbrowsershowmodalprompt
-        let event = MozBrowserEvent::ShowModalPrompt("alert".to_owned(), "Alert".to_owned(),
-                                                     String::from(s), "".to_owned());
-        self.Document().trigger_mozbrowser_event(event);
+        self.constellation_chan().0.send(ConstellationMsg::Alert(self.pipeline(), s.to_string())).unwrap();
     }
 
     // https://html.spec.whatwg.org/multipage/#dom-window-close
