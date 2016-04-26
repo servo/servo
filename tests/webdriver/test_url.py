50d15f98ec82e6613337430f132539410c53f84c
Krunal Gala <krunal3103@gmail.com>
2016-04-26 17:00:20 -0400

    test fixed and simplefied

--- tests/webdriver/test_url.py
+++ tests/webdriver/test_url.py
@@ -13,34 +13,21 @@ import os.path as path
 import webdriver
 import subprocess
 import server
+from ServoProcess import ServoProcess
 
+def handler(self):
+    return 200, [('Content-Type', 'text/html')], '<html><body>hi there</body></html>'
 
-class TestStringMethods(unittest.TestCase):
-    
-    
-    def test_get(self):
-        from ServoProcess import ServoProcess
-        import server
-        import webdriver
-
-        def handler(self):
-            return 200, [('Content-Type', 'text/html')], '<html><body>hi there</body></html>'
-
-        with ServoProcess():
-            server.serve(handler)
-            session = webdriver.Session('127.0.0.1', 7000)
-            session.start()
-            session.url = "http://localhost:8001"
-            self.assertEqual(session.url, "http://localhost:8001/")
-            #session.end()
-            server.stop()
+with ServoProcess():
+    server.serve(handler)
+    session = webdriver.Session('127.0.0.1', 7000)
+    session.start()
+    session.url = "http://localhost:8001"
+    try:
+        assert session.url == "http://localhost:8001/"
+    except AssertionError, e:
+        raise Exception("URL don`t match!") 
+    #session.end()
+    server.stop()
         
 
-# if __name__ == '__main__':
-#     unittest.main()    
-
-suite = unittest.TestSuite()
-for method in dir(TestStringMethods):
-    if method.startswith("test"):
-        suite.addTest(TestStringMethods(method))
-unittest.TextTestRunner().run(suite)
