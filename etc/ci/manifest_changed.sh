42047ef361f6f250149ec39ea72fa6cc01d23ae0
askeing <askeing@gmail.com>
2016-04-29 20:54:11 +0900

    The manifest_changed.sh doesn't need binary to update the manifests

--- etc/ci/manifest_changed.sh
+++ etc/ci/manifest_changed.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-./mach test-wpt --manifest-update SKIP_TESTS > /dev/null
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# We shouldn't need any binary at all to update the manifests.
+# Adding "SKIP_TESTS" to skip tests, and "--binary=" to skip binary check.
+./mach test-wpt --manifest-update --binary= SKIP_TESTS > /dev/null
+
 diff=$(git diff -- tests/*/MANIFEST.json)
 echo "$diff"
 [[ ! $diff ]]
