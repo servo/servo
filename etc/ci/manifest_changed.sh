ed2871e68187f4f26c07dd07d9f9cc242aa560f9
askeing <askeing@gmail.com>
2016-04-29 20:54:11 +0900

    Handle error when manifest_changed.sh failed

--- etc/ci/manifest_changed.sh
+++ etc/ci/manifest_changed.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-./mach test-wpt --manifest-update SKIP_TESTS > /dev/null
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if `./mach test-wpt --manifest-update SKIP_TESTS > /dev/null`
+then
+    ./mach test-wpt --release --manifest-update SKIP_TESTS > /dev/null
+fi
 diff=$(git diff -- tests/*/MANIFEST.json)
 echo "$diff"
 [[ ! $diff ]]
