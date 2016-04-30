c113d1ef1c2dd567806c9be1ba9e5fd827217ee3
6br <ina_univ-science@yahoo.co.jp>
2016-04-30 13:45:06 +0900

    Rename check_no_unwrap.sh to check_no_panic.sh

--- etc/ci/check_no_panic.sh
+++ etc/ci/check_no_panic.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #
-# Make sure listed files do not contain "unwrap"
+# Make sure listed files do not use unwrap() or panic!()
 set -o errexit
 set -o nounset
 set -o pipefail
 
 cd $(git rev-parse --show-toplevel) # cd into repo root so make sure paths works in any case
 
-# files that should not contain "unwrap"
 FILES=("components/compositing/compositor.rs"
        "components/compositing/pipeline.rs"
        "components/compositing/constellation.rs")
