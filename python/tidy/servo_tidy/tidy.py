a2b0585ffdc2eeffd61ab967901d0e9fac9e0870
Matt Brubeck <mbrubeck@limpet.net>
2016-05-02 16:05:31 -0700

    Update lazy_static, selectors, string_cache, futf, quickersort

--- python/tidy/servo_tidy/tidy.py
+++ python/tidy/servo_tidy/tidy.py
@@ -231,7 +231,7 @@ def check_lock(file_name, contents):
         raise StopIteration
 
     # package names to be neglected (as named by cargo)
-    exceptions = ["bitflags", "xml-rs", "byteorder"]
+    exceptions = ["bitflags", "xml-rs", "lazy_static"]
 
     import toml
     content = toml.loads(contents)
