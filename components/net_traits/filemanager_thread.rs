06edf6d03b58ff2ff16733cffe6bfdda0ffcbe2a
Zhen Zhang <izgzhen@gmail.com>
2016-05-03 08:47:27 +0800

    add filemanager_thread

---
+++ components/net_traits/filemanager_thread.rs
@@ -0,0 +1,31 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+use ipc_channel::ipc::IpcSender;
+use std::path::PathBuf;
+use uuid::Uuid;
+
+#[derive(Deserialize, Serialize)]
+pub enum FileManagerThreadMsg {
+    /// Select a single file, return triple (FileID, FileName, lastModified)
+    SelectFile(IpcSender<FileManagerResult<(Uuid, PathBuf, u64)>>),
+
+    /// Read file, return the bytes
+    ReadFile(IpcSender<FileManagerResult<Vec<u8>>>, Uuid),
+
+    /// Delete the FileID entry
+    DeleteFileID(Uuid),
+}
+
+pub type FileManagerResult<T> = Result<T, FileManagerThreadError>;
+
+#[derive(Deserialize, Serialize)]
+pub enum FileManagerThreadError {
+    /// The selection action is invalid, nothing is selected
+    InvalidSelection,
+    /// Failure to process file information such as file name, modified time etc.
+    FileInfoProcessingError,
+    /// Failure to read the file content
+    ReadFileError,
+}
