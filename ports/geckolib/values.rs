6ee89b5c5c7a1138730f45305c0a0b84d95f6f92
Cameron McCormack <cam@mcc.id.au>
2016-05-04 14:51:42 +1000

    Support max-{width,height} in geckolib.

--- ports/geckolib/values.rs
+++ ports/geckolib/values.rs
@@ -3,7 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 use gecko_style_structs::{nsStyleUnion, nsStyleUnit};
-use style::values::computed::{LengthOrPercentage, LengthOrPercentageOrAuto};
+use style::values::computed::{LengthOrPercentage, LengthOrPercentageOrAuto, LengthOrPercentageOrNone};
 
 pub trait ToGeckoStyleCoord {
     fn to_gecko_style_coord(&self, unit: &mut nsStyleUnit, union: &mut nsStyleUnion);
@@ -41,6 +41,26 @@ impl ToGeckoStyleCoord for LengthOrPercentageOrAuto {
                 unsafe { *union.mInt.as_mut() = 0; }
             },
             LengthOrPercentageOrAuto::Calc(_) => unimplemented!(),
+        };
+    }
+}
+
+impl ToGeckoStyleCoord for LengthOrPercentageOrNone {
+    fn to_gecko_style_coord(&self, unit: &mut nsStyleUnit, union: &mut nsStyleUnion) {
+        match *self {
+            LengthOrPercentageOrNone::Length(au) => {
+                *unit = nsStyleUnit::eStyleUnit_Coord;
+                unsafe { *union.mInt.as_mut() = au.0; }
+            },
+            LengthOrPercentageOrNone::Percentage(p) => {
+                *unit = nsStyleUnit::eStyleUnit_Percent;
+                unsafe { *union.mFloat.as_mut() = p; }
+            },
+            LengthOrPercentageOrNone::None => {
+                *unit = nsStyleUnit::eStyleUnit_None;
+                unsafe { *union.mInt.as_mut() = 0; }
+            },
+            LengthOrPercentageOrNone::Calc(_) => unimplemented!(),
         };
     }
 }
