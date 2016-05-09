2c06752d2c82d16acff0a4e4d1c724284d5323a9
Rohit Zambre <RZ13@dhcp-v014-249.mobile.uci.edu>
2016-05-09 09:49:50 -0700

    first fix to tidy-test errors
    replace unwrap with match ok/err
    
    fixing more tidy-test errors and removing another unwrap from compositor.rs

--- tests/unit/profile/lib.rs
+++ tests/unit/profile/lib.rs
@@ -2,9 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+extern crate ipc_channel;
 extern crate profile;
 extern crate profile_traits;
-extern crate ipc_channel;
 
 #[cfg(test)]
 mod time;
