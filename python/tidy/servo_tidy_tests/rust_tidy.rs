504680ceba1623f5baf48bc1da1444add80dd19d
mrmiywj <jsyangwenjie@gmail.com>
2016-05-15 18:19:49 +0800

    add &Root<T> checker
    update rust_tidy tests
    
    update rust_tidy.rs and test_tidy.py

--- python/tidy/servo_tidy_tests/rust_tidy.rs
+++ python/tidy/servo_tidy_tests/rust_tidy.rs
@@ -35,10 +35,9 @@ impl test {
         }
     }
 
-    fn test_fun2(y : &String, z : &Vec<f32>) -> f32 {
+    fn test_fun2(y : &String, z : &Vec<f32>, r: &Root<isize>) -> f32 {
         let x = true;
         x
             && x;
     }
-
 }
