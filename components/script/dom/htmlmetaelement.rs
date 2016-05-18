2322de147245131dccecef580f8fb6a6cee980e4
Joseph P DeBartola III <josephdebartola@gmail.com>
2016-05-18 18:50:13 -0500

    Imported AttrValue into HTMLMetaElement

--- components/script/dom/htmlmetaelement.rs
+++ components/script/dom/htmlmetaelement.rs
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+use dom::attr::AttrValue;
 use dom::bindings::cell::DOMRefCell;
 use dom::bindings::codegen::Bindings::HTMLMetaElementBinding;
 use dom::bindings::codegen::Bindings::HTMLMetaElementBinding::HTMLMetaElementMethods;
