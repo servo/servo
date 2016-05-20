91ecb2f4e04ce7595a23c0b6b0cc4f1534959e41
Hugo Thiessard <hugo.thiessard@etu.u-bordeaux.fr>
2016-05-20 07:25:16 +0200

    Unused import deleted

--- components/script/dom/macros.rs
+++ components/script/dom/macros.rs
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-use dom::document::Document;
-
 #[macro_export]
 macro_rules! make_getter(
     ( $attr:ident, $htmlname:tt ) => (
