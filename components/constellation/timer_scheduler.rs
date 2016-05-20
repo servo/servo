082a752cef4d7c2101064bebbed7ec38e63ed615
Alan Jeffrey <ajeffrey@mozilla.com>
2016-05-19 19:23:18 -0500

    fixup

--- components/constellation/timer_scheduler.rs
+++ components/constellation/timer_scheduler.rs
@@ -6,9 +6,6 @@ use ipc_channel::ipc::{self, IpcSender};
 use script_traits::{TimerEvent, TimerEventRequest};
 use std::cmp::{self, Ord};
 use std::collections::BinaryHeap;
-use std::sync::Arc;
-use std::sync::atomic::AtomicBool;
-use std::sync::atomic::Ordering::{Acquire, Release};
 use std::sync::mpsc;
 use std::sync::mpsc::TryRecvError::{Disconnected, Empty};
 use std::thread;
@@ -44,10 +41,12 @@ impl TimerScheduler {
     pub fn start() -> IpcSender<TimerEventRequest> {
         let (req_ipc_sender, req_ipc_receiver) = ipc::channel().unwrap();
         let (req_sender, req_receiver) = mpsc::sync_channel(1);
-        let queued_requests_1 = Arc::new(AtomicBool::new(false));
-        let queued_requests_2 = queued_requests_1.clone();
 
-        // util::thread doesn't give us access to the JoinHandle, which we need for park/unpark.
+        // We could do this much more directly with recv_timeout
+        // (https://github.com/rust-lang/rfcs/issues/962).
+
+        // util::thread doesn't give us access to the JoinHandle, which we need for park/unpark,
+        // so we use the builder directly.
         let timeout_thread = thread::Builder::new()
             .name(String::from("TimerScheduler"))
             .spawn(move || {
@@ -69,7 +68,6 @@ impl TimerScheduler {
                             let TimerEventRequest(_, _, _, delay) = req;
                             let schedule = Instant::now() + Duration::from_millis(delay.get());
                             let event = ScheduledEvent { request: req, for_time: schedule };
-                            queued_requests_1.store(false, Release);
                             scheduled_events.push(event);
                         },
                         Err(Empty) => match scheduled_events.peek() {
@@ -88,13 +86,8 @@ impl TimerScheduler {
             .name(String::from("TimerProxy"))
             .spawn(move || {
                 while let Ok(req) = req_ipc_receiver.recv() {
-                    queued_requests_2.store(true, Release);
                     req_sender.send(req).unwrap();
-                    // Annoyingly, park has no test-and-park option, so we have to spin-wait here.
-                    while queued_requests_2.load(Acquire) {
-                        timeout_thread.unpark();
-                        thread::yield_now();
-                    }
+                    timeout_thread.unpark();
                 }
             })
             .unwrap();
