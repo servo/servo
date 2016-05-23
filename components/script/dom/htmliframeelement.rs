bc1fe067cf8a8a45ca9385fa863e01c5990eaf74
Anthony Ramine <n.oxyde@gmail.com>
2016-05-24 00:56:48 +0200

    Use [Func] on HTMLIFrameElement.mozbrowser

--- components/script/dom/htmliframeelement.rs
+++ components/script/dom/htmliframeelement.rs
@@ -438,23 +438,14 @@ impl HTMLIFrameElementMethods for HTMLIFrameElement {
 
     // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe#attr-mozbrowser
     fn Mozbrowser(&self) -> bool {
-        // We don't want to allow mozbrowser iframes within iframes
-        let is_root_pipeline = window_from_node(self).parent_info().is_none();
-        if mozbrowser_enabled() && is_root_pipeline {
-            let element = self.upcast::<Element>();
-            element.has_attribute(&atom!("mozbrowser"))
-        } else {
-            false
-        }
+        let element = self.upcast::<Element>();
+        element.has_attribute(&atom!("mozbrowser"))
     }
 
     // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe#attr-mozbrowser
-    fn SetMozbrowser(&self, value: bool) -> ErrorResult {
-        if mozbrowser_enabled() {
-            let element = self.upcast::<Element>();
-            element.set_bool_attribute(&atom!("mozbrowser"), value);
-        }
-        Ok(())
+    fn SetMozbrowser(&self, value: bool) {
+        let element = self.upcast::<Element>();
+        element.set_bool_attribute(&atom!("mozbrowser"), value);
     }
 
     // https://developer.mozilla.org/en-US/docs/Web/API/HTMLIFrameElement/goBack
