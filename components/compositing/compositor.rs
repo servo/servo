041c907d1b88eee8909a65b66fbd1aaf934c293b
Ms2ger <Ms2ger@gmail.com>
2016-05-23 11:19:07 +0200

    Don't block the script listener thread on window.close().
    This does not appear to have any effect. I believe this has been the case
    since e84106535175211526729e532058a7514a0de372, when the helper thread was
    introduced.
    
    It was added in a7ef1cd35e9347a285f245041db4eb94047f4ab0, where it blocked the
    script thread rather than this helper thread.

--- components/compositing/compositor.rs
+++ components/compositing/compositor.rs
@@ -529,9 +529,8 @@ impl<Window: WindowMethods> IOCompositor<Window> {
                 return false
             }
 
-            (Msg::Exit(channel), _) => {
+            (Msg::Exit, _) => {
                 self.start_shutting_down();
-                let _ = channel.send(());
             }
 
             (Msg::ShutdownComplete, _) => {
