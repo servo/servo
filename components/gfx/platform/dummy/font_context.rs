74e79e96c5bd269eef0fb0d97e1f03e281247ba8
Jack Moffitt <jack@metajack.im>
2016-05-26 14:40:09 -0600

    Native MSVC windows build, convert to cmake

--- components/gfx/platform/dummy/font_context.rs
+++ components/gfx/platform/dummy/font_context.rs
@@ -2,8 +2,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-extern crate cmake;
+#[derive(Clone, HeapSizeOf, Debug)]
+pub struct FontContextHandle;
 
-fn main() {
-    let _ = cmake::Config::new(".").build();
+impl FontContextHandle {
+    pub fn new() -> FontContextHandle {
+        FontContextHandle
+    }
 }
+
