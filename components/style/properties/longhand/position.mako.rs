a4e5534d004fb9a513710b86714005f124659ac3
Pu Xingyu <pu.stshine@gmail.com>
2016-05-27 09:04:37 +0800

    Fixup for `align-self` and tests
    This is a fixup for `align-self`, the computed value of which should now
    properly inherit `align-items` value of its parent. Additional passed
    tests is also removed from ini folder. Fixup for geckolib errors.

--- components/style/properties/longhand/position.mako.rs
+++ components/style/properties/longhand/position.mako.rs
@@ -61,22 +61,36 @@
 // http://www.w3.org/TR/css3-flexbox/
 
 // Flex container properties
-${helpers.single_keyword("flex-direction", "row row-reverse column column-reverse")}
+${helpers.single_keyword("flex-direction", "row row-reverse column column-reverse", experimental=True)}
 
-${helpers.single_keyword("flex-wrap", "nowrap wrap wrap-reverse")}
+${helpers.single_keyword("flex-wrap", "nowrap wrap wrap-reverse", experimental=True)}
 
-${helpers.single_keyword("justify-content", "flex-start flex-end center space-between space-around")}
+// FIXME(stshine): The type of 'justify-content' and 'align-content' is uint16_t in gecko
+// FIXME(stshine): Its higher bytes are used to store fallback value. Disable them in geckolib for now
+${helpers.single_keyword("justify-content", "flex-start flex-end center space-between space-around",
+                         experimental=True,
+                         gecko_constant_prefix="NS_STYLE_JUSTIFY",
+                         products="servo")}
 
-${helpers.single_keyword("align-items", "stretch flex-start flex-end center baseline")}
+${helpers.single_keyword("align-items", "stretch flex-start flex-end center baseline",
+                         experimental=True,
+                         need_clone=True,
+                         gecko_constant_prefix="NS_STYLE_ALIGN")}
 
-${helpers.single_keyword("align-content", "stretch flex-start flex-end center space-between space-around")}
+${helpers.single_keyword("align-content", "stretch flex-start flex-end center space-between space-around",
+                         experimental=True,
+                         gecko_constant_prefix="NS_STYLE_ALIGN",
+                         products="servo")}
 
 // Flex item properties
-${helpers.predefined_type("flex-grow", "Number", "0.0", "parse_non_negative")}
+${helpers.predefined_type("flex-grow", "Number", "0.0", "parse_non_negative", experimental=True)}
 
-${helpers.predefined_type("flex-shrink", "Number", "1.0", "parse_non_negative")}
+${helpers.predefined_type("flex-shrink", "Number", "1.0", "parse_non_negative", experimental=True)}
 
-${helpers.single_keyword("align-self", "auto stretch flex-start flex-end center baseline")}
+${helpers.single_keyword("align-self", "auto stretch flex-start flex-end center baseline",
+                         experimental=True,
+                         need_clone=True,
+                         gecko_constant_prefix="NS_STYLE_ALIGN")}
 
 // https://drafts.csswg.org/css-flexbox/#propdef-order
 <%helpers:longhand name="order">
