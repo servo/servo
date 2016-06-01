679cf0a5e2af069b58825309fdd10be676c32433
Zhen Zhang <izgzhen@gmail.com>
2016-06-01 10:09:07 +0800

    Add blob loader

---
+++ components/net_traits/blob_url_store.rs
@@ -0,0 +1,50 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+use ipc_channel::ipc::IpcSender;
+use std::str::FromStr;
+use url::Url;
+use uuid::Uuid;
+
+/// Errors returns to BlobURLStoreMsg::Request
+#[derive(Clone, Serialize, Deserialize)]
+pub enum BlobURLStoreError {
+    /// Invalid UUID key
+    InvalidKey,
+    /// Invalid URL origin
+    InvalidOrigin,
+}
+
+/// Blob URL store entry, a packaged form of Blob DOM object
+#[derive(Clone, Serialize, Deserialize)]
+pub struct BlobURLStoreEntry {
+    /// MIME type string
+    pub type_string: String,
+    /// Some filename if the backend of Blob is a file
+    pub filename: Option<String>,
+    /// Size of content in bytes
+    pub size: u64,
+    /// Content of blob
+    pub bytes: Vec<u8>,
+}
+
+/// Message-passing style interface between store and loader
+#[derive(Serialize, Deserialize)]
+pub enum BlobURLStoreMsg {
+    /// Request for an blob entry identified by uuid
+    Request(Uuid, IpcSender<Result<BlobURLStoreEntry, BlobURLStoreError>>),
+}
+
+/// Parse URL as Blob URL scheme's definition
+/// https://w3c.github.io/FileAPI/#DefinitionOfScheme
+pub fn parse_blob_url(url: &Url) -> Option<(Uuid, Option<&str>)> {
+    url.path_segments().and_then(|mut segments| {
+        let id_str = match (segments.next(), segments.next()) {
+            (Some(s), None) => s,
+            _ => return None,
+        };
+
+        Uuid::from_str(id_str).map(|id| (id, url.fragment())).ok()
+    })
+}
