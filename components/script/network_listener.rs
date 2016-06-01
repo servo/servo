45f48f4cb9c2d4b40c618260dd5892272bc87255
Manish Goregaokar <manishsmail@gmail.com>
2016-06-01 14:59:55 +0530

    Abstractify the Listener abstractions further

--- components/script/network_listener.rs
+++ components/script/network_listener.rs
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-use net_traits::{AsyncResponseListener, ResponseAction};
+use net_traits::{Action, AsyncResponseListener, ResponseAction};
 use script_runtime::ScriptThreadEventCategory::NetworkEvent;
 use script_runtime::{CommonScriptMsg, ScriptChan};
 use script_thread::Runnable;
@@ -10,19 +10,26 @@ use std::sync::{Arc, Mutex};
 
 /// An off-thread sink for async network event runnables. All such events are forwarded to
 /// a target thread, where they are invoked on the provided context object.
-pub struct NetworkListener<T: AsyncResponseListener + PreInvoke + Send + 'static> {
-    pub context: Arc<Mutex<T>>,
+pub struct NetworkListener<Listener: PreInvoke + Send + 'static> {
+    pub context: Arc<Mutex<Listener>>,
     pub script_chan: Box<ScriptChan + Send>,
 }
 
-impl<T: AsyncResponseListener + PreInvoke + Send + 'static> NetworkListener<T> {
-    pub fn notify(&self, action: ResponseAction) {
+impl<Listener: PreInvoke + Send + 'static> NetworkListener<Listener> {
+    pub fn notify<A: Action<Listener>+Send+'static>(&self, action: A) {
         if let Err(err) = self.script_chan.send(CommonScriptMsg::RunnableMsg(NetworkEvent, box ListenerRunnable {
             context: self.context.clone(),
             action: action,
         })) {
             warn!("failed to deliver network data: {:?}", err);
         }
+    }
+}
+
+// helps type inference
+impl<Listener: AsyncResponseListener + PreInvoke + Send + 'static> NetworkListener<Listener> {
+    pub fn notify_action(&self, action: ResponseAction) {
+        self.notify(action);
     }
 }
 
@@ -36,13 +43,13 @@ pub trait PreInvoke {
 }
 
 /// A runnable for moving the async network events between threads.
-struct ListenerRunnable<T: AsyncResponseListener + PreInvoke + Send> {
-    context: Arc<Mutex<T>>,
-    action: ResponseAction,
+struct ListenerRunnable<A: Action<Listener>+Send+'static, Listener: PreInvoke + Send> {
+    context: Arc<Mutex<Listener>>,
+    action: A,
 }
 
-impl<T: AsyncResponseListener + PreInvoke + Send> Runnable for ListenerRunnable<T> {
-    fn handler(self: Box<ListenerRunnable<T>>) {
+impl<A: Action<Listener>+Send+'static, Listener: PreInvoke + Send> Runnable for ListenerRunnable<A, Listener> {
+    fn handler(self: Box<ListenerRunnable<A, Listener>>) {
         let this = *self;
         let mut context = this.context.lock().unwrap();
         if context.should_invoke() {
