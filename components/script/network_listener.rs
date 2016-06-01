22cb745f7c955d8ffeb31741a511e325053fe7ef
Manish Goregaokar <manishsmail@gmail.com>
2016-06-01 17:18:11 +0530

    Hook XHR up to fetch

--- components/script/network_listener.rs
+++ components/script/network_listener.rs
@@ -2,7 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-use net_traits::{Action, AsyncResponseListener, ResponseAction};
+use net_traits::{Action, AsyncResponseListener, FetchResponseListener};
+use net_traits::{FetchResponseMsg, ResponseAction};
 use script_runtime::ScriptThreadEventCategory::NetworkEvent;
 use script_runtime::{CommonScriptMsg, ScriptChan};
 use script_thread::Runnable;
@@ -29,6 +30,13 @@ impl<Listener: PreInvoke + Send + 'static> NetworkListener<Listener> {
 // helps type inference
 impl<Listener: AsyncResponseListener + PreInvoke + Send + 'static> NetworkListener<Listener> {
     pub fn notify_action(&self, action: ResponseAction) {
+        self.notify(action);
+    }
+}
+
+// helps type inference
+impl<Listener: FetchResponseListener + PreInvoke + Send + 'static> NetworkListener<Listener> {
+    pub fn notify_fetch(&self, action: FetchResponseMsg) {
         self.notify(action);
     }
 }
