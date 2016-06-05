753cb61dc6bcb21c4238514d3aa8cd7f2b024290
Ms2ger <Ms2ger@gmail.com>
2016-06-05 12:02:39 +0200

    Remove an obsolete comment in gfx::text.

--- components/gfx/text/mod.rs
+++ components/gfx/text/mod.rs
@@ -2,12 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* This file exists just to make it easier to import things inside of
- ./text/ without specifying the file they came out of imports.
-
-Note that you still must define each of the files as a module in
-servo.rc. This is not ideal and may be changed in the future. */
-
 pub use text::shaping::Shaper;
 pub use text::text_run::TextRun;
 
