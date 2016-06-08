479a2c12177bd7b031705b5ca66ab8b246059cd9
ddh <dianehosfelt@gmail.com>
2016-06-09 00:34:31 +0100

    added dom obj counting to decide sequential/parallel layout (#10110)

--- components/script/dom/element.rs
+++ components/script/dom/element.rs
@@ -2151,10 +2151,12 @@ impl VirtualMethods for Element {
             return;
         }
 
+        let doc = document_from_node(self);
         if let Some(ref value) = *self.id_attribute.borrow() {
-            let doc = document_from_node(self);
             doc.register_named_element(self, value.clone());
         }
+        // This is used for layout optimization.
+        doc.increment_dom_count();
     }
 
     fn unbind_from_tree(&self, context: &UnbindContext) {
@@ -2164,10 +2166,12 @@ impl VirtualMethods for Element {
             return;
         }
 
+        let doc = document_from_node(self);
         if let Some(ref value) = *self.id_attribute.borrow() {
-            let doc = document_from_node(self);
             doc.unregister_named_element(self, value.clone());
         }
+        // This is used for layout optimization.
+        doc.decrement_dom_count();
     }
 
     fn children_changed(&self, mutation: &ChildrenMutation) {
