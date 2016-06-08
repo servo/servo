8f2d624cbf2cf0dd73ee2a2a8b50381046b2be7c
Ms2ger <Ms2ger@gmail.com>
2016-06-08 12:21:16 +0200

    Move attributes for the bindings module to its mod.rs.

--- components/script/dom/mod.rs
+++ components/script/dom/mod.rs
@@ -217,9 +217,6 @@ pub mod abstractworkerglobalscope;
 pub mod activation;
 pub mod attr;
 pub mod beforeunloadevent;
-mod create;
-#[allow(unsafe_code)]
-#[deny(missing_docs, non_snake_case)]
 pub mod bindings;
 pub mod blob;
 pub mod bluetooth;
@@ -240,6 +237,7 @@ pub mod client;
 pub mod closeevent;
 pub mod comment;
 pub mod console;
+mod create;
 pub mod crypto;
 pub mod css;
 pub mod cssfontfacerule;
