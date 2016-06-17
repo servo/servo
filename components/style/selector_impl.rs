dca0b0c28add3c5cc52fb7855f9ba80dd921f603
Emilio Cobos Álvarez <me@emiliocobos.me>
2016-06-17 03:51:57 +0200

    style: Refactor to pass animations cleanly, land animation-name parsing as experimental

--- components/style/selector_impl.rs
+++ components/style/selector_impl.rs
@@ -87,6 +87,7 @@ pub trait SelectorImplExt : SelectorImpl + Sized {
         })
     }
 
+    fn pseudo_is_before_or_after(pseudo: &Self::PseudoElement) -> bool;
 
     fn pseudo_class_state_flag(pc: &Self::NonTSPseudoClass) -> ElementState;
 
@@ -106,6 +107,15 @@ pub enum PseudoElement {
 }
 
 impl PseudoElement {
+    #[inline]
+    pub fn is_before_or_after(&self) -> bool {
+        match *self {
+            PseudoElement::Before |
+            PseudoElement::After => true,
+            _ => false,
+        }
+    }
+
     #[inline]
     pub fn cascade_type(&self) -> PseudoElementCascadeType {
         match *self {
@@ -244,6 +254,11 @@ impl SelectorImplExt for ServoSelectorImpl {
     #[inline]
     fn pseudo_class_state_flag(pc: &NonTSPseudoClass) -> ElementState {
         pc.state_flag()
+    }
+
+    #[inline]
+    fn pseudo_is_before_or_after(pseudo: &PseudoElement) -> bool {
+        pseudo.is_before_or_after()
     }
 
     #[inline]
