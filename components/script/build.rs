dc0a954d0192a9637b10ec58b03e081f17d41e4d
Vladimir Vukicevic <vladimir@pobox.com>
2016-06-22 10:48:13 -0400

    Use Ninja for script build cmake, and explicitly use VC++ for windows-msvc

--- components/script/build.rs
+++ components/script/build.rs
@@ -3,11 +3,29 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 extern crate cmake;
+use std::env;
 
 fn main() {
     // This must use the Ninja generator -- it's the only one that
     // parallelizes cmake's output properly.  (Cmake generates
     // separate makefiles, each of which try to build
     // ParserResults.pkl, and then stomp on eachother.)
-    let _ = cmake::Config::new(".").generator("Ninja").build();
+    let mut build = cmake::Config::new(".");
+
+    build.generator("Ninja");
+
+    let target = env::var("TARGET").unwrap();
+    if target.contains("windows-msvc") {
+        // because we're using ninja, we need to explicitly set these
+        // to VC++, otherwise it'll try to use cc
+        build.define("CMAKE_C_COMPILER", "cl.exe")
+             .define("CMAKE_CXX_COMPILER", "cl.exe");
+        // We have to explicitly specify the full path to link.exe,
+        // for reasons that I don't understand.  If we just give
+        // link.exe, it tries to use script-*/out/link.exe, which of
+        // course does not exist.
+        build.define("CMAKE_LINKER", "C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VC\\bin\\amd64\\link.exe");
+    }
+
+    build.build();
 }
