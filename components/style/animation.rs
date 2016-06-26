8c68c2734ea2857975c33703da348efadbd15f91
Emilio Cobos Álvarez <me@emiliocobos.me>
2016-06-25 22:28:56 -0700

    fixup! style: Allow pausing and unpausing animations.

--- components/style/animation.rs
+++ components/style/animation.rs
@@ -86,13 +86,14 @@ impl KeyframesAnimationState {
             KeyframesRunningState::Running => {},
         }
 
-        let still_running = match self.iteration_state {
-            KeyframesIterationState::Finite(ref mut current, ref max) => {
-                *current += 1;
-                *current < *max
+        if let KeyframesIterationState::Finite(ref mut current, ref max) = self.iteration_state {
+            *current += 1;
+            // NB: This prevent us from updating the direction, which might be
+            // needed for the correct handling of animation-fill-mode.
+            if *current >= *max {
+                return false;
             }
-            KeyframesIterationState::Infinite => true,
-        };
+        }
 
         // Update the next iteration direction if applicable.
         match self.direction {
@@ -107,7 +108,7 @@ impl KeyframesAnimationState {
             _ => {},
         }
 
-        still_running
+        true
     }
 
     /// Updates the appropiate state from other animation.
@@ -132,8 +133,6 @@ impl KeyframesAnimationState {
 
         let mut new_started_at = old_started_at;
 
-        println!("running: {:?}, {:?}", self.running_state, old_running_state);
-
         // If we're unpausing the animation, fake the start time so we seem to
         // restore it.
         //
@@ -149,8 +148,6 @@ impl KeyframesAnimationState {
                 => *progress = (time::precise_time_s() - old_started_at) / old_duration,
             _ => {},
         }
-
-        println!("running after: {:?}", self.running_state);
 
         self.current_direction = old_direction;
         self.started_at = new_started_at;
