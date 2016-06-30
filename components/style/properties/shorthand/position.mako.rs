57161116c24a82d63ea5ec091218e1a2740d7454
Pu Xingyu <pu.stshine@gmail.com>
2016-06-30 19:01:38 +0800

    Add tests and small fixes for flexbox shorthands

--- components/style/properties/shorthand/position.mako.rs
+++ components/style/properties/shorthand/position.mako.rs
@@ -4,49 +4,47 @@
 
 <%namespace name="helpers" file="/helpers.mako.rs" />
 
+// https://drafts.csswg.org/css-flexbox/#flex-flow-property
 <%helpers:shorthand name="flex-flow" sub_properties="flex-direction flex-wrap"
                                      experimental="True">
     use properties::longhands::{flex_direction, flex_wrap};
 
     let mut direction = None;
     let mut wrap = None;
-    let mut any = false;
     loop {
         if direction.is_none() {
             if let Ok(value) = input.try(|input| flex_direction::parse(context, input)) {
                 direction = Some(value);
-                any = true;
                 continue
             }
         }
         if wrap.is_none() {
             if let Ok(value) = input.try(|input| flex_wrap::parse(context, input)) {
                 wrap = Some(value);
-                any = true;
                 continue
             }
         }
         break
     }
 
-    if any {
-        Ok(Longhands {
-            flex_direction: direction,
-            flex_wrap: wrap,
-        })
-    } else {
-        Err(())
+    if direction.is_none() && wrap.is_none() {
+        return Err(())
     }
+    Ok(Longhands {
+        flex_direction: direction,
+        flex_wrap: wrap,
+    })
 </%helpers:shorthand>
 
+// https://drafts.csswg.org/css-flexbox/#flex-property
 <%helpers:shorthand name="flex" sub_properties="flex-grow flex-shrink flex-basis"
                                 experimental="True">
     use app_units::Au;
     use values::specified::{Number, Length, LengthOrPercentageOrAutoOrContent};
 
     pub fn parse_flexibility(input: &mut Parser)
-                             -> Result<(Option<Number>, Option<Number>),()> {
-        let grow = Some(try!(Number::parse_non_negative(input)));
+                             -> Result<(Number, Option<Number>),()> {
+        let grow = try!(Number::parse_non_negative(input));
         let shrink = input.try(Number::parse_non_negative).ok();
         Ok((grow, shrink))
     }
@@ -54,7 +52,6 @@
     let mut grow = None;
     let mut shrink = None;
     let mut basis = None;
-    let mut any = false;
 
     if input.try(|input| input.expect_ident_matching("none")).is_ok() {
         return Ok(Longhands {
@@ -66,40 +63,29 @@
     loop {
         if grow.is_none() {
             if let Ok((flex_grow, flex_shrink)) = input.try(parse_flexibility) {
-                grow = flex_grow;
+                grow = Some(flex_grow);
                 shrink = flex_shrink;
-                any = true;
                 continue
             }
         }
         if basis.is_none() {
             if let Ok(value) = input.try(LengthOrPercentageOrAutoOrContent::parse) {
                 basis = Some(value);
-                any = true;
                 continue
             }
         }
         break
     }
-    if any {
-        if grow == None {
-            grow = Some(Number(1.0));
-        }
-        if shrink == None && basis == None {
-            basis = Some(LengthOrPercentageOrAutoOrContent::Length(Length::Absolute(Au(0))));
-        }
-        if shrink == None {
-            shrink = Some(Number(1.0));
-        }
-        if basis == None {
-            basis = Some(LengthOrPercentageOrAutoOrContent::Auto);
-        }
-        Ok(Longhands {
-            flex_grow: grow,
-            flex_shrink: shrink,
-            flex_basis: basis,
-        })
-    } else {
-        Err(())
+
+    if grow.is_none() && basis.is_none() {
+        return Err(())
     }
+    if shrink.is_none() && basis.is_none() {
+        basis = Some(LengthOrPercentageOrAutoOrContent::Length(Length::Absolute(Au(0))));
+    }
+    Ok(Longhands {
+        flex_grow: grow.or(Some(Number(1.0))),
+        flex_shrink: shrink.or(Some(Number(1.0))),
+        flex_basis: basis.or(Some(LengthOrPercentageOrAutoOrContent::Auto)),
+    })
 </%helpers:shorthand>
