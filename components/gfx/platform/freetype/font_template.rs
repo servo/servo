fb943ab54bfe8d1a58de965502a95497e3c621f0
Josh Matthews <josh@joshmatthews.net>
2016-07-01 11:26:32 -0400

    Make font template data load fallible. Fixes #12037.

--- components/gfx/platform/freetype/font_template.rs
+++ components/gfx/platform/freetype/font_template.rs
@@ -3,7 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 use std::fs::File;
-use std::io::Read;
+use std::io::{Read, Error};
 use string_cache::Atom;
 use webrender_traits::NativeFontHandle;
 
@@ -18,24 +18,24 @@ pub struct FontTemplateData {
 }
 
 impl FontTemplateData {
-    pub fn new(identifier: Atom, font_data: Option<Vec<u8>>) -> FontTemplateData {
+    pub fn new(identifier: Atom, font_data: Option<Vec<u8>>) -> Result<FontTemplateData, Error> {
         let bytes = match font_data {
             Some(bytes) => {
                 bytes
             },
             None => {
                 // TODO: Handle file load failure!
-                let mut file = File::open(&*identifier).unwrap();
+                let mut file = try!(File::open(&*identifier));
                 let mut buffer = vec![];
                 file.read_to_end(&mut buffer).unwrap();
                 buffer
             },
         };
 
-        FontTemplateData {
+        Ok(FontTemplateData {
             bytes: bytes,
             identifier: identifier,
-        }
+        })
     }
 
     /// Returns a clone of the data in this font. This may be a hugely expensive
