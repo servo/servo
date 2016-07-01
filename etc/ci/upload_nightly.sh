48db507f6dd301788ee2751395a90ae2f520263c
Jacob Clark <jacob.jh.clark@googlemail.com>
2016-07-01 08:45:37 +0100

    Add servo-latest on upload-nightly
    Allows easy linking to nightly builds.

--- etc/ci/upload_nightly.sh
+++ etc/ci/upload_nightly.sh
@@ -13,6 +13,7 @@ usage() {
 
 upload() {
     s3cmd put "${2}" "s3://servo-builds/nightly/${1}/"
+    s3cmd put "${2}" "s3://servo-builds/nightly/${1}/servo-latest${3}"
 }
 
 
@@ -22,17 +23,21 @@ main() {
         return 1
     fi
 
-    local platform package
+    local platform package extension
     platform="${1}"
 
     if [[ "${platform}" == "android" ]]; then
-        package=target/arm-linux-androideabi/release/*.apk
+        extension=.apk
+        package=target/arm-linux-androideabi/release/*${extension}
     elif [[ "${platform}" == "linux" ]]; then
-        package=target/*.tar.gz
+        extension=.tar.gz
+        package=target/*${extension}
     elif [[ "${platform}" == "mac" ]]; then
-        package=target/*.dmg
+        extension=.dmg
+        package=target/*${extension}
     elif [[ "${platform}" == "windows" ]]; then
-        package=target/*.tar.gz
+        extension=.tar.gz
+        package=target/*${extension}
     else
         usage >&2
         return 1
@@ -41,7 +46,7 @@ main() {
     # Lack of quotes on package allows glob expansion
     # Note that this is not robust in the case of embedded spaces
     # TODO(aneeshusa): make this glob robust using e.g. arrays or Python
-    upload "${platform}" ${package}
+    upload "${platform}" ${package} ${extension}
 }
 
 main "$@"
