30b970f4ae180d5e2f1056b07914c72cdf1c3963
Emilio Cobos Álvarez <me@emiliocobos.me>
2016-07-01 15:52:04 -0700

    style: Fix timing-function overriding from the keyframe declaration list.
    The previous behavior is plain wrong, since that array has always at least one
    element, so we effectively couldn't specify anything else than "ease" in our
    animations.

--- components/style/animation.rs
+++ components/style/animation.rs
@@ -626,7 +626,9 @@ where Impl: SelectorImplExt,
             // NB: The spec says that the timing function can be overwritten
             // from the keyframe style.
             let mut timing_function = style.get_box().animation_timing_function_mod(index);
-            if from_style.get_box().animation_timing_function_count() != 0 {
+            if last_keyframe.declared_timing_function {
+                // NB: animation_timing_function can never be empty, always has
+                // at least the default value (`ease`).
                 timing_function = from_style.get_box().animation_timing_function_at(0);
             }
 
