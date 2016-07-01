73abbeace3cd88047aca6590c359012169362cb0
Patrick Trottier <ptrottier@localhost.localdomain>
2016-07-01 14:57:16 -0400

    Remove u8 in HTMLIframeElement.rs

--- components/script/dom/htmliframeelement.rs
+++ components/script/dom/htmliframeelement.rs
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-extern crate bitflags;
-
 use document_loader::{LoadType, LoadBlocker};
 use dom::attr::Attr;
 use dom::bindings::cell::DOMRefCell;
