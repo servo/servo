d3c2759c960d8c99b9b2a80ae4240db04f85e8ef
Emilio Cobos Álvarez <me@emiliocobos.me>
2016-07-04 01:07:47 -0700

    stylo: Move all binding-generator code to a python script.
    This not only makes us more consistent with the rest of the codebase but also:
    
     * Makes us repeat less code like common flags and all that stuff.
     * Reduces the noise of the build: You only get the output of the commands on
       failure or when you pass the -v flag.
     * Makes you able to select a single kind of build or multiple in the same
       place.
    
    I've basically kept the regen.sh script because of the LIBCLANG_PATH checks, but
    at least from Linux I don't need them anymore. Also, that logic could be moved
    to the new script.
    
    The whole point of this isn't only making it prettier and easier to use, but
    also allowing me to write more complex logic in the binding generator scripts,
    that I will probably need to integrate the DOM enum types we need for animations
    and such easily (can't be just an include, because that pulls in another header
    with the same name bringing a lot of DOM and IDL churn).

---
+++ ports/geckolib/gecko_bindings/tools/regen.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 /path/to/gecko/objdir [other-regen.py-flags]"
+  exit 1
+fi
+
+# Check for rust-bindgen
+if [ ! -d rust-bindgen ]; then
+  echo "rust-bindgen not found. Run setup_bindgen.sh first."
+  exit 1
+fi
+
+# Check for /usr/include
+if [ ! -d /usr/include ]; then
+  echo "/usr/include doesn't exist. Mac users may need to run xcode-select --install."
+  exit 1
+fi
+
+if [ "$(uname)" == "Linux" ]; then
+  LIBCLANG_PATH=/usr/lib/llvm-3.8/lib;
+else
+  LIBCLANG_PATH=`brew --prefix llvm38`/lib/llvm-3.8/lib;
+fi
+
+./regen.py --target all "$@"
