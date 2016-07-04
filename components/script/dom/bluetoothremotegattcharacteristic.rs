2ab4511de81cedfa234d54b1c2946cd6ccaca4f7
zakorgyula <gyula.zakor@gmail.com>
2016-07-04 12:50:10 +0200

    Add missing steps to descriptors writeValue function

--- components/script/dom/bluetoothremotegattcharacteristic.rs
+++ components/script/dom/bluetoothremotegattcharacteristic.rs
@@ -27,7 +27,7 @@ use net_traits::bluetooth_thread::BluetoothMethodMsg;
 
 // Maximum length of an attribute value.
 // https://www.bluetooth.org/DocMan/handlers/DownloadDoc.ashx?doc_id=286439 (Vol. 3, page 2169)
-const MAXIMUM_ATTRIBUTE_LENGTH: usize = 512;
+pub const MAXIMUM_ATTRIBUTE_LENGTH: usize = 512;
 
 // https://webbluetoothcg.github.io/web-bluetooth/#bluetoothremotegattcharacteristic
 #[dom_struct]
