20ab47cb1aa038e546eb43531813648b1b4cd06e
ggomez <guillaume1.gomez@gmail.com>
2016-07-05 17:24:32 +0200

    Add video-metadata-rs dependencies

--- components/script/lib.rs
+++ components/script/lib.rs
@@ -88,6 +88,7 @@ extern crate url;
 #[macro_use]
 extern crate util;
 extern crate uuid;
+extern crate video_metadata;
 extern crate webrender_traits;
 extern crate websocket;
 extern crate xml5ever;
