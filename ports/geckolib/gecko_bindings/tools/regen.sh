0b76417b6e4e62736c2e8a796523a33930be2a6e
Jim Berlage <jberlage@mdsol.com>
2016-07-06 16:21:00 -0500

    Fix up shell scripts
    This brings these two scripts in line with the style guidelines.

--- ports/geckolib/gecko_bindings/tools/regen.sh
+++ ports/geckolib/gecko_bindings/tools/regen.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+set -o errexit
+set -o nounset
+set -o pipefail
 
 if [ $# -eq 0 ]; then
   echo "Usage: $0 /path/to/gecko/objdir [other-regen.py-flags]"
--- ports/geckolib/gecko_bindings/tools/setup_bindgen.sh
+++ ports/geckolib/gecko_bindings/tools/setup_bindgen.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+set -o errexit
+set -o nounset
+set -o pipefail
 
 # Run in the tools directory.
 cd "$(dirname $0)"
