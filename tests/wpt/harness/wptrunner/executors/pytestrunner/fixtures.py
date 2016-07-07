56ce7da020d75e11b23c9b0d602b561c45fe8e20
Josh Matthews <josh@joshmatthews.net>
2016-07-07 12:08:30 -0400

    Fix pytest import.

--- tests/wpt/harness/wptrunner/executors/pytestrunner/fixtures.py
+++ tests/wpt/harness/wptrunner/executors/pytestrunner/fixtures.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import pytest
+from tools import pytest
 
 @pytest.fixture
 def webdriver():
