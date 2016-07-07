c0c4b34f8321aaeefb7b563aa6bc6bca48b1a7bf
Manish Goregaokar <manishsmail@gmail.com>
2016-07-07 18:52:20 +0530

    Address review comments

--- ports/geckolib/gecko_bindings/sugar/ns_style_auto_array.rs
+++ ports/geckolib/gecko_bindings/sugar/ns_style_auto_array.rs
@@ -1,5 +1,9 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 use std::iter::{once, Chain, Once, IntoIterator};
-use std::slice::{IterMut};
+use std::slice::IterMut;
 use structs::nsStyleAutoArray;
 
 impl<T> nsStyleAutoArray<T> {
@@ -7,6 +11,9 @@ impl<T> nsStyleAutoArray<T> {
         once(&mut self.mFirstElement).chain(self.mOtherElements.iter_mut())
     }
 
+    // Note that often structs containing autoarrays will have
+    // additional member fields that contain the length, which must be kept
+    // in sync
     pub fn len(&self) -> usize {
         1 + self.mOtherElements.len()
     }
