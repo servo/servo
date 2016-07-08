cc17bf80616e4897183e54c07cd014c15b88ade7
Patrick Trottier <patrick.arrow206@outlook.com>
2016-07-08 09:28:35 -0400

    Format changes to build.rs

--- components/servo/build.rs
+++ components/servo/build.rs
@@ -64,11 +64,11 @@ fn android_main() {
 }
 
 fn windows_main() {
-        let out_dir = env::var("OUT_DIR").ok().expect("can't find out_dir");
+    let out_dir = env::var("OUT_DIR").ok().expect("can't find out_dir");
 
     Command::new("windres").args(&[" ../../resources/windows.rc",  "-o"])
-                       .arg(&format!("{}/windows.rc.o", out_dir))
-                       .status().unwrap();
+                           .arg(&format!("{}/windows.rc.o", out_dir))
+                           .status().unwrap();
     Command::new("ar").args(&["crus", "libwindows_rc.a", "windows.rc.o"])
                       .current_dir(&Path::new(&out_dir))
                       .status().unwrap();
