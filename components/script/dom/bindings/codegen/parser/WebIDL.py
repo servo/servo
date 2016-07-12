39f7ce8fd495451a1c0114daefb326be05cc97b1
Emilio Cobos Álvarez <ecoal95@gmail.com>
2016-07-12 14:02:33 -0700

    webidl: Apply and update debug.patch, remove abstract.patch which was already upstream.

--- components/script/dom/bindings/codegen/parser/WebIDL.py
+++ components/script/dom/bindings/codegen/parser/WebIDL.py
@@ -6823,7 +6823,8 @@ class Parser(Tokenizer):
             self.parser = yacc.yacc(module=self,
                                     outputdir=outputdir,
                                     tabmodule='webidlyacc',
-                                    errorlog=logger
+                                    errorlog=logger,
+                                    debug=False
                                     # Pickling the grammar is a speedup in
                                     # some cases (older Python?) but a
                                     # significant slowdown in others.
