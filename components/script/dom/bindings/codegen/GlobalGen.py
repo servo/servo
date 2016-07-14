ddcc769f8f1fa1deaa8f15c8d77192f2107d0eb8
Andrew Scorpil <scorpilmail@gmail.com>
2016-07-14 11:36:29 +0200

    Sort input filelist to preserve expected definition order.

--- components/script/dom/bindings/codegen/GlobalGen.py
+++ components/script/dom/bindings/codegen/GlobalGen.py
@@ -44,7 +44,7 @@ def main():
 
     # Parse the WebIDL.
     parser = WebIDL.Parser(options.cachedir)
-    for filename in fileList:
+    for filename in sorted(fileList):
         fullPath = os.path.normpath(os.path.join(baseDir, filename))
         with open(fullPath, 'rb') as f:
             lines = f.readlines()
