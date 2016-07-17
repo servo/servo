62577a379f3c3ae350ead80cff46419ee6189b55
Ying-Ruei Liang(KK) <thumbd03803@gmail.com>
2016-07-18 00:18:56 +0800

    Add referrer policy test cases for <a> and <link>

--- tests/wpt/mozilla/tests/mozilla/referrer-policy/generic/common.js
+++ tests/wpt/mozilla/tests/mozilla/referrer-policy/generic/common.js
@@ -115,7 +115,7 @@ function queryIframe(url, callback, referrer_policy) {
       xhr.onreadystatechange = function(e) {
         if (this.readyState == 4 && this.status == 200) {
           var server_data = JSON.parse(this.responseText);
-          callback(server_data, url);
+          callback(server_data);
         }
       };
       xhr.send();
@@ -188,10 +188,36 @@ function queryNavigable(element, url, callback, attributes) {
 }
 
 function queryLink(url, callback, referrer_policy) {
-  var a = document.createElement("a");
-  a.innerHTML = "Link to subresource";
-  document.body.appendChild(a);
-  queryNavigable(a, url, callback, referrer_policy)
+  var x = document.createElement('script');
+  x.src = '/common/utils.js';
+  x.onerror = function() { console.log('whoops') };
+  x.onload = function() { doQuery() };
+  document.getElementsByTagName("head")[0].appendChild(x);
+
+  function doQuery() {
+    var id = token();
+    var url_with_params = url + "&id=" + id + "&tagAttrs=" + JSON.stringify(referrer_policy);
+    var iframe = appendIframeToBody(url_with_params);
+    iframe.addEventListener("load", function listener() {
+      if ((iframe.contentWindow !== null) &&
+          (iframe.contentWindow.location.toString() === url_with_params)) {
+      } else {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', '/_mozilla/mozilla/referrer-policy/generic/subresource/stash.py?id=' + id, false);
+        xhr.onreadystatechange = function(e) {
+          if (this.readyState == 4 && this.status == 200) {
+            var server_data = JSON.parse(this.responseText);
+            server_data.referrer = unescape(server_data.referrer);
+            server_data.headers.referer = unescape(server_data.headers.referer);
+
+            callback(server_data, url_with_params);
+          }
+        };
+        xhr.send();
+        iframe.removeEventListener("load", listener);
+      }
+    });
+  }
 }
 
 function queryAreaLink(url, callback, referrer_policy) {
@@ -213,6 +239,56 @@ function queryScript(url, callback) {
   window.addEventListener("message", listener);
 
   document.body.appendChild(script);
+}
+
+function queryCssLink(url, callback, referrer_policy) {
+  var x = document.createElement('script');
+  x.src = '/common/utils.js';
+  x.onerror = function() { console.log('whoops') };
+  x.onload = function() { doQuery() };
+  document.getElementsByTagName("head")[0].appendChild(x);
+
+  function doQuery() {
+    var id = token();
+    var link = document.createElement("link");
+
+    if (referrer_policy) {
+      for (var attr in referrer_policy) {
+        // TODO crashed when you assigned value to rel attribute
+        if (attr === "rel") {
+          link.relList.add("noreferrer");
+        } else {
+          link[attr] = referrer_policy[attr];
+        }
+      }
+    }
+
+    link.href = url + "&id=" + id;
+    link.relList.add("stylesheet");
+
+    // XXX Should be removed after "onload" event can be fired
+    var timeout_func = setTimeout(function timeout() {
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', '/_mozilla/mozilla/referrer-policy/generic/subresource/stash.py?id=' + id, false);
+      xhr.onreadystatechange = function(e) {
+        if (this.readyState == 4 && this.status == 200) {
+          var server_data = JSON.parse(this.responseText);
+          // FIXME But why??
+          server_data.headers = JSON.parse(server_data.headers);
+          if (server_data.headers.referer == undefined) {
+            server_data.headers.referer = undefined;
+          }
+
+          callback(wrapResult(url, server_data));
+        }
+      };
+      xhr.send();
+
+      clearTimeout(timeout_func);
+    }, 1000);
+
+    document.body.appendChild(link);
+  }
 }
 
  // SanityChecker does nothing in release mode.
