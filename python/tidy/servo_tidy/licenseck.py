536314be79ba0ee4a45e3afd027c7bdd4200d95c
Alan Jeffrey <ajeffrey@mozilla.com>
2016-07-19 12:25:25 -0500

    Added test-tidy for dependent licenses.

--- python/tidy/servo_tidy/licenseck.py
+++ python/tidy/servo_tidy/licenseck.py
@@ -88,3 +88,38 @@ licenses_toml = [
     'license = "MPL-2.0"',
     'license = "MIT/Apache-2.0"',
 ]
+
+# The valid dependency licenses, in the form we'd expect to see them in a Cargo.toml file.
+licenses_dep_toml = [
+    # Licenses that are compatible with Servo's licensing
+    'license = "Apache-2 / MIT"',
+    'license = "Apache-2.0 / MIT"',
+    'license = "Apache-2.0"',
+    'license = "Apache-2.0/MIT"',
+    'license = "BSD-2-Clause"',
+    'license = "BSD-3-Clause"',
+    'license = "BSD-3-Clause/MIT"',
+    'license = "CC0-1.0"',
+    'license = "ISC"',
+    'license = "MIT / Apache-2.0"',
+    'license = "MIT OR Apache-2.0"',
+    'license = "MIT"',
+    'license = "MIT/Apache-2.0"',
+    'license = "MPL-2.0"',
+    'license = "Unlicense/MIT"',
+    'license = "zlib-acknowledgement"',
+    'license-file = "LICENSE-MIT"',
+    'license=  "MIT / Apache-2.0"',
+    # Whitelisted crates whose licensing has been checked manually
+    'name = "device"',
+    'name = "dylib"',
+    'name = "ipc-channel"',
+    'name = "mozjs_sys"',
+    'name = "azure"',
+    'name = "freetype"',
+    'name = "js"',
+    'name = "servo-freetype-sys"',
+    'name = "simd"',
+    'name = "webrender"',
+    'name = "webrender_traits"',
+]
