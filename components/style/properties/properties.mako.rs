23082b830c327f84ae0dc9ac6ea1ce91b5bbee77
David Raifaizen <d-raif@hotmail.com>
2016-07-18 23:00:56 -0400

    Added shorthand serialization for font

--- components/style/properties/properties.mako.rs
+++ components/style/properties/properties.mako.rs
@@ -475,6 +475,7 @@ fn append_shorthand_value<'a, W, I>(dest: &mut W,
         Shorthand::Animation => try!(append_animation_shorthand(dest, declarations)),
         Shorthand::Transition => try!(append_transition_shorthand(dest, declarations)),
         Shorthand::BorderWidth => try!(append_border_width_shorthand(dest, declarations)),
+        Shorthand::Font => try!(append_font_shorthand(dest, declarations)),
         _ => {}
     };
 
@@ -973,6 +974,65 @@ fn append_border_width_shorthand<'a, W, I>(dest: &mut W,
     let &LeftContainer(ref left) = left_container;
 
     append_positional_shorthand(dest, top, right, bottom, left)
+}
+
+fn append_font_shorthand<'a, W, I>(dest: &mut W,
+                                   declarations: I) -> fmt::Result
+                                   where W: fmt::Write, I: Iterator<Item=&'a PropertyDeclaration> {
+
+    let mut font_family = None;
+    let mut font_style = None;
+    let mut font_variant = None;
+    let mut font_weight = None;
+    let mut font_size = None;
+    let mut font_stretch = None;
+    let mut line_height = None;
+
+    for decl in declarations {
+        match decl {
+            &PropertyDeclaration::FontFamily(ref value) => { font_family = Some(value); },
+            &PropertyDeclaration::FontStyle(ref value) => { font_style = Some(value); },
+            &PropertyDeclaration::FontVariant(ref value) => { font_variant = Some(value); },
+            &PropertyDeclaration::FontWeight(ref value) => { font_weight = Some(value); },
+            &PropertyDeclaration::FontSize(ref value) => { font_size = Some(value); },
+            &PropertyDeclaration::FontStretch(ref value) => { font_stretch = Some(value); },
+            &PropertyDeclaration::LineHeight(ref value) => { line_height = Some(value); },
+            _ => return Err(fmt::Error)
+        }
+    }
+
+    let (font_family, font_style, font_variant, font_weight, font_size, font_stretch, line_height) =
+        try_unwrap_longhands!(font_family, font_style, font_variant, font_weight, font_size, font_stretch, line_height);
+
+    if let &DeclaredValue::Value(ref style) = font_style {
+        try!(style.to_css(dest));
+        try!(write!(dest, " "));
+    }
+
+    if let &DeclaredValue::Value(ref variant) = font_variant {
+        try!(variant.to_css(dest));
+        try!(write!(dest, " "));
+    }
+
+    if let &DeclaredValue::Value(ref weight) = font_weight {
+        try!(weight.to_css(dest));
+        try!(write!(dest, " "));
+    }
+
+    if let &DeclaredValue::Value(ref stretch) = font_stretch {
+        try!(stretch.to_css(dest));
+        try!(write!(dest, " "));
+    }
+
+    try!(font_size.to_css(dest));
+    if let &DeclaredValue::Value(ref height) = line_height {
+        try!(write!(dest, "/"));
+        try!(height.to_css(dest));
+    }
+
+    try!(write!(dest, " "));
+
+    font_family.to_css(dest)
 }
 
 fn append_serialization<'a, W, I>(dest: &mut W,
