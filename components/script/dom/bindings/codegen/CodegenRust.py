7bc67b8db8735a584aed2f65052a70323fe68f36
Mátyás Mustoha <mmatyas@inf.u-szeged.hu>
2016-07-20 12:47:55 +0200

    Add SpiderMonkey hooks for enqueuing promise jobs. Start porting various native Promise APIs.

--- components/script/dom/bindings/codegen/CodegenRust.py
+++ components/script/dom/bindings/codegen/CodegenRust.py
@@ -809,11 +809,11 @@ def getJSToNativeConversionInfo(type, descriptorProvider, failureCode=None,
                 { // Scope for our GlobalObject, ErrorResult, JSAutoCompartment,
                   // etc.
 
-                  let globalObj = RootedObject::new(cx, CurrentGlobalOrNull(cx));
+                  rooted!(in(cx) let globalObj = CurrentGlobalOrNull(cx));
                   let _ac = JSAutoCompartment::new(cx, globalObj.handle().get());
                   let promiseGlobal = global_root_from_object_maybe_wrapped(globalObj.handle().get());
 
-                  let mut valueToResolve = RootedValue::new(cx, $${val}.get());
+                  rooted!(in(cx) let mut valueToResolve = $${val}.get());
                   if !JS_WrapValue(cx, valueToResolve.handle_mut()) {
                     $*{exceptionCode}
                   }
@@ -5306,7 +5306,7 @@ def generate_imports(config, cgthings, descriptors, callbacks=None, dictionaries
         'js::jsapi::{JS_HasProperty, JS_HasPropertyById, JS_InitializePropertiesFromCompatibleNativeObject}',
         'js::jsapi::{JS_AtomizeAndPinString, JS_NewObject, JS_NewObjectWithGivenProto}',
         'js::jsapi::{JS_NewObjectWithoutMetadata, JS_SetProperty, JS_DefinePropertyById2}',
-        'js::jsapi::{JS_SplicePrototype, JS_SetReservedSlot, JSAutoCompartment}',
+        'js::jsapi::{JS_SplicePrototype, JS_SetReservedSlot, JS_WrapValue, JSAutoCompartment}',
         'js::jsapi::{JSContext, JSClass, JSFreeOp, JSFunctionSpec, JS_GetIteratorPrototype}',
         'js::jsapi::{JSJitGetterCallArgs, JSJitInfo, JSJitMethodCallArgs, JSJitSetterCallArgs}',
         'js::jsapi::{JSNative, JSObject, JSNativeWrapper, JSPropertySpec}',
