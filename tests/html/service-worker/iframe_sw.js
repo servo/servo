f9826cb0e43f2e474e614f5e8b07da5daba492d4
Rahul Sharma <rsconceptx@gmail.com>
2016-08-01 16:25:08 +0530

    Dispatch events to service worker object and refactor html tests

--- tests/html/service-worker/iframe_sw.js
+++ tests/html/service-worker/iframe_sw.js
@@ -1,1 +1,7 @@
-console.log("iframe service worker active");
+self.addEventListener('activate', function(e) {
+	console.log("iframe service worker active");
+});
+
+self.addEventListener('fetch', function(e) {
+	console.log("A fetch event detected by /iframe service worker");
+});
