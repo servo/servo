92d1a9e0a1db85b1bde5c15a7a3c4f912e8b12d5
Vladimir Vukicevic <vladimir@pobox.com>
2016-08-01 02:32:27 -0400

    Native MSVC windows build, convert to cmake

--- components/gfx/platform/mod.rs
+++ components/gfx/platform/mod.rs
@@ -2,13 +2,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#[cfg(any(target_os = "linux", target_os = "android", target_os = "windows"))]
+#[cfg(any(target_os = "linux", target_os = "android", all(target_os = "windows", target_env = "gnu")))]
 pub use platform::freetype::{font, font_context, font_list, font_template};
 
 #[cfg(target_os = "macos")]
 pub use platform::macos::{font, font_context, font_list, font_template};
 
-#[cfg(any(target_os = "linux", target_os = "android", target_os = "windows"))]
+#[cfg(all(target_os = "windows", target_env = "msvc"))]
+pub use platform::dummy::{font, font_context, font_list, font_template};
+
+#[cfg(any(target_os = "linux", target_os = "android", all(target_os = "windows", target_env = "gnu")))]
 mod freetype {
     use libc::c_char;
     use std::ffi::CStr;
@@ -28,6 +31,14 @@ mod freetype {
 
 #[cfg(target_os = "macos")]
 mod macos {
+    pub mod font;
+    pub mod font_context;
+    pub mod font_list;
+    pub mod font_template;
+}
+
+#[cfg(all(target_os = "windows", target_env = "msvc"))]
+mod dummy {
     pub mod font;
     pub mod font_context;
     pub mod font_list;
