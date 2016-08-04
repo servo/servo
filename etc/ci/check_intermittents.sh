8a4408a0ad3bb61af09dda888bc3079b3f6de96d
Michael Howell <michael@notriddle.com>
2016-08-04 10:19:53 -0700

    fixup! fixup! Add script to check for intermittent regressions

--- etc/ci/check_intermittents.sh
+++ etc/ci/check_intermittents.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 REPEAT_COUNT=100
+set -o errexit
+set -o nounset
+set -o pipefail
 
 for test_type in wpt css; do
     while read test_name; do
         echo "  - Checking ${test_name}"
-        ./mach "test-${test_type}" --release --repeat "${REPEAT_COUNT}" "${test_name}" > tmp_log.txt
-        if [ $? != 0 ]; then
-            cat tmp_log.txt
-            exit $?
-        fi
+        ./mach "test-${test_type}" --log-raw - --release --repeat "${REPEAT_COUNT}" "${test_name}" > intermittents.log < /dev/null
     done < "etc/ci/former_intermittents_${test_type}.txt"
 done
 
