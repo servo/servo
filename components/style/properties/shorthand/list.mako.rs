b1f0a76118f7923235afe09cc8bcea354b4c4472
David Raifaizen <d-raif@hotmail.com>
2016-08-04 20:26:51 -0400

    Cleaning up the organization of the various shorthand serializations

--- components/style/properties/shorthand/list.mako.rs
+++ components/style/properties/shorthand/list.mako.rs
@@ -8,85 +8,110 @@
                     sub_properties="list-style-image list-style-position list-style-type">
     use properties::longhands::{list_style_image, list_style_position, list_style_type};
 
-    // `none` is ambiguous until we've finished parsing the shorthands, so we count the number
-    // of times we see it.
-    let mut nones = 0u8;
-    let (mut image, mut position, mut list_style_type, mut any) = (None, None, None, false);
-    loop {
-        if input.try(|input| input.expect_ident_matching("none")).is_ok() {
-            nones = nones + 1;
-            if nones > 2 {
-                return Err(())
-            }
-            any = true;
-            continue
-        }
-
-        if list_style_type.is_none() {
-            if let Ok(value) = input.try(|input| list_style_type::parse(context, input)) {
-                list_style_type = Some(value);
+    pub fn parse_value(context: &ParserContext, input: &mut Parser) -> Result<Longhands, ()> {
+        // `none` is ambiguous until we've finished parsing the shorthands, so we count the number
+        // of times we see it.
+        let mut nones = 0u8;
+        let (mut image, mut position, mut list_style_type, mut any) = (None, None, None, false);
+        loop {
+            if input.try(|input| input.expect_ident_matching("none")).is_ok() {
+                nones = nones + 1;
+                if nones > 2 {
+                    return Err(())
+                }
                 any = true;
                 continue
             }
+
+            if list_style_type.is_none() {
+                if let Ok(value) = input.try(|input| list_style_type::parse(context, input)) {
+                    list_style_type = Some(value);
+                    any = true;
+                    continue
+                }
+            }
+
+            if image.is_none() {
+                if let Ok(value) = input.try(|input| list_style_image::parse(context, input)) {
+                    image = Some(value);
+                    any = true;
+                    continue
+                }
+            }
+
+            if position.is_none() {
+                if let Ok(value) = input.try(|input| list_style_position::parse(context, input)) {
+                    position = Some(value);
+                    any = true;
+                    continue
+                }
+            }
+            break
         }
 
-        if image.is_none() {
-            if let Ok(value) = input.try(|input| list_style_image::parse(context, input)) {
-                image = Some(value);
-                any = true;
-                continue
+        // If there are two `none`s, then we can't have a type or image; if there is one `none`,
+        // then we can't have both a type *and* an image; if there is no `none` then we're fine as
+        // long as we parsed something.
+        match (any, nones, list_style_type, image) {
+            (true, 2, None, None) => {
+                Ok(Longhands {
+                    list_style_position: position,
+                    list_style_image: Some(list_style_image::SpecifiedValue::None),
+                    list_style_type: Some(list_style_type::SpecifiedValue::none),
+                })
             }
-        }
-
-        if position.is_none() {
-            if let Ok(value) = input.try(|input| list_style_position::parse(context, input)) {
-                position = Some(value);
-                any = true;
-                continue
+            (true, 1, None, Some(image)) => {
+                Ok(Longhands {
+                    list_style_position: position,
+                    list_style_image: Some(image),
+                    list_style_type: Some(list_style_type::SpecifiedValue::none),
+                })
             }
+            (true, 1, Some(list_style_type), None) => {
+                Ok(Longhands {
+                    list_style_position: position,
+                    list_style_image: Some(list_style_image::SpecifiedValue::None),
+                    list_style_type: Some(list_style_type),
+                })
+            }
+            (true, 1, None, None) => {
+                Ok(Longhands {
+                    list_style_position: position,
+                    list_style_image: Some(list_style_image::SpecifiedValue::None),
+                    list_style_type: Some(list_style_type::SpecifiedValue::none),
+                })
+            }
+            (true, 0, list_style_type, image) => {
+                Ok(Longhands {
+                    list_style_position: position,
+                    list_style_image: image,
+                    list_style_type: list_style_type,
+                })
+            }
+            _ => Err(()),
         }
-        break
     }
 
-    // If there are two `none`s, then we can't have a type or image; if there is one `none`,
-    // then we can't have both a type *and* an image; if there is no `none` then we're fine as
-    // long as we parsed something.
-    match (any, nones, list_style_type, image) {
-        (true, 2, None, None) => {
-            Ok(Longhands {
-                list_style_position: position,
-                list_style_image: Some(list_style_image::SpecifiedValue::None),
-                list_style_type: Some(list_style_type::SpecifiedValue::none),
-            })
+    impl<'a> ToCss for LonghandsToSerialize<'a>  {
+        fn to_css<W>(&self, dest: &mut W) -> fmt::Result where W: fmt::Write {
+            match *self.list_style_position {
+                DeclaredValue::Initial => try!(write!(dest, "outside")),
+                _ => try!(self.list_style_position.to_css(dest))
+            }
+
+            try!(write!(dest, " "));
+
+            match *self.list_style_image {
+                DeclaredValue::Initial => try!(write!(dest, "none")),
+                _ => try!(self.list_style_image.to_css(dest))
+            };
+
+            try!(write!(dest, " "));
+
+            match *self.list_style_type {
+                DeclaredValue::Initial => write!(dest, "disc"),
+                _ => self.list_style_type.to_css(dest)
+            }
         }
-        (true, 1, None, Some(image)) => {
-            Ok(Longhands {
-                list_style_position: position,
-                list_style_image: Some(image),
-                list_style_type: Some(list_style_type::SpecifiedValue::none),
-            })
-        }
-        (true, 1, Some(list_style_type), None) => {
-            Ok(Longhands {
-                list_style_position: position,
-                list_style_image: Some(list_style_image::SpecifiedValue::None),
-                list_style_type: Some(list_style_type),
-            })
-        }
-        (true, 1, None, None) => {
-            Ok(Longhands {
-                list_style_position: position,
-                list_style_image: Some(list_style_image::SpecifiedValue::None),
-                list_style_type: Some(list_style_type::SpecifiedValue::none),
-            })
-        }
-        (true, 0, list_style_type, image) => {
-            Ok(Longhands {
-                list_style_position: position,
-                list_style_image: image,
-                list_style_type: list_style_type,
-            })
-        }
-        _ => Err(()),
     }
 </%helpers:shorthand>
--- components/style/properties/shorthand/position.mako.rs
+++ components/style/properties/shorthand/position.mako.rs
@@ -9,31 +9,50 @@
                                      experimental="True">
     use properties::longhands::{flex_direction, flex_wrap};
 
-    let mut direction = None;
-    let mut wrap = None;
-    loop {
-        if direction.is_none() {
-            if let Ok(value) = input.try(|input| flex_direction::parse(context, input)) {
-                direction = Some(value);
-                continue
+    pub fn parse_value(context: &ParserContext, input: &mut Parser) -> Result<Longhands, ()> {
+        let mut direction = None;
+        let mut wrap = None;
+        loop {
+            if direction.is_none() {
+                if let Ok(value) = input.try(|input| flex_direction::parse(context, input)) {
+                    direction = Some(value);
+                    continue
+                }
             }
-        }
-        if wrap.is_none() {
-            if let Ok(value) = input.try(|input| flex_wrap::parse(context, input)) {
-                wrap = Some(value);
-                continue
+            if wrap.is_none() {
+                if let Ok(value) = input.try(|input| flex_wrap::parse(context, input)) {
+                    wrap = Some(value);
+                    continue
+                }
             }
+            break
         }
-        break
+
+        if direction.is_none() && wrap.is_none() {
+            return Err(())
+        }
+        Ok(Longhands {
+            flex_direction: direction,
+            flex_wrap: wrap,
+        })
     }
 
-    if direction.is_none() && wrap.is_none() {
-        return Err(())
+
+    impl<'a> ToCss for LonghandsToSerialize<'a>  {
+        fn to_css<W>(&self, dest: &mut W) -> fmt::Result where W: fmt::Write {
+            match *self.flex_direction {
+                DeclaredValue::Initial => try!(write!(dest, "row")),
+                _ => try!(self.flex_direction.to_css(dest))
+            };
+
+            try!(write!(dest, " "));
+
+            match *self.flex_wrap {
+                DeclaredValue::Initial => write!(dest, "nowrap"),
+                _ => self.flex_wrap.to_css(dest)
+            }
+        }
     }
-    Ok(Longhands {
-        flex_direction: direction,
-        flex_wrap: wrap,
-    })
 </%helpers:shorthand>
 
 // https://drafts.csswg.org/css-flexbox/#flex-property
@@ -49,40 +68,54 @@
         Ok((grow, shrink))
     }
 
-    let mut grow = None;
-    let mut shrink = None;
-    let mut basis = None;
+    pub fn parse_value(_: &ParserContext, input: &mut Parser) -> Result<Longhands, ()> {
+        let mut grow = None;
+        let mut shrink = None;
+        let mut basis = None;
 
-    if input.try(|input| input.expect_ident_matching("none")).is_ok() {
-        return Ok(Longhands {
-            flex_grow: Some(Number(0.0)),
-            flex_shrink: Some(Number(0.0)),
-            flex_basis: Some(LengthOrPercentageOrAutoOrContent::Auto)
+        if input.try(|input| input.expect_ident_matching("none")).is_ok() {
+            return Ok(Longhands {
+                flex_grow: Some(Number(0.0)),
+                flex_shrink: Some(Number(0.0)),
+                flex_basis: Some(LengthOrPercentageOrAutoOrContent::Auto)
+            })
+        }
+        loop {
+            if grow.is_none() {
+                if let Ok((flex_grow, flex_shrink)) = input.try(parse_flexibility) {
+                    grow = Some(flex_grow);
+                    shrink = flex_shrink;
+                    continue
+                }
+            }
+            if basis.is_none() {
+                if let Ok(value) = input.try(LengthOrPercentageOrAutoOrContent::parse) {
+                    basis = Some(value);
+                    continue
+                }
+            }
+            break
+        }
+
+        if grow.is_none() && basis.is_none() {
+            return Err(())
+        }
+        Ok(Longhands {
+            flex_grow: grow.or(Some(Number(1.0))),
+            flex_shrink: shrink.or(Some(Number(1.0))),
+            flex_basis: basis.or(Some(LengthOrPercentageOrAutoOrContent::Length(Length::Absolute(Au(0)))))
         })
     }
-    loop {
-        if grow.is_none() {
-            if let Ok((flex_grow, flex_shrink)) = input.try(parse_flexibility) {
-                grow = Some(flex_grow);
-                shrink = flex_shrink;
-                continue
-            }
-        }
-        if basis.is_none() {
-            if let Ok(value) = input.try(LengthOrPercentageOrAutoOrContent::parse) {
-                basis = Some(value);
-                continue
-            }
-        }
-        break
-    }
 
-    if grow.is_none() && basis.is_none() {
-        return Err(())
+    impl<'a> ToCss for LonghandsToSerialize<'a>  {
+        fn to_css<W>(&self, dest: &mut W) -> fmt::Result where W: fmt::Write {
+            try!(self.flex_grow.to_css(dest));
+            try!(write!(dest, " "));
+
+            try!(self.flex_shrink.to_css(dest));
+            try!(write!(dest, " "));
+
+            self.flex_basis.to_css(dest)
+        }
     }
-    Ok(Longhands {
-        flex_grow: grow.or(Some(Number(1.0))),
-        flex_shrink: shrink.or(Some(Number(1.0))),
-        flex_basis: basis.or(Some(LengthOrPercentageOrAutoOrContent::Length(Length::Absolute(Au(0)))))
-    })
 </%helpers:shorthand>
