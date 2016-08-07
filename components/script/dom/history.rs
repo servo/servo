8d133ea2322ec2e770a6df7a50e966f5f1f263a6
Connor Brewster <connor.brewster@eagles.oc.edu>
2016-08-06 19:45:59 -0600

    Implement history state, pushState, replaceState

--- components/script/dom/history.rs
+++ components/script/dom/history.rs
@@ -6,12 +6,17 @@ use dom::bindings::codegen::Bindings::HistoryBinding;
 use dom::bindings::codegen::Bindings::HistoryBinding::HistoryMethods;
 use dom::bindings::codegen::Bindings::LocationBinding::LocationMethods;
 use dom::bindings::codegen::Bindings::WindowBinding::WindowMethods;
-use dom::bindings::inheritance::Castable;
+use dom::bindings::error::{Error, ErrorResult};
+use dom::bindings::structuredclone::StructuredCloneData;
+use dom::bindings::globalscope::GlobalScope;
 use dom::bindings::js::{JS, Root};
 use dom::bindings::reflector::{Reflector, reflect_dom_object};
-use dom::globalscope::GlobalScope;
+use dom::bindings::str::{DOMString, USVString};
+use dom::urlhelper::UrlHelper;
 use dom::window::Window;
 use ipc_channel::ipc;
+use js::jsapi::{HandleValue, JSContext};
+use js::jsval::{JSVal, NullValue};
 use msg::constellation_msg::TraversalDirection;
 use script_traits::ScriptMsg as ConstellationMsg;
 
@@ -35,6 +40,64 @@ impl History {
                            window,
                            HistoryBinding::Wrap)
     }
+
+    fn add_state(&self,
+                 cx: *mut JSContext,
+                 data: HandleValue,
+                 title: DOMString,
+                 url: Option<USVString>,
+                 replace: bool) -> ErrorResult {
+        // Step 1
+        let document = self.window.Document();
+        // Step 2
+        if !document.is_fully_active() {
+            return Err(Error::Security);
+        }
+        // Step 5
+        let cloned_data = try!(StructuredCloneData::write(cx, data));
+        rooted!(in(cx) let mut state = NullValue());
+        cloned_data.read(GlobalRef::Window(&*self.window), state.handle_mut());
+        let url = match url {
+            Some(url) => {
+                // Step 6
+                let document_url = document.url();
+                // 6.1
+                let url = match document_url.join(&url.0) {
+                    Ok(url) => url,
+                    // 6.2
+                    Err(_) => return Err(Error::Security),
+                };
+
+                // 6.4
+                if url.scheme() != document_url.scheme() ||
+                   url.username() != document_url.username() ||
+                   url.password() != document_url.password() ||
+                   url.host() != document_url.host() ||
+                   url.port() != document_url.port() {
+                    return Err(Error::Security);
+                }
+
+                // 6.5
+                if !UrlHelper::SameOrigin(&url, document_url) &&
+                   (url.path() != document_url.path() || url.query() != document_url.query()) {
+                    return Err(Error::Security);
+                }
+
+                url
+            },
+            // Step 7
+            None => document.url().clone(),
+        };
+
+        // Step 8
+        if replace {
+            self.window.browsing_context().replace_session_history_entry(Some(title), Some(url), state.handle());
+        } else {
+            self.window.browsing_context().push_session_history_entry(&*document, Some(title), Some(url), Some(state.handle()));
+        }
+
+        Ok(())
+    }
 }
 
 impl History {
@@ -55,6 +118,11 @@ impl HistoryMethods for History {
         let msg = ConstellationMsg::JointSessionHistoryLength(pipeline, sender);
         let _ = global_scope.constellation_chan().send(msg);
         recv.recv().unwrap()
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-history-state
+    fn State(&self, _cx: *mut JSContext) -> JSVal {
+        self.window.browsing_context().state()
     }
 
     // https://html.spec.whatwg.org/multipage/#dom-history-go
@@ -79,5 +147,23 @@ impl HistoryMethods for History {
     // https://html.spec.whatwg.org/multipage/#dom-history-forward
     fn Forward(&self) {
         self.traverse_history(TraversalDirection::Forward(1));
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-history-pushtstae
+    fn PushState(&self,
+                 cx: *mut JSContext,
+                 data: HandleValue,
+                 title: DOMString,
+                 url: Option<USVString>) -> ErrorResult {
+        self.add_state(cx, data, title, url, false)
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-history-replacestate
+    fn ReplaceState(&self,
+                    cx: *mut JSContext,
+                    data: HandleValue,
+                    title: DOMString,
+                    url: Option<USVString>) -> ErrorResult {
+        self.add_state(cx, data, title, url, true)
     }
 }
