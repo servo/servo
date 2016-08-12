b715bbb86a2fd9f3ec2f044121d79508f99cf4dd
Manish Goregaokar <manishsmail@gmail.com>
2016-08-12 18:58:01 +0530

    Add safer bindings for dealing with borrowed Arcs over FFI

--- ports/geckolib/gecko_bindings/sugar/refptr.rs
+++ ports/geckolib/gecko_bindings/sugar/refptr.rs
@@ -1,7 +1,53 @@
+use std::marker::PhantomData;
+use std::mem::transmute;
+use std::ptr::null;
 use std::sync::Arc;
 
-pub unsafe trait HasStrong {
+pub unsafe trait RefCounted<'a> where Self: Sized {
     type Strong;
+    type Borrowed;
     fn into_strong(x: Arc<Self>) -> Self::Strong;
     fn null_strong() -> Self::Strong;
+    fn from_borrowed(x: Self::Borrowed) -> &'a Arc<Self>;
+    fn into_borrowed(x: &'a Arc<Self>) -> Self::Borrowed;
+    fn from_borrowed_opt(x: Self::Borrowed) -> Option<&'a Arc<Self>> {
+        let arc = Self::from_borrowed(x);
+        let ptr: *const Self = &**arc as *const Self;
+        if ptr == null() {
+            None
+        } else {
+            Some(arc)
+        }
+    }
+    unsafe fn decref_borrowed(x: Self::Borrowed) where Self: 'a {
+        let _: Arc<Self> = transmute(Self::from_borrowed(x) as *const _);
+    }
+    fn make_strong(x: Self::Borrowed) -> Self::Strong where Self: 'a {
+        let arc = Self::from_borrowed(x).clone();
+        Self::into_strong(arc)
+    }
+}
+
+#[derive(Copy, Clone)]
+pub struct ServoComputedValuesBorrowed<'a> {
+    ptr: usize, // actual type defined in style crate
+    _marker: PhantomData<&'a ()>,
+}
+
+impl<'a> ServoComputedValuesBorrowed<'a> {
+    pub fn is_null(&self) -> bool {
+        self.ptr == 0
+    }
+}
+
+#[derive(Copy, Clone)]
+pub struct RawServoStyleSheetBorrowed<'a> {
+    ptr: usize, // actual type defined in style crate
+    _marker: PhantomData<&'a ()>,
+}
+
+impl<'a> RawServoStyleSheetBorrowed<'a> {
+    pub fn is_null(&self) -> bool {
+        self.ptr == 0
+    }
 }
