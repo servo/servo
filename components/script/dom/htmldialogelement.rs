e970b7056954e87b08bc531e400fc0db01422981
clstl <reamles@gmail.com>
2016-08-13 16:17:03 +0300

    Implements HTMLDialogElement#close

--- components/script/dom/htmldialogelement.rs
+++ components/script/dom/htmldialogelement.rs
@@ -1,15 +1,20 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 use dom::bindings::cell::DOMRefCell;
 use dom::bindings::codegen::Bindings::HTMLDialogElementBinding;
 use dom::bindings::codegen::Bindings::HTMLDialogElementBinding::HTMLDialogElementMethods;
+use dom::bindings::error::{Error, ErrorResult};
+use dom::bindings::inheritance::Castable;
 use dom::bindings::js::Root;
 use dom::bindings::str::DOMString;
 use dom::document::Document;
+use dom::element::Element;
+use dom::event::{EventBubbles, EventCancelable};
+use dom::eventtarget::EventTarget;
 use dom::htmlelement::HTMLElement;
-use dom::node::Node;
+use dom::node::{Node, window_from_node};
 use string_cache::Atom;
 
 #[dom_struct]
@@ -20,40 +25,62 @@ pub struct HTMLDialogElement {
 
 impl HTMLDialogElement {
     fn new_inherited(localName: Atom,
-                     prefix: Option<DOMString>,
-                     document: &Document) -> HTMLDialogElement {
-        HTMLDialogElement {
-            htmlelement:
+        prefix: Option<DOMString>,
+        document: &Document) -> HTMLDialogElement {
+            HTMLDialogElement {
+                htmlelement:
                 HTMLElement::new_inherited(localName, prefix, document),
-            return_value: DOMRefCell::new(DOMString::new()),
+                return_value: DOMRefCell::new(DOMString::new()),
+            }
         }
-    }
 
-    #[allow(unrooted_must_root)]
-    pub fn new(localName: Atom,
-               prefix: Option<DOMString>,
-               document: &Document) -> Root<HTMLDialogElement> {
-        Node::reflect_node(box HTMLDialogElement::new_inherited(localName, prefix, document),
-                           document,
-                           HTMLDialogElementBinding::Wrap)
-    }
-}
+        #[allow(unrooted_must_root)]
+        pub fn new(localName: Atom,
+            prefix: Option<DOMString>,
+            document: &Document) -> Root<HTMLDialogElement> {
+                Node::reflect_node(box HTMLDialogElement::new_inherited(localName, prefix, document),
+                document,
+                HTMLDialogElementBinding::Wrap)
+            }
+        }
 
-impl HTMLDialogElementMethods for HTMLDialogElement {
-    // https://html.spec.whatwg.org/multipage/#dom-dialog-open
-    make_bool_getter!(Open, "open");
+        impl HTMLDialogElementMethods for HTMLDialogElement {
+            // https://html.spec.whatwg.org/multipage/#dom-dialog-open
+            make_bool_getter!(Open, "open");
 
-    // https://html.spec.whatwg.org/multipage/#dom-dialog-open
-    make_bool_setter!(SetOpen, "open");
+            // https://html.spec.whatwg.org/multipage/#dom-dialog-open
+            make_bool_setter!(SetOpen, "open");
 
-    // https://html.spec.whatwg.org/multipage/#dom-dialog-returnvalue
-    fn ReturnValue(&self) -> DOMString {
-        let return_value = self.return_value.borrow();
-        return_value.clone()
-    }
+            // https://html.spec.whatwg.org/multipage/#dom-dialog-returnvalue
+            fn ReturnValue(&self) -> DOMString {
+                let return_value = self.return_value.borrow();
+                return_value.clone()
+            }
 
-    // https://html.spec.whatwg.org/multipage/#dom-dialog-returnvalue
-    fn SetReturnValue(&self, return_value: DOMString) {
-        *self.return_value.borrow_mut() = return_value;
-    }
-}
+            // https://html.spec.whatwg.org/multipage/#dom-dialog-returnvalue
+            fn SetReturnValue(&self, return_value: DOMString) {
+                *self.return_value.borrow_mut() = return_value;
+            }
+
+            // https://html.spec.whatwg.org/multipage/#the-dialog-element:dom-dialog-close
+            fn Close(&self, return_value: Option<DOMString>) -> ErrorResult {
+                let element = self.upcast::<Element>();
+                let target = self.upcast::<EventTarget>();
+                let win = window_from_node(self);
+
+                if !element.has_attribute(&atom!("open")) {
+                    return Err(Error::InvalidState);
+                }
+
+                element.remove_attribute_by_name(&atom!("open"));
+
+                if let Some(new_value) = return_value {
+                    *self.return_value.borrow_mut() = new_value;
+                }
+
+                win.dom_manipulation_task_source().queue_event(target, atom!("close"), EventBubbles::DoesNotBubble,
+                EventCancelable::NotCancelable, win.r());
+                Ok(())
+            }
+
+        }
