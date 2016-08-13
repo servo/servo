4bcaff449bf7422cad781d10d33f7e1c718ae391
clstl <reamles@gmail.com>
2016-08-13 16:50:27 +0300

    fixup! Implements HTMLDialogElement#close

--- components/script/dom/htmldialogelement.rs
+++ components/script/dom/htmldialogelement.rs
@@ -2,20 +2,20 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
- use dom::bindings::cell::DOMRefCell;
- use dom::bindings::codegen::Bindings::HTMLDialogElementBinding;
- use dom::bindings::codegen::Bindings::HTMLDialogElementBinding::HTMLDialogElementMethods;
- use dom::bindings::error::{Error, ErrorResult};
- use dom::bindings::inheritance::Castable;
- use dom::bindings::js::Root;
- use dom::bindings::str::DOMString;
- use dom::document::Document;
- use dom::element::Element;
- use dom::event::{EventBubbles, EventCancelable};
- use dom::eventtarget::EventTarget;
- use dom::htmlelement::HTMLElement;
- use dom::node::{Node, window_from_node};
- use string_cache::Atom;
+use dom::bindings::cell::DOMRefCell;
+use dom::bindings::codegen::Bindings::HTMLDialogElementBinding;
+use dom::bindings::codegen::Bindings::HTMLDialogElementBinding::HTMLDialogElementMethods;
+use dom::bindings::error::{Error, ErrorResult};
+use dom::bindings::inheritance::Castable;
+use dom::bindings::js::Root;
+use dom::bindings::str::DOMString;
+use dom::document::Document;
+use dom::element::Element;
+use dom::event::{EventBubbles, EventCancelable};
+use dom::eventtarget::EventTarget;
+use dom::htmlelement::HTMLElement;
+use dom::node::{Node, window_from_node};
+use string_cache::Atom;
 
 #[dom_struct]
 pub struct HTMLDialogElement {
