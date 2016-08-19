a2a45a77fd2978ed4d8e5eb1994993c47a79a420
Alan Jeffrey <ajeffrey@mozilla.com>
2016-08-19 13:46:28 -0500

    Removed extra space from --userscripts option.

--- etc/ci/performance/runner.py
+++ etc/ci/performance/runner.py
@@ -42,7 +42,7 @@ def execute_test(url, command, timeout):
 def get_servo_command(url):
     ua_script_path = "{}/user-agent-js".format(os.getcwd())
     return ["./servo/servo", url,
-            " --userscripts", ua_script_path,
+            "--userscripts", ua_script_path,
             "-x", "-o", "output.png"]
 
 
