516676aa5fd0443018ee7ff5e8c5ad84ed2333e1
UK992 <urbankrajnc92@gmail.com>
2016-08-20 21:18:42 +0200

    Windows MSVC: Define vcvars in mach.bat

--- mach.bat
+++ mach.bat
@@ -1,2 +1,19 @@
 @echo off
+
+SET VS_VCVARS=%VS140COMNTOOLS%..\..\VC\vcvarsall.bat
+IF EXIST "%VS_VCVARS%" (
+  IF NOT DEFINED VisualStudioVersion (
+    IF EXIST "%ProgramFiles(x86)%" (
+      call "%VS_VCVARS%" x64
+    ) ELSE (
+      ECHO 32-bit Windows is currently unsupported.
+      EXIT /B
+    )
+  )
+) ELSE (
+  ECHO Visual Studio 2015 is not installed.
+  ECHO Download and install Visual Studio 2015 from https://www.visualstudio.com/
+  EXIT /B
+)
+
 python mach %*
