b9d91929aac89c3fb42d5bffb1e19a685f334694
Emilio Cobos Álvarez <ecoal95@gmail.com>
2016-08-30 08:57:44 -0700

    Fix debug assertion introduced in #13108 by me.
    I'm stupid, and when I did the last moving-the-code-around, I failed miserably
    to double-check it in a debug build.

--- components/style/restyle_hints.rs
+++ components/style/restyle_hints.rs
@@ -470,10 +470,10 @@ impl DependencySet {
             return;
         }
         for dep in deps {
-            debug_assert!(state_changes.intersects(dep.sensitivities.states) ||
-                          attrs_changed && dep.sensitivities.attrs,
-                          "Testing a completely ineffective dependency?");
-            if !hint.intersects(dep.hint) {
+            debug_assert!((!state_changes.is_empty() && !dep.sensitivities.states.is_empty()) ||
+                          (attrs_changed && dep.sensitivities.attrs),
+                          "Testing a known ineffective dependency?");
+            if (attrs_changed || state_changes.intersects(dep.sensitivities.states)) && !hint.intersects(dep.hint) {
                 let matched_then =
                     matches_complex_selector(&dep.selector, snapshot, None,
                                              &mut StyleRelations::empty());
