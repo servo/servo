5f5266a3e54bff056c75e212a6c2b8d77dd49297
Anthony Ramine <n.oxyde@gmail.com>
2016-09-11 14:41:42 +0200

    Reorder features in script crate

--- components/script/lib.rs
+++ components/script/lib.rs
@@ -14,9 +14,9 @@
 #![feature(on_unimplemented)]
 #![feature(optin_builtin_traits)]
 #![feature(plugin)]
+#![feature(question_mark)]
 #![feature(slice_patterns)]
 #![feature(stmt_expr_attributes)]
-#![feature(question_mark)]
 #![feature(try_borrow)]
 #![feature(try_from)]
 
