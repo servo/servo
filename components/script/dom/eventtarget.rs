4f8255cd696794e0831d3555e0141477f3d2130d
Taryn Hill <Phrohdoh@gmail.com>
2016-09-21 19:15:56 -0500

    Implement step 1.2 of event handler compilation

--- components/script/dom/eventtarget.rs
+++ components/script/dom/eventtarget.rs
@@ -375,7 +375,10 @@ impl EventTarget {
             None => self.downcast::<Window>().unwrap().Document(),
         };
 
-        // TODO step 1.2 (browsing context/scripting enabled)
+        // Step 1.2
+        if document.browsing_context().is_none() || !document.is_scripting_enabled() {
+            return None;
+        }
 
         // Step 1.3
         let body: Vec<u16> = handler.source.encode_utf16().collect();
