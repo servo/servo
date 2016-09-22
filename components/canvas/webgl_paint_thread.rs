f8e731d280c98522b9546bff9c416dd09c2f7f85
Glenn Watson <github@intuitionlibrary.com>
2016-09-23 08:51:42 +1000

    Update webgl paint code to match webrender behaviour change. This means that creation of the fallback reader painter in webrender mode is handled by webrender itself. This allows webrender to create the correct kind of context (osmesa or hardware) instead of the webgl paint thread having to deal with this.

--- components/canvas/webgl_paint_thread.rs
+++ components/canvas/webgl_paint_thread.rs
@@ -14,8 +14,8 @@ use util::thread::spawn_named;
 use webrender_traits;
 
 enum WebGLPaintTaskData {
-    WebRender(webrender_traits::RenderApi, webrender_traits::WebGLContextId),
-    Readback(GLContext<NativeGLContext>, (Option<(webrender_traits::RenderApi, webrender_traits::ImageKey)>)),
+    WebRender(webrender_traits::RenderApi, webrender_traits::WebGLContextId, Option<webrender_traits::ImageKey>),
+    Readback(GLContext<NativeGLContext>),
 }
 
 pub struct WebGLPaintThread {
@@ -23,19 +23,13 @@ pub struct WebGLPaintThread {
     data: WebGLPaintTaskData,
 }
 
-fn create_readback_painter(size: Size2D<i32>,
-                           attrs: GLContextAttributes,
-                           webrender_api: Option<webrender_traits::RenderApi>)
-    -> Result<(WebGLPaintThread, GLLimits), String> {
+fn create_readback_painter(size: Size2D<i32>, attrs: GLContextAttributes)
+                            -> Result<(WebGLPaintThread, GLLimits), String> {
     let context = try!(GLContext::<NativeGLContext>::new(size, attrs, ColorAttachmentType::Texture, None));
     let limits = context.borrow_limits().clone();
-    let webrender_api_and_image_key = webrender_api.map(|wr| {
-        let key = wr.alloc_image();
-        (wr, key)
-    });
     let painter = WebGLPaintThread {
         size: size,
-        data: WebGLPaintTaskData::Readback(context, webrender_api_and_image_key)
+        data: WebGLPaintTaskData::Readback(context)
     };
 
     Ok((painter, limits))
@@ -48,31 +42,30 @@ impl WebGLPaintThread {
         -> Result<(WebGLPaintThread, GLLimits), String> {
         if let Some(sender) = webrender_api_sender {
             let wr_api = sender.create_api();
-            match wr_api.request_webgl_context(&size, attrs) {
-                Ok((id, limits)) => {
-                    let painter = WebGLPaintThread {
-                        data: WebGLPaintTaskData::WebRender(wr_api, id),
-                        size: size
-                    };
-                    Ok((painter, limits))
-                },
-                Err(msg) => {
-                    warn!("Initial context creation failed, falling back to readback: {}", msg);
-                    create_readback_painter(size, attrs, Some(wr_api))
-                }
-            }
+            let (id, limits, is_shared) = try!(wr_api.request_webgl_context(&size, attrs));
+            let image_key = if is_shared {
+                None
+            } else {
+                Some(wr_api.alloc_image())
+            };
+
+            let painter = WebGLPaintThread {
+                data: WebGLPaintTaskData::WebRender(wr_api, id, image_key),
+                size: size
+            };
+            Ok((painter, limits))
         } else {
-            create_readback_painter(size, attrs, None)
+            create_readback_painter(size, attrs)
         }
     }
 
     fn handle_webgl_message(&self, message: webrender_traits::WebGLCommand) {
         debug!("WebGL message: {:?}", message);
         match self.data {
-            WebGLPaintTaskData::WebRender(ref api, id) => {
+            WebGLPaintTaskData::WebRender(ref api, id, _) => {
                 api.send_webgl_command(id, message);
             }
-            WebGLPaintTaskData::Readback(ref ctx, _) => {
+            WebGLPaintTaskData::Readback(ref ctx) => {
                 message.apply(ctx);
             }
         }
@@ -122,47 +115,61 @@ impl WebGLPaintThread {
         result_port.recv().unwrap().map(|limits| (sender, limits))
     }
 
+    fn read_back_pixels(&self) -> Vec<u8> {
+        let width = self.size.width as usize;
+        let height = self.size.height as usize;
+
+        let mut pixels = gl::read_pixels(0, 0,
+                                         self.size.width as gl::GLsizei,
+                                         self.size.height as gl::GLsizei,
+                                         gl::RGBA, gl::UNSIGNED_BYTE);
+        // flip image vertically (texture is upside down)
+        let orig_pixels = pixels.clone();
+        let stride = width * 4;
+        for y in 0..height {
+            let dst_start = y * stride;
+            let src_start = (height - y - 1) * stride;
+            let src_slice = &orig_pixels[src_start .. src_start + stride];
+            (&mut pixels[dst_start .. dst_start + stride]).clone_from_slice(&src_slice[..stride]);
+        }
+
+        // rgba -> bgra
+        byte_swap(&mut pixels);
+
+        pixels
+    }
+
     fn send_data(&mut self, chan: IpcSender<CanvasData>) {
         match self.data {
-            WebGLPaintTaskData::Readback(_, ref webrender_api_and_image_key) => {
-                let width = self.size.width as usize;
-                let height = self.size.height as usize;
-
-                let mut pixels = gl::read_pixels(0, 0,
-                                                 self.size.width as gl::GLsizei,
-                                                 self.size.height as gl::GLsizei,
-                                                 gl::RGBA, gl::UNSIGNED_BYTE);
-                // flip image vertically (texture is upside down)
-                let orig_pixels = pixels.clone();
-                let stride = width * 4;
-                for y in 0..height {
-                    let dst_start = y * stride;
-                    let src_start = (height - y - 1) * stride;
-                    let src_slice = &orig_pixels[src_start .. src_start + stride];
-                    (&mut pixels[dst_start .. dst_start + stride]).clone_from_slice(&src_slice[..stride]);
-                }
-
-                // rgba -> bgra
-                byte_swap(&mut pixels);
-
-                if let Some((ref wr, wr_image_key)) = *webrender_api_and_image_key {
-                    // TODO: This shouldn't be a common path, but try to avoid
-                    // the spurious clone().
-                    wr.update_image(wr_image_key,
-                                    width as u32,
-                                    height as u32,
-                                    webrender_traits::ImageFormat::RGBA8,
-                                    pixels.clone());
-                }
+            WebGLPaintTaskData::Readback(..) => {
+                let pixels = self.read_back_pixels();
 
                 let pixel_data = CanvasPixelData {
                     image_data: IpcSharedMemory::from_bytes(&pixels[..]),
-                    image_key: webrender_api_and_image_key.as_ref().map(|&(_, key)| key),
+                    image_key: None,
                 };
 
                 chan.send(CanvasData::Pixels(pixel_data)).unwrap();
             }
-            WebGLPaintTaskData::WebRender(_, id) => {
+            WebGLPaintTaskData::WebRender(ref wr_api, _, Some(image_key)) => {
+                let pixels = self.read_back_pixels();
+
+                // TODO: This shouldn't be a common path, but try to avoid
+                // the spurious clone().
+                wr_api.update_image(image_key,
+                                    self.size.width as u32,
+                                    self.size.height as u32,
+                                    webrender_traits::ImageFormat::RGBA8,
+                                    pixels.clone());
+
+                let pixel_data = CanvasPixelData {
+                    image_data: IpcSharedMemory::from_bytes(&pixels[..]),
+                    image_key: Some(image_key),
+                };
+
+                chan.send(CanvasData::Pixels(pixel_data)).unwrap();
+            }
+            WebGLPaintTaskData::WebRender(_, id, None) => {
                 chan.send(CanvasData::WebGL(id)).unwrap();
             }
         }
@@ -171,7 +178,7 @@ impl WebGLPaintThread {
     #[allow(unsafe_code)]
     fn recreate(&mut self, size: Size2D<i32>) -> Result<(), &'static str> {
         match self.data {
-            WebGLPaintTaskData::Readback(ref mut context, _) => {
+            WebGLPaintTaskData::Readback(ref mut context) => {
                 if size.width > self.size.width ||
                    size.height > self.size.height {
                     try!(context.resize(size));
@@ -181,7 +188,7 @@ impl WebGLPaintThread {
                     unsafe { gl::Scissor(0, 0, size.width, size.height); }
                 }
             }
-            WebGLPaintTaskData::WebRender(_, _) => {
+            WebGLPaintTaskData::WebRender(..) => {
                 // TODO
             }
         }
@@ -190,7 +197,7 @@ impl WebGLPaintThread {
     }
 
     fn init(&mut self) {
-        if let WebGLPaintTaskData::Readback(ref context, _) = self.data {
+        if let WebGLPaintTaskData::Readback(ref context) = self.data {
             context.make_current().unwrap();
         }
     }
@@ -198,7 +205,7 @@ impl WebGLPaintThread {
 
 impl Drop for WebGLPaintThread {
     fn drop(&mut self) {
-        if let WebGLPaintTaskData::Readback(_, Some((ref mut wr, image_key))) = self.data {
+        if let WebGLPaintTaskData::WebRender(ref wr, _, Some(image_key)) = self.data {
             wr.delete_image(image_key);
         }
     }
