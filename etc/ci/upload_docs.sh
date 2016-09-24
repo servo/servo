80166176a3a53498e989bfa6eb8a58859c0428ad
Josh Matthews <josh@joshmatthews.net>
2016-09-24 15:06:06 -0400

    Fix doc build by resurrecting generation of supported DOM APIs.h

--- etc/ci/upload_docs.sh
+++ etc/ci/upload_docs.sh
@@ -20,9 +20,11 @@ cp etc/doc.servo.org/* target/doc/
 
 python components/style/properties/build.py servo html
 
-OUT_DIR="$(pwd)/target/doc/servo" \
-  make -f makefile.cargo -C components/script dom_docs
-rm -rf target/doc/servo/.cache
+cd components/script
+cmake .
+cmake --build . --target supported-apis
+cp apis.html target/doc/servo/
+cd ../..
 
 ghp-import -n target/doc
 git push -qf "https://${TOKEN}@github.com/servo/doc.servo.org.git" gh-pages
