servo/servo

commit 0a86543e6decf02860a5a98c46e01940e79af6fa

Let textarea wrap lines

resources/servo.css
@@ -9,7 +9,7 @@ input {
   color: black;
   font-family: sans-serif;
   font-size: 0.8333em;
-  white-space: nowrap;
+  white-space: pre;
   text-align: left;
   line-height: 1.8;
 }
@@ -24,7 +24,7 @@ textarea {
   color: black;
   font-family: sans-serif;
   font-size: 0.8333em;
-  white-space: pre;
+  white-space: pre-wrap;
 }
 
 input::selection,

components/layout/display_list_builder.rs
@@ -29,7 +29,6 @@ use gfx::display_list::{LayeredItem, LayerInfo, LineDisplayItem, OpaqueNode};
 use gfx::display_list::{SolidColorDisplayItem, StackingContext, StackingContextType};
 use gfx::display_list::{TextDisplayItem, TextOrientation, WebRenderImageInfo};
 use gfx::paint_thread::THREAD_TINT_COLORS;
-use gfx::text::glyph::ByteIndex;
 use gfx_traits::{color, ScrollPolicy, StackingContextId};
 use inline::{FIRST_FRAGMENT_OF_ELEMENT, InlineFlow, LAST_FRAGMENT_OF_ELEMENT};
 use ipc_channel::ipc;
@@ -970,7 +969,8 @@ impl FragmentDisplayListBuilding for Fragment {
             Some(insertion_point_index) => insertion_point_index,
             None => return,
         };
-        let range = Range::new(ByteIndex(0), insertion_point_index);
+        let range = Range::new(scanned_text_fragment_info.range.begin(),
+                               insertion_point_index - scanned_text_fragment_info.range.begin());
         let advance = scanned_text_fragment_info.run.advance_for_range(&range);
 
         let insertion_point_bounds;

components/layout/fragment.rs
@@ -882,16 +882,23 @@ impl Fragment {
         let size = LogicalSize::new(self.style.writing_mode,
                                     split.inline_size,
                                     self.border_box.size.block);
-        let flags = match self.specific {
-            SpecificFragmentInfo::ScannedText(ref info) => info.flags,
-            _ => ScannedTextFlags::empty()
+        // Preserve the insertion point if it is in this fragment's range or it is at line end.
+        let (flags, insertion_point) = match self.specific {
+            SpecificFragmentInfo::ScannedText(ref info) => {
+                match info.insertion_point {
+                    Some(index) if split.range.contains(index) => (info.flags, info.insertion_point),
+                    Some(index) if index == ByteIndex(text_run.text.chars().count() as isize - 1) &&
+                        index == split.range.end() => (info.flags, info.insertion_point),
+                    _ => (info.flags, None)
+                }
+            },
+            _ => (ScannedTextFlags::empty(), None)
         };
-        // FIXME(pcwalton): This should modify the insertion point as necessary.
         let info = box ScannedTextFragmentInfo::new(
             text_run,
             split.range,
             size,
-            None,
+            insertion_point,
             flags);
         self.transform(size, SpecificFragmentInfo::ScannedText(info))
     }

components/style/properties/properties.mako.rs
@@ -2130,10 +2130,6 @@ pub fn modify_style_for_input_text(style: &mut Arc<ServoComputedValues>) {
     margin_style.margin_right = computed::LengthOrPercentageOrAuto::Length(Au(0));
     margin_style.margin_bottom = computed::LengthOrPercentageOrAuto::Length(Au(0));
     margin_style.margin_left = computed::LengthOrPercentageOrAuto::Length(Au(0));
-
-    // whitespace inside text input should not be collapsed
-    let inherited_text = Arc::make_mut(&mut style.inheritedtext);
-    inherited_text.white_space = longhands::white_space::computed_value::T::pre;
 }
 
 /// Adjusts the `clip` property so that an inline absolute hypothetical fragment doesn't clip its

commit 2d31546ebada34270b9e2a2c8d0a3d4a0c77d70b

resources: Give button elements `white-space: normal` again.
Fixes Twitter.

Originally regressed in 0a86543e6decf02860a5a98c46e01940e79af6fa.

resources/servo.css
@@ -9,7 +9,6 @@ input {
   color: black;
   font-family: sans-serif;
   font-size: 0.8333em;
-  white-space: pre;
   text-align: left;
   line-height: 1.8;
 }
@@ -130,6 +129,10 @@ textarea {
   cursor: text;
   overflow: hidden;
   -servo-overflow-clip-box: content-box;
+}
+
+input:not([type=radio i]):not([type=checkbox i]):not([type=reset i]):not([type=button i]):not([type=submit i]) {
+  white-space: pre;
 }
 
 textarea {

tests/wpt/mozilla/tests/css/button_whitespace_a.html
@@ -1,0 +1,7 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<title></title>
+<link rel="match" href="button_whitespace_ref.html">
+<button>A
+
+B</button>

tests/wpt/mozilla/tests/css/button_whitespace_ref.html
@@ -1,0 +1,5 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<title></title>
+<button>A B</button>
+