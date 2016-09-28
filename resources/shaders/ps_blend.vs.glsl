f562c426e345ed39eeb09c3ea618685252a7807a
Glenn Watson <github@intuitionlibrary.com>
2016-09-28 17:18:50 +1000

    Switch default renderer to webrender, and update webrender.

--- resources/shaders/ps_blend.vs.glsl
+++ resources/shaders/ps_blend.vs.glsl
@@ -15,7 +15,7 @@ void main(void) {
     vec2 st0 = vec2(src.target_rect.xy) / 2048.0;
     vec2 st1 = vec2(src.target_rect.xy + src.target_rect.zw) / 2048.0;
     vUv = mix(st0, st1, aPosition.xy);
-    vOpacity = blend.src_id_target_id_opacity.z;
+    vBrightnessOpacity = blend.src_id_target_id_opacity.zw;
 
     gl_Position = uTransform * vec4(local_pos, 0, 1);
 }
