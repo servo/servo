dc8bdff29b582dca93c5cfdcb7533556ede61536
Rafael Gomes Dantas <rafagd@gmail.com>
2016-10-05 19:49:40 +0100

    Correcting source tidyness

--- components/style/gecko_bindings/check_bindings.py
+++ components/style/gecko_bindings/check_bindings.py
@@ -1,16 +1,23 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 import re
 
 bindings = open("bindings.rs", "r")
-tests    = open("test_bindings.rs", "w")
+tests = open("test_bindings.rs", "w")
 
+tests.write("/* This Source Code Form is subject to the terms of the Mozilla Public\n")
+tests.write(" * License, v. 2.0. If a copy of the MPL was not distributed with this\n")
+tests.write(" * file, You can obtain one at http://mozilla.org/MPL/2.0/. */\n\n")
 tests.write("fn assert_types() {\n")
 
-pattern  = re.compile("fn\s*Servo_([a-zA-Z0-9]+)\s*\(")
+pattern = re.compile("fn\s*Servo_([a-zA-Z0-9]+)\s*\(")
 
 for line in bindings:
-    match = pattern.search(line);
+    match = pattern.search(line)
 
-    if match :
+    if match:
         tests.write("    [ Servo_" + match.group(1) + ", bindings::Servo_" + match.group(1) + " ];\n")
 
 tests.write("}\n")
--- components/style/gecko_bindings/test_bindings.rs
+++ components/style/gecko_bindings/test_bindings.rs
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 fn assert_types() {
     [ Servo_ParseStyleAttribute, bindings::Servo_ParseStyleAttribute ];
     [ Servo_CSSSupports, bindings::Servo_CSSSupports ];
