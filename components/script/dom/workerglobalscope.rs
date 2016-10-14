ffdc15368f9629142d73ba96ed3614aa6d25cf13
Keith Yeung <kungfukeith11@gmail.com>
2016-10-13 17:41:48 -0700

    Properly implement TaskSource for NetworkingTaskSource

--- components/script/dom/workerglobalscope.rs
+++ components/script/dom/workerglobalscope.rs
@@ -42,6 +42,7 @@ use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::mpsc::Receiver;
 use task_source::file_reading::FileReadingTaskSource;
+use task_source::networking::NetworkingTaskSource;
 use timers::{IsInterval, TimerCallback};
 use url::Url;
 
@@ -362,6 +363,10 @@ impl WorkerGlobalScope {
 
     pub fn file_reading_task_source(&self) -> FileReadingTaskSource {
         FileReadingTaskSource(self.script_chan())
+    }
+
+    pub fn networking_task_source(&self) -> NetworkingTaskSource {
+        NetworkingTaskSource(self.script_chan())
     }
 
     pub fn new_script_pair(&self) -> (Box<ScriptChan + Send>, Box<ScriptPort + Send>) {
