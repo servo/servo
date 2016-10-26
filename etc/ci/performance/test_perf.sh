6c0788a5143a095389c24370f3308cceb8b4bc79
Shing Lyu <shing.lyu@gmail.com>
2016-10-26 17:40:22 +0800

    Use headless rendering for performance test

--- etc/ci/performance/test_perf.sh
+++ etc/ci/performance/test_perf.sh
@@ -12,8 +12,14 @@ TP5N_SOURCE="https://people.mozilla.org/~jmaher/taloszips/zips/tp5n.zip"
 TP5N_PATH="page_load_test/tp5n.zip"
 if [[ ! -f "${TP5N_PATH}" ]]; then
     wget "${TP5N_SOURCE}" -O "${TP5N_PATH}"
+else
+    echo "Found existing test cases, skip download."
 fi
-unzip -o "${TP5N_PATH}" -d "$(dirname "${TP5N_PATH}")"
+if [[ ! -f "$(dirname "${TP5N_PATH}")/tp5n/tp5n.manifest" ]]; then
+    unzip -o "${TP5N_PATH}" -d "$(dirname "${TP5N_PATH}")"
+else
+    echo "Found existing test cases, skip unzip."
+fi
 
 virtualenv venv --python="$(which python3)"
 PS1="" source venv/bin/activate
