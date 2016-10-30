32ba45ab6fe350c70aaad0ed29b66995571186c7
Rohit Burra <iamrohit7@gmail.com>
2016-10-30 20:01:53 +0530

    Fix tidy issues

--- tests/unit/style/lib.rs
+++ tests/unit/style/lib.rs
@@ -3,8 +3,8 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #![cfg(test)]
-#![feature(plugin)]
 #![feature(core_intrinsics)]
+#![feature(plugin)]
 
 extern crate app_units;
 extern crate cssparser;
