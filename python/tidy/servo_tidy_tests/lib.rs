c4cb3f345bfba617fd090454b842437014dd269d
Rohit Burra <iamrohit7@gmail.com>
2016-10-30 13:55:31 +0530

    Tests alphabetical ordering of #![feature(...)]

---
+++ python/tidy/servo_tidy_tests/lib.rs
@@ -0,0 +1,14 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#![feature(def)]
+#![feature(abc)]
+
+#![feature(abc, def, ghi)]
+#![feature(aaa)]
+
+#![feature(def, ghi, abc)]
+
+#![feature(ghi)]
+#![feature(abd, hef)]
