b216e5c97b188057d7d156bf757c7271dd270856
Ravi Shankar <wafflespeanut@gmail.com>
2016-11-06 16:22:11 +0530

    Move Servo-specific ToCss to style_traits

--- components/style_traits/lib.rs
+++ components/style_traits/lib.rs
@@ -23,6 +23,7 @@ extern crate euclid;
 extern crate rustc_serialize;
 #[cfg(feature = "servo")] extern crate serde;
 #[cfg(feature = "servo")] #[macro_use] extern crate serde_derive;
+extern crate url;
 
 /// Opaque type stored in type-unsafe work queues for parallel layout.
 /// Must be transmutable to and from TNode.
@@ -61,3 +62,4 @@ pub mod cursor;
 pub mod values;
 pub mod viewport;
 
+pub use values::ToCss;
