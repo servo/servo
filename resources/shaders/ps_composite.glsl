c474e48e726f85a39b9f625ae3a259f77b9ad9b2
Glenn Watson <github@intuitionlibrary.com>
2016-11-21 09:50:55 +1000

    Remove old shaders (these are embedded now in WR).

--- resources/shaders/ps_composite.glsl
+++
@@ -1,8 +0,0 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-varying vec3 vUv0;
-varying vec3 vUv1;
-flat varying vec4 vUv1Rect;
-flat varying int vOp;
