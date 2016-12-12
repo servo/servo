9db92d8e9ea2c793da8b058d7a5ccd94e02b62d3
Keith Yeung <kungfukeith11@gmail.com>
2016-12-12 14:00:20 -0800

    Add basic skeletons to MessagePort

--- components/script/dom/mod.rs
+++ components/script/dom/mod.rs
@@ -392,6 +392,7 @@ pub mod medialist;
 pub mod mediaquerylist;
 pub mod mediaquerylistevent;
 pub mod messageevent;
+pub mod messageport;
 pub mod mimetype;
 pub mod mimetypearray;
 pub mod mouseevent;
