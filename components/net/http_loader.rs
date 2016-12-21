b5101f506eccb6f8f32f5f5c2e679401819fd941
Nick Price <nick@spun.io>
2016-12-21 08:48:12 -0500

    move to methods.rs

--- components/net/http_loader.rs
+++ components/net/http_loader.rs
@@ -537,20 +537,6 @@ pub fn http_fetch(request: Rc<Request>,
                   done_chan: &mut DoneChannel,
                   context: &FetchContext)
                   -> Response {
-    // Bad port blocking - https://fetch.spec.whatwg.org/#block-bad-port
-    if let Some(port) = request.url().port() {
-        let is_ftp = request.url().scheme() == "ftp" && (port == 20 || port == 21);
-        static BAD_PORTS: [u16; 64] = [1, 7, 9, 11, 13, 15, 17, 19, 20, 21, 22, 23, 25, 37, 42,
-                                       43, 53, 77, 79, 87, 95, 101, 102, 103, 104, 109, 110, 111,
-                                       113, 115, 117, 119, 123, 135, 139, 143, 179, 389, 465, 512,
-                                       513, 514, 515, 526, 530, 531, 532, 540, 556, 563, 587, 601,
-                                       636, 993, 995, 2049, 3659, 4045, 6000, 6665, 6666, 6667,
-                                       6668, 6669];
-        if !is_ftp && BAD_PORTS.binary_search(&port).is_ok() {
-            return Response::network_error(NetworkError::Internal("Request attempted on bad port".into()));
-        }
-    }
-
     // This is a new async fetch, reset the channel we are waiting on
     *done_chan = None;
     // Step 1
