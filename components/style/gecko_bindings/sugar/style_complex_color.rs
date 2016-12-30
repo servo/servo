429f2ab386298261a8c2a9cd49f17ea75e6b975f
Cameron McCormack <cam@mcc.id.au>
2016-12-30 16:33:21 +0800

    stylo: Update glue for recent StyleComplexColor changes.

--- components/style/gecko_bindings/sugar/style_complex_color.rs
+++ components/style/gecko_bindings/sugar/style_complex_color.rs
@@ -11,6 +11,7 @@ impl From<nscolor> for StyleComplexColor {
         StyleComplexColor {
             mColor: other,
             mForegroundRatio: 0,
+            mIsAuto: false,
         }
     }
 }
@@ -20,6 +21,7 @@ impl StyleComplexColor {
         StyleComplexColor {
             mColor: 0,
             mForegroundRatio: 255,
+            mIsAuto: false,
         }
     }
 }
