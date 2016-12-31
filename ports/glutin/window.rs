4541c1e2bff359b9e8477bf138adbbf51a3f98ca
Gregory Terzian <gterzian@users.noreply.github.com>
2016-12-31 19:38:12 +0100

    update mouse position when receiving mouse wheel events

--- ports/glutin/window.rs
+++ ports/glutin/window.rs
@@ -421,10 +421,13 @@ impl Window {
                 self.event_queue.borrow_mut().push(
                     WindowEvent::MouseWindowMoveEventClass(TypedPoint2D::new(x as f32, y as f32)));
             }
-            Event::MouseWheel(delta, phase) => {
+            Event::MouseWheel(delta, phase, pos) => {
                 let (dx, dy) = match delta {
                     MouseScrollDelta::LineDelta(dx, dy) => (dx, dy * LINE_HEIGHT),
                     MouseScrollDelta::PixelDelta(dx, dy) => (dx, dy),
+                };
+                if let Some((x, y)) = pos {
+                  self.mouse_pos.set(Point2D::new(x, y));
                 };
                 let phase = glutin_phase_to_touch_event_type(phase);
                 self.scroll_window(dx, dy, phase);
