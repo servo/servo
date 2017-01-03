f3841294bdcc42d2b316fd41b81fb486ed92f411
Manish Goregaokar <manishsmail@gmail.com>
2017-01-02 19:09:07 -0800

    Add spec links for list properties

--- components/style/properties/shorthand/list.mako.rs
+++ components/style/properties/shorthand/list.mako.rs
@@ -5,7 +5,8 @@
 <%namespace name="helpers" file="/helpers.mako.rs" />
 
 <%helpers:shorthand name="list-style"
-                    sub_properties="list-style-image list-style-position list-style-type">
+                    sub_properties="list-style-image list-style-position list-style-type"
+                    spec="https://drafts.csswg.org/css-lists/#propdef-list-style">
     use properties::longhands::{list_style_image, list_style_position, list_style_type};
     use values::{Either, None_};
 
