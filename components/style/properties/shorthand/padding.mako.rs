70cf017c6b5300860e3403a678cefc0d752eb6f4
Manish Goregaokar <manishsmail@gmail.com>
2017-01-02 19:25:22 -0800

    Add spec links for padding properties

--- components/style/properties/shorthand/padding.mako.rs
+++ components/style/properties/shorthand/padding.mako.rs
@@ -4,4 +4,5 @@
 
 <%namespace name="helpers" file="/helpers.mako.rs" />
 
-${helpers.four_sides_shorthand("padding", "padding-%s", "specified::LengthOrPercentage::parse")}
+${helpers.four_sides_shorthand("padding", "padding-%s", "specified::LengthOrPercentage::parse",
+                               spec="https://drafts.csswg.org/css-box-3/#propdef-padding")}
