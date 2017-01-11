6b00847fe03252ad4a96bdc7403cf5482c8126ef
Aneesh Agrawal <aneeshusa@gmail.com>
2017-01-11 16:43:54 -0500

    Add a Salt bootstrapper
    The Salt bootstrapper invokes Salt during `./mach bootstrap`
    to install Servo's build dependencies.
    It uses salt-call pinned to the same version of Salt as used in saltfs.
    
    Currently, the implementation uses gitfs and reads directly from
    the master branch of the saltfs repo;
    in the future this should be changed when the relevant Salt states
    are moved in-tree as part of Dockerization for TaskCluster.
    
    We have not Salted our Windows machines, so the existing Windows
    bootstrappers are retained. Currently this is only tested on
    Ubuntu Trusty.
    
    Salt uses various system python libraries,
    including `python-apt` on Debian-based OSes to interact with apt.
    `python-apt` does not seem to be installable via the requirements.txt
    file, and the versions available on PyPI are far behind the versions
    installed on actual Ubuntu machines.
    Additionally, adding `python-apt` as an unconditional python dependency
    would add bloat for users of other OSes, and lead to more churn
    as additional OSes are supported.
    However, as `python-apt` is already installed via apt on these machines,
    we can allow Salt to instead use the module by using
    `--system-site-packages` for the python virtualenv.
    We also add the `-I` flag to `pip install` to ensure we have a local,
    untouched copy of any other python packages used.
    However, because this prints system-level Python packages in scope,
    it slightly breaks isolation, so it is important to always pin
    all dependencies in the requirements.txt file.

---
+++ etc/salt/pillars/bootstrap.sls
@@ -0,0 +1,1 @@
+fully_managed: False
--- python/servo/bootstrap.py
+++ python/servo/bootstrap.py
@@ -5,12 +5,90 @@
 from __future__ import absolute_import, print_function
 
 import distutils.spawn
+import json
 import os
+import platform
 import subprocess
 import sys
 
 from servo.util import extract, download_file
 import servo.packages as packages
+
+
+def salt(context, force=False):
+    salt_root = os.path.join(context.sharedir, 'salt')
+    config_dir = os.path.join(salt_root, 'etc', 'salt')
+    pillar_dir = os.path.join(context.topdir, 'etc', 'salt', 'pillars')
+
+    config = {
+        'fileserver_backend': ['git'],
+        # TODO: replace aneeshusa remote with servo remote,
+        # remove gitfs_base after relevant saltfs PR is merged
+        'gitfs_base': 'enable-mach-bootstrap',
+        'gitfs_env_whitelist': 'base',
+        'gitfs_provider': 'gitpython',
+        'gitfs_remotes': [
+            'https://github.com/aneeshusa/servo-saltfs.git',
+        ],
+        'hash_type': 'sha384',
+        'master': 'localhost',
+        'root_dir': salt_root,
+        'state_output': 'changes',
+        'state_tabular': True,
+    }
+
+    if not os.path.exists(config_dir):
+        os.makedirs(config_dir, mode=0o700)
+    # In order to allow `mach bootstrap` to work from any CWD,
+    # the `root_dir` must be an absolute path.
+    # We place it under `context.sharedir` because
+    # Salt caches data (e.g. gitfs files) in its `var` subdirectory.
+    # Hence, dynamically generate the config with an appropriate `root_dir`
+    # and serialize it as JSON (which is valid YAML).
+    with open(os.path.join(config_dir, 'minion'), 'w') as config_file:
+        config_file.write(json.dumps(config))
+
+    cmd = [
+        'sudo',
+        # sudo escapes from the venv, need to use full path
+        distutils.spawn.find_executable('salt-call'),
+        '--local',
+        '--config-dir={}'.format(config_dir),
+        '--pillar-root={}'.format(pillar_dir),
+        'state.apply',
+        'servo-build-dependencies',
+    ]
+
+    if not force:
+        print('Running bootstrap in dry-run mode to show changes')
+        # Because `test=True` mode runs each state individually without
+        # considering how required/previous states affect the system,
+        # it will often report states with requisites as failing due
+        # to the requisites not actually being run,
+        # even though these are spurious and will succeed during
+        # the actual highstate.
+        # Hence `--retcode-passthrough` is not helpful in dry-run mode,
+        # so only detect failures of the actual salt-call binary itself.
+        retcode = subprocess.call(cmd + ['test=True'])
+        if retcode != 0:
+            print('Something went wrong while bootstrapping')
+            return retcode
+
+        proceed = raw_input(
+            'Proposed changes are above, proceed with bootstrap? [y/N]: '
+        )
+        if proceed not in ['y', 'yes', 'Y', 'Yes', 'YES']:
+            return 0
+
+        print('')
+
+    print('Running Salt bootstrap')
+    retcode = subprocess.call(cmd + ['--retcode-passthrough'])
+    if retcode == 0:
+        print('Salt bootstrapping complete')
+    else:
+        print('Salt bootstrapping encountered errors')
+    return retcode
 
 
 def windows_gnu(context, force=False):
@@ -99,9 +177,12 @@ def bootstrap(context, force=False):
 
     if sys.platform.startswith('msys'):
         bootstrapper = windows_gnu
-
     elif sys.platform.startswith('win32'):
         bootstrapper = windows_msvc
+    elif sys.platform.startswith('linux'):
+        distro, version, _ = platform.linux_distribution()
+        if distro == 'Ubuntu' and version == '14.04':
+            bootstrapper = salt
 
     if bootstrapper is None:
         sys.exit('Bootstrap support is not yet available for your OS.')
