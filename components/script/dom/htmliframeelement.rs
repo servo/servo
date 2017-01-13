46220d409e75ddedefa3585115c3ea89a1e74b25
Connor Brewster <connor.brewster@eagles.oc.edu>
2017-01-13 13:33:44 -0600

    script: cancel load event if new load is initiated If no src is set when binding an iframe to the tree, it will queue a load event. If a new load has been triggered by setting src before the load event has been fired, it should be cancelled.

--- components/script/dom/htmliframeelement.rs
+++ components/script/dom/htmliframeelement.rs
@@ -90,6 +90,7 @@ pub struct HTMLIFrameElement {
     sandbox_allowance: Cell<Option<SandboxAllowance>>,
     load_blocker: DOMRefCell<Option<LoadBlocker>>,
     visibility: Cell<bool>,
+    cancel_initial_load_event: Cell<bool>,
 }
 
 impl HTMLIFrameElement {
@@ -180,6 +181,7 @@ impl HTMLIFrameElement {
                 ScriptThread::process_attach_layout(new_layout_info, document.origin().alias());
             },
             NavigationType::Normal => {
+                self.cancel_initial_load_event.set(true);
                 let load_info = IFrameLoadInfoWithData {
                     info: load_info,
                     load_data: load_data,
@@ -264,6 +266,7 @@ impl HTMLIFrameElement {
             sandbox_allowance: Cell::new(None),
             load_blocker: DOMRefCell::new(None),
             visibility: Cell::new(true),
+            cancel_initial_load_event: Cell::new(false),
         }
     }
 
@@ -770,6 +773,8 @@ impl IFrameLoadEventSteps {
 impl Runnable for IFrameLoadEventSteps {
     fn handler(self: Box<IFrameLoadEventSteps>) {
         let this = self.frame_element.root();
-        this.iframe_load_event_steps(self.pipeline_id);
+        if !this.cancel_initial_load_event.get() {
+            this.iframe_load_event_steps(self.pipeline_id);
+        }
     }
 }
