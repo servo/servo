5d2bb99d49a4333bd4cbaf1807ee3cdaa8094249
Xidorn Quan <me@upsuper.org>
2017-01-17 11:32:09 +1100

    Never blockify display: none

--- components/style/properties/properties.mako.rs
+++ components/style/properties/properties.mako.rs
@@ -1973,7 +1973,7 @@ pub fn apply_declarations<'a, F, I>(viewport_size: Size2D<Au>,
             % endif
 
             // Values that are not changed by blockification.
-            T::block | T::flex | T::list_item | T::table => None,
+            T::none | T::block | T::flex | T::list_item | T::table => None,
             % if product == "gecko":
             T::contents | T::grid | T::_webkit_box => None,
             % endif
