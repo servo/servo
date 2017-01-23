35192b03c31eef1275fdb823f4fbc51db601697a
Hiroyuki Ikezoe <hikezoe@mozilla.com>
2017-01-23 15:11:27 +0900

    Assert if there is an important declaration while collecting animated properties in keyframes.
    Parser has already dropped it.

--- components/style/keyframes.rs
+++ components/style/keyframes.rs
@@ -254,7 +254,9 @@ fn get_animated_properties(keyframe: &Keyframe) -> Vec<TransitionProperty> {
     let mut ret = vec![];
     // NB: declarations are already deduplicated, so we don't have to check for
     // it here.
-    for &(ref declaration, _) in keyframe.block.read().declarations.iter() {
+    for &(ref declaration, importance) in keyframe.block.read().declarations.iter() {
+        assert!(!importance.important());
+
         if let Some(property) = TransitionProperty::from_declaration(declaration) {
             ret.push(property);
         }
