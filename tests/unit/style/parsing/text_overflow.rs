da993b884a1299a6e78aa03dd952f00ca026515b
Alberto Leal <mailforalberto@gmail.com>
2017-01-25 12:20:37 -0500

    Fix text-overflow serialization.
    Fixes https://github.com/servo/servo/issues/15208

---
+++ tests/unit/style/parsing/text_overflow.rs
@@ -0,0 +1,20 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+use cssparser::Parser;
+use media_queries::CSSErrorReporterTest;
+use parsing::parse;
+use style::parser::{Parse, ParserContext};
+use style::stylesheets::Origin;
+use style_traits::ToCss;
+
+#[test]
+fn test_text_overflow() {
+    use style::properties::longhands::text_overflow;
+
+    assert_roundtrip_with_context!(text_overflow::parse, r#""x" "y""#);
+
+    // TODO: more tests pending PR review
+
+}
