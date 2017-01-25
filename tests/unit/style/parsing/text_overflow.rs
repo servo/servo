28e9d6d4a05b3478673ab749f03a14ef41d8257d
Alberto Leal <mailforalberto@gmail.com>
2017-01-25 12:32:04 -0500

    remove unused imports

--- tests/unit/style/parsing/text_overflow.rs
+++ tests/unit/style/parsing/text_overflow.rs
@@ -4,8 +4,7 @@
 
 use cssparser::Parser;
 use media_queries::CSSErrorReporterTest;
-use parsing::parse;
-use style::parser::{Parse, ParserContext};
+use style::parser::{ParserContext};
 use style::stylesheets::Origin;
 use style_traits::ToCss;
 
