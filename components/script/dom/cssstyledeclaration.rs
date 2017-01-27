684545b396b0cb144cf00bee8e4b3924baabb8dc
Emilio Cobos Álvarez <emilio@crisal.io>
2017-01-27 19:40:15 +0100

    script: Also do so for element owners.

--- components/script/dom/cssstyledeclaration.rs
+++ components/script/dom/cssstyledeclaration.rs
@@ -127,7 +127,7 @@ impl CSSStyleOwner {
 
     fn base_url(&self) -> ServoUrl {
         match *self {
-            CSSStyleOwner::Element(ref el) => window_from_node(&**el).get_url(),
+            CSSStyleOwner::Element(ref el) => window_from_node(&**el).Document().base_url(),
             CSSStyleOwner::CSSRule(ref rule, _) => {
                 rule.parent_stylesheet().style_stylesheet().base_url.clone()
             }
