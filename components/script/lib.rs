1563e12bb325d3af4a535abf780ba7422963887b
Ms2ger <Ms2ger@gmail.com>
2017-02-06 10:33:51 +0100

    Remove a gate for the stabilized more_struct_aliases feature.

--- components/script/lib.rs
+++ components/script/lib.rs
@@ -7,7 +7,6 @@
 #![feature(const_fn)]
 #![feature(core_intrinsics)]
 #![feature(field_init_shorthand)]
-#![feature(more_struct_aliases)]
 #![feature(mpsc_select)]
 #![feature(nonzero)]
 #![feature(on_unimplemented)]
