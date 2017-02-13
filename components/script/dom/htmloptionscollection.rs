d4ad51bfde796caf2f674b653a2717caaf6ecac2
Corey Farwell <coreyf@rwell.org>
2017-02-12 19:27:08 -0500

    Implement `selectedIndex` IDL attribute on `HTMLOptionsCollection`.

--- components/script/dom/htmloptionscollection.rs
+++ components/script/dom/htmloptionscollection.rs
@@ -6,6 +6,7 @@ use dom::bindings::codegen::Bindings::ElementBinding::ElementMethods;
 use dom::bindings::codegen::Bindings::HTMLCollectionBinding::HTMLCollectionMethods;
 use dom::bindings::codegen::Bindings::HTMLOptionsCollectionBinding;
 use dom::bindings::codegen::Bindings::HTMLOptionsCollectionBinding::HTMLOptionsCollectionMethods;
+use dom::bindings::codegen::Bindings::HTMLSelectElementBinding::HTMLSelectElementMethods;
 use dom::bindings::codegen::Bindings::NodeBinding::NodeBinding::NodeMethods;
 use dom::bindings::codegen::UnionTypes::{HTMLOptionElementOrHTMLOptGroupElement, HTMLElementOrLong};
 use dom::bindings::error::{Error, ErrorResult};
@@ -16,6 +17,7 @@ use dom::bindings::str::DOMString;
 use dom::element::Element;
 use dom::htmlcollection::{CollectionFilter, HTMLCollection};
 use dom::htmloptionelement::HTMLOptionElement;
+use dom::htmlselectelement::HTMLSelectElement;
 use dom::node::{document_from_node, Node};
 use dom::window::Window;
 
@@ -181,5 +183,23 @@ impl HTMLOptionsCollectionMethods for HTMLOptionsCollection {
         if let Some(element) = self.upcast().IndexedGetter(index as u32) {
             element.Remove();
         }
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-htmloptionscollection-selectedindex
+    fn SelectedIndex(&self) -> i32 {
+        self.upcast()
+            .root_node()
+            .downcast::<HTMLSelectElement>()
+            .expect("HTMLOptionsCollection not rooted on a HTMLSelectElement")
+            .SelectedIndex()
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-htmloptionscollection-selectedindex
+    fn SetSelectedIndex(&self, index: i32) {
+        self.upcast()
+            .root_node()
+            .downcast::<HTMLSelectElement>()
+            .expect("HTMLOptionsCollection not rooted on a HTMLSelectElement")
+            .SetSelectedIndex(index)
     }
 }
