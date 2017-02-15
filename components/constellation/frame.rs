fa2602cb271d984232d323a37d5c6a2886895378
Connor Brewster <connor.brewster@eagles.oc.edu>
2017-02-15 09:47:55 -0600

    Link frame entries together that share pipelines Various frame-related cleanup

--- components/constellation/frame.rs
+++ components/constellation/frame.rs
@@ -5,9 +5,11 @@
 use msg::constellation_msg::{FrameId, PipelineId};
 use pipeline::Pipeline;
 use servo_url::ServoUrl;
+use std::cell::Cell;
 use std::collections::HashMap;
 use std::iter::once;
 use std::mem::replace;
+use std::rc::Rc;
 use std::time::Instant;
 
 /// A frame in the frame tree.
@@ -23,14 +25,8 @@ pub struct Frame {
     /// The frame id.
     pub id: FrameId,
 
-    /// The timestamp for the current session history entry
-    pub instant: Instant,
-
-    /// The pipeline for the current session history entry
-    pub pipeline_id: PipelineId,
-
-    /// The URL for the current session history entry
-    pub url: ServoUrl,
+    /// The current session history entry.
+    pub current: FrameState,
 
     /// The past session history, ordered chronologically.
     pub prev: Vec<FrameState>,
@@ -45,31 +41,23 @@ impl Frame {
     pub fn new(id: FrameId, pipeline_id: PipelineId, url: ServoUrl) -> Frame {
         Frame {
             id: id,
-            pipeline_id: pipeline_id,
-            instant: Instant::now(),
-            url: url,
+            current: FrameState::new(pipeline_id, url, id),
             prev: vec!(),
             next: vec!(),
         }
     }
 
-    /// Get the current frame state.
-    pub fn current(&self) -> FrameState {
-        FrameState {
-            instant: self.instant,
-            frame_id: self.id,
-            pipeline_id: Some(self.pipeline_id),
-            url: self.url.clone(),
-        }
+    /// The active pipeline id of the current session history entry
+    pub fn pipeline_id(&self) -> PipelineId {
+        self.current.pipeline_id().expect("Active pipeline cannot be None")
     }
 
     /// Set the current frame entry, and push the current frame entry into the past.
     pub fn load(&mut self, pipeline_id: PipelineId, url: ServoUrl) {
-        let current = self.current();
+        let current = self.current.clone();
         self.prev.push(current);
-        self.instant = Instant::now();
-        self.pipeline_id = pipeline_id;
-        self.url = url;
+        self.current.instant = Instant::now();
+        self.current.replace_pipeline(pipeline_id, url);
     }
 
     /// Set the future to be empty.
@@ -77,11 +65,40 @@ impl Frame {
         replace(&mut self.next, vec!())
     }
 
-    /// Update the current entry of the Frame from an entry that has been traversed to.
-    pub fn update_current(&mut self, pipeline_id: PipelineId, entry: &FrameState) {
-        self.pipeline_id = pipeline_id;
-        self.instant = entry.instant;
-        self.url = entry.url.clone();
+    pub fn discard_entry(&self, entry: &FrameState) -> Option<PipelineId> {
+        // Ensure that we are not discarding the active pipeline.
+        if entry.pipeline_id() != Some(self.pipeline_id()) {
+            return entry.discard_pipeline()
+        }
+        None
+    }
+
+    pub fn traverse_to_entry(&mut self, entry: FrameState) {
+        let mut curr_entry = self.current.clone();
+
+        if entry.instant > self.current.instant {
+            // We are traversing to the future.
+            while let Some(next) = self.next.pop() {
+                self.prev.push(curr_entry);
+                curr_entry = next;
+                if entry.instant <= curr_entry.instant { break; }
+            }
+        } else if entry.instant < self.current.instant {
+            // We are traversing to the past.
+            while let Some(prev) = self.prev.pop() {
+                self.next.push(curr_entry);
+                curr_entry = prev;
+                if entry.instant >= curr_entry.instant { break; }
+            }
+        }
+
+        debug_assert_eq!(entry.instant, curr_entry.instant);
+
+        self.current = entry;
+    }
+
+    pub fn entry_iter<'a>(&'a self) -> impl Iterator<Item=&'a FrameState> {
+        self.prev.iter().chain(self.next.iter()).chain(once(&self.current))
     }
 }
 
@@ -95,24 +112,58 @@ pub struct FrameState {
     /// The timestamp for when the session history entry was created
     pub instant: Instant,
 
-    /// The pipeline for the document in the session history,
-    /// None if the entry has been discarded
-    pub pipeline_id: Option<PipelineId>,
-
     /// The URL for this entry, used to reload the pipeline if it has been discarded
     pub url: ServoUrl,
 
     /// The frame that this session history entry is part of
     pub frame_id: FrameId,
+
+    /// The pipeline for the document in the session history
+    pipeline_id: Rc<Cell<Option<PipelineId>>>,
 }
 
 impl FrameState {
+    fn new(pipeline_id: PipelineId, url: ServoUrl, frame_id: FrameId) -> FrameState {
+        FrameState {
+            instant: Instant::now(),
+            pipeline_id: Rc::new(Cell::new(Some(pipeline_id))),
+            url: url,
+            frame_id: frame_id,
+        }
+    }
+
     /// Updates the entry's pipeline and url. This is used when navigating with replacement
     /// enabled.
     pub fn replace_pipeline(&mut self, pipeline_id: PipelineId, url: ServoUrl) {
-        self.pipeline_id = Some(pipeline_id);
+        self.pipeline_id = Rc::new(Cell::new(Some(pipeline_id)));
         self.url = url;
     }
+
+    fn discard_pipeline(&self) -> Option<PipelineId> {
+        let old_pipeline_id = self.pipeline_id.get();
+        self.pipeline_id.set(None);
+        old_pipeline_id
+    }
+
+    pub fn update_pipeline(&self, pipeline_id: PipelineId) {
+        self.pipeline_id.set(Some(pipeline_id));
+    }
+
+    pub fn pipeline_id(&self) -> Option<PipelineId> {
+        self.pipeline_id.get()
+    }
+}
+
+/// Whether the pipeline should be updated or replaced when a naviagtion with replacement
+/// matures.
+pub enum ReplaceOrUpdate {
+    /// Replaces the pipeline of the entry. This does not affect any other entries even
+    /// if they share a pipeline id.
+    Replace(FrameState),
+    /// Updates the pipeline of the entry. This will update the pipeline of all entries
+    /// that share the same pipeline. This is only used when reloading a document that
+    /// was discarded from the distant history.
+    Update(FrameState),
 }
 
 /// Represents a pending change in the frame tree, that will be applied
@@ -133,7 +184,7 @@ pub struct FrameChange {
 
     /// Is the new document replacing the current document (e.g. a reload)
     /// or pushing it into the session history (e.g. a navigation)?
-    pub replace: Option<FrameState>,
+    pub replace: Option<ReplaceOrUpdate>,
 }
 
 /// An iterator over a frame tree, returning the fully active frames in
@@ -168,10 +219,10 @@ impl<'a> Iterator for FrameTreeIterator<'a> {
                     continue;
                 },
             };
-            let pipeline = match self.pipelines.get(&frame.pipeline_id) {
+            let pipeline = match self.pipelines.get(&frame.pipeline_id()) {
                 Some(pipeline) => pipeline,
                 None => {
-                    warn!("Pipeline {:?} iterated after closure.", frame.pipeline_id);
+                    warn!("Pipeline {:?} iterated after closure.", frame.pipeline_id());
                     continue;
                 },
             };
@@ -213,9 +264,8 @@ impl<'a> Iterator for FullFrameTreeIterator<'a> {
                     continue;
                 },
             };
-            let child_frame_ids = frame.prev.iter().chain(frame.next.iter())
-                .filter_map(|entry| entry.pipeline_id)
-                .chain(once(frame.pipeline_id))
+            let child_frame_ids = frame.entry_iter()
+                .filter_map(|entry| entry.pipeline_id())
                 .filter_map(|pipeline_id| pipelines.get(&pipeline_id))
                 .flat_map(|pipeline| pipeline.children.iter());
             self.stack.extend(child_frame_ids);
