b0499e34224019bf895dac66cbdf611ecdeea2d6
Michael Howell <michael@notriddle.com>
2017-03-06 19:58:32 +0000

    Assert that `domain_match` is operating on lowercase text
    > (Note that both the domain string and the string will have been
    > canonicalized to lower case at this point.)
    
    Related to #15789

--- components/net/cookie.rs
+++ components/net/cookie.rs
@@ -139,6 +139,8 @@ impl Cookie {
 
     // http://tools.ietf.org/html/rfc6265#section-5.1.3
     pub fn domain_match(string: &str, domain_string: &str) -> bool {
+        debug_assert!(string.to_lowercase() == string);
+        debug_assert!(domain_string.to_lowercase() == domain_string);
         string == domain_string ||
         (string.ends_with(domain_string) &&
          string.as_bytes()[string.len()-domain_string.len()-1] == b'.' &&
