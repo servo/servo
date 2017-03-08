25f221674fbc219904c7afb3180078ec75b4e73e
Morris Tseng <mtseng@mozilla.com>
2017-03-08 13:56:33 +0800

    Add gradient border support.

--- components/gfx/display_list/mod.rs
+++ components/gfx/display_list/mod.rs
@@ -885,10 +885,7 @@ pub struct IframeDisplayItem {
 
 /// Paints a gradient.
 #[derive(Clone, Deserialize, HeapSizeOf, Serialize)]
-pub struct GradientDisplayItem {
-    /// Fields common to all display items.
-    pub base: BaseDisplayItem,
-
+pub struct Gradient {
     /// The start point of the gradient (computed during display list construction).
     pub start_point: Point2D<Au>,
 
@@ -897,6 +894,14 @@ pub struct GradientDisplayItem {
 
     /// A list of color stops.
     pub stops: Vec<GradientStop>,
+}
+
+#[derive(Clone, Deserialize, HeapSizeOf, Serialize)]
+pub struct GradientDisplayItem {
+    /// Fields common to all display items.
+    pub base: BaseDisplayItem,
+
+    pub gradient: Gradient,
 }
 
 /// A normal border, supporting CSS border styles.
@@ -938,11 +943,22 @@ pub struct ImageBorder {
     pub repeat_vertical: webrender_traits::RepeatMode,
 }
 
+/// A border that is made of linear gradient
+#[derive(Clone, HeapSizeOf, Deserialize, Serialize)]
+pub struct GradientBorder {
+    /// The gradient info that his border uses, border-image-source.
+    pub gradient: Gradient,
+
+    /// Outsets for the border, as per border-image-outset.
+    pub outset: SideOffsets2D<f32>,
+}
+
 /// Specifies the type of border
 #[derive(Clone, HeapSizeOf, Deserialize, Serialize)]
 pub enum BorderDetails {
     Normal(NormalBorder),
     Image(ImageBorder),
+    Gradient(GradientBorder),
 }
 
 /// Paints a border.
