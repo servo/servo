5b25cb33450e13acd6160af81cd462d3b7778538
SendilKumar N <sendilkumarn@live.com>
2017-03-09 01:03:00 +0800

    apply user scripts correctly
    replace to utf8_lossy

--- components/script/dom/userscripts.rs
+++ components/script/dom/userscripts.rs
@@ -2,25 +2,25 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-use dom::bindings::codegen::Bindings::DocumentBinding::DocumentMethods;
-use dom::bindings::codegen::Bindings::NodeBinding::NodeMethods;
 use dom::bindings::inheritance::Castable;
-use dom::bindings::js::RootedReference;
-use dom::bindings::str::DOMString;
+use dom::globalscope::GlobalScope;
 use dom::htmlheadelement::HTMLHeadElement;
 use dom::node::Node;
+use js::jsval::UndefinedValue;
 use servo_config::opts;
 use servo_config::resource_files::resources_dir_path;
-use std::borrow::ToOwned;
-use std::fs::read_dir;
+use std::fs::{File, read_dir};
+use std::io::Read;
 use std::path::PathBuf;
 
 
 pub fn load_script(head: &HTMLHeadElement) {
     if let Some(ref path_str) = opts::get().userscripts {
         let node = head.upcast::<Node>();
-        let first_child = node.GetFirstChild();
         let doc = node.owner_doc();
+        let win = doc.window();
+        let cx = win.get_cx();
+        rooted!(in(cx) let mut rval = UndefinedValue());
 
         let path = if &**path_str == "" {
             if let Ok(mut p) = resources_dir_path() {
@@ -40,13 +40,11 @@ pub fn load_script(head: &HTMLHeadElement) {
         files.sort();
 
         for file in files {
-            let name = match file.into_os_string().into_string() {
-                Ok(ref s) if s.ends_with(".js") => "file://".to_owned() + &s[..],
-                _ => continue
-            };
-            let new_script = doc.CreateElement(DOMString::from("script")).unwrap();
-            new_script.set_string_attribute(&local_name!("src"), DOMString::from(name));
-            node.InsertBefore(new_script.upcast(), first_child.r()).unwrap();
+            let mut f = File::open(&file).unwrap();
+            let mut contents = vec![];
+            f.read_to_end(&mut contents).unwrap();
+            let script_text = String::from_utf8_lossy(&contents);
+            win.upcast::<GlobalScope>().evaluate_js_on_global_with_result(&script_text, rval.handle_mut());
         }
     }
 }
