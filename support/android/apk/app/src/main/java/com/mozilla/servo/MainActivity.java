5346c61f2b81236dd533a05ce90a38a2a6936472
Imanol Fernandez <mortimergoro@gmail.com>
2017-03-09 13:05:28 +0100

    Android API 18 support

--- support/android/apk/app/src/main/java/com/mozilla/servo/MainActivity.java
+++ support/android/apk/app/src/main/java/com/mozilla/servo/MainActivity.java
@@ -127,13 +127,22 @@ public class MainActivity extends android.app.NativeActivity {
 
     // Dim toolbar and make the view fullscreen
     private void setFullScreen() {
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // Hides navigation bar
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN; // Hides status bar
+        if( android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+            flags |= getImmersiveFlag();
+        } else {
+            flags |= View.SYSTEM_UI_FLAG_LOW_PROFILE;
+        }
+        getWindow().getDecorView().setSystemUiVisibility(flags);
+    }
+
+    @TargetApi(19)
+    private int getImmersiveFlag() {
+        return View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
     }
 
     private void addFullScreenListener() {
