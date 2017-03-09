c1e8beacdccefa75d10dc97d2884a6ad8d2d2134
Ms2ger <Ms2ger@gmail.com>
2017-03-09 10:29:34 +0100

    Remove unused content-blocker dependency from unit tests.

--- tests/unit/net/http_loader.rs
+++ tests/unit/net/http_loader.rs
@@ -2,7 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-use content_blocker::parse_list;
 use cookie_rs::Cookie as CookiePair;
 use devtools_traits::{ChromeToDevtoolsControlMsg, DevtoolsControlMsg, NetworkEvent};
 use devtools_traits::HttpRequest as DevtoolsHttpRequest;
