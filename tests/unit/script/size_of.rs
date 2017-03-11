31678b426c9675cb66f011358dd42c59d46cd4a3
Simon Sapin <simon.sapin@exyr.org>
2017-03-11 11:45:19 +0100

    Update to rustc 1.17.0-nightly (8c72b7651 2017-03-11)
    This fixes the DOM node size regression introduced by a previous Rust update:
    https://github.com/servo/servo/issues/15704

--- tests/unit/script/size_of.rs
+++ tests/unit/script/size_of.rs
@@ -29,14 +29,14 @@ macro_rules! sizeof_checker (
 );
 
 // Update the sizes here
-sizeof_checker!(size_event_target, EventTarget, 48);
-sizeof_checker!(size_node, Node, 160);
-sizeof_checker!(size_element, Element, 320);
-sizeof_checker!(size_htmlelement, HTMLElement, 336);
-sizeof_checker!(size_div, HTMLDivElement, 336);
-sizeof_checker!(size_span, HTMLSpanElement, 336);
-sizeof_checker!(size_text, Text, 192);
-sizeof_checker!(size_characterdata, CharacterData, 192);
+sizeof_checker!(size_event_target, EventTarget, 40);
+sizeof_checker!(size_node, Node, 152);
+sizeof_checker!(size_element, Element, 312);
+sizeof_checker!(size_htmlelement, HTMLElement, 328);
+sizeof_checker!(size_div, HTMLDivElement, 328);
+sizeof_checker!(size_span, HTMLSpanElement, 328);
+sizeof_checker!(size_text, Text, 184);
+sizeof_checker!(size_characterdata, CharacterData, 184);
 sizeof_checker!(size_servothreadsafelayoutnode, ServoThreadSafeLayoutNode, 16);
 
 // We use these types in the parallel traversal. They should stay pointer-sized.
