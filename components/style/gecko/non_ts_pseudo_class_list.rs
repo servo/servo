b37f991cb86aa969e87e16a8d3639ab930a43cb2
Manish Goregaokar <manishearth@gmail.com>
2017-03-15 16:44:07 -0700

    stylo: Add parsing support for functional non-ts pseudoclasses, add stub -moz-system-metric pseudo
    MozReview-Commit-ID: KADDH6adZqR

--- components/style/gecko/non_ts_pseudo_class_list.rs
+++ components/style/gecko/non_ts_pseudo_class_list.rs
@@ -11,17 +11,17 @@
  *
  * Expected usage is as follows:
  * ```
- * fn use_pseudo_class() {
- *     macro_rules! use_pseudo_class_list {
- *         ( $(
- *             ($css:expr, $name:ident, $gecko_type:tt, $state:tt, $flags:tt),
- *         )* ) => {
- *             // Do stuff.
- *         }
+ * macro_rules! pseudo_class_macro{
+ *     (bare: [$(($css:expr, $name:ident, $gecko_type:tt, $state:tt, $flags:tt),)*],
+ *      string: [$(($s_css:expr, $s_name:ident, $s_gecko_type:tt, $s_state:tt, $s_flags:tt),)*]) => {
+ *         // do stuff
  *     }
- *     apply_non_ts_list!(use_pseudo_class_list)
  * }
+ * apply_non_ts_list!(pseudo_class_macro)
  * ```
+ *
+ * The string variables will be applied to pseudoclasses that are of the form
+ * of a function with a string argument.
  *
  * $gecko_type can be either "_" or an ident in Gecko's CSSPseudoClassType.
  * $state can be either "_" or an expression of type ElementState.
@@ -32,22 +32,27 @@
 macro_rules! apply_non_ts_list {
     ($apply_macro:ident) => {
         $apply_macro! {
-            ("any-link", AnyLink, anyLink, _, _),
-            ("link", Link, link, _, _),
-            ("visited", Visited, visited, _, _),
-            ("active", Active, active, IN_ACTIVE_STATE, _),
-            ("focus", Focus, focus, IN_FOCUS_STATE, _),
-            ("fullscreen", Fullscreen, fullscreen, IN_FULLSCREEN_STATE, _),
-            ("hover", Hover, hover, IN_HOVER_STATE, _),
-            ("enabled", Enabled, enabled, IN_ENABLED_STATE, _),
-            ("disabled", Disabled, disabled, IN_DISABLED_STATE, _),
-            ("checked", Checked, checked, IN_CHECKED_STATE, _),
-            ("indeterminate", Indeterminate, indeterminate, IN_INDETERMINATE_STATE, _),
-            ("read-write", ReadWrite, _, IN_READ_WRITE_STATE, _),
-            ("read-only", ReadOnly, _, IN_READ_WRITE_STATE, _),
+            bare: [
+                ("any-link", AnyLink, anyLink, _, _),
+                ("link", Link, link, _, _),
+                ("visited", Visited, visited, _, _),
+                ("active", Active, active, IN_ACTIVE_STATE, _),
+                ("focus", Focus, focus, IN_FOCUS_STATE, _),
+                ("fullscreen", Fullscreen, fullscreen, IN_FULLSCREEN_STATE, _),
+                ("hover", Hover, hover, IN_HOVER_STATE, _),
+                ("enabled", Enabled, enabled, IN_ENABLED_STATE, _),
+                ("disabled", Disabled, disabled, IN_DISABLED_STATE, _),
+                ("checked", Checked, checked, IN_CHECKED_STATE, _),
+                ("indeterminate", Indeterminate, indeterminate, IN_INDETERMINATE_STATE, _),
+                ("read-write", ReadWrite, _, IN_READ_WRITE_STATE, _),
+                ("read-only", ReadOnly, _, IN_READ_WRITE_STATE, _),
 
-            ("-moz-browser-frame", MozBrowserFrame, mozBrowserFrame, _, PSEUDO_CLASS_INTERNAL),
-            ("-moz-table-border-nonzero", MozTableBorderNonzero, mozTableBorderNonzero, _, PSEUDO_CLASS_INTERNAL),
+                ("-moz-browser-frame", MozBrowserFrame, mozBrowserFrame, _, PSEUDO_CLASS_INTERNAL),
+                ("-moz-table-border-nonzero", MozTableBorderNonzero, mozTableBorderNonzero, _, PSEUDO_CLASS_INTERNAL),
+            ],
+            string: [
+                ("-moz-system-metric", MozSystemMetric, mozSystemMetric, _, PSEUDO_CLASS_INTERNAL),
+            ]
         }
     }
 }
