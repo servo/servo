4aded872fc72ad5915a0bca9f28adc2a736a1c94
Matt Brubeck <mbrubeck@limpet.net>
2017-03-22 08:39:12 -0700

    Fix compiler warnings in stylo unit tests

--- tests/unit/stylo/lib.rs
+++ tests/unit/stylo/lib.rs
@@ -2,17 +2,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-extern crate app_units;
 extern crate atomic_refcell;
 extern crate cssparser;
 extern crate env_logger;
-extern crate euclid;
 extern crate geckoservo;
 #[macro_use] extern crate lazy_static;
 #[macro_use] extern crate log;
-extern crate num_cpus;
 extern crate parking_lot;
-extern crate rayon;
 extern crate selectors;
 extern crate servo_url;
 #[macro_use] extern crate style;
