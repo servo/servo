dbc9dd95a268aa88389f0542d3dae45857155367
Manish Goregaokar <manishearth@gmail.com>
2017-03-31 02:33:21 -0700

    stylo: Handle text-emphasis-position pres attrs

--- ports/geckolib/glue.rs
+++ ports/geckolib/glue.rs
@@ -1148,6 +1148,7 @@ pub extern "C" fn Servo_DeclarationBlock_SetKeywordValue(declarations:
         Float => longhands::float::SpecifiedValue::from_gecko_keyword(value),
         VerticalAlign => longhands::vertical_align::SpecifiedValue::from_gecko_keyword(value),
         TextAlign => longhands::text_align::SpecifiedValue::from_gecko_keyword(value),
+        TextEmphasisPosition => longhands::text_emphasis_position::SpecifiedValue::from_gecko_keyword(value),
         Clear => longhands::clear::SpecifiedValue::from_gecko_keyword(value),
         FontSize => {
             // We rely on Gecko passing in font-size values (0...7) here.
