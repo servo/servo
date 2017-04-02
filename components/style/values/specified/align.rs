4fa9090a0e982fa51c98dd8b9091d5d236d3d985
Hiroyuki Ikezoe <hikezoe@mozilla.com>
2017-04-03 06:41:28 +0900

    Fix justify-content values.

--- components/style/values/specified/align.rs
+++ components/style/values/specified/align.rs
@@ -358,9 +358,9 @@ fn parse_content_distribution(input: &mut Parser) -> Result<AlignFlags, ()> {
     let ident = input.expect_ident()?;
     match_ignore_ascii_case! { &ident,
       "stretch" => Ok(ALIGN_STRETCH),
-      "space_between" => Ok(ALIGN_SPACE_BETWEEN),
-      "space_around" => Ok(ALIGN_SPACE_AROUND),
-      "space_evenly" => Ok(ALIGN_SPACE_EVENLY),
+      "space-between" => Ok(ALIGN_SPACE_BETWEEN),
+      "space-around" => Ok(ALIGN_SPACE_AROUND),
+      "space-evenly" => Ok(ALIGN_SPACE_EVENLY),
       _ => Err(())
     }
 }
