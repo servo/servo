2cce8c506d834c266f642dbb8717ea7808fd8203
Sumit <srivassumit@gmail.com>
2017-04-04 20:42:45 -0400

    modifications for review comments

--- components/script/dom/mutationobserver.rs
+++ components/script/dom/mutationobserver.rs
@@ -1,8 +1,12 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 use core::ptr::null;
 use dom;
 use dom::bindings::codegen::Bindings::MutationObserverBinding::MutationCallback;
 use dom::bindings::codegen::Bindings::MutationObserverBinding::MutationObserverInit;
-use dom::bindings::error::Error;
+use dom::bindings::error::Fallible;
 use dom::bindings::js::Root;
 use dom::bindings::reflector::Reflector;
 use dom::bindings::trace::JSTraceable;
@@ -21,16 +25,16 @@ pub struct MutationObserver {
 }
 
 impl MutationObserver {
-    fn new(global: &Window, callback: MutationCallback) -> MutationObserver {
+    fn new(global: &Window, callback: Rc<MutationCallback>) -> MutationObserver {
         MutationObserver {
             reflector_: Reflector::new(),
             callback: callback,
         }
     }
 
-    pub fn Constructor(global: &Window, callback: Rc<MutationCallback>) -> Result<Root<MutationObserver>, Error> {
-        let observer = MutationObserver::new(global, Rc::deref(&callback));
-        ScriptThread::add_mutation_observer(&observer)
+    pub fn Constructor(global: &Window, callback: Rc<MutationCallback>) -> Fallible<Root<MutationObserver>> {
+        let observer = MutationObserver::new(global, callback);
+        ScriptThread::add_mutation_observer(Root::from_ref(&*observer))
     }
 
 }
--- components/script/dom/mutationrecord.rs
+++ components/script/dom/mutationrecord.rs
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 use core::ptr;
 use dom::bindings::codegen::Bindings::MutationRecordBinding::MutationRecordBinding::MutationRecordMethods;
 use dom::bindings::js::{JS, Root};
@@ -21,11 +25,11 @@ pub struct MutationRecord {
 }
 
 impl MutationRecord {
-    fn new(window: &Window, record_type: DOMString, target: JS<Node>) -> MutationRecord {
+    fn new(window: &Window, record_type: DOMString, target: &Node) -> MutationRecord {
         MutationRecord {
             reflector_: Reflector::new(),
             record_type: record_type,
-            target: target,
+            target: JS::from_ref(target),
         }
     }
 }
