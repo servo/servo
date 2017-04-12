097f73a51cda6263de276d801c84ab6d07979fa6
Emilio Cobos Álvarez <emilio@crisal.io>
2017-04-12 14:34:43 +0800

    style: Simplify dependency visitor, avoid tracking dependencies of nested complex selectors separately.

--- components/selectors/visitor.rs
+++ components/selectors/visitor.rs
@@ -34,7 +34,7 @@ pub trait SelectorVisitor {
     /// Gets the combinator to the right of the selector, or `None` if the
     /// selector is the leftmost one.
     fn visit_complex_selector(&mut self,
-                              _: &Arc<ComplexSelector<Self::Impl>>,
+                              _: &ComplexSelector<Self::Impl>,
                               _combinator_to_right: Option<Combinator>)
                               -> bool {
         true
