6f062e585794d077bc391ccbcaad0d86a95879b6
Ravi Shankar <wafflespeanut@gmail.com>
2017-04-14 15:39:13 +0530

    Add a generic PositionValue struct

--- components/style/values/generics/mod.rs
+++ components/style/values/generics/mod.rs
@@ -14,6 +14,7 @@ use super::computed::{Context, ToComputedValue};
 pub use self::basic_shape::serialize_radius_values;
 
 pub mod basic_shape;
+pub mod position;
 
 #[derive(Clone, PartialEq, Debug)]
 #[cfg_attr(feature = "servo", derive(HeapSizeOf))]
