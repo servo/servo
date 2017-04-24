715efbdc93887fe1aa03a6414eac26219069c69c
Sadman Kazi <sadman@sadmansk.com>
2017-04-23 20:17:36 -0400

    Pass URL to Browser::new(), delegate url checking logic to third party

--- ports/servo/main.rs
+++ ports/servo/main.rs
@@ -37,6 +37,8 @@ use servo::compositing::windowing::WindowEvent;
 use servo::config;
 use servo::config::opts::{self, ArgumentParsingResult};
 use servo::config::servo_version;
+use servo::servo_config::prefs::PREFS;
+use servo::servo_url::ServoUrl;
 use std::env;
 use std::panic;
 use std::process;
@@ -142,7 +144,10 @@ fn main() {
     // Our wrapper around `Browser` that also implements some
     // callbacks required by the glutin window implementation.
     let mut browser = BrowserWrapper {
-        browser: Browser::new(window.clone()),
+        browser: Browser::new(window.clone(),
+                opts::get().url.clone()
+                    .unwrap_or(ServoUrl::parse(PREFS.get("shell.homepage").as_string()
+                    .unwrap_or("about:blank")).unwrap()))
     };
 
     browser.browser.setup_logging();
