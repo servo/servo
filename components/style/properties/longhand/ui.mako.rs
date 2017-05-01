8c5e88507e8569e8cfdfc1bed6a695237e5c970c
Anthony Ramine <n.oxyde@gmail.com>
2017-05-01 13:18:29 +0200

    Implement -moz-force-broken-image-icon in geckolib

--- components/style/properties/longhand/ui.mako.rs
+++ components/style/properties/longhand/ui.mako.rs
@@ -30,3 +30,61 @@ ${helpers.single_keyword("-moz-window-dragging", "default drag no-drag", product
                          gecko_enum_prefix="StyleWindowDragging",
                          animation_value_type="none",
                          spec="None (Nonstandard Firefox-only property)")}
+
+<%helpers:longhand name="-moz-force-broken-image-icon"
+                   products="gecko"
+                   animation_value_type="none"
+                   spec="None (Nonstandard Firefox-only property)">
+    use cssparser::Token;
+    use std::fmt;
+    use style_traits::ToCss;
+    use values::HasViewportPercentage;
+
+    no_viewport_percentage!(SpecifiedValue);
+
+    pub mod computed_value {
+        #[derive(Debug, Clone, Copy, PartialEq)]
+        #[cfg_attr(feature = "servo", derive(HeapSizeOf))]
+        pub struct T(pub bool);
+    }
+
+    pub use self::computed_value::T as SpecifiedValue;
+
+    impl ToCss for computed_value::T {
+        fn to_css<W>(&self, dest: &mut W) -> fmt::Result where W: fmt::Write {
+            write!(dest, "{}", self.0 as u8)
+        }
+    }
+
+    #[inline]
+    pub fn get_initial_value() -> computed_value::T {
+        computed_value::T(false)
+    }
+
+    #[inline]
+    pub fn get_initial_specified_value() -> SpecifiedValue {
+        computed_value::T(false)
+    }
+
+    impl ToComputedValue for SpecifiedValue {
+        type ComputedValue = computed_value::T;
+
+        #[inline]
+        fn to_computed_value(&self, _context: &Context) -> computed_value::T {
+            *self
+        }
+
+        #[inline]
+        fn from_computed_value(computed: &computed_value::T) -> Self {
+            *computed
+        }
+    }
+
+    pub fn parse(_context: &ParserContext, input: &mut Parser) -> Result<SpecifiedValue, ()> {
+        match try!(input.expect_integer()) {
+            0 => Ok(computed_value::T(false)),
+            1 => Ok(computed_value::T(true)),
+            _ => Err(()),
+        }
+    }
+</%helpers:longhand>
