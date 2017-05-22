66aa38e1a161d7ced8f077f2321c6ff259b7e04b
Anthony Ramine <n.oxyde@gmail.com>
2017-05-22 13:40:29 +0200

    Fix serialisation of computed circles

--- components/style/values/computed/basic_shape.rs
+++ components/style/values/computed/basic_shape.rs
@@ -41,6 +41,7 @@ pub type ShapeRadius = GenericShapeRadius<LengthOrPercentage>;
 
 impl ToCss for Circle {
     fn to_css<W>(&self, dest: &mut W) -> fmt::Result where W: fmt::Write {
+        dest.write_str("circle(")?;
         self.radius.to_css(dest)?;
         dest.write_str(" at ")?;
         self.position.to_css(dest)
