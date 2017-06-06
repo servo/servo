a9cb635722bd94962f0f7a57d162545e531a1482
Anthony Ramine <n.oxyde@gmail.com>
2017-06-06 14:28:24 +0200

    Use generics for scroll-snap-points-*

---
+++ components/style/values/generics/box.rs
@@ -0,0 +1,9 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+//! Generic types for box properties.
+
+#[cfg_attr(feature = "servo", derive(HeapSizeOf))]
+#[derive(Clone, Copy, Debug, HasViewportPercentage, PartialEq, ToComputedValue, ToCss)]
+pub enum VerticalAlign<LengthOrPercentage>
