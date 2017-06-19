c617649e679e19a7d65416b693c5444c0e88481e
Brad Werth <bwerth@mozilla.com>
2017-06-19 10:23:00 -0700

    Add an update_stylesheet method to StylesheetSet.
    MozReview-Commit-ID: KlRkApYL8wk

--- ports/geckolib/glue.rs
+++ ports/geckolib/glue.rs
@@ -910,6 +910,17 @@ pub extern "C" fn Servo_StyleSet_InsertStyleSheetBefore(raw_data: RawServoStyleS
 }
 
 #[no_mangle]
+pub extern "C" fn Servo_StyleSet_UpdateStyleSheet(raw_data: RawServoStyleSetBorrowed,
+                                                  raw_sheet: RawServoStyleSheetBorrowed,
+                                                  unique_id: u64) {
+    let mut data = PerDocumentStyleData::from_ffi(raw_data).borrow_mut();
+    let sheet = HasArcFFI::as_arc(&raw_sheet);
+    data.stylesheets.update_stylesheet(
+        sheet,
+        unique_id);
+}
+
+#[no_mangle]
 pub extern "C" fn Servo_StyleSet_RemoveStyleSheet(raw_data: RawServoStyleSetBorrowed,
                                                   unique_id: u64) {
     let mut data = PerDocumentStyleData::from_ffi(raw_data).borrow_mut();
