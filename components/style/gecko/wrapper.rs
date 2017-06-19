10917ce73fe223500f646abd64df1eeea6953824
Wes Kocher <kwierso@gmail.com>
2017-06-19 15:44:00 -0700

    Revert "Add pseudo-classes to match on the state of the "dir" attribute." for preventing me from backing out a gecko-side patch
    This reverts commit e4660c2b5b62779e25432e5acf9d091ce6f04ddc.

--- components/style/gecko/wrapper.rs
+++ components/style/gecko/wrapper.rs
@@ -1517,10 +1517,6 @@ impl<'le> ::selectors::Element for GeckoElement<'le> {
             NonTSPseudoClass::MozMeterOptimum |
             NonTSPseudoClass::MozMeterSubOptimum |
             NonTSPseudoClass::MozMeterSubSubOptimum |
-            NonTSPseudoClass::MozHasDirAttr |
-            NonTSPseudoClass::MozDirAttrLTR |
-            NonTSPseudoClass::MozDirAttrRTL |
-            NonTSPseudoClass::MozDirAttrLikeAuto |
             NonTSPseudoClass::MozAutofill |
             NonTSPseudoClass::MozAutofillPreview => {
                 self.get_state().intersects(pseudo_class.state_flag())
