6b69903725ff89e25462f56c3f4020f834d3222c
Daisuke Akatsuka <dakatsuka@mozilla.com>
2017-06-28 03:33:05 +0900

    implements nsStyleImageRequest type properties animatable

--- components/style/properties/longhand/list.mako.rs
+++ components/style/properties/longhand/list.mako.rs
@@ -100,7 +100,7 @@ ${helpers.single_keyword("list-style-position", "outside inside", animation_valu
     </%helpers:longhand>
 % endif
 
-<%helpers:longhand name="list-style-image" animation_value_type="none"
+<%helpers:longhand name="list-style-image" animation_value_type="discrete"
                    boxed="${product == 'gecko'}"
                    spec="https://drafts.csswg.org/css-lists/#propdef-list-style-image">
     use values::computed::ComputedValueAsSpecified;
