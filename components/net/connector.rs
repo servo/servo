31133c0fd6eb4d6882e708880d829c7eb6979566
SimranGujral <gujralsimran19@gmail.com>
2017-07-20 10:39:18 -0700

    Servo using Hyper-Rustls for verification
    Removed OpenSSL imports
    
    Using Hyper-Sync-Rustls instead of Rust-OpenSSL
    
    Servo integrated with HyperSyncRustls- v 0.6.1
    
    Making Formatting changes after review

--- components/net/connector.rs
+++ components/net/connector.rs
@@ -6,19 +6,19 @@ use hosts::replace_host;
 use hyper::client::Pool;
 use hyper::error::{Result as HyperResult, Error as HyperError};
 use hyper::net::{NetworkConnector, HttpsStream, HttpStream, SslClient};
-use hyper_openssl::OpensslClient;
-use openssl::ssl::{SSL_OP_NO_COMPRESSION, SSL_OP_NO_SSLV2, SSL_OP_NO_SSLV3};
-use openssl::ssl::{SslConnectorBuilder, SslMethod};
-use std::io;
+use hyper_sync_rustls::TlsClient;
+use rustls;
+use std::{io, fs};
 use std::net::TcpStream;
 use std::path::PathBuf;
+use std::sync::Arc;
 
 pub struct HttpsConnector {
-    ssl: OpensslClient,
+    ssl: TlsClient,
 }
 
 impl HttpsConnector {
-    fn new(ssl: OpensslClient) -> HttpsConnector {
+    fn new(ssl: TlsClient) -> HttpsConnector {
         HttpsConnector {
             ssl: ssl,
         }
@@ -26,7 +26,7 @@ impl HttpsConnector {
 }
 
 impl NetworkConnector for HttpsConnector {
-    type Stream = HttpsStream<<OpensslClient as SslClient>::Stream>;
+    type Stream = HttpsStream<<TlsClient as SslClient>::Stream>;
 
     fn connect(&self, host: &str, port: u16, scheme: &str) -> HyperResult<Self::Stream> {
         if scheme != "http" && scheme != "https" {
@@ -50,35 +50,17 @@ impl NetworkConnector for HttpsConnector {
 
 pub type Connector = HttpsConnector;
 
-pub fn create_ssl_client(ca_file: &PathBuf) -> OpensslClient {
-    let mut ssl_connector_builder = SslConnectorBuilder::new(SslMethod::tls()).unwrap();
-    {
-        let context = ssl_connector_builder.builder_mut();
-        context.set_ca_file(ca_file).expect("could not set CA file");
-        context.set_cipher_list(DEFAULT_CIPHERS).expect("could not set ciphers");
-        context.set_options(SSL_OP_NO_SSLV2 | SSL_OP_NO_SSLV3 | SSL_OP_NO_COMPRESSION);
-    }
-    let ssl_connector = ssl_connector_builder.build();
-    OpensslClient::from(ssl_connector)
+pub fn create_ssl_client(ca_file: &PathBuf) -> TlsClient {
+    let mut ca = {
+        let f = fs::File::open(ca_file).expect("cannot open CA file");
+        io::BufReader::new(f)
+    };
+    let mut tls = rustls::ClientConfig::new();
+    tls.root_store.add_pem_file(&mut ca).unwrap();
+    TlsClient { cfg: Arc::new(tls) }
 }
 
-pub fn create_http_connector(ssl_client: OpensslClient) -> Pool<Connector> {
+pub fn create_http_connector(ssl_client: TlsClient ) -> Pool<Connector> {
     let https_connector = HttpsConnector::new(ssl_client);
     Pool::with_connector(Default::default(), https_connector)
 }
-
-// The basic logic here is to prefer ciphers with ECDSA certificates, Forward
-// Secrecy, AES GCM ciphers, AES ciphers, and finally 3DES ciphers.
-// A complete discussion of the issues involved in TLS configuration can be found here:
-// https://wiki.mozilla.org/Security/Server_Side_TLS
-const DEFAULT_CIPHERS: &'static str = concat!(
-    "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:",
-    "ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:",
-    "DHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-SHA256:",
-    "ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:",
-    "ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA:",
-    "ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:",
-    "DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:ECDHE-RSA-DES-CBC3-SHA:",
-    "ECDHE-ECDSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:",
-    "AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA"
-);
