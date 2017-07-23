533ea86bab8f2383389f38e0731a0ffcc40c8bbc
Emilio Cobos Álvarez <emilio@crisal.io>
2017-07-24 00:04:45 +0200

    style: Mention when visited styles may be disabled.

--- components/style/context.rs
+++ components/style/context.rs
@@ -118,6 +118,9 @@ pub struct SharedStyleContext<'a> {
     pub stylist: &'a Stylist,
 
     /// Whether visited styles are enabled.
+    ///
+    /// They may be disabled when Gecko's pref layout.css.visited_links_enabled
+    /// is false, or when in private browsing mode.
     pub visited_styles_enabled: bool,
 
     /// Configuration options.
