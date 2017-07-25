aadae108778f9f50f6a29bfdc6b2c696f8fe9b4e
Manish Goregaokar <manishearth@gmail.com>
2017-07-25 13:38:42 -0700

    stylo: Use saturating ops for clip computation
    MozReview-Commit-ID: DYQRM4CJbAg

--- components/style/properties/gecko.mako.rs
+++ components/style/properties/gecko.mako.rs
@@ -3723,14 +3723,14 @@ fn static_assert() {
                 }
 
                 if let Some(bottom) = rect.bottom {
-                    self.gecko.mClip.height = bottom.0 - self.gecko.mClip.y;
+                    self.gecko.mClip.height = bottom.0.saturating_sub(self.gecko.mClip.y);
                 } else {
                     self.gecko.mClip.height = 1 << 30; // NS_MAXSIZE
                     self.gecko.mClipFlags |= NS_STYLE_CLIP_BOTTOM_AUTO as u8;
                 }
 
                 if let Some(right) = rect.right {
-                    self.gecko.mClip.width = right.0 - self.gecko.mClip.x;
+                    self.gecko.mClip.width = right.0.saturating_sub(self.gecko.mClip.x);
                 } else {
                     self.gecko.mClip.width = 1 << 30; // NS_MAXSIZE
                     self.gecko.mClipFlags |= NS_STYLE_CLIP_RIGHT_AUTO as u8;
