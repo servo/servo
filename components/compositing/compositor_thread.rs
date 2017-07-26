8c588e8c36fc79c9d8b2ae6957026403692b8f0f
Dzmitry Malyshau <kvark@mozilla.com>
2017-07-26 12:28:38 -0400

    WR multi-document update

--- components/compositing/compositor_thread.rs
+++ components/compositing/compositor_thread.rs
@@ -194,5 +194,6 @@ pub struct InitialCompositorState {
     pub mem_profiler_chan: mem::ProfilerChan,
     /// Instance of webrender API
     pub webrender: webrender::Renderer,
-    pub webrender_api_sender: webrender_api::RenderApiSender,
+    pub webrender_document: webrender_api::DocumentId,
+    pub webrender_api: webrender_api::RenderApi,
 }
