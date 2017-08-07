720b17a54b6abea9a407f80870fba6b9b859c8a4
Emilio Cobos Álvarez <emilio@crisal.io>
2017-08-07 13:36:34 +0200

    style: Avoid hashing the precomputed hash atoms already have.
    This shaves a fair amount of time of stylist rebuild.
    
    Most of the extra remaining overhead are memory allocations...
    
    I'll try to get those sorted out next, though fixing that may overlap quite a
    bit with the work Cameron is doing in bug 1382925.
    
    MozReview-Commit-ID: 3j2SBY4QnKf
    Signed-off-by: Emilio Cobos Álvarez <emilio@crisal.io>

--- components/style/lib.rs
+++ components/style/lib.rs
@@ -71,7 +71,7 @@ extern crate ordered_float;
 extern crate owning_ref;
 extern crate parking_lot;
 extern crate pdqsort;
-#[cfg(feature = "gecko")] extern crate precomputed_hash;
+extern crate precomputed_hash;
 extern crate rayon;
 extern crate selectors;
 #[cfg(feature = "servo")] #[macro_use] extern crate serde;
