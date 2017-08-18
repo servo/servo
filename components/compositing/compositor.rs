73fe2f0e96b6385ddd150d1678d26d8360c6477c
Glenn Watson <github@intuitionlibrary.com>
2017-08-18 13:06:33 +1000

    Update WR (remove internal webgl implementation).

--- components/compositing/compositor.rs
+++ components/compositing/compositor.rs
@@ -327,17 +327,6 @@ impl webrender_api::RenderNotifier for RenderNotifier {
     }
 }
 
-// Used to dispatch functions from webrender to the main thread's event loop.
-struct CompositorThreadDispatcher {
-    compositor_proxy: CompositorProxy
-}
-
-impl webrender_api::RenderDispatcher for CompositorThreadDispatcher {
-    fn dispatch(&self, f: Box<Fn() + Send>) {
-        self.compositor_proxy.send(Msg::Dispatch(f));
-    }
-}
-
 impl<Window: WindowMethods> IOCompositor<Window> {
     fn new(window: Rc<Window>, state: InitialCompositorState)
            -> IOCompositor<Window> {
@@ -393,15 +382,6 @@ impl<Window: WindowMethods> IOCompositor<Window> {
         let render_notifier = RenderNotifier::new(compositor_proxy_for_webrender,
                                                   compositor.constellation_chan.clone());
         compositor.webrender.set_render_notifier(Box::new(render_notifier));
-
-        if cfg!(target_os = "windows") {
-            // Used to dispatch functions from webrender to the main thread's event loop.
-            // Required to allow WGL GLContext sharing in Windows.
-            let dispatcher = Box::new(CompositorThreadDispatcher {
-                compositor_proxy: compositor.channel_to_self.clone_compositor_proxy()
-            });
-            compositor.webrender.set_main_thread_dispatcher(dispatcher);
-        }
 
         // Set the size of the root layer.
         compositor.update_zoom_transform();
