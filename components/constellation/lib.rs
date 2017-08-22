2927f2873058bce5afb965cef0967a6eda54c744
Jonathan Jayet <jonathan@artus.io>
2017-08-22 16:30:32 +0200

    First step toward iOS Support

--- components/constellation/lib.rs
+++ components/constellation/lib.rs
@@ -16,7 +16,7 @@ extern crate compositing;
 extern crate debugger;
 extern crate devtools_traits;
 extern crate euclid;
-#[cfg(not(target_os = "windows"))]
+#[cfg(all(not(target_os = "windows"), not(target_os = "ios")))]
 extern crate gaol;
 extern crate gfx;
 extern crate gfx_traits;
@@ -46,11 +46,11 @@ mod constellation;
 mod event_loop;
 mod network_listener;
 mod pipeline;
-#[cfg(not(target_os = "windows"))]
+#[cfg(all(not(target_os = "windows"), not(target_os = "ios")))]
 mod sandboxing;
 mod timer_scheduler;
 
 pub use constellation::{Constellation, FromCompositorLogger, FromScriptLogger, InitialConstellationState};
 pub use pipeline::UnprivilegedPipelineContent;
-#[cfg(not(target_os = "windows"))]
+#[cfg(all(not(target_os = "windows"), not(target_os = "ios")))]
 pub use sandboxing::content_process_sandbox_profile;
