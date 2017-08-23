a27b6fd941c886ee6f38e08248311e1287c74914
Remi THEBAULT <remi.thebault@gmail.com>
2017-08-23 22:28:58 +0200

    remove unneeded accessor

--- components/script/dom/performanceentry.rs
+++ components/script/dom/performanceentry.rs
@@ -55,10 +55,6 @@ impl PerformanceEntry {
     pub fn start_time(&self) -> f64 {
         self.start_time
     }
-
-    pub fn duration(&self) -> f64 {
-        self.duration
-    }
 }
 
 impl PerformanceEntryMethods for PerformanceEntry {
