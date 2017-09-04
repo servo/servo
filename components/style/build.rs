cd13888a1bb3c0a4f9b3e8fe27ed678bf176d9d9
Paul Rouget <me@paulrouget.com>
2017-09-04 04:46:33 +0200

    use cargo manifest path to find build.py script

--- components/style/build.rs
+++ components/style/build.rs
@@ -69,7 +69,8 @@ fn generate_properties() {
         }
     }
 
-    let script = Path::new(file!()).parent().unwrap().join("properties").join("build.py");
+    let script = Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap())
+        .join("properties").join("build.py");
     let product = if cfg!(feature = "gecko") { "gecko" } else { "servo" };
     let status = Command::new(&*PYTHON)
         .arg(&script)
