5b541ee6cfc3e17875df95d2fa2536874d749f10
Alan Jeffrey <ajeffrey@mozilla.com>
2017-09-18 09:52:13 -0500

    Added tests/unit/msg.

---
+++ tests/unit/msg/lib.rs
@@ -0,0 +1,8 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+extern crate msg;
+#[macro_use] extern crate size_of_test;
+
+#[cfg(all(test, target_pointer_width = "64"))] mod size_of;
