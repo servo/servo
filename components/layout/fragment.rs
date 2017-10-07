a5b173a55fe81d99f3e6c23fc02db9bc8bf403ad
Permutator <permutatorem@gmail.com>
2017-10-07 13:47:45 -0700

    Just check the len()s instead of doing a weird map-chain thing

--- components/layout/fragment.rs
+++ components/layout/fragment.rs
@@ -2840,8 +2840,9 @@ impl Fragment {
     pub fn can_fully_meld_with_next_inline_fragment(&self, next_fragment: &Fragment) -> bool {
         if let Some(ref inline_context_of_next_fragment) = next_fragment.inline_context {
             if let Some(ref inline_context_of_this_fragment) = self.inline_context {
+                inline_context_of_this_fragment.nodes.len() >=
+                    inline_context_of_next_fragment.nodes.len() &&
                 inline_context_of_this_fragment.nodes.iter().rev()
-                    .map(Some).chain(::std::iter::repeat(None))
                     .zip(inline_context_of_next_fragment.nodes.iter().rev())
                     .all(|(inline_context_node_from_this_fragment,
                            inline_context_node_from_next_fragment)|
@@ -2856,9 +2857,8 @@ impl Fragment {
                                 // There's nothing to meld, so compatibility doesn't matter.
                                 return true;
                             }
-                            inline_context_node_from_this_fragment.map(|node| {
-                                inline_context_node_from_next_fragment.address == node.address
-                            }).unwrap_or(false)
+                            inline_context_node_from_next_fragment.address ==
+                                inline_context_node_from_this_fragment.address
                         })
             } else {
                 false
@@ -2871,8 +2871,9 @@ impl Fragment {
     pub fn can_fully_meld_with_prev_inline_fragment(&self, prev_fragment: &Fragment) -> bool {
         if let Some(ref inline_context_of_prev_fragment) = prev_fragment.inline_context {
             if let Some(ref inline_context_of_this_fragment) = self.inline_context {
+                inline_context_of_this_fragment.nodes.len() >=
+                    inline_context_of_prev_fragment.nodes.len() &&
                 inline_context_of_this_fragment.nodes.iter().rev()
-                    .map(Some).chain(::std::iter::repeat(None))
                     .zip(inline_context_of_prev_fragment.nodes.iter().rev())
                     .all(|(inline_context_node_from_this_fragment,
                            inline_context_node_from_prev_fragment)|
@@ -2885,9 +2886,8 @@ impl Fragment {
                                     InlineFragmentNodeFlags::FIRST_FRAGMENT_OF_ELEMENT) {
                                 return true;
                             }
-                            inline_context_node_from_this_fragment.map(|node| {
-                                inline_context_node_from_prev_fragment.address == node.address
-                            }).unwrap_or(false)
+                            inline_context_node_from_prev_fragment.address ==
+                                inline_context_node_from_this_fragment.address
                         })
             } else {
                 false
