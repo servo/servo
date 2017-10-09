398a15e6e020a64cac7f7c95d93039c0ba98a040
Josh Matthews <josh@joshmatthews.net>
2017-10-09 06:21:48 -0400

    Make update_manifest.sh more verbose.

--- etc/ci/manifest_changed.sh
+++ etc/ci/manifest_changed.sh
@@ -8,13 +8,17 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+echo "About to update manifest."
+
 # We shouldn't need any binary at all to update the manifests.
 # Adding "SKIP_TESTS" to skip tests, it doesn't really skip the tests.
 # It will run "run_wpt" with "'test_list': ['SKIP_TESTS']",
 # and then pass it into wptrunner, which won't be able to find any tests named
 # "SKIP_TESTS", and thus won't run any.
 # Adding "--binary=" to skip looking for a compiled servo binary.
-./mach test-wpt --manifest-update --binary= SKIP_TESTS > /dev/null
+./mach test-wpt --manifest-update --binary= SKIP_TESTS
+
+echo "Updated manifest; about to check if any changes were made to it."
 
 diff="$(git diff -- tests/*/MANIFEST.json)"
 echo "${diff}"
