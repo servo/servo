99945adec22f839eb9effd775531239d63f10f8b
Xidorn Quan <me@upsuper.org>
2017-10-13 15:20:17 +1100

    Correct the position and record count of broken canary

--- components/hashglobe/src/diagnostic.rs
+++ components/hashglobe/src/diagnostic.rs
@@ -60,17 +60,19 @@ impl<K: Hash + Eq, V, S: BuildHasher> DiagnosticHashMap<K, V, S>
 
     fn verify(&self) {
         let mut position = 0;
+        let mut count = 0;
         let mut bad_canary: Option<(usize, *const usize)> = None;
         for (_,v) in self.map.iter() {
             let canary_ref = &v.0;
+            position += 1;
             if *canary_ref == CANARY {
-                position += 1;
                 continue;
             }
+            count += 1;
             bad_canary = Some((*canary_ref, canary_ref));
         }
         if let Some(c) = bad_canary {
-            self.report_corruption(c.0, c.1, position);
+            self.report_corruption(c.0, c.1, position, count);
         }
     }
 
@@ -158,7 +160,8 @@ impl<K: Hash + Eq, V, S: BuildHasher> DiagnosticHashMap<K, V, S>
         &self,
         canary: usize,
         canary_addr: *const usize,
-        position: usize
+        position: usize,
+        count: usize,
     ) {
         use ::std::ffi::CString;
         let key = b"HashMapJournal\0";
@@ -170,11 +173,13 @@ impl<K: Hash + Eq, V, S: BuildHasher> DiagnosticHashMap<K, V, S>
             );
         }
         panic!(
-            "HashMap Corruption (sz={}, cap={}, pairsz={}, cnry={:#x}, pos={}, base_addr={:?}, cnry_addr={:?}, jrnl_len={})",
+            concat!("HashMap Corruption (sz={}, cap={}, pairsz={}, cnry={:#x}, count={}, ",
+                    "last_pos={}, base_addr={:?}, cnry_addr={:?}, jrnl_len={})"),
             self.map.len(),
             self.map.raw_capacity(),
             ::std::mem::size_of::<(K, (usize, V))>(),
             canary,
+            count,
             position,
             self.map.raw_buffer(),
             canary_addr,
