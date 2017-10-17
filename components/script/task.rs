e4a080ba7de54a8e731ea65644658667f005bda7
ddh <dianehosfelt@gmail.com>
2017-10-17 23:31:43 +0100

    i think i fixed runnable vs task. need to fit pipeline back in. WIP

--- components/script/task.rs
+++ components/script/task.rs
@@ -7,6 +7,7 @@
 use std::fmt;
 use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
+use msg::constellation_msg::PipelineId;
 
 macro_rules! task {
     ($name:ident: move || $body:tt, $pipeline:stmt) => {{
@@ -43,6 +44,8 @@ pub trait TaskOnce: Send {
     }
 
     fn run_once(self);
+
+    fn pipeline(&self) -> Option<PipelineId> { None }
 }
 
 /// A boxed version of `TaskOnce`.
@@ -120,4 +123,65 @@ where
             self.inner.run_once()
         }
     }
+
+    pub fn pipeline(&self) -> Option<PipelineId> {
+        None
+    }
 }
+
+// impl TaskOnce for ConstellationControlMsg {
+//     fn pipeline(&self) -> Option<PipelineId> {
+//         use script_thread::ConstellationControlMsg::*;
+//         match *self {
+//             NavigationResponse(id, _) => Some(id),
+//             AttachLayout(ref new_layout_info) => Some(new_layout_info.new_pipeline_id),
+//             Resize(id, ..) => Some(id),
+//             ResizeInactive(id, ..) => Some(id),
+//             ExitPipeline(id, ..) => Some(id),
+//             ExitScriptThread => None,
+//             SendEvent(id, ..) => Some(id),
+//             Viewport(id, ..) => Some(id),
+//             SetScrollState(id, ..) => Some(id),
+//             GetTitle(id) => Some(id),
+//             SetDocumentActivity(id, ..) => Some(id),
+//             ChangeFrameVisibilityStatus(id, ..) => Some(id),
+//             NotifyVisibilityChange(id, ..) => Some(id),
+//             Navigate(id, ..) => Some(id),
+//             PostMessage(id, ..) => Some(id),
+//             MozBrowserEvent(id, ..) => Some(id),
+//             UpdatePipelineId(_, _, id, _) => Some(id),
+//             FocusIFrame(id, ..) => Some(id),
+//             WebDriverScriptCommand(id, ..) => Some(id),
+//             TickAllAnimations(id) => Some(id),
+//             TransitionEnd(..) => None,
+//             WebFontLoaded(id) => Some(id),
+//             DispatchIFrameLoadEvent { .. } => None,
+//             DispatchStorageEvent(id, ..) => Some(id),
+//             ReportCSSError(id, ..) => Some(id),
+//             Reload(id, ..) => Some(id),
+//             WebVREvents(id, ..) => Some(id),
+//             PaintMetric(..) => None,
+//             InteractiveMetric(..) => None,
+//         }
+//     }
+// }
+
+// impl TaskOnce for MainThreadScriptMsg {
+//     fn pipeline(&self) -> Option<PipelineId> {
+//         use self::MainThreadScriptMsg::*;
+//         match *self {
+//             Common(_) => None,
+//             ExitWindow(pipeline_id) => Some(pipeline_id),
+//             Navigate(pipeline_id, ..) => Some(pipeline_id),
+//             WorkletLoaded(pipeline_id) => Some(pipeline_id),
+//             RegisterPaintWorklet { pipeline_id, .. } => Some(pipeline_id),
+//             DispatchJobQueue { .. } => None,
+//         }
+//     }
+// }
+
+// impl TaskOnce for DevtoolScriptControlMsg {
+//     fn pipeline(&self) -> Option<PipelineId> {
+//         None
+//     }
+// }
