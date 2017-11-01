6370c0b2b8676555f61f2c34317a567e2a5879a4
Keith Yeung <kungfukeith11@gmail.com>
2017-11-01 03:38:48 -0700

    Allow structured clone reads to return a boolean

--- components/script/dom/dedicatedworkerglobalscope.rs
+++ components/script/dom/dedicatedworkerglobalscope.rs
@@ -465,7 +465,7 @@ impl DedicatedWorkerGlobalScope {
                 let target = self.upcast();
                 let _ac = enter_realm(self);
                 rooted!(in(scope.get_cx()) let mut message = UndefinedValue());
-                data.read(scope.upcast(), message.handle_mut());
+                assert!(data.read(scope.upcast(), message.handle_mut()));
                 MessageEvent::dispatch_jsval(target, scope.upcast(), message.handle(), None, None);
             },
             WorkerScriptMsg::Common(msg) => {
