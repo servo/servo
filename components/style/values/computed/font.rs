e3be17cb0c219a8b40b374e93e013c9db6e867c6
CYBAI <cyb.ai.815@gmail.com>
2017-11-10 00:09:55 +0800

    style: Move font-synthesis outside of mako

--- components/style/values/computed/font.rs
+++ components/style/values/computed/font.rs
@@ -14,7 +14,7 @@ use values::specified::font as specified;
 use values::specified::length::{FontBaseSize, NoCalcLength};
 
 pub use values::computed::Length as MozScriptMinSize;
-pub use values::specified::font::XTextZoom;
+pub use values::specified::font::{XTextZoom, FontSynthesis};
 
 /// As of CSS Fonts Module Level 3, only the following values are
 /// valid: 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900
