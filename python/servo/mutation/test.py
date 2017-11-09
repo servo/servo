d578e393b0d41d9f355d5474b841f2bf46c80373
Sandeep Hegde <dsandeephegde@gmail.com>
2017-11-09 02:19:30 -0500

    Refactored mutator strategies using strategy pattern

--- python/servo/mutation/test.py
+++ python/servo/mutation/test.py
@@ -15,20 +15,6 @@ import os
 import random
 from enum import Enum
 DEVNULL = open(os.devnull, 'wb')
-strategies = [
-    {
-        'regex': r'\s&&\s',
-        'replaceString': ' || '
-    },
-    {
-        'regex': r'(?<=if\s)(.*)(?=\s\{)',
-        'replaceString': 'true'
-    },
-    {
-        'regex': r'(?<=if\s)(.*)(?=\s\{)',
-        'replaceString': 'false'
-    }
-]
 
 
 class Status(Enum):
@@ -38,21 +24,59 @@ class Status(Enum):
     UNEXPECTED = 3
 
 
-def mutate_random_line(file_name):
-    line_numbers = []
-    strategy = strategies[random.randint(0, len(strategies) - 1)]
-    for line in fileinput.input(file_name):
-        if re.search(strategy['regex'], line):
-            line_numbers.append(fileinput.lineno())
-    if len(line_numbers) == 0:
-        return -1
-    else:
-        mutation_line_number = line_numbers[random.randint(0, len(line_numbers) - 1)]
-        for line in fileinput.input(file_name, inplace=True):
-            if fileinput.lineno() == mutation_line_number:
-                line = re.sub(strategy['regex'], strategy['replaceString'], line)
-            print line.rstrip()
-        return mutation_line_number
+class Mutator:
+    def __init__(self, strategy):
+        self._strategy = strategy
+
+    def mutate(self, file_name):
+        return self._strategy.mutate_random_line(file_name)
+
+
+class Strategy:
+    def __init__(self):
+        self._replace_strategy = {}
+
+    def mutate_random_line(self, file_name):
+        line_numbers = []
+        for line in fileinput.input(file_name):
+            if re.search(self._replace_strategy['regex'], line):
+                line_numbers.append(fileinput.lineno())
+        if len(line_numbers) == 0:
+            return -1
+        else:
+            mutation_line_number = line_numbers[random.randint(0, len(line_numbers) - 1)]
+            for line in fileinput.input(file_name, inplace=True):
+                if fileinput.lineno() == mutation_line_number:
+                    line = re.sub(self._replace_strategy['regex'], self._replace_strategy['replaceString'], line)
+                print line.rstrip()
+            return mutation_line_number
+
+
+class AndOr(Strategy):
+    def __init__(self):
+        Strategy.__init__(self)
+        self._replace_strategy = {
+            'regex': r'\s&&\s',
+            'replaceString': ' || '
+        }
+
+
+class IfTrue(Strategy):
+    def __init__(self):
+        Strategy.__init__(self)
+        self._replace_strategy = {
+            'regex': r'(?<=if\s)(.*)(?=\s\{)',
+            'replaceString': 'true'
+        }
+
+
+class IfFalse(Strategy):
+    def __init__(self):
+        Strategy.__init__(self)
+        self._replace_strategy = {
+            'regex': r'(?<=if\s)(.*)(?=\s\{)',
+            'replaceString': 'false'
+        }
 
 
 def mutation_test(file_name, tests):
@@ -62,7 +86,10 @@ def mutation_test(file_name, tests):
         status = Status.SKIPPED
         print "{0} has local changes, please commit/remove changes before running the test".format(file_name)
     else:
-        mutated_line = mutate_random_line(file_name)
+        mutation_strategies = (AndOr, IfTrue, IfFalse)
+        strategy = random.choice(mutation_strategies)()
+        mutator = Mutator(strategy)
+        mutated_line = mutator.mutate(file_name)
         if mutated_line != -1:
             print "Mutating {0} at line {1}".format(file_name, mutated_line)
             print "compiling mutant {0}:{1}".format(file_name, mutated_line)
