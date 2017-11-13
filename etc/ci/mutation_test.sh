ed47f89e79c79aa2f69652c7302c64d8bf0880ad
Sandeep Hegde <dsandeephegde@gmail.com>
2017-11-13 01:01:09 -0500

    Changed invocation of muatation test in CI to bash script to use virtualenv

---
+++ etc/ci/mutation_test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+source python/_virtualenv/bin/activate
+python python/servo/mutation/init.py components/script/dom
