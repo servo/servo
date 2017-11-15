a1dfce4d3cd4556d36ce197330add511771dc22d
Alan Jeffrey <ajeffrey@mozilla.com>
2017-11-15 12:20:37 -0600

    Made .csv the default file format for perf tests.

--- etc/ci/performance/test_all.sh
+++ etc/ci/performance/test_all.sh
@@ -49,7 +49,7 @@ python3 -m http.server > /dev/null 2>&1 &
 # MANIFEST="page_load_test/tp5n/20160509.manifest" # A manifest that excludes
 MANIFEST="page_load_test/example.manifest" # A manifest that excludes
                                                  # timeout test cases
-PERF_FILE="output/perf-$(date +%s).json"
+PERF_FILE="output/perf-$(date +%s).csv"
 
 echo "Running tests"
 python3 runner.py ${engine} --runs 4 --timeout "${timeout}" --base "${base}" \
