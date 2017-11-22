b1ce298d4b20065bb77f62974a4c4106e9cd91e1
Simon Sapin <simon.sapin@exyr.org>
2017-11-22 16:21:11 +0100

    Replace compiletest suite by doc-tests with `compile_fail`
    compiletest-rs use internal rustc APIs and is broken in today’s Nightly.
    rustdoc however is maintained with rustc and so much less fragile.

--- tests/unit/deny_public_fields/lib.rs
+++ tests/unit/deny_public_fields/lib.rs
@@ -2,17 +2,32 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#![allow(dead_code)]
-
-#[macro_use]
-extern crate deny_public_fields;
+/**
+```compile_fail
+#[macro_use] extern crate deny_public_fields;
 
 #[derive(DenyPublicFields)]
-//~^ ERROR proc-macro derive panicked
-//~| HELP Field `v1` should not be public
 struct Foo {
     pub v1: i32,
     v2: i32
 }
 
 fn main() {}
+```
+*/
+pub fn deny_public_fields_failing() {}
+
+/**
+```
+#[macro_use] extern crate deny_public_fields;
+
+#[derive(DenyPublicFields)]
+struct Foo {
+    v1: i32,
+    v2: i32
+}
+
+fn main() {}
+```
+*/
+pub fn deny_public_fields_ok() {}
