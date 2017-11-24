09c11d49d56d84e7a55ec8f3e3032c81f817763e
Emilio Cobos Álvarez <emilio@crisal.io>
2017-11-24 14:30:35 +0100

    style: Make a Servo-only assertion apply to Gecko too.
    MozReview-Commit-ID: CWhSfDQ8naE

--- components/style/traversal.rs
+++ components/style/traversal.rs
@@ -256,8 +256,10 @@ pub trait DomTraversal<E: TElement> : Sync {
         parent: E,
         parent_data: &ElementData,
     ) -> bool {
-        debug_assert!(cfg!(feature = "gecko") ||
-                      parent.has_current_styles_for_traversal(parent_data, context.shared.traversal_flags));
+        debug_assert!(parent.has_current_styles_for_traversal(
+            parent_data,
+            context.shared.traversal_flags,
+        ));
 
         // If the parent computed display:none, we don't style the subtree.
         if parent_data.styles.is_display_none() {
