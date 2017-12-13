d15607f9bef4b6ffcd4f190765d7bd9439f1717a
E. Dunham <github@edunham.net>
2017-12-13 12:46:39 -0800

    update tc test from buildbot steps and document

--- etc/ci/taskcluster-test.sh
+++ etc/ci/taskcluster-test.sh
@@ -4,18 +4,22 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+
+
 set -x
 set -o errexit
 set -o nounset
 set -o pipefail
 
+
+# Update this from the linux-dev builder in etc/ci/buildbot_steps.yml
 ./mach test-tidy --no-progress --all
 ./mach test-tidy --no-progress --self-test
-env SERVO_RUSTC_LLVM_ASSERTIONS=1 ./mach build --dev
-env SERVO_RUSTC_LLVM_ASSERTIONS=1 ./mach test-compiletest
-env SERVO_RUSTC_LLVM_ASSERTIONS=1 ./mach test-unit
-env SERVO_RUSTC_LLVM_ASSERTIONS=1 ./mach package --dev
-env SERVO_RUSTC_LLVM_ASSERTIONS=1 ./mach build-cef
+env CC=gcc-5 CXX=g++-5 ./mach build --dev
+env ./mach test-unit
+env ./mach package --dev
+env ./mach build-cef
+env ./mach build --dev --no-default-features --features default-except-unstable
 ./mach build-geckolib
 ./mach test-stylo
 bash ./etc/ci/lockfile_changed.sh
