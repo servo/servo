a311e0f569f8aba447aeef3034a37ee900720307
Anthony Ramine <n.oxyde@gmail.com>
2018-01-19 12:12:47 +0100

    Merge layout and layout_tests

--- components/layout/tests/size_of.rs
+++ components/layout/tests/size_of.rs
@@ -2,6 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#![cfg(target_pointer_width = "64")]
+
+extern crate layout;
+#[macro_use] extern crate size_of_test;
+
 use layout::Fragment;
 use layout::SpecificFragmentInfo;
 
