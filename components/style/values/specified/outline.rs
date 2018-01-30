bac8781cc782978a68443f20726e762f50a7eeeb
Anthony Ramine <n.oxyde@gmail.com>
2018-01-30 10:13:15 +0100

    Kill define_numbered_css_keyword_enum

--- components/style/values/specified/outline.rs
+++ components/style/values/specified/outline.rs
@@ -39,10 +39,10 @@ impl OutlineStyle {
 
 impl Parse for OutlineStyle {
     fn parse<'i, 't>(
-        context: &ParserContext,
+        _context: &ParserContext,
         input: &mut Parser<'i, 't>
     ) -> Result<OutlineStyle, ParseError<'i>> {
-        if let Ok(border_style) = input.try(|i| BorderStyle::parse(context, i)) {
+        if let Ok(border_style) = input.try(BorderStyle::parse) {
             if let BorderStyle::Hidden = border_style {
                 return Err(input.new_custom_error(SelectorParseErrorKind::UnexpectedIdent("hidden".into())));
             }
