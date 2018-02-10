a90e660eacf78daba3b30048fd7111afe4659e72
Anthony Ramine <n.oxyde@gmail.com>
2018-02-10 01:28:38 +0100

    Kill AnimatedValueAsComputed

--- components/style/values/animated/mod.rs
+++ components/style/values/animated/mod.rs
@@ -76,9 +76,6 @@ pub trait ToAnimatedValue {
     fn from_animated_value(animated: Self::AnimatedValue) -> Self;
 }
 
-/// Marker trait for computed values with the same representation during animations.
-pub trait AnimatedValueAsComputed {}
-
 /// Returns a value similar to `self` that represents zero.
 ///
 /// This trait is derivable with `#[derive(ToAnimatedValue)]`. If a field is
@@ -243,30 +240,31 @@ where
     }
 }
 
-impl AnimatedValueAsComputed for Au {}
-impl AnimatedValueAsComputed for ComputedAngle {}
-impl AnimatedValueAsComputed for SpecifiedUrl {}
-#[cfg(feature = "servo")]
-impl AnimatedValueAsComputed for ComputedUrl {}
-impl AnimatedValueAsComputed for bool {}
-impl AnimatedValueAsComputed for f32 {}
+macro_rules! trivial_to_animated_value {
+    ($ty:ty) => {
+        impl $crate::values::animated::ToAnimatedValue for $ty {
+            type AnimatedValue = Self;
 
-impl<T> ToAnimatedValue for T
-where
-    T: AnimatedValueAsComputed,
-{
-    type AnimatedValue = Self;
+            #[inline]
+            fn to_animated_value(self) -> Self {
+                self
+            }
 
-    #[inline]
-    fn to_animated_value(self) -> Self {
-        self
-    }
-
-    #[inline]
-    fn from_animated_value(animated: Self::AnimatedValue) -> Self {
-        animated
+            #[inline]
+            fn from_animated_value(animated: Self::AnimatedValue) -> Self {
+                animated
+            }
+        }
     }
 }
+
+trivial_to_animated_value!(Au);
+trivial_to_animated_value!(ComputedAngle);
+trivial_to_animated_value!(SpecifiedUrl);
+#[cfg(feature = "servo")]
+trivial_to_animated_value!(ComputedUrl);
+trivial_to_animated_value!(bool);
+trivial_to_animated_value!(f32);
 
 impl ToAnimatedValue for ComputedNonNegativeNumber {
     type AnimatedValue = Self;
