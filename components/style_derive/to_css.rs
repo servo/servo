6ad1ae2f0162d07ff8a88299389919720cbf3ee6
Emilio Cobos Álvarez <emilio@crisal.io>
2018-03-07 03:12:12 +0100

    style: Introduce css(skip_if) to conditionally skip serialization of a field.

--- components/style_derive/to_css.rs
+++ components/style_derive/to_css.rs
@@ -5,7 +5,7 @@
 use cg::{self, WhereClause};
 use darling::util::Override;
 use quote::{ToTokens, Tokens};
-use syn::{self, Data};
+use syn::{self, Data, Ident};
 use synstructure::{BindingInfo, Structure, VariantInfo};
 
 pub fn derive(input: syn::DeriveInput) -> Tokens {
@@ -128,7 +128,15 @@ fn derive_variant_fields_expr(
         if !attrs.ignore_bound {
             where_clause.add_trait_bound(&first.ast().ty);
         }
-        return quote! { ::style_traits::ToCss::to_css(#first, dest) };
+        let mut expr = quote! { ::style_traits::ToCss::to_css(#first, dest) };
+        if let Some(condition) = attrs.skip_if {
+            expr = quote! {
+                if !#first.#condition() {
+                    #expr
+                }
+            }
+        }
+        return expr;
     }
 
     let mut expr = derive_single_field_expr(first, attrs, where_clause);
@@ -148,7 +156,7 @@ fn derive_single_field_expr(
     attrs: CssFieldAttrs,
     where_clause: &mut WhereClause,
 ) -> Tokens {
-    if attrs.iterable {
+    let mut expr = if attrs.iterable {
         quote! {
             for item in #field.iter() {
                 writer.item(&item)?;
@@ -159,7 +167,17 @@ fn derive_single_field_expr(
             where_clause.add_trait_bound(&field.ast().ty);
         }
         quote! { writer.item(#field)?; }
+    };
+
+    if let Some(condition) = attrs.skip_if {
+        expr = quote! {
+            if !#field.#condition() {
+                #expr
+            }
+        }
     }
+
+    expr
 }
 
 #[darling(attributes(css), default)]
@@ -188,4 +206,5 @@ struct CssFieldAttrs {
     ignore_bound: bool,
     iterable: bool,
     skip: bool,
+    skip_if: Option<Ident>,
 }
