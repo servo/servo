7a9873e26c2a45bc8428ef1019709699ca440e04
Josh Matthews <josh@joshmatthews.net>
2018-03-08 09:50:51 -0500

    Ensure readonly files can be removed on Windows.

--- python/servo/util.py
+++ python/servo/util.py
@@ -34,10 +34,14 @@ else:
     STATIC_RUST_LANG_ORG_DIST = "https://static-rust-lang-org.s3.amazonaws.com/dist"
     URLOPEN_KWARGS = {}
 
+def remove_readonly(func, path, _):
+    "Clear the readonly bit and reattempt the removal"
+    os.chmod(path, stat.S_IWRITE)
+    func(path)
 
 def delete(path):
     if os.path.isdir(path) and not os.path.islink(path):
-        shutil.rmtree(path)
+        shutil.rmtree(path, onerror=remove_readonly)
     else:
         os.remove(path)
 
