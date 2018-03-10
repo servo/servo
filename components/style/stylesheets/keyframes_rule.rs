a688e9252738bb83c4779f9bf886ff2da14afe16
Florian Wagner <florian.wagner@student.hpi.de>
2018-03-10 10:34:44 +0100

    fixing more nits

--- components/style/stylesheets/keyframes_rule.rs
+++ components/style/stylesheets/keyframes_rule.rs
@@ -126,15 +126,18 @@ impl KeyframePercentage {
     }
 
     fn parse<'i, 't>(input: &mut Parser<'i, 't>) -> Result<KeyframePercentage, ParseError<'i>> {
-    let token = input.next()?.clone();
+        let token = input.next()?;
         match token {
-            Token::Ident(ref identifier) if identifier.as_ref().eq_ignore_ascii_case("from")
-                => Ok(KeyframePercentage::new(0.)),
-            Token::Ident(ref identifier) if identifier.as_ref().eq_ignore_ascii_case("to")
-                => Ok(KeyframePercentage::new(1.)),
-            Token::Percentage { unit_value: percentage, .. } if percentage >= 0. && percentage <= 1.
-                => Ok(KeyframePercentage::new(percentage)),
-            _ => Err(input.new_unexpected_token_error(token)),
+            Token::Ident(ref identifier) if identifier.as_ref().eq_ignore_ascii_case("from") => {
+                Ok(KeyframePercentage::new(0.))
+            },
+            Token::Ident(ref identifier) if identifier.as_ref().eq_ignore_ascii_case("to") => {
+                Ok(KeyframePercentage::new(1.))
+            },
+            Token::Percentage { unit_value: percentage, .. } if percentage >= 0. && percentage <= 1. => {
+                Ok(KeyframePercentage::new(percentage)),
+                _ => Err(input.new_unexpected_token_error(token.clone())),
+            }
         }
     }
 }
