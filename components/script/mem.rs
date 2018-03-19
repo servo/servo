7f7fc917586fa5ea9c0681f7a479c7ec8ad69019
Anthony Weston <anthonyelliotweston@gmail.com>
2018-03-18 21:20:20 -0400

    Pass new method in CollectServoSizes for accurate DOM heap use reporting

--- components/script/mem.rs
+++ components/script/mem.rs
@@ -4,23 +4,8 @@
 
 //! Routines for handling measuring the memory usage of arbitrary DOM nodes.
 
-use dom::bindings::conversions::get_dom_class;
-use dom::bindings::reflector::DomObject;
 use malloc_size_of::{MallocSizeOf, MallocSizeOfOps};
 use std::os::raw::c_void;
-
-// This is equivalent to measuring a Box<T>, except that DOM objects lose their
-// associated box in order to stash their pointers in a reserved slot of their
-// JS reflector.
-#[allow(unsafe_code)]
-pub fn malloc_size_of_including_self<T: DomObject + MallocSizeOf>(
-    ops: &mut MallocSizeOfOps, obj: &T) -> usize
-{
-    unsafe {
-        let class = get_dom_class(obj.reflector().get_jsobject().get()).unwrap();
-        (class.malloc_size_of)(ops, obj as *const T as *const c_void)
-    }
-}
 
 /// Used by codegen to include the pointer to the `MallocSizeOf` implementation of each
 /// IDL interface. This way we don't have to find the most-derived interface of DOM
