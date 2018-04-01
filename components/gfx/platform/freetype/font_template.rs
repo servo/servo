dc683a1fc928af7bd30d6420128a8d31c89067dc
Jon Leighton <j@jonathanleighton.com>
2018-04-01 21:04:18 +1000

    Linux: Don't hold onto bytes of system fonts
    FontTemplateData gets passed over IPC during the communication between
    FontContext and FontCacheThread. Serializing and deserializing these
    bytes is expensive, so this change ensures that we only do that when the
    bytes can't be read from disk. A similar strategy is already used on
    macos and windows.
    
    The performance problem was particularly noticeable after implenting
    font fallback, where the content process would potentially work through
    a list of fonts, trying to find one which contains a certain glyph. That
    could result in lots of font bytes going over IPC.

--- components/gfx/platform/freetype/font_template.rs
+++ components/gfx/platform/freetype/font_template.rs
@@ -16,34 +16,21 @@ use webrender_api::NativeFontHandle;
 pub struct FontTemplateData {
     // If you add members here, review the Debug impl below
 
-    pub bytes: Vec<u8>,
+    pub bytes: Option<Vec<u8>>,
     pub identifier: Atom,
 }
 
 impl fmt::Debug for FontTemplateData {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
         fmt.debug_struct("FontTemplateData")
-           .field("bytes", &format!("[{} bytes]", self.bytes.len()))
+           .field("bytes", &self.bytes.as_ref().map(|b| format!("[{} bytes]", b.len())))
            .field("identifier", &self.identifier)
            .finish()
     }
 }
 
 impl FontTemplateData {
-    pub fn new(identifier: Atom, font_data: Option<Vec<u8>>) -> Result<FontTemplateData, Error> {
-        let bytes = match font_data {
-            Some(bytes) => {
-                bytes
-            },
-            None => {
-                // TODO: Handle file load failure!
-                let mut file = File::open(&*identifier)?;
-                let mut buffer = vec![];
-                file.read_to_end(&mut buffer).unwrap();
-                buffer
-            },
-        };
-
+    pub fn new(identifier: Atom, bytes: Option<Vec<u8>>) -> Result<FontTemplateData, Error> {
         Ok(FontTemplateData {
             bytes: bytes,
             identifier: identifier,
@@ -54,17 +41,29 @@ impl FontTemplateData {
     /// operation (depending on the platform) which performs synchronous disk I/O
     /// and should never be done lightly.
     pub fn bytes(&self) -> Vec<u8> {
-        self.bytes.clone()
+        self.bytes_if_in_memory().unwrap_or_else(|| {
+            let mut file = File::open(&*self.identifier).expect("Couldn't open font file!");
+            let mut buffer = vec![];
+            file.read_to_end(&mut buffer).unwrap();
+            buffer
+        })
     }
 
     /// Returns a clone of the bytes in this font if they are in memory. This function never
     /// performs disk I/O.
     pub fn bytes_if_in_memory(&self) -> Option<Vec<u8>> {
-        Some(self.bytes())
+        self.bytes.clone()
     }
 
     /// Returns the native font that underlies this font template, if applicable.
     pub fn native_font(&self) -> Option<NativeFontHandle> {
-        None
+        if self.bytes.is_none() {
+            Some(NativeFontHandle {
+                pathname: String::from(&*self.identifier),
+                index: 0,
+            })
+        } else {
+            None
+        }
     }
 }
