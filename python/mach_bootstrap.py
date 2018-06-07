cb18d8aff0bbd71db395339a5abfdb351ccf3d6d
Matthew Young <mabufo@gmail.com>
2018-06-06 20:25:32 -0500

    removed sys.platform check & consolidated the pip invocations
    The sys.platform check in need_pip_upgrade  was doing effectively the same pip invocation regardless of what
    platform we were evaluating to.
    
    Additionally, removed some duplicate definitions of the python variable which was already in scope
    higher up in the function.

--- python/mach_bootstrap.py
+++ python/mach_bootstrap.py
@@ -193,18 +193,7 @@ def _activate_virtualenv(topdir, is_firefox):
     if need_pip_upgrade:
         # Upgrade pip when virtualenv is created to fix the issue
         # https://github.com/servo/servo/issues/11074
-        if sys.platform in ['msys', 'win32']:
-            python = _get_exec_path(PYTHON_NAMES, is_valid_path=check_exec_path)
-            if not python:
-                sys.exit("Python is either not installed or not found in virtualenv.")
-
-            _process_exec([python, "-m", "pip", "install", "-I", "-U", "pip"])
-        else:
-            pip = _get_exec_path(PIP_NAMES, is_valid_path=check_exec_path)
-            if not pip:
-                sys.exit("Python pip is either not installed or not found in virtualenv.")
-
-            _process_exec([pip, "install", "-I", "-U", "pip"])
+        _process_exec([python, "-m", "pip", "install", "-I", "-U", "pip"])
 
     for req_rel_path in requirements_paths:
         req_path = os.path.join(topdir, req_rel_path)
@@ -216,10 +205,6 @@ def _activate_virtualenv(topdir, is_firefox):
                 continue
         except OSError:
             pass
-
-        pip = _get_exec_path(PIP_NAMES, is_valid_path=check_exec_path)
-        if not pip:
-            sys.exit("Python pip is either not installed or not found in virtualenv.")
 
         _process_exec([python, "-m", "pip", "install", "-I", "-r", req_path])
 
