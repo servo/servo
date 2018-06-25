efa16065e163228b6ce34be9f40893f12745c413
Simon Sapin <simon.sapin@exyr.org>
2018-06-25 22:17:05 +0200

    cargo run into Android/adb: configurable target device

--- support/android/egl-configs/run.sh
+++ support/android/egl-configs/run.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
+# Any
+#target=""
+
+# Specific device
+#target="-s something"
+
+# Emulator
+target="-e"
+
+# USB
+#target="-d"
+
 path="$1"
 base="$(basename $1)"
 remote_path="/data/local/tmp/$base"
 shift
 
-adb -e wait-for-device
-adb -e push "$path" "$remote_path"
-adb -e shell "$remote_path" "$@"
+adb $target "wait-for-device"
+adb $target push "$path" "$remote_path" >&2
+adb $target shell "$remote_path" "$@"
