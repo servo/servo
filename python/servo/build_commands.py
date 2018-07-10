98af35b40ad5d543a49f0d4a5e5f32b55519e12b
Paul Rouget <me@paulrouget.com>
2018-07-10 22:58:27 +0800

    fix android build

--- python/servo/build_commands.py
+++ python/servo/build_commands.py
@@ -181,7 +181,6 @@ class MachCommands(CommandBase):
               debug_mozjs=False, params=None, with_debug_assertions=False):
 
         opts = params or []
-        opts += ["--manifest-path", self.servo_manifest()]
 
         if android is None:
             android = self.config["build"]["android"]
@@ -412,4 +411,4 @@ class MachCommands(CommandBase):
             opts += ["-v"]
         opts += params
         return check_call(["cargo", "clean"] + opts,
-                          env=self.build_env(), cwd=self.servo_crate(), verbose=verbose)
+                          env=self.build_env(), cwd=self.ports_servo_crate(), verbose=verbose)
