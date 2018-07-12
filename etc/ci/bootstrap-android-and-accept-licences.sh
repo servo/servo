abb2ac6d201e1da1345e7f513ea961895caacebc
Simon Sapin <simon.sapin@exyr.org>
2018-07-12 14:45:36 +0200

    Test Android x86 on CI

---
+++ etc/ci/bootstrap-android-and-accept-licences.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+cd $(dirname ${0})/../..
+yes | ./mach bootstrap-android
