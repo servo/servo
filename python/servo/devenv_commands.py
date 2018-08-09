fc0a86c038f54d90c34da63bcf7a6750e8092b72
JoshBrudnak <jobrud314@gmail.com>
2018-08-09 17:26:04 -0400

    Add rustfmt command to devenv commands

--- python/servo/devenv_commands.py
+++ python/servo/devenv_commands.py
@@ -227,3 +227,16 @@ class MachCommands(CommandBase):
 
         with cd(self.context.topdir):
             return self.call_rustup_run(["cargo", "fetch"], env=self.build_env())
+
+    @Command('rustfmt',
+             description='Format the Rust code using Cargo fmt',
+             category='devenv')
+    @CommandArgument(
+        '--directory', '-d', default=None,
+        help='Command-line argument to specify the directory for formatting')
+    def rustfmt(self, directory=""):
+        if directory == "":
+            directory = self.context.topdir
+
+        with cd(self.context.topdir):
+            return self.call_rustup_run(["cargo", "fmt", "--", directory], env=self.build_env())
