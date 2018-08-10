b16a6268d994f6fd57c2ce09a3cd7ed7c8809efc
Josh Matthews <josh@joshmatthews.net>
2018-08-10 12:44:43 -0400

    Fix servodriver server configuration.

--- tests/wpt/web-platform-tests/tools/wptrunner/wptrunner/browsers/servodriver.py
+++ tests/wpt/web-platform-tests/tools/wptrunner/wptrunner/browsers/servodriver.py
@@ -38,7 +38,7 @@ def browser_kwargs(test_type, run_info_data, config, **kwargs):
         "binary": kwargs["binary"],
         "binary_args": kwargs["binary_args"],
         "debug_info": kwargs["debug_info"],
-        "server_config": config.ssl_config,
+        "server_config": config,
         "user_stylesheets": kwargs.get("user_stylesheets"),
     }
 
