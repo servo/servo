fb483e0c7e08d1fd930259a628bf541a1be324d2
Alan Jeffrey <ajeffrey@mozilla.com>
2018-08-17 10:31:47 -0500

    WIP

--- etc/ci/DAFT-HACK.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-./mach build --rel --target=arm-unknown-linux-gnueabihf || cat /home/servo/buildbot/slave/arm32/build/target/arm-unknown-linux-gnueabihf/release/build/backtrace-sys-*/out/config.*
--- support/arm32/fake-ld.sh
+++ support/arm32/fake-ld.sh
@@ -4,4 +4,4 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-arm-linux-gnueabihf-gcc -Lsupport/arm32/usr/lib/arm-linux-gnueabihf $*
+arm-linux-gnueabihf-gcc -L/usr/lib/arm-linux-gnueabihf $*
