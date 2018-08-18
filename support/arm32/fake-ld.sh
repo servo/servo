a1f75b57665444c3c074408911e23c7502a233fb
Alan Jeffrey <ajeffrey@mozilla.com>
2018-08-18 05:59:48 -0500

    WIP

--- support/arm32/fake-ld.sh
+++ support/arm32/fake-ld.sh
@@ -4,4 +4,5 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-arm-linux-gnueabihf-gcc -L/usr/lib/arm-linux-gnueabihf $*
+echo /usr/bin/arm-linux-gnueabihf-ld -L/usr/lib/arm-linux-gnueabihf $*
+/usr/bin/arm-linux-gnueabihf-ld -L/usr/lib/arm-linux-gnueabihf $*
