64cbf02084c4122c87ef9f03b356938a05623c9c
Josh Matthews <josh@joshmatthews.net>
2018-08-20 12:58:59 -0400

    Revert "build(mach): locate vs via vswhere on win32"
    This reverts commit c20efd5e9a1999d8253be3063113ab3c3a9ee77e.

--- mach.bat
+++ mach.bat
@@ -1,2 +1,39 @@
 @echo off
+
+pushd .
+
+IF EXIST "%ProgramFiles(x86)%" (
+  set "ProgramFiles32=%ProgramFiles(x86)%"
+) ELSE (
+  set "ProgramFiles32=%ProgramFiles%"
+)
+
+set VC14VARS=%VS140COMNTOOLS%..\..\VC\vcvarsall.bat
+IF EXIST "%VC14VARS%" (
+  set "VS_VCVARS=%VC14VARS%"
+) ELSE (
+  for %%e in (Enterprise Professional Community BuildTools) do (
+    IF EXIST "%ProgramFiles32%\Microsoft Visual Studio\2017\%%e\VC\Auxiliary\Build\vcvarsall.bat" (
+      set "VS_VCVARS=%ProgramFiles32%\Microsoft Visual Studio\2017\%%e\VC\Auxiliary\Build\vcvarsall.bat"
+    )
+  )
+)
+
+IF EXIST "%VS_VCVARS%" (
+  IF NOT DEFINED Platform (
+    IF EXIST "%ProgramFiles(x86)%" (
+      call "%VS_VCVARS%" x64
+    ) ELSE (
+      ECHO 32-bit Windows is currently unsupported.
+      EXIT /B
+    )
+  )
+) ELSE (
+  ECHO Visual Studio 2015 or 2017 is not installed.
+  ECHO Download and install Visual Studio 2015 or 2017 from https://www.visualstudio.com/
+  EXIT /B
+)
+
+popd
+
 python mach %*
