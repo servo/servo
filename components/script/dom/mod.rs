9e7345faa02717438afc1c1fcd4e46f82c31cf0b
Víctor Manuel Jáquez Leal <vjaquez@igalia.com>
2018-08-22 16:44:02 +0200

    dom: add mediaframewebrenderer

--- components/script/dom/mod.rs
+++ components/script/dom/mod.rs
@@ -387,6 +387,7 @@ pub mod inputevent;
 pub mod keyboardevent;
 pub mod location;
 pub mod mediaerror;
+pub mod mediaframerenderer;
 pub mod medialist;
 pub mod mediaquerylist;
 pub mod mediaquerylistevent;
