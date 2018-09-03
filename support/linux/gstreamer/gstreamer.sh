3b8ff935149a297aefca529fca9156ca49cdc4c0
Manish Goregaokar <manishsmail@gmail.com>
2018-09-03 12:06:27 +0530

    Add gstreamer setup script; use

---
+++ support/linux/gstreamer/gstreamer.sh
@@ -0,0 +1,4 @@
+wget https://github.com/ferjm/gstreamer-1.14.1-ubuntu-trusty/raw/master/gstreamer.tar.gz
+tar -zxvf gstreamer.tar.gz
+rm gstreamer.tar.gz
+sed -i "s;prefix=/root/gstreamer;prefix=$PWD/gstreamer;g" $PWD/gstreamer/lib/x86_64-linux-gnu/pkgconfig/*.pc
