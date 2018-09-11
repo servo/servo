7f4ca2192890498534a3615dcc036f3c25618737
kingdido999 <kingdido999@gmail.com>
2018-09-11 15:04:55 +0800

    Format msg #21373

--- components/msg/tests/size_of.rs
+++ components/msg/tests/size_of.rs
@@ -5,7 +5,8 @@
 #![cfg(target_pointer_width = "64")]
 
 extern crate msg;
-#[macro_use] extern crate size_of_test;
+#[macro_use]
+extern crate size_of_test;
 
 use msg::constellation_msg::BrowsingContextId;
 use msg::constellation_msg::PipelineId;
@@ -14,6 +15,18 @@ use msg::constellation_msg::TopLevelBrowsingContextId;
 size_of_test!(test_size_of_pipeline_id, PipelineId, 8);
 size_of_test!(test_size_of_optional_pipeline_id, Option<PipelineId>, 8);
 size_of_test!(test_size_of_browsing_context_id, BrowsingContextId, 8);
-size_of_test!(test_size_of_optional_browsing_context_id, Option<BrowsingContextId>, 8);
-size_of_test!(test_size_of_top_level_browsing_context_id, TopLevelBrowsingContextId, 8);
-size_of_test!(test_size_of_top_level_optional_browsing_context_id, Option<TopLevelBrowsingContextId>, 8);
+size_of_test!(
+    test_size_of_optional_browsing_context_id,
+    Option<BrowsingContextId>,
+    8
+);
+size_of_test!(
+    test_size_of_top_level_browsing_context_id,
+    TopLevelBrowsingContextId,
+    8
+);
+size_of_test!(
+    test_size_of_top_level_optional_browsing_context_id,
+    Option<TopLevelBrowsingContextId>,
+    8
+);
