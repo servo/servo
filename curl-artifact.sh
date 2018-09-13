1a6fc7ce77b9817672a61448ca9dc9dba39cc47d
Simon Sapin <simon.sapin@exyr.org>
2018-09-13 15:12:06 +0200

    curl logging to stderr

--- curl-artifact.sh
+++ curl-artifact.sh
@@ -3,7 +3,7 @@ task_id="$1"
 artifact="$2"
 shift 2
 url="https://queue.taskcluster.net/v1/task/${task_id}/artifacts/${artifact}"
-echo "Fetching $url"
+echo "Fetching $url" >&2
 curl \
     --retry 5 \
     --connect-timeout 10 \
