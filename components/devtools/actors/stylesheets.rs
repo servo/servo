6ccab1442cca047d0988e497bbef4cdccea113de
codehag <yulia.startsev@gmail.com>
2018-10-14 12:50:23 +0200

    DevTools - add preliminary StyleSheetActor

---
+++ components/devtools/actors/stylesheets.rs
@@ -0,0 +1,33 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+use actor::{Actor, ActorMessageStatus, ActorRegistry};
+use serde_json::{Map, Value};
+use std::net::TcpStream;
+
+pub struct StyleSheetsActor {
+    pub name: String,
+}
+
+impl Actor for StyleSheetsActor {
+    fn name(&self) -> String {
+        self.name.clone()
+    }
+    fn handle_message(
+        &self,
+        _: &ActorRegistry,
+        _: &str,
+        _: &Map<String, Value>,
+        _: &mut TcpStream,
+    ) -> Result<ActorMessageStatus, ()> {
+        Ok(ActorMessageStatus::Ignored)
+    }
+}
+
+impl StyleSheetsActor {
+    pub fn new(name: String) -> StyleSheetsActor {
+        StyleSheetsActor { name: name }
+    }
+}
+
