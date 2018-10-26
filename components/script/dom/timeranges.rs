3c341a1b20fcd71d1279ce5dcfd6226616b6104d
Fernando Jiménez Moreno <ferjmoreno@gmail.com>
2018-10-26 16:54:35 +0200

    Implement TimeRanges interface

---
+++ components/script/dom/timeranges.rs
@@ -0,0 +1,200 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+use dom::bindings::cell::DomRefCell;
+use dom::bindings::codegen::Bindings::TimeRangesBinding;
+use dom::bindings::codegen::Bindings::TimeRangesBinding::TimeRangesMethods;
+use dom::bindings::error::{Error, Fallible};
+use dom::bindings::num::Finite;
+use dom::bindings::reflector::{Reflector, reflect_dom_object};
+use dom::bindings::root::DomRoot;
+use dom::window::Window;
+use dom_struct::dom_struct;
+use std::cmp::Ordering;
+use std::fmt;
+
+#[derive(JSTraceable, MallocSizeOf)]
+struct TimeRange {
+    start: f64,
+    end: f64,
+}
+
+impl TimeRange {
+    pub fn union(&mut self, other: &TimeRange) {
+        self.start = f64::min(self.start, other.start);
+        self.end = f64::max(self.end, other.end);
+    }
+
+    fn contains(&self, time: f64) -> bool {
+        self.start <= time && time < self.end
+    }
+
+    fn is_overlapping(&self, other: &TimeRange) -> bool {
+        self.contains(other.start) || self.contains(other.end) || other.contains(self.start)
+    }
+
+    fn is_contiguous(&self, other: &TimeRange) -> bool {
+        other.start == self.end || other.end == self.start
+    }
+}
+
+impl Eq for TimeRange {}
+impl PartialEq for TimeRange {
+    fn eq(&self, other: &TimeRange) -> bool {
+        self.is_overlapping(other) || self.is_contiguous(other)
+    }
+}
+
+impl PartialOrd for TimeRange {
+    fn partial_cmp(&self, other: &TimeRange) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for TimeRange {
+    fn cmp(&self, other: &TimeRange) -> Ordering {
+        if self.end < other.start {
+            return Ordering::Less;
+        }
+
+        if self.start > other.end {
+            return Ordering::Greater;
+        }
+
+        // This can be exactly the same, overlapping or contiguous
+        Ordering::Equal
+    }
+}
+
+impl fmt::Debug for TimeRange {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        write!(fmt, "[{},{})", self.start, self.end)
+    }
+}
+
+#[derive(Debug, JSTraceable, MallocSizeOf)]
+pub struct TimeRangesContainer {
+    ranges: Vec<TimeRange>,
+}
+
+impl TimeRangesContainer {
+    pub fn new() -> Self {
+        Self {
+            ranges: Vec::new(),
+        }
+    }
+
+    pub fn len(&self) -> u32 {
+        self.ranges.len() as u32
+    }
+
+    pub fn start(&self, index: u32) -> Result<f64, ()> {
+        if index >= self.len() {
+            return Err(());
+        }
+        Ok(self.ranges[index as usize].start)
+    }
+
+    pub fn end(&self, index: u32) -> Result<f64, ()> {
+        if index >= self.len() {
+            return Err(());
+        }
+        Ok(self.ranges[index as usize].end)
+    }
+
+    pub fn add(&mut self, start: f64, end: f64) -> Result<(), ()> {
+        if start > end {
+            eprintln!("Cannot add a range if the end is older than the start.");
+            return Err(());
+        }
+
+        let mut new_range = TimeRange { start, end };
+
+        // For each present range check if we need to:
+        // - merge with the added range, in case we are overlapping or contiguous,
+        // - insert in place, we are completely, not overlapping and not contiguous
+        //   in between two ranges.
+        let mut idx = 0;
+        while idx < self.ranges.len() {
+            if new_range == self.ranges[idx] {
+                // The ranges are either overlapping or contiguous,
+                // we need to merge the new range with the existing one.
+                new_range.union(&self.ranges[idx]);
+                self.ranges.remove(idx);
+            } else if new_range < self.ranges[idx] && (idx == 0 || self.ranges[idx - 1] < new_range) {
+                // We are exactly after the current previous range and before the current
+                // range, while not overlapping with none of them.
+                // Or we are simply at the beginning.
+                self.ranges.insert(idx, new_range);
+                return Ok(());
+            } else {
+                idx += 1;
+            }
+        }
+
+        // Insert at the end.
+        self.ranges.insert(idx, new_range);
+
+        Ok(())
+    }
+}
+
+#[dom_struct]
+pub struct TimeRanges {
+    reflector_: Reflector,
+    ranges: DomRefCell<TimeRangesContainer>,
+}
+
+//XXX(ferjm) We'll get warnings about unused methods until we use this
+//           on the media element implementation.
+#[allow(dead_code)]
+impl TimeRanges {
+    pub fn new_inherited() -> TimeRanges {
+        Self {
+            reflector_: Reflector::new(),
+            ranges: DomRefCell::new(TimeRangesContainer::new()),
+        }
+    }
+
+    pub fn new(window: &Window) -> DomRoot<TimeRanges> {
+        reflect_dom_object(
+            Box::new(TimeRanges::new_inherited()),
+            window,
+            TimeRangesBinding::Wrap,
+        )
+    }
+}
+
+impl TimeRangesMethods for TimeRanges {
+    // https://html.spec.whatwg.org/multipage/#dom-timeranges-length
+    fn Length(&self) -> u32 {
+        self.ranges.borrow().len()
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-timeranges-start
+    fn Start(&self, index: u32) -> Fallible<Finite<f64>> {
+        self.ranges
+            .borrow()
+            .start(index)
+            .map(|range| {
+                Finite::wrap(range)
+            })
+            .map_err(|_| {
+                Error::IndexSize
+            })
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-timeranges-end
+    fn End(&self, index: u32) -> Fallible<Finite<f64>> {
+        self.ranges
+            .borrow()
+            .end(index)
+            .map(|range| {
+                Finite::wrap(range)
+            })
+            .map_err(|_| {
+                Error::IndexSize
+            })
+    }
+}
