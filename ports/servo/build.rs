27186751913beb7b81220404ea4cf58fac6f2d0d
Paul Rouget <me@paulrouget.com>
2018-11-02 10:40:59 +0100

    Count threads at shutdown and warn if not all finished

--- ports/servo/build.rs
+++ ports/servo/build.rs
@@ -2,6 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#[cfg(target_os = "macos")]
+extern crate cc;
+
 #[cfg(windows)]
 extern crate winres;
 
@@ -12,5 +15,11 @@ fn main() {
         res.set_icon("../../resources/Servo.ico");
         res.set_manifest_file("platform/windows/servo.exe.manifest");
         res.compile().unwrap();
+    }
+    #[cfg(target_os = "macos")]
+    {
+        cc::Build::new()
+            .file("platform/macos/count_threads.c")
+            .compile("count_threads");
     }
 }
