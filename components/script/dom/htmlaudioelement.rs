977ab6c9837caea65bd90c3b710ba1c98e572ed9
Vishnunarayan K I <appukuttancr@gmail.com>
2018-11-07 00:38:12 +0530

    Implement Audio constructor

--- components/script/dom/htmlaudioelement.rs
+++ components/script/dom/htmlaudioelement.rs
@@ -3,10 +3,16 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 use crate::dom::bindings::codegen::Bindings::HTMLAudioElementBinding;
+use crate::dom::bindings::codegen::Bindings::HTMLMediaElementBinding::HTMLMediaElementBinding::HTMLMediaElementMethods;
+use crate::dom::bindings::codegen::Bindings::WindowBinding::WindowMethods;
+use crate::dom::bindings::error::Fallible;
+use crate::dom::bindings::inheritance::Castable;
 use crate::dom::bindings::root::DomRoot;
+use crate::dom::bindings::str::DOMString;
 use crate::dom::document::Document;
 use crate::dom::htmlmediaelement::HTMLMediaElement;
 use crate::dom::node::Node;
+use crate::dom::window::Window;
 use dom_struct::dom_struct;
 use html5ever::{LocalName, Prefix};
 
@@ -39,5 +45,20 @@ impl HTMLAudioElement {
             document,
             HTMLAudioElementBinding::Wrap,
         )
+    }
+
+    pub fn Audio(
+        window: &Window,
+        src: Option<DOMString>,
+    ) -> Fallible<DomRoot<HTMLAudioElement>> {
+        let document = window.Document();
+        let audio = HTMLAudioElement::new(local_name!("audio"), None, &document);
+
+        audio.upcast::<HTMLMediaElement>().SetPreload(DOMString::from("auto"));
+        if let Some(s) = src {
+            audio.upcast::<HTMLMediaElement>().SetSrc(s);
+        }
+
+        Ok(audio)
     }
 }
