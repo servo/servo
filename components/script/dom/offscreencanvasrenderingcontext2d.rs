555af4fffc4c5b8dd75159acaef2d7570ec5a8a1
Chiranjivi <chiaggs@gmail.com>
2018-11-10 17:21:39 -0500

    added a file

--- components/script/dom/offscreencanvasrenderingcontext2d.rs
+++ components/script/dom/offscreencanvasrenderingcontext2d.rs
@@ -2,28 +2,30 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-use dom_struct::dom_struct;
-use dom::bindings::codegen::Bindings::OffscreenCanvasRenderingContext2DBinding;
-use dom::bindings::codegen::Bindings::OffscreenCanvasBinding::{Wrap as OffscreenCanvasRenderingContext2DWrap};
-use dom::offscreencanvas::OffscreenCanvas;
-use dom::bindings::root::DomRoot;
-use dom::globalscope::GlobalScope;
-use dom::bindings::reflector::{DomObject, Reflector, reflect_dom_object};
+ use dom_struct::dom_struct;
+ use dom::bindings::codegen::Bindings::OffscreenCanvasRenderingContext2DBinding;
+ use dom::bindings::codegen::Bindings::OffscreenCanvasRenderingContext2DBinding::{Wrap as OffscreenCanvasRenderingContext2DWrap};
+ use dom::offscreencanvas::{OffscreenRenderingContext,OffscreenCanvas};
+ use dom::bindings::root::DomRoot;
+ use dom::globalscope::GlobalScope;
+ use dom::bindings::reflector::{DomObject, Reflector, reflect_dom_object};
+ use dom::bindings::root::Dom;
 
-#[dom_struct]
-pub struct OffscreenCanvasRenderingContext2D{
-canvas: OffscreenCanvas,
-}
+ #[dom_struct]
+ pub struct OffscreenCanvasRenderingContext2D{
+     reflector_: Reflector,
+     canvas: Option<Dom<OffscreenCanvas>>,
+ }
 
-impl OffscreenCanvasRenderingContext2D {
-pub fn new_inherited(canvas: OffscreenCanvas) -> OffscreenCanvasRenderingContext2D {
-OffscreenCanvasRenderingContext2D {
-canvas: canvas,
-}
-}
+ impl OffscreenCanvasRenderingContext2D {
+     pub fn new_inherited(canvas: Option<&OffscreenCanvas>) -> OffscreenCanvasRenderingContext2D {
+         OffscreenCanvasRenderingContext2D {
+             reflector_: Reflector::new(),
+             canvas: canvas.map(Dom::from_ref),
+         }
+     }
 
-pub fn new(global: &GlobalScope,canvas: OffscreenCanvas) -> DomRoot<OffscreenCanvasRenderingContext2D> {
-reflect_dom_object(Box::new(OffscreenCanvasRenderingContext2D::new_inherited()), global, OffscreenCanvasRenderingContext2DWrap)
-}
-}
-
+     pub fn new(canvas: Option<&OffscreenCanvas>) -> DomRoot<OffscreenCanvasRenderingContext2D> {
+         reflect_dom_object(Box::new(OffscreenCanvasRenderingContext2D::new_inherited(canvas)), OffscreenCanvasRenderingContext2DWrap)
+     }
+ }
