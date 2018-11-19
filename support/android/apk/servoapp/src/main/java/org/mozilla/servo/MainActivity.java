f5ab95786f9cbbf5804d87f265812325fc80ad7b
Jan Andre Ikenmeyer <jan@ikenmeyer.eu>
2018-11-19 14:46:43 +0100

    Update MPL license to https (part 1)

--- support/android/apk/servoapp/src/main/java/org/mozilla/servo/MainActivity.java
+++ support/android/apk/servoapp/src/main/java/org/mozilla/servo/MainActivity.java
@@ -1,7 +1,7 @@
 /* -*- Mode: Java; c-basic-offset: 4; tab-width: 4; indent-tabs-mode: nil; -*-
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 package org.mozilla.servo;
 
--- support/arm64/fake-ld.sh
+++ support/arm64/fake-ld.sh
@@ -2,7 +2,7 @@
 
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 echo /usr/bin/aarch64-linux-gnu-ld -L/usr/lib/aarch64-linux-gnu $*
 /usr/bin/aarch64-linux-gnu-ld -L/usr/lib/aarch64-linux-gnu $*
