12c0d1350ada99201f5c55afb04d9a7d40b75aa7
Jan Andre Ikenmeyer <jan@ikenmeyer.eu>
2018-11-19 14:46:20 +0100

    servo-tidy: Allow to use MPL with http and https

--- python/tidy/servo_tidy/licenseck.py
+++ python/tidy/servo_tidy/licenseck.py
@@ -7,10 +7,16 @@
 # option. This file may not be copied, modified, or distributed
 # except according to those terms.
 
-MPL = """\
+OLD_MPL = """\
 This Source Code Form is subject to the terms of the Mozilla Public \
 License, v. 2.0. If a copy of the MPL was not distributed with this \
 file, You can obtain one at http://mozilla.org/MPL/2.0/.\
+"""
+
+MPL = """\
+This Source Code Form is subject to the terms of the Mozilla Public \
+License, v. 2.0. If a copy of the MPL was not distributed with this \
+file, You can obtain one at https://mozilla.org/MPL/2.0/.\
 """
 
 APACHE = """\
