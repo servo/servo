bad285366f3e6e9bb7b91766bcc7cee413a42772
Josh Matthews <josh@joshmatthews.net>
2018-12-02 14:58:15 -0500

    Reintroduce parent runtimes for worker threads.

--- components/script/script_runtime.rs
+++ components/script/script_runtime.rs
@@ -43,6 +43,7 @@ use js::rust::wrappers::{GetPromiseIsHandled, GetPromiseResult};
 use js::rust::Handle;
 use js::rust::IntoHandle;
 use js::rust::JSEngine;
+use js::rust::ParentRuntime;
 use js::rust::Runtime as RustRuntime;
 use malloc_size_of::MallocSizeOfOps;
 use msg::constellation_msg::PipelineId;
@@ -57,6 +58,7 @@ use std::os;
 use std::os::raw::c_void;
 use std::panic::AssertUnwindSafe;
 use std::ptr;
+use std::sync::Arc;
 use style::thread_state::{self, ThreadState};
 use time::{now, Tm};
 
@@ -324,13 +326,27 @@ impl Deref for Runtime {
 }
 
 lazy_static! {
-    static ref JS_ENGINE: JSEngine = JSEngine::init().unwrap();
+    static ref JS_ENGINE: Arc<JSEngine> = JSEngine::init().unwrap();
 }
 
 #[allow(unsafe_code)]
-pub unsafe fn new_rt_and_cx() -> Runtime {
+pub unsafe fn new_child_runtime(parent: ParentRuntime) -> Runtime {
+    new_rt_and_cx_with_parent(Some(parent))
+}
+
+#[allow(unsafe_code)]
+pub fn new_rt_and_cx() -> Runtime {
+    unsafe { new_rt_and_cx_with_parent(None) }
+}
+
+#[allow(unsafe_code)]
+unsafe fn new_rt_and_cx_with_parent(parent: Option<ParentRuntime>) -> Runtime {
     LiveDOMReferences::initialize();
-    let runtime = RustRuntime::new(&JS_ENGINE);
+    let runtime = if let Some(parent) = parent {
+        RustRuntime::create_with_parent(parent)
+    } else {
+        RustRuntime::new(JS_ENGINE.clone())
+    };
     let cx = runtime.cx();
 
     JS_AddExtraGCRootsTracer(cx, Some(trace_rust_roots), ptr::null_mut());
@@ -444,11 +460,12 @@ pub unsafe fn new_rt_and_cx() -> Runtime {
     }
     // NOTE: This is disabled above, so enabling it here will do nothing for now.
     if let Some(val) = PREFS.get("js.mem.gc.incremental.enabled").as_boolean() {
-        let compartment = if let Some(val) = PREFS.get("js.mem.gc.per_zone.enabled").as_boolean() {
-            val
-        } else {
-            false
-        };
+        let compartment =
+            if let Some(val) = PREFS.get("js.mem.gc.per_zone.enabled").as_boolean() {
+                val
+            } else {
+                false
+            };
         let mode = if val {
             JSGCMode::JSGC_MODE_INCREMENTAL
         } else if compartment {
