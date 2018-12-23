c3b93a967d0cf79bb19d78fbfb4f4b0f9e99d3ae
Manish Goregaokar <manishsmail@gmail.com>
2018-12-23 15:54:33 -0800

    Silence unused warnings

--- components/script/dom/xrstationaryreferencespace.rs
+++ components/script/dom/xrstationaryreferencespace.rs
@@ -14,6 +14,7 @@ pub struct XRStationaryReferenceSpace {
     xrreferencespace: XRReferenceSpace,
 }
 
+#[allow(unused)]
 impl XRStationaryReferenceSpace {
     pub fn new_inherited() -> XRStationaryReferenceSpace {
         XRStationaryReferenceSpace {
