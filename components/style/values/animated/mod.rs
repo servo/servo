a8943d2ec519c4fce68fffcee4e6512e63eb4602
Boris Chiou <boris.chiou@gmail.com>
2019-01-12 02:19:04 +0000

    style: Part 1: Support field_bound on Animate.
    So we can derive Animate on more generic types.
    
    Differential Revision: https://phabricator.services.mozilla.com/D16339

--- components/style/values/animated/mod.rs
+++ components/style/values/animated/mod.rs
@@ -113,7 +113,8 @@ pub fn animate_multiplicative_factor(
 /// function has been specified through `#[animate(fallback)]`.
 ///
 /// Trait bounds for type parameter `Foo` can be opted out of with
-/// `#[animation(no_bound(Foo))]` on the type definition.
+/// `#[animation(no_bound(Foo))]` on the type definition, trait bounds for
+/// fields can be opted into with `#[animation(field_bound)]` on the field.
 pub trait Animate: Sized {
     /// Animate a value towards another one, given an animation procedure.
     fn animate(&self, other: &Self, procedure: Procedure) -> Result<Self, ()>;
