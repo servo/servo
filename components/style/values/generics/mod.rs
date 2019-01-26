a1153f8b3cd8725fe2ddd7c4229faf5250d2bcc6
Emilio Cobos Álvarez <emilio@crisal.io>
2019-01-26 11:00:22 +0100

    style: Use the style system's LengthPercentage for shape-margin.
    This also makes us pass a few WPTs because we stop losing precision when
    serializing the computed value.
    
    Differential Revision: https://phabricator.services.mozilla.com/D17738

--- components/style/values/generics/mod.rs
+++ components/style/values/generics/mod.rs
@@ -174,6 +174,7 @@ impl SpecifiedValueInfo for CounterStyleOrNone {
     ToComputedValue,
     ToCss,
 )]
+#[repr(transparent)]
 pub struct NonNegative<T>(pub T);
 
 /// A wrapper of greater-than-or-equal-to-one values.
