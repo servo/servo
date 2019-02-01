5cf72b5797d0e9601b7e7a02d78470311313684e
kkpoon <kkpoon@eml.cc>
2019-02-01 11:25:18 +0800

    Add PerformanceResourceTiming: redirectStart

--- components/script/dom/performanceresourcetiming.rs
+++ components/script/dom/performanceresourcetiming.rs
@@ -116,7 +116,7 @@ impl PerformanceResourceTiming {
             initiator_type: initiator_type,
             next_hop: next_hop,
             worker_start: 0.,
-            redirect_start: 0.,
+            redirect_start: resource_timing.redirect_start as f64,
             redirect_end: 0.,
             fetch_start: resource_timing.fetch_start as f64,
             domain_lookup_start: 0.,
@@ -177,6 +177,11 @@ impl PerformanceResourceTimingMethods for PerformanceResourceTiming {
     fn RequestStart(&self) -> DOMHighResTimeStamp {
         // TODO
         Finite::wrap(self.request_start)
+    }
+
+    // https://w3c.github.io/resource-timing/#dom-performanceresourcetiming-redirectstart
+    fn RedirectStart(&self) -> DOMHighResTimeStamp {
+        Finite::wrap(self.redirect_start)
     }
 
     // https://w3c.github.io/resource-timing/#dom-performanceresourcetiming-responsestart
