538e13ab59bd0749bf3ab2b8fec7f80f7639a4b4
Fernando Jiménez Moreno <ferjmoreno@gmail.com>
2019-02-21 16:54:37 +0100

    Add invalidate_stylesheets to StyleSheetsListOwner trait

--- components/script/dom/document.rs
+++ components/script/dom/document.rs
@@ -4658,4 +4658,8 @@ impl StyleSheetListOwner for Dom<Document> {
             &mut *self.stylesheets.borrow_mut(),
         )
     }
+
+    fn invalidate_stylesheets(&self) {
+        Document::invalidate_stylesheets(self);
+    }
 }
