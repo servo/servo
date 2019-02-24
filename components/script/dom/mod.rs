35bca991ad4db3fe52ff6dd30bb3f7c567ba26e9
Robert Snakard <robert.snakard@gmail.com>
2019-02-24 21:24:36 +0000

    Implement WheelEvent Interface
    Note: The WheelEvent interface supports rotation in all 3 spatial
    dimensions. This implementation only supports two due to limitations
    in the Glutin compositor.
    
    The wheelevent interface is a dom interface that triggers for any
    attached device that can rotate in one or more spatial dimensions.
    Traditionally this is the mouse wheel though other devices could be
    used as well. E.g. the trackball on a trackball mouse.

--- components/script/dom/mod.rs
+++ components/script/dom/mod.rs
@@ -528,6 +528,7 @@ pub mod webgltexture;
 pub mod webgluniformlocation;
 pub mod webglvertexarrayobjectoes;
 pub mod websocket;
+pub mod wheelevent;
 pub mod window;
 pub mod windowproxy;
 pub mod worker;
