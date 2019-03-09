9b02b8194af4c0ff17c25ec242111b43105840c6
Aron Zwaan <aronzwaan@gmail.com>
2019-03-09 19:17:01 +0100

    Remove remaining visibility updates

--- components/constellation/constellation.rs
+++ components/constellation/constellation.rs
@@ -2673,13 +2673,6 @@ where
                 },
             };
 
-        if let Some(old_pipeline) = self.pipelines.get(&old_pipeline_id) {
-            old_pipeline.notify_visibility(false);
-        }
-        if let Some(new_pipeline) = self.pipelines.get(&new_pipeline_id) {
-            new_pipeline.notify_visibility(true);
-        }
-
         self.update_activity(old_pipeline_id);
         self.update_activity(new_pipeline_id);
 
@@ -3367,10 +3360,6 @@ where
                 self.notify_history_changed(change.top_level_browsing_context_id);
             },
             Some(old_pipeline_id) => {
-                if let Some(pipeline) = self.pipelines.get(&old_pipeline_id) {
-                    pipeline.notify_visibility(false);
-                }
-
                 // https://html.spec.whatwg.org/multipage/#unload-a-document
                 self.unload_document(old_pipeline_id);
                 // Deactivate the old pipeline, and activate the new one.
