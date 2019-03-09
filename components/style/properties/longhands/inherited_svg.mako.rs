9a8cee28005efef9ff327c7985828a3d783dfc44
violet <>
2019-03-09 13:05:56 +0000

    style: Change stroke-miterlimit restriction from GreaterThanOrEqualToOneNumber to NonNegativeNumber.

--- components/style/properties/longhands/inherited_svg.mako.rs
+++ components/style/properties/longhands/inherited_svg.mako.rs
@@ -110,11 +110,11 @@ ${helpers.single_keyword(
 
 ${helpers.predefined_type(
     "stroke-miterlimit",
-    "GreaterThanOrEqualToOneNumber",
+    "NonNegativeNumber",
     "From::from(4.0)",
     products="gecko",
-    animation_value_type="crate::values::computed::GreaterThanOrEqualToOneNumber",
-    spec="https://www.w3.org/TR/SVG11/painting.html#StrokeMiterlimitProperty",
+    animation_value_type="crate::values::computed::NonNegativeNumber",
+    spec="https://www.w3.org/TR/SVG2/painting.html#StrokeMiterlimitProperty",
 )}
 
 ${helpers.predefined_type(
