c49a88ec84defc10e6a3202bd8f8257fc700de86
Emilio Cobos Álvarez <emilio@crisal.io>
2019-04-01 21:47:43 +0000

    style: Trivially cleanup and remove some dead font code.
    Differential Revision: https://phabricator.services.mozilla.com/D24271

--- components/style/values/specified/font.rs
+++ components/style/values/specified/font.rs
@@ -530,18 +530,8 @@ impl FontFamily {
 
     /// Parse a specified font-family value
     pub fn parse_specified<'i, 't>(input: &mut Parser<'i, 't>) -> Result<Self, ParseError<'i>> {
-        input
-            .parse_comma_separated(|input| SingleFontFamily::parse(input))
-            .map(|v| FontFamily::Values(FontFamilyList::new(v.into_boxed_slice())))
-    }
-
-    #[cfg(feature = "gecko")]
-    /// Return the generic ID if it is a single generic font
-    pub fn single_generic(&self) -> Option<u8> {
-        match *self {
-            FontFamily::Values(ref values) => values.single_generic(),
-            _ => None,
-        }
+        let values = input.parse_comma_separated(SingleFontFamily::parse)?;
+        Ok(FontFamily::Values(FontFamilyList::new(values.into_boxed_slice())))
     }
 }
 
