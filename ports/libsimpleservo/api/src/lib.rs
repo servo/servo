ce6ed5f2a85f1cad35085bca33eefa297fb7bcf0
Fernando Jiménez Moreno <ferjmoreno@gmail.com>
2019-04-05 10:50:01 +0200

    Barebones media UI

--- ports/libsimpleservo/api/src/lib.rs
+++ ports/libsimpleservo/api/src/lib.rs
@@ -643,6 +643,9 @@ impl ResourceReaderMethods for ResourceReaderInstance {
             Resource::BluetoothBlocklist => {
                 &include_bytes!("../../../../resources/gatt_blocklist.txt")[..]
             },
+            Resource::MediaControls => {
+                &include_bytes!("../../../../resources/media_controls.js")[..]
+            },
         })
     }
 
