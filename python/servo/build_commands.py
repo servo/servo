c038ba4c7c98a21ccbf7f415a7bbc14231d6920a
Josh Matthews <josh@joshmatthews.net>
2019-04-24 16:19:37 -0400

    Force the use of clang-cl.exe for Windows builds.

--- python/servo/build_commands.py
+++ python/servo/build_commands.py
@@ -310,6 +310,10 @@ class MachCommands(CommandBase):
         if with_debug_assertions:
             env['RUSTFLAGS'] = env.get('RUSTFLAGS', "") + " -C debug_assertions"
 
+        if sys.platform == "win32":
+            env["CC"] = "clang-cl.exe"
+            env["CXX"] = "clang-cl.exe"
+
         if android:
             if "ANDROID_NDK" not in env:
                 print("Please set the ANDROID_NDK environment variable.")
