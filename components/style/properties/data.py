9636ef7be7981eb2541510186822e1385f362f8c
Brian Birtles <birtles@gmail.com>
2019-04-26 00:47:40 +0000

    style: Allow animating the 'all' property from Web Animations.
    Differential Revision: https://phabricator.services.mozilla.com/D28763

--- components/style/properties/data.py
+++ components/style/properties/data.py
@@ -400,8 +400,6 @@ class Shorthand(object):
             and allowed_in_keyframe_block != "False"
 
     def get_animatable(self):
-        if self.ident == "all":
-            return False
         for sub in self.sub_properties:
             if sub.animatable:
                 return True
