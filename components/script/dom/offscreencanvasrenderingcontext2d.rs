6e1ca0a0ddb68cee70aee9eed131d2bb50623b38
Maharsh <maharsh312@gmail.com>
2019-05-14 02:15:20 -0400

    Created CanvasFillStrokeStyles, CanvasShadowStyles, CanvasFilters for OffscreenCanvas

--- components/script/dom/offscreencanvasrenderingcontext2d.rs
+++ components/script/dom/offscreencanvasrenderingcontext2d.rs
@@ -3,13 +3,21 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 use crate::dom::bindings::cell::DomRefCell;
+use crate::dom::bindings::codegen::Bindings::CanvasRenderingContext2DBinding::CanvasImageSource;
 use crate::dom::bindings::codegen::Bindings::OffscreenCanvasRenderingContext2DBinding;
 use crate::dom::bindings::codegen::Bindings::OffscreenCanvasRenderingContext2DBinding::OffscreenCanvasRenderingContext2DMethods;
+use crate::dom::bindings::codegen::UnionTypes::StringOrCanvasGradientOrCanvasPattern;
+use crate::dom::bindings::error::Fallible;
+use crate::dom::bindings::num::Finite;
 use crate::dom::bindings::reflector::{reflect_dom_object, Reflector};
 use crate::dom::bindings::root::{Dom, DomRoot};
+use crate::dom::bindings::str::DOMString;
+use crate::dom::canvasgradient::CanvasGradient;
+use crate::dom::canvaspattern::CanvasPattern;
 use crate::dom::canvasrenderingcontext2d::CanvasState;
 use crate::dom::globalscope::GlobalScope;
 use crate::dom::offscreencanvas::OffscreenCanvas;
+use crate::dom::node::window_from_node;
 use dom_struct::dom_struct;
 use euclid::Size2D;
 
@@ -26,10 +34,19 @@ impl OffscreenCanvasRenderingContext2D {
         canvas: Option<&OffscreenCanvas>,
         size: Size2D<u64>,
     ) -> OffscreenCanvasRenderingContext2D {
+        let window = window_from_node(canvas);
+        let image_cache = window.image_cache();
+        let base_url = window.get_url();
         OffscreenCanvasRenderingContext2D {
             reflector_: Reflector::new(),
             canvas: canvas.map(Dom::from_ref),
-            canvas_state: DomRefCell::new(CanvasState::new(global, size)),
+            canvas_state: DomRefCell::new(CanvasState::new(
+                global,
+                canvas,
+                image_cache,
+                base_url,
+                Size2D::new(size.width as u64, size.height as u64),
+            )),
         }
     }
 
@@ -70,5 +87,102 @@ impl OffscreenCanvasRenderingContext2DMethods for OffscreenCanvasRenderingContex
     // https://html.spec.whatwg.org/multipage/#dom-context-2d-strokerect
     fn StrokeRect(&self, x: f64, y: f64, width: f64, height: f64) {
         self.canvas_state.borrow().StrokeRect(x, y, width, height);
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-context-2d-shadowoffsetx
+    fn ShadowOffsetX(&self) -> f64 {
+        self.canvas_state.borrow().ShadowOffsetX()
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-context-2d-shadowoffsetx
+    fn SetShadowOffsetX(&self, value: f64) {
+        self.canvas_state.borrow().SetShadowOffsetX(value)
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-context-2d-shadowoffsety
+    fn ShadowOffsetY(&self) -> f64 {
+        self.canvas_state.borrow().ShadowOffsetY()
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-context-2d-shadowoffsety
+    fn SetShadowOffsetY(&self, value: f64) {
+        self.canvas_state.borrow().SetShadowOffsetY(value)
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-context-2d-shadowblur
+    fn ShadowBlur(&self) -> f64 {
+        self.canvas_state.borrow().ShadowBlur()
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-context-2d-shadowblur
+    fn SetShadowBlur(&self, value: f64) {
+        self.canvas_state.borrow().SetShadowBlur(value)
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-context-2d-shadowcolor
+    fn ShadowColor(&self) -> DOMString {
+        self.canvas_state.borrow().ShadowColor()
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-context-2d-shadowcolor
+    fn SetShadowColor(&self, value: DOMString) {
+        self.canvas_state.borrow().SetShadowColor(value)
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-context-2d-strokestyle
+    fn StrokeStyle(&self) -> StringOrCanvasGradientOrCanvasPattern {
+        self.canvas_state.borrow().StrokeStyle()
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-context-2d-strokestyle
+    fn SetStrokeStyle(&self, value: StringOrCanvasGradientOrCanvasPattern) {
+        self.canvas_state.borrow().SetStrokeStyle(value)
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-context-2d-strokestyle
+    fn FillStyle(&self) -> StringOrCanvasGradientOrCanvasPattern {
+        self.canvas_state.borrow().FillStyle()
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-context-2d-strokestyle
+    fn SetFillStyle(&self, value: StringOrCanvasGradientOrCanvasPattern) {
+        self.canvas_state.borrow().SetFillStyle(value)
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-context-2d-createlineargradient
+    fn CreateLinearGradient(
+        &self,
+        x0: Finite<f64>,
+        y0: Finite<f64>,
+        x1: Finite<f64>,
+        y1: Finite<f64>,
+    ) -> DomRoot<CanvasGradient> {
+        self.canvas_state
+            .borrow()
+            .CreateLinearGradient(x0, y0, x1, y1)
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-context-2d-createradialgradient
+    fn CreateRadialGradient(
+        &self,
+        x0: Finite<f64>,
+        y0: Finite<f64>,
+        r0: Finite<f64>,
+        x1: Finite<f64>,
+        y1: Finite<f64>,
+        r1: Finite<f64>,
+    ) -> Fallible<DomRoot<CanvasGradient>> {
+        self.canvas_state
+            .borrow()
+            .CreateRadialGradient(x0, y0, r0, x1, y1, r1)
+    }
+
+    // https://html.spec.whatwg.org/multipage/#dom-context-2d-createpattern
+    fn CreatePattern(
+        &self,
+        image: CanvasImageSource,
+        mut repetition: DOMString,
+    ) -> Fallible<DomRoot<CanvasPattern>> {
+        self.canvas_state.borrow().CreatePattern(image, repetition)
     }
 }
