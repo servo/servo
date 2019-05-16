cbf3dfa0c0c06446b1bbd2b432efd843db114d79
Josh Matthews <josh@joshmatthews.net>
2019-05-16 12:25:57 -0400

    Add debugging info to macOS TC build tasks.

--- etc/taskcluster/decision_task.py
+++ etc/taskcluster/decision_task.py
@@ -765,6 +765,9 @@ def macos_build_task(name):
         .with_repo()
         .with_python2()
         .with_rustup()
+        # Debugging for surprising generic-worker behaviour
+        .with_early_script("ls")
+        .with_script("ls target")
     )
     return (
         with_homebrew(build_task, [
