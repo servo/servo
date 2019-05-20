6b2a581f048de11f7ec13fb696fc9e77636ca4ce
Fernando Jiménez Moreno <ferjmoreno@gmail.com>
2019-05-20 14:19:08 +0200

    Implement WindowMethods for Android

--- ports/libsimpleservo/api/src/gl_glue.rs
+++ ports/libsimpleservo/api/src/gl_glue.rs
@@ -27,6 +27,7 @@ pub mod egl {
     pub type khronos_uint64_t = libc::uint64_t;
     pub type khronos_ssize_t = libc::c_long;
     pub type EGLint = libc::int32_t;
+    pub type EGLContext = *const libc::c_void;
     pub type EGLNativeDisplayType = *const libc::c_void;
     pub type EGLNativePixmapType = *const libc::c_void;
     pub type NativeDisplayType = EGLNativeDisplayType;
@@ -35,13 +36,19 @@ pub mod egl {
 
     include!(concat!(env!("OUT_DIR"), "/egl_bindings.rs"));
 
+    pub struct EGLInitResult {
+        pub gl_wrapper: crate::gl_glue::ServoGl,
+        pub gl_context: EGLContext,
+        pub display: EGLNativeDisplayType,
+    }
+
     #[cfg(target_os = "android")]
-    pub fn init() -> Result<crate::gl_glue::ServoGl, &'static str> {
+    pub fn init() -> Result<EGLInitResult, &'static str> {
         info!("Loading EGL...");
         unsafe {
             let egl = Egl;
-            let d = egl.GetCurrentDisplay();
-            egl.SwapInterval(d, 1);
+            let display = egl.GetCurrentDisplay();
+            egl.SwapInterval(display, 1);
             let egl = GlesFns::load_with(|addr| {
                 let addr = CString::new(addr.as_bytes()).unwrap();
                 let addr = addr.as_ptr();
@@ -49,7 +56,11 @@ pub mod egl {
                 egl.GetProcAddress(addr) as *const c_void
             });
             info!("EGL loaded");
-            Ok(egl)
+            Ok(EGLInitResult {
+                gl_wrapper: egl,
+                gl_context: Egl.GetCurrentContext(),
+                display,
+            })
         }
     }
 
