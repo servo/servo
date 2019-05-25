900e951d5371e8866ac3772e1e80f61f41abe158
Martin Boros <martinboros@localhost.localdomain>
2019-05-24 21:45:01 -0400

    fixed formatting in bootstrap file

--- python/servo/bootstrap.py
+++ python/servo/bootstrap.py
@@ -115,7 +115,7 @@ def linux(context, force=False):
                 'libosmesa6-dev', 'libxmu6', 'libxmu-dev', 'libglu1-mesa-dev',
                 'libgles2-mesa-dev', 'libegl1-mesa-dev', 'libdbus-1-dev', 'libharfbuzz-dev',
                 'ccache', 'clang', 'autoconf2.13', 'libunwind-dev']
-    pkgs_dnf = ['libtool', 'gcc-c++', 'libXi-devel', 'freetype-devel','libunwind-devel',
+    pkgs_dnf = ['libtool', 'gcc-c++', 'libXi-devel', 'freetype-devel', 'libunwind-devel',
                 'mesa-libGL-devel', 'mesa-libEGL-devel', 'glib2-devel', 'libX11-devel',
                 'libXrandr-devel', 'gperf', 'fontconfig-devel', 'cabextract', 'ttmkfdir',
                 'python2', 'python2-virtualenv', 'python2-pip', 'expat-devel', 'rpm-build',
