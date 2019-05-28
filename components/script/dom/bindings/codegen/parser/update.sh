871239a3e302d4aefdbbad1b0141511dcd66afc4
sreeise <reeisesean@gmail.com>
2019-05-28 03:23:47 -0400

    Change bindings generation to make Exposed annotation aware of members/partial interfaces

--- components/script/dom/bindings/codegen/parser/update.sh
+++ components/script/dom/bindings/codegen/parser/update.sh
@@ -4,6 +4,7 @@ patch < debug.patch
 patch < callback-location.patch
 patch < union-typedef.patch
 patch < inline.patch
+patch < exposed-globals.patch
 
 wget https://hg.mozilla.org/mozilla-central/archive/tip.tar.gz/dom/bindings/parser/tests/ -O tests.tar.gz
 rm -r tests
