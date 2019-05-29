22b0af13bcadfe4cab7fc8bf59fe6d3487fc8f04
Josh Matthews <josh@joshmatthews.net>
2019-05-29 16:30:09 -0400

    Build raqote backend on CI.

--- etc/taskcluster/decision_task.py
+++ etc/taskcluster/decision_task.py
@@ -175,6 +175,7 @@ def linux_tidy_unit_docs():
             ./mach build --dev
             ./mach test-unit
             ./mach package --dev
+            ./mach build --dev --features raqote_backend
             ./mach build --dev --libsimpleservo
             ./mach build --dev --no-default-features --features default-except-unstable
             ./mach test-tidy --no-progress --self-test
