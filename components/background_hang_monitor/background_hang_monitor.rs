b8aac8b1529b7a4d1505f51f60a80b1076cdf1d3
Josh Matthews <josh@joshmatthews.net>
2019-05-30 11:13:31 -0400

    Hide windows sampler on arm64.

--- components/background_hang_monitor/background_hang_monitor.rs
+++ components/background_hang_monitor/background_hang_monitor.rs
@@ -51,7 +51,10 @@ impl BackgroundHangMonitorRegister for HangMonitorRegister {
     ) -> Box<BackgroundHangMonitor> {
         let bhm_chan = BackgroundHangMonitorChan::new(self.sender.clone(), component_id);
 
-        #[cfg(target_os = "windows")]
+        #[cfg(all(
+            target_os = "windows",
+            any(target_arch = "x86_64", target_arch = "x86")
+        ))]
         let sampler = crate::sampler_windows::WindowsSampler::new();
         #[cfg(target_os = "macos")]
         let sampler = crate::sampler_mac::MacOsSampler::new();
