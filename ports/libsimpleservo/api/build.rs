0c586c8b33e03b24b046e288ad5e15c044db257e
Paul Rouget <me@paulrouget.com>
2019-06-03 10:41:50 +0200

    Use StaticStructGenerator on Windows

--- ports/libsimpleservo/api/build.rs
+++ ports/libsimpleservo/api/build.rs
@@ -15,16 +15,19 @@ fn main() {
     // For now, we only support EGL, and only on Windows and Android.
     if target.contains("android") || target.contains("windows") {
         let mut file = File::create(&dest.join("egl_bindings.rs")).unwrap();
-        if target.contains("android") {
-            Registry::new(Api::Egl, (1, 5), Profile::Core, Fallbacks::All, [])
-                .write_bindings(gl_generator::StaticStructGenerator, &mut file)
-                .unwrap();
-        }
+        Registry::new(Api::Egl, (1, 5), Profile::Core, Fallbacks::All, [])
+            .write_bindings(gl_generator::StaticStructGenerator, &mut file)
+            .unwrap();
+
+        // Historically, Android builds have succeeded with rust-link-lib=EGL.
+        // On Windows when relying on %LIBS% to contain libEGL.lib, however,
+        // we must explicitly use rustc-link-lib=libEGL or rustc will attempt
+        // to link EGL.lib instead.
         if target.contains("windows") {
-            Registry::new(Api::Egl, (1, 5), Profile::Core, Fallbacks::All, [])
-                .write_bindings(gl_generator::StructGenerator, &mut file)
-                .unwrap();
-        };
+            println!("cargo:rustc-link-lib=libEGL");
+        } else {
+            println!("cargo:rust-link-lib=EGL");
+        }
     }
 
     if target.contains("linux") ||
