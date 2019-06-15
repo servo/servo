884b54a626213f21085f8b913e052b49f8c5fd54
Michal Mieczkowski <mieczkowski@fastmail.com>
2019-06-15 11:57:32 +0200

    Remove unneeded clipboard import.

--- ports/glutin/main2.rs
+++ ports/glutin/main2.rs
@@ -2,7 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-extern crate clipboard;
 #[macro_use]
 extern crate lazy_static;
 #[macro_use]
