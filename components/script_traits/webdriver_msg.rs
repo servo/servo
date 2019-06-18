41b0dc0326fb65e79adb3aabe8205e02b7a9db61
George Roman <george.roman.99@gmail.com>
2019-06-18 22:02:27 +0300

    Implement GetElementProperty WebDriver command

--- components/script_traits/webdriver_msg.rs
+++ components/script_traits/webdriver_msg.rs
@@ -33,6 +33,7 @@ pub enum WebDriverScriptCommand {
     GetCookie(String, IpcSender<Vec<Serde<Cookie<'static>>>>),
     GetCookies(IpcSender<Vec<Serde<Cookie<'static>>>>),
     GetElementAttribute(String, String, IpcSender<Result<Option<String>, ()>>),
+    GetElementProperty(String, String, IpcSender<Result<WebDriverJSValue, ()>>),
     GetElementCSS(String, String, IpcSender<Result<String, ()>>),
     GetElementRect(String, IpcSender<Result<Rect<f64>, ()>>),
     GetElementTagName(String, IpcSender<Result<String, ()>>),
@@ -51,7 +52,7 @@ pub enum WebDriverCookieError {
     UnableToSetCookie,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub enum WebDriverJSValue {
     Undefined,
     Null,
