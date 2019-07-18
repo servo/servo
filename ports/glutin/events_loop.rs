822c32a630603dd300a5eea9d64449c8f8c755b8
Josh Matthews <josh@joshmatthews.net>
2019-07-18 10:10:20 -0400

    tmp

--- ports/glutin/events_loop.rs
+++ ports/glutin/events_loop.rs
@@ -8,26 +8,32 @@
 use glutin;
 use servo::embedder_traits::EventLoopWaker;
 use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::rc::Rc;
 use std::cell::RefCell;
 use std::thread;
 use std::time;
 
-pub struct EventsLoop(Option<glutin::EventsLoop>);
+enum EventLoop {
+    Glutin(Option<glutin::EventsLoop>),
+    Headless(Arc<AtomicBool>),
+}
+
+pub struct EventsLoop(EventLoop);
 
 impl EventsLoop {
     // Ideally, we could use the winit event loop in both modes,
     // but on Linux, the event loop requires a X11 server.
-    #[cfg(not(target_os = "linux"))]
+    /*#[cfg(not(target_os = "linux"))]
     pub fn new(_headless: bool) -> Rc<RefCell<EventsLoop>> {
         Rc::new(RefCell::new(EventsLoop(Some(glutin::EventsLoop::new()))))
-    }
-    #[cfg(target_os = "linux")]
+    }*/
+    //#[cfg(target_os = "linux")]
     pub fn new(headless: bool) -> Rc<RefCell<EventsLoop>> {
         let events_loop = if headless {
-            None
+            EventLoop::Headless(Arc::new(AtomicBool::new(false)))
         } else {
-            Some(glutin::EventsLoop::new())
+            EventLoop::Glutin(Some(glutin::EventsLoop::new()))
         };
         Rc::new(RefCell::new(EventsLoop(events_loop)))
     }
@@ -35,33 +41,51 @@ impl EventsLoop {
 
 impl EventsLoop {
     pub fn create_event_loop_waker(&self) -> Box<dyn EventLoopWaker> {
-        if let Some(ref events_loop) = self.0 {
-            Box::new(HeadedEventLoopWaker::new(&events_loop))
-        } else {
-            Box::new(HeadlessEventLoopWaker)
+        match self.0 {
+            EventLoop::Glutin(ref events_loop) => {
+                let events_loop = events_loop
+                    .as_ref()
+                    .expect("Can't create waker for unavailable event loop.");
+                Box::new(HeadedEventLoopWaker::new(&events_loop))
+            },
+            EventLoop::Headless(ref flag) =>
+                Box::new(HeadlessEventLoopWaker(flag.clone())),
         }
     }
     pub fn as_winit(&self) -> &glutin::EventsLoop {
-        &self.0.as_ref().expect("Can't access winit event loop while using the fake headless event loop")
+        match self.0 {
+            EventLoop::Glutin(Some(ref event_loop)) => event_loop,
+            EventLoop::Glutin(None) | EventLoop::Headless(..) =>
+                panic!("Can't access winit event loop while using the fake headless event loop"),
+        }
     }
     pub fn take(&mut self) -> Option<glutin::EventsLoop> {
-        self.0.take()
+        match self.0 {
+            EventLoop::Glutin(ref mut event_loop) => event_loop.take(),
+            EventLoop::Headless(..) => None,
+        }
     }
     pub fn poll_events<F>(&mut self, callback: F) where F: FnMut(glutin::Event) {
-        if let Some(ref mut events_loop) = self.0 {
+        if let EventLoop::Glutin(Some(ref mut events_loop)) = self.0 {
             events_loop.poll_events(callback);
         } else {
             self.sleep();
         }
     }
     pub fn run_forever<F>(&mut self, mut callback: F) where F: FnMut(glutin::Event) -> glutin::ControlFlow {
-        if let Some(ref mut events_loop) = self.0 {
-            events_loop.run_forever(callback);
-        } else {
-            loop {
-                self.sleep();
-                if callback(glutin::Event::Awakened) == glutin::ControlFlow::Break {
-                    break;
+        match self.0 {
+            EventLoop::Glutin(ref mut events_loop) => {
+                let events_loop = events_loop
+                    .as_mut()
+                    .expect("Can't run an unavailable event loop.");
+                events_loop.run_forever(callback);
+            }
+            EventLoop::Headless(ref flag) => {
+                while !flag.swap(false, Ordering::SeqCst) {
+                    self.sleep();
+                    if callback(glutin::Event::Awakened) == glutin::ControlFlow::Break {
+                        break;
+                    }
                 }
             }
         }
@@ -94,8 +118,10 @@ impl EventLoopWaker for HeadedEventLoopWaker {
     }
 }
 
-struct HeadlessEventLoopWaker;
+struct HeadlessEventLoopWaker(Arc<AtomicBool>);
 impl EventLoopWaker for HeadlessEventLoopWaker {
-    fn wake(&self) {}
-    fn clone(&self) -> Box<dyn EventLoopWaker + Send> { Box::new(HeadlessEventLoopWaker) }
+    fn wake(&self) { self.0.store(true, Ordering::SeqCst); }
+    fn clone(&self) -> Box<dyn EventLoopWaker + Send> {
+        Box::new(HeadlessEventLoopWaker(self.0.clone()))
+    }
 }
