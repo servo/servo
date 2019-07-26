e9a7544e7e4daedba0db31dc1a9432cf7fdb38da
Josh Matthews <josh@joshmatthews.net>
2019-07-26 13:27:09 -0400

    Load GStreamer plugins on Windows arm64.

--- components/servo/lib.rs
+++ components/servo/lib.rs
@@ -126,7 +126,8 @@ pub use servo_url as url;
 
 #[cfg(any(
     all(target_os = "android", target_arch = "arm"),
-    target_arch = "x86_64"
+    target_arch = "x86_64",
+    all(target_os = "windows", target_arch = "aarch64"),
 ))]
 mod media_platform {
     use super::ServoMedia;
@@ -207,7 +208,8 @@ mod media_platform {
 
 #[cfg(not(any(
     all(target_os = "android", target_arch = "arm"),
-    target_arch = "x86_64"
+    target_arch = "x86_64",
+    all(target_os = "windows", target_arch = "aarch64"),
 )))]
 mod media_platform {
     use super::ServoMedia;
