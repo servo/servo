servo/servo

commit 72e28dffcdf560543d312abc0ec4ff76b7c7dfaf

Do not require setting PKG_CONFIG_PATH on Windows

python/servo/command_base.py
@@ -239,6 +239,25 @@ def set_osmesa_env(bin_path, env):
     return env
 
 
+def gstreamer_root(target, env, topdir=None):
+    if is_windows:
+        arch = {
+            "x86_64": "X86_64",
+            "x86": "X86",
+            "aarch64": "ARM64",
+        }
+        gst_x64 = arch[target.split('-')[0]]
+        gst_default_path = path.join("C:\\gstreamer\\1.0", gst_x64)
+        gst_env = "GSTREAMER_1_0_ROOT_" + gst_x64
+        if env.get(gst_env) is not None:
+            return env.get(gst_env)
+        elif os.path.exists(path.join(gst_default_path, "bin", "ffi-7.dll")):
+            return gst_default_path
+    elif sys.platform == "linux2":
+        return path.join(topdir, "support", "linux", "gstreamer", "gst")
+    return None
+
+
 class BuildNotFound(Exception):
     def __init__(self, message):
         self.message = message
@@ -364,9 +383,6 @@ class CommandBase(object):
         apk_name = "servoapp.apk"
         build_type = "release" if release else "debug"
         return path.join(base_path, build_type, apk_name)
-
-    def get_gstreamer_path(self):
-        return path.join(self.context.topdir, "support", "linux", "gstreamer", "gst")
 
     def get_binary_path(self, release, dev, target=None, android=False, magicleap=False, simpleservo=False):
         # TODO(autrilla): this function could still use work - it shouldn't
@@ -542,7 +558,7 @@ class CommandBase(object):
 
         return self.get_executable(destination_folder)
 
-    def needs_gstreamer_env(self, target):
+    def needs_gstreamer_env(self, target, env):
         try:
             if check_gstreamer_lib():
                 return False
@@ -554,8 +570,8 @@ class CommandBase(object):
         if "x86_64" not in effective_target or "android" in effective_target:
             # We don't build gstreamer for non-x86_64 / android yet
             return False
-        if sys.platform == "linux2":
-            if path.isdir(self.get_gstreamer_path()):
+        if sys.platform == "linux2" or is_windows:
+            if path.isdir(gstreamer_root(effective_target, env, self.get_top_dir)):
                 return True
             else:
                 raise Exception("Your system's gstreamer libraries are out of date \
@@ -569,8 +585,10 @@ install them, let us know by filing a bug!")
     def set_run_env(self, android=False):
         """Some commands, like test-wpt, don't use a full build env,
            but may still need dynamic search paths. This command sets that up"""
-        if not android and self.needs_gstreamer_env(None):
-            gstpath = self.get_gstreamer_path()
+        if not android and self.needs_gstreamer_env(None, os.environ):
+            gstpath = gstreamer_root(host_triple(), os.environ, self.get_top_dir)
+            if gstpath is None:
+                return
             os.environ["LD_LIBRARY_PATH"] = path.join(gstpath, "lib")
             os.environ["GST_PLUGIN_SYSTEM_PATH"] = path.join(gstpath, "lib", "gstreamer-1.0")
             os.environ["PKG_CONFIG_PATH"] = path.join(gstpath, "lib", "pkgconfig")
@@ -624,8 +642,8 @@ install them, let us know by filing a bug!")
             # Always build harfbuzz from source
             env["HARFBUZZ_SYS_NO_PKG_CONFIG"] = "true"
 
-        if self.needs_gstreamer_env(target):
-            gstpath = self.get_gstreamer_path()
+        if self.needs_gstreamer_env(target or host_triple(), env):
+            gstpath = gstreamer_root(target or host_triple(), env, self.get_top_dir())
             extra_path += [path.join(gstpath, "bin")]
             libpath = path.join(gstpath, "lib")
             # we append in the reverse order so that system gstreamer libraries

python/servo/build_commands.py
@@ -30,7 +30,7 @@ from mach.decorators import (
 from mach.registrar import Registrar
 
 from mach_bootstrap import _get_exec_path
-from servo.command_base import CommandBase, cd, call, check_call, BIN_SUFFIX, append_to_path_env
+from servo.command_base import CommandBase, cd, call, check_call, BIN_SUFFIX, append_to_path_env, gstreamer_root
 from servo.util import host_triple
 
 
@@ -774,23 +774,6 @@ class MachCommands(CommandBase):
             return False
 
         return True
-
-
-def gstreamer_root(target, env):
-    arch = {
-        "x86_64": "X86_64",
-        "x86": "X86",
-        "aarch64": "ARM64",
-    }
-    gst_x64 = arch[target.split('-')[0]]
-    gst_default_path = path.join("C:\\gstreamer\\1.0", gst_x64)
-    gst_env = "GSTREAMER_1_0_ROOT_" + gst_x64
-    if env.get(gst_env) is not None:
-        return env.get(gst_env)
-    elif os.path.exists(path.join(gst_default_path, "bin", "ffi-7.dll")):
-        return gst_default_path
-    else:
-        return None
 
 
 def package_gstreamer_dlls(env, servo_exe_dir, target, uwp):

commit 1291d52f3f36a8563170c59d7d3a1c55b32ba1b7

Call gstreamer_root with top_dir string, not func.
Commit 72e28dffcdf560543d312abc0ec4ff76b7c7dfaf introduced a build
regression on Linux when resolving the in-tree gstreamer libs.

The error:

------------------------------------
$ ./mach build --release
...
The details of the failure are as follows:

AttributeError: 'function' object has no attribute 'endswith'

  File "/data/joelm/personal/UTA/dissertation/servo/servo-master.git/python/servo/build_commands.py", line 237, in build
    env = self.build_env(target=target, is_build=True)
  File "/data/joelm/personal/UTA/dissertation/servo/servo-master.git/python/servo/command_base.py", line 646, in build_env
    if self.needs_gstreamer_env(target or host_triple(), env):
  File "/data/joelm/personal/UTA/dissertation/servo/servo-master.git/python/servo/command_base.py", line 574, in needs_gstreamer_env
    if path.isdir(gstreamer_root(effective_target, env, self.get_top_dir)):
  File "/data/joelm/personal/UTA/dissertation/servo/servo-master.git/python/servo/command_base.py", line 257, in gstreamer_root
    return path.join(topdir, "support", "linux", "gstreamer", "gst")
  File "/usr/lib/python2.7/posixpath.py", line 70, in join
    elif path == '' or path.endswith('/'):
------------------------------------

The problem is that get_top_dir is being passed as a function to
gstreamer_root in a couple of places instead of being invoked first.

python/servo/command_base.py
@@ -571,7 +571,7 @@ class CommandBase(object):
             # We don't build gstreamer for non-x86_64 / android yet
             return False
         if sys.platform == "linux2" or is_windows():
-            if path.isdir(gstreamer_root(effective_target, env, self.get_top_dir)):
+            if path.isdir(gstreamer_root(effective_target, env, self.get_top_dir())):
                 return True
             else:
                 raise Exception("Your system's gstreamer libraries are out of date \
@@ -586,7 +586,7 @@ install them, let us know by filing a bug!")
         """Some commands, like test-wpt, don't use a full build env,
            but may still need dynamic search paths. This command sets that up"""
         if not android and self.needs_gstreamer_env(None, os.environ):
-            gstpath = gstreamer_root(host_triple(), os.environ, self.get_top_dir)
+            gstpath = gstreamer_root(host_triple(), os.environ, self.get_top_dir())
             if gstpath is None:
                 return
             os.environ["LD_LIBRARY_PATH"] = path.join(gstpath, "lib")