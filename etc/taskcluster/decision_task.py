c9054b34bfc8aedc7992aa741572de6f6a3082fd
Josh Matthews <josh@joshmatthews.net>
2019-09-17 12:07:08 -0400

    reduce work

--- etc/taskcluster/decision_task.py
+++ etc/taskcluster/decision_task.py
@@ -608,7 +608,7 @@ def macos_wpt():
             )
         )
     wpt_chunks("macOS x64", macos_run_task, build_task, repo_dir="repo",
-               total_chunks=6, processes=4, chunks=[2,4])
+               total_chunks=6, processes=4, chunks=[4])
 
 
 def wpt_chunks(platform, make_chunk_task, build_task, total_chunks, processes,
