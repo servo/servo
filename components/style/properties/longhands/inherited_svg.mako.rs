0c44382348ee78c823a3c01eaa722f7556e3a25a
jeffin143 <jeffinsam@karunya.edu.in>
2019-11-21 06:07:30 +0000

    style: convert NS_STYLE_STROKE_LINECAP_* to an enum class in nsStyleConsts.h
    Differential Revision: https://phabricator.services.mozilla.com/D53908

--- components/style/properties/longhands/inherited_svg.mako.rs
+++ components/style/properties/longhands/inherited_svg.mako.rs
@@ -108,6 +108,7 @@ ${helpers.single_keyword(
     engines="gecko",
     animation_value_type="discrete",
     spec="https://www.w3.org/TR/SVG11/painting.html#StrokeLinecapProperty",
+    gecko_enum_prefix = "StyleStrokeLinecap",
 )}
 
 ${helpers.single_keyword(
