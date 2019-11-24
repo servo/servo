f1b34aa9950a0d240bee76aa13ad729429a0bcd7
Patrick Shaughnessy <pshaughn@comcast.net>
2019-11-23 22:06:39 -0500

    llvm-dev is required on Debian

--- python/servo/bootstrap.py
+++ python/servo/bootstrap.py
@@ -117,7 +117,7 @@ def linux(context, force=False):
                 'libbz2-dev', 'liblzma-dev',
                 'libosmesa6-dev', 'libxmu6', 'libxmu-dev', 'libglu1-mesa-dev',
                 'libgles2-mesa-dev', 'libegl1-mesa-dev', 'libdbus-1-dev', 'libharfbuzz-dev',
-                'ccache', 'clang', 'autoconf2.13', 'libunwind-dev']
+                'ccache', 'clang', 'autoconf2.13', 'libunwind-dev', 'llvm-dev']
     pkgs_dnf = ['libtool', 'gcc-c++', 'libXi-devel', 'freetype-devel', 'libunwind-devel',
                 'mesa-libGL-devel', 'mesa-libEGL-devel', 'glib2-devel', 'libX11-devel',
                 'libXrandr-devel', 'gperf', 'fontconfig-devel', 'cabextract', 'ttmkfdir',
