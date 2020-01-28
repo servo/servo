fc13bd8b2f8f7b06aea928e3e5ab56a4b3a2584a
Patrick Shaughnessy <pshaughn@comcast.net>
2020-01-28 12:47:18 -0500

    Cookie header values can be utf8

--- components/net/http_loader.rs
+++ components/net/http_loader.rs
@@ -272,7 +272,7 @@ fn set_cookies_from_headers(
     cookie_jar: &RwLock<CookieStorage>,
 ) {
     for cookie in headers.get_all(header::SET_COOKIE) {
-        if let Ok(cookie_str) = cookie.to_str() {
+        if let Ok(cookie_str) = std::str::from_utf8(cookie.as_bytes()) {
             set_cookie_for_url(&cookie_jar, &url, &cookie_str);
         }
     }
