d83820061ec2df8b60235e430f2ff6b5f7eb2abd
Paul Rouget <me@paulrouget.com>
2020-02-10 06:44:30 +0100

    enable devtools for hololens

--- support/hololens/ServoApp/ServoControl/Servo.cpp
+++ support/hololens/ServoApp/ServoControl/Servo.cpp
@@ -92,7 +92,7 @@ Servo::Servo(hstring url, hstring args, GLsizei width, GLsizei height,
     : mWindowHeight(height), mWindowWidth(width), mDelegate(aDelegate) {
 
   capi::CInitOptions o;
-  hstring defaultPrefs = L" --pref dom.webxr.enabled";
+  hstring defaultPrefs = L" --pref dom.webxr.enabled  --devtools=6000";
   o.args = *hstring2char(args + defaultPrefs);
   o.url = *hstring2char(url);
   o.width = mWindowWidth;
