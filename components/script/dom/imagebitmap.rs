eac43023fd9df482cffbc0a2dae459cc6c736b15
SasiDharKM <sasidhar.km97@gmail.com>
2020-03-21 23:57:11 -0400

    Reformatting and minor error fixes

--- components/script/dom/imagebitmap.rs
+++ components/script/dom/imagebitmap.rs
@@ -4,74 +4,78 @@
 
 // the following are the statements
 use crate::dom::bindings::codegen::Bindings::CanvasGradientBinding;
-use crate::dom::bindings::error::Error;
+use crate::dom::bindings::error::{Error, Fallible};
 use crate::dom::globalscope::GlobalScope;
-use crate::dom::bindings::cell::DOMRefCell;
-use crate::dom::bindings::ImageBitMapBinding::{ImageBitMapMethods, ImageBitMapInit, Wrap as ImageBitMapWrap};
-// for the datatype
+use crate::dom::bindings::cell::DomRefCell;
+use crate::dom::bindings::root::DomRoot;
+use crate::dom::bindings::codegen::Bindings::ImageBitmapBinding;
+use crate::dom::bindings::codegen::Bindings::ImageBitmapBinding::ImageBitmapMethods;
 use crate::dom::bindings::codegen::Bindings::CanvasRenderingContext2DBinding::CanvasImageSource;
 use crate::dom::imagedata::ImageData;
 use crate::dom::blob::Blob;
-//use crate::dom::bindings::serializable::Serializable;
-//use crate::dom::bindings::transferable::Transferable;
-// There should be an implmentation under this like in blob.rs
-//use script_traits::serializable::Serializable;
-//use script_traits::transferable;
-use crate dom::domexception::DOMException;
+
+
 use crate::dom::bindings::callback::ExceptionHandling;
 // for the reflector to be used
 use crate::dom::bindings::reflector::{Reflector, reflect_dom_object};
 //not sure if these two work
 use crate::dom::bindings::codegen::Bindings::CanvasPatternBinding; //--don't know why I did this!
-//use crate::dom::bindings::codegen::Bindings::ImageBitMapBinding;
-// in the serializable with the imagebitmap_impls
-//use crate::dom::bindings::structuredclone::StructuredDataHolder;
-// dom_struct things
-// use crate::dom::bindings::ImageOrientation;
-// use crate::dom::bindings::PremultiplyAlpha;
-// use crate::dom::bindings::ColorSpaceConversion;
-// use crate::dom::bindings::ResizeQuality;
-// use dom_struct::dom_struct;
+use js::jsapi::JSObject;
 use dom_struct::dom_struct;
 use std::vec::Vec;
 
 //as mentioned in bluetoothuuid.rs
-pub type ImageBitMapSource = CanvasImageSourceorImageDataorBlob;
+//pub type ImageBitmapSource = CanvasImageSourceorImageDataorBlob;
 
 #[dom_struct]
-pub struct ImageBitMap{
-	reflector_:Reflector,
-	width: u64,
-	height: u64,
-	ibm_vector: DOMRefCell<Vec<u8>>,
+pub struct ImageBitmap{
+	reflector_: Reflector,
+	width: u32,
+	height: u32,
+	ibm_vector: DomRefCell<Vec<u8>>,
 }
 
 //#[allow (dead_code)]
-impl ImageBitMap {
-	pub fn new_inherited(width_arg: u64, height_arg: u64) -> ImageBitMap {
-		ImageBitMap{
+impl ImageBitmap {
+	#[allow(unsafe_code)]
+	pub fn new_inherited(
+		width_arg: u32,
+		height_arg: u32,
+	) -> ImageBitmap {
+		ImageBitmap{
 			reflector_: Reflector::new(),
 			width: width_arg,
 			height: height_arg,
-			ibm_vector: DOMRefCell::new(vec![]),
+			ibm_vector: DomRefCell::new(vec![]),
 		}
 	}
 
-	pub fn new (global: &GlobalScope, width: u64, height: u64) -> DOMRoot<ImageBitMap> {
+	pub fn new (global: &GlobalScope,
+		width: u32,
+		height: u32
+	) -> Fallible<DomRoot<ImageBitmap>> {
+
+		let imagebitmap = Box::new(
+			ImageBitmap::new_inherited(width, height));
+
 		//reflect_dom_object(box ImageBitMap::new_inherited(width, height), global, ImageBitMapBinding::Wrap);
-		reflect_dom_object(box ImageBitMap::new_inherited(width, height), global, ImageBitMapWrap);
+		Ok(reflect_dom_object(
+			imagebitmap,
+			global,
+			ImageBitmapBinding::Wrap,
+		))
 	}
 }
 
 // uncomment when working on it
-impl ImageBitMapMethods for ImageBitMap{
-	fn Height(&self) -> u64 {
+impl ImageBitmapMethods for ImageBitmap{
+	fn Height(&self) -> u32 {
 		//to do: add a condition for checking detached internal slot
-        return self.height.get();
+        self.height
     }
 
-	fn Width(&self) -> u64 {
+	fn Width(&self) -> u32 {
 		//to do: add a condition to check detached internal slot
-        return self.width.get();
+        self.width
     }
 }
