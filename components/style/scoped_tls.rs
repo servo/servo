e7cb736796e43bd564fd3994da8915f7d06f3768
Anthony Ramine <nox@nox.paris>
2020-04-04 22:26:58 +0200

    Replace ScopedTLS::unsafe_get by ScopedTLS::into_slots
    We only ever look at the slots after we are done with the thread pool,
    so we don't need to expose any unsafety to inspect the slots.

--- components/style/scoped_tls.rs
+++ components/style/scoped_tls.rs
@@ -71,9 +71,8 @@ impl<'scope, T: Send> ScopedTLS<'scope, T> {
         RefMut::map(opt, |x| x.as_mut().unwrap())
     }
 
-    /// Unsafe access to the slots. This can be used to access the TLS when
-    /// the caller knows that the pool does not have access to the TLS.
-    pub unsafe fn unsafe_get(&self) -> &[RefCell<Option<T>>] {
-        &self.slots
+    /// Returns the slots, consuming the scope.
+    pub fn into_slots(self) -> Box<[RefCell<Option<T>>]> {
+        self.slots
     }
 }
