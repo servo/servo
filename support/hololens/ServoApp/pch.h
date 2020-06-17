bd8c7d6f4d0c84f901bb0822cb48deb38b86037f
Paul Rouget <me@paulrouget.com>
2020-06-17 11:36:04 +0200

    [UWP] Basic console panel

--- support/hololens/ServoApp/pch.h
+++ support/hololens/ServoApp/pch.h
@@ -14,6 +14,7 @@
 #include <d3d11_4.h>
 #include <DirectXColors.h>
 #include <dwrite_2.h>
+#include <sstream>
 #include <Windows.Graphics.Directx.Direct3D11.Interop.h>
 #include <windows.h>
 #include <WindowsNumerics.h>
@@ -36,6 +37,7 @@
 #include <winrt/Windows.Gaming.Input.h>
 #include <winrt/Windows.Graphics.Display.h>
 #include <winrt/Windows.Graphics.Holographic.h>
+#include <winrt/Windows.Networking.Sockets.h>
 #include <winrt/Windows.Perception.People.h>
 #include <winrt/Windows.Perception.Spatial.h>
 #include <winrt/Windows.Storage.h>
@@ -49,14 +51,19 @@
 #include <winrt/Windows.UI.Xaml.Controls.Primitives.h>
 #include <winrt/Windows.UI.Xaml.Data.h>
 #include <winrt/Windows.UI.Xaml.h>
+#include <winrt/Windows.UI.Xaml.Documents.h>
 #include <winrt/Windows.UI.Xaml.Input.h>
 #include <winrt/Windows.UI.Xaml.Interop.h>
 #include <winrt/Windows.UI.Xaml.Markup.h>
 #include <winrt/Windows.UI.Xaml.Navigation.h>
 #include <winrt/Windows.UI.Notifications.h>
+#include <winrt/Windows.UI.Text.h>
 #include <winrt/Windows.Data.Xml.Dom.h>
+#include <winrt/Windows.Storage.Streams.h>
+#include <winrt/Windows.Data.Json.h>
 
-#include "winrt/Microsoft.UI.Xaml.Automation.Peers.h"
-#include "winrt/Microsoft.UI.Xaml.Controls.Primitives.h"
-#include "winrt/Microsoft.UI.Xaml.Media.h"
-#include "winrt/Microsoft.UI.Xaml.XamlTypeInfo.h"
+#include <winrt/Microsoft.UI.Xaml.Automation.Peers.h>
+#include <winrt/Microsoft.UI.Xaml.Controls.h>
+#include <winrt/Microsoft.UI.Xaml.Controls.Primitives.h>
+#include <winrt/Microsoft.UI.Xaml.Media.h>
+#include <winrt/Microsoft.UI.Xaml.XamlTypeInfo.h>
