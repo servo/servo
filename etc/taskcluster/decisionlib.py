0badd7d4ac28593f156d7fbf07baf30ff80d7b2f
Josh Matthews <josh@joshmatthews.net>
2021-02-02 10:05:56 -0500

    add python3 to path

--- etc/taskcluster/decisionlib.py
+++ etc/taskcluster/decisionlib.py
@@ -653,6 +653,7 @@ class WindowsGenericWorkerTask(GenericWorkerTask):
             sha256="6de14c9223226cf0cd8c965ecb08c51d62c770171a256991b4fddc25188cfa8e",
             path="python3",
         ) \
+        .with_path_from_homedir("python3") \
         .with_early_script("""
             python -m ensurepip
             pip install virtualenv==20.2.1
