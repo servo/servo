655bb48617ccb105048ebb3489bd71c3a2feaa87
Josh Matthews <josh@joshmatthews.net>
2021-02-04 03:28:04 -0500

    adjust CI python for mac

--- etc/taskcluster/decision_task.py
+++ etc/taskcluster/decision_task.py
@@ -121,7 +121,7 @@ linux_build_env = {
     "SCCACHE_MAX_FRAME_LENGTH": str(100 * 1024 * 1024),  # 100 MiB
 }
 macos_build_env = {
-    "PYTHON": "${HOME}/Library/Python/2.7/bin"
+    "PYTHON": "${HOME}/Library/Python/2.7/bin/python"
 }
 windows_build_env = {
     "x86_64": {
