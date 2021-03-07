b1d72f65eedb6f27028d2162796093ecf674817a
delosrogers <delosrogers@gmail.com>
2021-03-07 13:48:32 -0800

    Use swap() instead of load()/store() in get_and_reset_text_shaping_performance_counter()
    Signed-off-by: Mattias de los Rios Rogers <delosrogers@gmail.com>

--- components/gfx/font.rs
+++ components/gfx/font.rs
@@ -522,8 +522,7 @@ impl RunMetrics {
 }
 
 pub fn get_and_reset_text_shaping_performance_counter() -> usize {
-    let value = TEXT_SHAPING_PERFORMANCE_COUNTER.load(Ordering::SeqCst);
-    TEXT_SHAPING_PERFORMANCE_COUNTER.store(0, Ordering::SeqCst);
+    let value = TEXT_SHAPING_PERFORMANCE_COUNTER.swap(0,Ordering::SeqCst);
     value
 }
 
