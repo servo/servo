033ee2d9d15554fb5b235a2704a50185fea33898
keiya01 <keiya.s.0210@gmail.com>
2021-04-21 14:42:37 +0900

    refactor: add droppable inner struct to WebGLQuery

--- components/script/dom/webglquery.rs
+++ components/script/dom/webglquery.rs
@@ -6,34 +6,62 @@ use crate::dom::bindings::codegen::Bindings::WebGL2RenderingContextBinding::WebG
 use crate::dom::bindings::inheritance::Castable;
 use crate::dom::bindings::refcounted::Trusted;
 use crate::dom::bindings::reflector::{reflect_dom_object, DomObject};
-use crate::dom::bindings::root::DomRoot;
+use crate::dom::bindings::root::{DomRoot};
 use crate::dom::webglobject::WebGLObject;
-use crate::dom::webglrenderingcontext::{Operation, WebGLRenderingContext};
+use crate::dom::webglrenderingcontext::{Operation, WebGLRenderingContext, WebGLMessageSender, stub_webgl_backtrace};
 use crate::task_source::TaskSource;
 use canvas_traits::webgl::WebGLError::*;
 use canvas_traits::webgl::{webgl_channel, WebGLCommand, WebGLQueryId};
 use dom_struct::dom_struct;
 use std::cell::Cell;
 
+struct DroppableField {
+    sender: WebGLMessageSender,
+    gl_id: WebGLQueryId,
+    marked_for_deletion: Cell<bool>,
+}
+
+impl DroppableField {
+    fn delete(&self, operation_fallibility: Operation) {
+        if !self.marked_for_deletion.get() {
+            self.marked_for_deletion.set(true);
+
+            let command = WebGLCommand::DeleteQuery(self.gl_id);
+            match operation_fallibility {
+                Operation::Fallible => self.sender.send(command, stub_webgl_backtrace()),
+                Operation::Infallible => self.sender.send(command, stub_webgl_backtrace()).unwrap(),
+            }
+        }
+    }
+}
+
+impl Drop for DroppableField {
+    fn drop(&mut self) {
+        self.delete(Operation::Fallible);
+    }
+}
+
 #[dom_struct]
 pub struct WebGLQuery {
     webgl_object: WebGLObject,
-    gl_id: WebGLQueryId,
     gl_target: Cell<Option<u32>>,
-    marked_for_deletion: Cell<bool>,
     query_result_available: Cell<Option<u32>>,
     query_result: Cell<u32>,
+    droppable_field: DroppableField,
 }
 
 impl WebGLQuery {
     fn new_inherited(context: &WebGLRenderingContext, id: WebGLQueryId) -> Self {
         Self {
             webgl_object: WebGLObject::new_inherited(context),
-            gl_id: id,
             gl_target: Cell::new(None),
-            marked_for_deletion: Cell::new(false),
             query_result_available: Cell::new(None),
             query_result: Cell::new(0),
+            droppable_field: DroppableField {
+                sender: context.webgl_sender(),
+                gl_id: id,
+                marked_for_deletion: Cell::new(false),
+            },
         }
     }
 
@@ -53,7 +81,7 @@ impl WebGLQuery {
         context: &WebGLRenderingContext,
         target: u32,
     ) -> Result<(), canvas_traits::webgl::WebGLError> {
-        if self.marked_for_deletion.get() {
+        if self.droppable_field.marked_for_deletion.get() {
             return Err(InvalidOperation);
         }
         if let Some(current_target) = self.gl_target.get() {
@@ -69,7 +97,7 @@ impl WebGLQuery {
         }
         self.gl_target.set(Some(target));
 
-        context.send_command(WebGLCommand::BeginQuery(target, self.gl_id));
+        context.send_command(WebGLCommand::BeginQuery(target, self.droppable_field.gl_id));
         Ok(())
     }
 
@@ -78,7 +106,7 @@ impl WebGLQuery {
         context: &WebGLRenderingContext,
         target: u32,
     ) -> Result<(), canvas_traits::webgl::WebGLError> {
-        if self.marked_for_deletion.get() {
+        if self.droppable_field.marked_for_deletion.get() {
             return Err(InvalidOperation);
         }
         if let Some(current_target) = self.gl_target.get() {
@@ -97,20 +125,11 @@ impl WebGLQuery {
     }
 
     pub fn delete(&self, operation_fallibility: Operation) {
-        if !self.marked_for_deletion.get() {
-            self.marked_for_deletion.set(true);
-
-            let context = self.upcast::<WebGLObject>().context();
-            let command = WebGLCommand::DeleteQuery(self.gl_id);
-            match operation_fallibility {
-                Operation::Fallible => context.send_command_ignored(command),
-                Operation::Infallible => context.send_command(command),
-            }
-        }
+        self.droppable_field.delete(operation_fallibility);
     }
 
     pub fn is_valid(&self) -> bool {
-        !self.marked_for_deletion.get() && self.target().is_some()
+        !self.droppable_field.marked_for_deletion.get() && self.target().is_some()
     }
 
     pub fn target(&self) -> Option<u32> {
@@ -121,7 +140,7 @@ impl WebGLQuery {
         let (sender, receiver) = webgl_channel().unwrap();
         context.send_command(WebGLCommand::GetQueryState(
             sender,
-            self.gl_id,
+            self.droppable_field.gl_id,
             constants::QUERY_RESULT_AVAILABLE,
         ));
         let is_available = receiver.recv().unwrap();
@@ -131,11 +150,13 @@ impl WebGLQuery {
         }
 
         let (sender, receiver) = webgl_channel().unwrap();
-        context.send_command(WebGLCommand::GetQueryState(
-            sender,
-            self.gl_id,
-            constants::QUERY_RESULT,
-        ));
+        self.upcast::<WebGLObject>()
+            .context()
+            .send_command(WebGLCommand::GetQueryState(
+                sender,
+                self.droppable_field.gl_id,
+                constants::QUERY_RESULT,
+            ));
 
         self.query_result.set(receiver.recv().unwrap());
         self.query_result_available.set(Some(is_available));
@@ -181,11 +202,5 @@ impl WebGLQuery {
             constants::QUERY_RESULT_AVAILABLE => Ok(self.query_result_available.get().unwrap()),
             _ => unreachable!(),
         }
-    }
-}
-
-impl Drop for WebGLQuery {
-    fn drop(&mut self) {
-        self.delete(Operation::Fallible);
     }
 }
