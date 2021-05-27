de2266874846f2dc299335d20bf8cfcb3707bf67
Josh Matthews <josh@joshmatthews.net>
2021-05-27 00:07:13 -0400

    Disable all http_loader unit tests on windows.

--- components/net/tests/http_loader.rs
+++ components/net/tests/http_loader.rs
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+#![cfg(not(target_os = "windows"))]
+
 use crate::fetch;
 use crate::fetch_with_context;
 use crate::make_server;
