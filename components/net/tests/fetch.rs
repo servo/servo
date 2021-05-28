8583e505c8a69e4fac5537d6cf80b036d80a6597
Josh Matthews <josh@joshmatthews.net>
2021-05-27 21:48:31 -0400

    Don't error on unused imports.

--- components/net/tests/fetch.rs
+++ components/net/tests/fetch.rs
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+#![allow(unused_imports)]
+
 use crate::fetch_with_context;
 use crate::fetch_with_cors_cache;
 #[cfg(not(target_os = "windows"))]
