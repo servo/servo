d3b04d65aa15e7df39eb4d335832f25f32ef50d0
5225225 <5225225@mailbox.org>
2022-01-09 16:15:29 +0000

    Scan for resources directory more smartly

--- ports/winit/resources.rs
+++ ports/winit/resources.rs
@@ -38,48 +38,70 @@ pub fn init() {
     resources::set(Box::new(ResourceReader));
 }
 
-fn resources_dir_path() -> io::Result<PathBuf> {
+fn resources_dir_path() -> io::Result<Option<PathBuf>> {
     // This needs to be called before the process is sandboxed
     // as we only give permission to read inside the resources directory,
     // not the permissions the "search" for the resources directory.
     let mut dir = CMD_RESOURCE_DIR.lock().unwrap();
     if let Some(ref path) = *dir {
-        return Ok(PathBuf::from(path));
+        return Ok(Some(PathBuf::from(path)));
     }
 
-    // FIXME: Find a way to not rely on the executable being
-    // under `<servo source>[/$target_triple]/target/debug`
-    // or `<servo source>[/$target_triple]/target/release`.
-    let mut path = env::current_exe()?;
+    let mut current_env_path = env::current_exe()?;
     // Follow symlink
+    current_env_path = current_env_path.canonicalize()?;
+
+    if let Some(path) = resources_dir_scan(current_env_path)? {
+        *dir = Some(path.to_str().unwrap().to_owned());
+        Ok(Some(path))
+    } else if let Some(path) = resources_dir_scan(std::env::current_dir()?)? {
+        *dir = Some(path.to_str().unwrap().to_owned());
+        Ok(Some(path))
+    } else {
+        Ok(None)
+    }
+}
+
+fn resources_dir_scan(mut path: PathBuf) -> io::Result<Option<PathBuf>> {
     path = path.canonicalize()?;
+
+    path.push("resources");
+    if path.is_dir() {
+        return Ok(Some(path));
+    }
 
     while path.pop() {
         path.push("resources");
         if path.is_dir() {
-            break;
+            return Ok(Some(path));
         }
         path.pop();
         // Check for Resources on mac when using a case sensitive filesystem.
         path.push("Resources");
         if path.is_dir() {
-            break;
+            return Ok(Some(path));
         }
         path.pop();
     }
-    *dir = Some(path.to_str().unwrap().to_owned());
-    Ok(path)
+
+    Ok(None)
 }
 
 impl resources::ResourceReaderMethods for ResourceReader {
     fn read(&self, file: Resource) -> Vec<u8> {
         let file = filename(file);
-        let mut path = resources_dir_path().expect("Can't find resources directory");
+
+        let mut path = resources_dir_path()
+            .expect("Error when searching for resources directory")
+            .expect("Can't find resources directory");
+
         path.push(file);
         fs::read(path).expect("Can't read file")
     }
     fn sandbox_access_files_dirs(&self) -> Vec<PathBuf> {
-        vec![resources_dir_path().expect("Can't find resources directory")]
+        vec![resources_dir_path()
+             .expect("Error when searching for resources directory")
+             .expect("Can't find resources directory")]
     }
     fn sandbox_access_files(&self) -> Vec<PathBuf> {
         vec![]
