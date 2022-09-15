e78d8844cded1d99790f6242cebd5e4c92581d5c
Ziran Sun <zsun@igalia.com>
2022-09-15 13:37:16 +0000

    style: Rename initial value of `container-type' from 'none' to 'normal'
    Differential Revision: https://phabricator.services.mozilla.com/D157098

--- components/style/properties/longhands/box.mako.rs
+++ components/style/properties/longhands/box.mako.rs
@@ -448,7 +448,7 @@ ${helpers.predefined_type(
 ${helpers.predefined_type(
     "container-type",
     "ContainerType",
-    "computed::ContainerType::NONE",
+    "computed::ContainerType::NORMAL",
     engines="gecko servo",
     animation_value_type="none",
     gecko_pref="layout.css.container-queries.enabled",
