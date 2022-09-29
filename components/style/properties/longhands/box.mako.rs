bbe0b8b890bf69d40a7daed10fa2e3b1775b7c13
Ting-Yu Lin <tlin@mozilla.com>
2022-09-29 03:40:56 +0000

    style: More rename and clean up related to StyleClear
    * Rename `mBreakType` to `mFloatClearType` in nsLineBox and nsReflowStatus and
      the methods around it.
    * Rename `mBreakType` to `mClear` in nsStyleDisplay.
    * Many other method parameters or local variables rename such as from
      `aBreakType` to `aClearType`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D158276

--- components/style/properties/longhands/box.mako.rs
+++ components/style/properties/longhands/box.mako.rs
@@ -79,7 +79,6 @@ ${helpers.predefined_type(
     "computed::Clear::None",
     engines="gecko servo",
     animation_value_type="discrete",
-    gecko_ffi_name="mBreakType",
     spec="https://drafts.csswg.org/css2/#propdef-clear",
     servo_restyle_damage="rebuild_and_reflow",
 )}
