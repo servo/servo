a5971232a43c3d148c411f0c91e9ed496123c920
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-25 06:35:00 +0000

    Adding tarfile member sanitization to extractall()

--- python/servo/command_base.py
+++ python/servo/command_base.py
@@ -456,7 +456,26 @@ class CommandBase(object):
                     sys.exit(1)
             else:
                 with tarfile.open(os.path.join(nightlies_folder, destination_file), "r") as tar:
-                    tar.extractall(destination_folder)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(tar, destination_folder)
 
     def get_executable(self, destination_folder):
         if is_windows():
