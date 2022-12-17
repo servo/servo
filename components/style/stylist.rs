7223e391d8d4ecee01048e3ce1183daf1c1f4b58
Oriol Brufau <oriol-bugzilla@hotmail.com>
2022-12-17 02:04:02 +0000

    style: Properly handle ::backdrop
    Depends on D164807
    
    Differential Revision: https://phabricator.services.mozilla.com/D164908

--- components/style/stylist.rs
+++ components/style/stylist.rs
@@ -966,6 +966,7 @@ impl Stylist {
         element: E,
         pseudo: &PseudoElement,
         rule_inclusion: RuleInclusion,
+        originating_element_style: &Arc<ComputedValues>,
         parent_style: &Arc<ComputedValues>,
         is_probe: bool,
         matching_fn: Option<&dyn Fn(&PseudoElement) -> bool>,
@@ -976,6 +977,7 @@ impl Stylist {
         let cascade_inputs = self.lazy_pseudo_rules(
             guards,
             element,
+            originating_element_style,
             parent_style,
             pseudo,
             is_probe,
@@ -1105,6 +1107,7 @@ impl Stylist {
         &self,
         guards: &StylesheetGuards,
         element: E,
+        originating_element_style: &Arc<ComputedValues>,
         parent_style: &Arc<ComputedValues>,
         pseudo: &PseudoElement,
         is_probe: bool,
@@ -1134,7 +1137,8 @@ impl Stylist {
         );
 
         matching_context.pseudo_element_matching_fn = matching_fn;
-        matching_context.extra_data.originating_element_style = Some(parent_style.clone());
+        matching_context.extra_data.originating_element_style =
+            Some(originating_element_style.clone());
 
         self.push_applicable_declarations(
             element,
@@ -1165,7 +1169,8 @@ impl Stylist {
                 needs_selector_flags,
             );
             matching_context.pseudo_element_matching_fn = matching_fn;
-            matching_context.extra_data.originating_element_style = Some(parent_style.clone());
+            matching_context.extra_data.originating_element_style =
+                Some(originating_element_style.clone());
 
             self.push_applicable_declarations(
                 element,
