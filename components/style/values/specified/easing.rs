210bd73029240b69693e5614db9c7e3f90f4a73e
David Shin <dshin@mozilla.com>
2023-02-17 17:13:21 +0000

    style: `linear()` easing function should not consider less than 2 linear stop arguments valid
    Latest spec no longer handles `linear() == linear` and `linear(<single value>) == <single value>`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D169955

--- components/style/values/specified/easing.rs
+++ components/style/values/specified/easing.rs
@@ -136,10 +136,8 @@ impl TimingFunction {
         if !linear_timing_function_enabled() {
             return Err(input.new_custom_error(StyleParseErrorKind::ExperimentalProperty));
         }
-        if input.is_exhausted() {
-            return Ok(GenericTimingFunction::LinearFunction(LinearStops::default()));
-        }
         let mut result = vec![];
+        // Closely follows `parse_comma_separated`, but can generate multiple entries for one comma-separated entry.
         loop {
             input.parse_until_before(Delimiter::Comma, |i| {
                 let mut input_start = i.try_parse(|i| Percentage::parse(context, i)).ok();
@@ -174,6 +172,9 @@ impl TimingFunction {
                 Ok(&Token::Comma) => continue,
                 Ok(_) => unreachable!(),
             }
+        }
+        if result.len() < 2 {
+            return Err(input.new_custom_error(StyleParseErrorKind::UnspecifiedError));
         }
 
         Ok(GenericTimingFunction::LinearFunction(LinearStops::new(
