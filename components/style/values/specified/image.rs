b99593552d9d05082ddf5c5ed709ebe624d7557d
Emilio Cobos Álvarez <emilio@crisal.io>
2023-03-13 11:22:06 +0000

    style: Make image-set without valid images render nothing
    Differential Revision: https://phabricator.services.mozilla.com/D172341

--- components/style/values/specified/image.rs
+++ components/style/values/specified/image.rs
@@ -399,7 +399,7 @@ impl ImageSet {
             })
         })?;
         Ok(Self {
-            selected_index: 0,
+            selected_index: std::usize::MAX,
             items: items.into(),
         })
     }
