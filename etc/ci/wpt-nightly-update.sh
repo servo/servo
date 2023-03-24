95bfad3ac5070f28fa5fb0ba852c340167aeebea
Martin Robinson <mrobinson@igalia.com>
2023-03-24 18:12:45 +0100

    Add Layout 2020 support to nightly WPT update

--- etc/ci/wpt-nightly-update.sh
+++ etc/ci/wpt-nightly-update.sh
@@ -8,22 +8,25 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# Using an existing log file, update the expected test results and amend the
-# last commit with the new results.
-function unsafe_update_metadata() {
-    ./mach update-wpt "${1}" || return 1
-    # Hope that any test result changes from layout-2013 are
-    # also applicable to layout-2020.
-    ./mach update-wpt --layout-2020 "${1}" || return 2
+# For a given chunk, use the existing log files to update the expected test
+# results and amend the last commit with the new results.
+function unsafe_update_metadata_chunk() {
+    ./mach update-wpt \
+        "wpt-logs-linux/test-wpt.layout-2013.${1}.log" || return 1
+    ./mach update-wpt --layout-2020 \
+        "wpt-logs-linux/test-wpt.layout-2020.${1}.log" || return 2
+
     # Ensure any new directories or ini files are included in these changes.
-    git add tests/wpt/metadata tests/wpt/metadata-layout-2020 \
+    git add tests/wpt/metadata \
+        tests/wpt/metadata-layout-2020 \
         tests/wpt/mozilla/meta || return 3
+
     # Merge all changes with the existing commit.
     git commit -a --amend --no-edit || return 3
 }
 
-function update_metadata() {
-    unsafe_update_metadata "${1}" || \
+function update_metadata_chunk() {
+    unsafe_update_metadata_chunk "${1}" || \
         { code="${?}"; cleanup; return "${code}"; }
 }
 
@@ -31,8 +34,7 @@ function main() {
     for n in $(seq 1 "${MAX_CHUNK_ID}")
     do
         code=""
-        update_metadata "wpt-logs-linux/test-wpt.${n}.log" || \
-            code="${?}"
+        update_metadata_chunk "${n}" || code="${?}"
         if [[ "${code}" != "" ]]; then
             return "${code}"
         fi
