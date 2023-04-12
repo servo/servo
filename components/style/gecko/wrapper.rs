a2df8f7ea5e76e7753d9649b7dfb1b0329510078
Jonathan Kew <jkew@mozilla.com>
2023-04-12 21:46:47 +0000

    style: Accept a comma-separated list of language codes in the :lang() pseudo
    This is the other extension to the :lang() pseudo-class in  Selectors-4.
    (Also supported in Safari.)
    
    Depends on D174999
    
    Differential Revision: https://phabricator.services.mozilla.com/D175000

--- components/style/gecko/wrapper.rs
+++ components/style/gecko/wrapper.rs
@@ -1560,20 +1560,31 @@ impl<'le> TElement for GeckoElement<'le> {
     }
 
     fn match_element_lang(&self, override_lang: Option<Option<AttrValue>>, value: &Lang) -> bool {
-        // Gecko supports :lang() from CSS Selectors 3, which only accepts a
-        // single language tag, and which performs simple dash-prefix matching
-        // on it.
+        // Gecko supports :lang() from CSS Selectors 4, which accepts a list
+        // of language tags, and does BCP47-style range matching.
         let override_lang_ptr = match override_lang {
             Some(Some(ref atom)) => atom.as_ptr(),
             _ => ptr::null_mut(),
         };
-        unsafe {
-            Gecko_MatchLang(
-                self.0,
-                override_lang_ptr,
-                override_lang.is_some(),
-                value.as_slice().as_ptr(),
-            )
+        match value {
+            Lang::Single(lang) => unsafe {
+                Gecko_MatchLang(
+                    self.0,
+                    override_lang_ptr,
+                    override_lang.is_some(),
+                    lang.as_slice().as_ptr(),
+                )
+            },
+            Lang::List(list) => {
+                list.iter().any(|lang| unsafe {
+                    Gecko_MatchLang(
+                        self.0,
+                        override_lang_ptr,
+                        override_lang.is_some(),
+                        lang.as_slice().as_ptr(),
+                    )
+                })
+            },
         }
     }
 
