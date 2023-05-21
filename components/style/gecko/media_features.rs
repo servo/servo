d2a7ebf789359c06ea1891a5773e4354e74757be
Michelle Goossens <michelle@masterwayz.nl>
2023-05-21 23:38:22 +0200

    style: Remove browser.proton.modals.enabled pref
    Differential Revision: https://phabricator.services.mozilla.com/D116903

--- components/style/gecko/media_features.rs
+++ components/style/gecko/media_features.rs
@@ -680,7 +680,7 @@ macro_rules! bool_pref_feature {
 /// to support new types in these entries and (2) ensuring that either
 /// nsPresContext::MediaFeatureValuesChanged is called when the value that
 /// would be returned by the evaluator function could change.
-pub static MEDIA_FEATURES: [MediaFeatureDescription; 61] = [
+pub static MEDIA_FEATURES: [MediaFeatureDescription; 60] = [
     feature!(
         atom!("width"),
         AllowsRanges::Yes,
@@ -927,6 +927,5 @@ pub static MEDIA_FEATURES: [MediaFeatureDescription; 61] = [
     lnf_int_feature!(atom!("-moz-gtk-csd-reversed-placement"), GTKCSDReversedPlacement),
     lnf_int_feature!(atom!("-moz-system-dark-theme"), SystemUsesDarkTheme),
     bool_pref_feature!(atom!("-moz-proton"), "browser.proton.enabled"),
-    bool_pref_feature!(atom!("-moz-proton-modals"), "browser.proton.modals.enabled"),
     bool_pref_feature!(atom!("-moz-proton-places-tooltip"), "browser.proton.places-tooltip.enabled"),
 ];
