77cab0edc3b98f801558f8020bcfc7b90f860971
Michelle Goossens <michelle@masterwayz.nl>
2023-05-21 22:26:03 +0200

    style: Remove browser.proton.doorhangers.enabled pref
    Differential Revision: https://phabricator.services.mozilla.com/D117091

--- components/style/gecko/media_features.rs
+++ components/style/gecko/media_features.rs
@@ -680,7 +680,7 @@ macro_rules! bool_pref_feature {
 /// to support new types in these entries and (2) ensuring that either
 /// nsPresContext::MediaFeatureValuesChanged is called when the value that
 /// would be returned by the evaluator function could change.
-pub static MEDIA_FEATURES: [MediaFeatureDescription; 62] = [
+pub static MEDIA_FEATURES: [MediaFeatureDescription; 61] = [
     feature!(
         atom!("width"),
         AllowsRanges::Yes,
@@ -928,6 +928,5 @@ pub static MEDIA_FEATURES: [MediaFeatureDescription; 62] = [
     lnf_int_feature!(atom!("-moz-system-dark-theme"), SystemUsesDarkTheme),
     bool_pref_feature!(atom!("-moz-proton"), "browser.proton.enabled"),
     bool_pref_feature!(atom!("-moz-proton-modals"), "browser.proton.modals.enabled"),
-    bool_pref_feature!(atom!("-moz-proton-doorhangers"), "browser.proton.doorhangers.enabled"),
     bool_pref_feature!(atom!("-moz-proton-places-tooltip"), "browser.proton.places-tooltip.enabled"),
 ];
