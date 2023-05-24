520f851d8c68b710f7d5e3c150cea4cf63c9d12b
Emilio Cobos Álvarez <emilio@crisal.io>
2023-05-24 11:13:58 +0000

    style: Simplify some appearance code
    I was looking into simplifying our scrollbar styles:
    
      * StyleAppearance::Resizer is not used in content, and some of the
        values were only for <xul:window> which are not supported anymore.
    
      * Statusbarpanel and Resizerpanel aren't used. Statusbar is only used
        once on macOS so we only need to keep it there.
    
    Differential Revision: https://phabricator.services.mozilla.com/D178374

--- components/style/values/specified/box.rs
+++ components/style/values/specified/box.rs
@@ -1489,13 +1489,6 @@ pub enum Appearance {
     Range,
     #[parse(condition = "ParserContext::in_ua_or_chrome_sheet")]
     RangeThumb,
-    /// The resizer background area in a status bar for the resizer widget in
-    /// the corner of a window.
-    #[parse(condition = "ParserContext::in_ua_or_chrome_sheet")]
-    Resizerpanel,
-    /// The resizer itself.
-    #[parse(condition = "ParserContext::in_ua_or_chrome_sheet")]
-    Resizer,
     /// The scrollbar slider
     #[parse(condition = "ParserContext::in_ua_or_chrome_sheet")]
     ScrollbarHorizontal,
@@ -1546,9 +1539,6 @@ pub enum Appearance {
     /// A status bar in a main application window.
     #[parse(condition = "ParserContext::in_ua_or_chrome_sheet")]
     Statusbar,
-    /// A single pane of a status bar.
-    #[parse(condition = "ParserContext::in_ua_or_chrome_sheet")]
-    Statusbarpanel,
     /// A single tab in a tab widget.
     #[parse(condition = "ParserContext::in_ua_or_chrome_sheet")]
     Tab,
