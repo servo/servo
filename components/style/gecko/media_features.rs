d830ec8562f0b00c8738bd6e403e4bf43ef3c0b0
Stephen A Pohl <spohl.mozilla.bugs@gmail.com>
2023-05-27 14:58:02 +0200

    style: Place window control buttons on the right side on macOS when using an RTL system locale, regardless of the Firefox locale
    Differential Revision: https://phabricator.services.mozilla.com/D116921

--- components/style/gecko/media_features.rs
+++ components/style/gecko/media_features.rs
@@ -680,7 +680,7 @@ macro_rules! bool_pref_feature {
 /// to support new types in these entries and (2) ensuring that either
 /// nsPresContext::MediaFeatureValuesChanged is called when the value that
 /// would be returned by the evaluator function could change.
-pub static MEDIA_FEATURES: [MediaFeatureDescription; 60] = [
+pub static MEDIA_FEATURES: [MediaFeatureDescription; 61] = [
     feature!(
         atom!("width"),
         AllowsRanges::Yes,
@@ -913,6 +913,7 @@ pub static MEDIA_FEATURES: [MediaFeatureDescription; 60] = [
     lnf_int_feature!(atom!("-moz-windows-default-theme"), WindowsDefaultTheme),
     lnf_int_feature!(atom!("-moz-mac-graphite-theme"), MacGraphiteTheme),
     lnf_int_feature!(atom!("-moz-mac-big-sur-theme"), MacBigSurTheme),
+    lnf_int_feature!(atom!("-moz-mac-rtl"), MacRTL),
     lnf_int_feature!(atom!("-moz-windows-accent-color-in-titlebar"), WindowsAccentColorInTitlebar),
     lnf_int_feature!(atom!("-moz-windows-compositor"), DWMCompositor),
     lnf_int_feature!(atom!("-moz-windows-classic"), WindowsClassic),
