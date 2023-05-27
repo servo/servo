c204f3a96d03724af6a97c092418bbf7447c1860
Oriol Brufau <obrufau@igalia.com>
2023-05-27 17:30:04 +0200

    Further changes required by Servo

--- components/style/servo/media_queries.rs
+++ components/style/servo/media_queries.rs
@@ -221,6 +221,12 @@ impl Device {
         }
     }
 
+    /// Returns the gtk titlebar radius in CSS pixels.
+    /// TODO: implement this method.
+    pub fn titlebar_radius(&self) -> f32 {
+        0.0
+    }
+
     /// Return whether the document is a chrome document.
     #[inline]
     pub fn is_chrome_document(&self) -> bool {
