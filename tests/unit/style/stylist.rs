2d8866d6c852f5789edc9d759430420d69f2375a
Oriol Brufau <obrufau@igalia.com>
2023-05-30 23:35:42 +0200

    Further changes required by Servo

--- tests/unit/style/stylist.rs
+++ tests/unit/style/stylist.rs
@@ -15,7 +15,7 @@ use style::properties::{PropertyDeclaration, PropertyDeclarationBlock};
 use style::selector_map::SelectorMap;
 use style::selector_parser::{SelectorImpl, SelectorParser};
 use style::shared_lock::SharedRwLock;
-use style::stylesheets::layer_rule::LayerOrder;
+use style::stylesheets::layer_rule::LayerId;
 use style::stylesheets::StyleRule;
 use style::stylist::needs_revalidation_for_testing;
 use style::stylist::{Rule, Stylist};
@@ -53,7 +53,7 @@ fn get_mock_rules(css_selectors: &[&str]) -> (Vec<Vec<Rule>>, SharedRwLock) {
                             AncestorHashes::new(s, QuirksMode::NoQuirks),
                             locked.clone(),
                             i as u32,
-                            LayerOrder::top_level(),
+                            LayerId::root(),
                         )
                     })
                     .collect()
