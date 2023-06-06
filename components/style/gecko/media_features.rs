9e7b8e92022cdb63871a8c4c4c904f2ccff1ee32
Emilio Cobos Álvarez <emilio@crisal.io>
2023-06-06 17:31:12 +0200

    style: Don't draw window decorations when painting headerbar on wayland
    This is a better fix for the double decorations than clipping them using
    CSS.
    
    Differential Revision: https://phabricator.services.mozilla.com/D133871

--- components/style/gecko/media_features.rs
+++ components/style/gecko/media_features.rs
@@ -652,7 +652,7 @@ macro_rules! bool_pref_feature {
 /// to support new types in these entries and (2) ensuring that either
 /// nsPresContext::MediaFeatureValuesChanged is called when the value that
 /// would be returned by the evaluator function could change.
-pub static MEDIA_FEATURES: [MediaFeatureDescription; 58] = [
+pub static MEDIA_FEATURES: [MediaFeatureDescription; 57] = [
     feature!(
         atom!("width"),
         AllowsRanges::Yes,
@@ -919,7 +919,6 @@ pub static MEDIA_FEATURES: [MediaFeatureDescription; 58] = [
         atom!("-moz-gtk-csd-reversed-placement"),
         GTKCSDReversedPlacement
     ),
-    lnf_int_feature!(atom!("-moz-gtk-wayland"), GTKWayland),
     lnf_int_feature!(atom!("-moz-system-dark-theme"), SystemUsesDarkTheme),
     bool_pref_feature!(
         atom!("-moz-proton-places-tooltip"),
