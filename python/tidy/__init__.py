81433a8684d078ae629ba24f30f877028e361136
Martin Robinson <mrobinson@igalia.com>
2023-06-15 12:34:27 +0200

    Convert tidy to a non-egg Python package
    It seems that servo-tidy is only used by webrender in my GitHub
    searches. WebRender could simply use `rustfmt` and the tidy on pypi
    hasn't been updated since 2018. Converting tidy to a normal Python
    package removes the maintenance burden of continually fixing the easy
    install configuration.
    
    Fixes #29094.
    Fixes #29334.

--- python/tidy/__init__.py
+++ python/tidy/__init__.py
@@ -6,3 +6,6 @@
 # <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
 # option. This file may not be copied, modified, or distributed
 # except according to those terms.
+
+from .tidy import scan  # noqa
+from .test import do_tests  # noqa
