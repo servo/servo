6fe164464c1deaffec228437eb55f2b853308218
atbrakhi <atbrakhi@igalia.com>
2023-08-29 10:40:34 +0200

    fix fmt

--- ports/servoshell/tests/mod.rs
+++ ports/servoshell/tests/mod.rs
@@ -2,5 +2,5 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
- #[cfg(test)]
- mod servoshell_test;
+#[cfg(test)]
+mod servoshell_test;
