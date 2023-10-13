2b349a7965cc197f9a2d3567a042a41a4dd9080e
Delan Azabani <dazabani@igalia.com>
2023-10-13 20:02:59 +0800

    oops forgot to check in embedder_traits/build.rs

---
+++ components/embedder_traits/build.rs
@@ -0,0 +1,19 @@
+use std::error::Error;
+use std::path::Path;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Cargo does not expose the profile name to crates or their build scripts,
+    // but we can extract it from OUT_DIR and set a custom cfg() ourselves.
+    let out = std::env::var("OUT_DIR")?;
+    let out = Path::new(&out);
+    let krate = out.parent().unwrap();
+    let build = krate.parent().unwrap();
+    let profile = build.parent().unwrap();
+    if profile.file_name().unwrap() == "production" {
+        println!("cargo:rustc-cfg=servo_production");
+    } else {
+        println!("cargo:rustc-cfg=servo_do_not_use_in_production");
+    }
+
+    Ok(())
+}
