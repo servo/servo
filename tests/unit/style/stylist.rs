c0c2eb60645a83f177f6a33120dddc89a29f5b0c
Oriol Brufau <obrufau@igalia.com>
2023-11-17 20:02:32 +0100

    Further changes required by Servo

--- tests/unit/style/stylist.rs
+++ tests/unit/style/stylist.rs
@@ -206,13 +206,13 @@ fn test_insert() {
         0,
         selector_map
             .class_hash
-            .get(&Atom::from("intro"), QuirksMode::NoQuirks)
+            .get(&Atom::from("foo"), QuirksMode::NoQuirks)
             .unwrap()[0]
             .source_order
     );
     assert!(selector_map
         .class_hash
-        .get(&Atom::from("foo"), QuirksMode::NoQuirks)
+        .get(&Atom::from("intro"), QuirksMode::NoQuirks)
         .is_none());
 }
 
