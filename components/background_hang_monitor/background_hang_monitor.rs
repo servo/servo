servo/servo

commit a31e2ea57627bf5cdf8de81c6646d4f2fe25092b

Fix macOS's sameple isn't used in background_hang_monitor (#30490)

components/background_hang_monitor/background_hang_monitor.rs
@@ -100,7 +100,10 @@ impl BackgroundHangMonitorRegister for HangMonitorRegister {
             not(any(target_arch = "arm", target_arch = "aarch64"))
         ))]
         let sampler = crate::sampler_linux::LinuxSampler::new();
-        #[cfg(any(target_os = "android", target_arch = "arm", target_arch = "aarch64"))]
+        #[cfg(all(
+            any(target_os = "android", target_os = "linux"),
+            any(target_arch = "arm", target_arch = "aarch64")
+        ))]
         let sampler = crate::sampler::DummySampler::new();
 
         // When a component is registered, and there's an exit request that

commit 1b6230679d4516f0e59d919a620352fb2981eed9

fix sampler compilation bug introduced in #30490
Signed-off-by: Mukilan Thiyagarajan <mukilan@igalia.com>

components/background_hang_monitor/background_hang_monitor.rs
@@ -100,9 +100,9 @@ impl BackgroundHangMonitorRegister for HangMonitorRegister {
             not(any(target_arch = "arm", target_arch = "aarch64"))
         ))]
         let sampler = crate::sampler_linux::LinuxSampler::new();
-        #[cfg(all(
-            any(target_os = "android", target_os = "linux"),
-            any(target_arch = "arm", target_arch = "aarch64")
+        #[cfg(any(
+            target_os = "android",
+            all(target_os = "linux", any(target_arch = "arm", target_arch = "aarch64"))
         ))]
         let sampler = crate::sampler::DummySampler::new();
 