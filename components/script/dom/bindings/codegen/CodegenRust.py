4bacb9d16276b0663c7538c509904856d4f1ac52
Bentaimia Haddadi <haddadi.taym@gmail.com>
2024-01-15 10:39:09 +0100

    use FLoat32Array in XRRigidTransform
    Signed-off-by: Bentaimia Haddadi <haddadi.taym@gmail.com>

--- components/script/dom/bindings/codegen/CodegenRust.py
+++ components/script/dom/bindings/codegen/CodegenRust.py
@@ -1461,7 +1461,7 @@ def getConversionConfigForType(type, isEnforceRange, isClamp, treatNullAs):
 
 
 def todo_switch_float_32(des):
-    return des.interface.identifier.name in ['XRRigidTransform', 'XRRay', 'GamepadPose']
+    return des.interface.identifier.name in ['XRRay', 'GamepadPose']
 
 
 def builtin_return_type(returnType):
