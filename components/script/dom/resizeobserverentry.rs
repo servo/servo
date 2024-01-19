b9210a80c66912706fa7d2591c32064e0501d35d
gterzian <2792687+gterzian@users.noreply.github.com>
2024-01-19 12:32:14 +0800

    implement resize observer entry methods

--- components/script/dom/resizeobserverentry.rs
+++ components/script/dom/resizeobserverentry.rs
@@ -5,32 +5,40 @@
 use dom_struct::dom_struct;
 use js::jsval::JSVal;
 
+use crate::dom::bindings::cell::DomRefCell;
 use crate::dom::bindings::codegen::Bindings::ResizeObserverEntryBinding::ResizeObserverEntryMethods;
 use crate::dom::bindings::reflector::Reflector;
-use crate::dom::bindings::root::DomRoot;
+use crate::dom::bindings::root::{Dom, DomRoot};
+use crate::dom::bindings::utils::to_frozen_array;
 use crate::dom::domrectreadonly::DOMRectReadOnly;
 use crate::dom::element::Element;
+use crate::dom::resizeobserversize::ResizeObserverSize;
 use crate::script_runtime::JSContext as SafeJSContext;
 
 #[dom_struct]
 pub struct ResizeObserverEntry {
     reflector_: Reflector,
+    target: Dom<Element>,
+    content_rect: Dom<DOMRectReadOnly>,
+    border_box_size: Vec<ResizeObserverSize>,
+    content_box_size: Vec<ResizeObserverSize>,
+    device_pixel_content_box_size: Vec<ResizeObserverSize>,
 }
 
 impl ResizeObserverEntryMethods for ResizeObserverEntry {
     fn Target(&self) -> DomRoot<Element> {
-        unimplemented!()
+        DomRoot::from_ref(&*self.target)
     }
     fn ContentRect(&self) -> DomRoot<DOMRectReadOnly> {
-        unimplemented!()
+        DomRoot::from_ref(&*self.content_rect)
     }
     fn BorderBoxSize(&self, cx: SafeJSContext) -> JSVal {
-        unimplemented!()
+        to_frozen_array(&self.border_box_size, cx)
     }
     fn ContentBoxSize(&self, cx: SafeJSContext) -> JSVal {
-        unimplemented!()
+        to_frozen_array(&self.content_box_size, cx)
     }
     fn DevicePixelContentBoxSize(&self, cx: SafeJSContext) -> JSVal {
-        unimplemented!()
+        to_frozen_array(&self.device_pixel_content_box_size, cx)
     }
 }
