104205257deb151bc41f4e7b7ab58266a1543cb1
Martin Robinson <mrobinson@igalia.com>
2024-01-30 09:10:13 +0100

    deps: Stop vendoring WebRender (#31212)
    The new strategy for dependencies with upstream in Gecko is to manage
    them in separate repositories, which will more easily allow rebasing our
    changes on top of newer Gecko work.

--- third_party/webrender/webrender/src/device/mod.rs
+++
@@ -1,9 +0,0 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-mod gl;
-pub mod query_gl;
-
-pub use self::gl::*;
-pub use self::query_gl as query;
