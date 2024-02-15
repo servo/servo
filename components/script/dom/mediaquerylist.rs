bab529b7db449f12d0eed1d1cd56a2348dfd8014
syvb <me@iter.ca>
2024-02-15 15:22:26 -0500

    Implement AbortController
    Signed-off-by: syvb <me@iter.ca>

--- components/script/dom/mediaquerylist.rs
+++ components/script/dom/mediaquerylist.rs
@@ -99,6 +99,7 @@ impl MediaQueryListMethods for MediaQueryList {
             AddEventListenerOptions {
                 parent: EventListenerOptions { capture: false },
                 once: false,
+                signal: None,
             },
         );
     }
