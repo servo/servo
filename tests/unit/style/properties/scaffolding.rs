2f555bfdef8f440b160ce367fccc6f48ce75a5ca
Delan Azabani <dazabani@igalia.com>
2024-02-27 18:56:20 +0800

    Add temporary code to debug Windows test failures

--- tests/unit/style/properties/scaffolding.rs
+++ tests/unit/style/properties/scaffolding.rs
@@ -20,9 +20,45 @@ fn properties_list_json() {
         .join("stylo")
         .join("css-properties.json");
 
+    #[cfg(windows)]
+    {
+        let mut paths = vec![];
+        dump_tree(&mut paths, top.join("target"));
+        paths.sort();
+        panic!("{}", paths.join("\n"));
+    }
+
     let properties: Value = serde_json::from_reader(File::open(json).unwrap()).unwrap();
     let longhands = properties["longhands"].as_object().unwrap();
     assert!(longhands.len() > 100);
     assert!(longhands.get("margin-top").is_some());
     assert!(properties["shorthands"].get("margin").is_some());
+}
+
+#[cfg(windows)]
+fn dump_tree(result: &mut Vec<String>, path: impl AsRef<Path>) {
+    let path = path.as_ref();
+    if path.is_dir() {
+        for entry in std::fs::read_dir(path).unwrap() {
+            let entry = entry.unwrap();
+            let path = entry.path();
+            dump_tree(result, path);
+        }
+        return;
+    }
+    if let Some(path) = path.to_str() {
+        if "jemalloc mozjs incremental lsp fingerprint"
+            .split(" ")
+            .any(|word| path.contains(word))
+        {
+            return;
+        }
+        if ".o .h .d .rs .rmeta .rlib"
+            .split(" ")
+            .any(|suffix| path.ends_with(suffix))
+        {
+            return;
+        }
+        result.push(path.to_owned());
+    }
 }
