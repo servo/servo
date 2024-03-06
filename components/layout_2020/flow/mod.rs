11badf46495407d678baa2a31d9298e5beed1b54
Martin Robinson <mrobinson@igalia.com>
2024-03-06 12:55:08 +0100

    layout: Add start of documentation for inline layout
    Co-authored-by: Rakhi Sharma <atbrakhi@igalia.com>

--- components/layout_2020/flow/mod.rs
+++ components/layout_2020/flow/mod.rs
@@ -1,6 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+#![allow(rustdoc::private_intra_doc_links)]
 
 //! Flow layout, also known as block-and-inline layout.
 
