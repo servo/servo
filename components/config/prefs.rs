007a31c1b521118b49572b1790eff312817106d5
Oriol Brufau <obrufau@igalia.com>
2024-03-07 11:09:10 +0100

    Enable CSS Tables by default (#31470)
    And remove the pref `layout.tables.enabled`.

--- components/config/prefs.rs
+++ components/config/prefs.rs
@@ -519,9 +519,6 @@ mod gen {
                     enabled: bool,
                 },
                 legacy_layout: bool,
-                tables: {
-                    enabled: bool,
-                },
                 #[serde(default = "default_layout_threads")]
                 threads: i64,
                 writing_mode: {
