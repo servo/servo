543273a7afa2ec049b8b4a2ebf5ec46b075f520f
Delan Azabani <dazabani@igalia.com>
2024-03-15 13:17:08 +0800

    NixOS: add gst-plugins-{good,ugly} for better codec support

--- etc/shell.nix
+++ etc/shell.nix
@@ -71,7 +71,9 @@ stdenv.mkDerivation (androidEnvironment // rec {
 
     gst_all_1.gstreamer
     gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-good
     gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-ugly
 
     rustup
     taplo
