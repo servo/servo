aecf6e545fa21e4d1f322ac7893e6284a873346e
six-shot <okhuomonajayi54@gmail.com>
2024-03-20 12:02:42 +0000

    fix clippy problems

--- components/script/stylesheet_loader.rs
+++ components/script/stylesheet_loader.rs
@@ -2,7 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-
 use std::sync::atomic::AtomicBool;
 use std::sync::Mutex;
 
