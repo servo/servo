e77dc3684236ad6693b8df6131f2ba8f99bbef2f
Oriol Brufau <obrufau@igalia.com>
2024-03-25 17:19:06 +0100

    Pick the first or last baseline as appropriate (#31705)
    The old logic was always picking the last baseline, but this should only
    happen for inline-blocks.
    
    Since replaced elements and flex containers aren't currently setting
    their baselines, this is only an improvement for inline-tables.

--- components/layout_2020/flow/inline.rs
+++ components/layout_2020/flow/inline.rs
@@ -101,7 +101,9 @@ use crate::cell::ArcRefCell;
 use crate::context::LayoutContext;
 use crate::flow::float::{FloatBox, SequentialLayoutState};
 use crate::flow::FlowLayout;
-use crate::formatting_contexts::{Baselines, IndependentFormattingContext};
+use crate::formatting_contexts::{
+    Baselines, IndependentFormattingContext, NonReplacedFormattingContextContents,
+};
 use crate::fragment_tree::{
     BaseFragmentInfo, BoxFragment, CollapsedBlockMargins, CollapsedMargin, Fragment, FragmentFlags,
     PositioningFragment,
@@ -2114,9 +2116,8 @@ impl IndependentFormattingContext {
         }
 
         let size = &pbm_sums.sum().into() + &fragment.content_rect.size;
-        let baseline_offset = fragment
-            .baselines
-            .last
+        let baseline_offset = self
+            .pick_baseline(&fragment.baselines)
             .map(|baseline| pbm_sums.block_start + baseline)
             .unwrap_or(size.block.into());
 
@@ -2137,6 +2138,18 @@ impl IndependentFormattingContext {
 
         // Defer a soft wrap opportunity for when we next process text content.
         ifc.have_deferred_soft_wrap_opportunity = true;
+    }
+
+    /// Picks either the first or the last baseline, depending on `baseline-source`.
+    /// <https://drafts.csswg.org/css-inline/#baseline-source>
+    fn pick_baseline(&self, baselines: &Baselines) -> Option<Au> {
+        // TODO: Currently this only supports the initial `baseline-source: auto`.
+        if let Self::NonReplaced(non_replaced) = self {
+            if let NonReplacedFormattingContextContents::Flow(_) = non_replaced.contents {
+                return baselines.last;
+            }
+        }
+        baselines.first
     }
 
     fn get_block_sizes_and_baseline_offset(
