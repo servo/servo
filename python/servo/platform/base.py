6ca3bb440ea23480a3825cefa7a85a429d793a94
Samson <16504129+sagudev@users.noreply.github.com>
2024-04-29 15:33:12 +0200

    Run platform_bootstrap first (#31840)

--- python/servo/platform/base.py
+++ python/servo/platform/base.py
@@ -55,9 +55,9 @@ class Base:
             return False
 
     def bootstrap(self, force: bool):
-        installed_something = self.install_taplo(force)
+        installed_something = self._platform_bootstrap(force)
+        installed_something |= self.install_taplo(force)
         installed_something |= self.install_crown(force)
-        installed_something |= self._platform_bootstrap(force)
         if not installed_something:
             print("Dependencies were already installed!")
 
