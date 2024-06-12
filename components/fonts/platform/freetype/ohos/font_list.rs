b589951c643a5a7988601612b18694589e4112c8
Jonathan Schwender <jonathan.schwender@huawei.com>
2024-06-12 14:04:29 +0200

    ohos: Add fallback font for serif
    The fallback behavior seems to have changed recently.
    Now we need to explicitly add a fallback for `serif`
    otherwise no font is selected.
    
    Signed-off-by: Jonathan Schwender <jonathan.schwender@huawei.com>

--- components/fonts/platform/freetype/ohos/font_list.rs
+++ components/fonts/platform/freetype/ohos/font_list.rs
@@ -80,6 +80,8 @@ impl FontList {
         let alternatives = [
             ("HarmonyOS Sans", "HarmonyOS_Sans_SC_Regular.ttf"),
             ("sans-serif", "HarmonyOS_Sans_SC_Regular.ttf"),
+            // Todo: It's unclear what font should be used, but we need to use something
+            ("serif", "HarmonyOS_Sans_SC_Regular.ttf"),
         ];
 
         alternatives
