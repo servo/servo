d21af5798334190a5e7571a2f781bfa3d2214fc1
Wu Wayne <yuweiwu@pm.me>
2024-06-14 21:16:30 +0900

    Fix missing downcasts

--- components/script/dom/bindings/codegen/CodegenRust.py
+++ components/script/dom/bindings/codegen/CodegenRust.py
@@ -3980,7 +3980,10 @@ class CGAbstractStaticBindingMethod(CGAbstractMethod):
 let args = CallArgs::from_vp(vp, argc);
 let global = GlobalScope::from_object(args.callee());
 """
-        if len(self.exposureSet) == 1:
+
+        if len(self.exposureSet) == 2 and "Window" in self.exposureSet:
+            preamble += "let global = DomRoot::downcast::<dom::types::Window>(global).unwrap();\n"
+        elif len(self.exposureSet) == 1:
             preamble += ("let global = DomRoot::downcast::<dom::types::%s>(global).unwrap();\n" %
                          list(self.exposureSet)[0])
         return CGList([CGGeneric(preamble), self.generate_code()])
@@ -6129,10 +6132,13 @@ class CGClassConstructHook(CGAbstractExternMethod):
 let args = CallArgs::from_vp(vp, argc);
 let global = GlobalScope::from_object(JS_CALLEE(*cx, vp).to_object());
 """
-        if len(self.exposureSet) > 0:
+
+        if len(self.exposureSet) == 2 and "Window" in self.exposureSet:
+            preamble += "let global = DomRoot::downcast::<dom::types::Window>(global).unwrap();\n"
+        elif len(self.exposureSet) == 1:
             preamble += """\
 let global = DomRoot::downcast::<dom::types::%s>(global).unwrap();
-""" % list(self.exposureSet)[len(self.exposureSet) - 1]
+""" % list(self.exposureSet)[0]
         if self.constructor.isHTMLConstructor():
             signatures = self.constructor.signatures()
             assert len(signatures) == 1
