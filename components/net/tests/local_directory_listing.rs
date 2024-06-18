a275e293bafe3ac04df3910b91c7cf453d820f88
Bobulous <Bobulous@users.noreply.github.com>
2024-06-18 20:37:51 +0100

    Removed unnecessary configuration from unit test file for local directory listing feature.
    Signed-off-by: Bobulous <Bobulous@users.noreply.github.com>

--- components/net/tests/local_directory_listing.rs
+++ components/net/tests/local_directory_listing.rs
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#![cfg(not(target_os = "windows"))]
-
 use std::collections::BTreeMap;
 
 use embedder_traits::resources::{read_string, Resource};
