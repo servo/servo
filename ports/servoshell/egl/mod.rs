b0a5abc12c56b6d9e5f5c0a43d19628d435c852e
Jonathan Schwender <jonathan.schwender@huawei.com>
2024-06-19 12:09:19 +0200

    Generate EGL bindings for ohos
    Signed-off-by: Jonathan Schwender <jonathan.schwender@huawei.com>

--- ports/servoshell/egl/mod.rs
+++ ports/servoshell/egl/mod.rs
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#[cfg(target_os = "android")]
+#[cfg(any(target_os = "android", target_env = "ohos"))]
 pub mod gl_glue;
 
 #[cfg(target_os = "android")]
