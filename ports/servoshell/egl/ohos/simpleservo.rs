e0eee924ef906e9c32b3295c664d0dc9b319855c
Jonathan Schwender <jonathan.schwender@huawei.com>
2024-06-27 10:22:15 +0200

    fixup! Share ResourceReaderInstance
    Signed-off-by: Jonathan Schwender <jonathan.schwender@huawei.com>

--- ports/servoshell/egl/ohos/simpleservo.rs
+++ ports/servoshell/egl/ohos/simpleservo.rs
@@ -13,7 +13,7 @@ use ohos_sys::ace::xcomponent::native_interface_xcomponent::{
 };
 use servo::compositing::windowing::EmbedderEvent;
 use servo::compositing::CompositeTarget;
-use servo::embedder_traits::resources::{self, Resource, ResourceReaderMethods};
+use servo::embedder_traits::resources;
 /// The EventLoopWaker::wake function will be called from any thread.
 /// It will be called to notify embedder that some events are available,
 /// and that perform_updates need to be called
@@ -26,6 +26,7 @@ use surfman::{Connection, SurfaceType};
 
 use crate::egl::host_trait::HostTrait;
 use crate::egl::ohos::InitOpts;
+use crate::egl::resources::ResourceReaderInstance;
 use crate::egl::servo_glue::{
     Coordinates, ServoEmbedderCallbacks, ServoGlue, ServoWindowCallbacks,
 };
@@ -119,51 +120,4 @@ pub fn init(
     let _ = servo_glue.process_event(EmbedderEvent::NewWebView(initial_url, servo.browser_id));
 
     Ok(servo_glue)
-}
-
-struct ResourceReaderInstance;
-
-impl ResourceReaderInstance {
-    fn new() -> ResourceReaderInstance {
-        ResourceReaderInstance
-    }
-}
-
-impl ResourceReaderMethods for ResourceReaderInstance {
-    fn read(&self, res: Resource) -> Vec<u8> {
-        Vec::from(match res {
-            Resource::Preferences => &include_bytes!("../../../../resources/prefs.json")[..],
-            Resource::HstsPreloadList => {
-                &include_bytes!("../../../../resources/hsts_preload.json")[..]
-            },
-            Resource::BadCertHTML => &include_bytes!("../../../../resources/badcert.html")[..],
-            Resource::NetErrorHTML => &include_bytes!("../../../../resources/neterror.html")[..],
-            Resource::UserAgentCSS => &include_bytes!("../../../../resources/user-agent.css")[..],
-            Resource::ServoCSS => &include_bytes!("../../../../resources/servo.css")[..],
-            Resource::PresentationalHintsCSS => {
-                &include_bytes!("../../../../resources/presentational-hints.css")[..]
-            },
-            Resource::QuirksModeCSS => &include_bytes!("../../../../resources/quirks-mode.css")[..],
-            Resource::RippyPNG => &include_bytes!("../../../../resources/rippy.png")[..],
-            Resource::DomainList => &include_bytes!("../../../../resources/public_domains.txt")[..],
-            Resource::BluetoothBlocklist => {
-                &include_bytes!("../../../../resources/gatt_blocklist.txt")[..]
-            },
-            Resource::MediaControlsCSS => {
-                &include_bytes!("../../../../resources/media-controls.css")[..]
-            },
-            Resource::MediaControlsJS => {
-                &include_bytes!("../../../../resources/media-controls.js")[..]
-            },
-            Resource::CrashHTML => &include_bytes!("../../../../resources/crash.html")[..],
-        })
-    }
-
-    fn sandbox_access_files(&self) -> Vec<PathBuf> {
-        vec![]
-    }
-
-    fn sandbox_access_files_dirs(&self) -> Vec<PathBuf> {
-        vec![]
-    }
 }
