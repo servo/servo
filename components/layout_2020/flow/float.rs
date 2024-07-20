32104ec2a9f74b5e4272bbb3ddd3e37a56e588bc
valadaptive <valadaptive@protonmail.com>
2024-07-19 22:51:08 -0400

    Remove unnecessary clone
    Signed-off-by: valadaptive <valadaptive@protonmail.com>

--- components/layout_2020/flow/float.rs
+++ components/layout_2020/flow/float.rs
@@ -889,7 +889,7 @@ impl FloatBox {
         positioning_context: &mut PositioningContext,
         containing_block: &ContainingBlock,
     ) -> BoxFragment {
-        let style = self.contents.style.clone();
+        let style = &self.contents.style;
         positioning_context.layout_maybe_position_relative_fragment(
             layout_context,
             containing_block,
