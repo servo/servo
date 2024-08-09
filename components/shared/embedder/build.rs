2ebb71f08a9e2521a2fd277c2bc2b54b9e21dd8d
webbeef <me@webbeef.org>
2024-08-09 02:16:51 -0700

    Set the cfg properly for the production-stripped profile (#32991)
    Signed-off-by: webbeef <me@webbeef.org>

--- components/shared/embedder/build.rs
+++ components/shared/embedder/build.rs
@@ -6,14 +6,21 @@ use std::error::Error;
 use std::path::Path;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    println!("cargo::rustc-check-cfg=cfg(servo_production)");
+    println!("cargo::rustc-check-cfg=cfg(servo_do_not_use_in_production)");
     // Cargo does not expose the profile name to crates or their build scripts,
     // but we can extract it from OUT_DIR and set a custom cfg() ourselves.
     let out = std::env::var("OUT_DIR")?;
     let out = Path::new(&out);
     let krate = out.parent().unwrap();
     let build = krate.parent().unwrap();
-    let profile = build.parent().unwrap();
-    if profile.file_name().unwrap() == "production" {
+    let profile = build
+        .parent()
+        .unwrap()
+        .file_name()
+        .unwrap()
+        .to_string_lossy();
+    if profile == "production" || profile.starts_with("production-") {
         println!("cargo:rustc-cfg=servo_production");
     } else {
         println!("cargo:rustc-cfg=servo_do_not_use_in_production");
