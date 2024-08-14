8b5d9250365adc0e26210096fa0aadf488e7e5ea
Hayashi Mikihiro <34ttrweoewiwe28@gmail.com>
2024-08-14 19:12:43 +0900

    replace in net/async_runtime.rs
    Signed-off-by: Hayashi Mikihiro <34ttrweoewiwe28@gmail.com>

--- components/net/async_runtime.rs
+++ components/net/async_runtime.rs
@@ -2,11 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-use std::sync::Mutex;
+use std::sync::{LazyLock, Mutex};
 
-use lazy_static::lazy_static;
 use tokio::runtime::Runtime;
 
-lazy_static! {
-    pub static ref HANDLE: Mutex<Option<Runtime>> = Mutex::new(Some(Runtime::new().unwrap()));
-}
+pub static HANDLE: LazyLock<Mutex<Option<Runtime>>> =
+    LazyLock::new(|| Mutex::new(Some(Runtime::new().unwrap())));
