f792055b697212776611b9d5a2da1b45dc0ee560
sagudev <16504129+sagudev@users.noreply.github.com>
2024-08-26 14:28:50 +0200

    Extract DataBlock, DataView impl from GPUBuffer
    Signed-off-by: sagudev <16504129+sagudev@users.noreply.github.com>

--- components/script/dom/gpubuffer.rs
+++ components/script/dom/gpubuffer.rs
@@ -2,19 +2,17 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-use std::ffi::c_void;
 use std::ops::Range;
 use std::rc::Rc;
 use std::string::String;
-use std::sync::Arc;
 
 use dom_struct::dom_struct;
 use ipc_channel::ipc::IpcSharedMemory;
-use js::jsapi::{DetachArrayBuffer, NewExternalArrayBuffer};
-use js::typedarray::{ArrayBuffer, HeapArrayBuffer};
+use js::typedarray::ArrayBuffer;
 use webgpu::wgc::device::HostMap;
 use webgpu::{wgt, Mapping, WebGPU, WebGPUBuffer, WebGPURequest, WebGPUResponse};
 
+use super::bindings::buffer_source::DataBlock;
 use super::bindings::codegen::Bindings::WebGPUBinding::{
     GPUBufferMapState, GPUFlagsConstant, GPUMapModeFlags,
 };
@@ -34,33 +32,15 @@ use crate::realms::InRealm;
 use crate::script_runtime::JSContext;
 
 #[derive(JSTraceable, MallocSizeOf)]
-pub struct MappingView {
-    #[no_trace]
-    range: Range<usize>,
-    #[ignore_malloc_size_of = "defined in mozjs"]
-    buffer: HeapArrayBuffer,
-}
-
-impl Drop for MappingView {
-    #[allow(unsafe_code)]
-    fn drop(&mut self) {
-        let cx = GlobalScope::get_cx();
-        assert!(unsafe { DetachArrayBuffer(*cx, self.buffer.underlying_object().handle()) })
-    }
-}
-
-#[derive(JSTraceable, MallocSizeOf)]
 pub struct ActiveBufferMapping {
     // TODO(sagudev): Use IpcSharedMemory when https://github.com/servo/ipc-channel/pull/356 lands
     /// <https://gpuweb.github.io/gpuweb/#active-buffer-mapping-data>
-    #[ignore_malloc_size_of = "Rc"]
-    data: Arc<Box<[u8]>>,
+    /// <https://gpuweb.github.io/gpuweb/#active-buffer-mapping-views>
+    pub data: DataBlock,
     /// <https://gpuweb.github.io/gpuweb/#active-buffer-mapping-mode>
     mode: GPUMapModeFlags,
     /// <https://gpuweb.github.io/gpuweb/#active-buffer-mapping-range>
     range: Range<u64>,
-    /// <https://gpuweb.github.io/gpuweb/#active-buffer-mapping-views>
-    views: Vec<MappingView>,
 }
 
 impl ActiveBufferMapping {
@@ -72,56 +52,11 @@ impl ActiveBufferMapping {
         if size > (1 << 53) - 1 {
             return Err(Error::Range("Over MAX_SAFE_INTEGER".to_string()));
         }
-        let data = vec![0; size];
         Ok(Self {
-            data: Arc::new(data.into_boxed_slice()),
+            data: DataBlock::new_zeroed(size),
             mode,
             range,
-            views: Vec::new(),
         })
-    }
-
-    /// Panics if there is any active view
-    pub fn load_data(&mut self, src: &[u8]) {
-        // `Arc::get_mut` ensures there are no views
-        Arc::get_mut(&mut self.data).unwrap().clone_from_slice(src)
-    }
-
-    #[allow(unsafe_code)]
-    pub fn view(&mut self, range: Range<usize>) -> Result<&MappingView, ()> {
-        if self
-            .views
-            .iter()
-            .any(|view| range_overlap(&view.range, &range))
-        {
-            return Err(());
-        }
-        let cx = GlobalScope::get_cx();
-        /// `freeFunc()` must be threadsafe, should be safely callable from any thread
-        /// without causing conflicts, unexpected behavior.
-        unsafe extern "C" fn free_func(_contents: *mut c_void, free_user_data: *mut c_void) {
-            // Clippy warns about "creating a `Arc` from a void raw pointer" here, but suggests
-            // the exact same line to fix it. Doing the cast is tricky because of the use of
-            // a generic type in this parameter.
-            #[allow(clippy::from_raw_with_void_ptr)]
-            drop(Arc::from_raw(free_user_data as *const _));
-        }
-        let raw: *mut Box<[u8]> = Arc::into_raw(Arc::clone(&self.data)) as _;
-        rooted!(in(*cx) let object = unsafe {
-            NewExternalArrayBuffer(
-                *cx,
-                range.end - range.start,
-                // SAFETY: This is safe because we have checked there is no overlapping view
-                (*raw)[range.clone()].as_mut_ptr() as _,
-                Some(free_func),
-                raw as _,
-            )
-        });
-        self.views.push(MappingView {
-            range,
-            buffer: HeapArrayBuffer::from(*object).unwrap(),
-        });
-        Ok(self.views.last().unwrap())
     }
 }
 
@@ -217,11 +152,11 @@ impl GPUBufferMethods for GPUBuffer {
         };
 
         // Step 3
-        mapping.views.clear();
+        mapping.data.clear_views();
         // Step 5&7
         if let Err(e) = self.channel.0.send(WebGPURequest::UnmapBuffer {
             buffer_id: self.id().0,
-            array_buffer: IpcSharedMemory::from_bytes(&mapping.data),
+            array_buffer: IpcSharedMemory::from_bytes(mapping.data.data()),
             write_back: mapping.mode >= GPUMapModeConstants::WRITE,
             offset: mapping.range.start,
             size: mapping.range.end - mapping.range.start,
@@ -329,10 +264,9 @@ impl GPUBufferMethods for GPUBuffer {
         // so we need to rebase range to mapped.range
         let rebased_offset = (offset - mapping.range.start) as usize;
         mapping
+            .data
             .view(rebased_offset..rebased_offset + range_size as usize)
-            .map(|mapping_view| unsafe {
-                ArrayBuffer::from(mapping_view.buffer.underlying_object().get()).unwrap()
-            })
+            .map(|view| view.array_buffer())
             .map_err(|()| Error::Operation)
     }
 
@@ -367,12 +301,6 @@ impl GPUBufferMethods for GPUBuffer {
             GPUBufferMapState::Unmapped
         }
     }
-}
-
-/// Returns true if two non-inclusive ranges overlap
-// https://stackoverflow.com/questions/3269434/whats-the-most-efficient-way-to-test-if-two-ranges-overlap
-fn range_overlap<T: std::cmp::PartialOrd>(range1: &Range<T>, range2: &Range<T>) -> bool {
-    range1.start < range2.end && range2.start < range1.end
 }
 
 impl GPUBuffer {
@@ -423,7 +351,7 @@ impl GPUBuffer {
             },
             Ok(mut mapping) => {
                 // Step 5
-                mapping.load_data(&wgpu_mapping.data);
+                mapping.data.load(&wgpu_mapping.data);
                 // Step 6
                 self.mapping.borrow_mut().replace(mapping);
                 // Step 7
