46fe9429920e964b918870663dab928649d5137b
Andriy Sultanov <sultanovandriy@gmail.com>
2024-09-07 13:26:00 +0100

    Add canGc as a parameter to autogenerated trait methods
    Signed-off-by: Andriy Sultanov <sultanovandriy@gmail.com>

--- components/script/dom/bindings/codegen/Configuration.py
+++ components/script/dom/bindings/codegen/Configuration.py
@@ -232,6 +232,7 @@ class Descriptor(DescriptorProvider):
         self.register = desc.get('register', True)
         self.path = desc.get('path', pathDefault)
         self.inRealmMethods = [name for name in desc.get('inRealms', [])]
+        self.canGcMethods = [name for name in desc.get('canGc', [])]
         self.bindingPath = f"{getModuleFromObject(self.interface)}::{ifaceName}_Binding"
         self.outerObjectHook = desc.get('outerObjectHook', 'None')
         self.proxy = False
