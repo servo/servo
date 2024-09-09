674d7f80fda0e82eeb1d83da203e44e681268620
crbrz <cristianb@gmail.com>
2024-09-09 14:30:48 +0300

    Implement missing spec step in Location::SetHash
    Signed-off-by: crbrz <cristianb@gmail.com>

--- components/script/dom/location.rs
+++ components/script/dom/location.rs
@@ -315,13 +315,19 @@ impl LocationMethods for Location {
             // Step 5: Set copyURL's fragment to the empty string.
             // Step 6: Basic URL parse input, with copyURL as url and fragment state as
             // state override.
-            copy_url.as_mut_url().set_fragment(match value.0.as_str() {
+            let new_fragment = match value.0.as_str() {
                 "" => Some("#"),
                 _ if value.0.starts_with('#') => Some(&value.0[1..]),
-                _ => Some(&value.0),
-            });
+                _ => Some(value.0.as_str()),
+            };
+            // Step 7: If copyURL's fragment is this's url's fragment, then return.
+            if copy_url.fragment() == new_fragment {
+                Ok(None)
+            } else {
+                copy_url.as_mut_url().set_fragment(new_fragment);
 
-            Ok(Some(copy_url))
+                Ok(Some(copy_url))
+            }
         })
     }
 
