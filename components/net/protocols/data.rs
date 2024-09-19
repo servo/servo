3f462e265f68f5bd39c044d76026e4c25c495301
webbeef <me@webbeef.org>
2024-09-19 07:39:09 -0700

    Simplify the data: protocol handler
    Signed-off-by: webbeef <me@webbeef.org>

--- components/net/protocols/data.rs
+++ components/net/protocols/data.rs
@@ -5,70 +5,18 @@
 use std::future::Future;
 use std::pin::Pin;
 
-use data_url::forgiving_base64;
-use headers::{ContentType, HeaderMapExt};
+use data_url::DataUrl;
+use headers::HeaderValue;
 use http::StatusCode;
-use mime::Mime;
 use net_traits::request::Request;
 use net_traits::response::{Response, ResponseBody};
 use net_traits::{NetworkError, ResourceFetchTiming};
-use percent_encoding::percent_decode;
-use servo_url::ServoUrl;
-use url::Position;
 
 use crate::fetch::methods::{DoneChannel, FetchContext};
 use crate::protocols::ProtocolHandler;
 
 #[derive(Default)]
 pub struct DataProtocolHander {}
-
-enum DecodeError {
-    InvalidDataUri,
-    NonBase64DataUri,
-}
-
-type DecodeData = (Mime, Vec<u8>);
-
-fn decode(url: &ServoUrl) -> Result<DecodeData, DecodeError> {
-    // data_url could do all of this work for us,
-    // except that it currently (Nov 2019) parses mime types into a
-    // different Mime class than other code expects
-
-    assert_eq!(url.scheme(), "data");
-    // Split out content type and data.
-    let parts: Vec<&str> = url[Position::BeforePath..Position::AfterQuery]
-        .splitn(2, ',')
-        .collect();
-    if parts.len() != 2 {
-        return Err(DecodeError::InvalidDataUri);
-    }
-
-    // ";base64" must come at the end of the content type, per RFC 2397.
-    // rust-http will fail to parse it because there's no =value part.
-    let mut ct_str = parts[0];
-    let is_base64 = ct_str.ends_with(";base64");
-    if is_base64 {
-        ct_str = &ct_str[..ct_str.len() - ";base64".len()];
-    }
-    let ct_str = if ct_str.starts_with(";charset=") {
-        format!("text/plain{}", ct_str)
-    } else {
-        ct_str.to_owned()
-    };
-
-    let content_type = ct_str
-        .parse()
-        .unwrap_or_else(|_| "text/plain; charset=US-ASCII".parse().unwrap());
-
-    let mut bytes = percent_decode(parts[1].as_bytes()).collect::<Vec<_>>();
-    if is_base64 {
-        match forgiving_base64::decode_to_vec(&bytes) {
-            Err(..) => return Err(DecodeError::NonBase64DataUri),
-            Ok(data) => bytes = data,
-        }
-    }
-    Ok((content_type, bytes))
-}
 
 impl ProtocolHandler for DataProtocolHander {
     fn load(
@@ -78,20 +26,32 @@ impl ProtocolHandler for DataProtocolHander {
         _context: &FetchContext,
     ) -> Pin<Box<dyn Future<Output = Response> + Send>> {
         let url = request.current_url();
-        let response = match decode(&url) {
-            Ok((mime, bytes)) => {
-                let mut response =
-                    Response::new(url, ResourceFetchTiming::new(request.timing_type()));
-                *response.body.lock().unwrap() = ResponseBody::Done(bytes);
-                response.headers.typed_insert(ContentType::from(mime));
-                response.status = Some((StatusCode::OK, "OK".to_string()));
-                response.raw_status = Some((StatusCode::OK.as_u16(), b"OK".to_vec()));
-                response
+
+        assert_eq!(url.scheme(), "data");
+
+        let response = match DataUrl::process(url.clone().as_str()) {
+            Ok(data_url) => match data_url.decode_to_vec() {
+                Ok((bytes, _fragment_id)) => {
+                    let mut response =
+                        Response::new(url, ResourceFetchTiming::new(request.timing_type()));
+                    *response.body.lock().unwrap() = ResponseBody::Done(bytes);
+                    let mime = data_url.mime_type();
+                    response.headers.insert(
+                        http::header::CONTENT_TYPE,
+                        HeaderValue::from_str(&mime.to_string()).unwrap(),
+                    );
+                    response.status = Some((StatusCode::OK, "OK".to_string()));
+                    response.raw_status = Some((StatusCode::OK.as_u16(), b"OK".to_vec()));
+                    Some(response)
+                },
+                Err(_) => None,
             },
-            Err(_) => {
-                Response::network_error(NetworkError::Internal("Decoding data URL failed".into()))
-            },
-        };
+            Err(_) => None,
+        }
+        .unwrap_or_else(|| {
+            Response::network_error(NetworkError::Internal("Decoding data URL failed".into()))
+        });
+
         Box::pin(std::future::ready(response))
     }
 }
