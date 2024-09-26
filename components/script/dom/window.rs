2d2e34167ebe9b02e0c51ae2045140ddd7ca6598
Josh Matthews <josh@joshmatthews.net>
2024-09-25 21:34:48 -0400

    Reset document state notification when replacing a window.

--- components/script/dom/window.rs
+++ components/script/dom/window.rs
@@ -2687,6 +2687,7 @@ impl Window {
         });
         *self.task_manager.borrow_mut() = data.task_manager;
         *self.layout.borrow_mut() = data.layout;
+        self.has_sent_idle_message.set(false);
     }
 }
 
