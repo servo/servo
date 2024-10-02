56f0abeb8961bb0481c44cb7cf9f7fedd51d2938
tanishka <109246904+taniishkaaa@users.noreply.github.com>
2024-10-02 19:06:59 +0530

    clippy: Fix warnings in components/devtools/actors/inspector (#33618)
    Signed-off-by: taniishkaaa <tanishkasingh2004@gmail.com>

--- components/devtools/actors/inspector/style_rule.rs
+++ components/devtools/actors/inspector/style_rule.rs
@@ -93,9 +93,9 @@ impl Actor for StyleRuleActor {
     /// The style rule configuration actor can handle the following messages:
     ///
     /// - `setRuleText`: Applies a set of modifications to the css rules that this actor manages.
-    /// There is also `modifyProperties`, which has a slightly different API to do the same, but
-    /// this is preferred. Which one the devtools client sends is decided by the `traits` defined
-    /// when returning the list of rules.
+    ///   There is also `modifyProperties`, which has a slightly different API to do the same, but
+    ///   this is preferred. Which one the devtools client sends is decided by the `traits` defined
+    ///   when returning the list of rules.
     fn handle_message(
         &self,
         registry: &ActorRegistry,
