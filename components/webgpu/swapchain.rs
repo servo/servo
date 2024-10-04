7b8c864eedd2dc5825eee722db9b0483b1722fd2
Samson <16504129+sagudev@users.noreply.github.com>
2024-10-04 18:49:49 +0200

    Update components/webgpu/swapchain.rs
    Co-authored-by: Martin Robinson <mrobinson@igalia.com>
    Signed-off-by: Samson <16504129+sagudev@users.noreply.github.com>

--- components/webgpu/swapchain.rs
+++ components/webgpu/swapchain.rs
@@ -465,7 +465,7 @@ impl crate::WGPU {
             let _guard = self.poller.lock();
             global
                 .queue_submit(queue_id, &[command_buffer_id])
-                .map_err(|(_, err)| Error::from_error(err))?;
+                .map_err(|(_, error)| Error::from_error(error))?;
         }
         let callback = {
             let global = Arc::clone(&self.global);
