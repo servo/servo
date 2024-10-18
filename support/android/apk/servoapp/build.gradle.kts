0d6ac8d7c83f6b2d3106b12ab66e864de728b56a
clocks <doomsdayrs@gmail.com>
2024-10-18 18:25:00 -0400

    Move dependencies from servoview to servoapp
    ServoView does not use them.
    
    Signed-off-by: clocks <doomsdayrs@gmail.com>

--- support/android/apk/servoapp/build.gradle.kts
+++ support/android/apk/servoapp/build.gradle.kts
@@ -168,4 +168,8 @@ dependencies {
     } else {
         implementation(project(":servoview"))
     }
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
 }
--- support/android/apk/servoview/build.gradle.kts
+++ support/android/apk/servoview/build.gradle.kts
@@ -214,10 +214,6 @@ dependencies {
             }
         }
     }
-
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
 }
 
 // folderFilter can be used to improve search performance
