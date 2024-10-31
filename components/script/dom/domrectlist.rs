02f4fb20aa97fdb7abcfc7111db24583ce97f1e2
L Ashwin B <lashwinib@gmail.com>
2024-10-31 16:11:17 +0530

    suggested changes + updated interfaces.https.html + updated passing test expectations
    Signed-off-by: L Ashwin B <lashwinib@gmail.com>

--- components/script/dom/domrectlist.rs
+++ components/script/dom/domrectlist.rs
@@ -1,13 +1,12 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-
 use dom_struct::dom_struct;
 
 use crate::dom::bindings::cell::DomRefCell;
 use crate::dom::bindings::codegen::Bindings::DOMRectListBinding::DOMRectListMethods;
 use crate::dom::bindings::reflector::{reflect_dom_object_with_proto, DomObject, Reflector};
-use crate::dom::bindings::root::DomRoot;
+use crate::dom::bindings::root::{Dom, DomRoot};
 use crate::dom::domrect::DOMRect;
 use crate::dom::window::Window;
 use crate::script_runtime::CanGc;
@@ -15,14 +14,19 @@ use crate::script_runtime::CanGc;
 #[dom_struct]
 pub struct DOMRectList {
     reflector_: Reflector,
-    rects: DomRefCell<Vec<DomRoot<DOMRect>>>,
+    rects: DomRefCell<Vec<Dom<DOMRect>>>,
 }
 
 impl DOMRectList {
     fn new_inherited(rects: Vec<DomRoot<DOMRect>>) -> DOMRectList {
         DOMRectList {
             reflector_: Reflector::new(),
-            rects: DomRefCell::new(rects),
+            rects: DomRefCell::new(
+                rects
+                    .into_iter()
+                    .map(|dom_root| dom_root.as_traced())
+                    .collect(),
+            ),
         }
     }
 
@@ -39,20 +43,15 @@ impl DOMRectList {
         )
     }
 
-    #[allow(dead_code)]
-    pub fn empty(window: &Window, can_gc: CanGc) -> DomRoot<DOMRectList> {
-        DOMRectList::new(window, vec![], can_gc)
-    }
-
     pub fn first(&self) -> Option<DomRoot<DOMRect>> {
-        self.rects.borrow().first().cloned()
+        self.rects.borrow().first().map(Dom::as_rooted)
     }
 }
 
 impl DOMRectListMethods for DOMRectList {
     /// <https://drafts.fxtf.org/geometry/#DOMRectList>
     fn Item(&self, index: u32) -> Option<DomRoot<DOMRect>> {
-        self.rects.borrow().get(index as usize).cloned()
+        self.rects.borrow().get(index as usize).map(Dom::as_rooted)
     }
 
     /// <https://drafts.fxtf.org/geometry/#DOMRectList>
