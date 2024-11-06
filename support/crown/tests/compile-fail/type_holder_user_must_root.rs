5dfde6c9c2bb904c893cc9c34e9c7816b2b102f6
sagudev <16504129+sagudev@users.noreply.github.com>
2024-11-06 17:09:26 +0100

    make new test use RUSTC_BOOTSTRAP=1
    Signed-off-by: sagudev <16504129+sagudev@users.noreply.github.com>

--- support/crown/tests/compile-fail/type_holder_user_must_root.rs
+++ support/crown/tests/compile-fail/type_holder_user_must_root.rs
@@ -1,3 +1,8 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+//@rustc-env:RUSTC_BOOTSTRAP=1
+
 struct Foo(i32);
 
 struct Bar<TH: TypeHolderTrait>(TH::F);
