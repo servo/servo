fdb4d1594054a7f50a92ef9554f40076cbb7ef8b
Gae24 <96017547+Gae24@users.noreply.github.com>
2024-11-09 10:42:39 +0100

    implement DataTransfer interface
    implemented Constructor, setter and getter for drop_effect and effect_allowed, Items and SetDragImage
    
    Signed-off-by: Gae24 <96017547+Gae24@users.noreply.github.com>

--- components/script/dom/datatransfer.rs
+++ components/script/dom/datatransfer.rs
@@ -2,18 +2,51 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+use std::cell::Cell;
+
 use dom_struct::dom_struct;
 use js::rust::HandleObject;
 
 use crate::dom::bindings::cell::DomRefCell;
 use crate::dom::bindings::codegen::Bindings::DataTransferBinding::DataTransferMethods;
-use crate::dom::bindings::reflector::Reflector;
+use crate::dom::bindings::inheritance::Castable;
+use crate::dom::bindings::reflector::{reflect_dom_object_with_proto, Reflector};
 use crate::dom::bindings::root::{Dom, DomRoot};
 use crate::dom::bindings::str::DOMString;
 use crate::dom::datatransferitemlist::DataTransferItemList;
 use crate::dom::element::Element;
+use crate::dom::htmlimageelement::HTMLImageElement;
 use crate::dom::window::Window;
 use crate::script_runtime::CanGc;
+
+const VALID_DROP_EFFECTS: [&str; 4] = ["none", "copy", "link", "move"];
+const VALID_EFFECTS_ALLOWED: [&str; 9] = [
+    "none",
+    "copy",
+    "copyLink",
+    "copyMove",
+    "link",
+    "linkMove",
+    "move",
+    "all",
+    "uninitialized",
+];
+
+// An image that can be shown when a drag event occurs
+#[derive(JSTraceable, MallocSizeOf)]
+#[crown::unrooted_must_root_lint::must_root]
+struct DragBitmap {
+    image: Dom<HTMLImageElement>,
+    hot_spot_x: i32,
+    hot_spot_y: i32,
+}
+
+#[derive(Clone, Copy, Eq, JSTraceable, MallocSizeOf, PartialEq)]
+pub enum Mode {
+    ReadWrite,
+    ReadOnly,
+    Protected,
+}
 
 #[dom_struct]
 pub struct DataTransfer {
@@ -21,6 +54,39 @@ pub struct DataTransfer {
     drop_effect: DomRefCell<DOMString>,
     effect_allowed: DomRefCell<DOMString>,
     items: Dom<DataTransferItemList>,
+    drag_image: DomRefCell<Option<DragBitmap>>,
+    mode: Cell<Mode>,
+}
+
+impl DataTransfer {
+    pub fn new_inherited(item_list: &DataTransferItemList) -> DataTransfer {
+        DataTransfer {
+            reflector_: Reflector::new(),
+            drop_effect: DomRefCell::new(DOMString::from("none")),
+            effect_allowed: DomRefCell::new(DOMString::from("none")),
+            items: Dom::from_ref(item_list),
+            drag_image: DomRefCell::new(None),
+            mode: Cell::new(Mode::ReadWrite),
+        }
+    }
+
+    pub fn new_with_proto(
+        window: &Window,
+        proto: Option<HandleObject>,
+        can_gc: CanGc,
+    ) -> DomRoot<DataTransfer> {
+        let item_list = DataTransferItemList::new(window);
+        reflect_dom_object_with_proto(
+            Box::new(DataTransfer::new_inherited(&item_list)),
+            window,
+            proto,
+            can_gc,
+        )
+    }
+
+    pub fn can_write(&self) -> bool {
+        self.mode.get() == Mode::ReadWrite
+    }
 }
 
 impl DataTransferMethods for DataTransfer {
@@ -30,36 +96,54 @@ impl DataTransferMethods for DataTransfer {
         proto: Option<HandleObject>,
         can_gc: CanGc,
     ) -> DomRoot<DataTransfer> {
-        todo!()
+        DataTransfer::new_with_proto(window, proto, can_gc)
     }
 
     /// <https://html.spec.whatwg.org/multipage/#dom-datatransfer-dropeffect>
     fn DropEffect(&self) -> DOMString {
-        todo!()
+        self.drop_effect.borrow().clone()
     }
 
     /// <https://html.spec.whatwg.org/multipage/#dom-datatransfer-dropeffect>
     fn SetDropEffect(&self, value: DOMString) {
-        todo!()
+        if VALID_DROP_EFFECTS.contains(&value.as_ref()) {
+            *self.drop_effect.borrow_mut() = value;
+        }
     }
 
     /// <https://html.spec.whatwg.org/multipage/#dom-datatransfer-effectallowed>
     fn EffectAllowed(&self) -> DOMString {
-        todo!()
+        self.effect_allowed.borrow().clone()
     }
 
     /// <https://html.spec.whatwg.org/multipage/#dom-datatransfer-effectallowed>
     fn SetEffectAllowed(&self, value: DOMString) {
-        todo!()
+        if self.can_write() && VALID_EFFECTS_ALLOWED.contains(&value.as_ref()) {
+            *self.drop_effect.borrow_mut() = value;
+        }
     }
 
     /// <https://html.spec.whatwg.org/multipage/#dom-datatransfer-items>
     fn Items(&self) -> DomRoot<DataTransferItemList> {
-        todo!()
+        DomRoot::from_ref(&self.items)
     }
 
     /// <https://html.spec.whatwg.org/multipage/#dom-datatransfer-setdragimage>
     fn SetDragImage(&self, image: &Element, x: i32, y: i32) {
-        todo!()
+        // Step 1 If the DataTransfer is no longer associated with a data store, return. TODO
+
+        // Step 2 If the data store's mode is not the read/write mode, return.
+        if !self.can_write() {
+            return;
+        }
+
+        // Step 3
+        if let Some(image) = image.downcast::<HTMLImageElement>() {
+            *self.drag_image.borrow_mut() = Some(DragBitmap {
+                image: Dom::from_ref(image),
+                hot_spot_x: x,
+                hot_spot_y: y,
+            });
+        }
     }
 }
--- components/script/dom/datatransferitemlist.rs
+++ components/script/dom/datatransferitemlist.rs
@@ -5,15 +5,28 @@
 use dom_struct::dom_struct;
 
 use crate::dom::bindings::codegen::Bindings::DataTransferItemListBinding::DataTransferItemListMethods;
-use crate::dom::bindings::reflector::Reflector;
+use crate::dom::bindings::reflector::{reflect_dom_object, Reflector};
 use crate::dom::bindings::root::DomRoot;
 use crate::dom::bindings::str::DOMString;
 use crate::dom::datatransferitem::DataTransferItem;
 use crate::dom::file::File;
+use crate::dom::window::Window;
 
 #[dom_struct]
 pub struct DataTransferItemList {
     reflector_: Reflector,
+}
+
+impl DataTransferItemList {
+    pub fn new_inherited() -> DataTransferItemList {
+        DataTransferItemList {
+            reflector_: Reflector::new(),
+        }
+    }
+
+    pub fn new(window: &Window) -> DomRoot<DataTransferItemList> {
+        reflect_dom_object(Box::new(DataTransferItemList::new_inherited()), window)
+    }
 }
 
 impl DataTransferItemListMethods for DataTransferItemList {
