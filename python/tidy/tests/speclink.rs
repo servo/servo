b46e07061c8b124b1460eacd78175bf2b8fd5a3f
Josh Matthews <josh@joshmatthews.net>
2024-12-09 23:23:07 -0500

    tidy: Update pattern for checking for missing specification links.
    Signed-off-by: Josh Matthews <josh@joshmatthews.net>

--- python/tidy/tests/speclink.rs
+++ python/tidy/tests/speclink.rs
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-impl SpecLinkMethods for SpecLink {
+impl SpecLinkMethods<crate::DomTypeHolder> for SpecLink {
     amacro!("Macros inside impls should trigger spec checks.")
 
     // Method declarations should trigger spec checks.
