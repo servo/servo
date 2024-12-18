3e5b56d19d082b8e357786210390081169b1e18c
Jonathan Schwender <schwenderjonathan@gmail.com>
2024-12-18 13:28:15 +0100

    Add dockerfile for CI msrv builds
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>

---
+++ .github/docker/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null || exit 1
+
+#docker build \
+# --label "org.opencontainers.image.source=https://github.com/servo/servo" \
+# --label "org.opencontainers.image.description=Docker image for servo CI" \
+# --target servo_cooked_dev-crown-default_features \
+# --tag ghcr.io/jschwe/servo_ci_testing_dev-crown-default_features:latest \
+# .
+
+docker build \
+ --platform linux/amd64 \
+ --label "org.opencontainers.image.source=https://github.com/servo/servo" \
+ --label "org.opencontainers.image.description=Docker image for servo CI" \
+ --target servo_cooked_dev \
+ --tag ghcr.io/jschwe/servo_cooked_dev:latest \
+ .
