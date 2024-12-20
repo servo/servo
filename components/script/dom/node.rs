5ff807d89b28813251232b4124784618dba5e683
Simon Wülker <simon.wuelker@arcor.de>
2024-12-20 22:22:25 +0100

    Retarget EventTargets instead of Nodes
    Signed-off-by: Simon Wülker <simon.wuelker@arcor.de>

--- components/script/dom/node.rs
+++ components/script/dom/node.rs
@@ -1280,27 +1280,6 @@ impl Node {
         }
     }
 
-    /// <https://dom.spec.whatwg.org/#retarget>
-    pub fn retarget(&self, b: &Node) -> DomRoot<Node> {
-        let mut a = DomRoot::from_ref(self);
-        loop {
-            // Step 1.
-            let a_root = a.GetRootNode(&GetRootNodeOptions::empty());
-            if !a_root.is::<ShadowRoot>() || a_root.is_shadow_including_inclusive_ancestor_of(b) {
-                return DomRoot::from_ref(&a);
-            }
-
-            // Step 2.
-            a = DomRoot::from_ref(
-                a_root
-                    .downcast::<ShadowRoot>()
-                    .unwrap()
-                    .Host()
-                    .upcast::<Node>(),
-            );
-        }
-    }
-
     pub fn is_styled(&self) -> bool {
         self.style_data.borrow().is_some()
     }
