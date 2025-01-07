259c4f6dd979600e4678ddf5421972788acb7976
lazypassion <25536767+lazypassion@users.noreply.github.com>
2025-01-07 10:05:00 -0500

    remove unused code from FetchParams
    Signed-off-by: Lloyd Massiah <artmis9@protonmail.com>

--- components/net/fetch/fetch_params.rs
+++ components/net/fetch/fetch_params.rs
@@ -2,40 +2,19 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-use std::sync::{Arc, Mutex};
-
 use net_traits::request::Request;
 
-/// https://fetch.spec.whatwg.org/#fetch-params
+/// <https://fetch.spec.whatwg.org/#fetch-params>
 #[derive(Clone)]
 pub struct FetchParams {
-    /// https://fetch.spec.whatwg.org/#concept-request
+    /// <https://fetch.spec.whatwg.org/#concept-request>
     pub request: Request,
-    /// https://fetch.spec.whatwg.org/#fetch-controller
-    pub controller: Arc<Mutex<FetchController>>,
 }
 
 impl FetchParams {
     pub fn new(request: Request) -> FetchParams {
         FetchParams {
             request,
-            controller: Arc::new(Mutex::new(FetchController::default())),
         }
     }
-}
-
-/// https://fetch.spec.whatwg.org/#fetch-controller
-#[derive(Clone, Debug, Default)]
-pub struct FetchController {
-    /// https://fetch.spec.whatwg.org/#fetch-controller-state
-    pub state: FetchControllerState,
-}
-
-/// https://fetch.spec.whatwg.org/#fetch-controller-state
-#[derive(Clone, Copy, Debug, Default)]
-pub enum FetchControllerState {
-    #[default]
-    Ongoing,
-    Terminated,
-    Aborted,
 }
