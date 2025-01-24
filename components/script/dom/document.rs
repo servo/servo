ac22157491abcfa7aba2fc3a59de5cc4019c5a0b
Josh Matthews <josh@joshmatthews.net>
2025-01-23 20:14:16 -0500

    script: Handle shadow roots when determining common ancestors of dirty roots. (#35132)
    Signed-off-by: Josh Matthews <josh@joshmatthews.net>

--- components/script/dom/document.rs
+++ components/script/dom/document.rs
@@ -606,8 +606,15 @@ impl Document {
             ancestor.set_flag(NodeFlags::HAS_DIRTY_DESCENDANTS, has_dirty_descendants);
             has_dirty_descendants &= *ancestor != *new_dirty_root;
         }
-        self.dirty_root
-            .set(Some(new_dirty_root.downcast::<Element>().unwrap()));
+
+        let maybe_shadow_host = new_dirty_root
+            .downcast::<ShadowRoot>()
+            .map(ShadowRootMethods::Host);
+        let new_dirty_root_element = new_dirty_root
+            .downcast::<Element>()
+            .or(maybe_shadow_host.as_deref());
+
+        self.dirty_root.set(new_dirty_root_element);
     }
 
     pub(crate) fn take_dirty_root(&self) -> Option<DomRoot<Element>> {
