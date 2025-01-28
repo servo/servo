d1cd34ca91340bb5ba798a213e44270ddc253b58
Lloyd Massiah <artmis9@protonmail.com>
2025-01-28 16:06:07 -0500

    run formatter
    Signed-off-by: Lloyd Massiah <artmis9@protonmail.com>

--- components/net/http_loader.rs
+++ components/net/http_loader.rs
@@ -1825,7 +1825,6 @@ async fn http_network_fetch(
 ) -> Response {
     let mut response_end_timer = ResponseEndTimer(Some(context.timing.clone()));
 
-
     // Step 1: Let request be fetchParams’s request.
     let request = &mut fetch_params.request;
 
