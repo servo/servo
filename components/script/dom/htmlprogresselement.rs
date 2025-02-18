a3dc6ef335b021e6dda921853d235150a9a80303
Simon Wülker <simon.wuelker@arcor.de>
2025-02-18 23:07:11 +0100

    Fixup doccomments
    Signed-off-by: Simon Wülker <simon.wuelker@arcor.de>

--- components/script/dom/htmlprogresselement.rs
+++ components/script/dom/htmlprogresselement.rs
@@ -94,7 +94,7 @@ impl HTMLProgressElementMethods<crate::DomTypeHolder> for HTMLProgressElement {
         }
     }
 
-    // https://html.spec.whatwg.org/multipage/#dom-progress-max
+    /// <https://html.spec.whatwg.org/multipage/#dom-progress-max>
     fn Max(&self) -> Finite<f64> {
         // In case of missing `max`, parse error, or negative `max`, `max` should be interpreted as
         // 1.0. As `get_string_attribute` returns an empty string in case the attribute is missing,
@@ -126,7 +126,7 @@ impl HTMLProgressElementMethods<crate::DomTypeHolder> for HTMLProgressElement {
         }
     }
 
-    // https://html.spec.whatwg.org/multipage/#dom-progress-position
+    /// <https://html.spec.whatwg.org/multipage/#dom-progress-position>
     fn Position(&self) -> Finite<f64> {
         let value = self
             .upcast::<Element>()
