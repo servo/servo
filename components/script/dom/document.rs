00b8798b47200a4876d464d4edf20ae463b00303
batu_hoang <longvatrong111@gmail.com>
2025-02-22 06:06:33 +0800

    Set allowDeclarativeShadowRoots false for innerHTML
    Signed-off-by: batu_hoang <longvatrong111@gmail.com>

--- components/script/dom/document.rs
+++ components/script/dom/document.rs
@@ -4619,6 +4619,10 @@ impl Document {
     pub fn allow_declarative_shadow_roots(&self) -> bool {
         self.allow_declarative_shadow_roots.get()
     }
+
+    pub fn set_allow_declarative_shadow_roots(&self, value: bool) {
+        self.allow_declarative_shadow_roots.set(value)
+    }
 }
 
 impl ProfilerMetadataFactory for Document {
