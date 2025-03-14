RussPalms/servo_dev

libservo: Notify delegates of send errors in request objects
Signed-off-by: Delan Azabani <dazabani@igalia.com>

components/servo/lib.rs
@@ -18,6 +18,7 @@
 //! `WindowMethods` trait.
 
 mod clipboard_delegate;
+mod delegate;
 mod proxies;
 mod servo_delegate;
 mod webview;
@@ -121,8 +122,10 @@ pub use {
 pub use {bluetooth, bluetooth_traits};
 
 use crate::proxies::ConstellationProxy;
+use crate::servo_delegate::ServoErrorChannel;
 pub use crate::servo_delegate::{ServoDelegate, ServoError};
 pub use crate::webview::WebView;
+use crate::webview_delegate::WebViewErrorChannel;
 pub use crate::webview_delegate::{
     AllowOrDenyRequest, AuthenticationRequest, NavigationRequest, PermissionRequest,
     WebResourceLoad, WebViewDelegate,
@@ -208,6 +211,8 @@ pub struct Servo {
     /// When accessed, `Servo` will be reponsible for cleaning up the invalid `Weak`
     /// references.
     webviews: RefCell<HashMap<WebViewId, Weak<RefCell<WebViewInner>>>>,
+    servo_errors: ServoErrorChannel,
+    webview_errors: WebViewErrorChannel,
     /// For single-process Servo instances, this field controls the initialization
     /// and deinitialization of the JS Engine. Multiprocess Servo instances have their
     /// own instance that exists in the content process instead.
@@ -539,6 +544,8 @@ impl Servo {
             embedder_receiver,
             shutdown_state,
             webviews: Default::default(),
+            servo_errors: ServoErrorChannel::default(),
+            webview_errors: WebViewErrorChannel::default(),
             _js_engine_setup: js_engine_setup,
         }
     }
@@ -588,6 +595,7 @@ impl Servo {
 
         self.compositor.borrow_mut().perform_updates();
         self.send_new_frame_ready_messages();
+        self.handle_delegate_errors();
         self.clean_up_destroyed_webview_handles();
 
         if self.shutdown_state.get() == ShutdownState::FinishedShuttingDown {
@@ -609,6 +617,17 @@ impl Servo {
             .filter_map(WebView::from_weak_handle)
         {
             webview.delegate().notify_new_frame_ready(webview);
+        }
+    }
+
+    fn handle_delegate_errors(&self) {
+        while let Some(error) = self.servo_errors.try_recv() {
+            self.delegate().notify_error(self, error);
+        }
+        while let Some((webview, error)) = self.webview_errors.try_recv() {
+            if let Some(webview) = WebView::from_weak_handle(&webview) {
+                webview.delegate().notify_error(&webview, error);
+            }
         }
     }
 
@@ -766,7 +785,11 @@ impl Servo {
             },
             EmbedderMsg::AllowUnload(webview_id, response_sender) => {
                 if let Some(webview) = self.get_webview_handle(webview_id) {
-                    let request = AllowOrDenyRequest::new(response_sender, AllowOrDeny::Allow);
+                    let request = AllowOrDenyRequest::new(
+                        response_sender,
+                        AllowOrDeny::Allow,
+                        Box::new(self.webview_errors.sender_for_webview(&webview)),
+                    );
                     webview.delegate().request_unload(webview, request);
                 }
             },
@@ -832,12 +855,24 @@ impl Servo {
                 web_resource_request,
                 response_sender,
             ) => {
-                let web_resource_load = WebResourceLoad::new(web_resource_request, response_sender);
-                match webview_id.and_then(|webview_id| self.get_webview_handle(webview_id)) {
-                    Some(webview) => webview
+                if let Some(webview) =
+                    webview_id.and_then(|webview_id| self.get_webview_handle(webview_id))
+                {
+                    let web_resource_load = WebResourceLoad::new(
+                        web_resource_request,
+                        response_sender,
+                        Box::new(self.webview_errors.sender_for_webview(&webview)),
+                    );
+                    webview
                         .delegate()
-                        .load_web_resource(webview, web_resource_load),
-                    None => self.delegate().load_web_resource(web_resource_load),
+                        .load_web_resource(webview, web_resource_load);
+                } else {
+                    let web_resource_load = WebResourceLoad::new(
+                        web_resource_request,
+                        response_sender,
+                        Box::new(self.servo_errors.sender()),
+                    );
+                    self.delegate().load_web_resource(web_resource_load);
                 }
             },
             EmbedderMsg::Panic(webview_id, reason, backtrace) => {
@@ -872,9 +907,13 @@ impl Servo {
                 }
             },
             EmbedderMsg::RequestAuthentication(webview_id, url, for_proxy, response_sender) => {
-                let authentication_request =
-                    AuthenticationRequest::new(url.into_url(), for_proxy, response_sender);
                 if let Some(webview) = self.get_webview_handle(webview_id) {
+                    let authentication_request = AuthenticationRequest::new(
+                        url.into_url(),
+                        for_proxy,
+                        response_sender,
+                        self.webview_errors.sender_for_webview(&webview),
+                    );
                     webview
                         .delegate()
                         .request_authentication(webview, authentication_request);
@@ -887,6 +926,7 @@ impl Servo {
                         allow_deny_request: AllowOrDenyRequest::new(
                             response_sender,
                             AllowOrDeny::Deny,
+                            Box::new(self.webview_errors.sender_for_webview(&webview)),
                         ),
                     };
                     webview
@@ -929,7 +969,11 @@ impl Servo {
             EmbedderMsg::RequestDevtoolsConnection(response_sender) => {
                 self.delegate().request_devtools_connection(
                     self,
-                    AllowOrDenyRequest::new(response_sender, AllowOrDeny::Deny),
+                    AllowOrDenyRequest::new(
+                        response_sender,
+                        AllowOrDeny::Deny,
+                        Box::new(self.servo_errors.sender()),
+                    ),
                 );
             },
             EmbedderMsg::PlayGamepadHapticEffect(

components/servo/servo_delegate.rs
@@ -2,10 +2,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+use crossbeam_channel::{unbounded, Receiver, Sender, TryRecvError};
+use log::warn;
+
+use crate::delegate::DelegateErrorSender;
 use crate::webview_delegate::{AllowOrDenyRequest, WebResourceLoad};
 use crate::Servo;
 
-#[derive(Clone, Copy, Debug, Hash, PartialEq)]
+#[derive(Debug)]
 pub enum ServoError {
     /// The channel to the off-the-main-thread web engine has been lost. No further
     /// attempts to communicate will happen. This is an unrecoverable error in Servo.
@@ -13,6 +17,53 @@ pub enum ServoError {
     /// The devtools server, used to expose pages to remote web inspectors has failed
     /// to start.
     DevtoolsFailedToStart,
+    /// Failed to send response to delegate request.
+    ResponseSend(bincode::Error),
+}
+
+/// Channel for errors raised by [`WebViewDelegate`] request objects.
+///
+/// This allows errors to be raised asynchronously.
+pub(crate) struct ServoErrorChannel {
+    sender: Sender<ServoError>,
+    receiver: Receiver<ServoError>,
+}
+
+impl Default for ServoErrorChannel {
+    fn default() -> Self {
+        let (sender, receiver) = unbounded();
+        Self { sender, receiver }
+    }
+}
+
+impl ServoErrorChannel {
+    pub(crate) fn sender(&self) -> ServoErrorSender {
+        ServoErrorSender {
+            sender: self.sender.clone(),
+        }
+    }
+    pub(crate) fn try_recv(&self) -> Option<ServoError> {
+        match self.receiver.try_recv() {
+            Ok(result) => Some(result),
+            Err(error) => {
+                debug_assert_eq!(error, TryRecvError::Empty);
+                None
+            },
+        }
+    }
+}
+
+/// Sender for [`ServoError`] that identifies the [`WebView`] in question.
+pub(crate) struct ServoErrorSender {
+    sender: Sender<ServoError>,
+}
+
+impl DelegateErrorSender for ServoErrorSender {
+    fn raise_response_send_error(&self, error: bincode::Error) {
+        if let Err(error) = self.sender.send(ServoError::ResponseSend(error)) {
+            warn!("Failed to send Servo error: {error:?}");
+        }
+    }
 }
 
 pub trait ServoDelegate {

servo/servo

components/servo/lib.rs
@@ -19,6 +19,7 @@
 
 mod clipboard_delegate;
 mod proxies;
+mod responders;
 mod servo_delegate;
 mod webview;
 mod webview_delegate;
@@ -118,8 +119,10 @@ pub use {
 pub use {bluetooth, bluetooth_traits};
 
 use crate::proxies::ConstellationProxy;
+use crate::servo_delegate::ServoErrorChannel;
 pub use crate::servo_delegate::{ServoDelegate, ServoError};
 pub use crate::webview::WebView;
+use crate::webview_delegate::WebViewErrorChannel;
 pub use crate::webview_delegate::{
     AllowOrDenyRequest, AuthenticationRequest, NavigationRequest, PermissionRequest,
     WebResourceLoad, WebViewDelegate,
@@ -205,6 +208,8 @@ pub struct Servo {
     /// When accessed, `Servo` will be reponsible for cleaning up the invalid `Weak`
     /// references.
     webviews: RefCell<HashMap<WebViewId, Weak<RefCell<WebViewInner>>>>,
+    servo_errors: ServoErrorChannel,
+    webview_errors: WebViewErrorChannel,
     /// For single-process Servo instances, this field controls the initialization
     /// and deinitialization of the JS Engine. Multiprocess Servo instances have their
     /// own instance that exists in the content process instead.
@@ -536,6 +541,8 @@ impl Servo {
             embedder_receiver,
             shutdown_state,
             webviews: Default::default(),
+            servo_errors: ServoErrorChannel::default(),
+            webview_errors: WebViewErrorChannel::default(),
             _js_engine_setup: js_engine_setup,
         }
     }
@@ -585,6 +592,7 @@ impl Servo {
 
         self.compositor.borrow_mut().perform_updates();
         self.send_new_frame_ready_messages();
+        self.handle_delegate_errors();
         self.clean_up_destroyed_webview_handles();
 
         if self.shutdown_state.get() == ShutdownState::FinishedShuttingDown {
@@ -606,6 +614,17 @@ impl Servo {
             .filter_map(WebView::from_weak_handle)
         {
             webview.delegate().notify_new_frame_ready(webview);
+        }
+    }
+
+    fn handle_delegate_errors(&self) {
+        while let Some(error) = self.servo_errors.try_recv() {
+            self.delegate().notify_error(self, error);
+        }
+        while let Some((webview, error)) = self.webview_errors.try_recv() {
+            if let Some(webview) = WebView::from_weak_handle(&webview) {
+                webview.delegate().notify_error(&webview, error);
+            }
         }
     }
 
@@ -758,7 +777,11 @@ impl Servo {
             },
             EmbedderMsg::AllowUnload(webview_id, response_sender) => {
                 if let Some(webview) = self.get_webview_handle(webview_id) {
-                    let request = AllowOrDenyRequest::new(response_sender, AllowOrDeny::Allow);
+                    let request = AllowOrDenyRequest::new(
+                        response_sender,
+                        AllowOrDeny::Allow,
+                        Box::new(self.webview_errors.sender_for_webview(&webview)),
+                    );
                     webview.delegate().request_unload(webview, request);
                 }
             },
@@ -824,12 +847,24 @@ impl Servo {
                 web_resource_request,
                 response_sender,
             ) => {
-                let web_resource_load = WebResourceLoad::new(web_resource_request, response_sender);
-                match webview_id.and_then(|webview_id| self.get_webview_handle(webview_id)) {
-                    Some(webview) => webview
+                if let Some(webview) =
+                    webview_id.and_then(|webview_id| self.get_webview_handle(webview_id))
+                {
+                    let web_resource_load = WebResourceLoad::new(
+                        web_resource_request,
+                        response_sender,
+                        Box::new(self.webview_errors.sender_for_webview(&webview)),
+                    );
+                    webview
                         .delegate()
-                        .load_web_resource(webview, web_resource_load),
-                    None => self.delegate().load_web_resource(web_resource_load),
+                        .load_web_resource(webview, web_resource_load);
+                } else {
+                    let web_resource_load = WebResourceLoad::new(
+                        web_resource_request,
+                        response_sender,
+                        Box::new(self.servo_errors.sender()),
+                    );
+                    self.delegate().load_web_resource(web_resource_load);
                 }
             },
             EmbedderMsg::Panic(webview_id, reason, backtrace) => {
@@ -864,9 +899,13 @@ impl Servo {
                 }
             },
             EmbedderMsg::RequestAuthentication(webview_id, url, for_proxy, response_sender) => {
-                let authentication_request =
-                    AuthenticationRequest::new(url.into_url(), for_proxy, response_sender);
                 if let Some(webview) = self.get_webview_handle(webview_id) {
+                    let authentication_request = AuthenticationRequest::new(
+                        url.into_url(),
+                        for_proxy,
+                        response_sender,
+                        self.webview_errors.sender_for_webview(&webview),
+                    );
                     webview
                         .delegate()
                         .request_authentication(webview, authentication_request);
@@ -879,6 +918,7 @@ impl Servo {
                         allow_deny_request: AllowOrDenyRequest::new(
                             response_sender,
                             AllowOrDeny::Deny,
+                            Box::new(self.webview_errors.sender_for_webview(&webview)),
                         ),
                     };
                     webview
@@ -921,7 +961,11 @@ impl Servo {
             EmbedderMsg::RequestDevtoolsConnection(response_sender) => {
                 self.delegate().request_devtools_connection(
                     self,
-                    AllowOrDenyRequest::new(response_sender, AllowOrDeny::Deny),
+                    AllowOrDenyRequest::new(
+                        response_sender,
+                        AllowOrDeny::Deny,
+                        Box::new(self.servo_errors.sender()),
+                    ),
                 );
             },
             EmbedderMsg::PlayGamepadHapticEffect(

components/servo/servo_delegate.rs
@@ -2,10 +2,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+use crossbeam_channel::{Receiver, Sender, TryRecvError, unbounded};
+use log::warn;
+
 use crate::Servo;
+use crate::responders::DelegateErrorSender;
 use crate::webview_delegate::{AllowOrDenyRequest, WebResourceLoad};
 
-#[derive(Clone, Copy, Debug, Hash, PartialEq)]
+#[derive(Debug)]
 pub enum ServoError {
     /// The channel to the off-the-main-thread web engine has been lost. No further
     /// attempts to communicate will happen. This is an unrecoverable error in Servo.
@@ -13,6 +17,53 @@ pub enum ServoError {
     /// The devtools server, used to expose pages to remote web inspectors has failed
     /// to start.
     DevtoolsFailedToStart,
+    /// Failed to send response to delegate request.
+    ResponseSend(bincode::Error),
+}
+
+/// Channel for errors raised by [`WebViewDelegate`] request objects.
+///
+/// This allows errors to be raised asynchronously.
+pub(crate) struct ServoErrorChannel {
+    sender: Sender<ServoError>,
+    receiver: Receiver<ServoError>,
+}
+
+impl Default for ServoErrorChannel {
+    fn default() -> Self {
+        let (sender, receiver) = unbounded();
+        Self { sender, receiver }
+    }
+}
+
+impl ServoErrorChannel {
+    pub(crate) fn sender(&self) -> ServoErrorSender {
+        ServoErrorSender {
+            sender: self.sender.clone(),
+        }
+    }
+    pub(crate) fn try_recv(&self) -> Option<ServoError> {
+        match self.receiver.try_recv() {
+            Ok(result) => Some(result),
+            Err(error) => {
+                debug_assert_eq!(error, TryRecvError::Empty);
+                None
+            },
+        }
+    }
+}
+
+/// Sender for [`ServoError`] that identifies the [`WebView`] in question.
+pub(crate) struct ServoErrorSender {
+    sender: Sender<ServoError>,
+}
+
+impl DelegateErrorSender for ServoErrorSender {
+    fn raise_response_send_error(&self, error: bincode::Error) {
+        if let Err(error) = self.sender.send(ServoError::ResponseSend(error)) {
+            warn!("Failed to send Servo error: {error:?}");
+        }
+    }
 }
 
 pub trait ServoDelegate {