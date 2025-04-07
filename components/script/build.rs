6d35c2e09c26316b2e2eff4a9e23523b1fec51ff
sagudev <16504129+sagudev@users.noreply.github.com>
2025-04-07 18:03:55 +0200

    copy include! files from script_bindings's OUT_DIR, to script's OUT_DIR
    Signed-off-by: sagudev <16504129+sagudev@users.noreply.github.com>

--- components/script/build.rs
+++ components/script/build.rs
@@ -3,10 +3,45 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 use std::env;
+use std::path::{Path, PathBuf};
 
 fn main() {
-    println!(
-        "cargo:rustc-env=BINDINGS_OUT_DIR={}",
-        env::var("DEP_SCRIPT_BINDINGS_CRATE_OUT_DIR").unwrap(),
-    );
+    // copy include! files from script_bindings's OUT_DIR, to script's OUT_DIR
+    // this is done to bypass limitation of Rust Analyzer: https://github.com/rust-lang/rust-analyzer/issues/17040
+    let script_bindings_out_dir =
+        PathBuf::from(env::var_os("DEP_SCRIPT_BINDINGS_CRATE_OUT_DIR").unwrap());
+    let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    // copy concrete files
+    [
+        "InterfaceTypes.rs",
+        "DomTypeHolder.rs",
+        "InterfaceObjectMap.rs",
+        "ConcreteInheritTypes.rs",
+        "UnionTypes.rs",
+        "InterfaceObjectMapPhf.rs",
+    ]
+    .iter()
+    .map(Path::new)
+    .for_each(|file| {
+        std::fs::copy(
+            script_bindings_out_dir.join(file),
+            out_dir.join(file.file_name().unwrap()),
+        )
+        .unwrap();
+    });
+    // copy ConcreteBindings folder
+    let _ = std::fs::create_dir(out_dir.join("ConcreteBindings"));
+    std::fs::read_dir(script_bindings_out_dir.join("ConcreteBindings"))
+        .unwrap()
+        .filter_map(|res| res.map(|e| e.path()).ok())
+        .filter(|path| path.is_file())
+        .for_each(|file| {
+            std::fs::copy(
+                &file,
+                out_dir
+                    .join("ConcreteBindings")
+                    .join(file.file_name().unwrap()),
+            )
+            .unwrap();
+        });
 }
