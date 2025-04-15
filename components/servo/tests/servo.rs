738628280d8401ec4d70b9db7c253809866275fc
Martin Robinson <mrobinson@igalia.com>
2025-04-15 10:29:47 +0200

    libservo: Allow running more than on Servo test in a run
    A `Servo` instance can only be constructed once per program execution
    and cannot be passed between threads. This change adds a special thread
    to run `Servo` unit tests. This will allow creating suites of `WebView`
    unit tests.
    
    Signed-off-by: Martin Robinson <mrobinson@igalia.com>

--- components/servo/tests/servo.rs
+++ components/servo/tests/servo.rs
@@ -2,12 +2,31 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+//! Servo API unit tests.
+//!
+//! Since all Servo tests must rust serially on the same thread, it is important
+//! that tests never panic. In order to ensure this, use `anyhow::ensure!` instead
+//! of `assert!` for test assertions. `ensure!` will produce a `Result::Err` in
+//! place of panicking.
+
 mod common;
 
+use anyhow::ensure;
 use common::*;
+use servo::WebViewBuilder;
 
 #[test]
-fn test_simple_servo_start_and_stop() {
-    let shared_test = ServoTest::new();
-    assert!(!shared_test.servo().animating());
+fn test_simple_servo_is_not_animating_by_default() {
+    ServoTest::run(|servo_test| {
+        ensure!(!servo_test.servo().animating());
+        Ok(())
+    });
+}
+
+#[test]
+fn test_simple_servo_construct_webview() {
+    ServoTest::run(|servo_test| {
+        WebViewBuilder::new(servo_test.servo()).build();
+        Ok(())
+    });
 }
