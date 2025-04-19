fc16534c8dd0e3472a97a31c300f053bd5b9e9fe
pylbrecht <pylbrecht@mailbox.org>
2025-04-19 17:44:56 +0200

    Remove redundant trace!()
    Copy/paste leftover.
    
    Signed-off-by: pylbrecht <pylbrecht@mailbox.org>

--- components/net/http_loader.rs
+++ components/net/http_loader.rs
@@ -1985,8 +1985,6 @@ async fn http_network_fetch(
                 return Response::network_internal_error("cannot create request");
             };
 
-            trace!("starting WS connection to {}", url);
-
             let mut tls_config = create_tls_config(
                 context.ca_certificates.clone(),
                 context.ignore_certificate_errors,
