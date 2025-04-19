e192508e28c48afc34a7d62df523590d70ab27c5
pylbrecht <pylbrecht@mailbox.org>
2025-04-19 17:44:56 +0200

    Remove unnecessary FIXME
    We already call `apply_hsts_rules()` in `main_fetch()` beforehand.
    
    Signed-off-by: pylbrecht <pylbrecht@mailbox.org>

--- components/net/http_loader.rs
+++ components/net/http_loader.rs
@@ -1925,8 +1925,6 @@ async fn http_network_fetch(
                  websocket_chan.1.take().unwrap())
             };
 
-            // FIXME(pylbrecht): apply_hsts_rules()
-
             let mut req_url = request.url();
             if req_url.scheme() == "https" {
                 req_url.as_mut_url().set_scheme("wss").unwrap();
