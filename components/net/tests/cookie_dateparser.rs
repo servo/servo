4ee7a5257e3384b3f06a8dff84adc5b9812dae66
Kingsley Yung <kingsley@kkoyung.dev>
2025-04-21 16:49:35 +0800

    Formatting components/net/tests/cookie_dateparser.rs
    Signed-off-by: Kingsley Yung <kingsley@kkoyung.dev>

--- components/net/tests/cookie_dateparser.rs
+++ components/net/tests/cookie_dateparser.rs
@@ -3,11 +3,13 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 use net::cookie_dateparser::extract_expiry;
-use time::{error::ComponentRange, Date, Month, OffsetDateTime, Time};
+use time::error::ComponentRange;
+use time::{Date, Month, OffsetDateTime, Time};
 
 #[test]
 fn test_extract_expiry() -> Result<(), ComponentRange> {
-    assert_eq!(extract_expiry("test=1; expires=01 jan 2024 12:59:59 GMT; Path=/"),
+    assert_eq!(
+        extract_expiry("test=1; expires=01 jan 2024 12:59:59 GMT; Path=/"),
         Some(OffsetDateTime::new_utc(
             Date::from_calendar_date(2024, Month::January, 1)?,
             Time::from_hms(12, 59, 59)?,
