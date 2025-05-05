0858785b3517e0fa2184dd7a540d2e3a06016387
Kingsley Yung <kingsley@kkoyung.dev>
2025-05-05 21:13:26 +0800

    Remove `ref_filter_map` dependency
    Use `filter_map` function of `std::cell::Ref`, which provide the same
    functionality, instead.
    
    Replace `ref_filter_map::ref_filter_map` with a wrapper function, named
    `ref_filter_map`, that wrap `Ref::filter_map` and `Result`-to-`Option`
    conversion. Its function signature aligns with `ref_filter_map` from the
    crates `ref_filter_map` and `accountable_refcall`, so the caller can
    remain unchanged.
    
    Signed-off-by: Kingsley Yung <kingsley@kkoyung.dev>

--- components/script/dom/bindings/cell.rs
+++ components/script/dom/bindings/cell.rs
@@ -7,11 +7,16 @@
 use std::cell::{BorrowError, BorrowMutError};
 #[cfg(not(feature = "refcell_backtrace"))]
 pub(crate) use std::cell::{Ref, RefCell, RefMut};
+#[cfg(not(feature = "refcell_backtrace"))]
+pub(crate) fn ref_filter_map<T: ?Sized, U: ?Sized, F: FnOnce(&T) -> Option<&U>>(
+    orig: Ref<'_, T>,
+    f: F,
+) -> Option<Ref<'_, U>> {
+    Ref::filter_map(orig, f).ok()
+}
 
 #[cfg(feature = "refcell_backtrace")]
 pub(crate) use accountable_refcell::{Ref, RefCell, RefMut, ref_filter_map};
-#[cfg(not(feature = "refcell_backtrace"))]
-pub(crate) use ref_filter_map::ref_filter_map;
 
 use crate::dom::bindings::root::{assert_in_layout, assert_in_script};
 
