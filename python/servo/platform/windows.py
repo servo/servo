6f36eb5192cda8b0f05da3067aeba7a5c46ea94b
Jonathan Schwender <schwenderjonathan@gmail.com>
2025-05-08 19:35:17 +0200

    Switch to winget install instead of winget import
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>

--- python/servo/platform/windows.py
+++ python/servo/platform/windows.py
@@ -29,10 +29,48 @@ GSTREAMER_URL = f"{DEPS_URL}/gstreamer-1.0-msvc-x86_64-1.22.8.msi"
 GSTREAMER_DEVEL_URL = f"{DEPS_URL}/gstreamer-1.0-devel-msvc-x86_64-1.22.8.msi"
 DEPENDENCIES_DIR = os.path.join(util.get_target_dir(), "dependencies")
 
+WINGET_DEPENDENCIES = [
+    "Kitware.CMake",
+    "LLVM.LLVM",
+    "Ninja-build.Ninja",
+    "WiXToolset.WiXToolset"
+]
 
 def get_dependency_dir(package):
     """Get the directory that a given Windows dependency should extract to."""
     return os.path.join(DEPENDENCIES_DIR, package, DEPENDENCIES[package])
+
+
+def _winget_import():
+    try:
+        # We install tools like LLVM / CMake, so we probably don't want to force-upgrade
+        # a user installed version without good reason.
+        cmd =  ['winget','install', '--no-upgrade', '--interactive']
+        cmd.extend(WINGET_DEPENDENCIES)
+        # output is printed to the terminal that `./mach bootstrap` is running in
+        subprocess.run(cmd, encoding='utf-8')
+    except subprocess.CalledProcessError as e:
+        print("Could not run winget.  Follow manual build setup instructions.")
+        raise e
+
+
+def _choco_install(force: bool = False):
+    try:
+        choco_config = os.path.join(util.SERVO_ROOT, "support", "windows", "chocolatey.config")
+
+        # This is the format that PowerShell wants arguments passed to it.
+        cmd_exe_args = f"'/K','choco','install','-y', '\"{choco_config}\"'"
+        if force:
+            cmd_exe_args += ",'-f'"
+
+        print(cmd_exe_args)
+        subprocess.check_output([
+            "powershell", "Start-Process", "-Wait", "-verb", "runAs",
+            "cmd.exe", "-ArgumentList", f"@({cmd_exe_args})"
+        ]).decode("utf-8")
+    except subprocess.CalledProcessError as e:
+        print("Could not run chocolatey.  Follow manual build setup instructions.")
+        raise e
 
 
 class Windows(Base):
@@ -66,41 +104,9 @@ class Windows(Base):
         installed_something = self.passive_bootstrap()
         # If `winget` works well in practice, we could switch the default in the future.
         if shutil.which('choco') is not None:
-            self._choco_install(force)
+            _choco_install(force)
         else:
-            self._winget_import()
-
-    def _winget_import(self):
-        try:
-            winget_config = os.path.join(util.SERVO_ROOT, "support", "windows", "winget.json")
-
-            # Todo: Discuss using `--no-upgrade`
-            cmd_exe_args = f"'/K','winget','import','--import-file','{str(winget_config)}'"
-            print(cmd_exe_args)
-            subprocess.check_output(["powershell", "Start-Process", "-Wait", "-verb", "runAs",
-                                     "cmd.exe", "-ArgumentList", f"@({cmd_exe_args})"],
-                                    encoding='utf-8')
-        except subprocess.CalledProcessError as e:
-            print("Could not run winget.  Follow manual build setup instructions.")
-            raise e
-
-    def _choco_install(self, force: bool = False):
-        try:
-            choco_config = os.path.join(util.SERVO_ROOT, "support", "windows", "chocolatey.config")
-
-            # This is the format that PowerShell wants arguments passed to it.
-            cmd_exe_args = f"'/K','choco','install','-y', '\"{choco_config}\"'"
-            if force:
-                cmd_exe_args += ",'-f'"
-
-            print(cmd_exe_args)
-            subprocess.check_output([
-                "powershell", "Start-Process", "-Wait", "-verb", "runAs",
-                "cmd.exe", "-ArgumentList", f"@({cmd_exe_args})"
-            ]).decode("utf-8")
-        except subprocess.CalledProcessError as e:
-            print("Could not run chocolatey.  Follow manual build setup instructions.")
-            raise e
+            _winget_import()
 
         target = BuildTarget.from_triple(None)
         installed_something |= self._platform_bootstrap_gstreamer(target, force)
