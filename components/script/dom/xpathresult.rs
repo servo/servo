bc72d7e5369c955dcac78d07008c6e932359c1d2
Ville Lindholm <ville@lindholm.dev>
2025-06-01 14:35:59 +0300

    test-tidy
    Signed-off-by: Ville Lindholm <ville@lindholm.dev>

--- components/script/dom/xpathresult.rs
+++ components/script/dom/xpathresult.rs
@@ -54,7 +54,7 @@ impl TryFrom<u16> for XPathResultType {
     }
 }
 
-#[derive(JSTraceable, MallocSizeOf, Debug)]
+#[derive(Debug, JSTraceable, MallocSizeOf)]
 pub(crate) enum XPathResultValue {
     Boolean(bool),
     /// A IEEE-754 double-precision floating point number
