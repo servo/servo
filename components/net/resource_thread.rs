22f53716a848866f5b59a83d7271a0f3cf350360
pylbrecht <pylbrecht@mailbox.org>
2025-06-17 16:12:31 +0200

    Spike integration of websockets into fetch
    This is a *very rough* spike of integrating websockets into the main
    fetch implementation. We just copied all websocket-specific bits into
    the fetch code and made all failing WPT tests pass. 
    
    This commit serves as a checkpoint. We will clean up the implementation
    next (moving things where they belong, taking care of the unused
    functions in `components/net/websocket_loader.rs`, etc.).
    
    Signed-off-by: pylbrecht <pylbrecht@mailbox.org>

--- components/net/resource_thread.rs
+++ components/net/resource_thread.rs
@@ -59,7 +59,6 @@ use crate::http_cache::HttpCache;
 use crate::http_loader::{HttpState, http_redirect_fetch};
 use crate::protocols::ProtocolRegistry;
 use crate::request_interceptor::RequestInterceptor;
-use crate::websocket_loader;
 
 /// Load a file with CA certificate and produce a RootCertStore with the results.
 fn load_root_cert_store_from_file(file_path: String) -> io::Result<RootCertStore> {
@@ -370,12 +369,19 @@ impl ResourceChannelManager {
                 FetchChannels::WebSocket {
                     event_sender,
                     action_receiver,
-                } => self.resource_manager.websocket_connect(
-                    request_builder,
-                    event_sender,
-                    action_receiver,
-                    http_state,
-                ),
+                } => {
+                    let cancellation_listener =
+                        self.get_or_create_cancellation_listener(request_builder.id);
+
+                    self.resource_manager.websocket_connect(
+                        request_builder,
+                        event_sender,
+                        action_receiver,
+                        http_state,
+                        cancellation_listener,
+                        protocols,
+                    )
+                },
                 FetchChannels::Prefetch => self.resource_manager.fetch(
                     request_builder,
                     None,
@@ -667,6 +673,9 @@ impl CoreResourceManager {
             _ => (FileTokenCheck::NotRequired, None),
         };
 
+        let ca_certificates = self.ca_certificates.clone();
+        let ignore_certificate_errors = self.ignore_certificate_errors;
+
         spawn_task(async move {
             // XXXManishearth: Check origin against pipeline id (also ensure that the mode is allowed)
             // todo load context / mimesniff in fetch
@@ -682,6 +691,9 @@ impl CoreResourceManager {
                 cancellation_listener,
                 timing: ServoArc::new(Mutex::new(ResourceFetchTiming::new(request.timing_type()))),
                 protocols,
+                websocket_chan: None,
+                ca_certificates,
+                ignore_certificate_errors,
             };
 
             match res_init_ {
@@ -722,14 +734,39 @@ impl CoreResourceManager {
         event_sender: IpcSender<WebSocketNetworkEvent>,
         action_receiver: IpcReceiver<WebSocketDomAction>,
         http_state: &Arc<HttpState>,
+        cancellation_listener: Arc<CancellationListener>,
+        protocols: Arc<ProtocolRegistry>,
     ) {
-        websocket_loader::init(
-            request,
-            event_sender,
-            action_receiver,
-            http_state.clone(),
-            self.ca_certificates.clone(),
-            self.ignore_certificate_errors,
-        );
+        let http_state = http_state.clone();
+        let filemanager = self.filemanager.clone();
+        let request_interceptor = self.request_interceptor.clone();
+
+        let ca_certificates = self.ca_certificates.clone();
+        let ignore_certificate_errors = self.ignore_certificate_errors;
+
+        spawn_task(async move {
+            let context = FetchContext {
+                state: http_state,
+                user_agent: servo_config::pref!(user_agent),
+                devtools_chan: None,
+                filemanager: Arc::new(Mutex::new(filemanager)),
+                file_token: FileTokenCheck::NotRequired, // FIXME(pylbrecht)
+                request_interceptor: Arc::new(Mutex::new(request_interceptor)),
+                cancellation_listener,
+                timing: ServoArc::new(Mutex::new(ResourceFetchTiming::new(
+                    ResourceTimingType::None, // FIXME(pylbrecht)
+                ))),
+                protocols,
+                websocket_chan: Some(Arc::new(Mutex::new((
+                    event_sender.clone(),
+                    Some(action_receiver),
+                )))),
+                ca_certificates,
+                ignore_certificate_errors,
+            };
+
+            let mut event_sender = event_sender;
+            fetch(request.build(), &mut event_sender, &context).await;
+        });
     }
 }
--- components/net/websocket_loader.rs
+++ components/net/websocket_loader.rs
@@ -43,7 +43,7 @@ use crate::async_runtime::spawn_task;
 use crate::connector::{CACertificates, TlsConfig, create_tls_config};
 use crate::cookie::ServoCookie;
 use crate::fetch::methods::{
-    convert_request_to_csp_request, should_request_be_blocked_by_csp,
+    FetchContext, convert_request_to_csp_request, should_request_be_blocked_by_csp,
     should_request_be_blocked_due_to_a_bad_port,
 };
 use crate::hosts::replace_host;
@@ -55,7 +55,7 @@ use crate::http_loader::HttpState;
 /// and `Cookie` headers as appropriate.
 /// Returns an error if any header values are invalid or tungstenite cannot create
 /// the desired request.
-fn create_request(
+pub fn create_request(
     resource_url: &ServoUrl,
     origin: &str,
     protocols: &[String],
@@ -112,7 +112,7 @@ fn create_request(
 /// This ensures that any `Cookie` or HSTS headers are recognized.
 /// Returns an error if the protocol selected by the handshake doesn't
 /// match the list of provided protocols in the original request.
-fn process_ws_response(
+pub fn process_ws_response(
     http_state: &HttpState,
     response: &Response,
     resource_url: &ServoUrl,
@@ -152,14 +152,14 @@ fn process_ws_response(
 }
 
 #[derive(Debug)]
-enum DomMsg {
+pub(crate) enum DomMsg {
     Send(Message),
     Close(Option<(u16, String)>),
 }
 
 /// Initialize a listener for DOM actions. These are routed from the IPC channel
 /// to a tokio channel that the main WS client task uses to receive them.
-fn setup_dom_listener(
+pub fn setup_dom_listener(
     dom_action_receiver: IpcReceiver<WebSocketDomAction>,
     initiated_close: Arc<AtomicBool>,
 ) -> UnboundedReceiver<DomMsg> {
@@ -201,7 +201,7 @@ fn setup_dom_listener(
 /// closes the connection or an error occurs. Since this is an async
 /// function that uses the select operation, it will run as a task
 /// on the WS tokio runtime.
-async fn run_ws_loop(
+pub async fn run_ws_loop(
     mut dom_receiver: UnboundedReceiver<DomMsg>,
     resource_event_sender: IpcSender<WebSocketNetworkEvent>,
     mut stream: WebSocketStream<ConnectStream>,
@@ -452,6 +452,7 @@ pub fn init(
     http_state: Arc<HttpState>,
     ca_certificates: CACertificates,
     ignore_certificate_errors: bool,
+    _context: FetchContext,
 ) {
     let resource_event_sender2 = resource_event_sender.clone();
     if let Err(e) = connect(
