4ba03b7ee38e6be9b011f447b24814b75d76e64a
Jonathan Schwender <schwenderjonathan@gmail.com>
2025-06-19 09:38:42 +0200

    wip: docker
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>

--- .github/docker/build.sh
+++ .github/docker/build.sh
@@ -13,6 +13,5 @@ docker build \
  --platform linux/amd64 \
  --label "org.opencontainers.image.source=https://github.com/servo/servo" \
  --label "org.opencontainers.image.description=Docker image for servo CI" \
- --target servo_cooked_dev \
- --tag ghcr.io/jschwe/servo_cooked_dev:latest \
+ --tag ghcr.io/jschwe/servo_msrv_ci:latest \
  .
