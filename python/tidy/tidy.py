f07c500e98c86053f57b3bb50b719b2aeae439dc
Jerens Lensun <jerensslensun@gmail.com>
2025-06-20 12:39:25 +0800

    add multiprocessing for paralel linting execution on test-tidy
    Signed-off-by: Jerens Lensun <jerensslensun@gmail.com>

--- python/tidy/tidy.py
+++ python/tidy/tidy.py
@@ -13,18 +13,20 @@ import glob
 import io
 import itertools
 import json
+import multiprocessing
 import os
+import queue  # for queue.Empty
 import re
 import subprocess
 import sys
+from multiprocessing import Process, Queue
 from typing import Any, Dict, List
 
 import colorama
 import toml
-
 import wpt.manifestupdate
 
-from .licenseck import OLD_MPL, MPL, APACHE, COPYRIGHT, licenses_toml
+from .licenseck import APACHE, COPYRIGHT, MPL, OLD_MPL, licenses_toml
 
 TOPDIR = os.path.abspath(os.path.dirname(sys.argv[0]))
 WPT_PATH = os.path.join(".", "tests", "wpt")
@@ -982,6 +984,23 @@ def collect_errors_for_files(files_to_check, checking_functions, line_checking_f
                     yield (filename,) + error
 
 
+def do_job(tasks_to_accomplish, tasks_that_are_done):
+    while True:
+        try:
+            task = tasks_to_accomplish.get_nowait()
+        except queue.Empty:
+            break
+        else:
+            gen_fn, args = task
+
+            try:
+                result = list(gen_fn(*args))
+                tasks_that_are_done.put(result)
+            except Exception as e:
+                tasks_that_are_done.put([f"Error from multiprocess: {e} from {gen_fn.__name__}"])
+    return True
+
+
 def scan(only_changed_files=False, progress=False):
     # check config file for errors
     config_errors = check_config_file(CONFIG_FILE_PATH)
@@ -1001,12 +1020,35 @@ def scan(only_changed_files=False, progress=False):
     )
     file_errors = collect_errors_for_files(files_to_check, checking_functions, line_checking_functions)
 
-    python_errors = check_ruff_lints()
-    cargo_lock_errors = run_cargo_deny_lints()
-    wpt_errors = run_wpt_lints(only_changed_files)
+    # Initialize Parallel
+    tasks_to_accomplish = Queue()
+    tasks_that_are_done = Queue()
+    num_of_core = multiprocessing.cpu_count()
 
-    # chain all the iterators
-    errors = itertools.chain(config_errors, directory_errors, file_errors, python_errors, wpt_errors, cargo_lock_errors)
+    jobs = [
+        (check_ruff_lints, ()),
+        (run_cargo_deny_lints, ()),
+        (run_wpt_lints, (only_changed_files)),
+    ]
+
+    for job in jobs:
+        tasks_to_accomplish.put(job)
+
+    processes = []
+    for _ in range(num_of_core):
+        p = Process(target=do_job, args=(tasks_to_accomplish, tasks_that_are_done))
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join()
+
+    # Collect all generator results
+    all_results = itertools.chain(config_errors, file_errors)
+    while not tasks_that_are_done.empty():
+        all_results.append(tasks_that_are_done.get())
+
+    errors = itertools.chain(config_errors, file_errors, *all_results)
 
     colorama.init()
     error = None
