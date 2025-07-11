00b8e58f3b114ec47c0e9a3ec4c0a42a96e1c8ad
Martin Robinson <mrobinson@igalia.com>
2025-07-11 19:49:26 +0200

    Fix typo
    Signed-off-by: Martin Robinson <mrobinson@igalia.com>

--- components/shared/embedder/lib.rs
+++ components/shared/embedder/lib.rs
@@ -772,7 +772,7 @@ pub struct ScreenGeometry {
     /// toolbars, docks, and interface elements. This will be converted to
     /// CSS pixels based on the pixel scaling of the `WebView`.
     pub available_size: DeviceIntSize,
-    /// The rectangle the `WebView`'s containing in device pixels for the purposes of the
+    /// The rectangle the `WebView`'s containing window in device pixels for the purposes of the
     /// `window.screenLeft` and similar APIs. This will be converted to CSS pixels based
     /// on the pixel scaling of the `WebView`.
     pub window_rect: DeviceIntRect,
