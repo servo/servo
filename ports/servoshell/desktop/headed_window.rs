51ed58bf02b72a4afb37505a3b1d6aa920a88186
Michael Mc Donnell <michael@mcdonnell.dk>
2025-07-13 16:13:44 -0400

    Gate for Linux and prefer Wayland over X11
    Two issue came up during the review. I had forgotten to gate the change
    for Linux. The other was import the Wayland window attributes or the X11
    attributes. They both call the exact same code but I think importing the
    Wayland version might be better since X11 is being deprecated by several
    distributions.
    
    Signed-off-by: Michael Mc Donnell <michael@mcdonnell.dk>

--- ports/servoshell/desktop/headed_window.rs
+++ ports/servoshell/desktop/headed_window.rs
@@ -33,7 +33,8 @@ use winit::event::{
 };
 use winit::event_loop::ActiveEventLoop;
 use winit::keyboard::{Key as LogicalKey, ModifiersState, NamedKey};
-use winit::platform::x11::WindowAttributesExtX11;
+#[cfg(target_os = "linux")]
+use winit::platform::wayland::WindowAttributesExtWayland;
 #[cfg(any(target_os = "linux", target_os = "windows"))]
 use winit::window::Icon;
 #[cfg(target_os = "macos")]
@@ -95,9 +96,11 @@ impl Window {
             .with_min_inner_size(LogicalSize::new(1, 1))
             // Must be invisible at startup; accesskit_winit setup needs to
             // happen before the window is shown for the first time.
-            .with_visible(false)
-            // Set a name so it can be pinned to taskbars in Linux.
-            .with_name("org.servo.Servo", "Servo");
+            .with_visible(false);
+
+        // Set a name so it can be pinned to taskbars in Linux.
+        #[cfg(target_os = "linux")]
+        let window_attr = window_attr.with_name("org.servo.Servo", "Servo");
 
         #[allow(deprecated)]
         let winit_window = event_loop
