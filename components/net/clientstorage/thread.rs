52f80abe1617e9ec2184c3eae43fa373d452b134
Jan Varga <jan.varga@gmail.com>
2025-07-14 20:10:43 +0200

    net: Use ROUTER for receiving messages in ClientStorageThread
    This change replaces direct IpcReceiver usage in ClientStorageThread with
    ROUTER-based registration. The ROUTER runs on the IPC thread, so incoming IPC
    messages are received and deserialized off-thread, then forwarded to the
    clientstorage thread via a crossbeam channel.
    
    This cleanly separates I/O from message processing and prepares the
    infrastructure for supporting dynamic registration of additional receivers,
    including independently created top-level actors with their own dedicated IPC
    channels.
    
    Signed-off-by: Jan Varga <jan.varga@gmail.com>

--- components/net/clientstorage/thread.rs
+++ components/net/clientstorage/thread.rs
@@ -4,11 +4,14 @@
 
 use std::path::PathBuf;
 
+use crossbeam_channel::unbounded;
 use ipc_channel::ipc::IpcReceiver;
+use ipc_channel::router::ROUTER;
 use net_traits::clientstorage::thread_msg::ClientStorageThreadMsg;
 
 pub struct ClientStorageThread {
     _base_dir: PathBuf,
+    exiting: bool,
 }
 
 impl ClientStorageThread {
@@ -19,18 +22,41 @@ impl ClientStorageThread {
 
         ClientStorageThread {
             _base_dir: base_dir,
+            exiting: false,
         }
     }
 
     pub fn start(&mut self, ipc_receiver: IpcReceiver<ClientStorageThreadMsg>) {
-        #[allow(clippy::never_loop)]
+        let (msg_sender, msg_receiver) = unbounded();
+
+        ROUTER.add_typed_route(
+            ipc_receiver,
+            Box::new(move |result| {
+                let _ = msg_sender.send(result.unwrap());
+            }),
+        );
+
         loop {
-            match ipc_receiver.recv().unwrap() {
-                ClientStorageThreadMsg::Exit(sender) => {
-                    let _ = sender.send(());
-                    return;
-                },
+            let msg = msg_receiver.recv().unwrap();
+
+            self.recv_thread_message(msg);
+
+            if self.exiting {
+                break;
             }
         }
+    }
+
+    fn recv_thread_message(&mut self, msg: ClientStorageThreadMsg) {
+        match msg {
+            ClientStorageThreadMsg::Exit(sender) => {
+                self.recv_exit();
+                let _ = sender.send(());
+            },
+        }
+    }
+
+    fn recv_exit(&mut self) {
+        self.exiting = true;
     }
 }
