7fe06294b9c390c00893716a1e1037fb519c867d
minghuaw <wuminghua7@huawei.com>
2025-07-14 17:11:39 +0800

    impl decode_and_qneue_a_chunk
    Signed-off-by: minghuaw <wuminghua7@huawei.com>

--- components/script/dom/textdecoderstream.rs
+++ components/script/dom/textdecoderstream.rs
@@ -6,9 +6,14 @@ use std::cell::RefCell;
 
 use dom_struct::dom_struct;
 use encoding_rs::{Decoder, Encoding};
-use js::rust::HandleObject;
+use js::conversions::{FromJSValConvertible, ToJSValConvertible};
+use js::jsapi::JSContext;
+use js::jsval::UndefinedValue;
+use js::rust::{HandleObject as SafeHandleObject, HandleValue as SafeHandleValue};
+use script_bindings::codegen::GenericBindings::TransformStreamDefaultControllerBinding::TransformStreamDefaultControllerMethods;
 
-use crate::dom::types::TransformStream;
+use crate::dom::bindings::codegen::UnionTypes::ArrayBufferOrArrayBufferView;
+use crate::dom::types::{TransformStream, TransformStreamDefaultController};
 use crate::DomTypes;
 use crate::dom::bindings::root::{Dom, DomRoot};
 use crate::dom::bindings::codegen::Bindings::TextDecoderBinding;
@@ -17,7 +22,7 @@ use crate::dom::bindings::error::{Error, Fallible};
 use crate::dom::bindings::reflector::{Reflector, reflect_dom_object_with_proto};
 use crate::dom::bindings::str::DOMString;
 use crate::dom::globalscope::GlobalScope;
-use crate::script_runtime::CanGc;
+use crate::script_runtime::{CanGc, JSContext as SafeJSContext};
 
 #[dom_struct]
 #[allow(non_snake_case)]
@@ -30,6 +35,7 @@ pub(crate) struct TextDecoderStream {
     #[ignore_malloc_size_of = "defined in encoding_rs"]
     #[no_trace]
     decoder: RefCell<Decoder>,
+    in_stream: RefCell<Vec<u8>>,
     transform_stream: Dom<TransformStream>,
 }
 
@@ -48,13 +54,14 @@ impl TextDecoderStream {
             fatal,
             ignoreBOM,
             decoder: RefCell::new(decoder),
+            in_stream: RefCell::new(Vec::new()),
             transform_stream: Dom::from_ref(transform_stream)
         }
     }
 
     fn new_with_proto(
         global: &GlobalScope,
-        proto: Option<HandleObject>,
+        proto: Option<SafeHandleObject>,
         encoding: &'static Encoding,
         fatal: bool,
         ignoreBOM: bool,
@@ -72,8 +79,49 @@ impl TextDecoderStream {
     }
 
     // https://encoding.spec.whatwg.org/#decode-and-enqueue-a-chunk
-    fn decode_and_enqueue_a_chunk(&self, chunk: &[u8]) {
-        todo!()
+    #[allow(unsafe_code)]
+    fn decode_and_enqueue_a_chunk(
+        &self, 
+        cx: SafeJSContext,
+        chunk: SafeHandleValue, 
+        controller: &TransformStreamDefaultController,
+        can_gc: CanGc,
+    ) -> Fallible<()> {
+        // Step 1. Let bufferSource be the result of converting chunk to an AllowSharedBufferSource. 
+        let conversion_result = unsafe {
+            ArrayBufferOrArrayBufferView::from_jsval(*cx, chunk, ())
+                .map_err(|_| Error::Type("Unable to convert chunk into ArrayBuffer or ArrayBufferView".to_string()))?
+        };
+        let buffer_source = conversion_result.get_success_value()
+            .ok_or(Error::Type("Unable to convert chunk into ArrayBuffer or ArrayBufferView".to_string()))?;
+
+        // Step 2. Push a copy of bufferSource to decoder’s I/O queue. 
+        unsafe {
+            match buffer_source {
+                ArrayBufferOrArrayBufferView::ArrayBuffer(a) => self.in_stream.borrow_mut().extend_from_slice(a.as_slice()),
+                ArrayBufferOrArrayBufferView::ArrayBufferView(a) => self.in_stream.borrow_mut().extend_from_slice(a.as_slice()),
+            }
+        }
+
+        // Step 3. Let output be the I/O queue of scalar values « end-of-queue ». 
+        let mut decoder = self.decoder.borrow_mut();
+        let mut in_stream = self.in_stream.borrow_mut();
+        let mut output_chunk = String::with_capacity(
+            decoder.max_utf8_buffer_length(in_stream.len())
+                .expect("failed to calculate MAX UTF8 buffer length")
+        );
+
+        // Step 4.
+        let (_result, read, _replaced) = decoder.decode_to_string(&in_stream, &mut output_chunk, false);
+        let mut remaining = in_stream.split_off(read);
+        std::mem::swap(&mut* in_stream, &mut remaining);
+
+        rooted!(in(*cx) let mut rval = UndefinedValue());
+        unsafe { output_chunk.to_jsval(*cx, rval.handle_mut()) };
+
+        controller.Enqueue(cx, rval.handle(), can_gc)?;
+
+        Ok(())
     }
 
     // https://encoding.spec.whatwg.org/#flush-and-enqueue
@@ -87,7 +135,7 @@ impl TextDecoderStreamMethods<crate::DomTypeHolder> for TextDecoderStream {
     // https://encoding.spec.whatwg.org/#dom-textdecoderstream
     fn Constructor(
         global: &GlobalScope,
-        proto: Option<HandleObject>,
+        proto: Option<SafeHandleObject>,
         can_gc: CanGc,
         label: DOMString,
         options: &TextDecoderBinding::TextDecoderOptions,
