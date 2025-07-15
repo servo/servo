12c1ad80bdb5f370f98bc8c4e8f2ef177d4a2323
minghuaw <wuminghua7@huawei.com>
2025-07-15 17:13:37 +0800

    impl flush_and_enqueue
    Signed-off-by: minghuaw <wuminghua7@huawei.com>

--- components/script/dom/textdecoderstream.rs
+++ components/script/dom/textdecoderstream.rs
@@ -6,7 +6,7 @@ use std::cell::RefCell;
 use std::rc::Rc;
 
 use dom_struct::dom_struct;
-use encoding_rs::{Decoder, Encoding};
+use encoding_rs::{Decoder, DecoderResult, Encoding};
 use js::conversions::{FromJSValConvertible, ToJSValConvertible};
 use js::jsval::UndefinedValue;
 use js::rust::{HandleObject as SafeHandleObject, HandleValue as SafeHandleValue};
@@ -18,19 +18,70 @@ use crate::dom::bindings::codegen::Bindings::TextDecoderStreamBinding::TextDecod
 use crate::dom::bindings::codegen::UnionTypes::ArrayBufferOrArrayBufferView;
 use crate::dom::bindings::error::{Error, Fallible};
 use crate::dom::bindings::reflector::{Reflector, reflect_dom_object_with_proto};
-use crate::dom::bindings::root::{Dom, DomRoot};
+use crate::dom::bindings::root::{Dom, DomRoot, MutNullableDom};
 use crate::dom::bindings::str::DOMString;
 use crate::dom::globalscope::GlobalScope;
 use crate::dom::transformstreamdefaultcontroller::{
-    TransformerTransformAlgorithm, TransformerTransformAlgorithmType,
+    TransformerFlushAlgorithm, TransformerFlushAlgorithmType, TransformerTransformAlgorithm, TransformerTransformAlgorithmType
 };
 use crate::dom::types::{Promise, TransformStream, TransformStreamDefaultController};
 use crate::script_runtime::{CanGc, JSContext as SafeJSContext};
+
+// The common part of [`decode_and_enqueue_a_chunk`] and [`flush_and_enqueue`]
+#[allow(unsafe_code)]
+fn decode_and_enqueue(
+    cx: SafeJSContext,
+    fatal: bool,
+    decoder: &RefCell<Decoder>,
+    in_stream: &RefCell<Vec<u8>>,
+    controller: &TransformStreamDefaultController,
+    last: bool,
+    can_gc: CanGc,
+) -> Fallible<()> {
+    // Step 3. Let output be the I/O queue of scalar values « end-of-queue ».
+    let mut decoder = decoder.borrow_mut();
+    let mut in_stream = in_stream.borrow_mut();
+    
+    // Step 4.
+    let (mut remaining, output_chunk) = if fatal {
+        let mut output_chunk = String::with_capacity(
+            decoder
+                .max_utf8_buffer_length_without_replacement(in_stream.len())
+                .expect("failed to calculate MX UTF8 buffer length")
+        );
+        match decoder.decode_to_string_without_replacement(
+            &in_stream,
+            &mut output_chunk,
+            last,
+        ) {
+            (DecoderResult::InputEmpty, read) => (in_stream.split_off(read), output_chunk),
+            _ => return Err(Error::Type("Decoding failed".to_owned())),
+        }
+    } else {
+        let mut output_chunk = String::with_capacity(
+            decoder
+                .max_utf8_buffer_length(in_stream.len())
+                .expect("failed to calculate MAX UTF8 buffer length"),
+        );
+        let (_result, read, _replaced) = decoder.decode_to_string(&in_stream, &mut output_chunk, last);
+        let remaining = in_stream.split_off(read);
+        (remaining, output_chunk)
+    };
+    std::mem::swap(&mut *in_stream, &mut remaining);
+
+    rooted!(in(*cx) let mut rval = UndefinedValue());
+    unsafe { output_chunk.to_jsval(*cx, rval.handle_mut()) };
+
+    controller.Enqueue(cx, rval.handle(), can_gc)?;
+
+    Ok(())
+}
 
 // https://encoding.spec.whatwg.org/#decode-and-enqueue-a-chunk
 #[allow(unsafe_code)]
 fn decode_and_enqueue_a_chunk(
     cx: SafeJSContext,
+    fatal: bool,
     chunk: SafeHandleValue,
     decoder: &RefCell<Decoder>,
     in_stream: &RefCell<Vec<u8>>,
@@ -59,36 +110,18 @@ fn decode_and_enqueue_a_chunk(
         }
     }
 
-    // Step 3. Let output be the I/O queue of scalar values « end-of-queue ».
-    let mut decoder = decoder.borrow_mut();
-    let mut in_stream = in_stream.borrow_mut();
-    let mut output_chunk = String::with_capacity(
-        decoder
-            .max_utf8_buffer_length(in_stream.len())
-            .expect("failed to calculate MAX UTF8 buffer length"),
-    );
-
-    // Step 4.
-    let (_result, read, _replaced) = decoder.decode_to_string(&in_stream, &mut output_chunk, false);
-    let mut remaining = in_stream.split_off(read);
-    std::mem::swap(&mut *in_stream, &mut remaining);
-
-    rooted!(in(*cx) let mut rval = UndefinedValue());
-    unsafe { output_chunk.to_jsval(*cx, rval.handle_mut()) };
-
-    controller.Enqueue(cx, rval.handle(), can_gc)?;
-
-    Ok(())
+    decode_and_enqueue(cx, fatal, decoder, in_stream, controller, false, can_gc)
 }
 
 #[derive(JSTraceable, MallocSizeOf)]
 struct TextDecoderStreamTransformAlgorithm {
-    #[no_trace]
-    #[ignore_malloc_size_of = "Rc is hard"]
-    decoder: Rc<RefCell<Decoder>>,
+    stream: MutNullableDom<TextDecoderStream>,
+}
 
-    #[ignore_malloc_size_of = "Rc is hard"]
-    in_stream: Rc<RefCell<Vec<u8>>>,
+impl TextDecoderStreamTransformAlgorithm {
+    fn set_stream(&self, stream: &TextDecoderStream) {
+        self.stream.set(Some(stream));
+    }
 }
 
 impl TransformerTransformAlgorithm for TextDecoderStreamTransformAlgorithm {
@@ -100,11 +133,15 @@ impl TransformerTransformAlgorithm for TextDecoderStreamTransformAlgorithm {
         controller: &TransformStreamDefaultController,
         can_gc: CanGc,
     ) -> Fallible<std::rc::Rc<super::types::Promise>> {
+        let stream = self.stream.get()
+            .expect("stream is not set");
+
         decode_and_enqueue_a_chunk(
             cx,
+            stream.fatal,
             chunk,
-            &self.decoder,
-            &self.in_stream,
+            &stream.decoder,
+            &stream.in_stream,
             controller,
             can_gc,
         )
@@ -112,8 +149,44 @@ impl TransformerTransformAlgorithm for TextDecoderStreamTransformAlgorithm {
     }
 }
 
+// https://encoding.spec.whatwg.org/#flush-and-enqueue
+fn flush_and_enqueue(
+    cx: SafeJSContext,
+    fatal: bool,
+    decoder: &RefCell<Decoder>,
+    in_stream: &RefCell<Vec<u8>>,
+    controller: &TransformStreamDefaultController,
+    can_gc: CanGc,
+) -> Fallible<()> {
+    decode_and_enqueue(cx, fatal, decoder, in_stream, controller, true, can_gc)
+}
+
 #[derive(JSTraceable, MallocSizeOf)]
-struct TextDecoderStreamFlushAlgorithm {}
+struct TextDecoderStreamFlushAlgorithm {
+    stream: MutNullableDom<TextDecoderStream>,
+}
+
+impl TextDecoderStreamFlushAlgorithm {
+    fn set_stream(&self, stream: &TextDecoderStream) {
+        self.stream.set(Some(stream));
+    }
+}
+
+impl TransformerFlushAlgorithm for TextDecoderStreamFlushAlgorithm {
+    fn run(
+        &self,
+        cx: SafeJSContext,
+        global: &GlobalScope,
+        controller: &TransformStreamDefaultController,
+        can_gc: CanGc,
+    ) -> Fallible<Rc<Promise>> {
+        let stream = self.stream.get()
+            .expect("stream is not set");
+
+        flush_and_enqueue(cx, stream.fatal, &stream.decoder, &stream.in_stream, controller, can_gc)
+            .map(|_| Promise::new_resolved(global, cx, (), can_gc))
+    }
+}
 
 #[dom_struct]
 #[allow(non_snake_case)]
@@ -125,10 +198,8 @@ pub(crate) struct TextDecoderStream {
     ignoreBOM: bool,
     #[ignore_malloc_size_of = "defined in encoding_rs"]
     #[no_trace]
-    #[ignore_malloc_size_of = "Rc is hard"]
-    decoder: Rc<RefCell<Decoder>>,
-    #[ignore_malloc_size_of = "Rc is hard"]
-    in_stream: Rc<RefCell<Vec<u8>>>,
+    decoder: RefCell<Decoder>,
+    in_stream: RefCell<Vec<u8>>,
     transform: Dom<TransformStream>,
 }
 
@@ -138,10 +209,16 @@ impl TextDecoderStream {
         encoding: &'static Encoding,
         fatal: bool,
         ignoreBOM: bool,
-        decoder: Rc<RefCell<Decoder>>,
-        in_stream: Rc<RefCell<Vec<u8>>>,
         transform_stream: &TransformStream,
     ) -> Self {
+        let decoder = if ignoreBOM {
+            encoding.new_decoder()
+        } else {
+            encoding.new_decoder_without_bom_handling()
+        };
+        let decoder = RefCell::new(decoder);
+        let in_stream = RefCell::new(Vec::new());
+
         Self {
             reflector_: Reflector::new(),
             encoding,
@@ -162,43 +239,30 @@ impl TextDecoderStream {
         ignoreBOM: bool,
         can_gc: CanGc,
     ) -> Fallible<DomRoot<Self>> {
-        let decoder = if ignoreBOM {
-            encoding.new_decoder()
-        } else {
-            encoding.new_decoder_without_bom_handling()
-        };
-        let decoder = Rc::new(RefCell::new(decoder));
-        let in_stream = Rc::new(RefCell::new(Vec::new()));
-
-        let transform = TextDecoderStreamTransformAlgorithm {
-            decoder: decoder.clone(),
-            in_stream: in_stream.clone(),
-        };
+        let transform = Rc::new(TextDecoderStreamTransformAlgorithm {
+            stream: MutNullableDom::new(None),
+        });
+        let flush = Rc::new(TextDecoderStreamFlushAlgorithm {
+            stream: MutNullableDom::new(None),
+        });
         let cancel = None;
-        let flush = None; // TODO: impl flush and enqueue
-        let transform = TransformerTransformAlgorithmType::Native(Rc::new(transform));
+
+        let transform_wrapper = TransformerTransformAlgorithmType::Native(transform.clone());
+        let flush_wrapper = TransformerFlushAlgorithmType::Native(flush.clone());
 
         let transform_stream = TransformStream::new_with_proto(global, None, can_gc);
-        transform_stream.set_up(cx, global, cancel, flush, transform, can_gc)?;
+        transform_stream.set_up(cx, global, cancel, Some(flush_wrapper), transform_wrapper, can_gc)?;
+
+        let stream = TextDecoderStream::new_inherited(encoding, fatal, ignoreBOM, &transform_stream);
+        transform.set_stream(&stream);
+        flush.set_stream(&stream);
 
         Ok(reflect_dom_object_with_proto(
-            Box::new(Self::new_inherited(
-                encoding,
-                fatal,
-                ignoreBOM,
-                decoder,
-                in_stream,
-                &transform_stream,
-            )),
+            Box::new(stream),
             global,
             proto,
             can_gc,
         ))
-    }
-
-    // https://encoding.spec.whatwg.org/#flush-and-enqueue
-    fn flush_and_enqueue() {
-        todo!()
     }
 }
 
