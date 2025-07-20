75fedf1d5fd94459bef443d28a09cc7c36577337
Josh Matthews <josh@joshmatthews.net>
2025-07-20 00:30:55 -0400

    servoshell: Add embedder URLs to retrieve servoshell config data in servo:preferences.
    Signed-off-by: Josh Matthews <josh@joshmatthews.net>

--- ports/servoshell/desktop/protocols/servo.rs
+++ ports/servoshell/desktop/protocols/servo.rs
@@ -4,18 +4,24 @@
 
 //! Loads resources using a mapping from well-known shortcuts to resource: urls.
 //! Recognized shortcuts:
+//! - servo:default-user-agent
+//! - servo:experimental-preferences
 //! - servo:newtab
 //! - servo:preferences
 
 use std::future::Future;
 use std::pin::Pin;
 
+use headers::{ContentType, HeaderMapExt};
 use net::fetch::methods::{DoneChannel, FetchContext};
 use net::protocols::ProtocolHandler;
+use net_traits::ResourceFetchTiming;
 use net_traits::request::Request;
-use net_traits::response::Response;
+use net_traits::response::{Response, ResponseBody};
+use servo::config::prefs::UserAgentPlatform;
 
 use crate::desktop::protocols::resource::ResourceProtocolHandler;
+use crate::prefs::EXPERIMENTAL_PREFS;
 
 #[derive(Default)]
 pub struct ServoProtocolHandler {}
@@ -23,6 +29,10 @@ pub struct ServoProtocolHandler {}
 impl ProtocolHandler for ServoProtocolHandler {
     fn privileged_paths(&self) -> &'static [&'static str] {
         &["preferences"]
+    }
+
+    fn is_fetchable(&self) -> bool {
+        true
     }
 
     fn load(
@@ -48,9 +58,36 @@ impl ProtocolHandler for ServoProtocolHandler {
                 "/preferences.html",
             ),
 
+            "experimental-preferences" => {
+                let pref_list = EXPERIMENTAL_PREFS
+                    .iter()
+                    .map(|pref| format!("\"{pref}\""))
+                    .collect::<Vec<String>>()
+                    .join(",");
+                json_response(request, format!("[{pref_list}]"))
+            },
+
+            "default-user-agent" => {
+                let user_agent = UserAgentPlatform::default().to_user_agent_string();
+                json_response(request, format!("\"{user_agent}\""))
+            },
+
             _ => Box::pin(std::future::ready(Response::network_internal_error(
                 "Invalid shortcut",
             ))),
         }
     }
+}
+
+fn json_response(
+    request: &Request,
+    body: String,
+) -> Pin<Box<dyn Future<Output = Response> + Send>> {
+    let mut response = Response::new(
+        request.current_url(),
+        ResourceFetchTiming::new(request.timing_type()),
+    );
+    response.headers.typed_insert(ContentType::json());
+    *response.body.lock().unwrap() = ResponseBody::Done(body.into_bytes());
+    Box::pin(std::future::ready(response))
 }
