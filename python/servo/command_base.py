a6ac0428fbe337235501d7391a2b5830785c7bad
Jerens Lensun <jerensslensun@gmail.com>
2025-07-25 15:09:57 +0800

    undo the unexpected changes on license
    Signed-off-by: Jerens Lensun <jerensslensun@gmail.com>

--- python/servo/command_base.py
+++ python/servo/command_base.py
@@ -1,4 +1,5 @@
-# Copyright 2013 The Servo Project Developers. See the COPYRIGHT file at the top-level directory of this distribution.
+# Copyright 2013 The Servo Project Developers. See the COPYRIGHT
+# file at the top-level directory of this distribution.
 #
 # Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
 # http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
