7070a4e431ae524844442b93265acff1de7d4025
Euclid Ye <yezhizhenjiakang@gmail.com>
2025-07-31 15:14:05 +0800

    Rewording and Lint
    Signed-off-by: Euclid Ye <euclid.ye@huawei.com>

--- ports/servoshell/desktop/minibrowser.rs
+++ ports/servoshell/desktop/minibrowser.rs
@@ -409,8 +409,9 @@ impl Minibrowser {
                 let rect = Box2D::from_origin_and_size(Point2D::origin(), size);
                 if rect != webview.rect() {
                     webview.move_resize(rect);
-                    // rect right now is just viewport size, which is required by `OffscreenRenderingContext`
-                    // See https://github.com/servo/servo/issues/38369#issuecomment-3138378527
+                    // `rect` is sized to just the WebView viewport, which is required by
+                    // `OffscreenRenderingContext` See:
+                    // <https://github.com/servo/servo/issues/38369#issuecomment-3138378527>
                     webview.resize(PhysicalSize::new(size.width as u32, size.height as u32))
                 }
 
