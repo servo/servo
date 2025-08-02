ff77e5084befd564b1929949cf512bf809733803
Leo Ring <leoring03@gmail.com>
2025-08-02 20:31:21 +0100

    Implement HTMLDocument API
    Signed-off-by: Leo Ring <leoring03@gmail.com>

---
+++ components/script/dom/htmldocument.rs
@@ -0,0 +1,35 @@
+use std::ptr::NonNull;
+
+use dom_struct::dom_struct;
+use js::jsapi::JSObject;
+use script_bindings::codegen::GenericBindings::DocumentBinding::DocumentMethods;
+use script_bindings::codegen::GenericBindings::HTMLDocumentBinding::HTMLDocumentMethods;
+use script_bindings::error::Fallible;
+use script_bindings::root::DomRoot;
+use script_bindings::script_runtime::JSContext;
+use script_bindings::str::DOMString;
+
+use super::types::{Document, Location};
+
+/// <https://html.spec.whatwg.org/multipage/nav-history-apis.html#htmldocument>
+#[dom_struct]
+pub(crate) struct HTMLDocument {
+    document: Document,
+}
+
+impl HTMLDocumentMethods<crate::DomTypeHolder> for HTMLDocument {
+    /// <https://html.spec.whatwg.org/multipage/#htmldocument>
+    fn GetLocation(&self) -> Option<DomRoot<Location>> {
+        self.document.GetLocation()
+    }
+
+    /// <https://html.spec.whatwg.org/multipage/#htmldocument>
+    fn SupportedPropertyNames(&self) -> Vec<DOMString> {
+        self.document.SupportedPropertyNames()
+    }
+
+    /// <https://html.spec.whatwg.org/multipage/#htmldocument>
+    fn NamedGetter(&self, _cx: JSContext, _name: DOMString) -> Fallible<Option<NonNull<JSObject>>> {
+        Err(script_bindings::error::Error::NotFound)
+    }
+}
