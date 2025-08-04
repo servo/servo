b99d4afe6511bd5805c6000c90a9e3433201d701
minghuaw <wuminghua7@huawei.com>
2025-08-04 11:41:02 +0800

    manual type conversion fails with undefined
    Signed-off-by: minghuaw <wuminghua7@huawei.com>

--- components/script/dom/textencoderstream.rs
+++ components/script/dom/textencoderstream.rs
@@ -2,15 +2,20 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+use std::borrow::Cow;
 use std::cell::Cell;
+use std::ptr;
 use std::rc::Rc;
 
 use dom_struct::dom_struct;
-use js::conversions::ToJSValConvertible;
-use js::jsapi::JS_GetTwoByteStringCharsAndLength;
-use js::jsval::UndefinedValue;
-use js::rust::{HandleObject as SafeHandleObject, HandleValue as SafeHandleValue, ToString};
+use js::conversions::{jsstr_to_string, ConversionResult, FromJSValConvertible, ToJSValConvertible};
+use js::jsapi::{HandleValueArray, JSObject, JS_GetTwoByteStringCharsAndLength, JS_IsTypedArrayObject, JS_ValueIsUndefined};
+use js::jsval::{JSVal, UndefinedValue};
+use js::rust::wrappers::ToPrimitive;
+use js::typedarray::{TypedArray, Uint8Array};
+use js::rust::{HandleObject as SafeHandleObject, HandleValue as SafeHandleValue, ToBoolean, ToInt32, ToNumber, ToString, IntoHandle};
 
+use crate::dom::bindings::buffer_source::create_buffer_source;
 use crate::dom::bindings::codegen::Bindings::TextEncoderStreamBinding::TextEncoderStreamMethods;
 use crate::dom::bindings::error::{Fallible, Error};
 use crate::dom::bindings::reflector::{reflect_dom_object_with_proto, Reflector};
@@ -22,6 +27,97 @@ use crate::script_runtime::CanGc;
 use crate::{DomTypeHolder, DomTypes};
 use crate::script_runtime::{JSContext as SafeJSContext};
 
+enum StrOrCodeUnits<'a> {
+    Str(&'a str),
+    String(String),
+    Array(Vec<String>),
+    CodeUnits(&'a [u16]),
+}
+
+#[allow(unsafe_code)]
+fn convert_chunk_to_dom_string<'chunk>(
+    cx: SafeJSContext,
+    chunk: &'chunk SafeHandleValue,
+) -> Fallible<StrOrCodeUnits<'chunk>> {
+    log::debug!("chunk.bits: {:?}", chunk.asBits_);
+
+    if chunk.is_undefined() {
+        log::debug!("chunk is undefined");
+        return Ok(StrOrCodeUnits::Str("undefined"))
+    } 
+    
+    if chunk.is_null() {
+        log::debug!("chunk is null");
+        return Ok(StrOrCodeUnits::Str("null"))
+    } 
+    
+    if chunk.is_boolean() {
+        log::debug!("chunk is boolean");
+        return Ok(StrOrCodeUnits::String(chunk.to_boolean().to_string()))
+    } 
+    
+    if chunk.is_int32() {
+        log::debug!("chunk is i32");
+        return Ok(StrOrCodeUnits::String(chunk.to_int32().to_string()))
+    } 
+    
+    if chunk.is_double() {
+        log::debug!("chunk is number");
+        return Ok(StrOrCodeUnits::String(chunk.to_double().to_string()))
+    }
+
+    if let Ok(result) = unsafe { Vec::<JSVal>::from_jsval(*cx, *chunk, ()) } {
+        log::debug!("try converting to vec");
+        log::debug!("conversion_result: {:?}", result);
+
+        if let ConversionResult::Success(vals) = result {
+            let arr = vals.into_iter().map(|val| {
+                let jsstr = val.to_string();
+                unsafe { jsstr_to_string(*cx, jsstr) }
+            }).collect();
+            return Ok(StrOrCodeUnits::Array(arr))
+        }
+    }
+
+    if chunk.is_object() {
+        log::debug!("chunk is object");
+
+        let obj = chunk.to_object();
+        dbg!(&obj);
+
+        // // return Ok(StrOrCodeUnits::Str("[object Object]"))
+        // let obj = chunk.to_object();
+        // let handle = unsafe { SafeHandleObject::from_marked_location(&obj as *const *mut _) };
+
+        // rooted!(in(*cx) let mut value = UndefinedValue());
+        // let _result = unsafe { ToPrimitive(*cx, handle, js::jsapi::JSType::JSTYPE_STRING, value.handle_mut()) };
+        // log::debug!("ToPrimitive result: {:?}", _result);
+
+        // let jsstr = value.to_string();
+        // let s = unsafe { jsstr_to_string(*cx, jsstr) };
+        // return Ok(StrOrCodeUnits::String(s))
+    }
+    
+    // Step 1. Let input be the result of converting chunk to a DOMString. 
+    // NOTE: servo's DOMString type is a wrapper over rust's String, which must be 
+    //      valid utf-8 so we will need to inspect each u16
+    let js_str = unsafe {std::ptr::NonNull::new(ToString(*cx, *chunk))
+        .ok_or_else(|| {
+            log::error!("ToString failed");
+            Error::JSFailed // ToString may set the exception
+        })?};
+    // Step 2. Convert input to an I/O queue of code units. 
+    let mut len = 0;
+    let data = unsafe {
+        JS_GetTwoByteStringCharsAndLength(*cx, std::ptr::null(), js_str.as_ptr(), &mut len)
+    };
+    let maybe_ill_formed_code_units = unsafe { std::slice::from_raw_parts(data, len) };
+
+    log::debug!("code_units: {:?}", maybe_ill_formed_code_units);
+
+    Ok(StrOrCodeUnits::CodeUnits(maybe_ill_formed_code_units))
+}
+
 #[derive(JSTraceable, MallocSizeOf)]
 pub(crate) struct Encoder {
     leading_surrogate: Cell<Option<u16>>,
@@ -32,6 +128,48 @@ impl Encoder {
         Self {
             leading_surrogate: Cell::new(None)
         }
+    }
+
+    #[allow(unsafe_code)]
+    fn encode_code_units(&self, code_units: &[u16]) -> String {
+        let mut output = String::with_capacity(code_units.len());
+        for result in char::decode_utf16(code_units.iter().cloned()) {
+            match result {
+                Ok(c) => {
+                    if let Some(_leading_surrogate) = self.leading_surrogate.take() {
+                        output.push('\u{FFFD}');
+                    }
+
+                    output.push(c);
+                },
+                Err(error) => {
+                    // output.push('\u{FFFD}');
+                    // encoder.unpaired_surrogate.replace(Some(error.unpaired_surrogate()));
+                    let unpaired_surrogate = error.unpaired_surrogate();
+                    match code_point_type(unpaired_surrogate) {
+                        CodePointType::ScalarValue => unreachable!(),
+                        CodePointType::LeadingSurrogate => {
+                            if let Some(_leading_surrogate) = self.leading_surrogate.take() {
+                                output.push('\u{FFFD}');
+                            }
+
+                            self.leading_surrogate.replace(Some(unpaired_surrogate));
+                        },
+                        CodePointType::TrailingSurrogate => match self.leading_surrogate.take() {
+                            Some(leading_surrogate) => {
+                                let c = char::decode_utf16([leading_surrogate, unpaired_surrogate]).next()
+                                    .expect("A pair of surrogate is supplied")
+                                    .expect("Decoding a pair of surrogate cannot fail");
+                                output.push(c);
+                            },
+                            None => output.push('\u{FFFD}'),
+                        },
+                    }
+                },
+            }
+        }
+
+        output
     }
 }
 
@@ -49,6 +187,7 @@ fn code_point_type(value: u16) -> CodePointType {
     }
 }
 
+/// <https://encoding.spec.whatwg.org/#encode-and-enqueue-a-chunk>
 #[allow(unsafe_code)]
 pub(crate) fn encode_and_enqueue_a_chunk(
     cx: SafeJSContext,
@@ -58,57 +197,19 @@ pub(crate) fn encode_and_enqueue_a_chunk(
     controller: &TransformStreamDefaultController,
     can_gc: CanGc
 ) -> Fallible<()> {
-    // Step 1. Let input be the result of converting chunk to a DOMString. 
-    // NOTE: DOMString is a wrapper over rust's String, which must be valid utf-8
-    //      so we will need to inspect each u16
-    let js_str = unsafe {std::ptr::NonNull::new(ToString(*cx, chunk))
-        .ok_or_else(|| {
-            log::error!("ToString failed");
-            Error::JSFailed // ToString may set the exception
-        })?};
-    let mut len = 0;
-    let data = unsafe {
-        JS_GetTwoByteStringCharsAndLength(*cx, std::ptr::null(), js_str.as_ptr(), &mut len)
+    let input = convert_chunk_to_dom_string(cx, &chunk)?;
+
+    let output = match input {
+        StrOrCodeUnits::Str(s) => Cow::Borrowed(s),
+        StrOrCodeUnits::String(s) => Cow::Owned(s),
+        StrOrCodeUnits::Array(items) => {
+            let s: String = items.into_iter().collect();
+            Cow::Owned(s)
+        },
+        StrOrCodeUnits::CodeUnits(code_units) => {
+                        Cow::Owned(encoder.encode_code_units(code_units))
+            },
     };
-    // Step 2. Convert input to an I/O queue of code units. 
-    let maybe_ill_formed_code_units = unsafe { std::slice::from_raw_parts(data, len) };
-
-    let mut output = String::with_capacity(len);
-    for result in char::decode_utf16(maybe_ill_formed_code_units.iter().cloned()) {
-        match result {
-            Ok(c) => {
-                if let Some(_leading_surrogate) = encoder.leading_surrogate.take() {
-                    output.push('\u{FFFD}');
-                }
-
-                output.push(c);
-            },
-            Err(error) => {
-                // output.push('\u{FFFD}');
-                // encoder.unpaired_surrogate.replace(Some(error.unpaired_surrogate()));
-                let unpaired_surrogate = error.unpaired_surrogate();
-                match code_point_type(unpaired_surrogate) {
-                    CodePointType::ScalarValue => unreachable!(),
-                    CodePointType::LeadingSurrogate => {
-                        if let Some(_leading_surrogate) = encoder.leading_surrogate.take() {
-                            output.push('\u{FFFD}');
-                        }
-
-                        encoder.leading_surrogate.replace(Some(unpaired_surrogate));
-                    },
-                    CodePointType::TrailingSurrogate => match encoder.leading_surrogate.take() {
-                        Some(leading_surrogate) => {
-                            let c = char::decode_utf16([leading_surrogate, unpaired_surrogate]).next()
-                                .expect("A pair of surrogate is supplied")
-                                .expect("Decoding a pair of surrogate cannot fail");
-                            output.push(c);
-                        },
-                        None => output.push('\u{FFFD}'),
-                    },
-                }
-            },
-        }
-    }
 
     if output.is_empty() {
         return Ok(())
@@ -117,9 +218,12 @@ pub(crate) fn encode_and_enqueue_a_chunk(
     log::debug!("output: {:?}", output);
 
     let output = output.as_bytes();
-    rooted!(in(*cx) let mut chunk = UndefinedValue());
-    unsafe { output.to_jsval(*cx, chunk.handle_mut());}
-    controller.enqueue(cx, global, chunk.handle(), can_gc)
+    rooted!(in(*cx) let mut js_object = ptr::null_mut::<JSObject>());
+    let chunk: Uint8Array = create_buffer_source(cx, output, js_object.handle_mut(), can_gc)
+        .map_err(|_| Error::Type("Cannot convert byte sequence to Uint8Array".to_owned()))?;
+    rooted!(in(*cx) let mut chunk_val = UndefinedValue());
+    unsafe { chunk.to_jsval(*cx, chunk_val.handle_mut());}
+    controller.enqueue(cx, global, chunk_val.handle(), can_gc)
 }
 
 #[allow(unsafe_code)]
