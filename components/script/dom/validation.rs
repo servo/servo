e9102cef9170f55b4933360f30cd1eab4a035f6b
abdelrahman1234567 <abdelrahman.hossameldin.awadalla@huawei.com>
2025-08-07 00:13:32 +0800

    script: Implementing prevent_scroll
    This commit implements the `prevent_scroll` feature in the focus transaction system. It allows to control whether focusing an element should prevent scrolling or not.
    
    Testing: Existing WPT tests
    
    Signed-off-by: abdelrahman1234567 <abdelrahman.hossameldin.awadalla@huawei.com>

--- components/script/dom/validation.rs
+++ components/script/dom/validation.rs
@@ -1,6 +1,8 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+use script_bindings::codegen::GenericBindings::HTMLOrSVGElementBinding::FocusOptions;
+
 use crate::dom::bindings::codegen::Bindings::EventBinding::Event_Binding::EventMethods;
 use crate::dom::bindings::codegen::Bindings::HTMLElementBinding::HTMLElementMethods;
 use crate::dom::bindings::inheritance::Castable;
@@ -75,7 +77,7 @@ pub(crate) trait Validatable {
                 validation_message_for_flags(&self.validity_state(), flags)
             );
             if let Some(html_elem) = self.as_element().downcast::<HTMLElement>() {
-                html_elem.Focus(can_gc);
+                html_elem.Focus(&FocusOptions::default(), can_gc);
             }
         }
 
