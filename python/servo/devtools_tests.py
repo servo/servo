20fd24e1ba82fdd57d43308aa0738facbb09e57c
Delan Azabani <dazabani@igalia.com>
2025-08-07 18:23:58 +0800

    Handle patterns not containing `*` like unittest.main() `-k`
    Signed-off-by: Delan Azabani <dazabani@igalia.com>

--- python/servo/devtools_tests.py
+++ python/servo/devtools_tests.py
@@ -323,7 +323,14 @@ def run_tests(script_path, build_type: BuildType, test_names: list[str]):
     verbosity = 1 if logging.getLogger().level >= logging.WARN else 2
     loader = unittest.TestLoader()
     if test_names:
-        loader.testNamePatterns = test_names
+        patterns = []
+        # unittest.main() `-k` treats any `pattern` not containing `*` like `*pattern*`
+        for pattern in test_names:
+            if "*" in pattern:
+                patterns.append(pattern)
+            else:
+                patterns.append(f"*{pattern}*")
+        loader.testNamePatterns = patterns
     suite = loader.loadTestsFromTestCase(DevtoolsTests)
     print(f"Running {suite.countTestCases()} tests:")
     for test in suite:
