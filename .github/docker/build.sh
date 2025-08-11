6a4833f530b30d3ac217c4ed5d9a1d20fdec607e
Jonathan Schwender <schwenderjonathan@gmail.com>
2025-08-11 19:10:17 +0200

    wip
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>

--- .github/docker/build.sh
+++ .github/docker/build.sh
@@ -9,9 +9,14 @@ cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null || exit 1
 # --tag ghcr.io/jschwe/servo_ci_testing_dev-crown-default_features:latest \
 # .
 
+# update servo if the repo changed
+SERVO_COMMIT_HASH=$(git rev-parse main)
+
 docker build \
  --platform linux/amd64 \
  --label "org.opencontainers.image.source=https://github.com/servo/servo" \
  --label "org.opencontainers.image.description=Docker image for servo CI" \
+ --build-arg "SERVO_COMMIT_HASH=${SERVO_COMMIT_HASH}" \
  --tag ghcr.io/jschwe/servo_msrv_ci:latest \
+ --tag ghcr.io/jschwe/servo_msrv_ci:0.1 \
  .
