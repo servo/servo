638e52ea54f74900ca4bf90cf1b1706680164294
Euclid Ye <euclid.ye@huawei.com>
2025-08-13 10:48:08 +0800

    Retrieve session gracefully
    Signed-off-by: Euclid Ye <euclid.ye@huawei.com>

--- components/webdriver_server/lib.rs
+++ components/webdriver_server/lib.rs
@@ -543,6 +543,7 @@ impl Handler {
     fn session_mut(&mut self) -> WebDriverResult<&mut WebDriverSession> {
         match self.session {
             Some(ref mut x) => Ok(x),
+            // https://w3c.github.io/webdriver/#ref-for-dfn-invalid-session-id-1
             None => Err(WebDriverError::new(
                 ErrorStatus::SessionNotCreated,
                 "Session not created",
@@ -1079,7 +1080,7 @@ impl Handler {
 
     /// <https://w3c.github.io/webdriver/#get-window-handle>
     fn handle_window_handle(&self) -> WebDriverResult<WebDriverResponse> {
-        let session = self.session.as_ref().unwrap();
+        let session = self.session()?;
         // Step 1. If session's current top-level browsing context is no longer open,
         // return error with error code no such window.
         self.verify_top_level_browsing_context_is_open(session.webview_id)?;
@@ -1094,8 +1095,7 @@ impl Handler {
     /// <https://w3c.github.io/webdriver/#get-window-handles>
     fn handle_window_handles(&self) -> WebDriverResult<WebDriverResponse> {
         let handles = self
-            .session()
-            .unwrap()
+            .session()?
             .window_handles
             .values()
             .map(serde_json::to_value)
@@ -1134,19 +1134,11 @@ impl Handler {
         self.send_message_to_embedder(cmd_msg)?;
 
         wait_for_ipc_response(receiver)?;
-        self.session_mut()
-            .unwrap()
-            .window_handles
-            .remove(&webview_id);
+        self.session_mut()?.window_handles.remove(&webview_id);
 
         // Step 4. If there are no more open top-level browsing contexts, try to close the session.
-        let window_handles: Vec<String> = self
-            .session()
-            .unwrap()
-            .window_handles
-            .values()
-            .cloned()
-            .collect();
+        let window_handles: Vec<String> =
+            self.session()?.window_handles.values().cloned().collect();
 
         if window_handles.is_empty() {
             self.session = None;
@@ -1165,7 +1157,7 @@ impl Handler {
     ) -> WebDriverResult<WebDriverResponse> {
         let (sender, receiver) = ipc::channel().unwrap();
 
-        let session = self.session().unwrap();
+        let session = self.session()?;
 
         // Step 2. If session's current top-level browsing context is no longer open,
         // return error with error code no such window.
@@ -1180,9 +1172,9 @@ impl Handler {
         // This MUST be done without invoking the focusing steps.
         self.send_message_to_embedder(cmd_msg)?;
 
-        let mut handle = self.session.as_ref().unwrap().id.to_string();
+        let mut handle = self.session()?.id.to_string();
         if let Ok(new_webview_id) = receiver.recv() {
-            let session = self.session_mut().unwrap();
+            let session = self.session_mut()?;
             let new_handle = Uuid::new_v4().to_string();
             handle = new_handle.clone();
             session.window_handles.insert(new_webview_id, new_handle);
@@ -1267,7 +1259,7 @@ impl Handler {
         &mut self,
         parameters: &SwitchToWindowParameters,
     ) -> WebDriverResult<WebDriverResponse> {
-        let session = self.session_mut().unwrap();
+        let session = self.session_mut()?;
         if session.id.to_string() == parameters.handle {
             // There's only one main window, so there's nothing to do here.
             Ok(WebDriverResponse::Void)
@@ -1860,10 +1852,7 @@ impl Handler {
     }
 
     fn handle_get_timeouts(&mut self) -> WebDriverResult<WebDriverResponse> {
-        let session = self
-            .session
-            .as_ref()
-            .ok_or(WebDriverError::new(ErrorStatus::SessionNotCreated, ""))?;
+        let session = self.session()?;
 
         let timeouts = TimeoutsResponse {
             script: session.timeouts.script,
