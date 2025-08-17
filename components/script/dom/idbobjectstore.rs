6c909ae99f4c4a52dca90bfeaf283cbf1ca2d1a6
Ashwin Naren <arihant2math@gmail.com>
2025-08-17 00:44:52 -0700

    enable access to transaction from idbobjectstore
    Signed-off-by: Ashwin Naren <arihant2math@gmail.com>

--- components/script/dom/idbobjectstore.rs
+++ components/script/dom/idbobjectstore.rs
@@ -449,9 +449,9 @@ impl IDBObjectStoreMethods<crate::DomTypeHolder> for IDBObjectStore {
     // }
 
     // https://www.w3.org/TR/IndexedDB-2/#dom-idbobjectstore-transaction
-    // fn Transaction(&self) -> DomRoot<IDBTransaction> {
-    //     unimplemented!();
-    // }
+    fn GetTransaction(&self) -> Option<DomRoot<IDBTransaction>> {
+        self.transaction()
+    }
 
     // https://www.w3.org/TR/IndexedDB-2/#dom-idbobjectstore-autoincrement
     fn AutoIncrement(&self) -> bool {
