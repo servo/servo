328dc733978b809b7c32710d4346f15d5369bee0
Jonathan Schwender <schwenderjonathan@gmail.com>
2025-08-30 11:07:46 +0200

    tracing: Add abstraction for tracing macros
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>

--- components/shared/profile/lib.rs
+++ components/shared/profile/lib.rs
@@ -21,7 +21,7 @@ macro_rules! time_profile {
     ($category:expr, $meta:expr, $profiler_chan:expr, $($callback:tt)+) => {{
         let meta: Option<$crate::time::TimerMetadata> = $meta;
         #[cfg(feature = "tracing")]
-        let span = tracing::info_span!(
+        let span = $crate::servo_tracing::info_span!(
             $category.variant_name(),
             servo_profiling = true,
             url = meta.as_ref().map(|m| m.url.clone()),
@@ -31,3 +31,158 @@ macro_rules! time_profile {
         $crate::time::profile($category, meta, $profiler_chan, span, $($callback)+)
     }};
 }
+
+/// Provides API compatible dummies for the tracing-rs APIs we use
+/// if tracing is disabled. Hence, nothing will be traced
+pub mod dummy_tracing {
+    use std::fmt::Display;
+    use std::marker::PhantomData;
+
+    pub struct Span();
+
+    pub struct EnteredSpan(Span);
+    pub struct Entered<'a>(PhantomData<&'a Span>);
+
+    impl Span {
+        pub fn enter(&self) -> Entered<'_> {
+            Entered(PhantomData)
+        }
+
+        pub fn entered(self) -> EnteredSpan {
+            EnteredSpan(self)
+        }
+
+        pub fn in_scope<F: FnOnce() -> T, T>(&self, f: F) -> T {
+            // We still need to execute the function, even if tracing is disabled.
+            f()
+        }
+    }
+
+    impl EnteredSpan {}
+
+    impl Entered<'_> {}
+
+    #[derive(Debug)]
+    #[allow(dead_code)]
+    struct Level();
+
+    #[allow(dead_code)]
+    impl Level {
+        /// The "error" level.
+        ///
+        /// Designates very serious errors.
+        pub const ERROR: Level = Level();
+        /// The "warn" level.
+        ///
+        /// Designates hazardous situations.
+        pub const WARN: Level = Level();
+        /// The "info" level.
+        ///
+        /// Designates useful information.
+        pub const INFO: Level = Level();
+        /// The "debug" level.
+        ///
+        /// Designates lower priority information.
+        pub const DEBUG: Level = Level();
+        /// The "trace" level.
+        ///
+        /// Designates very low priority, often extremely verbose, information.
+        pub const TRACE: Level = Level();
+        /// Returns the string representation of the `Level`.
+        ///
+        /// This returns the same string as the `fmt::Display` implementation.
+        pub fn as_str(&self) -> &'static str {
+            "disabled"
+        }
+    }
+    impl Display for Level {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "{}", self.as_str())
+        }
+    }
+}
+
+/// Constructs a span at the trace level
+///
+/// This macro creates a Span for the purpose of instrumenting code to measure
+/// the execution time of the span.
+/// If the `tracing` feature (of the crate using this macro) is disabled, then
+/// the Span implementation will be replaced with a dummy, that does not record
+/// anything.
+///
+/// Attention: This macro requires the user crate to have a `tracing` feature,
+/// which can be used to disable the effects of this macro.
+#[macro_export]
+macro_rules! trace_span {
+    ($span_name:literal, $($field:tt)*) => {
+        {
+        #[cfg(feature = "tracing")]
+        {
+            $crate::servo_tracing::trace_span!(
+                $span_name,
+                servo_profiling = true,
+                $($field)*
+            )
+        }
+        #[cfg(not(feature = "tracing"))]
+        { $crate::dummy_tracing::Span() }
+        }
+    };
+    ($span_name:literal) => {
+        {
+         #[cfg(feature = "tracing")]
+         {
+             $crate::servo_tracing::trace_span!(
+                $span_name,
+                servo_profiling = true,
+             )
+         }
+        #[cfg(not(feature = "tracing"))]
+        { $crate::dummy_tracing::Span() }
+        }
+    };
+}
+
+/// Constructs a span at the info level
+///
+/// This macro creates a Span for the purpose of instrumenting code to measure
+/// the execution time of the span.
+/// If the `tracing` feature (of the crate using this macro) is disabled, then
+/// the Span implementation will be replaced with a dummy, that does not record
+/// anything.
+///
+/// Attention: This macro requires the user crate to have a `tracing` feature,
+/// which can be used to disable the effects of this macro.
+#[macro_export]
+macro_rules! info_span {
+    ($span_name:literal, $($field:tt)*) => {
+        {
+        #[cfg(feature = "tracing")]
+        {
+            $crate::servo_tracing::info_span!(
+                $span_name,
+                servo_profiling = true,
+                $($field)*
+            )
+        }
+        #[cfg(not(feature = "tracing"))]
+        { $crate::dummy_tracing::Span() }
+        }
+    };
+    ($span_name:literal) => {
+        {
+         #[cfg(feature = "tracing")]
+         {
+             $crate::servo_tracing::info_span!(
+                $span_name,
+                servo_profiling = true,
+             )
+         }
+        #[cfg(not(feature = "tracing"))]
+        { $crate::dummy_tracing::Span() }
+        }
+    };
+}
+
+#[cfg(feature = "tracing")]
+pub use tracing as servo_tracing;
