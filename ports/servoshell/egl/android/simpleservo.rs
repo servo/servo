69eb133bdd3de6ec968746b626242f414d724250
sharpshooter_pt <ibluegalaxy_taoj@163.com>
2025-09-01 21:38:57 +0800

    compositor: allow OHOS to integrate VSYNC into RefreshDriver
    Signed-off-by: sharpshooter_pt <ibluegalaxy_taoj@163.com>

--- ports/servoshell/egl/android/simpleservo.rs
+++ ports/servoshell/egl/android/simpleservo.rs
@@ -122,6 +122,7 @@ pub fn init(
             rendering_context,
             servo,
             window_callbacks,
+            None,
             servoshell_preferences,
             webdriver_receiver,
         );
