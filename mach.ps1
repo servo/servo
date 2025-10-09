9fbc06763974d5608dd777b13b214c74a02ba502
Euclid Ye <yezhizhenjiakang@gmail.com>
2025-10-09 15:07:18 +0800

    Powershell script
    Signed-off-by: Euclid Ye <yezhizhenjiakang@gmail.com>

---
+++ mach.ps1
@@ -0,0 +1,26 @@
+$workdir = $PSScriptRoot
+$arguments = $args
+$expanded = $false
+if ($arguments.Count -gt 0) {
+    $expandedArgs = @()
+    foreach ($arg in $arguments) {
+        if ($arg -match '[\*\?\[\]]') {
+            $expandedItems = Get-Item -Path $arg -ErrorAction SilentlyContinue
+            if ($expandedItems) {
+                $expandedArgs += $expandedItems | Select-Object -ExpandProperty FullName
+                $expanded = $true
+            } else {
+                # Expansion fails. Use original arg.
+                $expandedArgs += $arg
+            }
+        } else {
+            $expandedArgs += $arg
+        }
+    }
+    $arguments = $expandedArgs
+    if ($expanded){
+        Write-Host "Expanded arguments: $arguments"
+    }
+}
+
+uv run --no-project python (Join-Path $workdir "mach") @arguments
