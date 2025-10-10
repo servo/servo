servo/servo

commit fbbdce2e59058433dec203e8d0ff57bab123bb82

libservo: Remove `MouseButtonAction::Click` from the API (#39705)
The embedder should never be responsible for triggering click events, so
this change removes that possibility from the API. In addition,
triggering of click events is simplified by moving the logic to the
`DocumentEventHandler`. This has the benefit of making behavior
consistent between in-process and out-of-process `<iframe>`s. Now click
events are never triggered when the button up and down cross frame
boundaries.

Testing: This should be covered by existing tests.

Signed-off-by: Martin Robinson <mrobinson@igalia.com>

ports/servoshell/egl/android.rs
@@ -342,17 +342,6 @@ pub extern "C" fn Java_org_servo_servoview_JNIServo_pinchZoomEnd<'local>(
 }
 
 #[unsafe(no_mangle)]
-pub extern "C" fn Java_org_servo_servoview_JNIServo_click(
-    mut env: JNIEnv,
-    _: JClass,
-    x: jfloat,
-    y: jfloat,
-) {
-    debug!("click");
-    call(&mut env, |s| s.click(x, y));
-}
-
-#[unsafe(no_mangle)]
 pub extern "C" fn Java_org_servo_servoview_JNIServo_pauseCompositor(
     mut env: JNIEnv,
     _: JClass<'_>,

components/constellation/constellation.rs
@@ -2962,7 +2962,7 @@ where
         };
 
         match event.action {
-            MouseButtonAction::Click | MouseButtonAction::Down => {
+            MouseButtonAction::Down => {
                 self.pressed_mouse_buttons |= button_as_bitmask;
             },
             MouseButtonAction::Up => {
@@ -3026,13 +3026,6 @@ where
         // modifiers and updates the event here to reflect the current state.
         let pressed_mouse_buttons = self.pressed_mouse_buttons;
         let active_keyboard_modifiers = self.active_keyboard_modifiers;
-
-        // TODO: Click should be handled internally in the `Document`.
-        if let InputEvent::MouseButton(event) = &event {
-            if event.action == MouseButtonAction::Click {
-                self.pressed_mouse_buttons = 0;
-            }
-        }
 
         let Some(webview) = self.webviews.get_mut(webview_id) else {
             warn!("Got input event for unknown WebViewId: {webview_id:?}");

components/script/dom/document_event_handler.rs
@@ -79,6 +79,8 @@ pub(crate) struct DocumentEventHandler {
     #[ignore_malloc_size_of = "Defined in std"]
     #[no_trace]
     last_click_info: DomRefCell<Option<(Instant, Point2D<f32, CSSPixel>)>>,
+    #[no_trace]
+    last_mouse_button_down_point: Cell<Option<Point2D<f32, CSSPixel>>>,
     /// The element that is currently hovered by the cursor.
     current_hover_target: MutNullableDom<Element>,
     /// The element that was most recently clicked.
@@ -104,6 +106,7 @@ impl DocumentEventHandler {
             pending_input_events: Default::default(),
             mouse_move_event_index: Default::default(),
             last_click_info: Default::default(),
+            last_mouse_button_down_point: Default::default(),
             current_hover_target: Default::default(),
             most_recently_clicked_element: Default::default(),
             most_recent_mousemove_point: Default::default(),
@@ -555,7 +558,7 @@ impl DocumentEventHandler {
             event.action, hit_test_result.point_in_frame
         );
 
-        let Some(el) = hit_test_result
+        let Some(element) = hit_test_result
             .node
             .inclusive_ancestors(ShadowIncluding::Yes)
             .filter_map(DomRoot::downcast::<Element>)
@@ -564,17 +567,22 @@ impl DocumentEventHandler {
             return;
         };
 
-        let node = el.upcast::<Node>();
+        let node = element.upcast::<Node>();
         debug!("{:?} on {:?}", event.action, node.debug_str());
 
         // https://w3c.github.io/uievents/#hit-test
         // Prevent mouse event if element is disabled.
         // TODO: also inert.
-        if el.is_actually_disabled() {
+        if element.is_actually_disabled() {
             return;
         }
 
+        let mouse_event_type_string = match event.action {
+            embedder_traits::MouseButtonAction::Up => "mouseup",
+            embedder_traits::MouseButtonAction::Down => "mousedown",
+        };
         let dom_event = DomRoot::upcast::<Event>(MouseEvent::for_platform_mouse_event(
+            mouse_event_type_string,
             event,
             input_event.pressed_mouse_buttons,
             &self.window,
@@ -583,20 +591,12 @@ impl DocumentEventHandler {
             can_gc,
         ));
 
-        let activatable = el.as_maybe_activatable();
+        let activatable = element.as_maybe_activatable();
         match event.action {
-            // https://w3c.github.io/uievents/#handle-native-mouse-click
-            MouseButtonAction::Click => {
-                self.most_recently_clicked_element.set(Some(&el));
-
-                el.set_click_in_progress(true);
-                dom_event.dispatch(node.upcast(), false, can_gc);
-                el.set_click_in_progress(false);
-
-                self.maybe_fire_dblclick(node, &hit_test_result, input_event, can_gc);
-            },
-            // https://w3c.github.io/uievents/#handle-native-mouse-down
             MouseButtonAction::Down => {
+                self.last_mouse_button_down_point
+                    .set(Some(hit_test_result.point_in_frame));
+
                 if let Some(a) = activatable {
                     a.enter_formal_activation_state();
                 }
@@ -607,7 +607,7 @@ impl DocumentEventHandler {
                 // delegate the focus target into its shadow host.
                 // TODO: This focus delegation should be done
                 // with shadow DOM delegateFocus attribute.
-                let target_el = el.find_focusable_shadow_host_if_necessary();
+                let target_el = element.find_focusable_shadow_host_if_necessary();
 
                 let document = self.window.Document();
                 document.begin_focus_transaction();
@@ -646,8 +646,61 @@ impl DocumentEventHandler {
 
                 // Step 7. dispatch event at target.
                 dom_event.dispatch(node.upcast(), false, can_gc);
+
+                self.maybe_trigger_click_for_mouse_button_down_event(
+                    event,
+                    input_event,
+                    &hit_test_result,
+                    &element,
+                    can_gc,
+                );
             },
         }
+    }
+
+    /// <https://w3c.github.io/uievents/#handle-native-mouse-click>
+    fn maybe_trigger_click_for_mouse_button_down_event(
+        &self,
+        event: MouseButtonEvent,
+        input_event: &ConstellationInputEvent,
+        hit_test_result: &HitTestResult,
+        element: &Element,
+        can_gc: CanGc,
+    ) {
+        if event.button != MouseButton::Left {
+            return;
+        }
+        let Some(last_mouse_button_down_point) = self.last_mouse_button_down_point.take() else {
+            return;
+        };
+
+        let distance = last_mouse_button_down_point.distance_to(hit_test_result.point_in_frame);
+        let maximum_click_distance = 10.0 * self.window.device_pixel_ratio().get();
+        if distance > maximum_click_distance {
+            return;
+        }
+
+        // From <https://w3c.github.io/uievents/#event-type-click>
+        // > The click event type MUST be dispatched on the topmost event target indicated by the
+        // > pointer, when the user presses down and releases the primary pointer button.
+
+        self.most_recently_clicked_element.set(Some(element));
+
+        element.set_click_in_progress(true);
+        let dom_event = DomRoot::upcast::<Event>(MouseEvent::for_platform_mouse_event(
+            "click",
+            event,
+            input_event.pressed_mouse_buttons,
+            &self.window,
+            hit_test_result,
+            input_event.active_keyboard_modifiers,
+            can_gc,
+        ));
+        let node = element.upcast::<Node>();
+        dom_event.dispatch(node.upcast(), false, can_gc);
+        element.set_click_in_progress(false);
+
+        self.maybe_fire_dblclick(node, hit_test_result, input_event, can_gc);
     }
 
     /// <https://www.w3.org/TR/uievents/#maybe-show-context-menu>

components/script/dom/mouseevent.rs
@@ -256,6 +256,7 @@ impl MouseEvent {
     /// Create a [MouseEvent] triggered by the embedder
     /// <https://w3c.github.io/uievents/#create-a-cancelable-mouseevent-id>
     pub(crate) fn for_platform_mouse_event(
+        event_type_string: &'static str,
         event: embedder_traits::MouseButtonEvent,
         pressed_mouse_buttons: u16,
         window: &Window,
@@ -263,12 +264,6 @@ impl MouseEvent {
         modifiers: Modifiers,
         can_gc: CanGc,
     ) -> DomRoot<Self> {
-        let mouse_event_type_string = match event.action {
-            embedder_traits::MouseButtonAction::Click => "click",
-            embedder_traits::MouseButtonAction::Up => "mouseup",
-            embedder_traits::MouseButtonAction::Down => "mousedown",
-        };
-
         let client_point = hit_test_result.point_in_frame.to_i32();
         let page_point = hit_test_result
             .point_relative_to_initial_containing_block
@@ -277,7 +272,7 @@ impl MouseEvent {
         let click_count = 1;
         let mouse_event = MouseEvent::new(
             window,
-            mouse_event_type_string.into(),
+            event_type_string.into(),
             EventBubbles::Bubbles,
             EventCancelable::Cancelable,
             Some(window),

components/script/script_thread.rs
@@ -50,11 +50,9 @@ use devtools_traits::{
 };
 use embedder_traits::user_content_manager::UserContentManager;
 use embedder_traits::{
-    FocusSequenceNumber, InputEvent, JavaScriptEvaluationError, JavaScriptEvaluationId,
-    MediaSessionActionType, MouseButton, MouseButtonAction, MouseButtonEvent, Theme,
-    ViewportDetails, WebDriverScriptCommand,
+    FocusSequenceNumber, JavaScriptEvaluationError, JavaScriptEvaluationId, MediaSessionActionType,
+    Theme, ViewportDetails, WebDriverScriptCommand,
 };
-use euclid::Point2D;
 use euclid::default::Rect;
 use fonts::{FontContext, SystemFontServiceProxy};
 use headers::{HeaderMapExt, LastModified, ReferrerPolicy as ReferrerPolicyHeader};
@@ -98,7 +96,7 @@ use url::Position;
 #[cfg(feature = "webgpu")]
 use webgpu_traits::{WebGPUDevice, WebGPUMsg};
 use webrender_api::ExternalScrollId;
-use webrender_api::units::{DevicePixel, LayoutVector2D};
+use webrender_api::units::LayoutVector2D;
 
 use crate::document_collection::DocumentCollection;
 use crate::document_loader::DocumentLoader;
@@ -348,10 +346,6 @@ pub struct ScriptThread {
     /// A factory for making new layouts. This allows layout to depend on script.
     #[no_trace]
     layout_factory: Arc<dyn LayoutFactory>,
-
-    /// The screen coordinates where the primary mouse button was pressed.
-    #[no_trace]
-    relative_mouse_down_point: Cell<Point2D<f32, DevicePixel>>,
 
     /// The [`TimerId`] of a ScriptThread-scheduled "update the rendering" call, if any.
     /// The ScriptThread schedules calls to "update the rendering," but the renderer can
@@ -991,7 +985,6 @@ impl ScriptThread {
             gpu_id_hub,
             inherited_secure_context: state.inherited_secure_context,
             layout_factory,
-            relative_mouse_down_point: Cell::new(Point2D::zero()),
             scheduled_update_the_rendering: Default::default(),
             needs_rendering_update: Arc::new(AtomicBool::new(false)),
             debugger_global: debugger_global.as_traced(),
@@ -3423,61 +3416,6 @@ impl ScriptThread {
             warn!("Compositor event sent to closed pipeline {pipeline_id}.");
             return;
         };
-
-        // Also send a 'click' event with same hit-test result if this is release
-
-        // MAYBE? TODO: https://developer.mozilla.org/en-US/docs/Web/API/Element/click_event
-        // If the button is pressed on one element and the pointer is moved outside the element
-        // before the button is released, the event is fired on the most specific ancestor element
-        // that contained both elements.
-
-        // But spec doesn't specify this https://w3c.github.io/uievents/#event-type-click
-        // "The click event type MUST be dispatched on the topmost event target indicated by
-        // the pointer, when the user presses down and releases the primary pointer button"
-
-        // Servo-specific: Trigger if within 10px of the down point
-        if let InputEvent::MouseButton(mouse_button_event) = &event.event {
-            if let MouseButton::Left = mouse_button_event.button {
-                match mouse_button_event.action {
-                    MouseButtonAction::Up => {
-                        let pixel_dist =
-                            self.relative_mouse_down_point.get() - mouse_button_event.point;
-                        let pixel_dist =
-                            (pixel_dist.x * pixel_dist.x + pixel_dist.y * pixel_dist.y).sqrt();
-                        if pixel_dist < 10.0 * document.window().device_pixel_ratio().get() {
-                            // Pass webdriver_id to the newly generated click event
-                            document.event_handler().note_pending_input_event(
-                                ConstellationInputEvent {
-                                    hit_test_result: event.hit_test_result.clone(),
-                                    pressed_mouse_buttons: event.pressed_mouse_buttons,
-                                    active_keyboard_modifiers: event.active_keyboard_modifiers,
-                                    event: event.event.clone().with_webdriver_message_id(None),
-                                },
-                            );
-                            document.event_handler().note_pending_input_event(
-                                ConstellationInputEvent {
-                                    hit_test_result: event.hit_test_result,
-                                    pressed_mouse_buttons: event.pressed_mouse_buttons,
-                                    active_keyboard_modifiers: event.active_keyboard_modifiers,
-                                    event: InputEvent::MouseButton(MouseButtonEvent::new(
-                                        MouseButtonAction::Click,
-                                        mouse_button_event.button,
-                                        mouse_button_event.point,
-                                    ))
-                                    .with_webdriver_message_id(event.event.webdriver_message_id()),
-                                },
-                            );
-                            return;
-                        }
-                    },
-                    MouseButtonAction::Down => {
-                        self.relative_mouse_down_point.set(mouse_button_event.point)
-                    },
-                    MouseButtonAction::Click => {},
-                }
-            }
-        }
-
         document.event_handler().note_pending_input_event(event);
     }
 

components/shared/embedder/input_events.rs
@@ -155,7 +155,7 @@ impl MouseButtonEvent {
     }
 }
 
-#[derive(Clone, Copy, Debug, Deserialize, Serialize)]
+#[derive(Clone, Copy, Debug, Deserialize, PartialEq, Serialize)]
 pub enum MouseButton {
     Left,
     Middle,
@@ -195,8 +195,6 @@ impl From<MouseButton> for i16 {
 /// The types of mouse events
 #[derive(Clone, Copy, Debug, Deserialize, MallocSizeOf, PartialEq, Serialize)]
 pub enum MouseButtonAction {
-    /// Mouse button clicked
-    Click,
     /// Mouse button down
     Down,
     /// Mouse button up

ports/servoshell/egl/app_state.rs
@@ -844,17 +844,6 @@ impl RunningAppState {
         self.perform_updates();
     }
 
-    /// Perform a click.
-    pub fn click(&self, x: f32, y: f32) {
-        self.active_webview()
-            .notify_input_event(InputEvent::MouseButton(MouseButtonEvent::new(
-                MouseButtonAction::Click,
-                MouseButton::Left,
-                Point2D::new(x, y),
-            )));
-        self.perform_updates();
-    }
-
     pub fn key_down(&self, key: Key) {
         let key_event = KeyboardEvent::from_state_and_key(KeyState::Down, key);
         self.active_webview()

commit bbdc149b593179441bef5d8886259eef1ff6544f

servoshell: Reimplement "onClick" action on Android
The Android interface needs some way to communciate click events to
Servo, so the JNI layer needs to expose a click event. Even though there
is no longer a "click" action in the API, we can implement this behavior
via a mouse button down and then mouse button up event, which should be
a more complete implementation in the DOM.

This fixes a regression from #39705.

Fixes #39742.

Signed-off-by: Martin Robinson <mrobinson@igalia.com>

ports/servoshell/egl/android.rs
@@ -20,8 +20,8 @@ use raw_window_handle::{
     AndroidDisplayHandle, AndroidNdkWindowHandle, RawDisplayHandle, RawWindowHandle,
 };
 use servo::{
-    AlertResponse, EventLoopWaker, LoadStatus, MediaSessionActionType, PermissionRequest,
-    SimpleDialog, WebView,
+    AlertResponse, EventLoopWaker, LoadStatus, MediaSessionActionType, MouseButton,
+    PermissionRequest, SimpleDialog, WebView,
 };
 use simpleservo::{APP, DeviceIntRect, InitOptions, InputMethodType, MediaSessionPlaybackState};
 
@@ -339,6 +339,20 @@ pub extern "C" fn Java_org_servo_servoview_JNIServo_pinchZoomEnd<'local>(
 ) {
     debug!("pinchZoomEnd");
     call(&mut env, |s| s.pinchzoom_end(factor, x as u32, y as u32));
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn Java_org_servo_servoview_JNIServo_click(
+    mut env: JNIEnv,
+    _: JClass,
+    x: jfloat,
+    y: jfloat,
+) {
+    debug!("click");
+    call(&mut env, |s| {
+        s.mouse_down(x, y, MouseButton::Left);
+        s.mouse_up(x, y, MouseButton::Left);
+    });
 }
 
 #[unsafe(no_mangle)]