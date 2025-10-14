c1a4328eba86d0c3f64ebd922627a026fcf0aac1
Shubham Gupta <shubham.gupta@chromium.org>
2025-10-14 16:05:35 +0800

    Disable LCP feature on user interaction
    Signed-off-by: Shubham Gupta <shubham.gupta@chromium.org>

--- components/compositing/compositor.rs
+++ components/compositing/compositor.rs
@@ -1588,6 +1588,7 @@ impl IOCompositor {
         if let Some(webview_renderer) = self.webview_renderers.get_mut(webview_id) {
             webview_renderer.notify_input_event(event);
         }
+        self.disable_lcp_calculation();
     }
 
     pub fn notify_scroll_event(
@@ -1599,6 +1600,7 @@ impl IOCompositor {
         if let Some(webview_renderer) = self.webview_renderers.get_mut(webview_id) {
             webview_renderer.notify_scroll_event(scroll_location, cursor);
         }
+        self.disable_lcp_calculation();
     }
 
     pub fn pinch_zoom(&mut self, webview_id: WebViewId, pinch_zoom_delta: f32) {
@@ -1698,6 +1700,16 @@ impl IOCompositor {
             return;
         };
         webview_renderer.notify_input_event_handled(input_event_id, result);
+    }
+
+    /// Disable LCP feature when the user interacts with the page.
+    fn disable_lcp_calculation(&mut self) {
+        let mut current_preferences = servo_config::prefs::get().clone();
+        if current_preferences.largest_contentful_paint_enabled {
+            current_preferences.largest_contentful_paint_enabled = false;
+            servo_config::prefs::set(current_preferences);
+        }
+        self.lcp_calculator.clear();
     }
 }
 
