70389a1c0cc6c94b0105dba248a57f84b084a76b
Martin Robinson <mrobinson@igalia.com>
2025-10-25 10:07:46 +0200

    pixels: Store decoded images as RGBA premultiplied data
    Canvas expects input data to be in RGBA premultiplied format and
    WebRender already supports RGBA and BGRA data as long as they are
    premultiplied. Pre-multiplying up front allows:
    
    - Avoiding many conversion when painting images to canvas.
    - Passing the `RasterImage` IpcSharedMemory of the image instead of creating
      a new one with the premultiplied data every time we upload to
      WebRender. This is a big deal for animated gifs, because before every
      frame was creating a new shared memory segment.
    
    It seems that for rasterized SVGs were were already putting
    premultplied data into the `RasterImage` so it's quite likely SVGs were
    being composited incorrectly.
    
    Signed-off-by: Martin Robinson <mrobinson@igalia.com>

--- components/script/dom/canvas/imagebitmap.rs
+++ components/script/dom/canvas/imagebitmap.rs
@@ -9,7 +9,7 @@ use base::id::{ImageBitmapId, ImageBitmapIndex};
 use constellation_traits::SerializableImageBitmap;
 use dom_struct::dom_struct;
 use euclid::default::{Point2D, Rect, Size2D};
-use pixels::{CorsStatus, PixelFormat, Snapshot, SnapshotAlphaMode, SnapshotPixelFormat};
+use pixels::{CorsStatus, Snapshot, SnapshotAlphaMode, SnapshotPixelFormat};
 use rustc_hash::FxHashMap;
 use script_bindings::error::{Error, Fallible};
 use script_bindings::realms::{AlreadyInRealm, InRealm};
@@ -538,32 +538,14 @@ impl ImageBitmap {
                 // (e.g., a vector graphic with no natural size), then queue
                 // a global task, using the bitmap task source, to reject promise
                 // with an "InvalidStateError" DOMException and abort these steps.
-                let Some(img) = pixels::load_from_memory(&bytes, CorsStatus::Safe) else {
+                let Some(raster_image) = pixels::load_from_memory(&bytes, CorsStatus::Safe) else {
                     reject_promise_on_bitmap_task_source(&p);
                     return p;
                 };
 
-                let size = Size2D::new(img.metadata.width, img.metadata.height);
-                let format = match img.format {
-                    PixelFormat::BGRA8 => SnapshotPixelFormat::BGRA,
-                    PixelFormat::RGBA8 => SnapshotPixelFormat::RGBA,
-                    pixel_format => {
-                        unimplemented!("unsupported pixel format ({:?})", pixel_format)
-                    },
-                };
-                let alpha_mode = SnapshotAlphaMode::Transparent {
-                    premultiplied: false,
-                };
-
-                let snapshot = Snapshot::from_vec(
-                    size.cast(),
-                    format,
-                    alpha_mode,
-                    img.first_frame().bytes.to_vec(),
-                );
-
                 // Step 6.4. Set imageBitmap's bitmap data to imageData, cropped
                 // to the source rectangle with formatting.
+                let snapshot = raster_image.as_snapshot();
                 let Some(bitmap_data) =
                     ImageBitmap::crop_and_transform_bitmap_data(snapshot, sx, sy, sw, sh, options)
                 else {
