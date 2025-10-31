91613861129dfa24b478a3a4d771a833605d6dac
lumiscosity <averyrudelphe@gmail.com>
2025-10-31 19:57:28 +0100

    fix lint
    Signed-off-by: lumiscosity <averyrudelphe@gmail.com>

--- components/config/prefs.rs
+++ components/config/prefs.rs
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-use std::sync::{RwLock, RwLockReadGuard};
 use std::env::consts::ARCH;
+use std::sync::{RwLock, RwLockReadGuard};
 
 use serde::{Deserialize, Serialize};
 use servo_config_macro::ServoPreferences;
