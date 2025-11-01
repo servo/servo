1436c96670bc13546fa0538a3b6093dbb13bbfb7
Yerkebulan Tulibergenov <yerkebulan@gmail.com>
2025-10-31 23:26:26 -0700

    script: move media interfaces to script/dom/media module
    Signed-off-by: Yerkebulan Tulibergenov <yerkebulan@gmail.com>

---
+++ components/script/dom/media/mod.rs
@@ -0,0 +1,15 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+pub(crate) mod mediadeviceinfo;
+pub(crate) mod mediadevices;
+pub(crate) mod mediaerror;
+pub(crate) mod mediafragmentparser;
+pub(crate) mod medialist;
+pub(crate) mod mediametadata;
+pub(crate) mod mediaquerylist;
+pub(crate) mod mediaquerylistevent;
+pub(crate) mod mediasession;
+pub(crate) mod mediastream;
+pub(crate) mod mediastreamtrack;
