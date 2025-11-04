9081cc31dd79203f79bf56401cc900e0efb8c3a2
Luke Warlow <lwarlow@igalia.com>
2025-11-04 21:21:45 +0000

    Address comments
    Signed-off-by: Luke Warlow <lwarlow@igalia.com>

--- components/script/dom/html/htmldialogelement.rs
+++ components/script/dom/html/htmldialogelement.rs
@@ -63,29 +63,29 @@ impl HTMLDialogElement {
     /// <https://html.spec.whatwg.org/multipage/#show-a-modal-dialog>
     pub fn show_a_modal(&self, source: Option<DomRoot<Element>>, can_gc: CanGc) -> ErrorResult {
         let subject = self.upcast::<Element>();
-        // 1. If subject has an open attribute and is modal of subject is true, then return.
+        // Step 1. If subject has an open attribute and is modal of subject is true, then return.
         if subject.has_attribute(&local_name!("open")) && self.is_modal.get() {
             return Ok(());
         }
 
-        // 2. If subject has an open attribute, then throw an "InvalidStateError" DOMException.
+        // Step 2. If subject has an open attribute, then throw an "InvalidStateError" DOMException.
         if subject.has_attribute(&local_name!("open")) {
             return Err(Error::InvalidState(None));
         }
 
-        // 3. If subject's node document is not fully active, then throw an "InvalidStateError" DOMException.
+        // Step 3. If subject's node document is not fully active, then throw an "InvalidStateError" DOMException.
         if !subject.owner_document().is_fully_active() {
             return Err(Error::InvalidState(None));
         }
 
-        // 4. If subject is not connected, then throw an "InvalidStateError" DOMException.
+        // Step 4. If subject is not connected, then throw an "InvalidStateError" DOMException.
         if !subject.is_connected() {
             return Err(Error::InvalidState(None));
         }
 
-        // TODO 5. If subject is in the popover showing state, then throw an "InvalidStateError" DOMException.
+        // TODO: Step 5. If subject is in the popover showing state, then throw an "InvalidStateError" DOMException.
 
-        // 6. If the result of firing an event named beforetoggle, using ToggleEvent, with the cancelable attribute initialized to true, the oldState attribute initialized to "closed", the newState attribute initialized to "open", and the source attribute initialized to source at subject is false, then return.
+        // Step 6. If the result of firing an event named beforetoggle, using ToggleEvent, with the cancelable attribute initialized to true, the oldState attribute initialized to "closed", the newState attribute initialized to "open", and the source attribute initialized to source at subject is false, then return.
         let event = ToggleEvent::new(
             &self.owner_window(),
             atom!("beforetoggle"),
@@ -94,52 +94,52 @@ impl HTMLDialogElement {
             DOMString::from("closed"),
             DOMString::from("open"),
             source,
-            CanGc::note(),
+            can_gc,
         );
         let event = event.upcast::<Event>();
-        if !event.fire(self.upcast::<EventTarget>(), CanGc::note()) {
+        if !event.fire(self.upcast::<EventTarget>(), can_gc) {
             return Ok(());
         }
 
-        // 7. If subject has an open attribute, then return.
+        // Step 7. If subject has an open attribute, then return.
         if subject.has_attribute(&local_name!("open")) {
             return Ok(());
         }
 
-        // 8. If subject is not connected, then return.
+        // Step 8. If subject is not connected, then return.
         if !subject.is_connected() {
             return Ok(());
         }
 
-        // TODO 9. If subject is in the popover showing state, then return.
+        // TODO: Step 9. If subject is in the popover showing state, then return.
 
-        // TODO 10. Queue a dialog toggle event task given subject, "closed", "open", and source.
+        // TODO: Step 10. Queue a dialog toggle event task given subject, "closed", "open", and source.
 
-        // 11. Add an open attribute to subject, whose value is the empty string.
+        // Step 11. Add an open attribute to subject, whose value is the empty string.
         subject.set_bool_attribute(&local_name!("open"), true, can_gc);
 
-        // TODO 12. Assert: subject's close watcher is not null.
+        // TODO: Step 12. Assert: subject's close watcher is not null.
 
-        // 13. Set is modal of subject to true.
+        // Step 13. Set is modal of subject to true.
         self.is_modal.set(true);
 
-        // TODO: 14. Set subject's node document to be blocked by the modal dialog subject.
+        // TODO: Step 14. Set subject's node document to be blocked by the modal dialog subject.
 
-        // TODO: 15. If subject's node document's top layer does not already contain subject, then add an element to the top layer given subject.
+        // TODO: Step 15. If subject's node document's top layer does not already contain subject, then add an element to the top layer given subject.
 
-        // TODO: 16. Set subject's previously focused element to the focused element.
+        // TODO: Step 16. Set subject's previously focused element to the focused element.
 
-        // TODO: 17. Let document be subject's node document.
+        // TODO: Step 17. Let document be subject's node document.
 
-        // TODO: 18. Let hideUntil be the result of running topmost popover ancestor given subject, document's showing hint popover list, null, and false.
+        // TODO: Step 18. Let hideUntil be the result of running topmost popover ancestor given subject, document's showing hint popover list, null, and false.
 
-        // TODO: 19. If hideUntil is null, then set hideUntil to the result of running topmost popover ancestor given subject, document's showing auto popover list, null, and false.
+        // TODO: Step 19. If hideUntil is null, then set hideUntil to the result of running topmost popover ancestor given subject, document's showing auto popover list, null, and false.
 
-        // TODO: 20. If hideUntil is null, then set hideUntil to document.
+        // TODO: Step 20. If hideUntil is null, then set hideUntil to document.
 
-        // TODO: 21. Run hide all popovers until given hideUntil, false, and true.
+        // TODO: Step 21. Run hide all popovers until given hideUntil, false, and true.
 
-        // TODO(Issue #32702): 22. Run the dialog focusing steps given subject.
+        // TODO(Issue #32702): Step 22. Run the dialog focusing steps given subject.
         Ok(())
     }
 }
@@ -165,17 +165,17 @@ impl HTMLDialogElementMethods<crate::DomTypeHolder> for HTMLDialogElement {
     /// <https://html.spec.whatwg.org/multipage/#dom-dialog-show>
     fn Show(&self, can_gc: CanGc) -> ErrorResult {
         let element = self.upcast::<Element>();
-        // 1. If this has an open attribute and is modal of this is false, then return.
+        // Step 1. If this has an open attribute and is modal of this is false, then return.
         if element.has_attribute(&local_name!("open")) && !self.is_modal.get() {
             return Ok(());
         }
 
-        // 2. If this has an open attribute, then throw an "InvalidStateError" DOMException.
+        // Step 2. If this has an open attribute, then throw an "InvalidStateError" DOMException.
         if element.has_attribute(&local_name!("open")) {
             return Err(Error::InvalidState(None));
         }
 
-        // 3. If the result of firing an event named beforetoggle, using ToggleEvent, with the cancelable attribute initialized to true, the oldState attribute initialized to "closed", and the newState attribute initialized to "open" at this is false, then return.
+        // Step 3. If the result of firing an event named beforetoggle, using ToggleEvent, with the cancelable attribute initialized to true, the oldState attribute initialized to "closed", and the newState attribute initialized to "open" at this is false, then return.
         let event = ToggleEvent::new(
             &self.owner_window(),
             atom!("beforetoggle"),
@@ -184,36 +184,36 @@ impl HTMLDialogElementMethods<crate::DomTypeHolder> for HTMLDialogElement {
             DOMString::from("closed"),
             DOMString::from("open"),
             None,
-            CanGc::note(),
+            can_gc,
         );
         let event = event.upcast::<Event>();
-        if !event.fire(self.upcast::<EventTarget>(), CanGc::note()) {
+        if !event.fire(self.upcast::<EventTarget>(), can_gc) {
             return Ok(());
         }
 
-        // 4. If this has an open attribute, then return.
+        // Step 4. If this has an open attribute, then return.
         if element.has_attribute(&local_name!("open")) {
             return Ok(());
         }
 
-        // TODO: 5. Queue a dialog toggle event task given this, "closed", "open", and null.
+        // TODO: Step 5. Queue a dialog toggle event task given this, "closed", "open", and null.
 
-        // 6. Add an open attribute to this, whose value is the empty string.
+        // Step 6. Add an open attribute to this, whose value is the empty string.
         element.set_bool_attribute(&local_name!("open"), true, can_gc);
 
-        // TODO: 7. Set this's previously focused element to the focused element.
+        // TODO: Step 7. Set this's previously focused element to the focused element.
 
-        // TODO: 8. Let document be this's node document.
+        // TODO: Step 8. Let document be this's node document.
 
-        // TODO: 9. Let hideUntil be the result of running topmost popover ancestor given this, document's showing hint popover list, null, and false.
+        // TODO: Step 9. Let hideUntil be the result of running topmost popover ancestor given this, document's showing hint popover list, null, and false.
 
-        // TODO: 10. If hideUntil is null, then set hideUntil to the result of running topmost popover ancestor given this, document's showing auto popover list, null, and false.
+        // TODO: Step 10. If hideUntil is null, then set hideUntil to the result of running topmost popover ancestor given this, document's showing auto popover list, null, and false.
 
-        // TODO: 11. If hideUntil is null, then set hideUntil to document.
+        // TODO: Step 11. If hideUntil is null, then set hideUntil to document.
 
-        // TODO: 12. Run hide all popovers until given hideUntil, false, and true.
+        // TODO: Step 12. Run hide all popovers until given hideUntil, false, and true.
 
-        // TODO(Issue #32702): 13. Run the dialog focusing steps given this.
+        // TODO(Issue #32702): Step 13. Run the dialog focusing steps given this.
 
         Ok(())
     }
@@ -237,17 +237,17 @@ impl HTMLDialogElementMethods<crate::DomTypeHolder> for HTMLDialogElement {
             return;
         }
 
-        // 8. Set is modal of subject to false.
+        // Step 8. Set is modal of subject to false.
         self.is_modal.set(false);
 
-        // 9. If result is not null, then set subject's returnValue attribute to result.
+        // Step 9. If result is not null, then set subject's returnValue attribute to result.
         if let Some(new_value) = return_value {
             *self.return_value.borrow_mut() = new_value;
         }
 
         // TODO: Step 4 implement pending dialog stack removal
 
-        // 13. Queue an element task on the user interaction task source given the subject element to fire an event named close at subject.
+        // Step 13. Queue an element task on the user interaction task source given the subject element to fire an event named close at subject.
         self.owner_global()
             .task_manager()
             .dom_manipulation_task_source()
