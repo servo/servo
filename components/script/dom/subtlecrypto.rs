ffe9c45b2900387fdbccd094653d09914880f05c
Kingsley Yung <kingsley@kkoyung.dev>
2025-11-05 12:29:03 +0800

    script: Implement derive bits operation of ECDH (#40333)
    Finish adding ECDH support to WebCrypto API. This patch implements
    derive bits operation of ECDH.
    
    Testing: Pass some WPT tests that were expected to fail.
    Fixes: Part of #39060
    
    ---------
    
    Signed-off-by: Kingsley Yung <kingsley@kkoyung.dev>

--- components/script/dom/subtlecrypto.rs
+++ components/script/dom/subtlecrypto.rs
@@ -30,8 +30,9 @@ use crate::dom::bindings::codegen::Bindings::CryptoKeyBinding::{
 use crate::dom::bindings::codegen::Bindings::SubtleCryptoBinding::{
     AesCbcParams, AesCtrParams, AesDerivedKeyParams, AesGcmParams, AesKeyAlgorithm,
     AesKeyGenParams, Algorithm, AlgorithmIdentifier, EcKeyAlgorithm, EcKeyGenParams,
-    EcKeyImportParams, HkdfParams, HmacImportParams, HmacKeyAlgorithm, HmacKeyGenParams,
-    JsonWebKey, KeyAlgorithm, KeyFormat, Pbkdf2Params, RsaOtherPrimesInfo, SubtleCryptoMethods,
+    EcKeyImportParams, EcdhKeyDeriveParams, HkdfParams, HmacImportParams, HmacKeyAlgorithm,
+    HmacKeyGenParams, JsonWebKey, KeyAlgorithm, KeyFormat, Pbkdf2Params, RsaOtherPrimesInfo,
+    SubtleCryptoMethods,
 };
 use crate::dom::bindings::codegen::UnionTypes::{
     ArrayBufferViewOrArrayBuffer, ArrayBufferViewOrArrayBufferOrJsonWebKey, ObjectOrString,
@@ -1701,6 +1702,25 @@ impl From<EcKeyImportParams> for SubtleEcKeyImportParams {
     }
 }
 
+/// <https://w3c.github.io/webcrypto/#dfn-EcdhKeyDeriveParams>
+#[derive(Clone, MallocSizeOf)]
+struct SubtleEcdhKeyDeriveParams {
+    /// <https://w3c.github.io/webcrypto/#dom-algorithm-name>
+    name: String,
+
+    /// <https://w3c.github.io/webcrypto/#dfn-EcdhKeyDeriveParams-public>
+    public: Trusted<CryptoKey>,
+}
+
+impl From<EcdhKeyDeriveParams> for SubtleEcdhKeyDeriveParams {
+    fn from(value: EcdhKeyDeriveParams) -> Self {
+        SubtleEcdhKeyDeriveParams {
+            name: value.parent.name.to_string(),
+            public: Trusted::new(&value.public),
+        }
+    }
+}
+
 #[derive(Clone, Debug, MallocSizeOf)]
 pub(crate) struct SubtleAesCbcParams {
     pub(crate) name: String,
@@ -2207,11 +2227,12 @@ impl JsonWebKeyExt for JsonWebKey {
 /// binding of) IDL dictionary types.
 ///
 /// <https://w3c.github.io/webcrypto/#algorithm-normalization-normalize-an-algorithm>
-#[derive(Clone, Debug, MallocSizeOf)]
+#[derive(Clone, MallocSizeOf)]
 enum NormalizedAlgorithm {
     Algorithm(SubtleAlgorithm),
     EcKeyGenParams(SubtleEcKeyGenParams),
     EcKeyImportParams(SubtleEcKeyImportParams),
+    EcdhKeyDeriveParams(SubtleEcdhKeyDeriveParams),
     AesCtrParams(SubtleAesCtrParams),
     AesKeyGenParams(SubtleAesKeyGenParams),
     AesDerivedKeyParams(SubtleAesDerivedKeyParams),
@@ -2313,6 +2334,12 @@ fn normalize_algorithm(
                         dictionary_from_jsval::<EcKeyGenParams>(cx, value.handle(), can_gc)?;
                     params.parent.name = DOMString::from(alg_name);
                     NormalizedAlgorithm::EcKeyGenParams(params.into())
+                },
+                (ALG_ECDH, Operation::DeriveBits) => {
+                    let mut params =
+                        dictionary_from_jsval::<EcdhKeyDeriveParams>(cx, value.handle(), can_gc)?;
+                    params.parent.name = DOMString::from(alg_name);
+                    NormalizedAlgorithm::EcdhKeyDeriveParams(params.into())
                 },
                 (ALG_ECDH, Operation::ImportKey) => {
                     let mut params =
@@ -2672,6 +2699,7 @@ impl NormalizedAlgorithm {
             NormalizedAlgorithm::Algorithm(algo) => &algo.name,
             NormalizedAlgorithm::EcKeyGenParams(algo) => &algo.name,
             NormalizedAlgorithm::EcKeyImportParams(algo) => &algo.name,
+            NormalizedAlgorithm::EcdhKeyDeriveParams(algo) => &algo.name,
             NormalizedAlgorithm::AesCtrParams(algo) => &algo.name,
             NormalizedAlgorithm::AesKeyGenParams(algo) => &algo.name,
             NormalizedAlgorithm::AesDerivedKeyParams(algo) => &algo.name,
@@ -2795,6 +2823,10 @@ impl NormalizedAlgorithm {
 
     fn derive_bits(&self, key: &CryptoKey, length: Option<u32>) -> Result<Vec<u8>, Error> {
         match self {
+            NormalizedAlgorithm::EcdhKeyDeriveParams(algo) => match algo.name.as_str() {
+                ALG_ECDH => ecdh_operation::derive_bits(algo, key, length),
+                _ => Err(Error::NotSupported),
+            },
             NormalizedAlgorithm::HkdfParams(algo) => hkdf_operation::derive_bits(algo, key, length),
             NormalizedAlgorithm::Pbkdf2Params(algo) => {
                 pbkdf2_operation::derive_bits(algo, key, length)
--- components/script/dom/subtlecrypto/ecdh_operation.rs
+++ components/script/dom/subtlecrypto/ecdh_operation.rs
@@ -29,7 +29,7 @@ use crate::dom::globalscope::GlobalScope;
 use crate::dom::subtlecrypto::{
     ALG_ECDH, ExportedKey, JsonWebKeyExt, KeyAlgorithmAndDerivatives, NAMED_CURVE_P256,
     NAMED_CURVE_P384, NAMED_CURVE_P521, SUPPORTED_CURVES, SubtleEcKeyAlgorithm,
-    SubtleEcKeyGenParams, SubtleEcKeyImportParams,
+    SubtleEcKeyGenParams, SubtleEcKeyImportParams, SubtleEcdhKeyDeriveParams,
 };
 use crate::script_runtime::CanGc;
 
@@ -143,6 +143,132 @@ pub(crate) fn generate_key(
 
     // Step 20. Return result.
     Ok(result)
+}
+
+/// <https://w3c.github.io/webcrypto/#ecdh-operations-derive-bits>
+pub(crate) fn derive_bits(
+    normalized_algorithm: &SubtleEcdhKeyDeriveParams,
+    key: &CryptoKey,
+    length: Option<u32>,
+) -> Result<Vec<u8>, Error> {
+    // Step 1. If the [[type]] internal slot of key is not "private", then throw an
+    // InvalidAccessError.
+    if key.Type() != KeyType::Private {
+        return Err(Error::InvalidAccess);
+    }
+
+    // Step 2. Let publicKey be the public member of normalizedAlgorithm.
+    let public_key = normalized_algorithm.public.root();
+
+    // Step 3. If the [[type]] internal slot of publicKey is not "public", then throw an
+    // InvalidAccessError.
+    if public_key.Type() != KeyType::Public {
+        return Err(Error::InvalidAccess);
+    }
+
+    // Step 4. If the name attribute of the [[algorithm]] internal slot of publicKey is not equal
+    // to the name property of the [[algorithm]] internal slot of key, then throw an
+    // InvalidAccessError.
+    if public_key.algorithm().name() != key.algorithm().name() {
+        return Err(Error::InvalidAccess);
+    }
+
+    // Step 5. If the namedCurve attribute of the [[algorithm]] internal slot of publicKey is not
+    // equal to the namedCurve property of the [[algorithm]] internal slot of key, then throw an
+    // InvalidAccessError.
+    let (
+        KeyAlgorithmAndDerivatives::EcKeyAlgorithm(public_key_algorithm),
+        KeyAlgorithmAndDerivatives::EcKeyAlgorithm(key_algorithm),
+    ) = (public_key.algorithm(), key.algorithm())
+    else {
+        return Err(Error::Operation);
+    };
+    if public_key_algorithm.named_curve != key_algorithm.named_curve {
+        return Err(Error::InvalidAccess);
+    }
+
+    // Step 6.
+    // If the namedCurve property of the [[algorithm]] internal slot of key is "P-256", "P-384" or "P-521":
+    //     Step 6.1. Perform the ECDH primitive specified in [RFC6090] Section 4 with key as the EC
+    //     private key d and the EC public key represented by the [[handle]] internal slot of
+    //     publicKey as the EC public key.
+    //
+    //     Step 6.2. Let secret be a byte sequence containing the result of applying the field
+    //     element to octet string conversion defined in Section 6.2 of [RFC6090] to the output of
+    //     the ECDH primitive.
+    //
+    // If the namedCurve property of the [[algorithm]] internal slot of key is a value specified in
+    // an applicable specification that specifies the use of that value with ECDH:
+    //     Perform the ECDH derivation steps specified in that specification, passing in key and
+    //     publicKey and resulting in secret.
+    //
+    // Otherwise:
+    //     throw a NotSupportedError
+    //
+    // Step 7. If performing the operation results in an error, then throw a OperationError.
+    let secret = match key_algorithm.named_curve.as_str() {
+        NAMED_CURVE_P256 => {
+            let Handle::P256PrivateKey(private_key) = key.handle() else {
+                return Err(Error::Operation);
+            };
+            let Handle::P256PublicKey(public_key) = public_key.handle() else {
+                return Err(Error::Operation);
+            };
+            p256::ecdh::diffie_hellman(private_key.to_nonzero_scalar(), public_key.as_affine())
+                .raw_secret_bytes()
+                .to_vec()
+        },
+        NAMED_CURVE_P384 => {
+            let Handle::P384PrivateKey(private_key) = key.handle() else {
+                return Err(Error::Operation);
+            };
+            let Handle::P384PublicKey(public_key) = public_key.handle() else {
+                return Err(Error::Operation);
+            };
+            p384::ecdh::diffie_hellman(private_key.to_nonzero_scalar(), public_key.as_affine())
+                .raw_secret_bytes()
+                .to_vec()
+        },
+        NAMED_CURVE_P521 => {
+            let Handle::P521PrivateKey(private_key) = key.handle() else {
+                return Err(Error::Operation);
+            };
+            let Handle::P521PublicKey(public_key) = public_key.handle() else {
+                return Err(Error::Operation);
+            };
+            p521::ecdh::diffie_hellman(private_key.to_nonzero_scalar(), public_key.as_affine())
+                .raw_secret_bytes()
+                .to_vec()
+        },
+        _ => return Err(Error::NotSupported),
+    };
+
+    // Step 8.
+    // If length is null:
+    //     Return secret
+    // Otherwise:
+    //     If the length in bits of secret is less than length:
+    //         throw an OperationError.
+    //     Otherwise:
+    //         Return a byte sequence containing the first length bits of secret.
+    match length {
+        None => Ok(secret),
+        Some(length) => {
+            if secret.len() * 8 < length as usize {
+                Err(Error::Operation)
+            } else {
+                let mut secret = secret[..length.div_ceil(8) as usize].to_vec();
+                if length % 8 != 0 {
+                    // Clean excess bits in last byte of secret.
+                    let mask = u8::MAX << (8 - length % 8);
+                    if let Some(last_byte) = secret.last_mut() {
+                        *last_byte &= mask;
+                    }
+                }
+                Ok(secret)
+            }
+        },
+    }
 }
 
 /// <https://w3c.github.io/webcrypto/#ecdh-operations-import-key>
