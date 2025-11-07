f1d54097dfae875eec4fbfa038ecea37adfa64cf
Euclid Ye <yezhizhenjiakang@gmail.com>
2025-11-07 19:38:40 +0800

    Fix panic
    Signed-off-by: Euclid Ye <yezhizhenjiakang@gmail.com>

--- components/webdriver_server/lib.rs
+++ components/webdriver_server/lib.rs
@@ -2178,7 +2178,17 @@ impl Handler {
         }
 
         // Step 14. Remove an input source with input state and input id.
-        self.session_mut()?.input_state_table.remove(&id);
+        // It is possible that we only dispatched keydown.
+        // In that case, we cannot remove the id from input state table.
+        // This is a bug in spec: https://github.com/servo/servo/issues/37579#issuecomment-2990762713
+        if self
+            .session()?
+            .input_cancel_list
+            .iter()
+            .all(|(cancel_item_id, _)| &id != cancel_item_id)
+        {
+            self.session_mut()?.input_state_table.remove(&id);
+        }
 
         Ok(WebDriverResponse::Void)
     }
