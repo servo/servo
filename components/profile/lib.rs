0156f87219cd2cfdb63ded0ca9708630e41bd3d8
Euclid Ye <yezhizhenjiakang@gmail.com>
2025-11-11 14:19:22 +0900

    Ohos is linux
    Signed-off-by: Euclid Ye <yezhizhenjiakang@gmail.com>

--- components/profile/lib.rs
+++ components/profile/lib.rs
@@ -5,10 +5,7 @@
 #![deny(unsafe_code)]
 
 pub mod mem;
-#[cfg_attr(
-    not(any(target_os = "windows", target_env = "ohos")),
-    expect(unsafe_code)
-)]
+#[cfg_attr(not(target_os = "windows"), expect(unsafe_code))]
 pub mod system_reporter;
 pub mod time;
 pub mod trace_dump;
