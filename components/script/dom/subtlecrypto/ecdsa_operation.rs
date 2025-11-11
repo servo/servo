ce05d1e25cc7b50a13bfe07fa11bd30da8c37790
Kingsley Yung <kingsley@kkoyung.dev>
2025-11-11 17:06:20 +0800

    Implement verify operation of ECDSA
    Signed-off-by: Kingsley Yung <kingsley@kkoyung.dev>

--- components/script/dom/subtlecrypto/ecdsa_operation.rs
+++ components/script/dom/subtlecrypto/ecdsa_operation.rs
@@ -3,6 +3,9 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 use base64ct::{Base64UrlUnpadded, Encoding};
+use digest::Digest;
+use ecdsa::signature::hazmat::PrehashVerifier;
+use ecdsa::{Signature, VerifyingKey};
 use elliptic_curve::SecretKey;
 use elliptic_curve::rand_core::OsRng;
 use elliptic_curve::sec1::{FromEncodedPoint, ToEncodedPoint, ValidatePublicKey};
@@ -14,6 +17,8 @@ use pkcs8::spki::EncodePublicKey;
 use pkcs8::{AssociatedOid, EncodePrivateKey, PrivateKeyInfo, SubjectPublicKeyInfo};
 use sec1::der::asn1::BitString;
 use sec1::{EcParameters, EcPrivateKey, EncodedPoint};
+use sha1::Sha1;
+use sha2::{Sha256, Sha384, Sha512};
 
 use crate::dom::bindings::codegen::Bindings::CryptoKeyBinding::{
     CryptoKeyMethods, CryptoKeyPair, KeyType, KeyUsage,
@@ -25,11 +30,124 @@ use crate::dom::bindings::str::DOMString;
 use crate::dom::cryptokey::{CryptoKey, Handle};
 use crate::dom::globalscope::GlobalScope;
 use crate::dom::subtlecrypto::{
-    ALG_ECDSA, ExportedKey, JsonWebKeyExt, KeyAlgorithmAndDerivatives, NAMED_CURVE_P256,
-    NAMED_CURVE_P384, NAMED_CURVE_P521, SUPPORTED_CURVES, SubtleEcKeyAlgorithm,
-    SubtleEcKeyGenParams, SubtleEcKeyImportParams,
+    ALG_ECDSA, ALG_SHA1, ALG_SHA256, ALG_SHA384, ALG_SHA512, ExportedKey, JsonWebKeyExt,
+    KeyAlgorithmAndDerivatives, NAMED_CURVE_P256, NAMED_CURVE_P384, NAMED_CURVE_P521,
+    SUPPORTED_CURVES, SubtleEcKeyAlgorithm, SubtleEcKeyGenParams, SubtleEcKeyImportParams,
+    SubtleEcdsaParams,
 };
 use crate::script_runtime::CanGc;
+
+const P256_PREHASH_LENGTH: usize = 32;
+const P384_PREHASH_LENGTH: usize = 48;
+const P521_PREHASH_LENGTH: usize = 66;
+
+/// <https://w3c.github.io/webcrypto/#ecdsa-operations-verify>
+pub(crate) fn verify(
+    normalized_algorithm: &SubtleEcdsaParams,
+    key: &CryptoKey,
+    message: &[u8],
+    signature: &[u8],
+) -> Result<bool, Error> {
+    // Step 1. If the [[type]] internal slot of key is not "public", then throw an
+    // InvalidAccessError.
+    if key.Type() != KeyType::Public {
+        return Err(Error::InvalidAccess);
+    }
+
+    // Step 2. Let hashAlgorithm be the hash member of normalizedAlgorithm.
+    let hash_algorithm = &normalized_algorithm.hash;
+
+    // Step 3. Let M be the result of performing the digest operation specified by hashAlgorithm
+    // using message.
+    let m = match hash_algorithm.name.as_str() {
+        ALG_SHA1 => Sha1::new_with_prefix(message).finalize().to_vec(),
+        ALG_SHA256 => Sha256::new_with_prefix(message).finalize().to_vec(),
+        ALG_SHA384 => Sha384::new_with_prefix(message).finalize().to_vec(),
+        ALG_SHA512 => Sha512::new_with_prefix(message).finalize().to_vec(),
+        _ => return Err(Error::NotSupported),
+    };
+
+    // Step 4. Let Q be the ECDSA public key associated with key.
+    // Step 5. Let params be the EC domain parameters associated with key.
+    // Step 6.
+    // If the namedCurve attribute of the [[algorithm]] internal slot of key is "P-256", "P-384" or
+    // "P-521":
+    //     1. Let n be the smallest integer such that n * 8 is greater than the logarithm to base 2
+    //        of the order of the base point of the elliptic curve identified by params.
+    //     2. If signature does not have a length of n * 2 bytes, then return false.
+    //     3. Let r be the result of converting the first n bytes of signature to an integer.
+    //     4. Let s be the result of converting the last n bytes of signature to an integer.
+    //     5. Perform the ECDSA verifying process, as specified in [RFC6090], Section 5.4.3, with M
+    //        as the received message, (r, s) as the signature and using params as the EC domain
+    //        parameters, and Q as the public key.
+    // Otherwise, the namedCurve attribute of the [[algorithm]] internal slot of key is a value
+    // specified in an applicable specification:
+    //     Perform the ECDSA verification steps specified in that specification passing in M,
+    //     signature, params and Q and resulting in an indication of whether or not the purported
+    //     signature is valid.
+    // Step 7. Let result be a boolean with the value true if the signature is valid and the value
+    // false otherwise.
+    // NOTE: We currently do not support other applicable specifications.
+    let KeyAlgorithmAndDerivatives::EcKeyAlgorithm(algorithm) = key.algorithm() else {
+        return Err(Error::Operation);
+    };
+    let result = match algorithm.named_curve.as_str() {
+        NAMED_CURVE_P256 => {
+            // P-256 expects prehash with length at least 32 bytes. If M is shorter than 32 bytes,
+            // expand it by left padding with zeros.
+            let m = expand_prehash(m, P256_PREHASH_LENGTH);
+
+            let Handle::P256PublicKey(q) = key.handle() else {
+                return Err(Error::Operation);
+            };
+            match Signature::<NistP256>::from_slice(signature) {
+                Ok(signature) => {
+                    let verifying_key = VerifyingKey::<NistP256>::from(q);
+                    verifying_key.verify_prehash(&m, &signature).is_ok()
+                },
+                Err(_) => false,
+            }
+        },
+        NAMED_CURVE_P384 => {
+            // P-384 expects prehash with length at least 48 bytes. If M is shorter than 48 bytes,
+            // expand it by left padding with zeros.
+            let m = expand_prehash(m, P384_PREHASH_LENGTH);
+
+            let Handle::P384PublicKey(q) = key.handle() else {
+                return Err(Error::Operation);
+            };
+            match Signature::<NistP384>::from_slice(signature) {
+                Ok(signature) => {
+                    let verifying_key = VerifyingKey::<NistP384>::from(q);
+                    verifying_key.verify_prehash(&m, &signature).is_ok()
+                },
+                Err(_) => false,
+            }
+        },
+        NAMED_CURVE_P521 => {
+            // P-521 expects prehash with length at least 66 bytes. If M is shorter than 66 bytes,
+            // expand it by left padding with zeros.
+            let m = expand_prehash(m, P521_PREHASH_LENGTH);
+
+            let Handle::P521PublicKey(q) = key.handle() else {
+                return Err(Error::Operation);
+            };
+            match (
+                Signature::<NistP521>::from_slice(signature),
+                p521::ecdsa::VerifyingKey::from_sec1_bytes(q.to_sec1_bytes().to_vec().as_slice()),
+            ) {
+                (Ok(signature), Ok(verifying_key)) => {
+                    verifying_key.verify_prehash(&m, &signature).is_ok()
+                },
+                _ => false,
+            }
+        },
+        _ => return Err(Error::NotSupported),
+    };
+
+    // Step 8. Return result.
+    Ok(result)
+}
 
 /// <https://w3c.github.io/webcrypto/#ecdsa-operations-generate-key>
 pub(crate) fn generate_key(
@@ -1056,4 +1174,19 @@ pub(crate) fn export_key(format: KeyFormat, key: &CryptoKey) -> Result<ExportedK
 
     // Step 4. Return result.
     Ok(result)
+}
+
+/// A helper function that expand a prehash to a specified length if the prehash is shorter than
+/// the specified length.
+///
+/// If the length of `prehash` is less than `length`, return a prehash with length `length`
+/// constructed by left-padding `prehash` with zeros. Otherwire, return the unmodified `prehash`.
+fn expand_prehash(prehash: Vec<u8>, length: usize) -> Vec<u8> {
+    if prehash.len() < length {
+        let mut left_padded_prehash = vec![0u8; length];
+        left_padded_prehash[length - prehash.len()..].copy_from_slice(&prehash);
+        left_padded_prehash
+    } else {
+        prehash
+    }
 }
