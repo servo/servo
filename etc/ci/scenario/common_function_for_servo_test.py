d3adcbfd46133c63bf261ae8d31443cb61b721a3
monissaJ <2362314605@qq.com>
2025-11-12 15:33:36 +0800

    add modifyfor format
    Signed-off-by: monissaJ <2362314605@qq.com>

--- etc/ci/scenario/common_function_for_servo_test.py
+++ etc/ci/scenario/common_function_for_servo_test.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+# Copyright 2018 The Servo Project Developers. See the COPYRIGHT
+# file at the top-level directory of this distribution.
+#
+# Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+# http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+# <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+# option. This file may not be copied, modified, or distributed
+# except according to those terms.
 
 import re
 import os
