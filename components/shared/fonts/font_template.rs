563314a1659ab447d97bd7afa11fa206eb74a244
Simon Wülker <simon.wuelker@arcor.de>
2025-11-24 19:11:36 +0100

    fonts: Apply font-optical-sizing at the right time
    Signed-off-by: Simon Wülker <simon.wuelker@arcor.de>

--- components/shared/fonts/font_template.rs
+++ components/shared/fonts/font_template.rs
@@ -244,6 +244,14 @@ impl FontTemplate {
             .copied()
             .for_each(&mut add_variation);
 
+        // Step 9. Font variations implied by the value of the font-optical-sizing property are applied.
+        if descriptor.optical_sizing == FontOpticalSizing::Auto {
+            add_variation(FontVariation {
+                tag: Tag::new(b"opsz").to_u32(),
+                value: descriptor.pt_size.to_f32_px(),
+            });
+        }
+
         if let Some(font_face_rule) = &self.font_face_rule {
             // Step 6. If the font is defined via an @font-face rule, the font variations implied by the font-variation-settings
             // descriptor in the @font-face rule are applied.
@@ -257,14 +265,6 @@ impl FontTemplate {
                     })
                     .for_each(&mut add_variation);
             }
-        }
-
-        // Step 9. Font variations implied by the value of the font-optical-sizing property are applied.
-        if descriptor.optical_sizing == FontOpticalSizing::Auto {
-            add_variation(FontVariation {
-                tag: Tag::new(b"opsz").to_u32(),
-                value: descriptor.pt_size.to_f32_px(),
-            });
         }
 
         // Step 2. Font variations as enabled by the font-weight, font-width, and font-style properties are applied.
