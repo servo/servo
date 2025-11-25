8d50d608804e9ce3a8ed227c7fab9a81092c35fa
Euclid Ye <yezhizhenjiakang@gmail.com>
2025-11-25 11:25:20 +0800

    .
    Signed-off-by: Euclid Ye <yezhizhenjiakang@gmail.com>

--- components/background_hang_monitor/sampler_windows.rs
+++ components/background_hang_monitor/sampler_windows.rs
@@ -12,8 +12,7 @@ pub struct WindowsSampler {
 }
 
 impl WindowsSampler {
-    #[expect(unsafe_code, dead_code)]
-    pub fn new_boxed() -> Box<dyn Sampler> {
+    pub(super) fn new_boxed() -> Box<dyn Sampler> {
         let thread_id = 0; // TODO: use windows::Win32::System::Threading::GetThreadId
         Box::new(WindowsSampler { thread_id })
     }
