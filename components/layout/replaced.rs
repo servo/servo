b60a43c11a6da6b9c96090ff335a5280f4d9df22
Rodion Borovyk <rodion.borovyk@gmail.com>
2025-11-27 16:19:23 +0100

    use node.selected_style for the context
    Signed-off-by: Rodion Borovyk <rodion.borovyk@gmail.com>

--- components/layout/replaced.rs
+++ components/layout/replaced.rs
@@ -208,16 +208,17 @@ impl ReplacedContents {
                 });
 
                 let rule_cache_conditions = &mut RuleCacheConditions::default();
+                let parent_style = &node.selected_style();
                 let style_builder = StyleBuilder::new(
                     context.style_context.stylist.device(),
                     Some(context.style_context.stylist),
-                    None,
+                    Some(parent_style),
                     None,
                     None,
                     false,
                 );
 
-                let context = Context::new(
+                let to_computed_context = Context::new(
                     style_builder,
                     QuirksMode::Quirks,
                     rule_cache_conditions,
@@ -226,7 +227,7 @@ impl ReplacedContents {
 
                 let attr_to_computed = |attr_val: &AttrValue| {
                     if let AttrValue::Length(_, length) = attr_val {
-                        length.to_computed_value(&context)
+                        length.to_computed_value(&to_computed_context)
                     } else {
                         None
                     }
