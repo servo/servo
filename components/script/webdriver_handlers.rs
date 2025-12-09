2eed81cf2b29bede177209278bf240c18aafb49c
Narfinger <Narfinger@users.noreply.github.com>
2025-12-09 13:57:42 +0100

    formatting
    Signed-off-by: Narfinger <Narfinger@users.noreply.github.com>

--- components/script/webdriver_handlers.rs
+++ components/script/webdriver_handlers.rs
@@ -5,8 +5,8 @@
 use std::collections::{HashMap, HashSet};
 use std::ffi::CString;
 use std::ptr::NonNull;
-use base::generic_channel::{GenericOneshotSender, GenericSender};
-use base::generic_channel::{GenericSend, GenericSender};
+
+use base::generic_channel::{GenericOneshotSender, GenericSend, GenericSender, GenericSender};
 use base::id::{BrowsingContextId, PipelineId};
 use cookie::Cookie;
 use embedder_traits::{
