b8a253eb75312368b1b821750480b70a8d429995
Narfinger <Narfinger@users.noreply.github.com>
2025-12-12 14:06:08 +0100

    correct license
    Signed-off-by: Narfinger <Narfinger@users.noreply.github.com>

--- etc/blink-perf-test-runner/main.py
+++ etc/blink-perf-test-runner/main.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python3
+
+# Copyright 2025 The Servo Project Developers. See the COPYRIGHT
+# file at the top-level directory of this distribution.
+#
+# Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+# http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+# <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+# option. This file may not be copied, modified, or distributed
+# except according to those terms.
+
 import argparse
 import json
 import os
