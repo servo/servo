9fe5bd0f254f0b2bd7650a5a8c4496948db94e73
Narfinger <Narfinger@users.noreply.github.com>
2025-12-12 11:36:38 +0100

    allow to run blink perf tests in servo
    Signed-off-by: Narfinger <Narfinger@users.noreply.github.com>

---
+++ etc/blink-perf-test-runner/main.py
@@ -0,0 +1,152 @@
+import argparse
+import json
+import os
+from pathlib import PurePath
+import subprocess
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.options import ArgOptions
+from urllib3.exceptions import ProtocolError
+from selenium.common.exceptions import NoSuchElementException
+
+from enum import Enum
+
+
+class AbortReason(Enum):
+    NotFound = 1
+    Panic = 2
+
+
+# the time in seconds we wait at most
+MAX_WAIT_TIME = 60
+# tests that currently do not return results
+IGNORE_TESTS = [
+    "tall-content-short-columns.html",
+    "tall-content-short-columns-realistic.html",
+]
+
+
+def create_driver(timeout: int = 10) -> webdriver.Remote | None:
+    """Create the webdriver connection."""
+    print("Trying to create driver")
+    options = ArgOptions()
+    options.set_capability("browserName", "servo")
+    driver = None
+    start_time = time.time()
+    while driver is None and time.time() - start_time < timeout:
+        try:
+            driver = webdriver.Remote(
+                command_executor="http://127.0.0.1:7000", options=options
+            )
+        except (ConnectionError, ProtocolError):
+            time.sleep(0.2)
+        except Exception as e:
+            print(f"Unexpected exception when creating webdriver: {e}, {type(e)}")
+            time.sleep(1)
+    print(
+        "Established Webdriver connection in",
+    )
+    return driver
+
+
+def start_servo(servo_path: str) -> webdriver.Remote | None:
+    """Start servo and create webdriver"""
+    subprocess.Popen(
+        [
+            servo_path,
+            "--webdriver",
+            "--pref=network_http_proxy_uri=http://127.0.0.1:3128",
+        ]
+    )
+    return create_driver()
+
+
+def kill_servo():
+    subprocess.Popen(["killall", "servo"])
+
+
+def test(s: str, driver: webdriver.Remote) -> tuple[str, str, str] | AbortReason:
+    """Run a test by loading a website, and returning (avg, min, max).
+    This will run for MAX_WAIT_TIME seconds and return as soon as the avg line exists in the log element"""
+
+    print("Running: " + canonical_test_path(s))
+    try:
+        driver.get(s)
+        for i in range(MAX_WAIT_TIME):
+            element = driver.find_element(By.ID, "log")
+            text = element.text
+            result_lines = text.split("\n")
+            # get the avg line or return None if it doesn't exist yet
+            avg_line = next(filter(lambda x: "avg" in x, result_lines), None)
+            min_line = next(filter(lambda x: "min" in x, result_lines), None)
+            max_line = next(filter(lambda x: "max" in x, result_lines), None)
+            if avg_line is not None and min_line is not None and max_line is not None:
+                return (avg_line.split()[1], min_line.split()[1], max_line.split()[1])
+            time.sleep(1)
+    except NoSuchElementException:
+        print("Could not find log?")
+        return AbortReason.NotFound
+    except:  # noqa: E722
+        print("Some other exception for this test case")
+        return AbortReason.Panic
+    return AbortReason.NotFound
+
+
+def canonical_test_path(filePath: str) -> str:
+    """Make the filepath into just the directory and name"""
+    p = PurePath(filePath)
+    parts = p.parts
+    index = parts.index("perf_tests")
+    return "/".join(parts[index:])
+
+
+def test_file(file_name: str) -> bool:
+    return ".html" in file_name and (file_name not in IGNORE_TESTS)
+
+
+def write_file(results):
+    with open("results.json", "w") as f:
+        json.dump(results, f)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Run Chrome Tests on Servo Instance.")
+    parser.add_argument("servo_path", type=str, help="the servo binary")
+    parser.add_argument(
+        "test_dir", type=str, help="The directory for the chromium tests."
+    )
+    args = parser.parse_args()
+
+    webdriver = start_servo(args.servo_path)
+    final_result = {}
+    time.sleep(2)
+    if webdriver:
+        webdriver.implicitly_wait(30)
+        for root, dir, files in os.walk(args.test_dir):
+            for file in files:
+                if test_file(file):
+                    filePath = os.path.join(os.path.abspath(root), file)
+                    result = test("file://" + filePath, webdriver)
+                    if result == AbortReason.Panic:
+                        print("Restarting servo")
+                        start_servo(args.servo_path)
+                    elif result == AbortReason.NotFound:
+                        pass
+                    else:
+                        combined_result = {}
+                        combined_result["value"] = result[0]
+                        combined_result["lower_value"] = result[1]
+                        combined_result["upper_value"] = result[2]
+
+                        final_result[canonical_test_path(filePath)] = {
+                            "throughput": combined_result
+                        }
+
+    print(final_result)
+    write_file(final_result)
+    kill_servo()
+
+
+if __name__ == "__main__":
+    main()
