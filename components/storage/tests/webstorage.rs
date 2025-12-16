a39809c1aa5493de7fb12b6fc6607c80cab6c90c
Ashwin Naren <arihant2math@gmail.com>
2025-12-16 11:52:04 -0800

    add test
    Signed-off-by: Ashwin Naren <arihant2math@gmail.com>

--- components/storage/tests/webstorage.rs
+++ components/storage/tests/webstorage.rs
@@ -399,3 +399,60 @@ fn clear_data_for_sites_local_in_memory() {
     let test = WebStorageTest::new_in_memory();
     test_clear_data_for_sites(test, WebStorageType::Local);
 }
+
+#[test]
+fn no_storage_type_conflict() {
+    // Ensures that editing session storage does not affect local storage and vice versa.
+    let (_tmp_dir, threads) = init();
+    let url = ServoUrl::parse("https://example.com").unwrap();
+    // Set local storage item.
+    let (sender, receiver) = base_channel::channel().unwrap();
+    threads
+        .send(WebStorageThreadMsg::SetItem(
+            sender,
+            StorageType::Local,
+            TEST_WEBVIEW_ID,
+            url.clone(),
+            "key".into(),
+            "local_value".into(),
+        ))
+        .unwrap();
+    let _ = receiver.recv().unwrap();
+    // Set session storage item.
+    let (sender, receiver) = base_channel::channel().unwrap();
+    threads
+        .send(WebStorageThreadMsg::SetItem(
+            sender,
+            StorageType::Session,
+            TEST_WEBVIEW_ID,
+            url.clone(),
+            "key".into(),
+            "session_value".into(),
+        ))
+        .unwrap();
+    let _ = receiver.recv().unwrap();
+    // Get local storage item.
+    let (sender, receiver) = base_channel::channel().unwrap();
+    threads
+        .send(WebStorageThreadMsg::GetItem(
+            sender,
+            StorageType::Local,
+            TEST_WEBVIEW_ID,
+            url.clone(),
+            "key".into(),
+        ))
+        .unwrap();
+    assert_eq!(receiver.recv().unwrap(), Some("local_value".into()));
+    // Get session storage item.
+    let (sender, receiver) = base_channel::channel().unwrap();
+    threads
+        .send(WebStorageThreadMsg::GetItem(
+            sender,
+            StorageType::Session,
+            TEST_WEBVIEW_ID,
+            url.clone(),
+            "key".into(),
+        ))
+        .unwrap();
+    assert_eq!(receiver.recv().unwrap(), Some("session_value".into()));
+}
