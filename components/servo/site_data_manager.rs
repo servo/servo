d12b8574e40e309bd20a94bb85f1dfe187fe5153
Jan Varga <jvarga@igalia.com>
2025-12-18 11:23:57 +0100

    libservo: Add storage threads to `SiteDataManager`
    This patch adds access to storage threads to `SiteDataManager`, mirroring the
    existing handling of resource threads.
    
    This is preparatory work for invoking storage related operations from
    `SiteDataManager`.
    
    Signed-off-by: Jan Varga <jvarga@igalia.com>

--- components/servo/site_data_manager.rs
+++ components/servo/site_data_manager.rs
@@ -3,20 +3,27 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 use net_traits::ResourceThreads;
+use storage_traits::StorageThreads;
 
 pub struct SiteDataManager {
     public_resource_threads: ResourceThreads,
     private_resource_threads: ResourceThreads,
+    _public_storage_threads: StorageThreads,
+    _private_storage_threads: StorageThreads,
 }
 
 impl SiteDataManager {
     pub(crate) fn new(
         public_resource_threads: ResourceThreads,
         private_resource_threads: ResourceThreads,
+        public_storage_threads: StorageThreads,
+        private_storage_threads: StorageThreads,
     ) -> Self {
         Self {
             public_resource_threads,
             private_resource_threads,
+            _public_storage_threads: public_storage_threads,
+            _private_storage_threads: private_storage_threads,
         }
     }
 
