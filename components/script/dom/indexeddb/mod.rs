2e4a3060082f060086b10211fd3d3fa496fea23e
gterzian <2792687+gterzian@users.noreply.github.com>
2025-12-18 22:14:04 +0700

    lint
    Signed-off-by: gterzian <2792687+gterzian@users.noreply.github.com>

--- components/script/dom/indexeddb/mod.rs
+++ components/script/dom/indexeddb/mod.rs
@@ -2,10 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-use storage_traits::indexeddb::BackendError;
-
-use crate::dom::bindings::error::Error;
-
 pub(crate) mod idbcursor;
 pub(crate) mod idbcursorwithvalue;
 pub(crate) mod idbdatabase;
