85a5ad80c6bcf505e38dc0b0a04a02fcd4b95383
Narfinger <Narfinger@users.noreply.github.com>
2025-12-19 17:20:46 +0100

    incorporate suggestions
    Signed-off-by: Narfinger <Narfinger@users.noreply.github.com>

--- components/servo/servo.rs
+++ components/servo/servo.rs
@@ -549,15 +549,25 @@ impl ServoInner {
                         webview,
                         gamepad_index,
                         gamepad_haptic_effect_type,
-                        callback,
+                        Box::new(move |msg| {
+                            callback
+                                .send(msg)
+                                .expect("Could not send message via callback")
+                        }),
                     );
                 }
             },
             EmbedderMsg::StopGamepadHapticEffect(webview_id, gamepad_index, callback) => {
                 if let Some(webview) = self.get_webview_handle(webview_id) {
-                    webview
-                        .delegate()
-                        .stop_gamepad_haptic_effect(webview, gamepad_index, callback);
+                    webview.delegate().stop_gamepad_haptic_effect(
+                        webview,
+                        gamepad_index,
+                        Box::new(move |msg| {
+                            callback
+                                .send(msg)
+                                .expect("Could not send message via callback")
+                        }),
+                    );
                 }
             },
             EmbedderMsg::ShowNotification(webview_id, notification) => {
--- components/servo/webview_delegate.rs
+++ components/servo/webview_delegate.rs
@@ -5,7 +5,7 @@
 use std::path::PathBuf;
 use std::rc::Rc;
 
-use base::generic_channel::{GenericCallback, GenericSender};
+use base::generic_channel::GenericSender;
 use base::id::{PipelineId, WebViewId};
 use compositing_traits::rendering_context::RenderingContext;
 use constellation_traits::EmbedderToConstellationMessage;
@@ -945,11 +945,11 @@ pub trait WebViewDelegate {
         _webview: WebView,
         _: usize,
         _: GamepadHapticEffectType,
-        _: GenericCallback<bool>,
+        _: Box<dyn Fn(bool)>,
     ) {
     }
     /// Request to stop a haptic effect on a connected gamepad.
-    fn stop_gamepad_haptic_effect(&self, _webview: WebView, _: usize, _: GenericCallback<bool>) {}
+    fn stop_gamepad_haptic_effect(&self, _webview: WebView, _: usize, _: Box<dyn Fn(bool)>) {}
 
     /// Triggered when this [`WebView`] will load a web (HTTP/HTTPS) resource. The load may be
     /// intercepted and alternate contents can be loaded by the client by calling
