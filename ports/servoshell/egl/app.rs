96e0d8b35686e1af29e69228b91c9e6112ffded9
Wayne Van Son <waynevanson@gmail.com>
2026-01-01 11:28:41 +1100

    refactor: remove `dead_code` lint from ohos XrDiscoveryWebXrRegistry
    Signed-off-by: Wayne Van Son <waynevanson@gmail.com>

--- ports/servoshell/egl/app.rs
+++ ports/servoshell/egl/app.rs
@@ -618,7 +618,6 @@ pub(crate) struct XrDiscoveryWebXrRegistry {
 }
 
 #[cfg(feature = "webxr")]
-#[cfg_attr(target_env = "ohos", expect(dead_code))]
 impl XrDiscoveryWebXrRegistry {
     pub(crate) fn new(xr_discovery: Option<servo::webxr::Discovery>) -> Self {
         Self {
