2053cbbfb544eed2951ed65f8fedca52130278b0
Euclid Ye <yezhizhenjiakang@gmail.com>
2026-01-02 20:20:44 +0800

    Update invalid timeout tests
    Signed-off-by: Euclid Ye <yezhizhenjiakang@gmail.com>

--- tests/wpt/tests/webdriver/tests/classic/new_session/timeouts.py
+++ tests/wpt/tests/webdriver/tests/classic/new_session/timeouts.py
@@ -20,11 +20,15 @@ def test_timeouts(new_session, add_browser_capabilities, key, value):
     value = assert_success(response)
     assert value["capabilities"]["timeouts"] == timeouts
 
-@pytest.mark.parametrize("timeouts", [
-    {"implicit": None, "pageLoad": MAX_SAFE_INTEGER + 2,"script": 30000},
-    {"implicit": MAX_SAFE_INTEGER + 2, "pageLoad": None,"script": 30000},
-    {"implicit": None, "pageLoad": None,"script": MAX_SAFE_INTEGER + 2}
-])
-def test_invalid_timeouts(new_session, add_browser_capabilities, timeouts):
-    response, _ = new_session({"capabilities": {"alwaysMatch": add_browser_capabilities({"timeouts": timeouts})}})
+@pytest.mark.parametrize("value", [MAX_SAFE_INTEGER + 1, -11])
+@pytest.mark.parametrize("key", ["implicit", "pageLoad", "script"])
+def test_invalid_timeouts(new_session, add_browser_capabilities, key, value):
+    timeouts = {key: value}
+
+    response, _ = new_session({
+        "capabilities": {
+            "alwaysMatch": add_browser_capabilities({"timeouts": timeouts})
+        }
+    })
+
     assert_error(response, "invalid argument")
