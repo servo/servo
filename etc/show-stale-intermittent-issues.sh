02ea55db516eccabb485c61d6668baac08df1d17
Tim van der Lippe <TimvdLippe@users.noreply.github.com>
2026-01-08 22:11:37 +0100

    Update etc/show-stale-intermittent-issues.sh
    Co-authored-by: Martin Robinson <mrobinson@igalia.com>
    Signed-off-by: Tim van der Lippe <TimvdLippe@users.noreply.github.com>

--- etc/show-stale-intermittent-issues.sh
+++ etc/show-stale-intermittent-issues.sh
@@ -1,8 +1,7 @@
-#!/usr/bin/env bash
-
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#!/usr/bin/env sh
 
 set -o errexit
 set -o nounset
