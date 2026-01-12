b67f4ef15416753922f0e5e08f5408add1073105
Narfinger <Narfinger@users.noreply.github.com>
2026-01-12 09:36:10 +0100

    incorporate suggestions
    Signed-off-by: Narfinger <Narfinger@users.noreply.github.com>

--- components/shared/profile/generic_callback.rs
+++ components/shared/profile/generic_callback.rs
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-use base::generic_channel::{GenericReceiver, SendResult};
+use base::generic_channel::SendResult;
 use serde::{Deserialize, Serialize};
 
 use crate::time::{ProfilerCategory, ProfilerChan};
