servo/servo

commit e95afbf7cf877d2d38c9cae8e9e46b67bce067b7

devtools: Remove find_mut and make all actors immutable (#41744)
Remove `find_mut` from `ActorRegistry`. Force that all actors must be
immutable after being inserted in the registry, only allowing changes
through internal mutability.

First step in refactoring `ActorRegistry` to make it more reliable,
easier to use and less error prone.

Depends on #41741. `NetworkEventActor` was more complicated to refactor
and it needed special care so it is split into its own change.

Testing: This patch doesn't change behaviour.

Signed-off-by: eri <eri@igalia.com>

components/devtools/lib.rs
@@ -292,8 +292,8 @@ impl DevtoolsInstance {
     }
 
     fn handle_framerate_tick(&self, actor_name: String, tick: f64) {
-        let mut actors = self.registry.lock().unwrap();
-        let framerate_actor = actors.find_mut::<FramerateActor>(&actor_name);
+        let actors = self.registry.lock().unwrap();
+        let framerate_actor = actors.find::<FramerateActor>(&actor_name);
         framerate_actor.add_tick(tick);
     }
 
@@ -356,8 +356,8 @@ impl DevtoolsInstance {
                 script_chan: script_sender,
                 streams: Default::default(),
             };
-            let root = actors.find_mut::<RootActor>("root");
-            root.workers.push(worker.name.clone());
+            let root = actors.find::<RootActor>("root");
+            root.tabs.borrow_mut().push(worker.name.clone());
 
             self.actor_workers.insert(id, worker_name.clone());
             actors.register(worker);
@@ -570,7 +570,7 @@ impl DevtoolsInstance {
             };
 
             let thread_actor_name = actors.find::<WorkerActor>(worker_actor_name).thread.clone();
-            let thread_actor = actors.find_mut::<ThreadActor>(&thread_actor_name);
+            let thread_actor = actors.find::<ThreadActor>(&thread_actor_name);
 
             thread_actor.source_manager.add_source(&source_actor_name);
 
@@ -597,8 +597,7 @@ impl DevtoolsInstance {
                 browsing_context.thread.clone()
             };
 
-            let thread_actor = actors.find_mut::<ThreadActor>(&thread_actor_name);
-
+            let thread_actor = actors.find::<ThreadActor>(&thread_actor_name);
             thread_actor.source_manager.add_source(&source_actor_name);
 
             // Notify browsing context about the new source
@@ -619,9 +618,10 @@ impl DevtoolsInstance {
         let mut actors = self.registry.lock().unwrap();
 
         for actor_name in actors.source_actor_names_for_pipeline(pipeline_id) {
-            let source_actor: &mut SourceActor = actors.find_mut(&actor_name);
-            if source_actor.content.is_none() {
-                source_actor.content = Some(source_content.clone());
+            let source_actor = actors.find::<SourceActor>(&actor_name);
+            let mut content = source_actor.content.borrow_mut();
+            if content.is_none() {
+                *content = Some(source_content.clone());
             }
         }
 

components/devtools/actor.rs
@@ -65,14 +65,10 @@ pub(crate) trait Actor: Any + ActorAsAny + Send {
 
 pub(crate) trait ActorAsAny {
     fn actor_as_any(&self) -> &dyn Any;
-    fn actor_as_any_mut(&mut self) -> &mut dyn Any;
 }
 
 impl<T: Actor> ActorAsAny for T {
     fn actor_as_any(&self) -> &dyn Any {
-        self
-    }
-    fn actor_as_any_mut(&mut self) -> &mut dyn Any {
         self
     }
 }
@@ -163,12 +159,6 @@ impl ActorRegistry {
     pub fn find<'a, T: Any>(&'a self, name: &str) -> &'a T {
         let actor = self.actors.get(name).unwrap();
         actor.actor_as_any().downcast_ref::<T>().unwrap()
-    }
-
-    /// Find an actor by registered name
-    pub fn find_mut<'a, T: Any>(&'a mut self, name: &str) -> &'a mut T {
-        let actor = self.actors.get_mut(name).unwrap();
-        actor.actor_as_any_mut().downcast_mut::<T>().unwrap()
     }
 
     /// Find an actor by registered name and return its serialization

components/devtools/actors/framerate.rs
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-use std::mem;
+use std::cell::RefCell;
 
 use base::generic_channel::GenericSender;
 use base::id::PipelineId;
@@ -16,7 +16,7 @@ pub struct FramerateActor {
     pipeline_id: PipelineId,
     script_sender: GenericSender<DevtoolScriptControlMsg>,
     is_recording: bool,
-    ticks: Vec<HighResolutionStamp>,
+    ticks: RefCell<Vec<HighResolutionStamp>>,
 }
 
 impl Actor for FramerateActor {
@@ -38,7 +38,7 @@ impl FramerateActor {
             pipeline_id,
             script_sender,
             is_recording: false,
-            ticks: Vec::new(),
+            ticks: Default::default(),
         };
 
         actor.start_recording();
@@ -46,8 +46,10 @@ impl FramerateActor {
         actor_name
     }
 
-    pub fn add_tick(&mut self, tick: f64) {
-        self.ticks.push(HighResolutionStamp::wrap(tick));
+    pub fn add_tick(&self, tick: f64) {
+        self.ticks
+            .borrow_mut()
+            .push(HighResolutionStamp::wrap(tick));
 
         if self.is_recording {
             let msg = DevtoolScriptControlMsg::RequestAnimationFrame(self.pipeline_id, self.name());
@@ -55,8 +57,8 @@ impl FramerateActor {
         }
     }
 
-    pub fn take_pending_ticks(&mut self) -> Vec<HighResolutionStamp> {
-        mem::take(&mut self.ticks)
+    pub fn take_pending_ticks(&self) -> Vec<HighResolutionStamp> {
+        self.ticks.take()
     }
 
     fn start_recording(&mut self) {

components/devtools/actors/root.rs
@@ -42,7 +42,7 @@ struct ListAddonsReply {
 #[derive(Serialize)]
 enum AddonMsg {}
 
-#[derive(Clone, Serialize)]
+#[derive(Clone, Default, Serialize)]
 #[serde(rename_all = "camelCase")]
 struct GlobalActors {
     device_actor: String,
@@ -126,12 +126,13 @@ struct GetProcessResponse {
     process_descriptor: ProcessActorMsg,
 }
 
+#[derive(Default)]
 pub struct RootActor {
     active_tab: RefCell<Option<String>>,
     global_actors: GlobalActors,
     process: String,
-    pub tabs: Vec<String>,
-    pub workers: Vec<String>,
+    pub tabs: RefCell<Vec<String>>,
+    pub workers: RefCell<Vec<String>>,
 }
 
 impl Actor for RootActor {
@@ -220,6 +221,7 @@ impl Actor for RootActor {
                     from: "root".to_owned(),
                     tabs: self
                         .tabs
+                        .borrow()
                         .iter()
                         .filter_map(|target| {
                             let tab_actor = registry.find::<TabDescriptorActor>(target);
@@ -240,6 +242,7 @@ impl Actor for RootActor {
                     from: self.name(),
                     workers: self
                         .workers
+                        .borrow()
                         .iter()
                         .map(|name| registry.encode::<WorkerActor, _>(name))
                         .collect(),
@@ -277,15 +280,13 @@ impl RootActor {
 
         // Root actor
         let root = Self {
-            active_tab: None.into(),
             global_actors: GlobalActors {
                 device_actor: device.name(),
                 perf_actor: perf.name(),
                 preference_actor: preference.name(),
             },
             process: process.name(),
-            tabs: vec![],
-            workers: vec![],
+            ..Default::default()
         };
 
         registry.register(perf);
@@ -302,6 +303,7 @@ impl RootActor {
     ) -> Option<TabDescriptorActorMsg> {
         let mut tab_msg = self
             .tabs
+            .borrow()
             .iter()
             .map(|target| registry.encode::<TabDescriptorActor, _>(target))
             .find(|tab| tab.browser_id() == browser_id);

components/devtools/actors/source.rs
@@ -55,7 +55,7 @@ pub struct SourceActor {
     /// <https://firefox-source-docs.mozilla.org/devtools/backend/protocol.html#black-boxing-sources>
     pub is_black_boxed: bool,
 
-    pub content: Option<String>,
+    pub content: RefCell<Option<String>>,
     pub content_type: Option<String>,
 
     // TODO: use it in #37667, then remove this allow
@@ -127,7 +127,7 @@ impl SourceActor {
         SourceActor {
             name,
             url,
-            content,
+            content: RefCell::new(content),
             content_type,
             is_black_boxed: false,
             spidermonkey_id,
@@ -201,6 +201,7 @@ impl Actor for SourceActor {
                     // become available later (e.g. after a fetch)?
                     source: self
                         .content
+                        .borrow()
                         .as_deref()
                         .unwrap_or("<!-- not available; please reload! -->")
                         .to_owned(),

components/devtools/actors/tab.rs
@@ -139,8 +139,8 @@ impl TabDescriptorActor {
         is_top_level_global: bool,
     ) -> TabDescriptorActor {
         let name = actors.new_name::<Self>();
-        let root = actors.find_mut::<RootActor>("root");
-        root.tabs.push(name.clone());
+        let root = actors.find::<RootActor>("root");
+        root.tabs.borrow_mut().push(name.clone());
         TabDescriptorActor {
             name,
             browsing_context_actor,

components/devtools/actors/timeline.rs
@@ -336,7 +336,7 @@ impl Emitter {
         if let Some(ref actor_name) = self.framerate_actor {
             let mut lock = self.registry.lock();
             let registry = lock.as_mut().unwrap();
-            let framerate_actor = registry.find_mut::<FramerateActor>(actor_name);
+            let framerate_actor = registry.find::<FramerateActor>(actor_name);
             let framerate_reply = FramerateEmitterReply {
                 type_: "framerate".to_owned(),
                 from: framerate_actor.name(),

components/devtools/actors/watcher.rs
@@ -250,7 +250,7 @@ impl Actor for WatcherActor {
 
                     target.frame_update(&mut request);
                 } else if target_type == "worker" {
-                    for worker_name in &root.workers {
+                    for worker_name in &*root.workers.borrow() {
                         let worker_msg = WatchTargetsReply {
                             from: self.name(),
                             type_: "target-available-form".into(),
@@ -317,7 +317,7 @@ impl Actor for WatcherActor {
                                 &mut request,
                             );
 
-                            for worker_name in &root.workers {
+                            for worker_name in &*root.workers.borrow() {
                                 let worker = registry.find::<WorkerActor>(worker_name);
                                 let thread = registry.find::<ThreadActor>(&worker.thread);
 

commit 0112ffa812cab93899e4eb2df6aae6ecac2d97e4

devtools: Register workers in workers list, not tabs (#41929)
Devtools tests are failing on `./mach test-devtools`. Regression from
#41744

Testing: Manually tested

Signed-off-by: atbrakhi <atbrakhi@igalia.com>

components/devtools/lib.rs
@@ -356,7 +356,7 @@ impl DevtoolsInstance {
                 streams: Default::default(),
             };
             let root = actors.find::<RootActor>("root");
-            root.tabs.borrow_mut().push(worker.name.clone());
+            root.workers.borrow_mut().push(worker.name.clone());
 
             self.actor_workers.insert(id, worker_name.clone());
             actors.register(worker);