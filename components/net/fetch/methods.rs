5a84a644ee22e6654ea3863cec425e85e5e218ea
Narfinger <Narfinger@users.noreply.github.com>
2026-01-22 10:17:40 +0100

    Implement LazyGenericCallback
    Signed-off-by: Narfinger <Narfinger@users.noreply.github.com>

--- components/net/fetch/methods.rs
+++ components/net/fetch/methods.rs
@@ -6,6 +6,7 @@ use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::{io, mem, str};
 
+use base::generic_channel::CallbackSetter;
 use base::id::PipelineId;
 use base64::Engine as _;
 use base64::engine::general_purpose;
@@ -16,7 +17,7 @@ use embedder_traits::resources::{self, Resource};
 use headers::{AccessControlExposeHeaders, ContentType, HeaderMapExt};
 use http::header::{self, HeaderMap, HeaderName, RANGE};
 use http::{HeaderValue, Method, StatusCode};
-use ipc_channel::ipc::{self, IpcReceiver, IpcSender};
+use ipc_channel::ipc::{self, IpcSender};
 use log::{debug, trace, warn};
 use malloc_size_of_derive::MallocSizeOf;
 use mime::{self, Mime};
@@ -70,13 +71,13 @@ pub enum Data {
 
 pub struct WebSocketChannel {
     pub sender: IpcSender<WebSocketNetworkEvent>,
-    pub receiver: Option<IpcReceiver<WebSocketDomAction>>,
+    pub receiver: Option<CallbackSetter<WebSocketDomAction>>,
 }
 
 impl WebSocketChannel {
     pub fn new(
         sender: IpcSender<WebSocketNetworkEvent>,
-        receiver: Option<IpcReceiver<WebSocketDomAction>>,
+        receiver: Option<CallbackSetter<WebSocketDomAction>>,
     ) -> Self {
         Self { sender, receiver }
     }
--- components/script/dom/websocket.rs
+++ components/script/dom/websocket.rs
@@ -6,9 +6,9 @@ use std::borrow::ToOwned;
 use std::cell::Cell;
 use std::ptr::{self, NonNull};
 
+use base::generic_channel::{LazyCallback, lazy_callback};
 use constellation_traits::BlobImpl;
 use dom_struct::dom_struct;
-use ipc_channel::ipc::{self, IpcReceiver, IpcSender};
 use ipc_channel::router::ROUTER;
 use js::jsapi::JSObject;
 use js::jsval::UndefinedValue;
@@ -109,20 +109,20 @@ pub(crate) struct WebSocket {
     clearing_buffer: Cell<bool>, // Flag to tell if there is a running thread to clear buffered_amount
     #[ignore_malloc_size_of = "Defined in std"]
     #[no_trace]
-    sender: IpcSender<WebSocketDomAction>,
+    callback: LazyCallback<WebSocketDomAction>,
     binary_type: Cell<BinaryType>,
     protocol: DomRefCell<String>, // Subprotocol selected by server
 }
 
 impl WebSocket {
-    fn new_inherited(url: ServoUrl, sender: IpcSender<WebSocketDomAction>) -> WebSocket {
+    fn new_inherited(url: ServoUrl, callback: LazyCallback<WebSocketDomAction>) -> WebSocket {
         WebSocket {
             eventtarget: EventTarget::new_inherited(),
             url,
             ready_state: Cell::new(WebSocketRequestState::Connecting),
             buffered_amount: Cell::new(0),
             clearing_buffer: Cell::new(false),
-            sender,
+            callback,
             binary_type: Cell::new(BinaryType::Blob),
             protocol: DomRefCell::new("".to_owned()),
         }
@@ -132,7 +132,7 @@ impl WebSocket {
         global: &GlobalScope,
         proto: Option<HandleObject>,
         url: ServoUrl,
-        sender: IpcSender<WebSocketDomAction>,
+        sender: LazyCallback<WebSocketDomAction>,
         can_gc: CanGc,
     ) -> DomRoot<WebSocket> {
         let websocket = reflect_dom_object_with_proto(
@@ -262,14 +262,9 @@ impl WebSocketMethods<crate::DomTypeHolder> for WebSocket {
         }
 
         // Create the interface for communication with the resource thread
-        let (dom_action_sender, resource_action_receiver): (
-            IpcSender<WebSocketDomAction>,
-            IpcReceiver<WebSocketDomAction>,
-        ) = ipc::channel().unwrap();
-        let (resource_event_sender, dom_event_receiver): (
-            IpcSender<WebSocketNetworkEvent>,
-            ProfiledIpc::IpcReceiver<WebSocketNetworkEvent>,
-        ) = ProfiledIpc::channel(global.time_profiler_chan().clone()).unwrap();
+        let (dom_action_sender, resource_action_receiver) = lazy_callback();
+        let (resource_event_sender, dom_event_receiver) =
+            ProfiledIpc::channel(global.time_profiler_chan().clone()).unwrap();
 
         // Step 12. Establish a WebSocket connection given urlRecord, protocols, and client.
         let ws = WebSocket::new(global, proto, url_record.clone(), dom_action_sender, can_gc);
@@ -389,7 +384,7 @@ impl WebSocketMethods<crate::DomTypeHolder> for WebSocket {
 
         if send_data {
             let _ = self
-                .sender
+                .callback
                 .send(WebSocketDomAction::SendMessage(MessageData::Text(data.0)));
         }
 
@@ -408,7 +403,7 @@ impl WebSocketMethods<crate::DomTypeHolder> for WebSocket {
         if send_data {
             let bytes = blob.get_bytes().unwrap_or_default();
             let _ = self
-                .sender
+                .callback
                 .send(WebSocketDomAction::SendMessage(MessageData::Binary(bytes)));
         }
 
@@ -423,7 +418,7 @@ impl WebSocketMethods<crate::DomTypeHolder> for WebSocket {
 
         if send_data {
             let _ = self
-                .sender
+                .callback
                 .send(WebSocketDomAction::SendMessage(MessageData::Binary(bytes)));
         }
         Ok(())
@@ -437,7 +432,7 @@ impl WebSocketMethods<crate::DomTypeHolder> for WebSocket {
 
         if send_data {
             let _ = self
-                .sender
+                .callback
                 .send(WebSocketDomAction::SendMessage(MessageData::Binary(bytes)));
         }
         Ok(())
@@ -481,7 +476,7 @@ impl WebSocketMethods<crate::DomTypeHolder> for WebSocket {
                 // Kick off _Start the WebSocket Closing Handshake_
                 // https://tools.ietf.org/html/rfc6455#section-7.1.2
                 let reason = reason.map(|reason| reason.0);
-                let _ = self.sender.send(WebSocketDomAction::Close(code, reason));
+                let _ = self.callback.send(WebSocketDomAction::Close(code, reason));
             },
         }
         Ok(()) // Return Ok
