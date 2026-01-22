793dfc5463aa93df7ff841388ded80430cb776f4
Richard Tjokroutomo <richard.tjokro2@gmail.com>
2026-01-22 19:29:45 +0700

    Revert "layout: Suppress linebreak when word-break: keep-all"
    This reverts commit 54ec40a0f67b1712ad6a5a29262592a6d682da0a.
    
    Signed-off-by: Richard Tjokroutomo <richard.tjokro2@gmail.com>

--- components/layout/flow/inline/line_breaker.rs
+++ components/layout/flow/inline/line_breaker.rs
@@ -5,8 +5,6 @@
 use std::ops::Range;
 
 use icu_segmenter::LineSegmenter;
-use unicode_ident::{is_xid_start, is_xid_continue};
-use style::computed_values::word_break::T as WordBreak;
 
 pub(crate) struct LineBreaker {
     linebreaks: Vec<usize>,
@@ -14,31 +12,8 @@ pub(crate) struct LineBreaker {
 }
 
 impl LineBreaker {
-    pub(crate) fn new(string: &str, word_break: WordBreak) -> Self {
+    pub(crate) fn new(string: &str) -> Self {
         let line_segmenter = LineSegmenter::new_auto();
-        let linebreak_candidates: Vec<usize> = line_segmenter.segment_str(string).skip(1).collect();
-        let mut final_linebreaks = Vec::new();
-        
-        match word_break {
-            WordBreak::normal => {final_linebreaks = linebreak_candidates},
-            WordBreak::KeepAll => {
-                let mut idx = 0;
-                for linebreak_candidate in &linebreak_candidates {
-                    if idx < linebreak_candidates.len() - 1 {
-                        if !LineBreaker::suppress_linebreak(string[..*linebreak_candidate].chars().prev().next().expect("Char must exist since linebreak_candidates skipped first index."), string[*linebreak_candidate..].chars().next().expect("char must exist since we stop at the second last linebreak points")){
-                            final_linebreaks.push(*linebreak_candidate);
-                        }
-                    }
-                    idx += 1;
-
-                    if linebreak_candidates.len() > 0 {
-                        final_linebreaks.push(*linebreak_candidates[linebreak_candidates.len() - 1]);
-                    }
-                }
-            },
-            _ => {final_linebreaks = linebreak_candidates},
-        }
-
         Self {
             // From https://docs.rs/icu_segmenter/1.5.0/icu_segmenter/struct.LineSegmenter.html
             // > For consistency with the grapheme, word, and sentence segmenters, there is always a
@@ -77,9 +52,6 @@ impl LineBreaker {
         linebreaks_range.end = ending_linebreak_index;
         linebreaks_range
     }
-    fn suppress_linebreak(predecessor: char, successor: char){
-        (predecessor.is_alphanumeric() || is_xid_start(predecessor) || is_xid_continue(predecessor)) && (successor.is_alphanumeric() || is_xid_start(successor) || is_xid_continue(successor))
-    }
 }
 
 #[cfg(test)]
@@ -88,7 +60,7 @@ mod test {
 
     #[test]
     fn test_linebreaker_ranges() {
-        let linebreaker = LineBreaker::new("abc def", WordBreak::normal);
+        let linebreaker = LineBreaker::new("abc def");
         assert_eq!(linebreaker.linebreaks, [4, 7]);
         assert_eq!(
             linebreaker.linebreaks_in_range_after_current_offset(0..5),
@@ -100,7 +72,7 @@ mod test {
             0..1
         );
 
-        let linebreaker = LineBreaker::new("abc d def", WordBreak::normal);
+        let linebreaker = LineBreaker::new("abc d def");
         assert_eq!(linebreaker.linebreaks, [4, 6, 9]);
         assert_eq!(
             linebreaker.linebreaks_in_range_after_current_offset(0..5),
@@ -121,7 +93,7 @@ mod test {
         );
 
         std::panic::catch_unwind(|| {
-            let linebreaker = LineBreaker::new("abc def", WordBreak::normal);
+            let linebreaker = LineBreaker::new("abc def");
             linebreaker.linebreaks_in_range_after_current_offset(5..2);
         })
         .expect_err("Reversed range should cause an assertion failure.");
@@ -129,7 +101,7 @@ mod test {
 
     #[test]
     fn test_linebreaker_stateful_advance() {
-        let mut linebreaker = LineBreaker::new("abc d def", WordBreak::normal);
+        let mut linebreaker = LineBreaker::new("abc d def");
         assert_eq!(linebreaker.linebreaks, [4, 6, 9]);
         assert!(linebreaker.advance_to_linebreaks_in_range(0..7) == &[4, 6]);
         assert!(linebreaker.advance_to_linebreaks_in_range(8..9).is_empty());
@@ -145,7 +117,7 @@ mod test {
         linebreaker.current_offset = 0;
 
         std::panic::catch_unwind(|| {
-            let mut linebreaker = LineBreaker::new("abc d def", WordBreak::normal);
+            let mut linebreaker = LineBreaker::new("abc d def");
             linebreaker.advance_to_linebreaks_in_range(2..0);
         })
         .expect_err("Reversed range should cause an assertion failure.");
