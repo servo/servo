9041195dd38335be89b78f13a7e78c201f7b7e0f
Jonathan Schwender <schwenderjonathan@gmail.com>
2026-01-23 15:40:51 +0100

    Add tests for shell_tidy improvement.
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>

--- python/tidy/tests/shell_tidy.sh
+++ python/tidy/tests/shell_tidy.sh
@@ -12,3 +12,9 @@ if [ -z "${some_var}" ]; then
   echo "should have used [["
 fi
 [ -z "${another_var}" ]
+# Using $@, $1 etc. shouldn't trigger.
+echo "$@"
+if [[ -n "$1" ]]; then
+  echo "parameter 1 is $1"
+fi
+echo "item1 item2 item3" | awk '{$1=$1;print}'
