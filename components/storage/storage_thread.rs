0020ff962fe26b57bc788f3e71f424d8ffb5477b
Ashwin Naren <arihant2math@gmail.com>
2026-01-25 14:18:41 -0800

    Implement client storage registry in coordination thread
    Signed-off-by: Ashwin Naren <arihant2math@gmail.com>

--- components/storage/storage_thread.rs
+++ components/storage/storage_thread.rs
@@ -7,23 +7,27 @@ use std::path::PathBuf;
 use base::generic_channel::GenericSender;
 use profile_traits::mem::ProfilerChan as MemProfilerChan;
 use storage_traits::StorageThreads;
-use storage_traits::client_storage::ClientStorageThreadMessage;
 use storage_traits::indexeddb::IndexedDBThreadMsg;
 use storage_traits::webstorage_thread::WebStorageThreadMsg;
 
+use crate::client_storage::ClientStorageThreadHandle;
 use crate::{ClientStorageThreadFactory, IndexedDBThreadFactory, WebStorageThreadFactory};
 
 pub fn new_storage_threads(
     mem_profiler_chan: MemProfilerChan,
     config_dir: Option<PathBuf>,
 ) -> (StorageThreads, StorageThreads) {
-    let client_storage: GenericSender<ClientStorageThreadMessage> =
+    let client_storage: ClientStorageThreadHandle =
         ClientStorageThreadFactory::new(config_dir.clone());
     let idb: GenericSender<IndexedDBThreadMsg> = IndexedDBThreadFactory::new(config_dir.clone());
     let web_storage: GenericSender<WebStorageThreadMsg> =
         WebStorageThreadFactory::new(config_dir, mem_profiler_chan);
     (
-        StorageThreads::new(client_storage.clone(), idb.clone(), web_storage.clone()),
-        StorageThreads::new(client_storage, idb, web_storage),
+        StorageThreads::new(
+            client_storage.clone().into(),
+            idb.clone(),
+            web_storage.clone(),
+        ),
+        StorageThreads::new(client_storage.into(), idb, web_storage),
     )
 }
