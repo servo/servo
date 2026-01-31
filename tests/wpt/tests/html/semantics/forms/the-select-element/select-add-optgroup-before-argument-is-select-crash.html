servo/servo

commit d7354191d2b53d818b991ffa91c6790457cba8a3

dom: avoid short lived rooting in Node::is_ancestor_of (#40776)
When using the iterator nodes are rooted but they are dropped
immediately.

Testing: Green wpt run:
https://github.com/webbeef/servo/actions/runs/19560383039/job/56012660243

Signed-off-by: webbeef <me@webbeef.org>

components/script/dom/node.rs
@@ -915,7 +915,19 @@ impl Node {
     }
 
     pub(crate) fn is_ancestor_of(&self, child: &Node) -> bool {
-        child.ancestors().any(|ancestor| &*ancestor == self)
+        let mut current = &MutNullableDom::new(Some(child));
+        let mut done = false;
+
+        while let Some(node) = current.if_is_some(|node| {
+            done = node == self;
+            &node.parent_node
+        }) {
+            if done {
+                break;
+            }
+            current = node
+        }
+        done
     }
 
     pub(crate) fn is_shadow_including_inclusive_ancestor_of(&self, node: &Node) -> bool {

commit d6d320549356e3a9200dc28051946b0294b813d3

script: Do not consider a node an ancestor of itself in `Node::is_ancestor_of`
This change fixes a crash in options collections due to the fact that a
`<select>` element was being considered an ancestor of itself. This
regression likely introduced in #40776.

This change also fills in the specification text for
`HTMLOptionCollect::Add`.

Signed-off-by: Martin Robinson <mrobinson@igalia.com>

components/script/dom/html/htmloptionscollection.rs
@@ -192,25 +192,31 @@ impl HTMLOptionsCollectionMethods<crate::DomTypeHolder> for HTMLOptionsCollectio
             },
         };
 
-        // Step 1
+        // Step 1: If element is an ancestor of the select element on which the
+        // HTMLOptionsCollection is rooted, then throw a "HierarchyRequestError"
+        // DOMException.
         if node.is_ancestor_of(&root) {
             return Err(Error::HierarchyRequest(None));
         }
 
         if let Some(HTMLElementOrLong::HTMLElement(ref before_element)) = before {
-            // Step 2
+            // Step 2: If before is an element, but that element isn't a descendant
+            // of the select element on which the HTMLOptionsCollection is rooted,
+            // then throw a "NotFoundError" DOMException.
             let before_node = before_element.upcast::<Node>();
             if !root.is_ancestor_of(before_node) {
                 return Err(Error::NotFound(None));
             }
 
-            // Step 3
+            // Step 3: If element and before are the same element, then return.
             if node == before_node {
                 return Ok(());
             }
         }
 
-        // Step 4
+        // Step 4: If before is a node, then let reference be that node. Otherwise,
+        // if before is an integer, and there is a beforeth node in the collection,
+        // let reference be that node. Otherwise, let reference be null.
         let reference_node = before.and_then(|before| match before {
             HTMLElementOrLong::HTMLElement(element) => Some(DomRoot::upcast::<Node>(element)),
             HTMLElementOrLong::Long(index) => self
@@ -219,14 +225,16 @@ impl HTMLOptionsCollectionMethods<crate::DomTypeHolder> for HTMLOptionsCollectio
                 .map(DomRoot::upcast::<Node>),
         });
 
-        // Step 5
+        // Step 5: If reference is not null, let parent be the parent node of
+        // reference. Otherwise, let parent be the select element on which the
+        // HTMLOptionsCollection is rooted.
         let parent = if let Some(ref reference_node) = reference_node {
             reference_node.GetParentNode().unwrap()
         } else {
             root
         };
 
-        // Step 6
+        // Step 6: Pre-insert element into parent node before reference.
         Node::pre_insert(node, &parent, reference_node.as_deref(), CanGc::note()).map(|_| ())
     }
 

components/script/dom/node.rs
@@ -919,8 +919,8 @@ impl Node {
         self == child || self.is_ancestor_of(child)
     }
 
-    pub(crate) fn is_ancestor_of(&self, child: &Node) -> bool {
-        let mut current = &MutNullableDom::new(Some(child));
+    pub(crate) fn is_ancestor_of(&self, possible_descendant: &Node) -> bool {
+        let mut current = &possible_descendant.parent_node;
         let mut done = false;
 
         while let Some(node) = current.if_is_some(|node| {

tests/wpt/tests/html/semantics/forms/the-select-element/select-add-optgroup-before-argument-is-select-crash.html
@@ -1,0 +1,7 @@
+<!DOCTYPE html>
+<link rel=help href="https://github.com/servo/servo/issues/41080">
+<script>
+  const select = document.createElement("select");
+  const optgroup = document.createElement("optgroup");
+  select.options.add(optgroup, select);
+</script>