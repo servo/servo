servo/servo

commit 7358d73f7f4e0a932e8a169705fdb2311db7cd12

script: Do not track outdated oustanding loads for stylesheets. (#42208)
The `HTMLLinkElement::pending_loads` field should only be used to track
outstanding loads for the current generation; any loads from previous
generations can be ignored. This avoids confusion about when to dispatch
load/error events. See
https://github.com/servo/servo/issues/42187#issuecomment-3808888585 for
more details about how the timing of multiple loads for the same
`<link>` element resulted in intermittent timeouts in one WPT test.

Testing: Intermittent failure did not appear within 75 runs; previously
reproduced within 15.
Fixes: #42187

Signed-off-by: Josh Matthews <josh@joshmatthews.net>

components/script/stylesheet_loader.rs
@@ -379,13 +379,6 @@ impl FetchResponseListener for StylesheetContext {
             if !self.contributes_to_the_styling_processing_model(&element) {
                 // Step 2.1. Remove el from el's node document's script-blocking style sheet set.
                 self.decrement_load_and_render_blockers(&document);
-                let owner = element
-                    .upcast::<Element>()
-                    .as_stylesheet_owner()
-                    .expect("Stylesheet not loaded by <style> or <link> element!");
-                // Always consider ignored loads as successful, as they shouldn't cause any subsequent
-                // successful loads to fire an "error" event.
-                owner.load_finished(true);
                 document.finish_load(LoadType::Stylesheet(self.url), CanGc::note());
                 // Step 2.2. Return.
                 return;

components/script/dom/html/htmllinkelement.rs
@@ -629,6 +629,7 @@ impl HTMLLinkElement {
 
         self.request_generation_id
             .set(self.request_generation_id.get().increment());
+        self.pending_loads.set(0);
 
         let loader = ElementStylesheetLoader::new(self.upcast());
         loader.load(

commit cc58570fc2f0d2b8d01e8508c6df8d6a02bbf521

script: Fix crash when failed requests finish from a previous generation
Most likely a regression introduced in #42208 where the pending
loads counter has been reset. For successful request this wasn't
an issue, but for failed requests it was. Therefore, guard against
these generation request problems by bailing out as soon as we
post-process the request (which we do for all failed requests).

Fixes #42477

Signed-off-by: Tim van der Lippe <tvanderlippe@gmail.com>

components/script/stylesheet_loader.rs
@@ -226,7 +226,7 @@ impl StylesheetContext {
         // This might happen when we time out a resource, but that happens in `fetch` instead
     }
 
-    fn decrement_load_and_render_blockers(&self, document: &Document) {
+    fn decrement_blockers_and_finish_load(self, document: &Document) {
         if self.is_script_blocking {
             document.decrement_script_blocking_stylesheet_count();
         }
@@ -234,6 +234,8 @@ impl StylesheetContext {
         if self.is_render_blocking {
             document.decrement_render_blocking_element_count();
         }
+
+        document.finish_load(LoadType::Stylesheet(self.url), CanGc::note());
     }
 
     fn do_post_parse_tasks(self, success: bool, stylesheet: Arc<Stylesheet>) {
@@ -250,6 +252,16 @@ impl StylesheetContext {
                 let link = element
                     .downcast::<HTMLLinkElement>()
                     .expect("Should be HTMLinkElement due to StylesheetContextSource");
+                // For failed requests, we should bail out if it is from a previous generation.
+                // Since we can reissue another failed request, which resets the pending load counter
+                // in a link element.
+                if self
+                    .request_generation_id
+                    .is_some_and(|generation| generation != link.get_request_generation_id())
+                {
+                    self.decrement_blockers_and_finish_load(&document);
+                    return;
+                }
                 // https://html.spec.whatwg.org/multipage/#link-type-stylesheet
                 // > When the disabled attribute of a link element with a stylesheet keyword is set,
                 // > disable the associated CSS style sheet.
@@ -307,8 +319,7 @@ impl StylesheetContext {
 
         // Step 6. If el contributes a script-blocking style sheet, then:
         // Step 7. Unblock rendering on el.
-        self.decrement_load_and_render_blockers(&document);
-        document.finish_load(LoadType::Stylesheet(self.url), CanGc::note());
+        self.decrement_blockers_and_finish_load(&document);
     }
 }
 
@@ -378,8 +389,7 @@ impl FetchResponseListener for StylesheetContext {
             // or if, since the resource in question was fetched, it has become appropriate to fetch it again, then:
             if !self.contributes_to_the_styling_processing_model(&element) {
                 // Step 2.1. Remove el from el's node document's script-blocking style sheet set.
-                self.decrement_load_and_render_blockers(&document);
-                document.finish_load(LoadType::Stylesheet(self.url), CanGc::note());
+                self.decrement_blockers_and_finish_load(&document);
                 // Step 2.2. Return.
                 return;
             }

tests/wpt/tests/html/semantics/document-metadata/the-link-element/link-href-attribute-crash.html
@@ -1,0 +1,7 @@
+<!DOCTYPE html>
+<link rel=help href="https://github.com/servo/servo/issues/42477">
+<link id="link" href="does-not-exist-A.css">
+<script>
+  link.rel = "stylesheet";
+  link.href = "does-not-exist-B.css";
+</script>