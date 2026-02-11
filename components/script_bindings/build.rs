e9b0f4e9582efba6a5dafa414bdaa6d7bc115b14
Euclid Ye <yezhizhenjiakang@gmail.com>
2026-02-11 16:21:37 +0800

    .
    Signed-off-by: Euclid Ye <yezhizhenjiakang@gmail.com>

--- components/script_bindings/build.rs
+++ components/script_bindings/build.rs
@@ -5,7 +5,7 @@
 use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
-use std::process::Command;
+use std::process::{Command, Stdio};
 use std::time::Instant;
 use std::{env, fmt};
 
@@ -84,6 +84,7 @@ fn try_python_command(program: &str, args: &[&str]) -> Result<(), String> {
     let mut command = Command::new(program);
     command.args(args);
     command.arg("--version");
+    command.stdin(Stdio::null());
 
     let command_result = command.output();
 
