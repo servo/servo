5e383fce67b0b0f2d1a5e1e710d06bfe8a2118d7
Shubham Gupta <shubham.gupta@chromium.org>
2026-02-12 17:56:51 +0800

    paint: Get rid of unnecessary trait from LCPCandidateID
    Signed-off-by: Shubham Gupta <shubham.gupta@chromium.org>

--- components/shared/paint/largest_contentful_paint_candidate.rs
+++ components/shared/paint/largest_contentful_paint_candidate.rs
@@ -22,7 +22,7 @@ impl LCPCandidate {
     }
 }
 
-#[derive(Clone, Copy, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
+#[derive(Clone, Copy, Debug, Deserialize, PartialEq, PartialOrd, Serialize)]
 pub struct LCPCandidateID(pub usize);
 
 #[derive(Clone, Copy, Debug)]
