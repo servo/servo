e8564f9c5e9d75b923e19bbe0ae9619ebf3be6f2
Narfinger <Narfinger@users.noreply.github.com>
2026-02-16 11:10:00 +0100

    use Cargo.lock file
    Signed-off-by: Narfinger <Narfinger@users.noreply.github.com>

--- etc/vendor_servo.py
+++ etc/vendor_servo.py
@@ -55,7 +55,7 @@ def vendor():
         # vendoring crates
         print("Vendoring Crates")
         vendor_process = subprocess.run(
-            ["cargo", "vendor", "vendor/"], capture_output=True, encoding="utf-8", check=True
+            ["cargo", "vendor", "--locked", "vendor/"], capture_output=True, encoding="utf-8", check=True
         )
         out = vendor_process.stdout
         print("Modifying .cargo/config.toml")
