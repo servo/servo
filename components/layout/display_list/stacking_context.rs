2cd00bf16a8d39281322440b5e20b73b37916ee1
Simon Wülker <simon.wuelker@arcor.de>
2026-02-22 20:56:17 +0100

    layout: Create webrender stacking contexts for fragments with "transform-style: preserve-3d"
    Signed-off-by: Simon Wülker <simon.wuelker@arcor.de>

--- components/layout/display_list/stacking_context.rs
+++ components/layout/display_list/stacking_context.rs
@@ -31,6 +31,7 @@ use style::values::computed::basic_shape::ClipPath;
 use style::values::computed::{ClipRectOrAuto, Length, TextDecorationLine};
 use style::values::generics::box_::{OverflowClipMarginBox, Perspective};
 use style::values::generics::transform::{self, GenericRotate, GenericScale, GenericTranslate};
+use style::values::specified::TransformStyle;
 use style::values::specified::box_::DisplayOutside;
 use style_traits::CSSPixel;
 use webrender_api::units::{LayoutPoint, LayoutRect, LayoutTransform, LayoutVector2D};
@@ -597,7 +598,8 @@ impl StackingContext {
             effects.opacity == 1.0 &&
             effects.mix_blend_mode == ComputedMixBlendMode::Normal &&
             !style.has_effective_transform_or_perspective(FragmentFlags::empty()) &&
-            style.clone_clip_path() == ClipPath::None
+            style.clone_clip_path() == ClipPath::None &&
+            style.get_used_transform_style() == TransformStyle::Flat
         {
             return false;
         }
