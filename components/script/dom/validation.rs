87314d6f2745096b754a4ae6208a915d82187a71
Narfinger <Narfinger@users.noreply.github.com>
2026-02-25 09:40:55 +0100

    format fix
    Signed-off-by: Narfinger <Narfinger@users.noreply.github.com>

--- components/script/dom/validation.rs
+++ components/script/dom/validation.rs
@@ -1,8 +1,8 @@
-use js::context::JSContext;
-
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+use js::context::JSContext;
+
 use crate::dom::bindings::codegen::Bindings::HTMLElementBinding::HTMLElementMethods;
 use crate::dom::bindings::codegen::Bindings::HTMLOrSVGElementBinding::FocusOptions;
 use crate::dom::bindings::inheritance::Castable;
