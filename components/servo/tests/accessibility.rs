f90ef139ee2d23f27e9521e8e2b29a2cb7309779
delan azabani <dazabani@igalia.com>
2026-02-26 19:08:54 +0800

    Add test for bfcache navigation
    Signed-off-by: delan azabani <dazabani@igalia.com>

--- components/servo/tests/accessibility.rs
+++ components/servo/tests/accessibility.rs
@@ -127,6 +127,80 @@ fn test_navigate_creates_new_accessibility_update() {
     assert_eq!(text_node.value().as_deref(), Some("page 2"));
 }
 
+// FIXME(accessibility): when clicking back and forward, we currently rely on
+// layout and the accessibility tree being rebuilt from scratch, so that the full
+// a11y tree can be resent.
+// But if bfcache navigations stop redoing layout, or we implement incremental
+// a11y tree building, this test will break.
+#[test]
+fn test_accessibility_after_navigate_and_back() {
+    let servo_test = ServoTest::new_with_builder(|builder| {
+        let mut preferences = Preferences::default();
+        preferences.accessibility_enabled = true;
+        builder.preferences(preferences)
+    });
+
+    let page_1_url = Url::parse("data:text/html,<!DOCTYPE html> page 1").unwrap();
+    let page_2_url = Url::parse("data:text/html,<!DOCTYPE html> page 2").unwrap();
+
+    let delegate = Rc::new(WebViewDelegateImpl::default());
+    let webview = WebViewBuilder::new(servo_test.servo(), servo_test.rendering_context.clone())
+        .delegate(delegate.clone())
+        .url(page_1_url.clone())
+        .build();
+    webview.set_accessibility_active(true);
+
+    let load_webview = webview.clone();
+    servo_test.spin(move || load_webview.load_status() != LoadStatus::Complete);
+
+    let updates = wait_for_min_updates(&servo_test, delegate.clone(), 2);
+    let mut tree = build_tree(updates);
+
+    let root_web_area = assert_tree_structure_and_get_root_web_area(&tree);
+
+    let result = find_all_matching_nodes(root_web_area, |node| {
+        node.role() == accesskit::Role::TextRun
+    });
+    assert_eq!(result.len(), 1);
+    let text_node = result[0];
+
+    assert_eq!(text_node.value().as_deref(), Some("page 1"));
+
+    let load_webview = webview.clone();
+    webview.load(page_2_url.clone());
+    servo_test.spin(move || load_webview.url() != Some(page_2_url.clone()));
+
+    let new_updates = wait_for_min_updates(&servo_test, delegate.clone(), 2);
+    for tree_update in new_updates {
+        tree.update_and_process_changes(tree_update, &mut NoOpChangeHandler);
+    }
+
+    let root_node = tree.state().root();
+    let result = find_all_matching_nodes(root_node, |node| node.role() == accesskit::Role::TextRun);
+    assert_eq!(result.len(), 1);
+    let text_node = result[0];
+
+    assert_eq!(text_node.value().as_deref(), Some("page 2"));
+
+    let back_webview = webview.clone();
+    webview.go_back(1);
+    servo_test.spin(move || back_webview.url() != Some(page_1_url.clone()));
+
+    let new_updates = wait_for_min_updates(&servo_test, delegate.clone(), 2);
+    for tree_update in new_updates {
+        tree.update_and_process_changes(tree_update, &mut NoOpChangeHandler);
+    }
+
+    let root_node = tree.state().root();
+    let result = find_all_matching_nodes(root_node, |node| node.role() == accesskit::Role::TextRun);
+    assert_eq!(result.len(), 1);
+    let text_node = result[0];
+
+    assert_eq!(text_node.value().as_deref(), Some("page 1"));
+}
+
+// TODO(accessibility): write test for resend a11y tree when clicking back or forward
+
 fn wait_for_min_updates(
     servo_test: &ServoTest,
     delegate: Rc<WebViewDelegateImpl>,
@@ -206,11 +280,26 @@ fn find_first_matching_node(
     mut pred: impl FnMut(&accesskit_consumer::Node) -> bool,
 ) -> Option<accesskit_consumer::Node<'_>> {
     let mut children = root_node.children().collect::<VecDeque<_>>();
-    let mut result: Option<accesskit_consumer::Node> = None;
     while let Some(candidate) = children.pop_front() {
         if pred(&candidate) {
-            result = Some(candidate);
-            break;
+            return Some(candidate);
+        }
+        for child in candidate.children() {
+            children.push_back(child);
+        }
+    }
+    None
+}
+
+fn find_all_matching_nodes(
+    root_node: accesskit_consumer::Node<'_>,
+    mut pred: impl FnMut(&accesskit_consumer::Node) -> bool,
+) -> Vec<accesskit_consumer::Node<'_>> {
+    let mut children = root_node.children().collect::<VecDeque<_>>();
+    let mut result = vec![];
+    while let Some(candidate) = children.pop_front() {
+        if pred(&candidate) {
+            result.push(candidate);
         }
         for child in candidate.children() {
             children.push_back(child);
