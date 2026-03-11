a8142546ca85be7a182583926fb0954d9cc92e0d
Oriol Brufau <obrufau@igalia.com>
2026-03-11 14:21:19 +0100

    Improve `list-style-type` and counter styles (#43111)
    Bumps Stylo to https://github.com/servo/stylo/pull/329, and modifies the
    code accordingly:
    - Implements basic `counter()` and `counters()`, instead of treating
    them as empty string.
    - For `list-style-type`, uses normal space instead of non-breaking space
    in the suffix, since `::marker` has `white-space: pre` anyways.
    `counter()` and `counters()` don't get a suffix.
    - Changes `disclosure-closed` to use ▸ (U+25B8 Black Right-Pointing
    Small Triangle) instead of ‣ (U+2023 Triangular Bullet).
    - Implements `list-style-type: <string>`.
    - Implements basic `<counter-style-name> | <symbols()>` for both
    counters and `list-style-type`.
    
    However, since `@counter-style` and `counter-*` properties are still
    unsupported, non-predefined keywords just default to decimal, and
    counters are assumed to have a value of 0.
    
    The UA stylesheet is tweaked to avoid a problem when `list-style-type`
    is set to a string with multiple lines.
    
    Testing: 63 tests improve, 17 tests worsen (they were falsely passing
    beforehand)
    - #43117
    -
    `tests/wpt/tests/css/css-lists/content-property/marker-text-matches-circle.html`
    -
    `tests/wpt/tests/css/css-lists/content-property/marker-text-matches-disc.html`
    -
    `tests/wpt/tests/css/css-lists/content-property/marker-text-matches-square.html`
    - #43118
      - `tests/wpt/tests/css/css-lists/nested-marker-dynamic.html`
      - `tests/wpt/tests/css/css-lists/nested-marker.html`
    - #43036
    -
    `tests/wpt/tests/css/css-lists/list-style-type-decimal-line-height.html`
      - `tests/wpt/tests/css/css-pseudo/marker-content-002.html`
      - `tests/wpt/tests/css/css-pseudo/marker-content-003.html`
      - `tests/wpt/tests/css/css-pseudo/marker-content-003b.html`
      - `tests/wpt/tests/css/css-pseudo/marker-content-004.html`
      - `tests/wpt/tests/css/css-pseudo/marker-content-005.html`
      - `tests/wpt/tests/css/css-pseudo/marker-content-015.html`
      - `tests/wpt/tests/css/css-pseudo/marker-content-016.html`
      - `tests/wpt/tests/css/css-pseudo/marker-content-018.html`
    - #30365
    -
    `tests/wpt/tests/css/css-pseudo/marker-font-variant-numeric-default.html`
      - `tests/wpt/tests/css/css-pseudo/marker-line-height.html`
    - #43119
      - `tests/wpt/tests/css/css-pseudo/marker-tab-size.html`
    
    Fixes: #43037
    
    Signed-off-by: Oriol Brufau <obrufau@igalia.com>

--- components/layout/lists.rs
+++ components/layout/lists.rs
@@ -2,9 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+use style::counter_style::{CounterStyle, Symbol, SymbolsType};
 use style::properties::longhands::list_style_type::computed_value::T as ListStyleType;
-use style::properties::style_structs;
 use style::values::computed::Image;
+use stylo_atoms::atom;
 
 use crate::context::LayoutContext;
 use crate::dom_traversal::{NodeAndStyleInfo, PseudoElementContentItem};
@@ -39,56 +40,117 @@ pub(crate) fn make_marker<'dom>(
         Image::LightDark(..) => unreachable!("light-dark() should be disabled"),
     };
     let content = marker_image().or_else(|| {
-        Some(vec![PseudoElementContentItem::Text(
-            marker_string(list_style)?.into(),
-        )])
+        Some(vec![PseudoElementContentItem::Text(marker_string(
+            &list_style.list_style_type,
+        )?)])
     })?;
 
     Some((marker_info, content))
 }
 
-/// <https://drafts.csswg.org/css-lists/#marker-string>
-fn marker_string(style: &style_structs::List) -> Option<&'static str> {
-    match style.list_style_type {
-        ListStyleType::None => None,
-        // TODO: Using non-breaking space here is a bit of a hack to give a bit of margin to outside
-        // markers, but really we should be setting `white-space: pre` on them instead.
-        // See https://github.com/w3c/csswg-drafts/issues/4891.
-        ListStyleType::Disc => Some("•\u{00a0}"),
-        ListStyleType::Circle => Some("◦\u{00a0}"),
-        ListStyleType::Square => Some("▪\u{00a0}"),
-        ListStyleType::DisclosureOpen => Some("▾\u{00a0}"),
-        ListStyleType::DisclosureClosed => Some("‣\u{00a0}"),
-        ListStyleType::Decimal |
-        ListStyleType::LowerAlpha |
-        ListStyleType::UpperAlpha |
-        ListStyleType::ArabicIndic |
-        ListStyleType::Bengali |
-        ListStyleType::Cambodian |
-        ListStyleType::CjkDecimal |
-        ListStyleType::Devanagari |
-        ListStyleType::Gujarati |
-        ListStyleType::Gurmukhi |
-        ListStyleType::Kannada |
-        ListStyleType::Khmer |
-        ListStyleType::Lao |
-        ListStyleType::Malayalam |
-        ListStyleType::Mongolian |
-        ListStyleType::Myanmar |
-        ListStyleType::Oriya |
-        ListStyleType::Persian |
-        ListStyleType::Telugu |
-        ListStyleType::Thai |
-        ListStyleType::Tibetan |
-        ListStyleType::CjkEarthlyBranch |
-        ListStyleType::CjkHeavenlyStem |
-        ListStyleType::LowerGreek |
-        ListStyleType::Hiragana |
-        ListStyleType::HiraganaIroha |
-        ListStyleType::Katakana |
-        ListStyleType::KatakanaIroha => {
-            // TODO: Implement support for counters.
-            None
+fn symbol_to_string(symbol: &Symbol) -> &str {
+    match symbol {
+        Symbol::String(string) => string,
+        Symbol::Ident(ident) => &ident.0,
+    }
+}
+
+/// <https://drafts.csswg.org/css-counter-styles-3/#generate-a-counter>
+pub(crate) fn generate_counter_representation(counter_style: &CounterStyle) -> &str {
+    // TODO: Most counter styles produce different results depending on the counter value.
+    // Since we don't support counter properties yet, assume a value of 0 for now.
+    match counter_style {
+        CounterStyle::None | CounterStyle::String(_) => unreachable!("Invalid counter style"),
+        CounterStyle::Name(name) => match name.0 {
+            atom!("disc") => "\u{2022}",            /* "•" */
+            atom!("circle") => "\u{25E6}",          /* "◦" */
+            atom!("square") => "\u{25AA}",          /* "▪" */
+            atom!("disclosure-open") => "\u{25BE}", /* "▾" */
+            // TODO: Use U+25C2 "◂" depending on the direction.
+            atom!("disclosure-closed") => "\u{25B8}", /* "▸" */
+            atom!("decimal-leading-zero") => "00",
+            atom!("arabic-indic") => "\u{660}", /* "٠" */
+            atom!("bengali") => "\u{9E6}",      /* "০" */
+            atom!("cambodian") | atom!("khmer") => "\u{17E0}", /* "០" */
+            atom!("devanagari") => "\u{966}",   /* "०" */
+            atom!("gujarati") => "\u{AE6}",     /* "૦" */
+            atom!("gurmukhi") => "\u{A66}",     /* "੦" */
+            atom!("kannada") => "\u{CE6}",      /* "೦" */
+            atom!("lao") => "\u{ED0}",          /* "໐" */
+            atom!("malayalam") => "\u{D66}",    /* "൦" */
+            atom!("mongolian") => "\u{1810}",   /* "᠐" */
+            atom!("myanmar") => "\u{1040}",     /* "၀" */
+            atom!("oriya") => "\u{B66}",        /* "୦" */
+            atom!("persian") => "\u{6F0}",      /* "۰" */
+            atom!("tamil") => "\u{BE6}",        /* "௦" */
+            atom!("telugu") => "\u{C66}",       /* "౦" */
+            atom!("thai") => "\u{E50}",         /* "๐" */
+            atom!("tibetan") => "\u{F20}",      /* "༠" */
+            atom!("cjk-decimal") |
+            atom!("cjk-earthly-branch") |
+            atom!("cjk-heavenly-stem") |
+            atom!("japanese-informal") => "\u{3007}", /* "〇" */
+            atom!("korean-hangul-formal") => "\u{C601}", /* "영" */
+            atom!("korean-hanja-informal") |
+            atom!("korean-hanja-formal") |
+            atom!("japanese-formal") |
+            atom!("simp-chinese-informal") |
+            atom!("simp-chinese-formal") |
+            atom!("trad-chinese-informal") |
+            atom!("trad-chinese-formal") |
+            atom!("cjk-ideographic") => "\u{96F6}", /* "零" */
+            // Fall back to decimal.
+            _ => "0",
+        },
+        CounterStyle::Symbols { ty, symbols } => match ty {
+            // For numeric, use the first symbol, which represents the value 0.
+            SymbolsType::Numeric => {
+                symbol_to_string(symbols.0.first().expect("symbols() should have symbols"))
+            },
+            // For cyclic, the first symbol represents the value 1. However, it loops back,
+            // so the last symbol represents the value 0.
+            SymbolsType::Cyclic => {
+                symbol_to_string(symbols.0.last().expect("symbols() should have symbols"))
+            },
+            // For the others, the first symbol represents the value 1, and 0 is out of range.
+            // Therefore, fall back to `decimal`.
+            SymbolsType::Alphabetic | SymbolsType::Symbolic | SymbolsType::Fixed => "0",
         },
     }
+}
+
+/// <https://drafts.csswg.org/css-lists/#marker-string>
+pub(crate) fn marker_string(list_style_type: &ListStyleType) -> Option<String> {
+    let suffix = match &list_style_type.0 {
+        CounterStyle::None => return None,
+        CounterStyle::String(string) => return Some(string.to_string()),
+        CounterStyle::Name(name) => match name.0 {
+            atom!("disc") |
+            atom!("circle") |
+            atom!("square") |
+            atom!("disclosure-open") |
+            atom!("disclosure-closed") => " ",
+            atom!("hiragana") |
+            atom!("hiragana-iroha") |
+            atom!("katakana") |
+            atom!("katakana-iroha") |
+            atom!("cjk-decimal") |
+            atom!("cjk-earthly-branch") |
+            atom!("cjk-heavenly-stem") |
+            atom!("japanese-informal") |
+            atom!("japanese-formal") |
+            atom!("simp-chinese-informal") |
+            atom!("simp-chinese-formal") |
+            atom!("trad-chinese-informal") |
+            atom!("trad-chinese-formal") |
+            atom!("cjk-ideographic") => "\u{3001}", /* "、" */
+            atom!("korean-hangul-formal") |
+            atom!("korean-hanja-informal") |
+            atom!("korean-hanja-formal") => ", ",
+            atom!("ethiopic-numeric") => "/ ",
+            _ => ". ",
+        },
+        CounterStyle::Symbols { .. } => " ",
+    };
+    Some(generate_counter_representation(&list_style_type.0).to_string() + suffix)
 }
