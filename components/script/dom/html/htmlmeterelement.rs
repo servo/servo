136f1901359e147484f9b0577dafdc64ece324b2
Gae24 <96017547+Gae24@users.noreply.github.com>
2026-03-12 17:46:32 +0100

    remove `temp_cx` inside `HTMLMeterElement::create_shadow_tree`
    Signed-off-by: Gae24 <96017547+Gae24@users.noreply.github.com>

--- components/script/dom/html/htmlmeterelement.rs
+++ components/script/dom/html/htmlmeterelement.rs
@@ -72,11 +72,7 @@ impl HTMLMeterElement {
         )
     }
 
-    #[expect(unsafe_code)]
-    fn create_shadow_tree(&self, _can_gc: CanGc) {
-        let mut cx = unsafe { script_bindings::script_runtime::temp_cx() };
-        let cx = &mut cx;
-
+    fn create_shadow_tree(&self, cx: &mut JSContext) {
         let document = self.owner_document();
         let root = self.upcast::<Element>().attach_ua_shadow_root(cx, true);
 
@@ -100,9 +96,9 @@ impl HTMLMeterElement {
             .dirty(crate::dom::node::NodeDamage::Other);
     }
 
-    fn shadow_tree(&self, can_gc: CanGc) -> Ref<'_, ShadowTree> {
+    fn shadow_tree(&self, cx: &mut JSContext) -> Ref<'_, ShadowTree> {
         if !self.upcast::<Element>().is_shadow_host() {
-            self.create_shadow_tree(can_gc);
+            self.create_shadow_tree(cx);
         }
 
         Ref::filter_map(self.shadow_tree.borrow(), Option::as_ref)
@@ -110,7 +106,7 @@ impl HTMLMeterElement {
             .expect("UA shadow tree was not created")
     }
 
-    fn update_state(&self, can_gc: CanGc) {
+    fn update_state(&self, cx: &mut JSContext) {
         let value = *self.Value();
         let low = *self.Low();
         let high = *self.High();
@@ -159,12 +155,14 @@ impl HTMLMeterElement {
         self.upcast::<Element>().set_state(element_state, true);
 
         // Update the visual width of the meter
-        let shadow_tree = self.shadow_tree(can_gc);
+        let shadow_tree = self.shadow_tree(cx);
         let position = (value - min) / (max - min) * 100.0;
         let style = format!("width: {position}%");
-        shadow_tree
-            .meter_value
-            .set_string_attribute(&local_name!("style"), style.into(), can_gc);
+        shadow_tree.meter_value.set_string_attribute(
+            &local_name!("style"),
+            style.into(),
+            CanGc::from_cx(cx),
+        );
     }
 }
 
@@ -314,10 +312,15 @@ impl VirtualMethods for HTMLMeterElement {
         Some(self.upcast::<HTMLElement>() as &dyn VirtualMethods)
     }
 
-    fn attribute_mutated(&self, attr: &Attr, mutation: AttributeMutation, can_gc: CanGc) {
+    #[expect(unsafe_code)]
+    fn attribute_mutated(&self, attr: &Attr, mutation: AttributeMutation, _can_gc: CanGc) {
+        // TODO: https://github.com/servo/servo/issues/42812
+        let mut cx = unsafe { script_bindings::script_runtime::temp_cx() };
+        let cx = &mut cx;
+
         self.super_type()
             .unwrap()
-            .attribute_mutated(attr, mutation, can_gc);
+            .attribute_mutated(attr, mutation, CanGc::from_cx(cx));
 
         let is_important_attribute = matches!(
             attr.local_name(),
@@ -329,19 +332,19 @@ impl VirtualMethods for HTMLMeterElement {
                 &local_name!("value")
         );
         if is_important_attribute {
-            self.update_state(can_gc);
+            self.update_state(cx);
         }
     }
 
     fn children_changed(&self, cx: &mut JSContext, mutation: &ChildrenMutation) {
         self.super_type().unwrap().children_changed(cx, mutation);
 
-        self.update_state(CanGc::from_cx(cx));
+        self.update_state(cx);
     }
 
     fn bind_to_tree(&self, cx: &mut JSContext, context: &BindContext) {
         self.super_type().unwrap().bind_to_tree(cx, context);
 
-        self.update_state(CanGc::from_cx(cx));
+        self.update_state(cx);
     }
 }
