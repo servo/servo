4d54bbc4f1aa38bd9da570351efc44a4baf2f310
Martin Robinson <mrobinson@igalia.com>
2026-03-13 12:48:36 +0100

    layout: Properly find text index when clicking to left of text fragments
    When clicking just to the left of a text fragment, we should return the
    first index in that fragment. This means that when you click on the
    order of a text input, the first fragment is selected. We do this by
    improving the distance calculation logic to account for this situation.
    
    Signed-off-by: Martin Robinson <mrobinson@igalia.com>

--- components/servo/tests/webview.rs
+++ components/servo/tests/webview.rs
@@ -489,7 +489,7 @@ fn test_show_and_hide_ime() {
         .build();
 
     show_webview_and_wait_for_rendering_to_be_ready(&servo_test, &webview, &delegate);
-    click_at_point(&webview, Point2D::new(100., 100.));
+    click_at_point(&webview, Point2D::new(150., 150.));
 
     // The form control should be shown.
     let captured_delegate = delegate.clone();
