9bcb7dbbe39d62b8d749c2d5109a7a1068dc3f3c
gterzian <2792687+gterzian@users.noreply.github.com>
2026-03-26 18:10:01 +0700

    add comments to worflow test
    Signed-off-by: gterzian <2792687+gterzian@users.noreply.github.com>

--- components/storage/tests/client_storage.rs
+++ components/storage/tests/client_storage.rs
@@ -30,28 +30,29 @@ fn test_workflow() {
     PipelineNamespace::install(PipelineNamespaceId(1));
     let handle: ClientStorageThreadHandle = ClientStorageThreadFactory::new(None);
 
-    // Create some storage
     let url = ServoUrl::parse("https://example.com").unwrap();
 
-    let receiver = handle.obtain_a_storage_bottle_map(
+    // Obtain a first storage proxy map.
+    let storage_proxy_map = handle.obtain_a_storage_bottle_map(
         StorageType::Local,
         WebViewId::new(servo_base::id::TEST_PAINTER_ID),
         StorageIdentifier::IndexedDB,
         url.origin(),
-    );
+    ).recv().unwrap().unwrap();
 
-    let storage_proxy_map = receiver.recv().unwrap().unwrap();
-
+    // Create a db.
     let receiver = handle.create_database(storage_proxy_map.bottle_id, "test1".to_string());
     let path = receiver.recv().unwrap().expect("Path should be created");
 
     assert!(std::fs::read_dir(path.clone()).is_ok());
 
+    // Delete the db.
     let receiver = handle.delete_database(storage_proxy_map.bottle_id, "test1".to_string());
     receiver.recv().unwrap().expect("Db should be deleted");
 
     assert!(std::fs::read_dir(path).is_err());
 
+    // Get another proxy map fro the same parameters.
     let second_proxy_map = handle
         .obtain_a_storage_bottle_map(
             StorageType::Local,
@@ -63,6 +64,7 @@ fn test_workflow() {
         .unwrap()
         .unwrap();
 
+    // Bottle id should be the same, because the manager returned the existing id.
     assert_eq!(second_proxy_map.bottle_id, storage_proxy_map.bottle_id);
 
     // Workaround for https://github.com/servo/servo/issues/32912
