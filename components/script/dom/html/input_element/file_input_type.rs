563dd06e52e835e7695d4b91e8a0328040f16459
Luke Warlow <lwarlow@igalia.com>
2026-03-27 12:15:50 +0000

    script: Implement shadow tree for file input (#43498)
    script: Implement shadow tree for file input
    
    File inputs also now support ::file-selector-button
    
    Testing: Existing WPTs
    Fixes: https://github.com/servo/servo/issues/39565
    
    Stylo PR: https://github.com/servo/stylo/pull/341
    
    Signed-off-by: Luke Warlow <lwarlow@igalia.com>
    Co-authored-by: Vasiluță Mihai-Alexandru <alexv@siluta.me>

--- components/script/dom/html/input_element/file_input_type.rs
+++ components/script/dom/html/input_element/file_input_type.rs
@@ -1,41 +1,77 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+use std::cell::Ref;
 use std::path::PathBuf;
 use std::str::FromStr;
 
 use embedder_traits::{EmbedderControlRequest, FilePickerRequest, FilterPattern, SelectedFile};
+use html5ever::{local_name, ns};
 use js::context::JSContext;
+use markup5ever::QualName;
 use script_bindings::codegen::GenericBindings::FileListBinding::FileListMethods;
+use script_bindings::codegen::GenericBindings::HTMLButtonElementBinding::HTMLButtonElementMethods;
+use script_bindings::codegen::GenericBindings::HTMLElementBinding::HTMLElementMethods;
 use script_bindings::codegen::GenericBindings::HTMLInputElementBinding::HTMLInputElementMethods;
+use script_bindings::codegen::GenericBindings::NodeBinding::NodeMethods;
 use script_bindings::domstring::DOMString;
 use script_bindings::inheritance::Castable;
+use script_bindings::root::Dom;
 use script_bindings::script_runtime::CanGc;
+use style::selector_parser::PseudoElement;
 use style::str::split_commas;
 
 use crate::dom::bindings::cell::DomRefCell;
 use crate::dom::bindings::root::{DomRoot, MutNullableDom};
 use crate::dom::document_embedder_controls::ControlElement;
+use crate::dom::element::{CustomElementCreationMode, Element, ElementCreator};
 use crate::dom::event::{Event, EventBubbles, EventCancelable, EventComposed};
 use crate::dom::eventtarget::EventTarget;
 use crate::dom::file::File;
 use crate::dom::filelist::FileList;
-use crate::dom::htmlinputelement::text_value_widget::TextValueWidget;
+use crate::dom::htmlbuttonelement::HTMLButtonElement;
+use crate::dom::htmlelement::HTMLElement;
 use crate::dom::input_element::HTMLInputElement;
 use crate::dom::input_element::input_type::SpecificInputType;
-use crate::dom::node::NodeTraits;
+use crate::dom::node::{Node, NodeTraits};
 
 const DEFAULT_FILE_INPUT_VALUE: &str = "No file chosen";
 const DEFAULT_FILE_INPUT_MULTIPLE_VALUE: &str = "No files chosen";
+const SELECTOR_BUTTON_TEXT: &str = "Choose file";
+const SELECTOR_BUTTON_MULTIPLE_TEXT: &str = "Choose files";
 
 #[derive(Default, JSTraceable, MallocSizeOf, PartialEq)]
 #[cfg_attr(crown, crown::unrooted_must_root_lint::must_root)]
 pub(crate) struct FileInputType {
     filelist: MutNullableDom<FileList>,
-    text_value_widget: DomRefCell<TextValueWidget>,
+    shadow_tree: DomRefCell<Option<FileInputShadowTree>>,
 }
 
 impl FileInputType {
+    /// Get the shadow tree for this [`HTMLInputElement`], if it is created and valid, otherwise
+    /// recreate the shadow tree and return it.
+    fn get_or_create_shadow_tree(
+        &self,
+        cx: &mut JSContext,
+        input: &HTMLInputElement,
+    ) -> Ref<'_, FileInputShadowTree> {
+        {
+            if let Ok(shadow_tree) = Ref::filter_map(self.shadow_tree.borrow(), |shadow_tree| {
+                shadow_tree.as_ref()
+            }) {
+                return shadow_tree;
+            }
+        }
+
+        let element = input.upcast::<Element>();
+        let shadow_root = element
+            .shadow_root()
+            .unwrap_or_else(|| element.attach_ua_shadow_root(cx, true));
+        let shadow_root = shadow_root.upcast();
+        *self.shadow_tree.borrow_mut() = Some(FileInputShadowTree::new(cx, shadow_root));
+        self.get_or_create_shadow_tree(cx, input)
+    }
+
     pub(crate) fn handle_file_picker_response(
         &self,
         input: &HTMLInputElement,
@@ -179,9 +215,11 @@ impl SpecificInputType for FileInputType {
     }
 
     fn update_shadow_tree(&self, cx: &mut JSContext, input: &HTMLInputElement) {
-        self.text_value_widget
-            .borrow()
-            .update_shadow_tree(cx, input)
+        self.get_or_create_shadow_tree(cx, input).update(
+            cx,
+            self.value_for_shadow_dom(input),
+            input.Multiple(),
+        )
     }
 }
 
@@ -200,4 +238,80 @@ fn filter_from_accept(s: &DOMString) -> Vec<FilterPattern> {
     }
 
     filter
+}
+
+#[derive(Clone, JSTraceable, MallocSizeOf, PartialEq)]
+#[cfg_attr(crown, crown::unrooted_must_root_lint::must_root)]
+/// Contains references to the elements in the shadow tree for `<input type=file>`.
+///
+/// The shadow tree consists of the file selector button and a span for the chosen files text.
+pub(crate) struct FileInputShadowTree {
+    selector_button: Dom<Element>,
+    value_container: Dom<Element>,
+}
+
+impl FileInputShadowTree {
+    pub(crate) fn new(cx: &mut JSContext, shadow_root: &Node) -> Self {
+        let selector_button = Element::create(
+            cx,
+            QualName::new(None, ns!(html), local_name!("button")),
+            None,
+            &shadow_root.owner_document(),
+            ElementCreator::ScriptCreated,
+            CustomElementCreationMode::Asynchronous,
+            None,
+        );
+
+        selector_button
+            .downcast::<HTMLButtonElement>()
+            .expect("This should be guaranteed by the element type used above")
+            .SetType(DOMString::from("button"));
+
+        selector_button
+            .downcast::<HTMLElement>()
+            .expect("This should be guaranteed by the element type used above")
+            .SetTabIndex(-1, CanGc::from_cx(cx));
+
+        let _ = shadow_root.AppendChild(cx, selector_button.upcast());
+
+        selector_button
+            .upcast::<Node>()
+            .set_implemented_pseudo_element(PseudoElement::FileSelectorButton);
+
+        let value_container = Element::create(
+            cx,
+            QualName::new(None, ns!(html), local_name!("span")),
+            None,
+            &shadow_root.owner_document(),
+            ElementCreator::ScriptCreated,
+            CustomElementCreationMode::Asynchronous,
+            None,
+        );
+
+        let _ = shadow_root.AppendChild(cx, value_container.upcast());
+
+        Self {
+            selector_button: selector_button.as_traced(),
+            value_container: value_container.as_traced(),
+        }
+    }
+
+    pub(crate) fn update(&self, cx: &mut JSContext, input_value: DOMString, multiple: bool) {
+        if multiple {
+            self.selector_button
+                .upcast::<Node>()
+                .set_text_content_for_element(
+                    cx,
+                    Some(DOMString::from(SELECTOR_BUTTON_MULTIPLE_TEXT)),
+                );
+        } else {
+            self.selector_button
+                .upcast::<Node>()
+                .set_text_content_for_element(cx, Some(DOMString::from(SELECTOR_BUTTON_TEXT)));
+        }
+
+        self.value_container
+            .upcast::<Node>()
+            .set_text_content_for_element(cx, Some(input_value));
+    }
 }
