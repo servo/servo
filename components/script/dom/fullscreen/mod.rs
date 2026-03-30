80023548c39adba2c4155974e42b040c3f1e78b2
Niya Gupta <niyabits@disroot.org>
2026-03-30 19:21:09 +0530

    refactor: move document related methods to `document.rs`
    Signed-off-by: Niya Gupta <niyabits@disroot.org>

--- components/script/dom/fullscreen/mod.rs
+++ components/script/dom/fullscreen/mod.rs
@@ -2,5 +2,4 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#[allow(clippy::module_inception, reason = "The interface name is fullscreen")]
-pub mod fullscreen;
+pub(crate) mod document;
