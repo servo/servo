7a494f5a5c1dc584e2a0fc3a06e60e8d96809d2f
Kelechi Ebiri <ebiritg@gmail.com>
2026-04-01 03:56:16 +0100

    Revert to == for document comparison
    Signed-off-by: Kelechi Ebiri <ebiritg@gmail.com>

--- components/script/dom/html/htmlformelement.rs
+++ components/script/dom/html/htmlformelement.rs
@@ -885,13 +885,12 @@ impl HTMLFormElement {
         // Step 24. Let historyHandling be "auto".
         // Step 25. If form document equals targetNavigable's active document, and form document has not yet completely loaded,
         // then set historyHandling to "replace".
-        let history_handling = if std::ptr::eq(&*doc as *const _, &*target_document as *const _) &&
-            !doc.completely_loaded()
-        {
+        let history_handling = if doc == target_document && !doc.completely_loaded() {
             NavigationHistoryBehavior::Replace
         } else {
             NavigationHistoryBehavior::Auto
         };
+
         let target_window = target_document.window();
         let mut load_data = LoadData::new(
             LoadOrigin::Script(doc.origin().snapshot()),
