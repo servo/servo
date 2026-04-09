2529697714755b6ea74e87fdb5af9224ca974221
ak4shravikumar <ak4shravikumar@gmail.com>
2026-04-09 13:16:01 +0530

    Add license header to geometry mod.rs
    Signed-off-by: ak4shravikumar <ak4shravikumar@gmail.com>

--- components/script/dom/geometry/mod.rs
+++ components/script/dom/geometry/mod.rs
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 pub(crate) mod dommatrix;
 pub(crate) mod dommatrixreadonly;
 pub(crate) mod dompoint;
@@ -6,3 +10,13 @@ pub(crate) mod domquad;
 pub(crate) mod domrect;
 pub(crate) mod domrectlist;
 pub(crate) mod domrectreadonly;
+
+// Re-export geometry interfaces so they remain accessible via crate::dom::*
+pub(crate) use dommatrix::*;
+pub(crate) use dommatrixreadonly::*;
+pub(crate) use dompoint::*;
+pub(crate) use dompointreadonly::*;
+pub(crate) use domquad::*;
+pub(crate) use domrect::*;
+pub(crate) use domrectlist::*;
+pub(crate) use domrectreadonly::*;
