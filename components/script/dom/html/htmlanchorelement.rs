12dff46781b11db4db76528ebe3520f939a98ff6
Tim van der Lippe <tvanderlippe@gmail.com>
2026-04-25 11:36:46 +0200

    script: Pass `&mut JSContext` to HyperlinkElementUtils
    Part of #42812
    
    Testing: it compiles
    
    Signed-off-by: Tim van der Lippe <tvanderlippe@gmail.com>

--- components/script/dom/html/htmlanchorelement.rs
+++ components/script/dom/html/htmlanchorelement.rs
@@ -148,13 +148,16 @@ impl HTMLAnchorElementMethods<crate::DomTypeHolder> for HTMLAnchorElement {
     make_getter!(Rel, "rel");
 
     /// <https://html.spec.whatwg.org/multipage/#dom-a-rel>
-    fn SetRel(&self, rel: DOMString, can_gc: CanGc) {
-        self.upcast::<Element>()
-            .set_tokenlist_attribute(&local_name!("rel"), rel, can_gc);
+    fn SetRel(&self, cx: &mut JSContext, rel: DOMString) {
+        self.upcast::<Element>().set_tokenlist_attribute(
+            &local_name!("rel"),
+            rel,
+            CanGc::from_cx(cx),
+        );
     }
 
     /// <https://html.spec.whatwg.org/multipage/#dom-a-rellist>
-    fn RelList(&self, can_gc: CanGc) -> DomRoot<DOMTokenList> {
+    fn RelList(&self, cx: &mut JSContext) -> DomRoot<DOMTokenList> {
         self.rel_list.or_init(|| {
             DOMTokenList::new(
                 self.upcast(),
@@ -164,7 +167,7 @@ impl HTMLAnchorElementMethods<crate::DomTypeHolder> for HTMLAnchorElement {
                     Atom::from("noreferrer"),
                     Atom::from("opener"),
                 ]),
-                can_gc,
+                CanGc::from_cx(cx),
             )
         })
     }
@@ -173,49 +176,49 @@ impl HTMLAnchorElementMethods<crate::DomTypeHolder> for HTMLAnchorElement {
     make_getter!(Hreflang, "hreflang");
 
     // https://html.spec.whatwg.org/multipage/#dom-a-hreflang
-    make_setter!(SetHreflang, "hreflang");
+    make_setter!(cx, SetHreflang, "hreflang");
 
     // https://html.spec.whatwg.org/multipage/#dom-a-type
     make_getter!(Type, "type");
 
     // https://html.spec.whatwg.org/multipage/#dom-a-type
-    make_setter!(SetType, "type");
+    make_setter!(cx, SetType, "type");
 
     // https://html.spec.whatwg.org/multipage/#dom-a-coords
     make_getter!(Coords, "coords");
 
     // https://html.spec.whatwg.org/multipage/#dom-a-coords
-    make_setter!(SetCoords, "coords");
+    make_setter!(cx, SetCoords, "coords");
 
     // https://html.spec.whatwg.org/multipage/#dom-a-charset
     make_getter!(Charset, "charset");
 
     // https://html.spec.whatwg.org/multipage/#dom-a-charset
-    make_setter!(SetCharset, "charset");
+    make_setter!(cx, SetCharset, "charset");
 
     // https://html.spec.whatwg.org/multipage/#dom-a-name
     make_getter!(Name, "name");
 
     // https://html.spec.whatwg.org/multipage/#dom-a-name
-    make_atomic_setter!(SetName, "name");
+    make_atomic_setter!(cx, SetName, "name");
 
     // https://html.spec.whatwg.org/multipage/#dom-a-rev
     make_getter!(Rev, "rev");
 
     // https://html.spec.whatwg.org/multipage/#dom-a-rev
-    make_setter!(SetRev, "rev");
+    make_setter!(cx, SetRev, "rev");
 
     // https://html.spec.whatwg.org/multipage/#dom-a-shape
     make_getter!(Shape, "shape");
 
     // https://html.spec.whatwg.org/multipage/#dom-a-shape
-    make_setter!(SetShape, "shape");
+    make_setter!(cx, SetShape, "shape");
 
     // https://html.spec.whatwg.org/multipage/#attr-hyperlink-target
     make_getter!(Target, "target");
 
     // https://html.spec.whatwg.org/multipage/#attr-hyperlink-target
-    make_setter!(SetTarget, "target");
+    make_setter!(cx, SetTarget, "target");
 
     /// <https://html.spec.whatwg.org/multipage/#dom-hyperlink-href>
     fn Href(&self) -> USVString {
@@ -223,8 +226,8 @@ impl HTMLAnchorElementMethods<crate::DomTypeHolder> for HTMLAnchorElement {
     }
 
     /// <https://html.spec.whatwg.org/multipage/#dom-hyperlink-href>
-    fn SetHref(&self, value: USVString, can_gc: CanGc) {
-        self.set_href(value, can_gc);
+    fn SetHref(&self, cx: &mut JSContext, value: USVString) {
+        self.set_href(cx, value);
     }
 
     /// <https://html.spec.whatwg.org/multipage/#dom-hyperlink-origin>
@@ -238,8 +241,8 @@ impl HTMLAnchorElementMethods<crate::DomTypeHolder> for HTMLAnchorElement {
     }
 
     /// <https://html.spec.whatwg.org/multipage/#dom-hyperlink-protocol>
-    fn SetProtocol(&self, value: USVString, can_gc: CanGc) {
-        self.set_protocol(value, can_gc);
+    fn SetProtocol(&self, cx: &mut JSContext, value: USVString) {
+        self.set_protocol(cx, value);
     }
 
     /// <https://html.spec.whatwg.org/multipage/#dom-hyperlink-password>
@@ -248,8 +251,8 @@ impl HTMLAnchorElementMethods<crate::DomTypeHolder> for HTMLAnchorElement {
     }
 
     /// <https://html.spec.whatwg.org/multipage/#dom-hyperlink-password>
-    fn SetPassword(&self, value: USVString, can_gc: CanGc) {
-        self.set_password(value, can_gc);
+    fn SetPassword(&self, cx: &mut JSContext, value: USVString) {
+        self.set_password(cx, value);
     }
 
     /// <https://html.spec.whatwg.org/multipage/#dom-hyperlink-hash>
@@ -258,8 +261,8 @@ impl HTMLAnchorElementMethods<crate::DomTypeHolder> for HTMLAnchorElement {
     }
 
     /// <https://html.spec.whatwg.org/multipage/#dom-hyperlink-hash>
-    fn SetHash(&self, value: USVString, can_gc: CanGc) {
-        self.set_hash(value, can_gc);
+    fn SetHash(&self, cx: &mut JSContext, value: USVString) {
+        self.set_hash(cx, value);
     }
 
     /// <https://html.spec.whatwg.org/multipage/#dom-hyperlink-host>
@@ -268,8 +271,8 @@ impl HTMLAnchorElementMethods<crate::DomTypeHolder> for HTMLAnchorElement {
     }
 
     /// <https://html.spec.whatwg.org/multipage/#dom-hyperlink-host>
-    fn SetHost(&self, value: USVString, can_gc: CanGc) {
-        self.set_host(value, can_gc);
+    fn SetHost(&self, cx: &mut JSContext, value: USVString) {
+        self.set_host(cx, value);
     }
 
     /// <https://html.spec.whatwg.org/multipage/#dom-hyperlink-hostname>
@@ -278,8 +281,8 @@ impl HTMLAnchorElementMethods<crate::DomTypeHolder> for HTMLAnchorElement {
     }
 
     /// <https://html.spec.whatwg.org/multipage/#dom-hyperlink-hostname>
-    fn SetHostname(&self, value: USVString, can_gc: CanGc) {
-        self.set_hostname(value, can_gc);
+    fn SetHostname(&self, cx: &mut JSContext, value: USVString) {
+        self.set_hostname(cx, value);
     }
 
     /// <https://html.spec.whatwg.org/multipage/#dom-hyperlink-port>
@@ -288,8 +291,8 @@ impl HTMLAnchorElementMethods<crate::DomTypeHolder> for HTMLAnchorElement {
     }
 
     /// <https://html.spec.whatwg.org/multipage/#dom-hyperlink-port>
-    fn SetPort(&self, value: USVString, can_gc: CanGc) {
-        self.set_port(value, can_gc);
+    fn SetPort(&self, cx: &mut JSContext, value: USVString) {
+        self.set_port(cx, value);
     }
 
     /// <https://html.spec.whatwg.org/multipage/#dom-hyperlink-pathname>
@@ -298,8 +301,8 @@ impl HTMLAnchorElementMethods<crate::DomTypeHolder> for HTMLAnchorElement {
     }
 
     /// <https://html.spec.whatwg.org/multipage/#dom-hyperlink-pathname>
-    fn SetPathname(&self, value: USVString, can_gc: CanGc) {
-        self.set_pathname(value, can_gc);
+    fn SetPathname(&self, cx: &mut JSContext, value: USVString) {
+        self.set_pathname(cx, value);
     }
 
     /// <https://html.spec.whatwg.org/multipage/#dom-hyperlink-search>
@@ -308,8 +311,8 @@ impl HTMLAnchorElementMethods<crate::DomTypeHolder> for HTMLAnchorElement {
     }
 
     /// <https://html.spec.whatwg.org/multipage/#dom-hyperlink-search>
-    fn SetSearch(&self, value: USVString, can_gc: CanGc) {
-        self.set_search(value, can_gc);
+    fn SetSearch(&self, cx: &mut JSContext, value: USVString) {
+        self.set_search(cx, value);
     }
 
     /// <https://html.spec.whatwg.org/multipage/#dom-hyperlink-username>
@@ -318,8 +321,8 @@ impl HTMLAnchorElementMethods<crate::DomTypeHolder> for HTMLAnchorElement {
     }
 
     /// <https://html.spec.whatwg.org/multipage/#dom-hyperlink-username>
-    fn SetUsername(&self, value: USVString, can_gc: CanGc) {
-        self.set_username(value, can_gc);
+    fn SetUsername(&self, cx: &mut JSContext, value: USVString) {
+        self.set_username(cx, value);
     }
 
     /// <https://html.spec.whatwg.org/multipage/#dom-a-referrerpolicy>
@@ -328,7 +331,7 @@ impl HTMLAnchorElementMethods<crate::DomTypeHolder> for HTMLAnchorElement {
     }
 
     // https://html.spec.whatwg.org/multipage/#dom-script-referrerpolicy
-    make_setter!(SetReferrerPolicy, "referrerpolicy");
+    make_setter!(cx, SetReferrerPolicy, "referrerpolicy");
 }
 
 impl Activatable for HTMLAnchorElement {
