c17723743b84295550c0a60b1904fc0013d17ea8
Shubham Gupta <32428749+shubhamg13@users.noreply.github.com>
2026-04-29 10:24:48 +0800

    Implement `PerformanceMarkOptions` (#44471)
    Adding the implementation of `PerformanceMarkOptions`
    
    Testing: More WPT tests Passed.
    
    ---------
    
    Signed-off-by: Shubham Gupta <shubham.gupta@chromium.org>

--- components/script/dom/performance/performancemark.rs
+++ components/script/dom/performance/performancemark.rs
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+use js::gc::HandleValue;
 use js::jsapi::Heap;
 use js::jsval::JSVal;
 use js::rust::MutableHandleValue;
@@ -17,6 +18,12 @@ impl_performance_entry_struct!(
         detail: Heap<JSVal>,
     }
 );
+
+impl PerformanceMark {
+    pub(crate) fn set_detail(&self, handle: HandleValue<'_>) {
+        self.detail.set(handle.get());
+    }
+}
 
 impl PerformanceMarkMethods<crate::DomTypeHolder> for PerformanceMark {
     fn Detail(&self, _cx: JSContext, mut retval: MutableHandleValue) {
