0dca205bfb193d3e41c34af0a6dfe82d9873f425
Jonathan Schwender <schwenderjonathan@gmail.com>
2026-04-30 21:00:35 +0200

    release: Bump version to 0.2.0
    In preperation for the monthly (April) release, bump the version
    number to 0.2.0.
    Note: 0.1.x is reserved for the LTS version, and for regular releases
    we anyway don't perform analysis of semver compatibility yet, which
    would be required to make a semver compatible release.
    
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>

--- support/android/apk/servoapp/build.gradle.kts
+++ support/android/apk/servoapp/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 30
         targetSdk = 34
         versionCode = generatedVersionCode
-        versionName = "0.1.0"
+        versionName = "0.2.0"
     }
 
     compileOptions {
