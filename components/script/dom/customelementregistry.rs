2bad86827e82a5a26528418838b157a3d99be531
Javier Olaechea <pirata@gmail.com>
2026-05-01 00:51:48 -0500

    Add an error message when defining a custom element with an invalid name
    Signed-off-by: Javier Olaechea <pirata@gmail.com>

--- components/script/dom/customelementregistry.rs
+++ components/script/dom/customelementregistry.rs
@@ -418,7 +418,10 @@ impl CustomElementRegistryMethods<crate::DomTypeHolder> for CustomElementRegistr
 
         // Step 2. If name is not a valid custom element name, then throw a "SyntaxError" DOMException.
         if !is_valid_custom_element_name(&name) {
-            return Err(Error::Syntax(None));
+            return Err(Error::Syntax(Some(format!(
+                "{} name is not a valid custom element name",
+                name
+            ))));
         }
 
         // Step 3. If this's custom element definition set contains an item with name name,
