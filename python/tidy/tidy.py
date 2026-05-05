b4f435abfcce98f8dc104edfc871b7068f55b174
Simon Sapin <simon@igalia.com>
2026-05-05 11:56:38 +0200

    spelling
    Signed-off-by: Simon Sapin <simon@igalia.com>

--- python/tidy/tidy.py
+++ python/tidy/tidy.py
@@ -1081,7 +1081,7 @@ def run_coauthors_check() -> int:
 
     origin = parse_origin(subprocess.check_output(["git", "remote", "-v"], text=True))
     if origin:
-        log_format = "commit %H%nAuthor: %an <%ae>%nCommiter: %cn <%ce>%n%(trailers:key=Co-authored-by)"
+        log_format = "commit %H%nAuthor: %an <%ae>%nCommitter: %cn <%ce>%n%(trailers:key=Co-authored-by)"
         log_command = ["git", "log", f"{origin}/main..HEAD", f"--format={log_format}"]
         log = subprocess.check_output(log_command, text=True)
         errors = check_coauthors(pull_request_body, log, verbose=is_pr_ci)
