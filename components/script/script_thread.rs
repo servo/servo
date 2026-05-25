a53195ba575876fbca394d89f84a8c5dc8eb2c3f
Jonathan Schwender <schwenderjonathan@gmail.com>
2026-05-25 13:08:56 +0200

    format
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>

--- components/script/script_thread.rs
+++ components/script/script_thread.rs
@@ -3582,7 +3582,10 @@ impl ScriptThread {
         let refresh_header = metadata.headers.as_deref().and_then(|h| h.get(REFRESH));
         if let Some(refresh_val) = refresh_header {
             // There are tests that this header handles Unicode code points
-            document.shared_declarative_refresh_steps(refresh_val.as_bytes(), false /* from_meta_element */);
+            document.shared_declarative_refresh_steps(
+                refresh_val.as_bytes(),
+                /* from_meta_element */ false,
+            );
         }
 
         document.set_ready_state(cx, DocumentReadyState::Loading);
