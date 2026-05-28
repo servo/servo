servo/servo

commit 6a5093391796e26fe8b68df4ac4409324964bd20

layout: Move containing block calculation to stacking context tree construction (#44911)
Before this PR, calculating accumulated containing blocks is a
stand-alone traversal of the fragment tree that takes ~4% of the layout
time in one synthetic benchmark. This moves it to be part of the
existing tree traversal for stacking context construction, hopefully
improving performance.

Testing: expected not to change observable behavior or outcomes of
existing tests. The intermediate commit **before** “Remove now-redundant
containing block computation pass” has does calculations and asserts
they give the same result. Latest WPT run with this assertion:
https://github.com/servo/servo/actions/runs/25807772217

---------

Signed-off-by: Simon Sapin <simon@igalia.com>
Signed-off-by: Martin Robinson <mrobinson@igalia.com>
Co-authored-by: Martin Robinson <mrobinson@igalia.com>

components/layout/layout_impl.rs
@@ -20,7 +20,7 @@ use fonts::{FontContext, FontContextWebFontMethods, WebFontDocumentContext};
 use fonts_traits::StylesheetWebFontLoadFinishedCallback;
 use icu_locid::subtags::Language;
 use layout_api::{
-    AxesOverflow, BoxAreaType, CSSPixelRectIterator, DangerousStyleNode, IFrameSizes, Layout,
+    AxesOverflow, BoxAreaType, CSSPixelRectVec, DangerousStyleNode, IFrameSizes, Layout,
     LayoutConfig, LayoutElement, LayoutFactory, LayoutNode, NodeRenderingType,
     OffsetParentResponse, PhysicalSides, QueryMsg, ReflowGoal, ReflowPhasesRun, ReflowRequest,
     ReflowRequestRestyle, ReflowResult, ReflowStatistics, ScrollContainerQueryFlags,
@@ -173,6 +173,12 @@ pub struct LayoutThread {
     /// layout requests a display list, it is produced unconditionally, even when the
     /// layout trees remain the same.
     need_new_display_list: Cell<bool>,
+
+    /// Whether or not cumulative containing blocks offsets have been set into the
+    /// [`FragmentTree`]. This typically happens during [`StackingContextTree`]
+    /// construction, but if a layout query needs these value beforehand, they are
+    /// eagerly calculated.
+    need_containing_block_calculation: Cell<bool>,
 
     /// Whether or not the existing stacking context tree is dirty and needs to be
     /// rebuilt. This happens after a relayout or overflow update. The reason that we
@@ -393,6 +399,7 @@ impl Layout for LayoutThread {
             let stacking_context_tree = self.stacking_context_tree.borrow();
             let stacking_context_tree = stacking_context_tree.as_ref()?;
             process_box_area_request(
+                self,
                 stacking_context_tree,
                 node,
                 area,
@@ -406,7 +413,7 @@ impl Layout for LayoutThread {
     ///
     /// See <https://drafts.csswg.org/cssom-view/#dom-element-getclientrects>.
     #[servo_tracing::instrument(skip_all)]
-    fn query_box_areas(&self, node: TrustedNodeAddress, area: BoxAreaType) -> CSSPixelRectIterator {
+    fn query_box_areas(&self, node: TrustedNodeAddress, area: BoxAreaType) -> CSSPixelRectVec {
         with_layout_state(|| {
             // If we have not built a fragment tree yet, there is no way we have layout information for
             // this query, which can be run without forcing a layout (for IntersectionObserver).
@@ -417,9 +424,14 @@ impl Layout for LayoutThread {
             let node = unsafe { ServoLayoutNode::new(&node) };
             let stacking_context_tree = self.stacking_context_tree.borrow();
             let stacking_context_tree = stacking_context_tree.as_ref()?;
-            Some(process_box_areas_request(stacking_context_tree, node, area))
+            Some(process_box_areas_request(
+                self,
+                stacking_context_tree,
+                node,
+                area,
+            ))
         })
-        .unwrap_or_else(|| Box::new(std::iter::empty()))
+        .unwrap_or_default()
     }
 
     #[servo_tracing::instrument(skip_all)]
@@ -451,7 +463,7 @@ impl Layout for LayoutThread {
             let node = unsafe { ServoLayoutNode::new(&node) };
             let stacking_context_tree = self.stacking_context_tree.borrow();
             let stacking_context_tree = stacking_context_tree.as_ref()?;
-            process_offset_parent_query(&stacking_context_tree.paint_info.scroll_tree, node)
+            process_offset_parent_query(self, &stacking_context_tree.paint_info.scroll_tree, node)
         })
         .unwrap_or_default()
     }
@@ -496,7 +508,7 @@ impl Layout for LayoutThread {
                 TraversalFlags::empty(),
             );
 
-            process_resolved_style_request(&shared_style_context, node, &pseudo, &property_id)
+            process_resolved_style_request(self, &shared_style_context, node, &pseudo, &property_id)
         })
     }
 
@@ -540,7 +552,7 @@ impl Layout for LayoutThread {
     fn query_scrolling_area(&self, node: Option<TrustedNodeAddress>) -> Rect<i32, CSSPixel> {
         with_layout_state(|| {
             let node = node.map(|node| unsafe { ServoLayoutNode::new(&node) });
-            process_node_scroll_area_request(node, self.fragment_tree.borrow().clone())
+            process_node_scroll_area_request(self, node, self.fragment_tree.borrow().clone())
         })
     }
 
@@ -770,6 +782,7 @@ impl LayoutThread {
             have_ever_generated_display_list: Cell::new(false),
             last_display_list_was_empty: Cell::new(true),
             device_has_changed: false,
+            need_containing_block_calculation: Cell::new(false),
             need_new_display_list: Cell::new(false),
             need_new_stacking_context_tree: Cell::new(false),
             box_tree: Default::default(),
@@ -1284,6 +1297,9 @@ impl LayoutThread {
             .as_ref()
             .map(|tree| tree.paint_info.scroll_tree.scroll_offsets());
 
+        // This will be done during `StackingContextTree::new` below
+        self.need_containing_block_calculation.set(true);
+
         // Build the StackingContextTree. This turns the `FragmentTree` into a
         // tree of fragments in CSS painting order and also creates all
         // applicable spatial and clip nodes.
@@ -1493,6 +1509,20 @@ impl LayoutThread {
             reflow_phases_run: ReflowPhasesRun::BuiltDisplayList,
             ..Default::default()
         })
+    }
+
+    pub(crate) fn ensure_containing_block_calculation(&self) {
+        if !self.need_containing_block_calculation.get() {
+            return;
+        }
+        let fragment_tree = self.fragment_tree.borrow();
+        fragment_tree.as_ref().expect("missing fragment tree").find(
+            |fragment, _level, containing_block| {
+                fragment.set_containing_block(containing_block);
+                None::<()>
+            },
+        );
+        self.need_containing_block_calculation.set(false)
     }
 }
 

components/layout/display_list/mod.rs
@@ -53,8 +53,8 @@ use crate::context::{ImageResolver, ResolvedImage};
 pub(crate) use crate::display_list::conversions::ToWebRender;
 use crate::display_list::stacking_context::StackingContextSection;
 use crate::fragment_tree::{
-    BackgroundMode, BoxFragment, Fragment, FragmentFlags, FragmentStatus, FragmentTree,
-    SpecificLayoutInfo, Tag, TextFragment,
+    BackgroundMode, BoxFragment, ContainingBlockCalculation, Fragment, FragmentFlags,
+    FragmentStatus, FragmentTree, SpecificLayoutInfo, Tag, TextFragment,
 };
 use crate::geom::{
     LengthPercentageOrAuto, PhysicalPoint, PhysicalRect, PhysicalSides, PhysicalSize,
@@ -487,7 +487,10 @@ impl DisplayListBuilder<'_> {
                     }
 
                     let bounds = box_fragment
-                        .offset_by_containing_block(&fragment_relative_bounds)
+                        .offset_by_containing_block(
+                            &fragment_relative_bounds,
+                            ContainingBlockCalculation::AlreadyDoneWithStackingContextTree,
+                        )
                         .to_webrender();
 
                     // We paint each highlighted area as if it was a border for simplicity

components/layout/display_list/stacking_context.rs
@@ -44,10 +44,12 @@ use super::clip::StackingContextTreeClipStore;
 use crate::display_list::conversions::{FilterToWebRender, ToWebRender};
 use crate::display_list::{BuilderForBoxFragment, DisplayListBuilder, offset_radii};
 use crate::fragment_tree::{
-    BoxFragment, ContainingBlockManager, Fragment, FragmentFlags, FragmentTree,
-    PositioningFragment, SpecificLayoutInfo,
+    BoxFragment, ContainingBlockCalculation, ContainingBlockManager, Fragment, FragmentFlags,
+    FragmentTree, PositioningFragment, SpecificLayoutInfo,
 };
-use crate::geom::{AuOrAuto, LengthPercentageOrAuto, PhysicalPoint, PhysicalRect, PhysicalSides};
+use crate::geom::{
+    AuOrAuto, LengthPercentageOrAuto, PhysicalPoint, PhysicalRect, PhysicalSides, PhysicalVec,
+};
 use crate::style_ext::{ComputedValuesExt, TransformExt};
 
 #[derive(Clone)]
@@ -66,6 +68,13 @@ pub(crate) struct ContainingBlock {
 
     /// The physical rect of this containing block.
     rect: PhysicalRect<Au>,
+
+    /// Normally containing block offsets and display list items are positioned relative
+    /// to their parent reference frame, but cumulative containing block boundaries on
+    /// fragments need to disregard reference frames entirely. This value tracks the
+    /// accumulated offset from the origin of the parent reference frame of this
+    /// containing block.
+    accumulated_reference_frame_offset: PhysicalVec<Au>,
 }
 
 impl ContainingBlock {
@@ -74,12 +83,14 @@ impl ContainingBlock {
         scroll_node_id: ScrollTreeNodeId,
         scroll_frame_size: Option<LayoutSize>,
         clip_id: ClipId,
+        accumulated_reference_frame_offset: PhysicalVec<Au>,
     ) -> Self {
         ContainingBlock {
             scroll_node_id,
             scroll_frame_size,
             clip_id,
             rect,
+            accumulated_reference_frame_offset,
         }
     }
 
@@ -151,12 +162,14 @@ impl StackingContextTree {
             root_scroll_node_id,
             Some(viewport_size),
             ClipId::INVALID,
+            PhysicalVec::zero(),
         );
         let cb_for_fixed_descendants = ContainingBlock::new(
             fragment_tree.initial_containing_block,
             paint_info.root_reference_frame_id,
             None,
             ClipId::INVALID,
+            PhysicalVec::zero(),
         );
 
         // We need to specify all three containing blocks here, because absolute
@@ -293,8 +306,12 @@ impl StackingContextTree {
             .scroll_tree
             .reference_frame_offset(spatial_tree_node)
             .map(Au::from_f32_px);
-        let fragment_origin =
-            fragment.cumulative_content_box_rect().origin - reference_frame_origin.cast_unit();
+        let fragment_origin = fragment
+            .cumulative_content_box_rect(
+                ContainingBlockCalculation::AlreadyDoneWithStackingContextTree,
+            )
+            .origin -
+            reference_frame_origin.cast_unit();
 
         // Use that to find the offset from the fragment origin.
         Some(transformed_point - fragment_origin)
@@ -924,6 +941,12 @@ impl Fragment {
         mode: StackingContextBuildMode,
         text_decorations: &Arc<Vec<FragmentTextDecoration>>,
     ) {
+        let containing_block = containing_block_info.get_containing_block_for_fragment(self);
+        let cumulative_containing_block = containing_block
+            .rect
+            .translate(containing_block.accumulated_reference_frame_offset);
+        self.set_containing_block(&cumulative_containing_block);
+
         if self
             .base()
             .is_some_and(|base| base.flags.contains(FragmentFlags::IS_COLLAPSED))
@@ -931,7 +954,6 @@ impl Fragment {
             return;
         }
 
-        let containing_block = containing_block_info.get_containing_block_for_fragment(self);
         let fragment_clone = self.clone();
         match self {
             Fragment::Box(fragment) | Fragment::Float(fragment) => {
@@ -1106,7 +1128,12 @@ impl BoxFragment {
         }
 
         let style = self.style();
-        let frame_origin_for_query = self.cumulative_border_box_rect().origin.to_webrender();
+        let frame_origin_for_query = self
+            .cumulative_border_box_rect(
+                ContainingBlockCalculation::AlreadyDoneWithStackingContextTree,
+            )
+            .origin
+            .to_webrender();
         let new_spatial_id = stacking_context_tree.push_reference_frame(
             reference_frame_data.origin.to_webrender(),
             frame_origin_for_query,
@@ -1126,13 +1153,13 @@ impl BoxFragment {
         // containing blocks for absolute and fixed descendants, so those
         // properties will be replaced before recursing into children.
         assert!(style.establishes_containing_block_for_all_descendants(self.base.flags));
+        let reference_frame_offset = reference_frame_data.origin.to_vector();
         let adjusted_containing_block = ContainingBlock::new(
-            containing_block
-                .rect
-                .translate(-reference_frame_data.origin.to_vector()),
+            containing_block.rect.translate(-reference_frame_offset),
             new_spatial_id,
             None,
             containing_block.clip_id,
+            containing_block.accumulated_reference_frame_offset + reference_frame_offset,
         );
         let new_containing_block_info =
             containing_block_info.new_for_non_absolute_descendants(&adjusted_containing_block);
@@ -1365,12 +1392,14 @@ impl BoxFragment {
             new_scroll_node_id,
             new_scroll_frame_size,
             new_clip_id,
+            containing_block.accumulated_reference_frame_offset,
         );
         let for_non_absolute_descendants = ContainingBlock::new(
             content_rect,
             new_scroll_node_id,
             new_scroll_frame_size,
             new_clip_id,
+            containing_block.accumulated_reference_frame_offset,
         );
 
         // Create a new `ContainingBlockInfo` for descendants depending on

components/layout/fragment_tree/box_fragment.rs
@@ -23,6 +23,7 @@ use super::{BaseFragment, BaseFragmentInfo, CollapsedBlockMargins, Fragment, Fra
 use crate::SharedStyle;
 use crate::display_list::ToWebRender;
 use crate::formatting_contexts::Baselines;
+use crate::fragment_tree::ContainingBlockCalculation;
 use crate::geom::{
     AuOrAuto, LengthPercentageOrAuto, PhysicalPoint, PhysicalRect, PhysicalSides, ToLogical,
 };
@@ -387,7 +388,12 @@ impl BoxFragment {
         *self.cumulative_containing_block_rect.borrow_mut() = *containing_block;
     }
 
-    pub(crate) fn offset_by_containing_block(&self, rect: &PhysicalRect<Au>) -> PhysicalRect<Au> {
+    pub(crate) fn offset_by_containing_block(
+        &self,
+        rect: &PhysicalRect<Au>,
+        containing_block_computation: ContainingBlockCalculation<'_>,
+    ) -> PhysicalRect<Au> {
+        containing_block_computation.ensure();
         rect.translate(
             self.cumulative_containing_block_rect
                 .borrow()
@@ -396,16 +402,25 @@ impl BoxFragment {
         )
     }
 
-    pub(crate) fn cumulative_content_box_rect(&self) -> PhysicalRect<Au> {
-        self.offset_by_containing_block(&self.base.rect())
+    pub(crate) fn cumulative_content_box_rect(
+        &self,
+        containing_block_computation: ContainingBlockCalculation<'_>,
+    ) -> PhysicalRect<Au> {
+        self.offset_by_containing_block(&self.base.rect(), containing_block_computation)
     }
 
-    pub(crate) fn cumulative_padding_box_rect(&self) -> PhysicalRect<Au> {
-        self.offset_by_containing_block(&self.padding_rect())
+    pub(crate) fn cumulative_padding_box_rect(
+        &self,
+        containing_block_computation: ContainingBlockCalculation<'_>,
+    ) -> PhysicalRect<Au> {
+        self.offset_by_containing_block(&self.padding_rect(), containing_block_computation)
     }
 
-    pub(crate) fn cumulative_border_box_rect(&self) -> PhysicalRect<Au> {
-        self.offset_by_containing_block(&self.border_rect())
+    pub(crate) fn cumulative_border_box_rect(
+        &self,
+        containing_block_computation: ContainingBlockCalculation<'_>,
+    ) -> PhysicalRect<Au> {
+        self.offset_by_containing_block(&self.border_rect(), containing_block_computation)
     }
 
     pub(crate) fn content_rect(&self) -> PhysicalRect<Au> {
@@ -549,7 +564,10 @@ impl BoxFragment {
         }
     }
 
-    pub(crate) fn calculate_resolved_insets_if_positioned(&self) -> PhysicalSides<AuOrAuto> {
+    pub(crate) fn calculate_resolved_insets_if_positioned(
+        &self,
+        containing_block_computation: ContainingBlockCalculation<'_>,
+    ) -> PhysicalSides<AuOrAuto> {
         let style = self.style();
         let position = style.get_box().position;
         debug_assert_ne!(
@@ -571,8 +589,10 @@ impl BoxFragment {
             )
         };
 
-        let containing_block_size =
-            LazyCell::new(|| self.cumulative_containing_block_rect.borrow().size);
+        let containing_block_size = LazyCell::new(|| {
+            containing_block_computation.ensure();
+            self.cumulative_containing_block_rect.borrow().size
+        });
 
         // "A resolved value special case property like top defined in another
         // specification If the property applies to a positioned element and the

components/layout/fragment_tree/fragment.rs
@@ -24,6 +24,7 @@ use crate::SharedStyle;
 use crate::cell::ArcRefCell;
 use crate::flow::inline::line::TextRunOffsets;
 use crate::geom::{LogicalSides, PhysicalPoint, PhysicalRect};
+use crate::layout_impl::LayoutThread;
 use crate::style_ext::ComputedValuesExt;
 
 #[derive(Clone, MallocSizeOf)]
@@ -146,11 +147,10 @@ impl Fragment {
         }
     }
 
-    pub(crate) fn scrolling_area(&self) -> PhysicalRect<Au> {
+    pub(crate) fn scrolling_area(&self, layout_thread: &LayoutThread) -> PhysicalRect<Au> {
         match self {
-            Fragment::Box(fragment) | Fragment::Float(fragment) => {
-                fragment.offset_by_containing_block(&fragment.scrollable_overflow())
-            },
+            Fragment::Box(fragment) | Fragment::Float(fragment) => fragment
+                .offset_by_containing_block(&fragment.scrollable_overflow(), layout_thread.into()),
             _ => self.scrollable_overflow_for_parent(),
         }
     }
@@ -212,15 +212,28 @@ impl Fragment {
         }
     }
 
-    pub(crate) fn cumulative_box_area_rect(&self, area: BoxAreaType) -> Option<PhysicalRect<Au>> {
+    pub(crate) fn cumulative_box_area_rect(
+        &self,
+        area: BoxAreaType,
+        containing_block_computation: ContainingBlockCalculation<'_>,
+    ) -> Option<PhysicalRect<Au>> {
         match self {
             Fragment::Box(fragment) | Fragment::Float(fragment) => Some(match area {
-                BoxAreaType::Content => fragment.cumulative_content_box_rect(),
-                BoxAreaType::Padding => fragment.cumulative_padding_box_rect(),
-                BoxAreaType::Border => fragment.cumulative_border_box_rect(),
+                BoxAreaType::Content => {
+                    fragment.cumulative_content_box_rect(containing_block_computation)
+                },
+                BoxAreaType::Padding => {
+                    fragment.cumulative_padding_box_rect(containing_block_computation)
+                },
+                BoxAreaType::Border => {
+                    fragment.cumulative_border_box_rect(containing_block_computation)
+                },
             }),
             Fragment::Positioning(fragment) => {
-                Some(fragment.offset_by_containing_block(&fragment.base.rect()))
+                Some(fragment.offset_by_containing_block(
+                    &fragment.base.rect(),
+                    containing_block_computation,
+                ))
             },
             Fragment::Text(_) |
             Fragment::AbsoluteOrFixedPositioned(_) |
@@ -486,5 +499,39 @@ impl CollapsedMargin {
 
     pub fn solve(&self) -> Au {
         self.max_positive + self.min_negative
+    }
+}
+
+/// A token which ensures the calculation and assignment of cumulative containing
+/// blocks to fragments in the fragment tree. This is used because these cumulative
+/// containing block offsets are set during stacking context tree construction, but
+/// some queries might need them beforehand. If the query is executed before stacking
+/// context tree construction, a quick traversal is performed to calculate them for
+/// the purpose of the query.
+pub(crate) enum ContainingBlockCalculation<'a> {
+    /// This token variant is for the purpose of a layout query. In this case, if stacking
+    /// context tree construction has not yet taken place, a cumulative containing block
+    /// calculation traversal will be performed.
+    Lazy { layout_thread: &'a LayoutThread },
+    /// This token variant is used when the code can guarantee that stacking context
+    /// tree construction has already taken place.
+    ///
+    /// Note: Using this before stacking context tree construction can lead
+    /// to incorrect layout or layout query results!
+    AlreadyDoneWithStackingContextTree,
+}
+
+impl ContainingBlockCalculation<'_> {
+    pub(crate) fn ensure(&self) {
+        match self {
+            Self::Lazy { layout_thread } => layout_thread.ensure_containing_block_calculation(),
+            Self::AlreadyDoneWithStackingContextTree => {},
+        }
+    }
+}
+
+impl<'a> From<&'a LayoutThread> for ContainingBlockCalculation<'a> {
+    fn from(layout_thread: &'a LayoutThread) -> Self {
+        Self::Lazy { layout_thread }
     }
 }

components/layout/fragment_tree/fragment_tree.rs
@@ -43,22 +43,12 @@ impl FragmentTree {
         initial_containing_block: PhysicalRect<Au>,
         viewport_scroll_sensitivity: AxesScrollSensitivity,
     ) -> Self {
-        let fragment_tree = Self {
+        Self {
             root_fragments,
             scrollable_overflow: Cell::default(),
             initial_containing_block,
             viewport_scroll_sensitivity,
-        };
-
-        // Trigger scrollable overflow calculation unconditionally when creating a new Fragment.
-        fragment_tree.scrollable_overflow();
-
-        fragment_tree.find(|fragment, _level, containing_block| {
-            fragment.set_containing_block(containing_block);
-            None::<()>
-        });
-
-        fragment_tree
+        }
     }
 
     pub fn print(&self) {

components/layout/fragment_tree/positioning_fragment.rs
@@ -12,6 +12,7 @@ use servo_base::print_tree::PrintTree;
 use style::properties::ComputedValues;
 
 use super::{BaseFragment, BaseFragmentInfo, Fragment};
+use crate::fragment_tree::ContainingBlockCalculation;
 use crate::geom::PhysicalRect;
 
 /// Can contain child fragments with relative coordinates, but does not contribute to painting
@@ -65,7 +66,12 @@ impl PositioningFragment {
         *self.cumulative_containing_block_rect.borrow_mut() = *containing_block;
     }
 
-    pub fn offset_by_containing_block(&self, rect: &PhysicalRect<Au>) -> PhysicalRect<Au> {
+    pub fn offset_by_containing_block(
+        &self,
+        rect: &PhysicalRect<Au>,
+        containing_block_computation: ContainingBlockCalculation<'_>,
+    ) -> PhysicalRect<Au> {
+        containing_block_computation.ensure();
         rect.translate(
             self.cumulative_containing_block_rect
                 .borrow()

commit 7502880233ab294569cf53aea9a913db35c26e79

layout: Mark layout as not needing containing block calculation when building stacking context tree
This was a small logic bug introduced in #44911. When building the
stacking context tree we don't need to calculate containing blocks for
queries, as they are calculated during the stacking context tree
construction itself.

This seems to improve performance from ~32k runs/s to ~35k runs/s on the
`flexbox-deeply-nested-column-flow.html` benchmark (after #45208 is
applied).

Signed-off-by: Martin Robinson <mrobinson@igalia.com>

components/layout/layout_impl.rs
@@ -1313,7 +1313,7 @@ impl LayoutThread {
             .map(|tree| tree.paint_info.scroll_tree.scroll_offsets());
 
         // This will be done during `StackingContextTree::new` below
-        self.need_containing_block_calculation.set(true);
+        self.need_containing_block_calculation.set(false);
 
         // Build the StackingContextTree. This turns the `FragmentTree` into a
         // tree of fragments in CSS painting order and also creates all