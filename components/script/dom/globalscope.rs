4c5b317e1a8b323e74bece6a17f14780948f0f51
Euclid Ye <yezhizhenjiakang@gmail.com>
2026-06-03 19:19:13 +0800

    script: Generate new blob URL per `createObjectURL` call (#45182)
    Previously, we generate same URL when calling `createObjectURL` on same
    `Blob`.
    
    [Spec](https://w3c.github.io/FileAPI/#dfn-createObjectURL):
    > ..
    > Step 2. Let url be the result of [generating a new blob
    URL](https://w3c.github.io/FileAPI/#unicodeBlobURL).
    > Step 3. Let entry be a new [blob URL
    entry](https://w3c.github.io/FileAPI/#blob-url-entry) consisting of
    object and the [current settings
    object](https://html.spec.whatwg.org/multipage/webappapis.html#current-settings-object).
    
    I guess there is a good security reason: it is likely a website call
    `createObjectURL`
    on the same Blob object multiple times. We must be able to revoke them
    separately.
    
    Testing: Passing in existing tests.
    Part of #10778
    
    ---------
    
    Signed-off-by: Euclid Ye <yezhizhenjiakang@gmail.com>

--- components/script/dom/globalscope.rs
+++ components/script/dom/globalscope.rs
@@ -2135,6 +2135,28 @@ impl GlobalScope {
         }
     }
 
+    /// Send a PromoteMemory message to register a new blob URL entry
+    /// with the file manager for the given byte data.
+    /// Return the generated UUID.
+    fn promote_memory_entry(
+        &self,
+        blob_info: &BlobInfo,
+        blob_bytes: &[u8],
+        set_valid: bool,
+    ) -> Uuid {
+        let origin = self.origin().immutable();
+        let blob_buf = BlobBuf {
+            filename: None,
+            type_string: blob_info.blob_impl.type_string(),
+            size: blob_bytes.len() as u64,
+            bytes: blob_bytes.to_vec(),
+        };
+        let id = Uuid::new_v4();
+        let msg = FileManagerThreadMsg::PromoteMemory(id, blob_buf, set_valid, origin.clone());
+        self.send_to_file_manager(msg);
+        id
+    }
+
     /// Promote non-Slice blob:
     /// 1. Memory-based: The bytes in data slice will be transferred to file manager thread.
     /// 2. File-based: If set_valid, then activate the FileID so it can serve as URL
@@ -2149,6 +2171,12 @@ impl GlobalScope {
             },
             BlobData::File(f) => {
                 if set_valid {
+                    // File blobs with cached byte data (converted from Memory)
+                    // need a unique UUID per URL.createObjectURL call.
+                    if let Some(cached_bytes) = f.get_cache() {
+                        return self.promote_memory_entry(blob_info, &cached_bytes, true);
+                    }
+
                     let origin = self.origin().immutable();
                     let (tx, rx) = profile_ipc::channel(self.time_profiler_chan().clone()).unwrap();
 
@@ -2168,18 +2196,7 @@ impl GlobalScope {
             BlobData::Memory(bytes_in) => mem::swap(bytes_in, &mut bytes),
         };
 
-        let origin = self.origin().immutable();
-
-        let blob_buf = BlobBuf {
-            filename: None,
-            type_string: blob_info.blob_impl.type_string(),
-            size: bytes.len() as u64,
-            bytes: bytes.to_vec(),
-        };
-
-        let id = Uuid::new_v4();
-        let msg = FileManagerThreadMsg::PromoteMemory(id, blob_buf, set_valid, origin.clone());
-        self.send_to_file_manager(msg);
+        let id = self.promote_memory_entry(blob_info, &bytes, set_valid);
 
         *blob_info.blob_impl.blob_data_mut() = BlobData::File(FileBlob::new(
             id,
