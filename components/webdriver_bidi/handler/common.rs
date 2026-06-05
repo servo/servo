ccbf1db76e0064193b19af6aa7574260da9a754e
Xubai Wang <18016038327@189.cn>
2026-06-06 02:26:12 +0800

    webdriver: bidi web_extension stub
    Signed-off-by: Xubai Wang <18016038327@189.cn>

--- components/webdriver_bidi/handler/common.rs
+++ components/webdriver_bidi/handler/common.rs
@@ -1,6 +1,18 @@
+use std::{collections::HashSet, path::PathBuf};
+
 use embedder_traits::webdriver_bidi::WebDriverBidiToEmbedderMsg;
 
 use crate::{error::WebDriverBidiError, handler::Handler};
+
+impl Handler {
+    /// <https://fs.spec.whatwg.org/#locating-an-entry>
+    pub(super) async fn locate_entry<T: LocateEntryOutput>(
+        &self,
+        _locator: &T,
+    ) -> Option<T::Output> {
+        unimplemented!()
+    }
+}
 
 impl Handler {
     pub(super) fn send_message_to_embedder(
@@ -11,4 +23,56 @@ impl Handler {
         self.0.event_loop_waker.wake();
         Ok(())
     }
+}
+
+pub(super) struct FileLocator {
+    pub path: PathBuf,
+    pub root: String,
+}
+
+pub(super) struct DirectoryLocator {
+    pub path: PathBuf,
+    pub root: String,
+}
+
+/// <https://fs.spec.whatwg.org/#file-system-locator>
+pub(super) enum FileSystemLocator {
+    File(FileLocator),
+    Directory(DirectoryLocator),
+}
+
+pub(super) enum FileSystemLocatorKind {
+    File,
+    Directory,
+}
+
+pub(super) enum FileSystemEntry {
+    File(FileEntry),
+    Directory(DirectoryEntry),
+}
+
+pub(super) struct FileEntry {
+    pub binary_data: Vec<u8>,
+    pub modification_timestamp: (),
+    pub lock: (),
+}
+
+pub(super) struct DirectoryEntry {
+    pub children: HashSet<FileSystemEntry>,
+}
+
+pub(super) trait LocateEntryOutput {
+    type Output;
+}
+
+impl LocateEntryOutput for FileLocator {
+    type Output = FileEntry;
+}
+
+impl LocateEntryOutput for DirectoryLocator {
+    type Output = DirectoryEntry;
+}
+
+impl LocateEntryOutput for FileSystemLocator {
+    type Output = FileSystemEntry;
 }
