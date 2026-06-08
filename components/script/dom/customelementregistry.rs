e7253aa50b6c38712b2fa644a4089030161c8950
Gae24 <96017547+Gae24@users.noreply.github.com>
2026-06-08 22:57:22 +0200

    customelementregistry: pass `&mut JSContext` to `create_element`
    Signed-off-by: Gae24 <96017547+Gae24@users.noreply.github.com>

--- components/script/dom/customelementregistry.rs
+++ components/script/dom/customelementregistry.rs
@@ -5,22 +5,23 @@
 use std::cell::Cell;
 use std::collections::VecDeque;
 use std::ffi::CStr;
+use std::ptr::NonNull;
 use std::rc::Rc;
 use std::{mem, ptr};
 
 use dom_struct::dom_struct;
 use html5ever::{LocalName, Namespace, Prefix, ns};
 use js::context::JSContext;
+use js::conversions::FromJSValConvertible;
 use js::glue::UnwrapObjectStatic;
 use js::jsapi::{HandleValueArray, Heap, IsCallable, IsConstructor, JSAutoRealm, JSObject};
 use js::jsval::{BooleanValue, JSVal, NullValue, ObjectValue, UndefinedValue};
 use js::realm::{AutoRealm, CurrentRealm};
-use js::rust::wrappers::Construct1;
-use js::rust::wrappers2::{JS_GetProperty, SameValue};
+use js::rust::wrappers2::{Construct1, JS_GetProperty, SameValue};
 use js::rust::{HandleObject, MutableHandleValue};
 use rustc_hash::FxBuildHasher;
 use script_bindings::cell::DomRefCell;
-use script_bindings::conversions::{SafeFromJSValConvertible, SafeToJSValConvertible};
+use script_bindings::conversions::SafeToJSValConvertible;
 use script_bindings::reflector::{DomObject, Reflector, reflect_dom_object};
 use script_bindings::settings_stack::{run_a_callback, run_a_script};
 use style::attr::AttrValue;
@@ -692,28 +693,28 @@ impl CustomElementDefinition {
     #[expect(unsafe_code)]
     pub(crate) fn create_element(
         &self,
+        cx: &mut JSContext,
         document: &Document,
         prefix: Option<Prefix>,
         registry: Option<DomRoot<CustomElementRegistry>>,
-        // This function can cause GC through AutoEntryScript::Drop, but we can't pass a CanGc there
-        can_gc: CanGc,
     ) -> Fallible<DomRoot<Element>> {
         let window = document.window();
-        let cx = GlobalScope::get_cx();
+
         // Step 5.1.1. Let C be definition’s constructor.
-        rooted!(in(*cx) let constructor = ObjectValue(self.constructor.callback()));
-        rooted!(in(*cx) let mut element = ptr::null_mut::<JSObject>());
+        rooted!(&in(cx) let constructor = ObjectValue(self.constructor.callback()));
+        rooted!(&in(cx) let mut element = ptr::null_mut::<JSObject>());
         {
             // Go into the constructor's realm
-            let _ac = JSAutoRealm::new(*cx, self.constructor.callback());
+            let mut realm = AutoRealm::new(cx, NonNull::new(self.constructor.callback()).unwrap());
+            let cx = &mut realm;
+
             // Step 5.3.1. Set result to the result of constructing C, with no arguments.
             // https://webidl.spec.whatwg.org/#construct-a-callback-function
             run_a_script::<DomTypeHolder, _>(window.upcast(), || {
                 run_a_callback::<DomTypeHolder, _>(window.upcast(), || {
                     let args = HandleValueArray::empty();
-                    if unsafe {
-                        !Construct1(*cx, constructor.handle(), &args, element.handle_mut())
-                    } {
+                    if unsafe { !Construct1(cx, constructor.handle(), &args, element.handle_mut()) }
+                    {
                         Err(Error::JSFailed)
                     } else {
                         Ok(())
@@ -722,9 +723,9 @@ impl CustomElementDefinition {
             })?;
         }
 
-        rooted!(in(*cx) let element_val = ObjectValue(element.get()));
+        rooted!(&in(cx) let element_val = ObjectValue(element.get()));
         let element: DomRoot<Element> =
-            match SafeFromJSValConvertible::safe_from_jsval(cx, element_val.handle(), (), can_gc) {
+            match FromJSValConvertible::safe_from_jsval(cx, element_val.handle(), ()) {
                 Ok(ConversionResult::Success(element)) => element,
                 Ok(ConversionResult::Failure(..)) => {
                     return Err(Error::Type(
@@ -911,7 +912,7 @@ fn run_upgrade_constructor(
         }
 
         // Go into the constructor's realm
-        let mut realm = AutoRealm::new(cx, std::ptr::NonNull::new(constructor.callback()).unwrap());
+        let mut realm = AutoRealm::new(cx, NonNull::new(constructor.callback()).unwrap());
         let cx = &mut *realm;
 
         let args = HandleValueArray::empty();
@@ -924,7 +925,7 @@ fn run_upgrade_constructor(
             run_a_callback::<DomTypeHolder, _>(window.upcast(), || {
                 if unsafe {
                     !Construct1(
-                        cx.raw_cx(),
+                        cx,
                         constructor_val.handle(),
                         &args,
                         construct_result.handle_mut(),
