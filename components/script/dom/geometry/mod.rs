c9361505a74c59387b629ecc78f970ab8e93e2c0
Tim van der Lippe <TimvdLippe@users.noreply.github.com>
2026-06-16 20:56:50 +0200

    script: Move Geometry interfaces into `geometry/` (#45736)
    Part of #38901
    
    Supersedes and closes #44027
    
    Testing: it compiles
    
    Signed-off-by: Tim van der Lippe <tvanderlippe@gmail.com>
    Co-authored-by: ak4shravikumar <ak4shravikumar@gmail.com>

---
+++ components/script/dom/geometry/mod.rs
@@ -0,0 +1,12 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+pub(crate) mod dommatrix;
+pub(crate) mod dommatrixreadonly;
+pub(crate) mod dompoint;
+pub(crate) mod dompointreadonly;
+pub(crate) mod domquad;
+pub(crate) mod domrect;
+pub(crate) mod domrectlist;
+pub(crate) mod domrectreadonly;
