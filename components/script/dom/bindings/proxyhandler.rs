7deb8f0df026ef0694720e51a97426cd121b382d
Gae24 <96017547+Gae24@users.noreply.github.com>
2026-06-23 12:29:38 +0200

    remove `dom/bindings/proxyhandler.rs`
    Signed-off-by: Gae24 <96017547+Gae24@users.noreply.github.com>

--- components/script/dom/bindings/proxyhandler.rs
+++
@@ -1,9 +0,0 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-
-//! Utilities for the implementation of JSAPI proxy handlers.
-
-#![deny(missing_docs)]
-
-pub(crate) use script_bindings::proxyhandler::*;
