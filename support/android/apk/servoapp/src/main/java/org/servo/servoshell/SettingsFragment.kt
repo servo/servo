a1ec21f7ba361cfa9b8879c20f936e24420e8c7d
veyndan <veyndan@gmail.com>
2026-06-28 10:16:37 +0200

    android: Convert settings fragment to Compose UI
    Signed-off-by: veyndan <veyndan@gmail.com>

--- support/android/apk/servoapp/src/main/java/org/servo/servoshell/SettingsFragment.kt
+++ support/android/apk/servoapp/src/main/java/org/servo/servoshell/SettingsFragment.kt
@@ -6,8 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -16,6 +18,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.stringResource
 import androidx.core.content.edit
@@ -34,27 +37,28 @@ class SettingsFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View = inflater.inflate(R.layout.fragment_settings, container, false)
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        view.findViewById<ComposeView>(R.id.toolbar).setContent {
-            @OptIn(ExperimentalMaterial3Api::class)
-            TopAppBar(
-                title = { Text(stringResource(R.string.settings_title)) },
-            )
-        }
-        view.findViewById<ComposeView>(R.id.body).setContent {
-            Column {
-                SettingsItem(
-                    title = stringResource(R.string.settings_experimental_title),
-                    summary = stringResource(R.string.settings_experimental_summary),
-                    preferenceKey = "experimental",
-                )
-                SettingsItem(
-                    title = stringResource(R.string.settings_animating_title),
-                    summary = stringResource(R.string.settings_animating_summary),
-                    preferenceKey = "animating_indicator",
-                )
+    ): View = ComposeView(requireContext()).apply {
+        setContent {
+            Scaffold(
+                topBar = {
+                    @OptIn(ExperimentalMaterial3Api::class)
+                    TopAppBar(
+                        title = { Text(stringResource(R.string.settings_title)) },
+                    )
+                },
+            ) { innerPadding ->
+                Column(modifier = Modifier.padding(innerPadding)) {
+                    SettingsItem(
+                        title = stringResource(R.string.settings_experimental_title),
+                        summary = stringResource(R.string.settings_experimental_summary),
+                        preferenceKey = "experimental",
+                    )
+                    SettingsItem(
+                        title = stringResource(R.string.settings_animating_title),
+                        summary = stringResource(R.string.settings_animating_summary),
+                        preferenceKey = "animating_indicator",
+                    )
+                }
             }
         }
     }
