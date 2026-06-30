be01e10a8372e682b2bc1addcfede72b924f3b90
Martin Robinson <martin@abandonedwig.info>
2026-06-30 13:31:19 +0200

    script: Stop treating `<label>` as a form-associated element
    `<label>` was being treated as a form-associated element, but the
    spec[^1] does not list it as one. This led to it being added to
    `HTMLFormElement::controls`, but not removed consistently. This change
    stops treating it as a special-case form-associated element which fixes
    a panic in debug builds.
    
    Signed-off-by: Martin Robinson <martin@abandonedwig.info>
    
    [^1]: https://html.spec.whatwg.org/multipage/forms.html#form-associated-element

--- components/script/dom/html/htmlformelement.rs
+++ components/script/dom/html/htmlformelement.rs
@@ -68,7 +68,6 @@ use crate::dom::html::htmlelement::HTMLElement;
 use crate::dom::html::htmlfieldsetelement::HTMLFieldSetElement;
 use crate::dom::html::htmlformcontrolscollection::HTMLFormControlsCollection;
 use crate::dom::html::htmlimageelement::HTMLImageElement;
-use crate::dom::html::htmllabelelement::HTMLLabelElement;
 use crate::dom::html::htmllegendelement::HTMLLegendElement;
 use crate::dom::html::htmlobjectelement::HTMLObjectElement;
 use crate::dom::html::htmloutputelement::HTMLOutputElement;
@@ -1950,9 +1949,6 @@ impl FormControlElementHelpers for Element {
             NodeTypeId::Element(ElementTypeId::HTMLElement(
                 HTMLElementTypeId::HTMLInputElement,
             )) => Some(self.downcast::<HTMLInputElement>().unwrap() as &dyn FormControl),
-            NodeTypeId::Element(ElementTypeId::HTMLElement(
-                HTMLElementTypeId::HTMLLabelElement,
-            )) => Some(self.downcast::<HTMLLabelElement>().unwrap() as &dyn FormControl),
             NodeTypeId::Element(ElementTypeId::HTMLElement(
                 HTMLElementTypeId::HTMLLegendElement,
             )) => Some(self.downcast::<HTMLLegendElement>().unwrap() as &dyn FormControl),
