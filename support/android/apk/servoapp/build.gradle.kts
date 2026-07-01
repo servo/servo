bda37000b2c78ad700256dc1021f2cf613d188f7
Jonathan Schwender <55576758+jschwe@users.noreply.github.com>
2026-07-01 11:46:51 +0200

    android: Determine default config dir and lower minSdk (#46142)
    Android 33+ sets the TMPDIR environment variable, but on older versions
    it's unset. This leads to a crash on older android versions where the
    fallback is outside the app sandbox (/data/local/tmp).
    This allows us to lower the minSdk version and probably the default
    config directory should never have been TMPDIR in the first place, since
    that's not persistent.
    I tested with a Pixel 4 emulator API-29, and servoshell seems to work
    fine. However, our current build system uses API-30 NDK, so for now we
    just restore API-30 support. A follow-up PR could choose a lower NDK
    version, and thus lower our minSDK version further. But ideally we add
    CI testing first.
    
    This PR effectively reverts #46104 and fixes the crash on older devices.
    
    Testing: We don't have automatic tests for android in CI. 
    Fixes: #46115
    
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>

--- support/android/apk/servoapp/build.gradle.kts
+++ support/android/apk/servoapp/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "org.servo.servoshell"
-        minSdk = 33
+        minSdk = 30
         targetSdk = 34
         versionCode = generatedVersionCode
         versionName = "0.4.0"
