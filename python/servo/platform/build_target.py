2ab4f076e6eb596c0ab290b7407ed35a89e1e16f
Jonathan Schwender <schwenderjonathan@gmail.com>
2026-07-01 10:56:37 +0200

    seperate tikv jemalloc changes
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>

--- python/servo/platform/build_target.py
+++ python/servo/platform/build_target.py
@@ -224,15 +224,16 @@ class AndroidTarget(CrossBuildTarget):
 
         env["TARGET_AR"] = to_ndk_bin("llvm-ar")
         env["TARGET_RANLIB"] = to_ndk_bin("llvm-ranlib")
+        env["TARGET_OBJCOPY"] = to_ndk_bin("llvm-objcopy")
+        env["TARGET_YASM"] = to_ndk_bin("yasm")
+        env["TARGET_STRIP"] = to_ndk_bin("llvm-strip")
+        env["RUST_FONTCONFIG_DLOPEN"] = "on"
+
         # Needed for tikv-jemalloc, which doesn't respect TARGET_AR and co.
         # On macos this lead to it falling back to `ar` and missing jemalloc
         # symbols in libservoshell.so.
         env["AR"] = to_ndk_bin("llvm-ar")
         env["RANLIB"] = to_ndk_bin("llvm-ranlib")
-        env["TARGET_OBJCOPY"] = to_ndk_bin("llvm-objcopy")
-        env["TARGET_YASM"] = to_ndk_bin("yasm")
-        env["TARGET_STRIP"] = to_ndk_bin("llvm-strip")
-        env["RUST_FONTCONFIG_DLOPEN"] = "on"
 
         env["LIBCLANG_PATH"] = path.join(llvm_toolchain, "lib")
         env["CLANG_PATH"] = to_ndk_bin("clang")
