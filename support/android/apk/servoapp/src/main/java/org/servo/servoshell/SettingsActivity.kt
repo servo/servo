3b78ce02131f304d84cf681050e7d45a217498e1
veyndan <veyndan@gmail.com>
2026-07-06 22:39:45 +0200

    android: Remove settings fragment
    Signed-off-by: veyndan <veyndan@gmail.com>

--- support/android/apk/servoapp/src/main/java/org/servo/servoshell/SettingsActivity.kt
+++ support/android/apk/servoapp/src/main/java/org/servo/servoshell/SettingsActivity.kt
@@ -5,17 +5,73 @@
  */
 package org.servo.servoshell
 
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.commit
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.core.content.edit
+import androidx.preference.PreferenceManager
 
-class SettingsActivity : AppCompatActivity() {
+class SettingsActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
 
-        supportFragmentManager.commit {
-            replace(R.id.settings_container, SettingsFragment())
+        val preferences = PreferenceManager.getDefaultSharedPreferences(this)
+        setContent {
+            Scaffold(
+                topBar = {
+                    @OptIn(ExperimentalMaterial3Api::class)
+                    TopAppBar(
+                        title = { Text(stringResource(R.string.settings_title)) },
+                    )
+                },
+            ) { innerPadding ->
+                Column(modifier = Modifier.padding(innerPadding)) {
+                    SettingsItem(
+                        title = stringResource(R.string.settings_experimental_title),
+                        summary = stringResource(R.string.settings_experimental_summary),
+                        preferences = preferences,
+                        preferenceKey = "experimental",
+                    )
+                }
+            }
         }
+    }
+
+    @Composable
+    private fun SettingsItem(title: String, summary: String, preferences: SharedPreferences, preferenceKey: String) {
+        ListItem(
+            headlineContent = {
+                Text(title)
+            },
+            supportingContent = {
+                Text(summary)
+            },
+            trailingContent = {
+                var checked by remember { mutableStateOf(preferences.getBoolean(preferenceKey, false)) }
+                Switch(
+                    checked = checked,
+                    onCheckedChange = {
+                        checked = it
+                        preferences.edit { putBoolean(preferenceKey, it) }
+                    },
+                )
+            },
+        )
     }
 }
