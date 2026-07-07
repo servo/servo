1360d6b2bcaf8bbc0ec8f30d6b8987fdab32ad0e
veyndan <veyndan@gmail.com>
2026-07-07 21:41:25 +0200

    android: Remove needless key interceptors in `MainActivity`
    Signed-off-by: veyndan <veyndan@gmail.com>

--- support/android/apk/servoapp/src/main/java/org/servo/servoshell/MainActivity.kt
+++ support/android/apk/servoapp/src/main/java/org/servo/servoshell/MainActivity.kt
@@ -12,7 +12,6 @@ import android.os.Bundle
 import android.system.ErrnoException
 import android.system.Os
 import android.util.Log
-import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
@@ -39,7 +38,6 @@ class MainActivity : AppCompatActivity(), Servo.Client {
     private lateinit var servoView: ServoView
 
     private lateinit var urlField: EditText
-    private var urlFieldIsFocused = false
 
     private lateinit var progressBar: CircularProgressIndicator
     private var canGoBackState = mutableStateOf(false)
@@ -237,11 +235,8 @@ class MainActivity : AppCompatActivity(), Servo.Client {
             }
         }
         urlField.setOnFocusChangeListener { v, hasFocus ->
-            if (v.id == R.id.urlfield) {
-                urlFieldIsFocused = hasFocus
-                if (!hasFocus) {
-                    getSystemService<InputMethodManager>()?.hideSoftInputFromWindow(v.windowToken, 0)
-                }
+            if (v.id == R.id.urlfield && !hasFocus) {
+                getSystemService<InputMethodManager>()?.hideSoftInputFromWindow(v.windowToken, 0)
             }
         }
     }
@@ -256,20 +251,6 @@ class MainActivity : AppCompatActivity(), Servo.Client {
 
     override fun onImeHide() {
         getSystemService<InputMethodManager>()?.hideSoftInputFromWindow(servoView.windowToken, InputMethodManager.SHOW_IMPLICIT)
-    }
-
-    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-        if (urlFieldIsFocused) {
-            return true
-        }
-        return servoView.onKeyDown(keyCode, event)
-    }
-
-    override fun onKeyUp(keyCode: Int, event: KeyEvent?): Boolean {
-        if (urlFieldIsFocused) {
-            return true
-        }
-        return servoView.onKeyUp(keyCode, event)
     }
 
     override fun onAlert(message: String?) {
