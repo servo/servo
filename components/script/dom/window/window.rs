73221d3a14c4902e498c0cac1262be21e161d680
Lola <lolaodelola@gmail.com>
2026-07-17 11:39:25 +0100

    enable computed_role to access accessibility tree
    Signed-off-by: lola odelola <lolaodelola@gmail.com>

--- components/script/dom/window/window.rs
+++ components/script/dom/window/window.rs
@@ -3150,6 +3150,7 @@ impl Window {
         &self,
         element: TrustedNodeAddress,
     )-> Option<accesskit::Node> {
+        self.layout().set_needs_accessibility_update();
         self.layout_reflow(QueryMsg::AccessKitNodeQuery);
         self.layout.borrow().query_accesskit_node(element)
     }
