25b89d879cecef6e63203f70993ba22b5e7cd9ef
Veyndan Stuart <veyndan@gmail.com>
2026-07-20 21:49:45 +0200

    android: Upgrade all dependencies (#46666)
    Testing: There are no automated tests for Android.
    Fixes: Part of #45715.
    
    Signed-off-by: veyndan <veyndan@gmail.com>

--- support/android/apk/servoapp/build.gradle.kts
+++ support/android/apk/servoapp/build.gradle.kts
@@ -145,9 +145,9 @@ dependencies {
         implementation(project(":servoview"))
     }
     implementation("androidx.activity:activity-compose:1.13.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.13.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.google.android.material:material:1.14.0")
     implementation("androidx.compose.material3.adaptive:adaptive:1.2.0")
     implementation("androidx.compose.material3:material3:1.4.0")
-    implementation("androidx.preference:preference-ktx:1.2.0")
+    implementation("androidx.preference:preference-ktx:1.2.1")
 }
