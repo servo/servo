ccbb297e4f2b5777334dd4101647b14eb1c5ebc9
Veyndan Stuart <veyndan@gmail.com>
2026-07-21 18:07:03 +0200

    android: Remove usage of appcompat (#46694)
    The only usage of appcompat is via our inheritance of
    `AppCompatActivity`.
    
    The class hierarchy of `AppCompatActivity` is `... > ComponentActivity >
    FragmentActivity > AppCompatActivity`.
    
    The [docs of `FragmentActivity`
    says](https://developer.android.com/reference/androidx/fragment/app/FragmentActivity):
    
    > Base class for activities that want to use the support-based
    [Fragments](https://developer.android.com/reference/androidx/fragment/app/Fragment).
    
    We no longer use Fragments, so we don't care about its support.
    
    The [docs of `AppCompatActivity`
    says](https://developer.android.com/reference/androidx/appcompat/app/AppCompatActivity):
    
    > Base class for activities that wish to use some of the newer platform
    features on older Android devices. Some of these backported features
    include:
    >
    > - Using the action bar, including action items, navigation modes and
    more with the
    [setSupportActionBar](https://developer.android.com/reference/androidx/appcompat/app/AppCompatActivity#setSupportActionBar(androidx.appcompat.widget.Toolbar))
    API.
    > - Built-in switching between light and dark themes by using the
    [Theme.AppCompat.DayNight](https://developer.android.com/reference/androidx/appcompat/R.style#Theme_AppCompat_DayNight())
    theme and
    [setDefaultNightMode](https://developer.android.com/reference/androidx/appcompat/app/AppCompatDelegate#setDefaultNightMode(int))
    API.
    > - Integration with DrawerLayout by using the
    [getDrawerToggleDelegate](https://developer.android.com/reference/androidx/appcompat/app/AppCompatActivity#getDrawerToggleDelegate())
    API.
    
    We also don't use or care about any of these features (these are all
    legacy ways of achieving things, and have been replaced with Compose UI
    equivalents).
    
    Therefore, we can just directly depend on `ComponentActivity`.
    
    Testing: There are no automated tests for Android.
    
    Signed-off-by: veyndan <veyndan@gmail.com>

--- support/android/apk/servoapp/build.gradle.kts
+++ support/android/apk/servoapp/build.gradle.kts
@@ -145,7 +145,6 @@ dependencies {
         implementation(project(":servoview"))
     }
     implementation("androidx.activity:activity-compose:1.13.0")
-    implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.14.0")
     implementation("androidx.compose.material3.adaptive:adaptive:1.2.0")
     implementation("androidx.compose.material3:material3:1.4.0")
