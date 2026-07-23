c5753fd38a5899f24fc43d5030d72205089483ee
Veyndan Stuart <veyndan@gmail.com>
2026-07-23 22:10:35 +0200

    android: Add back icon in toolbar to settings and history (#46747)
    Adds a back icon in the toolbar as is common for nested screens.
    
    Here's what it looks lie for the settings screen (the history screen
    looks similar):
    
    |Before|After|
    |---|---|
    |<img width="1080" height="2400" alt="image"
    src="https://github.com/user-attachments/assets/07d29a32-6dc1-45ab-8e60-756f9556685e"
    />|<img width="1080" height="2400" alt="image"
    src="https://github.com/user-attachments/assets/47c99c50-cef0-4cb5-881c-5c156803e783"
    />|
    
    Testing: There are no automated tests for Android.
    
    Signed-off-by: veyndan <veyndan@gmail.com>

--- support/android/apk/servoapp/src/main/java/org/servo/servoshell/SettingsActivity.kt
+++ support/android/apk/servoapp/src/main/java/org/servo/servoshell/SettingsActivity.kt
@@ -12,6 +12,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
@@ -23,6 +25,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.core.content.edit
 import androidx.preference.PreferenceManager
@@ -38,6 +41,11 @@ class SettingsActivity : ComponentActivity() {
                     @OptIn(ExperimentalMaterial3Api::class)
                     TopAppBar(
                         title = { Text(stringResource(R.string.settings_title)) },
+                        navigationIcon = {
+                            IconButton(onClick = { finish() }) {
+                                Icon(painterResource(R.drawable.arrow_back), stringResource(R.string.back))
+                            }
+                        },
                     )
                 },
             ) { innerPadding ->
