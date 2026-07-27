2426cbef62a35574bed115a8e2c58e45e23f46d9
Budiman Arbenta <arbenta6@gmail.com>
2026-07-27 13:54:45 +0800

    Simplify text input unbind_from_tree
    Signed-off-by: Budiman Arbenta <arbenta6@gmail.com>
    Signed-off-by: Budiman Arbenta <50914575+BudiArb@users.noreply.github.com>

--- components/script/dom/html/htmltextareaelement.rs
+++ components/script/dom/html/htmltextareaelement.rs
@@ -733,15 +733,9 @@ impl VirtualMethods for HTMLTextAreaElement {
 
     fn unbind_from_tree(&self, cx: &mut JSContext, context: &UnbindContext) {
         // We make sure that when currently focused textarea element is removed, keyboard is hidden
-        let doc = self.owner_document();
-        if doc
-            .focus_handler()
-            .focused_area()
-            .element()
-            .is_some_and(|e| e == self.upcast::<Element>())
-        {
-            doc.embedder_controls().hide_embedder_control(self.upcast());
-        }
+        self.owner_document()
+            .embedder_controls()
+            .hide_embedder_control(self.upcast());
 
         self.super_type().unwrap().unbind_from_tree(cx, context);
 
