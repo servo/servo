a281db7eaf755be5a3f7afda7c43a1857d605fdb
Euclid Ye <yezhizhenjiakang@gmail.com>
2026-07-29 11:34:24 +0800

    refactor datatransferitem
    Signed-off-by: Euclid Ye <yezhizhenjiakang@gmail.com>

--- components/script/dom/datatransfer/datatransferitem.rs
+++ components/script/dom/datatransfer/datatransferitem.rs
@@ -130,9 +130,10 @@ impl DataTransferItemMethods<crate::DomTypeHolder> for DataTransferItem {
                 .task_manager()
                 .dom_manipulation_task_source()
                 .queue(task!(invoke_callback: move |cx| {
-                    let maybe_index = this.root().pending_callbacks.borrow().iter().position(|val| val.id == id);
+                    let this = this.root();
+                    let maybe_index = this.pending_callbacks.borrow().iter().position(|val| val.id == id);
                     if let Some(index) = maybe_index {
-                        let callback = this.root().pending_callbacks.borrow_mut().swap_remove(index).callback;
+                        let callback = this.pending_callbacks.safe_borrow_mut(cx.no_gc()).swap_remove(index).callback;
                         let _ = callback.Call__(cx, DOMString::from(string), ExceptionHandling::Report);
                     }
                 }));
