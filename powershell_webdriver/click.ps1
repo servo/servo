91ca240343a8826ea90f7d1d93f0dd748a1d0932
Euclid Ye <yezhizhenjiakang@gmail.com>
2026-07-29 11:07:38 +0800

    rm click.ps1
    Signed-off-by: Euclid Ye <yezhizhenjiakang@gmail.com>

--- powershell_webdriver/click.ps1
+++
@@ -1,6 +0,0 @@
-$body = '{}'
-$response_load = Invoke-RestMethod -Method Post `
-                              -Uri "http://127.0.0.1:7000/session/${SESSIONID}/element/${elementID}/click" `
-                              -Body $body `
-                              -ContentType "application/json" `
-                              -Verbose
