9345fe8aec8eef402ed947e3275e7955f87b7780
Jonathan Schwender <schwenderjonathan@gmail.com>
2026-08-02 19:56:25 +0200

    handle asan
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>

--- python/servo/build_commands.py
+++ python/servo/build_commands.py
@@ -53,6 +53,18 @@ SUPPORTED_TSAN_TARGETS = [
     "x86_64-unknown-linux-gnu",
 ]
 
+# Should be kept in sync with servoshell default features (best-effort).
+ASAN_SERVOSHELL_DEFAULT_FEATURES = [
+    "baked-in-resources",
+    "bundled_freetype",
+    "gamepad",
+    "servo/clipboard",
+    "js_jit",
+    "max_log_level",
+    "webgpu",
+    "webxr",
+]
+
 
 def get_rustc_llvm_version() -> Optional[list[int]]:
     """Determine the LLVM version of `rustc` and return it as a List[major, minor, patch, ...]
@@ -303,6 +315,9 @@ class MachCommands(CommandBase):
 
             # asan replaces system allocator with asan allocator
             # we need to make sure that we do not replace it with jemalloc
+            if "--no-default-features" not in opts:
+                opts += ["--no-default-features"]
+                self.features.extend(ASAN_SERVOSHELL_DEFAULT_FEATURES)
             self.features.append("servo-allocator/use-system-allocator")
         elif sanitizer.is_tsan():
             if target_triple not in SUPPORTED_TSAN_TARGETS:
