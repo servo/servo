a7a5eff507bf4054c93020d12535ecd80b810087
Tim van der Lippe <TimvdLippe@users.noreply.github.com>
2026-08-05 10:21:40 +0200

    script: Move form controls into form_controls (#46989)
    Testing: it compiles
    Part of #46329
    
    Signed-off-by: Tim van der Lippe <tvanderlippe@gmail.com>

--- components/script/dom/html/form_controls/mod.rs
+++ components/script/dom/html/form_controls/mod.rs
@@ -2,7 +2,20 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+pub(crate) mod htmlbuttonelement;
+pub(crate) mod htmldatalistelement;
+pub(crate) mod htmlfieldsetelement;
+pub(crate) mod htmlformelement;
 pub(crate) mod htmlinputelement;
+pub(crate) mod htmllabelelement;
+pub(crate) mod htmllegendelement;
+pub(crate) mod htmlmeterelement;
+pub(crate) mod htmloptgroupelement;
+pub(crate) mod htmloptionelement;
+pub(crate) mod htmloutputelement;
+pub(crate) mod htmlprogresselement;
+pub(crate) mod htmlselectelement;
+pub(crate) mod htmltextareaelement;
 pub(crate) mod input_type;
 pub(crate) mod text_control;
 pub(crate) mod text_input;
