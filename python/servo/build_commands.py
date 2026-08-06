d9349d02eac0a7f99c67c8e6ed308c0c07abc248
Jonathan Schwender <schwenderjonathan@gmail.com>
2026-08-06 14:19:00 +0200

    fix typo
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>

--- python/servo/build_commands.py
+++ python/servo/build_commands.py
@@ -310,7 +310,7 @@ class MachCommands(CommandBase):
                 print(
                     "Info: Explicit `--no-default-features` passed in an ASAN build."
                     " Skipping `mach` override of default features. If you are not sure what"
-                    " you are doing, consider removing `--no-default-features from your invocation."
+                    " you are doing, consider removing `--no-default-features` from your invocation."
                 )
         elif sanitizer.is_tsan():
             if target_triple not in SUPPORTED_TSAN_TARGETS:
