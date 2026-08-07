4a1aa7cf0fa5b54b7d4512263a07a8dad72b8097
Amit Anilkumar <aanilkumar30@gatech.edu>
2026-08-07 02:00:46 -0700

    script: Add CompletionValue enum for classic-script rval handling
    Use CompletionValue::Discarded/Needed instead of bare no_script_rval bool. Store on ClassicScript and debug_assert when rval is passed at run time.
    
    Signed-off-by: Amit Anilkumar <aanilkumar30@gatech.edu>

--- components/script/dom/serviceworker/serviceworkerglobalscope.rs
+++ components/script/dom/serviceworker/serviceworkerglobalscope.rs
@@ -53,7 +53,7 @@ use crate::dom::eventtarget::EventTarget;
 use crate::dom::extendableevent::ExtendableEvent;
 use crate::dom::extendablemessageevent::ExtendableMessageEvent;
 use crate::dom::globalscope::GlobalScope;
-use crate::dom::globalscope::script_execution::{ErrorReporting, RethrowErrors};
+use crate::dom::globalscope::script_execution::{CompletionValue, ErrorReporting, RethrowErrors};
 #[cfg(feature = "webgpu")]
 use crate::dom::webgpu::identityhub::IdentityHub;
 use crate::dom::worker::TrustedWorkerAddress;
@@ -475,9 +475,14 @@ impl ServiceWorkerGlobalScope {
                         Some(IntroductionType::WORKER),
                         1,
                         true,
-                        true,
+                        CompletionValue::Discarded,
                     );
-                    _ = global_scope.run_a_classic_script(&mut realm, script, RethrowErrors::No, None);
+                    _ = global_scope.run_a_classic_script(
+                        &mut realm,
+                        script,
+                        RethrowErrors::No,
+                        None,
+                    );
                     global.dispatch_activate(&mut realm);
                 }
 
