66009a1700f313a08343d838c99f6c6d2d71212a
Euclid Ye <yezhizhenjiakang@gmail.com>
2026-08-07 22:43:25 +0800

    readEntries
    Signed-off-by: Euclid Ye <yezhizhenjiakang@gmail.com>

--- components/script/dom/filesystem/filesystemdirectoryreader.rs
+++ components/script/dom/filesystem/filesystemdirectoryreader.rs
@@ -7,12 +7,16 @@ use std::rc::Rc;
 
 use dom_struct::dom_struct;
 use js::context::JSContext;
+use script_bindings::cell::DomRefCell;
 use script_bindings::reflector::{Reflector, reflect_dom_object_with_cx};
 
+use crate::dom::bindings::callback::ExceptionHandling;
 use crate::dom::bindings::codegen::Bindings::FileSystemDirectoryReaderBinding::{
     FileSystemDirectoryReaderMethods, FileSystemEntriesCallback,
 };
 use crate::dom::bindings::codegen::Bindings::FileSystemEntryBinding::ErrorCallback;
+use crate::dom::bindings::refcounted::Trusted;
+use crate::dom::bindings::reflector::DomGlobal;
 use crate::dom::bindings::root::{Dom, DomRoot};
 use crate::dom::filesystemdirectoryentry::FileSystemDirectoryEntry;
 use crate::dom::globalscope::GlobalScope;
@@ -24,6 +28,15 @@ pub(crate) struct FileSystemDirectoryReader {
     idx: Cell<usize>,
     reading_flag: Cell<bool>,
     done_flag: Cell<bool>,
+    pending_callbacks: DomRefCell<Vec<PendingEntriesCallback>>,
+    next_callback: Cell<usize>,
+}
+
+#[derive(JSTraceable, MallocSizeOf)]
+struct PendingEntriesCallback {
+    id: usize,
+    #[conditional_malloc_size_of]
+    callback: Rc<FileSystemEntriesCallback>,
 }
 
 impl FileSystemDirectoryReader {
@@ -34,6 +47,8 @@ impl FileSystemDirectoryReader {
             idx: Cell::new(0),
             reading_flag: Cell::new(false),
             done_flag: Cell::new(false),
+            pending_callbacks: Default::default(),
+            next_callback: Cell::new(0),
         }
     }
 
@@ -54,9 +69,46 @@ impl FileSystemDirectoryReaderMethods<crate::DomTypeHolder> for FileSystemDirect
     /// <https://wicg.github.io/entries-api/#dom-filesystemdirectoryreader-readentries>
     fn ReadEntries(
         &self,
-        _success_callback: Rc<FileSystemEntriesCallback>,
+        success_callback: Rc<FileSystemEntriesCallback>,
         _error_callback: Option<Rc<ErrorCallback>>,
     ) {
-        // TODO: Implement per spec 7.3. Need to hook embedder.
+        // Per spec §7.3: queue a task to invoke successCallback with the
+        // directory's children that have not yet been produced. The first
+        // call returns all children; subsequent calls return an empty list
+        // (done flag set).
+        let id = self.next_callback.get();
+        let pending_callback = PendingEntriesCallback {
+            id,
+            callback: success_callback,
+        };
+        self.pending_callbacks.borrow_mut().push(pending_callback);
+        self.next_callback.set(id + 1);
+
+        let this = Trusted::new(self);
+        self.global()
+            .task_manager()
+            .dom_manipulation_task_source()
+            .queue(task!(invoke_read_entries: move |cx| {
+                let this = this.root();
+                let maybe_index = this
+                    .pending_callbacks
+                    .borrow()
+                    .iter()
+                    .position(|val| val.id == id);
+                if let Some(index) = maybe_index {
+                    let callback = this
+                        .pending_callbacks
+                        .borrow_mut()
+                        .swap_remove(index)
+                        .callback;
+                    let entries = if this.done_flag.get() {
+                        Vec::new()
+                    } else {
+                        this.done_flag.set(true);
+                        this.dir.children()
+                    };
+                    let _ = callback.Call__(cx, entries, ExceptionHandling::Report);
+                }
+            }));
     }
 }
