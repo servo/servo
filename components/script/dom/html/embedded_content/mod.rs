f640ca3c3a854a50c4904e77ab3dac991e6eecc3
Tim van der Lippe <tvanderlippe@gmail.com>
2026-08-07 10:35:21 +0200

    script: Move HTML elements into `embedded_content/`
    Part of #46329
    
    Testing: it compiles
    
    Signed-off-by: Tim van der Lippe <tvanderlippe@gmail.com>

---
+++ components/script/dom/html/embedded_content/mod.rs
@@ -0,0 +1,17 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+pub(crate) mod htmlareaelement;
+pub(crate) mod htmlaudioelement;
+pub(crate) mod htmlcanvaselement;
+pub(crate) mod htmlembedelement;
+pub(crate) mod htmlframeelement;
+pub(crate) mod htmlframesetelement;
+pub(crate) mod htmliframeelement;
+pub(crate) mod htmlimageelement;
+pub(crate) mod htmlmapelement;
+pub(crate) mod htmlmediaelement;
+pub(crate) mod htmlobjectelement;
+pub(crate) mod htmltrackelement;
+pub(crate) mod htmlvideoelement;
