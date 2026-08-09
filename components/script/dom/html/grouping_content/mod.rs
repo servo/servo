7595af025e830feb67e6e118b946d6650ca3470d
Tim van der Lippe <TimvdLippe@users.noreply.github.com>
2026-08-09 12:55:57 +0200

    script: Move HTML elements to `grouping_content/` (#47091)
    Part of #46329
    
    Testing: it compiles
    
    Signed-off-by: Tim van der Lippe <tvanderlippe@gmail.com>

---
+++ components/script/dom/html/grouping_content/mod.rs
@@ -0,0 +1,14 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+pub(crate) mod htmldataelement;
+pub(crate) mod htmldivelement;
+pub(crate) mod htmldlistelement;
+pub(crate) mod htmlhrelement;
+pub(crate) mod htmllielement;
+pub(crate) mod htmlolistelement;
+pub(crate) mod htmlparagraphelement;
+pub(crate) mod htmlpreelement;
+pub(crate) mod htmlspanelement;
+pub(crate) mod htmlulistelement;
