629ff0e901bfa5c21ae98c961039c6381c06297b
Simon Sapin <simon@igalia.com>
2026-08-11 18:01:28 +0200

    License
    Signed-off-by: Simon Sapin <simon@igalia.com>

--- components/layout/flow/inline/small_kana.rs
+++ components/layout/flow/inline/small_kana.rs
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 /// <https://drafts.csswg.org/css-text-4/#small-kana>
 pub(crate) static SMALL_KANA_MAPPINGS: phf::Map<char, char> = phf::phf_map! {
     '\u{3041}' => '\u{3042}', // ぁ → あ
