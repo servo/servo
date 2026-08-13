6ca1eef3a3e5a5a58ef2b2d14421a858cd88fa49
Asset Malik <5373400+janeoa@users.noreply.github.com>
2026-08-13 11:46:57 +0200

    wip: More feature guards
    Signed-off-by: Asset Malik <5373400+janeoa@users.noreply.github.com>

--- components/net/http_loader.rs
+++ components/net/http_loader.rs
@@ -656,7 +656,7 @@ async fn obtain_response(
             let send_end = CrossProcessInstant::now();
 
             // TODO(#21271) response_start: immediately after receiving first byte of response
-
+            #[cfg(feature = "devtools")]
             let msg = if let Some(request_id) = request_id {
                 if let Some(pipeline_id) = pipeline_id {
                     if let Some(browsing_context_id) = browsing_context_id {
@@ -692,7 +692,10 @@ async fn obtain_response(
 
             future::ready(Ok((
                 Decoder::detect(res.map(|r| r.boxed()), is_secure_scheme),
+                #[cfg(feature = "devtools")]
                 msg,
+                #[cfg(not(feature = "devtools"))]
+                ()
             )))
         })
         .map_err(move |error| {
--- components/net/resource_thread.rs
+++ components/net/resource_thread.rs
@@ -91,8 +91,7 @@ fn load_root_cert_store_from_file(file_path: String) -> io::Result<Vec<Certifica
 /// Returns a tuple of (public, private) senders to the new threads.
 #[expect(clippy::too_many_arguments)]
 pub fn new_resource_threads(
-    #[cfg(feature = "devtools")]
-    devtools_sender: Option<Sender<DevtoolsControlMsg>>,
+    #[cfg(feature = "devtools")] devtools_sender: Option<Sender<DevtoolsControlMsg>>,
     time_profiler_chan: ProfilerChan,
     mem_profiler_chan: MemProfilerChan,
     embedder_proxy: GenericEmbedderProxy<NetToEmbedderMsg>,
@@ -133,8 +132,7 @@ pub fn new_resource_threads(
 /// Create a CoreResourceThread
 #[expect(clippy::too_many_arguments)]
 pub fn new_core_resource_thread(
-    #[cfg(feature = "devtools")]
-    devtools_sender: Option<Sender<DevtoolsControlMsg>>,
+    #[cfg(feature = "devtools")] devtools_sender: Option<Sender<DevtoolsControlMsg>>,
     time_profiler_chan: ProfilerChan,
     mem_profiler_chan: MemProfilerChan,
     embedder_proxy: GenericEmbedderProxy<NetToEmbedderMsg>,
@@ -728,8 +726,7 @@ pub struct CoreResourceManager {
 
 impl CoreResourceManager {
     pub fn new(
-        #[cfg(feature = "devtools")]
-        devtools_sender: Option<Sender<DevtoolsControlMsg>>,
+        #[cfg(feature = "devtools")] devtools_sender: Option<Sender<DevtoolsControlMsg>>,
         _profiler_chan: ProfilerChan,
         embedder_proxy: GenericEmbedderProxy<NetToEmbedderMsg>,
         ca_certificates: CACertificates<'static>,
