f2e9a67ada99313c8ceea906e63b6612638cc21f
Tim van der Lippe <tvanderlippe@gmail.com>
2026-08-20 20:50:13 +0200

    script: Create remaining folders in `script/dom/html`
    These are the last folders that were initially listed in #46329
    There are still a couple of files left that will be moved
    to their respective location in a follow-up, since they
    all end up in different places.
    
    Part of #46329
    
    Testing: it compiles
    
    Signed-off-by: Tim van der Lippe <tvanderlippe@gmail.com>

---
+++ components/script/dom/html/document_structure/mod.rs
@@ -0,0 +1,8 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+pub(crate) mod htmlbodyelement;
+pub(crate) mod htmldocument;
+pub(crate) mod htmlheadelement;
+pub(crate) mod htmlhtmlelement;
