1fd1e0d613ad935e161135ad0b36545f0a784af1
Simon Wülker <simon.wuelker@arcor.de>
2026-08-22 17:45:56 +0200

    fonts: Resolve ties during web font matching by cascade order of @font-face rules
    Signed-off-by: Simon Wülker <simon.wuelker@arcor.de>

--- components/script/dom/window/window.rs
+++ components/script/dom/window/window.rs
@@ -98,7 +98,6 @@ use style::error_reporting::{ContextualParseError, ParseErrorReporter};
 use style::properties::PropertyId;
 use style::properties::style_structs::Font;
 use style::selector_parser::PseudoElement;
-use style::shared_lock::StylesheetGuards;
 use style::str::HTML_SPACE_CHARACTERS;
 use style::stylesheets::UrlExtraData;
 use style_traits::CSSPixel;
@@ -3720,14 +3719,8 @@ impl Window {
         if !changed_web_fonts.added_font_faces.is_empty() {
             fonts.switch_to_loading(cx);
 
-            let shared_locks = document.shared_style_locks();
-            let guards = StylesheetGuards {
-                author: &shared_locks.author.read(),
-                ua_or_user: &shared_locks.ua_or_user.read(),
-            };
             for new_web_font in changed_web_fonts.added_font_faces {
-                if let Some(font_face) =
-                    FontFace::new_for_web_font(cx, self.upcast(), new_web_font, &guards)
+                if let Some(font_face) = FontFace::new_for_web_font(cx, self.upcast(), new_web_font)
                 {
                     fonts.add(cx, font_face);
                 }
