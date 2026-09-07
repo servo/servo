5e14abb0fef9310fb27804bdc0f29860c30d26b6
veyndan <veyndan@gmail.com>
2026-09-07 22:44:54 +0200

    android: Move key event handling out of `ServoView`
    Signed-off-by: veyndan <veyndan@gmail.com>

--- support/android/apk/servoview/src/main/java/org/servo/servoview/ServoView.kt
+++ support/android/apk/servoview/src/main/java/org/servo/servoview/ServoView.kt
@@ -12,7 +12,6 @@ import android.os.Looper
 import android.util.Log
 import android.util.Size
 import android.view.Choreographer
-import android.view.KeyEvent
 import android.view.MotionEvent
 import android.view.SurfaceHolder
 import android.view.SurfaceView
@@ -28,7 +27,7 @@ class ServoView(
     navigator: ServoNavigator,
 ) : SurfaceView(context), Servo.RunCallback, Choreographer.FrameCallback {
     private val glThread = GLThread().apply { start() }
-    private val servo =
+    internal val servo =
         Servo(
             servoArgs,
             initialUri,
@@ -43,7 +42,6 @@ class ServoView(
     init {
         isFocusable = true
         isFocusableInTouchMode = true
-        isClickable = true
         addTouchables(arrayListOf(this))
         val surfaceHolderCallback = SurfaceHolderCallback(servoView = this)
         holder.addCallback(surfaceHolderCallback)
@@ -55,22 +53,6 @@ class ServoView(
 
     override fun inUIThread(r: Runnable) {
         post(r)
-    }
-
-    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
-        if (event.keyCode != KeyEvent.KEYCODE_BACK) {
-            servo.onKeyDown(keyCode, event)
-            return true
-        }
-        return false
-    }
-
-    override fun onKeyUp(keyCode: Int, event: KeyEvent): Boolean {
-        if (event.keyCode != KeyEvent.KEYCODE_BACK) {
-            servo.onKeyUp(keyCode, event)
-            return true
-        }
-        return false
     }
 
     override fun onTouchEvent(motionEvent: MotionEvent): Boolean {
