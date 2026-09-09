784d59547643228e23ea013e910decb4975c162b
sagudev <16504129+sagudev@users.noreply.github.com>
2026-09-09 11:21:16 +0200

    f
    Signed-off-by: sagudev <16504129+sagudev@users.noreply.github.com>

--- components/script/dom/webgl/webglframebuffer.rs
+++ components/script/dom/webgl/webglframebuffer.rs
@@ -144,7 +144,7 @@ impl Drop for DroppableWebGLFramebuffer {
     }
 }
 
-#[dom_struct] // actual memory usage is reported in WebGLFramebufferAttachment
+#[dom_struct]
 pub(crate) struct WebGLFramebuffer {
     webgl_object: WebGLObject,
     #[no_trace]
