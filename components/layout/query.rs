ecbf05c0a4e28a7dbfb1dffe9cddd24b8e7d77a2
Alice Boxhall <alice@igalia.com>
2026-09-10 11:50:11 +0200

    Make `include_transforms` and `include_inlines` into a bitflags!
    Signed-off-by: Alice Boxhall <alice@igalia.com>

--- components/layout/query.rs
+++ components/layout/query.rs
@@ -10,6 +10,7 @@ use std::rc::Rc;
 use std::sync::Arc;
 
 use app_units::Au;
+use bitflags::bitflags;
 use embedder_traits::UntrustedNodeAddress;
 use euclid::{Point2D, Rect, Size2D};
 use layout_api::{
@@ -90,13 +91,20 @@ pub(crate) fn process_padding_request(node: ServoLayoutNode<'_>) -> Option<Physi
     )
 }
 
+bitflags! {
+    #[derive(Clone, Copy)]
+    pub(crate) struct BoxAreaInclusion: u8 {
+        const Transforms = 1 << 0;
+        const Inlines = 1 << 1;
+    }
+}
+
 pub(crate) fn process_box_area_request(
     layout_thread: &LayoutThread,
     stacking_context_tree: &StackingContextTree,
     node: ServoLayoutNode<'_>,
     area: BoxAreaType,
-    include_inline: bool,
-    include_transform: bool,
+    inclusion: BoxAreaInclusion,
 ) -> Option<Rect<Au, CSSPixel>> {
     // Borrow fragments to avoid cloning on this hot path for accessibility and
     // `getBoundingClientRect()`.
@@ -104,7 +112,7 @@ pub(crate) fn process_box_area_request(
         let mut rects = fragments
             .iter()
             .filter(|fragment| {
-                include_inline ||
+                inclusion.contains(BoxAreaInclusion::Inlines) ||
                     !fragment
                         .retrieve_box_fragment()
                         .is_none_or(|fragment| !fragment.with_style().is_inline_box())
@@ -115,7 +123,7 @@ pub(crate) fn process_box_area_request(
         rects.peek()?;
         let rect_union = rects.fold(Rect::zero(), |unioned_rect, rect| rect.union(&unioned_rect));
 
-        if !include_transform {
+        if !inclusion.contains(BoxAreaInclusion::Transforms) {
             return Some(rect_union);
         }
 
