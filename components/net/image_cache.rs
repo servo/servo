34cb5df922515f4972137052a56a5b5c74d506c0
Asset Malik <5373400+janeoa@users.noreply.github.com>
2026-09-10 18:42:30 +0200

    wip: mach fmt
    Signed-off-by: Asset Malik <5373400+janeoa@users.noreply.github.com>

--- components/net/image_cache.rs
+++ components/net/image_cache.rs
@@ -18,7 +18,7 @@ use mime::Mime;
 use net_traits::image_cache::{
     EncodedImage, FontResolver, Image, ImageCache, ImageCacheFactory, ImageCacheResponseCallback,
     ImageCacheResponseMessage, ImageCacheResult, ImageLoadListener, ImageOrMetadataAvailable,
-    ImageResponse, PendingImageId, RasterizationCompleteResponse, RasterDecodeDemandStatus,
+    ImageResponse, PendingImageId, RasterDecodeDemandStatus, RasterizationCompleteResponse,
     VectorImage,
 };
 use net_traits::request::CorsSettings;
@@ -1153,7 +1153,10 @@ impl ImageCache for ImageCacheImpl {
         }
     }
 
-    fn demand_driven_raster_image_key(&self, image_id: PendingImageId) -> Option<WebRenderImageKey> {
+    fn demand_driven_raster_image_key(
+        &self,
+        image_id: PendingImageId,
+    ) -> Option<WebRenderImageKey> {
         self.store
             .lock()
             .encoded_raster_images
