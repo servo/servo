1100fabefb417dfb3c87f1b8574577a5b852977e
Gae24 <96017547+Gae24@users.noreply.github.com>
2026-09-13 05:55:31 +0000

    backport: script: Remove `LoadState`'s `error_to_rethrow` (#47913)
    We already have access to the error to rethrow inside
    `on_load_requested_modules_rejected`, removing the need to keep a copy
    of it in `error_to_rethrow`. The specification behaviour is replicated
    by passing an undefined value to `FinishLoadingImportedModuleFailed`
    when `state.[[ErrorToRethrow]]` should be unset, unless the module
    loading request was started by a dynamic import which still needs a
    `TypeError`.
    
    As a result `host_defined` value is not captured anymore by
    `on_single_fetch_complete`.
    
    Testing: Covered by existing tests
    
    ---------
    
    Signed-off-by: Gae24 <96017547+Gae24@users.noreply.github.com>
    (cherry picked from commit 2c5b68f9205ceb914c85ea76543b0bc19aea7149)
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>

--- components/script/modules/script_module.rs
+++ components/script/modules/script_module.rs
@@ -6,7 +6,7 @@
 //! related to `type=module` for script thread or worker threads.
 
 use std::borrow::Cow;
-use std::cell::{OnceCell, RefCell};
+use std::cell::OnceCell;
 use std::collections::hash_map::Entry;
 use std::ffi::CStr;
 use std::fmt::Debug;
@@ -53,7 +53,7 @@ use script_bindings::trace::CustomTraceable;
 use servo_config::pref;
 use servo_url::ServoUrl;
 
-use crate::dom::bindings::error::{Error, ErrorToJsval, report_pending_exception};
+use crate::dom::bindings::error::{Error, report_pending_exception, throw_dom_exception};
 use crate::dom::bindings::inheritance::Castable;
 use crate::dom::bindings::refcounted::Trusted;
 use crate::dom::bindings::root::DomRoot;
@@ -78,17 +78,6 @@ use crate::realms::enter_auto_realm;
 use crate::runtime::script_runtime::IntroductionType;
 use crate::tasks::task::NonSendTaskBox;
 use crate::unminify::{ScriptSource, unminify_js};
-
-pub(crate) fn gen_type_error(
-    cx: &mut JSContext,
-    global: &GlobalScope,
-    error: Error,
-) -> RethrowError {
-    rooted!(&in(cx) let mut thrown = UndefinedValue());
-    error.to_jsval(cx, global, thrown.handle_mut());
-
-    RethrowError(RootedTraceableBox::from_box(Heap::boxed(thrown.get())))
-}
 
 #[derive(JSTraceable)]
 pub(crate) struct ModuleObject(RootedTraceableBox<Heap<*mut JSObject>>);
@@ -1012,15 +1001,7 @@ unsafe extern "C" fn import_meta_resolve(cx: *mut RawJSContext, argc: u32, vp: *
             true
         },
         Err(error) => {
-            let resolution_error = gen_type_error(cx, &global_scope, error);
-
-            unsafe {
-                JS_SetPendingException(
-                    cx,
-                    resolution_error.handle(),
-                    ExceptionStackBehavior::Capture,
-                );
-            }
+            throw_dom_exception(cx, &global_scope, error);
             false
         },
     }
@@ -1254,7 +1235,6 @@ fn fetch_the_descendants_and_link_module_script(
     // Step 3. Let state be Record
     // { [[ErrorToRethrow]]: null, [[Destination]]: destination, [[PerformFetch]]: null, [[FetchClient]]: fetchClient }.
     let state = Box::new(LoadState {
-        error_to_rethrow: RefCell::new(None),
         destination,
         fetch_client,
         module_script: DomRefCell::new(Some(module_script.clone())),
