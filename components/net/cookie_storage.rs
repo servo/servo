6a5a57b27dc85bdb97dbee34440e043e15020e33
Euclid Ye <yezhizhenjiakang@gmail.com>
2026-09-14 10:00:17 +0800

    debug_assert
    Signed-off-by: Euclid Ye <yezhizhenjiakang@gmail.com>

--- components/net/cookie_storage.rs
+++ components/net/cookie_storage.rs
@@ -305,6 +305,7 @@ fn reg_host(url: &str) -> String {
         .and_then(|url| url.strip_suffix(']'))
         .unwrap_or(url);
     if let Ok(address) = host_for_ip_parse.parse::<IpAddr>() {
+        debug_assert!(address.to_string() == address.to_string().to_lowercase());
         return address.to_string();
     }
 
