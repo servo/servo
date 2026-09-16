cc8272881363793d4a5ab0b77dc514c7a72e9deb
Narfinger <Narfinger@users.noreply.github.com>
2026-09-16 19:26:58 +0000

    script_bindings: Move buffer_source to script_bindings (#48132)
    This PR moves buffer_source file from script to script_bindings.
    
    Testing: This is just a refactor.
    
    Signed-off-by: Narfinger <Narfinger@users.noreply.github.com>

--- components/script/dom/bindings/mod.rs
+++ components/script/dom/bindings/mod.rs
@@ -134,7 +134,13 @@
 #![deny(missing_docs)]
 #![deny(non_snake_case)]
 
-pub(crate) mod buffer_source;
+pub(crate) mod buffer_source {
+    pub(crate) use script_bindings::buffer_source::{
+        Constructor, HeapBufferSource, byte_size, create_array_buffer_with_size,
+        create_buffer_source, create_buffer_source_with_constructor,
+        create_heap_buffer_source_with_length, get_buffer_source_copy, get_buffer_source_slice,
+    };
+}
 pub(crate) mod cell;
 pub(crate) mod constructor;
 pub(crate) mod conversions;
