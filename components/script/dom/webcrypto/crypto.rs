1cbeef1f58784c3f41b2a10ef121a7775d684191
Kingsley Yung <kingsley@kkoyung.dev>
2026-09-25 15:02:02 +0800

    script: Refactoring `Crypto` interface
    - Add specification URL of the `Crypto` interface.
    - Add specification text of steps.
    - Replace `reflect_dom_object_with_cx` with `reflect_dom_object`.
    - Remove unnecessary `js::context::` prefix.
    - Re-write `Crypto.getRandomValues` to align with the specification.
    - Add the missing error message in step 1 of `Crypto.getRandomValues`.
    - Use `rand::fill`, instead of `SysRng`, to improve performance.
    - Eliminate unsafe code in step 7 of `Crypto.getRandomValues`.
    
    Testing: Refactoring. Existing tests suffice.
    
    Signed-off-by: Kingsley Yung <kingsley@kkoyung.dev>

--- components/script/dom/webcrypto/crypto.rs
+++ components/script/dom/webcrypto/crypto.rs
@@ -3,13 +3,12 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 use dom_struct::dom_struct;
-use js::context::{JSContext, NoGC};
-use js::jsapi::{Heap, JSObject, Type};
+use js::context::JSContext;
+use js::jsapi::Type;
 use js::rust::CustomAutoRooterGuard;
-use js::typedarray::{ArrayBufferView, ArrayBufferViewU8, HeapArrayBufferView, TypedArray};
-use rand::TryRng;
-use rand::rngs::SysRng;
-use script_bindings::reflector::{Reflector, reflect_dom_object_with_cx};
+use js::typedarray::{ArrayBufferView, HeapArrayBufferView};
+use script_bindings::buffer_source::HeapBufferSource;
+use script_bindings::reflector::{Reflector, reflect_dom_object};
 use script_bindings::trace::RootedTraceableBox;
 use uuid::Uuid;
 
@@ -21,7 +20,7 @@ use crate::dom::bindings::str::DOMString;
 use crate::dom::globalscope::GlobalScope;
 use crate::dom::subtlecrypto::SubtleCrypto;
 
-// https://developer.mozilla.org/en-US/docs/Web/API/Crypto
+/// <https://w3c.github.io/webcrypto/#crypto-interface>
 #[dom_struct]
 pub(crate) struct Crypto {
     reflector_: Reflector,
@@ -37,71 +36,90 @@ impl Crypto {
     }
 
     pub(crate) fn new(cx: &mut JSContext, global: &GlobalScope) -> DomRoot<Crypto> {
-        reflect_dom_object_with_cx(Box::new(Crypto::new_inherited()), global, cx)
+        reflect_dom_object(cx, Box::new(Crypto::new_inherited()), global)
     }
 }
 
 impl CryptoMethods<crate::DomTypeHolder> for Crypto {
     /// <https://w3c.github.io/webcrypto/#dfn-Crypto-attribute-subtle>
-    fn Subtle(&self, cx: &mut js::context::JSContext) -> DomRoot<SubtleCrypto> {
+    fn Subtle(&self, cx: &mut JSContext) -> DomRoot<SubtleCrypto> {
         self.subtle
             .or_init(|| SubtleCrypto::new(cx, &self.global()))
     }
 
-    #[expect(unsafe_code)]
     /// <https://w3c.github.io/webcrypto/#Crypto-method-getRandomValues>
     fn GetRandomValues(
         &self,
-        no_gc: &NoGC,
-        mut input: CustomAutoRooterGuard<ArrayBufferView>,
+        cx: &mut JSContext,
+        mut array: CustomAutoRooterGuard<ArrayBufferView>,
     ) -> Fallible<RootedTraceableBox<HeapArrayBufferView>> {
-        let array_type = input.get_array_type();
-
-        if !is_integer_buffer(array_type) {
-            Err(Error::TypeMismatch(None))
-        } else {
-            let data = input.as_mut_slice_safe(no_gc).unwrap_or(&mut []);
-            if data.len() > 65536 {
-                return Err(Error::QuotaExceeded {
-                    quota: None,
-                    requested: None,
-                });
-            }
-
-            if SysRng.try_fill_bytes(data).is_err() {
-                return Err(Error::Operation(Some(
-                    "Failed to generate random values".into(),
-                )));
-            }
-
-            let underlying_object = unsafe { input.underlying_object() };
-            TypedArray::<ArrayBufferViewU8, Box<Heap<*mut JSObject>>>::from(*underlying_object)
-                .map(RootedTraceableBox::new)
-                .map_err(|_| Error::JSFailed)
+        // Step 1. If array is not an Int8Array, Uint8Array, Uint8ClampedArray, Int16Array,
+        // Uint16Array, Int32Array, Uint32Array, BigInt64Array, or BigUint64Array, then throw a
+        // TypeMismatchError and terminate the algorithm.
+        if !matches!(
+            array.get_array_type(),
+            Type::Int8 |
+                Type::Uint8 |
+                Type::Uint8Clamped |
+                Type::Int16 |
+                Type::Uint16 |
+                Type::Int32 |
+                Type::Uint32 |
+                Type::BigInt64 |
+                Type::BigUint64
+        ) {
+            return Err(Error::TypeMismatch(Some(
+                "The array must be an integer-based TypedArray".into(),
+            )));
         }
+
+        // Step 2. Let byteLength be the byte length of array.
+        // Step 3. If byteLength is greater than 65536, throw a QuotaExceededError and terminate the
+        // algorithm.
+        // Step 4. Let bytes be a byte sequence of length byteLength.
+        let bytes = array.as_mut_slice_safe(cx.no_gc()).unwrap_or_default();
+        let byte_length = bytes.len();
+        if byte_length > 65536 {
+            return Err(Error::QuotaExceeded {
+                quota: None,
+                requested: None,
+            });
+        }
+
+        // Step 5. Fill bytes with cryptographically secure random bytes.
+        // Step 6. Write bytes into array.
+        rand::fill(bytes);
+
+        // Step 7. Return array.
+        let array = HeapBufferSource::from_view(cx, array);
+        array.get_typed_array().map_err(|_| Error::JSFailed)
     }
 
     /// <https://w3c.github.io/webcrypto/#Crypto-method-randomUUID>
     fn RandomUUID(&self) -> DOMString {
+        // Step 1. Let bytes be a byte sequence of length 16.
+        // Step 2. Fill bytes with cryptographically secure random bytes.
+        // Step 3. Set the 4 most significant bits of bytes[6], which represent the UUID version, to 0100.
+        // Step 4. Set the 2 most significant bits of bytes[8], which represent the UUID variant, to 10.
+        // Step 5.
+        // Return the string concatenation of «
+        //     hexadecimal representation of bytes[0], hexadecimal representation of bytes[1],
+        //     hexadecimal representation of bytes[2], hexadecimal representation of bytes[3],
+        //     "-",
+        //     hexadecimal representation of bytes[4], hexadecimal representation of bytes[5],
+        //     "-",
+        //     hexadecimal representation of bytes[6], hexadecimal representation of bytes[7],
+        //     "-",
+        //     hexadecimal representation of bytes[8], hexadecimal representation of bytes[9],
+        //     "-",
+        //     hexadecimal representation of bytes[10], hexadecimal representation of bytes[11],
+        //     hexadecimal representation of bytes[12], hexadecimal representation of bytes[13],
+        //     hexadecimal representation of bytes[14], hexadecimal representation of bytes[15]
+        // ».
         let uuid = Uuid::new_v4();
         uuid.hyphenated()
             .encode_lower(&mut Uuid::encode_buffer())
             .to_owned()
             .into()
     }
-}
-
-fn is_integer_buffer(array_type: Type) -> bool {
-    matches!(
-        array_type,
-        Type::Uint8 |
-            Type::Uint8Clamped |
-            Type::Int8 |
-            Type::Uint16 |
-            Type::Int16 |
-            Type::Uint32 |
-            Type::Int32 |
-            Type::BigInt64 |
-            Type::BigUint64
-    )
 }
