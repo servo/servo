cb8a5884744e89f659bcf6dd9a4b26e6f6f7a737
Josh Matthews <josh@joshmatthews.net>
2026-09-25 01:49:58 -0400

    Fix clippy.
    Signed-off-by: Josh Matthews <josh@joshmatthews.net>

--- components/script/dom/stream/countqueuingstrategy.rs
+++ components/script/dom/stream/countqueuingstrategy.rs
@@ -126,7 +126,7 @@ pub(crate) fn extract_size_algorithm(
         let fun_obj = native_raw_obj_fn!(cx, count_queuing_strategy_size, c"size", 0, 0);
         #[expect(unsafe_code)]
         unsafe {
-            return RootedCallback::from(QueuingStrategySize::new(cx, fun_obj));
+            return QueuingStrategySize::new(cx, fun_obj);
         };
     }
     strategy.size.as_ref().unwrap().root(cx)
